d14fe26bfd4b89b7e1967634035a510188e1ac21
Guangcong Luo <zarelsl@gmail.com>
2012-02-25 20:21:32 -0500

    Make Magic Coat/Bounce never bounce non-Status-category moves

--- movedex.js
+++ movedex.js
@@ -6173,7 +6173,7 @@ exports.BattleMovedex = {
 				if (target === source) return;
 				if (typeof move.isBounceable === 'undefined')
 				{
-					move.isBounceable = !!(move.status || move.volatileStatus === 'confusion' || move.forceSwitch);
+					move.isBounceable = !!(move.category === 'Status' && (move.status || move.volatileStatus === 'confusion' || move.forceSwitch));
 				}
 				if (move.target !== 'foeSide' && target !== this.effectData.target)
 				{
