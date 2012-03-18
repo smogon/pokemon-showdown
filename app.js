aaa28460e30b62ef7d5ecfb0911da47c7498c486
小太 <nospam.kotarou.dono@gmail.com>
2012-03-18 21:21:34 +1100

    Just found out about an easier way to do stack traces

--- app.js
+++ app.js
@@ -1379,50 +1379,10 @@ function resolveUser(you, socket)
 	return you.user;
 }
 
-// Function stack trace pilfered off http://helephant.com/2007/05/12/diy-javascript-stack-trace/
-Function.prototype.trace = function()
-{
-    var trace = [];
-    var current = this;
-    while (current)
-    {
-        console.log("Tracing back one call");
-        trace.push(current.signature());
-        current = current.caller;
-    }
-    return trace;
-}
-Function.prototype.signature = function()
-{
-    var signature = {
-        name: this.getName(),
-        params: [],
-        toString: function()
-        {
-            var params = this.params.length > 0 ? "'" + this.params.join("', '") + "'" : "";
-            return this.name + "(" + params + ")";
-        }
-    };
-    if (this.arguments)
-        for (var x=0; x < this.arguments.length; x++)
-            signature.params.push(this.arguments[x]);
-    return signature;
-}
-Function.prototype.getName = function()
-{
-    if (this.name)
-        return this.name;
-    var definition = this.toString().split("\n")[0];
-    var exp = /^function ([^\s(]+).+/;
-    if (exp.test(definition))
-        return definition.split("\n")[0].replace(exp, "$1") || "anonymous";
-    return "anonymous";
-}
-
 Exception = function(name)
 {
     this.name = name;
-    this.stack = name + ":\n    at " + arguments.callee.trace().join("\n    at ");
+    this.stack = name + ":\n" + new Error().stack;
     this.toString = function() { return name; }
 }
 
