fb98ebd011e32c5fba9fa98ca3c4705fdb1d8bb3
Bill Meltsner <bill@meltsner.com>
2012-03-28 03:23:29 -0500

    Include Ataraxia's avatar.

--- users.js
+++ users.js
@@ -338,7 +338,7 @@ function User(name, person, token)
 					if (userid === "serei") avatar = 172;
 					else if (userid === "hobsgoblin") avatar = 52;
 					else if (userid === "etherealsol") avatar = 1001;
-					else if (userid === "elitefourlorelei") avatar = 1002;
+					else if (userid === "ataraxia") avatar = 1002;
 					else if (userid === "verbatim") avatar = 1003;
 					else if (userid === "mortygymleader") avatar = 144;
 					else if (userid === "leadermorty") avatar = 144;
