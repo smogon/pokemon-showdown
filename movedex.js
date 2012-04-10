20e5d3a874018bbc200aaa2195c69b0f6fc43db6
Guangcong Luo <zarelsl@gmail.com>
2012-04-10 01:10:27 -0400

    Soak should be blocked by Substitute

--- movedex.js
+++ movedex.js
@@ -10918,14 +10918,9 @@ exports.BattleMovedex = {
 				if (move.category === 'Status')
 				{
 					var SubBlocked = {
-						acupressure:1, block:1, dreameater:1, embargo:1, entrainment:1, flatter:1, gastroacid:1, grudge:1, healblock:1, leechseed:1, lockon:1, meanlook:1, mindreader:1, nightmare:1, painsplit:1, psychoshift:1, spiderweb:1, sketch:1, swagger:1, switcheroo:1, trick:1, worryseed:1, yawn:1
+						acupressure:1, block:1, dreameater:1, embargo:1, entrainment:1, flatter:1, gastroacid:1, grudge:1, healblock:1, leechseed:1, lockon:1, meanlook:1, mindreader:1, nightmare:1, painsplit:1, psychoshift:1, spiderweb:1, sketch:1, swagger:1, switcheroo:1, trick:1, worryseed:1, yawn:1, soak: 1
 					};
-					if (move.status || move.boosts || move.volatileStatus === 'confusion')
-					{
-						this.add('-activate', target, 'move: Substitute', '[block] '+move);
-						return null;
-					}
-					if (SubBlocked[move.id])
+					if (move.status || move.boosts || move.volatileStatus === 'confusion' || SubBlocked[move.id])
 					{
 						this.add('-activate', target, 'move: Substitute', '[block] '+move);
 						return null;
