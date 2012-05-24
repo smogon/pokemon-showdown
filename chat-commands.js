10ee41a8f93fd94c35c12b074aa9b5aa68ecf8f7
Guangcong Luo <guangcongluo@gmail.com>
2012-05-24 00:57:08 -0700

    Nothing to see here, Nintendo lawyers

--- chat-commands.js
+++ chat-commands.js
@@ -831,14 +831,6 @@ function parseCommandLocal(user, cmd, target, room, socket, message) {
 		return false;
 		break;
 
-	case 'donate':
-	case '!donate':
-		showOrBroadcastStart(user, cmd, room, socket, message);
-		showOrBroadcast(user, cmd, room, socket,
-			'<form action="https://www.paypal.com/cgi-bin/webscr" method="post" style="height:47px" target="_blank"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="EFLLWDFF9ZNLG"><input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></form>');
-		return false;
-		break;
-
 	case 'groups':
 	case '!groups':
 		showOrBroadcastStart(user, cmd, room, socket, message);
