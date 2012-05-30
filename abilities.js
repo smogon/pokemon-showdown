586d17aed41e74b2001513c07f65c90655fd6638
Bill Meltsner <bill@meltsner.com>
2012-05-30 16:30:09 -0500

    Trace cannot copy Illusion.

--- abilities.js
+++ abilities.js
@@ -2257,7 +2257,7 @@ exports.BattleAbilities = {
 		onStart: function(pokemon) {
 			var target = pokemon.side.foe.randomActive();
 			var ability = this.getAbility(target.ability);
-			if (ability.id === 'trace') return;
+			if (ability.id === 'trace' || ability.id === 'illusion') return;
 			if (pokemon.setAbility(ability)) {
 				this.add('-ability',pokemon, ability,'[from] ability: Trace','[of] '+target);
 			}
