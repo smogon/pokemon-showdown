7499f3f81ca54541b496b6053ce49eecbcfc66c4
Bill Meltsner <bill@meltsner.com>
2012-06-02 15:49:33 -0500

    Limit the size of the server-side chatlog.
    We're now only keeping the last 100ish messages (100 plus any that arrived between updates). There's really no good reason to keep the entire thing in memory. This should also hopefully mitigate some of the lag issues.

--- app.js
+++ app.js
@@ -884,6 +884,7 @@ function Lobby(roomid) {
 	this.update = function(excludeUser) {
 		var update = selfR.getUpdate(selfR.lastUpdate, !selfR.usersChanged, !selfR.roomsChanged);
 		update.room = selfR.id;
+		if (selfR.log.length > 100) selfR.log = selfR.log.slice(-100);
 		selfR.lastUpdate = selfR.log.length;
 		for (var i in selfR.users) {
 			if (selfR.users[i] === excludeUser) continue;
