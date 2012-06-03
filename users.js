f0e134a0ac5f99c8eaf31e418388ca9b643537db
Guangcong Luo <guangcongluo@gmail.com>
2012-06-03 14:07:56 -0700

    Revert bmelts's security fix (This should actually be fixed in the login server)

--- users.js
+++ users.js
@@ -411,8 +411,6 @@ var User = (function () {
 							group = '@';
 							break;
 						}
-						userid = data.userid;
-						name = data.username;
 						/* var userdata = JSON.parse(body.userdata);
 						avatar = parseInt(userdata.trainersprite);
 						if (!avatar || avatar > 263 || avatar < 1) {
