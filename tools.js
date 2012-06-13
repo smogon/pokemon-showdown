ce48902ee77ca0a658bd8c78e044d1e1b0d5b710
Guangcong Luo <guangcongluo@gmail.com>
2012-06-13 08:27:32 -0700

    Make tools.js support mods (past gens) - replace Tools.foo with Tools.mod('gen4').foo for gen 4 tools - mods are stored in mods/modname/ in the same directory structure as data/ - mods modify base data - further documentation coming soon in mods/modding.md

--- tools.js
+++ tools.js
@@ -1,21 +1,70 @@
-function BattleTools() {
-	var selfT = this;
-	this.effectToString = function() {
+module.exports = (function () {
+	var dataTypes = ['Pokedex', 'Movedex', 'Statuses', 'TypeChart', 'Scripts', 'Items', 'Abilities', 'Formats', 'FormatsData', 'Learnsets', 'Aliases'];
+	function Tools(mod) {
+		if (!mod) mod = 'base';
+		this.currentMod = mod;
+
+		if (mod === 'base') {
+			dataTypes.forEach(function(dataType) {
+				Data[mod][dataType] = Data.base[dataType];
+			}, this);
+		} else {
+			try {
+				Data[mod].Pokedex = require('./mods/'+mod+'/pokedex.js').BattlePokedex;
+				Data[mod].Movedex = require('./mods/'+mod+'/moves.js').BattleMovedex;
+				Data[mod].Statuses = require('./mods/'+mod+'/statuses.js').BattleStatuses;
+				Data[mod].TypeChart = require('./mods/'+mod+'/typechart.js').BattleTypeChart;
+				Data[mod].Scripts = require('./mods/'+mod+'/scripts.js').BattleScripts;
+				Data[mod].Items = require('./mods/'+mod+'/items.js').BattleItems;
+				Data[mod].Abilities = require('./mods/'+mod+'/abilities.js').BattleAbilities;
+				Data[mod].Formats = require('./mods/'+mod+'/formats.js').BattleFormats;
+				Data[mod].FormatsData = require('./mods/'+mod+'/formats-data.js').BattleFormatsData;
+				Data[mod].Learnsets = require('./mods/'+mod+'/learnsets.js').BattleLearnsets;
+				Data[mod].Aliases = require('./mods/'+mod+'/aliases.js').BattleAliases;
+			} catch (e) {}
+			dataTypes.forEach(function(dataType) {
+				if (!Data[mod][dataType]) Data[mod][dataType] = {};
+				for (var i in Data.base[dataType]) {
+					if (Data[mod][dataType][i] === null) {
+						// null means don't inherit
+						delete Data[mod][dataType][i];
+					} else if (typeof Data[mod][dataType][i] === 'undefined') {
+						// If it doesn't exist is inherited from the base data
+						Data[mod][dataType][i] = Object.clone(Data.base[dataType][i], true);
+					} else if (Data[mod][dataType][i] && Data[mod][dataType][i].inherit) {
+						// {inherit: true} can be used to modify only parts of the base data,
+						// instead of overwriting entirely
+						delete Data[mod][dataType][i].inherit;
+						Object.merge(Data[mod][dataType][i], Data.base[dataType][i], true, false);
+					}
+				}
+			});
+		}
+		this.data = Data[mod];
+	}
+
+	var moddedTools = {};
+	Tools.prototype.mod = function(mod) {
+		if (!mod) mod = 'base';
+		return moddedTools[mod];
+	};
+
+	Tools.prototype.effectToString = function() {
 		return this.name;
 	};
-	this.getImmunity = function(type, target) {
+	Tools.prototype.getImmunity = function(type, target) {
 		for (var i=0; i<target.types.length; i++) {
-			if (BattleTypeChart[target.types[i]] && BattleTypeChart[target.types[i]].damageTaken && BattleTypeChart[target.types[i]].damageTaken[type] === 3) {
+			if (this.data.TypeChart[target.types[i]] && this.data.TypeChart[target.types[i]].damageTaken && this.data.TypeChart[target.types[i]].damageTaken[type] === 3) {
 				return false;
 			}
 		}
 		return true;
 	};
-	this.getEffectiveness = function(type, target) {
+	Tools.prototype.getEffectiveness = function(type, target) {
 		var totalTypeMod = 0;
 		for (var i=0; i<target.types.length; i++) {
-			if (!BattleTypeChart[target.types[i]]) continue;
-			var typeMod = BattleTypeChart[target.types[i]].damageTaken[type];
+			if (!this.data.TypeChart[target.types[i]]) continue;
+			var typeMod = this.data.TypeChart[target.types[i]].damageTaken[type];
 			if (typeMod === 1) { // super-effective
 				totalTypeMod++;
 			}
@@ -27,25 +76,25 @@ function BattleTools() {
 		}
 		return totalTypeMod;
 	};
-	this.getTemplate = function(template) {
+	Tools.prototype.getTemplate = function(template) {
 		if (!template || typeof template === 'string') {
 			var name = (template||'').trim();
 			var id = toId(name);
-			if (BattleAliases[id]) {
-				name = BattleAliases[id];
+			if (this.data.Aliases[id]) {
+				name = this.data.Aliases[id];
 				id = toId(name);
 			}
 			template = {};
-			if (id && BattlePokedex[id]) {
-				template = BattlePokedex[id];
+			if (id && this.data.Pokedex[id]) {
+				template = this.data.Pokedex[id];
 				template.exists = true;
 			}
 			name = template.species || template.name || name;
-			if (BattleFormatsData[id]) {
-				Object.merge(template, BattleFormatsData[id]);
+			if (this.data.FormatsData[id]) {
+				Object.merge(template, this.data.FormatsData[id]);
 			}
-			if (BattleLearnsets[id]) {
-				Object.merge(template, BattleLearnsets[id]);
+			if (this.data.Learnsets[id]) {
+				Object.merge(template, this.data.Learnsets[id]);
 			}
 			if (!template.id) template.id = id;
 			if (!template.name) template.name = name;
@@ -75,7 +124,7 @@ function BattleTools() {
 		}
 		return template;
 	};
-	this.getMove = function(move) {
+	Tools.prototype.getMove = function(move) {
 		if (!move || typeof move === 'string') {
 			var name = (move||'').trim();
 			var id = toId(name);
@@ -85,14 +134,14 @@ function BattleTools() {
 				hptype = hptype.substr(0,hptype.length-1);
 				id = 'HiddenPower'+hptype;
 			}
-			if (id && BattleMovedex[id]) {
-				move = BattleMovedex[id];
+			if (id && this.data.Movedex[id]) {
+				move = this.data.Movedex[id];
 				move.exists = true;
 			}
 			if (!move.id) move.id = id;
 			if (!move.name) move.name = name;
 			if (!move.fullname) move.fullname = 'move: '+name;
-			move.toString = selfT.effectToString;
+			move.toString = this.effectToString;
 			if (!move.critRatio) move.critRatio = 1;
 			if (!move.baseType) move.baseType = move.type;
 			if (!move.effectType) move.effectType = 'Move';
@@ -120,36 +169,36 @@ function BattleTools() {
 	 * @param  move    Move ID, move object, or movecopy object describing move to copy
 	 * @return         movecopy object
 	 */
-	this.getMoveCopy = function(move) {
+	Tools.prototype.getMoveCopy = function(move) {
 		if (move && move.isCopy) return move;
 		var move = this.getMove(move);
 		var moveCopy = Object.clone(move, true);
 		moveCopy.isCopy = true;
 		return moveCopy;
 	};
-	this.getNature = function(nature) {
-		return BattleScripts.getNature.call(selfT, nature);
+	Tools.prototype.getNature = function(nature) {
+		return this.data.Scripts.getNature.call(this, nature);
 	};
-	this.getEffect = function(effect) {
+	Tools.prototype.getEffect = function(effect) {
 		if (!effect || typeof effect === 'string') {
 			var name = (effect||'').trim();
 			var id = toId(name);
 			effect = {};
-			if (id && BattleStatuses[id]) {
-				effect = BattleStatuses[id];
-				effect.name = effect.name || BattleStatuses[id].name;
-			} else if (id && BattleMovedex[id] && BattleMovedex[id].effect) {
-				effect = BattleMovedex[id].effect;
-				effect.name = effect.name || BattleMovedex[id].name;
-			} else if (id && BattleAbilities[id] && BattleAbilities[id].effect) {
-				effect = BattleAbilities[id].effect;
-				effect.name = effect.name || BattleAbilities[id].name;
-			} else if (id && BattleItems[id] && BattleItems[id].effect) {
-				effect = BattleItems[id].effect;
-				effect.name = effect.name || BattleItems[id].name;
-			} else if (id && BattleFormats[id]) {
-				effect = BattleFormats[id];
-				effect.name = effect.name || BattleFormats[id].name;
+			if (id && this.data.Statuses[id]) {
+				effect = this.data.Statuses[id];
+				effect.name = effect.name || this.data.Statuses[id].name;
+			} else if (id && this.data.Movedex[id] && this.data.Movedex[id].effect) {
+				effect = this.data.Movedex[id].effect;
+				effect.name = effect.name || this.data.Movedex[id].name;
+			} else if (id && this.data.Abilities[id] && this.data.Abilities[id].effect) {
+				effect = this.data.Abilities[id].effect;
+				effect.name = effect.name || this.data.Abilities[id].name;
+			} else if (id && this.data.Items[id] && this.data.Items[id].effect) {
+				effect = this.data.Items[id].effect;
+				effect.name = effect.name || this.data.Items[id].name;
+			} else if (id && this.data.Formats[id]) {
+				effect = this.data.Formats[id];
+				effect.name = effect.name || this.data.Formats[id].name;
 				if (!effect.effectType) effect.effectType = 'Format';
 			} else if (id === 'recoil') {
 				effect = {
@@ -163,44 +212,44 @@ function BattleTools() {
 			if (!effect.id) effect.id = id;
 			if (!effect.name) effect.name = name;
 			if (!effect.fullname) effect.fullname = name;
-			effect.toString = selfT.effectToString;
+			effect.toString = this.effectToString;
 			if (!effect.category) effect.category = 'Effect';
 			if (!effect.effectType) effect.effectType = 'Effect';
 		}
 		return effect;
 	};
-	this.getItem = function(item) {
+	Tools.prototype.getItem = function(item) {
 		if (!item || typeof item === 'string') {
 			var name = (item||'').trim();
 			var id = toId(name);
 			item = {};
-			if (id && BattleItems[id]) {
-				item = BattleItems[id];
+			if (id && this.data.Items[id]) {
+				item = this.data.Items[id];
 				item.exists = true;
 			}
 			if (!item.id) item.id = id;
 			if (!item.name) item.name = name;
 			if (!item.fullname) item.fullname = 'item: '+name;
-			item.toString = selfT.effectToString;
+			item.toString = this.effectToString;
 			if (!item.category) item.category = 'Effect';
 			if (!item.effectType) item.effectType = 'Item';
 			if (item.isBerry) item.fling = { basePower: 10 };
 		}
 		return item;
 	};
-	this.getAbility = function(ability) {
+	Tools.prototype.getAbility = function(ability) {
 		if (!ability || typeof ability === 'string') {
 			var name = (ability||'').trim();
 			var id = toId(name);
 			ability = {};
-			if (id && BattleAbilities[id]) {
-				ability = BattleAbilities[id];
+			if (id && this.data.Abilities[id]) {
+				ability = this.data.Abilities[id];
 				ability.exists = true;
 			}
 			if (!ability.id) ability.id = id;
 			if (!ability.name) ability.name = name;
 			if (!ability.fullname) ability.fullname = 'ability: '+name;
-			ability.toString = selfT.effectToString;
+			ability.toString = this.effectToString;
 			if (!ability.category) ability.category = 'Effect';
 			if (!ability.effectType) ability.effectType = 'Ability';
 			if (!ability.gen) {
@@ -212,12 +261,12 @@ function BattleTools() {
 		}
 		return ability;
 	};
-	this.getType = function(type) {
+	Tools.prototype.getType = function(type) {
 		if (!type || typeof type === 'string') {
 			var id = toId(type);
 			type = {};
-			if (id && BattleTypeChart[id]) {
-				type = BattleTypeChart[id];
+			if (id && this.data.TypeChart[id]) {
+				type = this.data.TypeChart[id];
 				type.isType = true;
 				type.effectType = 'Type';
 			}
@@ -231,7 +280,7 @@ function BattleTools() {
 	};
 
 
-	this.checkLearnset = function(move, template, lsetData) {
+	Tools.prototype.checkLearnset = function(move, template, lsetData) {
 		lsetData = lsetData || {set:{},format:{}};
 		var set = lsetData.set;
 		var format = lsetData.format;
@@ -277,7 +326,7 @@ function BattleTools() {
 					var getGlitch = false;
 					for (var i in glitchMoves) {
 						if (template.learnset[i]) {
-							if (i === 'mimic' && selfT.getAbility(set.ability).gen == 4 && !template.prevo) {
+							if (i === 'mimic' && this.getAbility(set.ability).gen == 4 && !template.prevo) {
 								// doesn't get the glitch
 							} else {
 								getGlitch = true;
@@ -286,7 +335,7 @@ function BattleTools() {
 						}
 					}
 					if (getGlitch) {
-						if (selfT.getMove(move).gen >= 5) {
+						if (this.getMove(move).gen >= 5) {
 							result = 1;
 						} else {
 							return true;
@@ -294,7 +343,7 @@ function BattleTools() {
 					}
 				}
 			}
-			if (Object.keys(template.abilities).length === 1 && BattleFormatsData[template.speciesid].dreamWorldRelease && !recheck) {
+			if (Object.keys(template.abilities).length === 1 && this.data.FormatsData[template.speciesid].dreamWorldRelease && !recheck) {
 				// Some Pokemon with no DW ability still have DW-exclusive moves (e.g. Gastly, Koffing, Chimecho)
 				isDW = !isDW;
 				recheck = true;
@@ -304,16 +353,16 @@ function BattleTools() {
 				recheck = false;
 			}
 			if (template.speciesid === 'shaymin') {
-				template = selfT.getTemplate('shayminsky');
+				template = this.getTemplate('shayminsky');
 			} else if (template.basespecies !== template.species) {
-				template = selfT.getTemplate(template.basespecies);
+				template = this.getTemplate(template.basespecies);
 			} else {
-				template = selfT.getTemplate(template.prevo);
+				template = this.getTemplate(template.prevo);
 			}
 		} while (template && template.species && !alreadyChecked[template.speciesid]);
 		return result;
 	};
-	this.getBanlistTable = function(format, subformat, depth) {
+	Tools.prototype.getBanlistTable = function(format, subformat, depth) {
 		var banlistTable;
 		if (!depth) depth = 0;
 		if (depth>4) return; // avoid infinite recursion
@@ -360,19 +409,19 @@ function BattleTools() {
 
 					banlistTable['Rule:'+toId(subformat.ruleset[i])] = true;
 
-					var subsubformat = selfT.getEffect(subformat.ruleset[i]);
+					var subsubformat = this.getEffect(subformat.ruleset[i]);
 					if (subsubformat.ruleset || subsubformat.banlist) {
-						selfT.getBanlistTable(format, subsubformat, depth+1);
+						this.getBanlistTable(format, subsubformat, depth+1);
 					}
 				}
 			}
 		}
 		return banlistTable;
 	};
-	this.validateTeam = function(team, format) {
+	Tools.prototype.validateTeam = function(team, format) {
 		var problems = [];
-		format = selfT.getEffect(format);
-		selfT.getBanlistTable(format);
+		format = this.getEffect(format);
+		this.getBanlistTable(format);
 		if (format.team === 'random') {
 			return false;
 		}
@@ -390,7 +439,7 @@ function BattleTools() {
 		}
 		var teamHas = {};
 		for (var i=0; i<team.length; i++) {
-			var setProblems = selfT.validateSet(team[i], format, teamHas);
+			var setProblems = this.validateSet(team[i], format, teamHas);
 			if (setProblems) {
 				problems = problems.concat(setProblems);
 			}
@@ -417,22 +466,22 @@ function BattleTools() {
 
 		if (format.ruleset) {
 			for (var i=0; i<format.ruleset.length; i++) {
-				var subformat = selfT.getEffect(format.ruleset[i]);
+				var subformat = this.getEffect(format.ruleset[i]);
 				if (subformat.validateTeam) {
-					problems = problems.concat(subformat.validateTeam.call(selfT, team, format)||[]);
+					problems = problems.concat(subformat.validateTeam.call(this, team, format)||[]);
 				}
 			}
 		}
 		if (format.validateTeam) {
-			problems = problems.concat(format.validateTeam.call(selfT, team, format)||[]);
+			problems = problems.concat(format.validateTeam.call(this, team, format)||[]);
 		}
 
 		if (!problems.length) return false;
 		return problems;
 	};
-	this.validateSet = function(set, format, teamHas) {
+	Tools.prototype.validateSet = function(set, format, teamHas) {
 		var problems = [];
-		format = selfT.getEffect(format);
+		format = this.getEffect(format);
 		if (!set) {
 			return ["This is not a pokemon."];
 		}
@@ -447,17 +496,17 @@ function BattleTools() {
 		set.name = set.name || set.species;
 		var name = set.species;
 		if (set.species !== set.name) name = set.name + " ("+set.species+")";
-		var template = selfT.getTemplate(set.species);
+		var template = this.getTemplate(set.species);
 		var source = '';
 
 		var setHas = {};
 
 		if (!template || !template.abilities) {
 			set.species = 'Bulbasaur';
-			template = selfT.getTemplate('Bulbasaur');
+			template = this.getTemplate('Bulbasaur');
 		}
 
-		var banlistTable = selfT.getBanlistTable(format);
+		var banlistTable = this.getBanlistTable(format);
 
 		setHas[toId(set.species)] = true;
 		if (banlistTable[toId(set.species)]) {
@@ -471,7 +520,7 @@ function BattleTools() {
 		if (banlistTable[toId(set.item)]) {
 			problems.push(name+"'s item "+set.item+" is banned.");
 		}
-		var item = selfT.getItem(set.item);
+		var item = this.getItem(set.item);
 		if (banlistTable['Unreleased'] && item.isUnreleased) {
 			problems.push(name+"'s item "+set.item+" is unreleased.");
 		}
@@ -485,7 +534,7 @@ function BattleTools() {
 				problems.push(name+" has more than 510 total EVs.");
 			}
 
-			var ability = selfT.getAbility(set.ability).name;
+			var ability = this.getAbility(set.ability).name;
 			if (ability !== template.abilities['0'] &&
 				ability !== template.abilities['1'] &&
 				ability !== template.abilities['DW']) {
@@ -516,7 +565,7 @@ function BattleTools() {
 			for (var i=0; i<set.moves.length; i++) {
 				if (!set.moves[i]) continue;
 				set.moves[i] = ''+(set.moves[i]||'');
-				var move = selfT.getMove(set.moves[i]);
+				var move = this.getMove(set.moves[i]);
 				setHas[move.id] = true;
 				if (banlistTable[move.id]) {
 					problems.push(name+"'s move "+set.moves[i]+" is banned.");
@@ -525,7 +574,7 @@ function BattleTools() {
 				}
 
 				if (banlistTable['illegal']) {
-					var lset = selfT.checkLearnset(move, template, lsetData);
+					var lset = this.checkLearnset(move, template, lsetData);
 					if (!lset) {
 						var problem = name+" can't learn "+move.name;
 						if (lset === null) {
@@ -576,26 +625,28 @@ function BattleTools() {
 
 		if (format.ruleset) {
 			for (var i=0; i<format.ruleset.length; i++) {
-				var subformat = selfT.getEffect(format.ruleset[i]);
+				var subformat = this.getEffect(format.ruleset[i]);
 				if (subformat.validateSet) {
-					problems = problems.concat(subformat.validateSet.call(selfT, set, format)||[]);
+					problems = problems.concat(subformat.validateSet.call(this, set, format)||[]);
 				}
 			}
 		}
 		if (format.validateSet) {
-			problems = problems.concat(format.validateSet.call(selfT, set, format)||[]);
+			problems = problems.concat(format.validateSet.call(this, set, format)||[]);
 		}
 
 		if (!problems.length) return false;
 		return problems;
 	};
 
-	/* for (var i in BattleScripts) {
-		var script = BattleScripts[i];
-		this[i] = function() {
-			return script.apply(selfT, arguments);
-		};
-	} */
-}
+	moddedTools.base = new Tools();
+	try {
+		var dirs = fs.readdirSync('./mods/');
 
-exports.BattleTools = BattleTools;
+		dirs.forEach(function(dir) {
+			moddedTools[dir] = new Tools(dir);
+		});
+	} catch (e) {}
+
+	return moddedTools.base;
+})();
