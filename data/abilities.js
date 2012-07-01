44dfcbcdb05a39a3f5a75b6180e2932b2c0d2d83
TheImmortal <the_immortal123@live.com>
2012-07-02 03:44:07 +0400

    Proper Trace implementation

--- data/abilities.js
+++ data/abilities.js
@@ -2252,10 +2252,10 @@ exports.BattleAbilities = {
 	"trace": {
 		desc: "When this Pokemon enters the field, it temporarily copies an opponent's ability (except Multitype). This ability remains with this Pokemon until it leaves the field.",
 		shortDesc: "On switch-in, or when it can, this Pokemon copies a random adjacent foe's Ability.",
-		onStart: function(pokemon) {
+		onModifyPokemon: function(pokemon) {
 			var target = pokemon.side.foe.randomActive();
 			var ability = this.getAbility(target.ability);
-			if (ability.id === 'trace' || ability.id === 'illusion') return;
+			if (ability.id === 'flowergift' || ability.id === 'forecast' || ability.id === 'illusion' || ability.id === 'imposter' || ability.id === 'multitype' || ability.id === 'trace' || ability.id === 'wonderguard' || ability.id === 'zenmode') return;
 			if (pokemon.setAbility(ability)) {
 				this.add('-ability',pokemon, ability,'[from] ability: Trace','[of] '+target);
 			}
