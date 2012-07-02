7de7e4db9388fe4ed5df9f57595edefc93843054
Bill Meltsner <bill@meltsner.com>
2012-07-02 02:57:56 -0500

    Actually fix Trace properly.

--- data/abilities.js
+++ data/abilities.js
@@ -2252,17 +2252,13 @@ exports.BattleAbilities = {
 	"trace": {
 		desc: "When this Pokemon enters the field, it temporarily copies an opponent's ability (except Multitype). This ability remains with this Pokemon until it leaves the field.",
 		shortDesc: "On switch-in, or when it can, this Pokemon copies a random adjacent foe's Ability.",
-		onStart: function(pokemon) {
-			pokemon.addVolatile('trace');
-		},
-		effect: {
-			onModifyPokemon: function(pokemon) {
-				var target = pokemon.side.foe.randomActive();
-				var ability = this.getAbility(target.ability);
-				if (ability.id === 'flowergift' || ability.id === 'forecast' || ability.id === 'illusion' || ability.id === 'imposter' || ability.id === 'multitype' || ability.id === 'trace' || ability.id === 'wonderguard' || ability.id === 'zenmode') return;
-				if (pokemon.setAbility(ability) && pokemon.removeVolatile('trace')) {
-					this.add('-ability',pokemon, ability,'[from] ability: Trace','[of] '+target);
-				}
+		onModifyPokemon: function(pokemon) {
+			var target = pokemon.side.foe.randomActive();
+			if (!target) return;
+			var ability = this.getAbility(target.ability);
+			if (ability.id === 'flowergift' || ability.id === 'forecast' || ability.id === 'illusion' || ability.id === 'imposter' || ability.id === 'multitype' || ability.id === 'trace' || ability.id === 'wonderguard' || ability.id === 'zenmode') return;
+			if (pokemon.setAbility(ability)) {
+				this.add('-ability',pokemon, ability,'[from] ability: Trace','[of] '+target);
 			}
 		},
 		id: "trace",
