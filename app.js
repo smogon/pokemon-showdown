0d947ba0fed493e0a6b2d20b691e41a78fd68ac2
Guangcong Luo <guangcongluo@gmail.com>
2012-07-16 17:09:32 -0700

    New request code - maybe it'll be better on the server?

--- app.js
+++ app.js
@@ -3,7 +3,29 @@ require('sugar');
 fs = require('fs');
 path = require('path');
 
-request = require('request');
+//request = require('request');
+var http = require("http");
+var url = require('url');
+request = function(options, callback) {
+    var req = http.get(url.parse(options.uri), function(res) {
+        var buffer = '';
+        res.setEncoding('utf8');
+
+        res.on('data', function(chunk) {
+            buffer += chunk;
+        });
+
+        res.on('end', function() {
+            callback(null, res.statusCode, buffer);
+        });
+    });
+
+    req.on('error', function(error) {
+        callback(error);
+    });
+
+    req.end();
+}
 
 // Synchronously copy config-example.js over to config.js if it doesn't exist
 if (!path.existsSync('./config/config.js')) {
