b02794468cde02fee3f1c2362e6e7913dd56a53d
verbiage <kk13777@yahoo.com>
2012-07-29 00:05:53 -0700

    Changing the /rules command to send the user to smogon's sim rules page instead of PS's now outdated page.

--- chat-commands.js
+++ chat-commands.js
@@ -853,7 +853,7 @@ function parseCommandLocal(user, cmd, target, room, socket, message) {
 		showOrBroadcastStart(user, cmd, room, socket, message);
 		showOrBroadcast(user, cmd, room, socket,
 			'<div style="border:1px solid #6688AA;padding:2px 4px">Please follow the rules:<br />' +
-			'- <a href="http://pokemonshowdown.com/rules" target="_blank">Rules</a><br />' +
+			'- <a href="http://www.smogon.com/sim/rules" target="_blank">Rules</a><br />' +
 			'</div>');
 		return false;
 		break;
