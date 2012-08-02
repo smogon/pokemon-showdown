c335870340e052d8d465e5696146fd1ecb6c32ea
Guangcong Luo <guangcongluo@gmail.com>
2012-08-01 19:54:16 -0700

    Make sure formats have their full ruleset

--- tools.js
+++ tools.js
@@ -256,6 +256,7 @@ module.exports = (function () {
 			effect.toString = this.effectToString;
 			if (!effect.category) effect.category = 'Effect';
 			if (!effect.effectType) effect.effectType = 'Effect';
+			this.getBanlistTable(effect);
 		}
 		return effect;
 	};
