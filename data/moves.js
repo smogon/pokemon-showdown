b980de80ce60887001740df2ed6bc566b9a303a5
Marty-D <math_man70@hotmail.com>
2012-08-05 13:22:30 -0300

    Actually fix Rapid Spin.
    Thanks, bmelts!

--- data/moves.js
+++ data/moves.js
@@ -8587,16 +8587,16 @@ exports.BattleMovedex = {
 		isContact: true,
 		self: {
 			onHit: function(pokemon) {
-				if (pokemon.removeVolatile('leechseed') && pokemon.hp) {
+				if (pokemon.hp && pokemon.removeVolatile('leechseed')) {
 					this.add('-end', pokemon, 'Leech Seed', '[from] move: Rapid Spin', '[of] '+pokemon);
 				}
 				var sideConditions = {spikes:1, toxicspikes:1, stealthrock:1};
 				for (var i in sideConditions) {
-					if (pokemon.side.removeSideCondition(i) && pokemon.hp) {
+					if (pokemon.hp && pokemon.side.removeSideCondition(i)) {
 						this.add('-sideend', pokemon.side, this.getEffect(i).name, '[from] move: Rapid Spin', '[of] '+pokemon);
 					}
 				}
-				if (pokemon.volatiles['partiallytrapped'] && pokemon.hp) {
+				if (pokemon.hp && pokemon.volatiles['partiallytrapped']) {
 					this.add('-remove', pokemon, pokemon.volatiles['partiallytrapped'].sourceEffect.name, '[from] move: Rapid Spin', '[of] '+pokemon, '[partiallytrapped]');
 					delete pokemon.volatiles['partiallytrapped'];
 				}
