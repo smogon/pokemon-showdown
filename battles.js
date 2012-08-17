5fae8749138674a6866d45bef1d3a03373b222cc
Guangcong Luo <guangcongluo@gmail.com>
2012-08-16 22:23:14 -0700

    Fudge factor as we understand what's wrong with the damage calculator

--- battles.js
+++ battles.js
@@ -2173,6 +2173,9 @@ function Battle(roomid, format, rated) {
 		//int(int(int(2*L/5+2)*A*P/D)/50);
 		var baseDamage = Math.floor(Math.floor(Math.floor(2*level/5+2) * basePower * attack/defense)/50) + 2;
 
+		// fudge factor because there's apparently something wrong with this formula
+		baseDamage--;
+
 		// multi-target modifier (doubles only)
 		// weather modifier (TODO: relocate here)
 		// crit
