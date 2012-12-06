4d12d57d3ca0da466d8daa40e4b6d5c3f3138d04
TheImmortal <the_immortal123@live.com>
2012-12-06 15:53:51 +0400

    Update Aurumoth viable moves

--- data/formats-data.js
+++ data/formats-data.js
@@ -22,7 +22,7 @@ exports.BattleFormatsData = {
 		tier: "G5CAP"
 	},
 	aurumoth: {
-		viableMoves: {"dragondance":1,"quiverdance":1,"willowisp":1,"tailglow":1,"wish":1,"bugbuzz":1,"hydropump":1,"megahorn":1,"psyshock":1,"overheat":1,"xscissor":1,"trick":1,"zenheadbutt":1},
+		viableMoves: {"dragondance":1,"quiverdance":1,"closecombat":1,"tailglow":1,"wish":1,"bugbuzz":1,"hydropump":1,"megahorn":1,"psychic":1,"overheat":1,"blizzard":1,"thunder":1,"focusblast":1},
 		isNonstandard: true,
 		dreamWorldRelease: true,
 		tier: "G5CAP"
