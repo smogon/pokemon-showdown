335de9c0d57edb47c29e11c49a9d66898cdb2423
Guangcong Luo <guangcongluo@gmail.com>
2013-01-02 07:59:09 -0800

    Log all relevant ratings instead of just ACRE

--- rooms.js
+++ rooms.js
@@ -99,8 +99,8 @@ function BattleRoom(roomid, format, p1, p2, parentid, rated) {
 						return;
 					} else {
 						try {
-							p1rating = data.p1rating.acre;
-							p2rating = data.p2rating.acre;
+							p1rating = data.p1rating;
+							p2rating = data.p2rating;
 							//selfR.add("Ladder updated.");
 
 							var oldacre = Math.round(data.p1rating.oldacre);
