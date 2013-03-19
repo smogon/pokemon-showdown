0e15b894ec16656ab8c4f3af18c388753f0ba3b9
Joim <joimpoke@gmail.com>
2013-03-19 17:32:45 +0100

    Gen 1: Fix style on statuses.js
    Fix code style.

--- mods/gen1/statuses.js
+++ mods/gen1/statuses.js
@@ -25,10 +25,8 @@ exports.BattleStatuses = {
 		},
 		onSwitchIn: function (pokemon){
 			pokemon.addVolatile('brnattackdrop');
-			if (pokemon.side.foe.active[0]) {
-				if (pokemon.speed <= pokemon.side.foe.active[0].speed) {
-					this.damage(pokemon.maxhp/16);
-				}
+			if (pokemon.side.foe.active[0] && pokemon.speed <= pokemon.side.foe.active[0].speed) {
+				this.damage(pokemon.maxhp/16);
 			}
 		}
 	},
@@ -104,10 +102,8 @@ exports.BattleStatuses = {
 			this.damage(pokemon.maxhp/16);
 		},
 		onSwitchIn: function (pokemon) {
-			if (pokemon.side.foe.active[0]) {
-				if (pokemon.speed <= pokemon.side.foe.active[0].speed) {
-					this.damage(pokemon.maxhp/16);
-				}
+			if (pokemon.side.foe.active[0] && pokemon.speed <= pokemon.side.foe.active[0].speed) {
+				this.damage(pokemon.maxhp/16);
 			}
 		}
 	},
@@ -129,13 +125,11 @@ exports.BattleStatuses = {
 			this.damage(clampIntRange(pokemon.maxhp/16, 1)*this.effectData.stage);
 		},
 		onSwitchIn: function (pokemon) {
-			if (pokemon.side.foe.active[0]) {
-				if (pokemon.speed <= pokemon.side.foe.active[0].speed) {
-					if (this.effectData.stage < 15) {
-						this.effectData.stage++;
-					}
-					this.damage(clampIntRange(pokemon.maxhp/16, 1)*this.effectData.stage);
+			if (pokemon.side.foe.active[0] && pokemon.speed <= pokemon.side.foe.active[0].speed) {
+				if (this.effectData.stage < 15) {
+					this.effectData.stage++;
 				}
+				this.damage(clampIntRange(pokemon.maxhp/16, 1)*this.effectData.stage);
 			}
 		}
 	},
