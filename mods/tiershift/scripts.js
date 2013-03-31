0c1e3d32372f36c53b5e10d55507f14d787ae3e5
Joimer <joimpoke@gmail.com>
2013-03-31 10:43:19 +0200

    Add Tier Shift mod for Other Meta of the Month

---
+++ mods/tiershift/scripts.js
@@ -0,0 +1,31 @@
+exports.BattleScripts = {
+	init: function() {
+		for (var i in this.data.Pokedex) {
+			var tier = '';
+			var adjustment = 0;
+			if (this.data.FormatsData[i]) tier = this.data.FormatsData[i].tier;
+			switch (tier) {
+			case 'BL':
+			case 'UU':
+				adjustment = 5;
+				break;
+			case 'BL2':
+			case 'RU':
+				adjustment = 10;
+				break;
+			case 'BL3':
+			case 'NU':
+			case 'NFE':
+			case 'LC':
+				adjustment = 15;
+			}
+
+			this.data.Pokedex[i].baseStats.hp += adjustment;
+			this.data.Pokedex[i].baseStats.atk += adjustment;
+			this.data.Pokedex[i].baseStats.def += adjustment;
+			this.data.Pokedex[i].baseStats.spa += adjustment;
+			this.data.Pokedex[i].baseStats.spd += adjustment;
+			this.data.Pokedex[i].baseStats.spe += adjustment;
+		}
+	}
+};
