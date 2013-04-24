a22700bb2b141fe3219ed7020e09da8fe3dd89aa
Guangcong Luo <guangcongluo@gmail.com>
2013-04-23 18:46:32 -0700

    Fix Gen 4 learnset checking

--- mods/gen4/scripts.js
+++ mods/gen4/scripts.js
@@ -6,6 +6,16 @@ exports.BattleScripts = {
 			if (template.gen > 4) template.isNonstandard = true;
 			delete template.abilities['DW'];
 		}
+		for (var i in this.data.Learnsets) {
+			var learnset = this.data.Learnsets[i].learnset;
+			for (var moveid in learnset) {
+				if (typeof learnset[moveid] === 'string') learnset[moveid] = [learnset[moveid]];
+				learnset[moveid] = learnset[moveid].filter(function(source) {
+					return source[0] !== '5';
+				});
+				if (!learnset[moveid].length) delete learnset[moveid];
+			}
+		}
 		for (var i in this.data.Movedex) {
 			var move = this.getMove(i);
 			if (move.gen > 4) move.isNonstandard = true;
