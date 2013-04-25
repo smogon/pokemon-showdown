da54b1b5a9aa681dbb0e411702190cf909a037d9
Cathy J. Fitzpatrick <cathy@cathyjf.com>
2013-04-25 00:15:54 -0600

    Actually commit crashlogger.js

---
+++ crashlogger.js
@@ -0,0 +1,40 @@
+module.exports = (function() {
+	var lastCrashLog = 0;
+	return function(err, description) {
+		console.log("\n"+err.stack+"\n");
+		fs.createWriteStream('logs/errors.txt', {'flags': 'a'}).on("open", function(fd) {
+			this.write("\n"+err.stack+"\n");
+			this.end();
+		}).on("error", function (err) {
+			console.log("\n"+err.stack+"\n");
+		});
+		var datenow = Date.now();
+		if (config.crashguardemail && ((datenow - lastCrashLog) > 1000 * 60 * 5)) {
+			lastCrashLog = datenow;
+			var transport;
+			try {
+				var transport = require('nodemailer').createTransport(
+					config.crashguardemail.transport,
+					config.crashguardemail.options
+				);
+				transport.sendMail({
+					from: config.crashguardemail.from,
+					to: config.crashguardemail.to,
+					subject: config.crashguardemail.subject,
+					text: description + ' crashed with this stack trace:\n' + err.stack
+				});
+				if (process.uptime() > 60 * 60) {
+					// no need to lock down the server
+					return true;
+				}
+			} catch (e) {
+				// could not send an email...
+				console.log('Error sending email: ' + e);
+			} finally {
+				if (transport) {
+					transport.close();
+				}
+			}
+		}
+	};
+})();
