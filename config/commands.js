afe444049dd4e2e629de61d82469be8bdddb3b78
Cathy J. Fitzpatrick <cathy@cathyjf.com>
2013-05-24 23:08:44 -0600

    Add mention of locking to driver powers

--- config/commands.js
+++ config/commands.js
@@ -318,7 +318,7 @@ var commands = exports.commands = {
 	groups: function(target, room, user) {
 		if (!this.canBroadcast()) return;
 		this.sendReplyBox('+ <b>Voice</b> - They can use ! commands like !groups, and talk during moderated chat<br />' +
-			'% <b>Driver</b> - The above, and they can also mute users and check for alts<br />' +
+			'% <b>Driver</b> - The above, and they can also mute and lock users and check for alts<br />' +
 			'@ <b>Moderator</b> - The above, and they can ban users<br />' +
 			'&amp; <b>Leader</b> - The above, and they can promote moderators and force ties<br />'+
 			'~ <b>Administrator</b> - They can do anything, like change what this message says');
