81ad4f97acc015a592184e166b18a4aa63cabb02
Marty-D <math_man70@hotmail.com>
2013-06-05 13:01:16 -0400

    Gen III: Preliminary status fixes.

--- mods/gen3/statuses.js
+++ mods/gen3/statuses.js
@@ -1,32 +1,22 @@
 exports.BattleStatuses = {
-	par: {
-		inherit: true,
-		onBeforeMovePriority: 2,
-		onBeforeMove: function(pokemon) {
-			if (pokemon.ability !== 'magicguard' && this.random(4) === 0) {
-				this.add('cant', pokemon.id, 'par');
-				return false;
-			}
-		}
-	},
 	slp: {
 		effectType: 'Status',
 		onStart: function(target) {
-			this.add('-status', target.id, 'slp');
-			// 2-7 turns
-			this.effectData.time = this.random(3,13);
-			if (target.getAbility().isHalfSleep) {
-				this.effectData.time = Math.floor(this.effectData.time / 2);
-			}
+			this.add('-status', target, 'slp');
+			// 1-4 turns
+			this.effectData.time = this.random(2,6);
 		},
 		onBeforeMovePriority: 2,
 		onBeforeMove: function(pokemon, target, move) {
+			if (pokemon.getAbility().isHalfSleep) {
+				pokemon.statusData.time--;
+			}
 			pokemon.statusData.time--;
-			if (!pokemon.statusData.time) {
+			if (pokemon.statusData.time <= 0) {
 				pokemon.cureStatus();
 				return;
 			}
-			this.add('cant', pokemon.id, 'slp');
+			this.add('cant', pokemon, 'slp');
 			if (move.sleepUsable) {
 				return;
 			}
@@ -41,33 +31,8 @@ exports.BattleStatuses = {
 		}
 	},
 	sandstorm: {
-		effectType: 'Weather',
-		duration: 5,
-		durationCallback: function(source, effect) {
-			if (source && source.item === 'smoothrock') {
-				return 8;
-			}
-			return 5;
-		},
-		onStart: function(battle, source, effect) {
-			if (effect && effect.effectType === 'Ability') {
-				this.effectData.duration = 0;
-				this.add('-weather', 'Sandstorm', '[from] ability: '+effect, '[of] '+source);
-			} else {
-				this.add('-weather', 'Sandstorm');
-			}
-		},
-		onResidualOrder: 1,
-		onResidual: function() {
-			this.add('-weather', 'Sandstorm', '[upkeep]');
-			this.eachEvent('Weather');
-		},
-		onWeather: function(target) {
-			this.damage(target.maxhp/16);
-		},
-		onEnd: function() {
-			this.add('-weather', 'none');
-		}
+		inherit: true,
+		onModifySpD: null
 	},
 	stall: {
 		// In gen 3, the chance of protect succeeding does not fall below 1/8.
