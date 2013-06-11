f885d646016bc38992c6de94456c48097380ffc4
Cathy J. Fitzpatrick <cathy@cathyjf.com>
2013-06-11 17:44:14 -0600

    Struggle does not require PP

--- data/scripts.js
+++ data/scripts.js
@@ -34,10 +34,12 @@ exports.BattleScripts = {
 		pokemon.lastDamage = 0;
 		var lockedMove = this.runEvent('LockMove', pokemon);
 		if (lockedMove === true) lockedMove = false;
-		if (!lockedMove && !pokemon.deductPP(move, null, target)) {
-			this.add('cant', pokemon, 'nopp', move);
-			this.clearActiveMove(true);
-			return;
+		if (!lockedMove) {
+			if (!pokemon.deductPP(move, null, target) && (move.id !== 'struggle')) {
+				this.add('cant', pokemon, 'nopp', move);
+				this.clearActiveMove(true);
+				return;
+			}
 		}
 		pokemon.moveUsed(move);
 		this.useMove(move, pokemon, target, sourceEffect);
