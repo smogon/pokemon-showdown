e49551b02d4183e31eed7965b2b091efd919e9b4
Guangcong Luo <guangcongluo@gmail.com>
2013-07-03 22:46:21 -0700

    NEXT: Fix Slow Start, Lilligant GossamerWing

--- mods/gennext/abilities.js
+++ mods/gennext/abilities.js
@@ -142,7 +142,7 @@ exports.BattleAbilities = {
 				move.sideCondition = 'flowergift';
 			}
 		},
-		onModifyStats: function(stats, pokemon) {
+		onUpdate: function(pokemon) {
 			if (this.isWeather('sunnyday')) {
 				if (pokemon.isActive && pokemon.speciesid === 'cherrim' && this.effectData.forme !== 'Sunshine') {
 					this.effectData.forme = 'Sunshine';
@@ -173,9 +173,19 @@ exports.BattleAbilities = {
 			onStart: function(target) {
 				this.add('-start', target, 'Slow Start');
 			},
-			onModifyStats: function(stats) {
-				stats.atk /= 2;
-				stats.spe /= 2;
+			onModifyAtk: function(atk, pokemon) {
+				if (pokemon.ability !== 'slowstart') {
+					pokemon.removeVolatile('slowstart');
+					return;
+				}
+				return atk / 2;
+			},
+			onModifySpe: function(spe, pokemon) {
+				if (pokemon.ability !== 'slowstart') {
+					pokemon.removeVolatile('slowstart');
+					return;
+				}
+				return spe / 2;
 			},
 			onEnd: function(target) {
 				this.add('-end', target, 'Slow Start');
@@ -225,6 +235,15 @@ exports.BattleAbilities = {
 				return basePower * 1/2;
 			}
 		}
+	},
+	"heatproof": {
+		inherit: true,
+		onSourceBasePower: function(basePower, attacker, defender, move) {
+			if (move.type === 'Fire') {
+				this.add('-message', "The attack was weakened by Heatproof!");
+				return basePower / 2;
+			}
+		},
 	},
 	"reckless": {
 		inherit: true,
