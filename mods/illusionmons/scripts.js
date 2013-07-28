cc5a5b62c6367004e67cb6dcd93644e17ffa07fa
Oiawesome <Oiawesome99@yahoo.com>
2013-07-28 14:36:22 -0700

    Update scripts.js

--- mods/illusionmons/scripts.js
+++ mods/illusionmons/scripts.js
@@ -2,9 +2,10 @@ exports.BattleScripts = {
   gen: 3,
 	init: function() {
 		for (var i in this.data.Pokedex) {
-			delete this.data.Pokedex[i].abilities['DW'];
+		delete this.data.Pokedex[i].abilities['DW'];
   		delete this.data.Pokedex[i].abilities['DW'];
-  		delete this.data.Pokedex[i].abilities['DW'];      
+  		delete this.data.Pokedex[i].abilities['DW'];
+		this.modData('Pokedex').learnset.hurricane = ['5L100'];  		
 		}
 	},
 };
