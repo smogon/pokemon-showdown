378680ff24ac0925ac0935121da14f3d67aedb71
Guangcong Luo <guangcongluo@gmail.com>
2013-08-14 11:00:11 -0700

    Fix an oversight in Keldeo-Resolution rename

--- data/formats.js
+++ data/formats.js
@@ -834,7 +834,7 @@ exports.BattleFormats = {
 				}
 			}
 			if (template.num == 647) { // Keldeo
-				if (set.species === 'Keldeo-Resolution' && set.moves.indexOf('Secret Sword') < 0) {
+				if (set.species === 'Keldeo-Resolute' && set.moves.indexOf('Secret Sword') < 0) {
 					set.species = 'Keldeo';
 				}
 			}
