ba1a9f8455161113d82c73b86f91763878ad83ed
Joimer <joimpoke@gmail.com>
2013-08-16 21:34:01 +0200

    Memory usage control: Add memory usage report command Admin-only, this command will show you the server's memory usage. Additionally, you can use ResourceMonitor.sizeOfObject in any scope to find out how many bytes does an object weigh.

--- app.js
+++ app.js
@@ -185,6 +185,27 @@ global.ResourceMonitor = {
 			this.battlePreps[ip] = 1;
 			this.battlePrepTimes[ip] = now;
 		}
+	},
+	/**
+	 * Counts roughly the size of an object to have an idea of the server load.
+	 */
+	sizeOfObject: function(object) {
+		var objectList = [];
+		var stack = [object];
+		var bytes = 0;
+
+		while (stack.length) {
+			var value = stack.pop();
+			if (typeof value === 'boolean') bytes += 4;
+			else if (typeof value === 'string') bytes += value.length * 2;
+			else if (typeof value === 'number') bytes += 8;
+			else if (typeof value === 'object' && objectList.indexOf( value ) === -1) {
+				objectList.push( value );
+				for (i in value) stack.push( value[ i ] );
+			}
+		}
+
+		return bytes;
 	}
 };
 
