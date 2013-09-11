be4416688cf0a4100f0e7151c1bc259296407c71
V4Victini <gugugtutuuttu@aol.com>
2013-09-11 03:12:23 -0400

    Overhaul the stat system and implement chaining
    - Add Battle.prototype.chain to chain modifiers
    - BattlePokemon.prototype.getStat is passed different parameters and no longer runs the ModifyBoost event nor the ModifyStat events.

--- battle-engine.js
+++ battle-engine.js
@@ -397,27 +397,20 @@ var BattlePokemon = (function() {
 		}
 		this.battle.runEvent('ModifyPokemon', this);
 
-		this.speed = this.getStat('spe');
+		var statMod = 1;
+		this.speed = this.getStat('spe', this.boosts['spe'], this.battle.runEvent('ModifySpe', this, null, null, statMod));
 	};
-	BattlePokemon.prototype.getStat = function(statName, unboosted, unmodified) {
+	BattlePokemon.prototype.getStat = function(statName, boost, modifier) {
 		statName = toId(statName);
-		var boost = this.boosts[statName];
+		// var boost = this.boosts[statName];
 
 		if (statName === 'hp') return this.maxhp; // please just read .maxhp directly
 
 		// base stat
 		var stat = this.stats[statName];
-		if (unmodified) return stat;
-
-		// stat modifier effects
-		var statTable = {atk:'Atk', def:'Def', spa:'SpA', spd:'SpD', spe:'Spe'};
-		stat = this.battle.runEvent('Modify'+statTable[statName], this, null, null, stat);
-		stat = Math.floor(stat);
-
-		if (unboosted) return stat;
 
 		// stat boosts
-		boost = this.battle.runEvent('ModifyBoost', this, null, null, boost);
+		// boost = this.boosts[statName];
 		var boostTable = [1,1.5,2,2.5,3,3.5,4];
 		if (boost > 6) boost = 6;
 		if (boost < -6) boost = -6;
@@ -426,6 +419,9 @@ var BattlePokemon = (function() {
 		} else {
 			stat = Math.floor(stat / boostTable[-boost]);
 		}
+
+		// stat modifier
+		stat = this.battle.modify(stat, (modifier || 1));
 
 		if (this.battle.getStatCallback) {
 			stat = this.battle.getStatCallback(stat, statName, this);
@@ -1354,6 +1350,7 @@ var Battle = (function() {
 		m = Math.floor(m);
 		n = Math.floor(n);
 		result = (m ? (n ? Math.floor(result*(n-m) / 0x100000000)+m : Math.floor(result*m / 0x100000000)) : result/0x100000000);
+		this.debug('randBW(' + (m ? (n ? m + ',' + n : m) : '') + ') = ' + result);
 		return result;
 	};
 
@@ -2506,6 +2503,14 @@ var Battle = (function() {
 		this.runEvent('Heal', target, source, effect, damage);
 		return damage;
 	};
+	Battle.prototype.chain = function(previousMod, nextMod) {
+		// previousMod or nextMod can be either a number or an array [numerator, denominator]
+		if (previousMod.length) previousMod = Math.floor(previousMod[0] * 4096 / previousMod[1]);
+		else previousMod = Math.floor(previousMod * 4096);
+		if (nextMod.length) nextMod = Math.floor(nextMod[0] * 4096 / nextMod[1]);
+		else nextMod = Math.floor(nextMod * 4096);
+		return ((previousMod * nextMod + 2048) >> 12) / 4096; // M'' = ((M * M') + 0x800) >> 12
+	}
 	Battle.prototype.modify = function(value, numerator, denominator) {
 		// You can also use:
 		// modify(value, [numerator, denominator])
@@ -2620,11 +2625,26 @@ var Battle = (function() {
 
 		var attacker = pokemon;
 		var defender = target;
+		var attackStat = category === 'Physical' ? 'atk' : 'spa';
+		var defenseStat = defensiveCategory === 'Physical' ? 'def' : 'spd';
+		var statTable = {atk:'Atk', def:'Def', spa:'SpA', spd:'SpD', spe:'Spe'};
+		var attack;
+		var defense;
+		/*
 		if (move.useTargetOffensive) attacker = target;
 		if (move.useSourceDefensive) defender = pokemon;
+		*/
 
-		var attack = attacker.getStat(category==='Physical'?'atk':'spa');
-		var defense = defender.getStat(defensiveCategory==='Physical'?'def':'spd');
+		var atkStatMod = 1;
+		atkStatMod = this.runEvent('Modify'+statTable[attackStat], attacker, defender, move, atkStatMod);
+		var defStatMod = 1;
+		defStatMod = this.runEvent('Modify'+statTable[defenseStat], attacker, defender, move, defStatMod);
+		
+		if (move.useTargetOffensive) attack = defender.getStat(attackStat, defender.boosts[attackStat], atkStatMod);
+		else attack = attacker.getStat(attackStat, attacker.boosts[attackStat], atkStatMod);
+		
+		if (move.useSourceDefensive) defense = attacker.getStat(defenseStat, attacker.boosts[defenseStat], defStatMod);
+		else defense = defender.getStat(defenseStat, defender.boosts[defenseStat], defStatMod);
 
 		var ignoreNegativeOffensive = !!move.ignoreNegativeOffensive;
 		var ignorePositiveDefensive = !!move.ignorePositiveDefensive;
@@ -2632,19 +2652,19 @@ var Battle = (function() {
 			ignoreNegativeOffensive = true;
 			ignorePositiveDefensive = true;
 		}
-		if (ignoreNegativeOffensive && attack < attacker.getStat(category==='Physical'?'atk':'spa', true)) {
+		if (ignoreNegativeOffensive && (move.useTargetOffensive ? defender.boosts[attackStat] : attacker.boosts[attackStat]) < 0) {
 			move.ignoreOffensive = true;
 		}
 		if (move.ignoreOffensive) {
 			this.debug('Negating (sp)atk boost/penalty.');
-			attack = attacker.getStat(category==='Physical'?'atk':'spa', true);
+			attack = attacker.getStat(attackStat, 0, atkStatMod);
 		}
-		if (ignorePositiveDefensive && defense > target.getStat(defensiveCategory==='Physical'?'def':'spd', true)) {
+		if (ignorePositiveDefensive && (move.useSourceDefensive ? attacker.boosts[defenseStat] : defender.boosts[defenseStat]) > 0) {
 			move.ignoreDefensive = true;
 		}
 		if (move.ignoreDefensive) {
 			this.debug('Negating (sp)def boost/penalty.');
-			defense = target.getStat(defensiveCategory==='Physical'?'def':'spd', true);
+			defense = target.getStat(defenseStat, 0, defStatMod);
 		}
 
 		//int(int(int(2*L/5+2)*A*P/D)/50);
@@ -2666,6 +2686,9 @@ var Battle = (function() {
 
 		// randomizer
 		// this is not a modifier
+		// gen 1-2
+		//var randFactor = Math.floor(Math.random()*39)+217;
+		//baseDamage *= Math.floor(randFactor * 100 / 255) / 100;
 		baseDamage = Math.floor(baseDamage * (100 - this.random(16)) / 100);
 
 		// STAB
