d7a17245d6e01d0e6c5cde8be7982b1c266e272d
Guangcong Luo <guangcongluo@gmail.com>
2013-09-14 10:50:20 -0700

    Documentation for dnsbl.js

--- dnsbl.js
+++ dnsbl.js
@@ -1,9 +1,30 @@
+/**
+ * DNSBL support [OPTIONAL]
+ * Pokemon Showdown - http://pokemonshowdown.com/
+ *
+ * This file controls support for DNSBLs. It's optional, so it can be
+ * removed entirely safely.
+ *
+ * DNSBLs are DNS-based blackhole lists, which list IPs known for
+ * running proxies, spamming, or other abuse. By default, PS will lock
+ * users using these IPs.
+ *
+ * @license MIT license
+ */
+
 var dns = require('dns');
 
 var blocklists = ['sbl.spamhaus.org', 'rbl.efnetrbl.org'];
 
 var dnsblCache = {};
 
+/**
+ * Dnsbl.query(ip, callback)
+ *
+ * Calls callback(blocklist), where blocklist is the blocklist domain
+ * if the passed IP is in a blocklist, or boolean false if the IP is
+ * not in any blocklist.
+ */
 exports.query = function queryDnsbl(ip, callback) {
 	if (ip in dnsblCache) {
 		callback(dnsblCache[ip]);
