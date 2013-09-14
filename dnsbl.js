8cedc5e1657b2ba4497fcfd260e5bb2726e4f59c
jd4564 <thelittlejd09@gmail.com>
2013-09-14 04:11:25 -0300

    Don't check IP addresses in Spamhaus ZEN DNSBL

--- dnsbl.js
+++ dnsbl.js
@@ -1,30 +1,24 @@
 var dns = require('dns');
-
 var dnsblCache = {};
+
 exports.query = function queryDnsbl(ip, callback) {
 	if (ip in dnsblCache) {
-		callback(dnsblCache[ip]);
+		var reason = "Your IP is on our abuse list and is permanently banned. If you are using a proxy, stop.";
+		callback(dnsblCache[ip], reason);
 		return;
 	}
 	var reversedIp = ip.split('.').reverse().join('.');
-	dns.resolve4(reversedIp+'.zen.spamhaus.org', function(err, addresses) {
-		if (!err && addresses[0] == "127.0.0.4") return; 
+	dns.resolve4(reversedIp+'.dnsbl.dronebl.org', function(err, addresses) {
 		var isBlocked = dnsblCache[ip] = !err;
-		var reason = "Your IP is listed in zen.spamhaus.org.\nFor more information, visit:\nhttp://www.spamhaus.org/query/ip/"+ip;
+		var reason = "Your IP address is listed in DroneBL.\nFor more information, visit:\nhttp://dronebl.org/lookup?ip="+ip;
 		callback(isBlocked, reason);
 		if (err) {
-			dns.resolve4(reversedIp+'.dnsbl.dronebl.org', function(err, addresses) {
+			dns.resolve4(reversedIp+'.rbl.efnetrbl.org', function(err, addresses) {
 				var isBlocked = dnsblCache[ip] = !err;
-				var reason = "Your IP address is listed in DroneBL.\nFor more information, visit:\nhttp://dronebl.org/lookup?ip="+ip;
+				var reason = "Your IP address is listed in EFnet RBL.\nFor more information, visit:\nhttp://rbl.efnetrbl.org/?i="+ip;
 				callback(isBlocked, reason);
-				if (err) {
-					dns.resolve4(reversedIp+'.rbl.efnetrbl.org', function(err, addresses) {
-						var isBlocked = dnsblCache[ip] = !err;
-						var reason = "Your IP address is listed in EFnet RBL.\nFor more information, visit:\nhttp://rbl.efnetrbl.org/?i="+ip;
-						callback(isBlocked, reason);
-					});
-				}
 			});
 		}
 	});
 }
+
