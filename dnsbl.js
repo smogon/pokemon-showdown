8b1c4a59d9bac8a9996615c2ee52383bf937bc08
Guangcong Luo <guangcongluo@gmail.com>
2013-09-14 10:40:30 -0700

    Extend dnsbl.js to support multiple blocklists

--- dnsbl.js
+++ dnsbl.js
@@ -1,6 +1,6 @@
 var dns = require('dns');
 
-var blocklist = 'sbl.spamhaus.org';
+var blocklists = ['sbl.spamhaus.org', 'rbl.efnetrbl.org'];
 
 var dnsblCache = {};
 
@@ -9,9 +9,24 @@ exports.query = function queryDnsbl(ip, callback) {
 		callback(dnsblCache[ip]);
 		return;
 	}
-	var reversedIp = ip.split('.').reverse().join('.');
-	dns.resolve4(reversedIp+'.'+blocklist, function(err, addresses) {
-		var isBlocked = dnsblCache[ip] = !err;
-		callback(isBlocked);
+	var reversedIpDot = ip.split('.').reverse().join('.')+'.';
+	queryDnsblLoop(ip, callback, reversedIpDot, 0);
+}
+
+function queryDnsblLoop(ip, callback, reversedIpDot, index) {
+	if (index >= blocklists.length) {
+		// not in any blocklist
+		callback(dnsblCache[ip] = false);
+		return;
+	}
+	var blocklist = blocklists[index];
+	dns.resolve4(reversedIpDot+blocklist, function(err, addresses) {
+		if (!err) {
+			// blocked
+			callback(dnsblCache[ip] = blocklist);
+		} else {
+			// not blocked, try next blocklist
+			queryDnsblLoop(ip, callback, reversedIpDot, index+1);
+		}
 	});
 }
