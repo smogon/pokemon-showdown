225195512af74eb5a8cdc1ea6e4e267dec2dbaeb
The Immortal <the_immortal123@live.com>
2013-09-20 16:27:04 +0400

    Revert "UU Suspect Test is over"
    This reverts commit 08a802d1b72be0fc90e13905ae9c20b5081376bc.

--- config/formats.js
+++ config/formats.js
@@ -47,8 +47,16 @@ exports.Formats = [
 		name: "UU",
 		section: "Singles",
 		
+		searchShow: false,
 		ruleset: ['OU'],
 		banlist: ['OU', 'BL', 'Drought', 'Sand Stream']
+	},
+	{
+		name: "UU (suspect test)",
+		section: "Singles",
+
+		ruleset: ['OU'],
+		banlist: ['OU', 'BL', 'Drought', 'Sand Stream', 'Victini']
 	},
 	{
 		name: "RU",
