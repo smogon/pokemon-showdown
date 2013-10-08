e74030b6bbb800055b082cfdf608abd2c77d5557
jd4564 <thelittlejd09@gmail.com>
2013-10-08 15:47:30 -0300

    fix /restart bug

--- restart.sh
+++ restart.sh
@@ -3,6 +3,6 @@ if  pgrep node > /dev/null
 then
 	pkill node && screen -L -dmS pokemonshowdown node app.js &
 else 
-	screen -L -dmS node app.js &
+	screen -L -dmS pokemonshowdown node app.js &
 fi
 
