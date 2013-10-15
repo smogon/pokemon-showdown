ac256a18e91e3952e6150d9b3e6c0f32786a87e7
Juanma Serrano <joimpoke@gmail.com>
2013-10-15 09:35:57 +0200

    Gen 5.5: Add Team Preview

--- config/formats.js
+++ config/formats.js
@@ -307,7 +307,7 @@ exports.Formats = [
 		name: "Generation 5.5",
 		section: "OM of the Month",
 
-		ruleset: ['Pokemon', 'HP Percentage Mod', 'Sleep Clause Mod', 'Species Clause', 'OHKO Clause'],
+		ruleset: ['Pokemon', 'HP Percentage Mod', 'Sleep Clause Mod', 'Species Clause', 'OHKO Clause', 'Team Preview'],
 		banlist: ['Unreleased', 'Illegal', 'Uber', 'Limbo']
 	},
 	{
