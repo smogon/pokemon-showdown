5ad1ce6d73cc23556b259a23191b285ba94a2730
Joimer <joimpoke@gmail.com>
2013-10-16 10:47:09 +0200

    Gen 5: Add abilities with gen 6 changes Oblivious and Overcoat have been updated on Gen 6, add them on Gen 5 mod so they keep working in gen 5 as before.

---
+++ mods/gen5/abilities.js
@@ -0,0 +1,21 @@
+exports.BattleAbilities = {
+	"oblivious": {
+		inherit: true,
+		onUpdate: function(pokemon) {
+			if (pokemon.volatiles['attract']) {
+				pokemon.removeVolatile('attract');
+				this.add("-message", pokemon.name+" got over its infatuation. (placeholder)");
+			}
+		},
+		onTryHit: function(pokemon, target, move) {
+			if (move.id === 'captivate') {
+				this.add('-immune', pokemon, '[msg]', '[from] Oblivious');
+				return null;
+			}
+		}
+	},
+	"overcoat": {
+		inherit: true,
+		onTryHit: function() {}
+	}
+};
