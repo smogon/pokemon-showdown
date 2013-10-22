5f0e5a5e768b56df168f8a210671784be9d64244
Joimer <joimpoke@gmail.com>
2013-10-22 16:05:55 +0200

    Update Pokedex Information from: http://www.smogon.com/forums/threads/oldmon-base-stat-changes.3490041/page-3 Added Golem and Staraptor since they're given proof. Updated old gen mods Pokedex to include the old base stats. Updated the format on Pokedex so it uses inherit.

--- mods/gen5/pokedex.js
+++ mods/gen5/pokedex.js
@@ -1,45 +1,223 @@
 exports.BattlePokedex = {
-butterfree:{num:12,species:"Butterfree",types:["Bug","Flying"],baseStats:{hp:60,atk:45,def:50,spa:80,spd:80,spe:70},abilities:{0:"Compoundeyes",H:"Tinted Lens"},heightm:1.1,weightkg:32,color:"White",prevo:"metapod",evoLevel:10,eggGroups:["Bug"]},
-beedrill:{num:15,species:"Beedrill",types:["Bug","Poison"],baseStats:{hp:65,atk:80,def:40,spa:45,spd:80,spe:75},abilities:{0:"Swarm",H:"Sniper"},heightm:1,weightkg:29.5,color:"Yellow",prevo:"kakuna",evoLevel:10,eggGroups:["Bug"]},
-pikachu:{num:25,species:"Pikachu",types:["Electric"],baseStats:{hp:35,atk:55,def:30,spa:50,spd:40,spe:90},abilities:{0:"Static",H:"Lightningrod"},heightm:0.4,weightkg:6,color:"Yellow",prevo:"pichu",evos:["raichu"],evoLevel:1,eggGroups:["Ground","Fairy"]},
-raichu:{num:26,species:"Raichu",types:["Electric"],baseStats:{hp:60,atk:90,def:55,spa:90,spd:80,spe:100},abilities:{0:"Static",H:"Lightningrod"},heightm:0.8,weightkg:30,color:"Yellow",prevo:"pikachu",evoLevel:1,eggGroups:["Ground","Fairy"]},
-clefairy:{num:35,species:"Clefairy",types:["Normal"],genderRatio:{M:0.25,F:0.75},baseStats:{hp:70,atk:45,def:48,spa:60,spd:65,spe:35},abilities:{0:"Cute Charm",1:"Magic Guard",H:"Friend Guard"},heightm:0.6,weightkg:7.5,color:"Pink",prevo:"cleffa",evos:["clefable"],evoLevel:1,eggGroups:["Fairy"]},
-clefable:{num:36,species:"Clefable",types:["Normal"],genderRatio:{M:0.25,F:0.75},baseStats:{hp:95,atk:70,def:73,spa:85,spd:90,spe:60},abilities:{0:"Cute Charm",1:"Magic Guard",H:"Unaware"},heightm:1.3,weightkg:40,color:"Pink",prevo:"clefairy",evoLevel:1,eggGroups:["Fairy"]},
-jigglypuff:{num:39,species:"Jigglypuff",types:["Normal"],genderRatio:{M:0.25,F:0.75},baseStats:{hp:115,atk:45,def:20,spa:45,spd:25,spe:20},abilities:{0:"Cute Charm",H:"Friend Guard"},heightm:0.5,weightkg:5.5,color:"Pink",prevo:"igglybuff",evos:["wigglytuff"],evoLevel:1,eggGroups:["Fairy"]},
-wigglytuff:{num:40,species:"Wigglytuff",types:["Normal"],genderRatio:{M:0.25,F:0.75},baseStats:{hp:140,atk:70,def:45,spa:75,spd:50,spe:45},abilities:{0:"Cute Charm",H:"Frisk"},heightm:1,weightkg:12,color:"Pink",prevo:"jigglypuff",evoLevel:1,eggGroups:["Fairy"]},
-alakazam:{num:65,species:"Alakazam",types:["Psychic"],genderRatio:{M:0.75,F:0.25},baseStats:{hp:55,atk:50,def:45,spa:135,spd:85,spe:120},abilities:{0:"Synchronize",1:"Inner Focus",H:"Magic Guard"},heightm:1.5,weightkg:48,color:"Brown",prevo:"kadabra",evoLevel:16,eggGroups:["Humanshape"]},
-mrmime:{num:122,species:"Mr. Mime",types:["Psychic"],baseStats:{hp:40,atk:45,def:65,spa:100,spd:120,spe:90},abilities:{0:"Soundproof",1:"Filter",H:"Technician"},heightm:1.3,weightkg:54.5,color:"Pink",prevo:"mimejr",evoLevel:1,evoMove:"Mimic",eggGroups:["Humanshape"]},
-igglybuff:{num:174,species:"Igglybuff",types:["Normal"],genderRatio:{M:0.25,F:0.75},baseStats:{hp:90,atk:30,def:15,spa:40,spd:20,spe:15},abilities:{0:"Cute Charm",H:"Friend Guard"},heightm:0.3,weightkg:1,color:"Pink",evos:["jigglypuff"],eggGroups:["No Eggs"]},
-togepi:{num:175,species:"Togepi",types:["Normal"],genderRatio:{M:0.875,F:0.125},baseStats:{hp:35,atk:20,def:65,spa:40,spd:65,spe:20},abilities:{0:"Hustle",1:"Serene Grace",H:"Super Luck"},heightm:0.3,weightkg:1.5,color:"White",evos:["togetic"],eggGroups:["No Eggs"]},
-togetic:{num:176,species:"Togetic",types:["Normal","Flying"],genderRatio:{M:0.875,F:0.125},baseStats:{hp:55,atk:40,def:85,spa:80,spd:105,spe:40},abilities:{0:"Hustle",1:"Serene Grace",H:"Super Luck"},heightm:0.6,weightkg:3.2,color:"White",prevo:"togepi",evos:["togekiss"],evoLevel:2,eggGroups:["Flying","Fairy"]},
-cleffa:{num:173,species:"Cleffa",types:["Normal"],genderRatio:{M:0.25,F:0.75},baseStats:{hp:50,atk:25,def:28,spa:45,spd:55,spe:15},abilities:{0:"Cute Charm",1:"Magic Guard",H:"Friend Guard"},heightm:0.3,weightkg:3,color:"Pink",evos:["clefairy"],eggGroups:["No Eggs"]},
-marill:{num:183,species:"Marill",types:["Water"],baseStats:{hp:70,atk:20,def:50,spa:20,spd:50,spe:40},abilities:{0:"Thick Fat",1:"Huge Power",H:"Sap Sipper"},heightm:0.4,weightkg:8.5,color:"Blue",prevo:"azurill",evos:["azumarill"],evoLevel:1,eggGroups:["Water 1","Fairy"]},
-azumarill:{num:184,species:"Azumarill",types:["Water"],baseStats:{hp:100,atk:50,def:80,spa:50,spd:80,spe:50},abilities:{0:"Thick Fat",1:"Huge Power",H:"Sap Sipper"},heightm:0.8,weightkg:28.5,color:"Blue",prevo:"marill",evoLevel:18,eggGroups:["Water 1","Fairy"]},
-snubbull:{num:209,species:"Snubbull",types:["Normal"],genderRatio:{M:0.25,F:0.75},baseStats:{hp:60,atk:80,def:50,spa:40,spd:40,spe:30},abilities:{0:"Intimidate",1:"Run Away",H:"Rattled"},heightm:0.6,weightkg:7.8,color:"Pink",evos:["granbull"],eggGroups:["Ground","Fairy"]},
-granbull:{num:210,species:"Granbull",types:["Normal"],genderRatio:{M:0.25,F:0.75},baseStats:{hp:90,atk:120,def:75,spa:60,spd:60,spe:45},abilities:{0:"Intimidate",1:"Quick Feet",H:"Rattled"},heightm:1.4,weightkg:48.7,color:"Purple",prevo:"snubbull",evoLevel:23,eggGroups:["Ground","Fairy"]},
-ralts:{num:280,species:"Ralts",types:["Psychic"],baseStats:{hp:28,atk:25,def:25,spa:45,spd:35,spe:40},abilities:{0:"Synchronize",1:"Trace",H:"Telepathy"},heightm:0.4,weightkg:6.6,color:"White",evos:["kirlia"],eggGroups:["Indeterminate"]},
-kirlia:{num:281,species:"Kirlia",types:["Psychic"],baseStats:{hp:38,atk:35,def:35,spa:65,spd:55,spe:50},abilities:{0:"Synchronize",1:"Trace",H:"Telepathy"},heightm:0.8,weightkg:20.2,color:"White",prevo:"ralts",evos:["gardevoir","gallade"],evoLevel:20,eggGroups:["Indeterminate"]},
-gardevoir:{num:282,species:"Gardevoir",types:["Psychic"],baseStats:{hp:68,atk:65,def:65,spa:125,spd:115,spe:80},abilities:{0:"Synchronize",1:"Trace",H:"Telepathy"},heightm:1.6,weightkg:48.4,color:"White",prevo:"kirlia",evoLevel:30,eggGroups:["Indeterminate"]},
-azurill:{num:298,species:"Azurill",types:["Normal"],genderRatio:{M:0.25,F:0.75},baseStats:{hp:50,atk:20,def:40,spa:20,spd:40,spe:20},abilities:{0:"Thick Fat",1:"Huge Power",H:"Sap Sipper"},heightm:0.2,weightkg:2,color:"Blue",evos:["marill"],eggGroups:["No Eggs"]},
-mawile:{num:303,species:"Mawile",types:["Steel"],baseStats:{hp:50,atk:85,def:85,spa:55,spd:55,spe:50},abilities:{0:"Hyper Cutter",1:"Intimidate",H:"Sheer Force"},heightm:0.6,weightkg:11.5,color:"Black",eggGroups:["Ground","Fairy"]},
-plusle:{num:311,species:"Plusle",types:["Electric"],baseStats:{hp:60,atk:50,def:40,spa:85,spd:75,spe:95},abilities:{0:"Plus"},heightm:0.4,weightkg:4.2,color:"Yellow",eggGroups:["Fairy"]},
-minun:{num:312,species:"Minun",types:["Electric"],baseStats:{hp:60,atk:40,def:50,spa:75,spd:85,spe:95},abilities:{0:"Minus"},heightm:0.4,weightkg:4.2,color:"Yellow",eggGroups:["Fairy"]},
-kecleon:{num:352,species:"Kecleon",types:["Normal"],baseStats:{hp:60,atk:90,def:70,spa:60,spd:120,spe:40},abilities:{0:"Color Change"},heightm:1,weightkg:22,color:"Green",eggGroups:["Ground"]},
-duskull:{num:355,species:"Duskull",types:["Ghost"],baseStats:{hp:20,atk:40,def:90,spa:30,spd:90,spe:25},abilities:{0:"Levitate"},heightm:0.8,weightkg:15,color:"Black",evos:["dusclops"],eggGroups:["Indeterminate"]},
-dusclops:{num:356,species:"Dusclops",types:["Ghost"],baseStats:{hp:40,atk:70,def:130,spa:60,spd:130,spe:25},abilities:{0:"Pressure"},heightm:1.6,weightkg:30.6,color:"Black",prevo:"duskull",evos:["dusknoir"],evoLevel:37,eggGroups:["Indeterminate"]},
-starly:{num:396,species:"Starly",types:["Normal","Flying"],baseStats:{hp:40,atk:55,def:30,spa:30,spd:30,spe:60},abilities:{0:"Keen Eye"},heightm:0.3,weightkg:2,color:"Brown",evos:["staravia"],eggGroups:["Flying"]},
-togekiss:{num:468,species:"Togekiss",types:["Normal","Flying"],genderRatio:{M:0.875,F:0.125},baseStats:{hp:85,atk:50,def:95,spa:120,spd:115,spe:80},abilities:{0:"Hustle",1:"Serene Grace",H:"Super Luck"},heightm:1.5,weightkg:38,color:"White",prevo:"togetic",evoLevel:2,eggGroups:["Flying","Fairy"]},
-dusknoir:{num:477,species:"Dusknoir",types:["Ghost"],baseStats:{hp:45,atk:100,def:135,spa:65,spd:135,spe:45},abilities:{0:"Pressure"},heightm:2.2,weightkg:106.6,color:"Black",prevo:"dusclops",evoLevel:37,eggGroups:["Indeterminate"]},
-venipede:{num:543,species:"Venipede",types:["Bug","Poison"],baseStats:{hp:30,atk:45,def:59,spa:30,spd:39,spe:57},abilities:{0:"Poison Point",1:"Swarm",H:"Quick Feet"},heightm:0.4,weightkg:5.3,color:"Red",evos:["whirlipede"],eggGroups:["Bug"]},
-whirlipede:{num:544,species:"Whirlipede",types:["Bug","Poison"],baseStats:{hp:40,atk:55,def:99,spa:40,spd:79,spe:47},abilities:{0:"Poison Point",1:"Swarm",H:"Quick Feet"},heightm:1.2,weightkg:58.5,color:"Gray",prevo:"venipede",evos:["scolipede"],evoLevel:22,eggGroups:["Bug"]},
-scolipede:{num:545,species:"Scolipede",types:["Bug","Poison"],baseStats:{hp:60,atk:90,def:89,spa:55,spd:69,spe:112},abilities:{0:"Poison Point",1:"Swarm",H:"Quick Feet"},heightm:2.5,weightkg:200.5,color:"Red",prevo:"whirlipede",evoLevel:30,eggGroups:["Bug"]},
-whimsicott:{num:547,species:"Whimsicott",types:["Grass"],baseStats:{hp:60,atk:67,def:85,spa:77,spd:75,spe:116},abilities:{0:"Prankster",1:"Infiltrator",H:"Chlorophyll"},heightm:0.7,weightkg:6.6,color:"Green",prevo:"cottonee",evoLevel:1,eggGroups:["Fairy","Plant"]},
-gothita:{num:574,species:"Gothita",types:["Psychic"],genderRatio:{M:0.25,F:0.75},baseStats:{hp:45,atk:30,def:50,spa:55,spd:65,spe:45},abilities:{0:"Frisk",H:"Shadow Tag"},heightm:0.4,weightkg:5.8,color:"Purple",evos:["gothorita"],eggGroups:["Humanshape"]},
-gothorita:{num:575,species:"Gothorita",types:["Psychic"],genderRatio:{M:0.25,F:0.75},baseStats:{hp:60,atk:45,def:70,spa:75,spd:85,spe:55},abilities:{0:"Frisk",H:"Shadow Tag"},heightm:0.7,weightkg:18,color:"Purple",prevo:"gothita",evos:["gothitelle"],evoLevel:32,eggGroups:["Humanshape"]},
-gothitelle:{num:576,species:"Gothitelle",types:["Psychic"],genderRatio:{M:0.25,F:0.75},baseStats:{hp:70,atk:55,def:95,spa:95,spd:110,spe:65},abilities:{0:"Frisk",H:"Shadow Tag"},heightm:1.5,weightkg:44,color:"Purple",prevo:"gothorita",evoLevel:41,eggGroups:["Humanshape"]},
-ferroseed:{num:597,species:"Ferroseed",types:["Grass","Steel"],baseStats:{hp:44,atk:50,def:91,spa:24,spd:86,spe:10},abilities:{0:"Iron Barbs"},heightm:0.6,weightkg:18.8,color:"Gray",evos:["ferrothorn"],eggGroups:["Plant","Mineral"]},
-ferrothorn:{num:598,species:"Ferrothorn",types:["Grass","Steel"],baseStats:{hp:74,atk:94,def:131,spa:54,spd:116,spe:20},abilities:{0:"Iron Barbs"},heightm:1,weightkg:110,color:"Gray",prevo:"ferroseed",evoLevel:40,eggGroups:["Plant","Mineral"]},
-litwick:{num:607,species:"Litwick",types:["Ghost","Fire"],baseStats:{hp:50,atk:30,def:55,spa:65,spd:55,spe:20},abilities:{0:"Flash Fire",1:"Flame Body",H:"Shadow Tag"},heightm:0.3,weightkg:3.1,color:"White",evos:["lampent"],eggGroups:["Indeterminate"]},
-lampent:{num:608,species:"Lampent",types:["Ghost","Fire"],baseStats:{hp:60,atk:40,def:60,spa:95,spd:60,spe:55},abilities:{0:"Flash Fire",1:"Flame Body",H:"Shadow Tag"},heightm:0.6,weightkg:13,color:"Black",prevo:"litwick",evos:["chandelure"],evoLevel:41,eggGroups:["Indeterminate"]},
-chandelure:{num:609,species:"Chandelure",types:["Ghost","Fire"],baseStats:{hp:60,atk:55,def:90,spa:145,spd:90,spe:80},abilities:{0:"Flash Fire",1:"Flame Body",H:"Shadow Tag"},heightm:1,weightkg:34.3,color:"Black",prevo:"lampent",evoLevel:41,eggGroups:["Indeterminate"]}
+	butterfree: {
+		inherit: true,
+		baseStats: {hp:60,atk:45,def:50,spa:80,spd:80,spe:70},
+		abilities: {0:"Compoundeyes",H:"Tinted Lens"}
+	},
+	beedrill: {
+		inherit: true,
+		baseStats: {hp:65,atk:80,def:40,spa:45,spd:80,spe:75},
+		abilities: {0:"Swarm",H:"Sniper"}
+	},
+	pikachu: {
+		inherit: true,
+		baseStats: {hp:35,atk:55,def:30,spa:50,spd:40,spe:90},
+		abilities: {0:"Static",H:"Lightningrod"}
+	},
+	raichu: {
+		inherit: true,
+		baseStats: {hp:60,atk:90,def:55,spa:90,spd:80,spe:100},
+		abilities: {0:"Static",H:"Lightningrod"}
+	},
+	clefairy: {
+		inherit: true,
+		types:["Normal"],
+		baseStats: {hp:70,atk:45,def:48,spa:60,spd:65,spe:35},
+		abilities: {0:"Cute Charm",1:"Magic Guard",H:"Friend Guard"}
+	},
+	clefable: {
+		inherit: true,
+		types:["Normal"],
+		baseStats: {hp:95,atk:70,def:73,spa:85,spd:90,spe:60},
+		abilities: {0:"Cute Charm",1:"Magic Guard",H:"Unaware"}
+	},
+	jigglypuff: {
+		inherit: true,
+		types:["Normal"],
+		abilities: {0:"Cute Charm",H:"Friend Guard"}
+	},
+	wigglytuff: {
+		inherit: true,
+		types:["Normal"],
+		baseStats: {hp:140,atk:70,def:45,spa:75,spd:50,spe:45},
+		abilities: {0:"Cute Charm",H:"Frisk"}
+	},
+	alakazam: {
+		inherit: true,
+		baseStats: {hp:55,atk:50,def:45,spa:135,spd:85,spe:120},
+		abilities: {0:"Synchronize",1:"Inner Focus",H:"Magic Guard"}
+	},
+	golem: {
+		inherit: true,
+		baseStats: {hp:80,atk:120,def:130,spa:55,spd:65,spe:45},
+		abilities: {0:"Rock Head",1:"Sturdy",H:"Sand Veil"}
+	},
+	mrmime: {
+		inherit: true,
+		baseStats: {hp:40,atk:45,def:65,spa:100,spd:120,spe:90},
+		abilities: {0:"Soundproof",1:"Filter",H:"Technician"}
+	},
+	igglybuff: {
+		inherit: true,
+		abilities: {0:"Cute Charm",H:"Friend Guard"}
+	},
+	togepi: {
+		inherit: true,
+		baseStats: {hp:35,atk:20,def:65,spa:40,spd:65,spe:20},
+		abilities: {0:"Hustle",1:"Serene Grace",H:"Super Luck"}
+	},
+	togetic: {
+		inherit: true,
+		baseStats: {hp:55,atk:40,def:85,spa:80,spd:105,spe:40},
+		abilities: {0:"Hustle",1:"Serene Grace",H:"Super Luck"}
+	},
+	cleffa: {
+		inherit: true,
+		baseStats: {hp:50,atk:25,def:28,spa:45,spd:55,spe:15},
+		abilities: {0:"Cute Charm",1:"Magic Guard",H:"Friend Guard"}
+	},
+	marill: {
+		inherit: true,
+		types:["Water"],
+		baseStats: {hp:70,atk:20,def:50,spa:20,spd:50,spe:40},
+		abilities: {0:"Thick Fat",1:"Huge Power",H:"Sap Sipper"}
+	},
+	azumarill: {
+		inherit: true,
+		types:["Water"],
+		baseStats: {hp:100,atk:50,def:80,spa:50,spd:80,spe:50},
+		abilities: {0:"Thick Fat",1:"Huge Power",H:"Sap Sipper"}
+	},
+	snubbull: {
+		inherit: true,
+		types:["Normal"],
+		baseStats: {hp:60,atk:80,def:50,spa:40,spd:40,spe:30},
+		abilities: {0:"Intimidate",1:"Run Away",H:"Rattled"}
+	},
+	granbull: {
+		inherit: true,
+		types:["Normal"],
+		baseStats: {hp:90,atk:120,def:75,spa:60,spd:60,spe:45},
+		abilities: {0:"Intimidate",1:"Quick Feet",H:"Rattled"}
+	},
+	ralts: {
+		inherit: true,
+		types:["Psychic"],
+		baseStats: {hp:28,atk:25,def:25,spa:45,spd:35,spe:40},
+		abilities: {0:"Synchronize",1:"Trace",H:"Telepathy"}
+	},
+	kirlia: {
+		inherit: true,
+		types:["Psychic"],
+		baseStats: {hp:38,atk:35,def:35,spa:65,spd:55,spe:50},
+		abilities: {0:"Synchronize",1:"Trace",H:"Telepathy"}
+	},
+	gardevoir: {
+		inherit: true,
+		types:["Psychic"],
+		baseStats: {hp:68,atk:65,def:65,spa:125,spd:115,spe:80},
+		abilities: {0:"Synchronize",1:"Trace",H:"Telepathy"}
+	},
+	azurill: {
+		inherit: true,
+		types:["Normal"],
+		baseStats: {hp:50,atk:20,def:40,spa:20,spd:40,spe:20},
+		abilities: {0:"Thick Fat",1:"Huge Power",H:"Sap Sipper"}
+	},
+	mawile: {
+		inherit: true,
+		baseStats: {hp:50,atk:85,def:85,spa:55,spd:55,spe:50},
+		abilities: {0:"Hyper Cutter",1:"Intimidate",H:"Sheer Force"}
+	},
+	plusle: {
+		inherit: true,
+		baseStats: {hp:60,atk:50,def:40,spa:85,spd:75,spe:95},
+		abilities: {0:"Plus"}
+	},
+	minun: {
+		inherit: true,
+		baseStats: {hp:60,atk:40,def:50,spa:75,spd:85,spe:95},
+		abilities: {0:"Minus"}
+	},
+	kecleon: {
+		inherit: true,
+		baseStats: {hp:60,atk:90,def:70,spa:60,spd:120,spe:40},
+		abilities: {0:"Color Change"}
+	},
+	duskull: {
+		inherit: true,
+		abilities: {0:"Levitate"}
+	},
+	dusclops: {
+		inherit: true,
+		abilities: {0:"Pressure"}
+	},
+	starly: {
+		inherit: true,
+		abilities: {0:"Keen Eye"}
+	},
+	staraptor: {
+		inherit: true,
+		baseStats: {hp:85,atk:120,def:70,spa:50,spd:50,spe:100},
+		abilities: {0:"Intimidate",H:"Reckless"}
+	},
+	togekiss: {
+		inherit: true,
+		types:["Normal","Flying"],
+		baseStats: {hp:85,atk:50,def:95,spa:120,spd:115,spe:80},
+		abilities: {0:"Hustle",1:"Serene Grace",H:"Super Luck"}
+	},
+	dusknoir: {
+		inherit: true,
+		abilities: {0:"Pressure"}
+	},
+	venipede: {
+		inherit: true,
+		abilities: {0:"Poison Point",1:"Swarm",H:"Quick Feet"}
+	},
+	whirlipede: {
+		inherit: true,
+		abilities: {0:"Poison Point",1:"Swarm",H:"Quick Feet"}
+	},
+	scolipede: {
+		inherit: true,
+		baseStats: {hp:60,atk:90,def:89,spa:55,spd:69,spe:112},
+		abilities: {0:"Poison Point",1:"Swarm",H:"Quick Feet"}
+	},
+	whimsicott: {
+		inherit: true,
+		baseStats: {hp:60,atk:67,def:85,spa:77,spd:75,spe:116},
+		abilities: {0:"Prankster",1:"Infiltrator",H:"Chlorophyll"}
+	},
+	gothita: {
+		inherit: true,
+		abilities: {0:"Frisk",H:"Shadow Tag"}
+	},
+	gothorita: {
+		inherit: true,
+		abilities: {0:"Frisk",H:"Shadow Tag"}
+	},
+	gothitelle: {
+		inherit: true,
+		abilities: {0:"Frisk",H:"Shadow Tag"}
+	},
+	ferroseed: {
+		inherit: true,
+		abilities: {0:"Iron Barbs"}
+	},
+	ferrothorn: {
+		inherit: true,
+		abilities: {0:"Iron Barbs"}
+	},
+	litwick: {
+		inherit: true,
+		abilities: {0:"Flash Fire",1:"Flame Body",H:"Shadow Tag"}
+	},
+	lampent: {
+		inherit: true,
+		abilities: {0:"Flash Fire",1:"Flame Body",H:"Shadow Tag"}
+	},
+	chandelure: {
+		abilities: {0:"Flash Fire",1:"Flame Body",H:"Shadow Tag"}
+	}
 };
