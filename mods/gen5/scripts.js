7b0a747fb2f48a7941655054663ee53f6f67b20e
Guangcong Luo <guangcongluo@gmail.com>
2013-11-03 22:27:03 -0800

    More efficient learnset checking

--- mods/gen5/scripts.js
+++ mods/gen5/scripts.js
@@ -1,16 +1,3 @@
 exports.BattleScripts = {
-	gen: 5,
-	init: function () {
-		for (var i in this.data.Learnsets) {
-			this.modData('Learnsets', i);
-			var learnset = this.data.Learnsets[i].learnset;
-			for (var moveid in learnset) {
-				if (typeof learnset[moveid] === 'string') learnset[moveid] = [learnset[moveid]];
-				learnset[moveid] = learnset[moveid].filter(function(source) {
-					return source[0] !== '6';
-				});
-				if (!learnset[moveid].length) delete learnset[moveid];
-			}
-		}
-	}
+	gen: 5
 };
