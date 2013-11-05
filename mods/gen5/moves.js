2cef2a5a8d80b7ec493e09792085dd125d6dd1cf
The Immortal <the_immortal123@live.com>
2013-11-05 21:21:04 +0400

    Knock Off is boosted by 50% if the target has an item

--- mods/gen5/moves.js
+++ mods/gen5/moves.js
@@ -292,8 +292,32 @@ exports.BattleMovedex = {
 		}
 	},
 	knockoff: {
-		inherit: true,
-		basePower: 20
+		num: 282,
+		accuracy: 100,
+		basePower: 20,
+		category: "Physical",
+		desc: "Deals damage to one adjacent target and causes it to drop its held item. This move cannot force Pokemon with the Ability Sticky Hold to lose their held item, or force a Giratina, an Arceus, or a Genesect to lose their Griseous Orb, Plate, or Drive, respectively. Items lost to this move cannot be regained with Recycle. Makes contact.",
+		shortDesc: "Removes the target's held item.",
+		id: "knockoff",
+		isViable: true,
+		name: "Knock Off",
+		pp: 20,
+		priority: 0,
+		isContact: true,
+		onHit: function(target, source) {
+			var item = target.getItem();
+			if (item.id === 'mail') {
+				target.setItem('');
+			} else {
+				item = target.takeItem(source);
+			}
+			if (item) {
+				this.add('-enditem', target, item.name, '[from] move: Knock Off', '[of] '+source);
+			}
+		},
+		secondary: false,
+		target: "normal",
+		type: "Dark"
 	},
 	leafstorm: {
 		inherit: true,
