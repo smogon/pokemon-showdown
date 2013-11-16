ab893548f38c1e38641f014adb97200961dd47eb
piiiikachuuu <acitrusfruit42@gmail.com>
2013-11-15 21:05:54 -0500

    Fix typo in coins.

--- commands.js
+++ commands.js
@@ -1017,7 +1017,7 @@ var commands = exports.commands = {
 			log.write("\n"+targetUser.userid+','+targetUser.coins);
 		}
 		var p = 'coins';
-		if (giveCoins < 2) p = 'coin';
+		if (takeCoins < 2) p = 'coin';
 		this.sendReply(targetUser.name + ' was had ' + takeCoins + ' ' + p + ' removed. This user now has ' + targetUser.coins + ' coins.');
 		targetUser.send(user.name + ' has given you ' + takeCoins + ' ' + p + '.');
 		} else {
