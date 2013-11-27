2e23b1a09d0529054375c53942bf43c0234d76de
BrittleWind <aaronthomsen21@gmail.com>
2013-11-26 17:05:51 -0800

    (TC) SmoothMoves TC

--- config/commands.js
+++ config/commands.js
@@ -1198,6 +1198,11 @@ var commands = exports.commands = {
         if (!this.canBroadcast()) return;
         this.sendReplyBox('<center><img src="http://i.imgur.com/tccxmCV.png"><br><img src="http://i.imgur.com/CvhWd2c.gif"><img src="http://i.imgur.com/lZ2fjGu.png"><br><font color="red"><blink>Ace: Tyranitar</blink></font><br>Don\'t send in your Latios. You know what\'s comin')
     },
+    
+   smoothmoves: function(target, room, user) {
+        if (!this.canBroadcast()) return;
+        this.sendReplyBox('<img src="http://www.sherv.net/cm/emo/funny/2/banana-smiley-emoticon.gif"><img src="http://i.imgur.com/7RTLz6V.gif"><img src="http://www.sherv.net/cm/emo/funny/2/dance.gif" height="60"><br /><center><font size="2"><b>Ace:</b> <font color="red">F</font><font color="orange">l</font><font color="green">u</font><font color="blue">f</font><font color="indigo">f</font><font color="violet">y</font> <font color="red">M</font><font color="orange">a</font><font color="green">r</font><font color="blue">e</font><font color="indigo">e</font><font color="violet">p</font><font color="red">s</font></center><br /><center><font size="2"><b>Catchphrase:</b> Time to split!</center><br />')
+    },
 
 	trainerbofish: function(target, room, user) {
         if (!this.canBroadcast()) return;
