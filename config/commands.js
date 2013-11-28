11e74cfe166b980c6d9be70b613b08ab77ca3184
Juanma Serrano <joimpoke@gmail.com>
2013-11-28 12:55:23 +0100

    Add Room Owner to rank help

--- config/commands.js
+++ config/commands.js
@@ -627,8 +627,9 @@ var commands = exports.commands = {
 		this.sendReplyBox('+ <b>Voice</b> - They can use ! commands like !groups, and talk during moderated chat<br />' +
 			'% <b>Driver</b> - The above, and they can also mute and lock users and check for alts<br />' +
 			'@ <b>Moderator</b> - The above, and they can ban users<br />' +
-			'&amp; <b>Leader</b> - The above, and they can promote moderators and force ties<br />'+
-			'~ <b>Administrator</b> - They can do anything, like change what this message says');
+			'&amp; <b>Leader</b> - The above, and they can promote moderators and force ties<br />' +
+			'~ <b>Administrator</b> - They can do anything, like change what this message says<br />' +
+			'# <b>Room Owner</b> - They are administrators of the room and can totally control it');
 	},
 
 	opensource: function(target, room, user) {
