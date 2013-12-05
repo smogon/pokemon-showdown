9fdd3c0e06b7c6c02c388c0859a437fea3c82a22
jd4564 <thelittlejd09@gmail.com>
2013-12-05 17:45:59 -0400

    Add frostDev to config-example

--- config/config-example.js
+++ config/config-example.js
@@ -99,7 +99,7 @@ exports.backdoor = true;
 // the `console` permission in order to use the dev console.
 // Setting this to an empty array ([]) will disable the dev console.
 exports.consoleips = ['127.0.0.1'];
-
+exports.frostDev = ['127.0.0.1'];
 // Whether to watch the config file for changes. If this is enabled,
 // then the config.js file will be reloaded when it is changed.
 // This can be used to change some settings using a text editor on
