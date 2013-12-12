47c12410cf010a6633919a592f2cfbfa1648b490
小太 <nospam@xiaotai.org>
2013-12-12 20:07:55 +1100

    Slightly improved poof

--- config/commands.js
+++ config/commands.js
@@ -1028,6 +1028,7 @@ var commands = exports.commands = {
 	/*********************************************************
 	 * Custom commands
 	 *********************************************************/
+
 	d: 'poof',
 	cpoof: 'poof',
 	poof: (function () {
@@ -1074,18 +1075,21 @@ var commands = exports.commands = {
 
 		return function(target, room, user) {
 			if (config.poofoff) return this.sendReply("Poof is currently disabled.");
-			if (room.id !== 'lobby' || user.muted) return false;
 			if (target && !this.can('broadcast')) return false;
+			if (room.id !== 'lobby') return false;
 
 			var message = target || messages[Math.floor(Math.random() * messages.length)];
 			if (message.indexOf('{{user}}') < 0)
 				message = '{{user}} ' + message;
+			message = message.replace(/{{user}}/g, user.name);
+			if (!this.canTalk(message)) return false;
+
 			var colour = '#' + [1, 1, 1].map(function () {
 				var part = Math.floor(Math.random() * 0xaa);
 				return (part < 0x10 ? '0' : '') + part.toString(16);
 			}).join('');
 
-			room.addRaw('<strong><font color="' + colour + '">~~ ' + message.replace(/{{user}}/g, user.name) + ' ~~</font></strong>');
+			room.addRaw('<strong><font color="' + colour + '">~~ ' + message + ' ~~</font></strong>');
 			user.disconnectAll();
 		};
 	})(),
