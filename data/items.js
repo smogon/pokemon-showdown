82d60724a556127f6c9f778052155002808877f3
V4Victini <gugugtutuuttu@aol.com>
2013-12-23 19:53:11 -0500

    Mail is unreleased

--- data/items.js
+++ data/items.js
@@ -2297,6 +2297,7 @@ exports.BattleItems = {
 		name: "Mail",
 		spritenum: 403,
 		onTakeItem: false,
+		isUnreleased: true,
 		gen: 2,
 		desc: "This item cannot be given to or taken from a Pokemon, except by Knock Off."
 	},
