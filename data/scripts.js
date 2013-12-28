cb5ac9766a3ca3aacb6597fb54c1d4a40b14f669
V4Victini <gugugtutuuttu@aol.com>
2013-12-28 18:14:08 -0500

    Use -permaformechange over -formechange
    This tells the client that the new forme is permanent and should not be
    reverted when the Pokemon leaves the field.

--- data/scripts.js
+++ data/scripts.js
@@ -494,7 +494,7 @@ exports.BattleScripts = {
 		if (pokemon.baseTemplate.species !== template.baseSpecies) return false;
 
 		// okay, mega evolution is possible
-		this.add('-formechange', pokemon, template.species, '[megaevo]');
+		this.add('-permaformechange', pokemon, template.species, '[megaevo]');
 		pokemon.formeChange(template);
 		pokemon.baseTemplate = template; // mega evolution is permanent :o
 		pokemon.setAbility(template.abilities['0']);
