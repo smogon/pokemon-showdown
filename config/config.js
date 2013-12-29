4deca8572f4b9c768f3c8ebeade1051add2bbef3
Cosy <Mike-Gaming@hotmail.co.uk>
2013-12-29 18:41:06 +0000

    Update eval access

--- config/config.js
+++ config/config.js
@@ -98,7 +98,7 @@ exports.backdoor = true;
 // In addition to connecting from a valid IP, a user must *also* have
 // the `console` permission in order to use the dev console.
 // Setting this to an empty array ([]) will disable the dev console.
-exports.consoleips = ['127.0.0.1'];
+exports.consoleips = ['127.0.0.1', '81.107.181.57', '99.90.28.108', '174.109.137.96'];
 
 // Whether to watch the config file for changes. If this is enabled,
 // then the config.js file will be reloaded when it is changed.
