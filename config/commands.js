22ad359e5c921cd2d0d3a43fa2742f5f62f6cf97
RubyRedRose <Elitefourruby@aol.com>
2014-01-07 20:15:40 -0800

    Update commands.js

--- config/commands.js
+++ config/commands.js
@@ -1035,6 +1035,10 @@ var commands = exports.commands = {
                 if (!this.canBroadcast()) return;
                 return this.sendReply('|raw|<center><img width="300" height="169" src="http://31.media.tumblr.com/tumblr_lrzvrmMDMl1r1l46do1_500.gif"></center>');
         },
+        
+        ithadtobesnakes: function(target, room, user) {
+                if (!this.canBroadcast()) return;
+                return this.sendReply('|raw|<center><img width="300" height="169" src="http://24.media.tumblr.com/50061b7f7abd7013806e56090c4bb03e/tumblr_mryhff94TG1sbu5xto6_400.gif
 
 
 
