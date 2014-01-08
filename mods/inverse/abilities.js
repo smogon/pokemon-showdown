0ddb4016dc8c71d25448d2ac4df2d7439ff14b05
Joimer <joimpoke@gmail.com>
2014-01-08 13:24:47 +0100

    Inverse Battle: Make grounded Flying-types able to be hit by hazards Since the exception to flying-types due to typechart changes must be done, the exception for those must be also added.

--- mods/inverse/abilities.js
+++ mods/inverse/abilities.js
@@ -2,12 +2,16 @@ exports.BattleAbilities = {
 	arenatrap: {
 		inherit: true,
 		onFoeModifyPokemon: function(pokemon) {
-			if (!pokemon.hasType('Flying') && pokemon.runImmunity('Ground', false)) {
+			// The pokémon is trapped if it is not flying type nor levitating or is either of the previous but
+			// it's holding an Iron Ball, there's Gravity on the field, or it's Ingrained
+			if ((!pokemon.hasType('Flying') || (pokemon.item === 'ironball' || pokemon.battle.pseudoWeather.gravity
+			|| pokemon.volatiles['ingrain'])) && pokemon.runImmunity('Ground', false)) {
 				pokemon.tryTrap();
 			}
 		},
 		onFoeMaybeTrapPokemon: function(pokemon) {
-			if (!pokemon.hasType('Flying') && pokemon.runImmunity('Ground', false)) {
+			if ((!pokemon.hasType('Flying') || (pokemon.item === 'ironball' || pokemon.battle.pseudoWeather.gravity
+			|| pokemon.volatiles['ingrain'])) && pokemon.runImmunity('Ground', false)) {
 				pokemon.maybeTrapped = true;
 			}
 		}
