smogon/pokemon-showdown

commit 9dd224b0ae7fc79becb3b06d3a7f42f50d9c6f47

Fix Quick and Wide Guard They were blocking ally moves. Furthermore, Quick Guard was also blocking foes´ moves not targetting the guarded side.

data/moves.js
@@ -10015,7 +10015,7 @@ exports.BattleMovedex = {
 			onTryHit: function(target, source, effect) {
 				// Quick Guard blocks moves with positive priority, even those given increased priority by Prankster or Gale Wings.
 				// (e.g. it blocks 0 priority moves boosted by Prankster or Gale Wings)
-				if (effect && (effect.id === 'feint' || effect.priority <= 0)) {
+				if (effect && (effect.id === 'feint' || effect.priority <= 0 || source.side === this.effectData.source.side || source.side === target.side)) {
 					return;
 				}
 				this.add('-activate', target, 'Quick Guard');
@@ -14592,7 +14592,7 @@ exports.BattleMovedex = {
 			onTryHitPriority: 4,
 			onTryHit: function(target, source, effect) {
 				// Wide Guard blocks damaging spread moves
-				if (effect && (effect.category === 'Status' || (effect.target !== 'allAdjacent' && effect.target !== 'allAdjacentFoes'))) {
+				if (effect && (effect.category === 'Status' || (effect.target !== 'allAdjacent' && effect.target !== 'allAdjacentFoes') || this.effectData.source.side === source.side)) {
 					return;
 				}
 				this.add('-activate', target, 'Wide Guard');

commit 9c1d1f56be6fbf55be35d5a859192f6d96691876

Fix Quick Guard not blocking friendly fire (Bug introduced in 9dd224b ). Overall, now Quick Guard does not block moves targetting the move user.

data/moves.js
@@ -10015,7 +10015,7 @@ exports.BattleMovedex = {
 			onTryHit: function(target, source, effect) {
 				// Quick Guard blocks moves with positive priority, even those given increased priority by Prankster or Gale Wings.
 				// (e.g. it blocks 0 priority moves boosted by Prankster or Gale Wings)
-				if (effect && (effect.id === 'feint' || effect.priority <= 0 || source.side === this.effectData.source.side || source.side === target.side)) {
+				if (effect && (effect.id === 'feint' || effect.priority <= 0 || effect.target === 'self')) {
 					return;
 				}
 				this.add('-activate', target, 'Quick Guard');