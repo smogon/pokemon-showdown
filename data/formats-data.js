866499d8d6a2174b74720f6f5872220881882cd3
Antar1011 <summer.kenster@gmail.com>
2014-02-03 05:48:53 -0500

    Kangaskhan also dropped to UU
    Missed it before.

--- data/formats-data.js
+++ data/formats-data.js
@@ -807,7 +807,7 @@ exports.BattleFormatsData = {
 			{"generation":3,"level":10,"gender":"F","abilities":["earlybird"],"moves":["cometpunch","leer","bite"]},
 			{"generation":3,"level":36,"gender":"F","abilities":["earlybird"],"moves":["sing","earthquake","tailwhip","dizzypunch"]}
 		],
-		tier: "OU"
+		tier: "UU"
 	},
 	kangaskhanmega: {
 		requiredItem: "Kangaskhanite"
