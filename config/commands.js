f0bdb62972974a4da6f23ede8e32c9a827eff6f5
jd4564 <thelittlejd09@gmail.com>
2014-02-03 02:12:43 -0400

    Zarif trainer card

--- config/commands.js
+++ config/commands.js
@@ -1195,6 +1195,15 @@ var commands = exports.commands = {
 	* Trainer Cards                        *
 	***************************************/
 
+	zarif: function(target, room, user) {
+		if (!this.canBroadcast()) return;
+		this.sendReplyBox(' <center><img src=http://i.imgur.com/lC0aRUH.gif>' +
+			'<img src=http://i.imgur.com/BPCyts3.png>' +
+			'<img src=http://i.imgur.com/3EIY2d9.png><br />' +
+			'<b> <blink> Ace: </b>Infernape</blink><br />' +
+			'Three things are infinite: magikarp\'s power, human stupidity and the fucking amount of zubats in a cave; and I\'m not sure about the universe.');
+	},
+
 	cark: 'amglcark',
 	amglcark: function(target, room, user) {
 		if (!this.canBroadcast()) return;
