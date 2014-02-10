1c2ec824d97484ef7e8cb7ae61deab4f1665290c
FrPSServ <frostservps@gmail.com>
2014-02-10 22:55:15 +0000

    Epin card

--- config/commands.js
+++ config/commands.js
@@ -1584,6 +1584,12 @@ var commands = exports.commands = {
     	this.sendReplyBox('<center><img src="http://i.imgur.com/lwL5Pce.png"><img src="http://i.imgur.com/D9M6VGi.gif"><img src="http://i.imgur.com/hZ0mB0U.png"><br><b>Ace: <font color="#58ACFA"><blink>Ace: Farfetch\'d</blink><br></font><b><font color="#00BFFF">Where can I find a leek in Pokemon Y?');
     },
     
+    epin: 'epinicion',
+    epinicion: function(target, room, user) {
+    	if (!this.canBroadcast()) return;
+    	this.sendReplyBox('<center><img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/crustle.gif"><img src="http://i.imgur.com/5aLcrWN.png"><img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/mew.gif"><br><b>Ace: </b>Crustle<br><b>Quote: </b>Si Vis Pacem, Para Bellum</center>');
+    },
+    
     belle: function(target, room, user) {
     	if (!this.canBroadcast()) return;
     	this.sendReplyBox('<center><img height="140" width="200" src="http://i.imgur.com/7Ar6RAd.pngg"><img src="http://i.imgur.com/VTxy0rU.gif"><br><b>Ace: </b>Garchomp<br><b>Quote: </b>Believing that you can do it means you\'re already halfway there!</center>');
