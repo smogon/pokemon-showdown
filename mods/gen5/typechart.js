00c04bc1d55f99528bcb5dc08e2ab452f23c05a4
Ivo Julca <ivojulca@hotmail.com>
2014-02-15 16:56:55 -0500

    Fix Gen 5 Steel Forgot the most important difference...

--- mods/gen5/typechart.js
+++ mods/gen5/typechart.js
@@ -65,5 +65,30 @@ exports.BattleTypeChart = {
 			"Water": 2
 		}
 	},
+	"Steel": {
+		inherit: true,
+		damageTaken: {
+			psn: 3,
+			tox: 3,
+			sandstorm: 3,
+			"Bug": 2,
+			"Dark": 2,
+			"Dragon": 2,
+			"Electric": 0,
+			"Fighting": 1,
+			"Fire": 1,
+			"Flying": 2,
+			"Ghost": 2,
+			"Grass": 2,
+			"Ground": 1,
+			"Ice": 2,
+			"Normal": 2,
+			"Poison": 3,
+			"Psychic": 2,
+			"Rock": 2,
+			"Steel": 2,
+			"Water": 0
+		}
+	},
 	"Fairy": null
 };
