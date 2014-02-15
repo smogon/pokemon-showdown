db9a7657f57189e484b9c04e8e5c8cd74632faab
Vacate <voltabsorb@gmail.com>
2014-02-15 19:50:01 +0000

    Added Roomban Appeal Message
    Added where to appeal roombans due to the large amount of roomban appeals on the forums.

--- commands.js
+++ commands.js
@@ -384,7 +384,7 @@ var commands = exports.commands = {
 		for (var ip in targetUser.ips) {
 			room.bannedIps[ip] = true;
 		}
-		targetUser.popup(user.name+" has banned you from the room " + room.id + "." + (target ? " (" + target + ")" : ""));
+		targetUser.popup(user.name+" has banned you from the room " + room.id + ". To appeal the ban, PM the moderator that banned you or a room owner." + (target ? " (" + target + ")" : ""));
 		this.addModCommand(""+targetUser.name+" was banned from room " + room.id + " by "+user.name+"." + (target ? " (" + target + ")" : ""));
 		var alts = targetUser.getAlts();
 		if (alts.length) {
