smogon/pokemon-showdown

commit 9064c930638e931baf2dd780c8e5da67e4264162

Hardcode Fake Out exception
for King's Shield and Spiky Shield, we'll find a better way to do this later.

data/moves.js
@@ -7028,7 +7028,8 @@ exports.BattleMovedex = {
 					target.removeVolatile('kingsshield');
 					return;
 				}
-				if (move && (move.category === 'Status' || move.isNotProtectable || move.id === 'suckerpunch')) return;
+				// TODO: Find a better way to implement the following hardcoded exceptions
+				if (move && (move.category === 'Status' || move.isNotProtectable || move.id === 'suckerpunch' || move.id === 'fakeout')) return;
 				this.add('-activate', target, 'Protect');
 				var lockedmove = source.getVolatile('lockedmove');
 				if (lockedmove) {
@@ -12222,7 +12223,8 @@ exports.BattleMovedex = {
 					target.removeVolatile('spikyshield');
 					return;
 				}
-				if (move && move.target === 'self') return;
+				// TODO: Find a better way to implement the following hardcoded exceptions
+				if (move && (move.target === 'self' || move.id === 'suckerpunch' || move.id === 'fakeout')) return;
 				this.add('-activate', target, 'move: Protect');
 				if (move.isContact) {
 					this.damage(source.maxhp/8, source, target);

commit ee34078909f796f91790ce1ca9d26db4ffe85dba

Fix Fake Out bypassing Spiky Shield and King's Shield
This was caused in 9064c930638e931baf2dd780c8e5da67e4264162
Also, Sucker Punch should not activate Protect

data/moves.js
@@ -7028,8 +7028,7 @@ exports.BattleMovedex = {
 					target.removeVolatile('kingsshield');
 					return;
 				}
-				// TODO: Find a better way to implement the following hardcoded exceptions
-				if (move && (move.category === 'Status' || move.isNotProtectable || move.id === 'suckerpunch' || move.id === 'fakeout')) return;
+				if (move && (move.category === 'Status' || move.isNotProtectable)) return;
 				this.add('-activate', target, 'Protect');
 				var lockedmove = source.getVolatile('lockedmove');
 				if (lockedmove) {
@@ -12223,8 +12222,7 @@ exports.BattleMovedex = {
 					target.removeVolatile('spikyshield');
 					return;
 				}
-				// TODO: Find a better way to implement the following hardcoded exceptions
-				if (move && (move.target === 'self' || move.id === 'suckerpunch' || move.id === 'fakeout')) return;
+				if (move && (move.target === 'self' || move.id === 'suckerpunch')) return;
 				this.add('-activate', target, 'move: Protect');
 				if (move.isContact) {
 					this.damage(source.maxhp/8, source, target);
@@ -12965,6 +12963,7 @@ exports.BattleMovedex = {
 		name: "Sucker Punch",
 		pp: 5,
 		priority: 1,
+		isNotProtectable: true,
 		isContact: true,
 		onTryHit: function(target) {
 			var decision = this.willMove(target);