6f30ca5086cac38795f9ab227ae117501871e161
FrPSServ <frostservps@gmail.com>
2014-02-19 20:04:58 +0000

    Soggey Trainer Card

--- config/commands.js
+++ config/commands.js
@@ -1701,6 +1701,11 @@ var commands = exports.commands = {
     	this.sendReplyBox('<center><img src="http://i.imgur.com/lwL5Pce.png"><img src="http://i.imgur.com/D9M6VGi.gif"><img src="http://i.imgur.com/hZ0mB0U.png"><br><b>Ace: <font color="#58ACFA"><blink>Ace: Farfetch\'d</blink><br></font><b><font color="#00BFFF">Where can I find a leek in Pokemon Y?');
     },
     
+    soggey: function(target, room, user) {
+    	if (!this.canBroadcast()) return;
+    	this.sendReplyBox('<center><img src="http://i.imgur.com/w9po1tP.gif?1"><img src="http://i.imgur.com/N48X8Vf.png"><img src="http://i.imgur.com/YTl10Yi.png"><br><b>Ace: </b>Sandslash<br><b>Quote: </b>It was all fun and games... but then you had to hax me >:(</center>')
+    },
+    
     miller: function(target, room, user) {
     	if (!this.canBroadcast()) return;
     	this.sendReplyBox('<center><img height="110" width="260" src="http://i.imgur.com/cc5BTsj.gif"><center><img height="150" width="220" src="http://25.media.tumblr.com/tumblr_m456ambdnz1qd87hlo1_500.gif"><center><br><b>Ace: </b>Wobbuffet<br><b>Catchphrase: </b>I\'ll get the job done.</center>');
