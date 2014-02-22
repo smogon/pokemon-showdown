31491d73ba3f934153359939a807303f3aa3bf4a
LynnHikaru <lynnburgess21@yahoo.com>
2014-02-22 11:10:19 -0500

    Update commands.js

--- commands.js
+++ commands.js
@@ -2755,7 +2755,7 @@ var commands = exports.commands = {
 
 	joinbattle: function(target, room, user) {
 		if (!room.joinBattle) return this.sendReply('You can only do this in battle rooms.');
-		if (!user.can('joinbattle', null, room)) return this.popupReply("You must be a roomvoice to join a battle you didn't start. Ask a player to use /roomvoice on you to join this battle.");
+		if (!user.can('joinbattle', null, room))
 
 		room.joinBattle(user);
 	},
