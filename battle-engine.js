cdfbdde90914cd30e4b93e8d3f876275cd9b30ea
Guangcong Luo <guangcongluo@gmail.com>
2014-02-22 21:50:12 -0500

    Record starting random seed for debugging

--- battle-engine.js
+++ battle-engine.js
@@ -1413,7 +1413,7 @@ var Battle = (function() {
 		this.messageLog = [];
 
 		// use a random initial seed (64-bit, [high -> low])
-		this.seed = [Math.floor(Math.random() * 0x10000),
+		this.startingSeed = this.seed = [Math.floor(Math.random() * 0x10000),
 			Math.floor(Math.random() * 0x10000),
 			Math.floor(Math.random() * 0x10000),
 			Math.floor(Math.random() * 0x10000)];
