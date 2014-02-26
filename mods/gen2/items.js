36cd951eeca10155f9005690a5a0834ed11ac95d
Relados <blagh0@yahoo.com>
2014-02-26 06:56:52 -0600

    Removing redundant berries
    The status curing berries can be implemented using aliases to the existing berries.

--- mods/gen2/items.js
+++ mods/gen2/items.js
@@ -31,32 +31,6 @@ exports.BattleItems = {
 		gen: 2,
 		desc: "Raises attack by 2 when holder is switched in.  Confuses holder.  Single use."
 	},
-	bitterberry: {
-		id: "bitterberry",
-		name: "Bitter Berry",
-		isBerry: true,
-		onUpdate: function(pokemon) {
-			if (pokemon.volatiles['confusion']) pokemon.eatItem();
-		},
-		onEat: function(pokemon) {
-			this.removeVolatile('confusion');
-		},
-		gen: 2,
-		desc: "Holder is cured if it is confused. Single use."
-	},
-	burntberry: {
-		id: "burntberry",
-		name: "Burnt Berry",
-		isBerry: true,
-		onUpdate: function(pokemon) {
-			if (pokemon.status === 'brn') pokemon.eatItem();
-		},
-		onEat: function(pokemon) {
-			if (pokemon.status === 'brn') pokemon.cureStatus();
-		},
-		gen: 2,
-		desc: "Holder is cured if it is burned. Single use."
-	},
 	dragonscale: {
 		id: "dragonscale",
 		name: "Dragon Scale",
@@ -77,19 +51,6 @@ exports.BattleItems = {
 		gen: 2,
 		desc: "Restores 30 HP when the holder of this item is at 50% HP or less. One-time use."
 	},
-	iceberry: {
-		id: "iceberry",
-		name: "Ice Berry",
-		isBerry: true,
-		onUpdate: function(pokemon) {
-			if (pokemon.status === 'frz') pokemon.eatItem();
-		},
-		onEat: function(pokemon) {
-			if (pokemon.status === 'frz') pokemon.cureStatus();
-		},
-		gen: 2,
-		desc: "Holder is cured if it is frozen. Single use."
-	},
 	metalpowder: {
 		inherit: true,
 		onModifyDef: function(def, pokemon) {
@@ -103,47 +64,8 @@ exports.BattleItems = {
 			}
 		}
 	},
-	mintberry: {
-		id: "mintberry",
-		name: "Mint Berry",
-		isBerry: true,
-		onUpdate: function(pokemon) {
-			if (pokemon.status === 'slp') pokemon.eatItem();
-		},
-		onEat: function(pokemon) {
-			if (pokemon.status === 'slp') pokemon.cureStatus();
-		},
-		gen: 2,
-		desc: "Holder wakes up if it is asleep. Single use."
-	},
-	miracleberry: {
-		id: "miracleberry",
-		name: "Miracle Berry",
-		isBerry: true,
-		onUpdate: function(pokemon) {
-			if (pokemon.status || pokemon.volatiles['confusion']) {
-				pokemon.eatItem();
-			}
-		},
-		onEat: function(pokemon) {
-			pokemon.cureStatus();
-			pokemon.removeVolatile('confusion');
-		},
-		gen: 2,
-		desc: "Holder cures itself if it is confused or has a major status problem. Single use."
-	},
-	mysteryberry: {
-		id: "mysteryberry",
-		name: "Mystery Berry",
-		isBerry: true,
-		onUpdate: function(pokemon) {
-			var move = pokemon.getMoveData(pokemon.lastMove);
-			if (move && move.pp === 0) {
-				pokemon.addVolatile('leppaberry');
-				pokemon.volatiles['leppaberry'].move = move;
-				pokemon.eatItem();
-			}
-		},
+	leppaberry: {
+		inherit: true,
 		onEat: function(pokemon) {
 			var move;
 			if (pokemon.volatiles['leppaberry']) {
@@ -164,31 +86,5 @@ exports.BattleItems = {
 		},
 		gen: 2,
 		desc: "Restores 5PP to the first of the holder's moves to reach 0PP. Single use."
-	},
-	przcureberry: {
-		id: "przcureberry",
-		name: "PrzcureBerry",
-		isBerry: true,
-		onUpdate: function(pokemon) {
-			if (pokemon.status === 'par') pokemon.eatItem();
-		},
-		onEat: function(pokemon) {
-			if (pokemon.status === 'par') pokemon.cureStatus();
-		},
-		gen: 2,
-		desc: "Holder cures itself if it is paralyzed. Single use."
-	},
-	psncureberry: {
-		id: "psncureberry",
-		name: "PsncureBerry",
-		isBerry: true,
-		onUpdate: function(pokemon) {
-			if (pokemon.status === 'psn') pokemon.eatItem();
-		},
-		onEat: function(pokemon) {
-			if (pokemon.status === 'psn') pokemon.cureStatus();
-		},
-		gen: 2,
-		desc: "Holder is cured if it is poisoned. Single use."
 	}
 };
