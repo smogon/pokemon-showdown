19e4c677c0d85e09421fd3030cf13bf3e19a2625
Slayer95 <ivojulca@hotmail.com>
2014-02-26 20:29:43 -0500

    Stance Change cannot be removed
    Properly do 8aad189f5f70c1cea0d7a1df26876e06e4d5bfee

--- battle-engine.js
+++ battle-engine.js
@@ -1029,7 +1029,7 @@ var BattlePokemon = (function() {
 		if (noForce && this.ability === ability.id) {
 			return false;
 		}
-		if (ability.id === 'multitype' || ability.id === 'illusion' || ability.id === 'stancechange' || this.ability === 'multitype') {
+		if (ability.id === 'multitype' || ability.id === 'illusion' || this.ability === 'stancechange' || this.ability === 'multitype') {
 			return false;
 		}
 		this.ability = ability.id;
