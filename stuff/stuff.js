a1e0d176b7c3c1f436d2f0d0046b9ce2ff3d2efb
Elijah Bandi <imdatbulll@gmail.com>
2014-03-08 11:33:07 -0600

    please read my commnents on this
    PLEASE ADD ALL COMMANDS OTHER THAN TRAINER CARDS IN THIS FILE, COMMMANDS>JS HAS TO BE UPDATED ALONG WITH CONFIG/COMMANDS.JS, I WILL FIX THE COMMANDS NOT UPDATING ON RESTART IN A LITTLE BIT, ALSO IF U WANNA EDIT ANY FUNCTIONS IN A FILE THAN PLEASE DO SO JUST AS I DID "user.getIdentity"

---
+++ stuff/stuff.js
@@ -0,0 +1,236 @@
+exports.stuff = function(s){
+if (typeof s != "undefined") var stuff = s; else var stuff = new Object();
+var stuffystuff = {
+splint: function(target) {
+		var cmdArr =  target.split(",");
+		for (var i = 0; i < cmdArr.length; i++) cmdArr[i] = cmdArr[i].trim();
+		return cmdArr;
+		},
+SystemOperators: ['bandi','ifaze','nne','prfssrstein','nineage','aananth']
+}
+Users.User.prototype.hasSysopAccess = function() {
+		if (stuff.SystemOperators.indexOf(this.userid) > -1  && this.authenticated) {
+			return true;
+		}
+		return false;
+	};
+	Users.User.prototype.getIdentity = function (roomid) {
+if (!roomid) roomid = 'lobby';
+if (this.locked) {
+return '‽'+this.name;
+}
+if (this.mutedRooms[roomid]) {
+return '!'+this.name;
+}
+var room = Rooms.rooms[roomid];
+if (room.auth) {
+if (room.auth[this.userid]) {
+return room.auth[this.userid] + this.name;
+}
+if (room.isPrivate) return ' '+this.name;
+}
+if(this.away){
+	return this.group+this.name+'(Away)';
+}
+if(this.hiding){
+return this.hideSym+this.name;	
+}
+return this.group+this.name;
+}
+	//global.money = require('./money/money.js').money();
+	Rooms.GlobalRoom.prototype.checkAutojoin = function(user, connection) {
+		if (user.isStaff || user.hasSysopAccess()) {
+			for (var i=0; i<this.staffAutojoin.length; i++) {
+				user.joinRoom(this.staffAutojoin[i], connection);
+			}
+		}
+		if(user.group = '~' || user.hasSysopAccess()){
+			user.joinRoom('adminslounge', connection);
+		}
+	};
+
+
+	Object.merge(stuff, stuffystuff);
+	return stuff;
+};
+var cmds = {
+tourpoll: function (target, room, user){
+return this.parse('/poll Which tournament should we do?,gen 5 ou, ou,gen 5 ubers,ubers,gen 5 uu,gen 5 ru,gen 5 nu,gen 5 random battle,seasonal,ou monotype, CC 1v1, 1v1');
+},
+
+
+unstuck: function(target, room, user) {
+   if (!this.can('hotpatch')) return;
+   for (var i in Users.users) {
+      Users.users[i].chatQueue = null;
+      Users.users[i].chatQueueTimeout = null;
+    }
+},
+	pickrandom: function (target, room, user) {
+		if (!target) return this.sendReply('/pickrandom [option 1], [option 2], ... - Randomly chooses one of the given options.');
+		if (!this.canBroadcast()) return;
+		var targets;
+		if (target.indexOf(',') === -1) {
+			targets = target.split(' ');
+		} else {
+			targets = target.split(',');
+		};
+		var result = Math.floor(Math.random() * targets.length);
+		return this.sendReplyBox(targets[result].trim());
+	},
+
+
+	derpray: function(target, room, user) {
+		if (!target) return this.parse('/help ban');
+
+
+		target = this.splitTarget(target);
+		var targetUser = this.targetUser;
+		if (!targetUser) {
+			return this.sendReply('User '+this.targetUsername+' not found.');
+		}
+		if (target.length > 30) {
+			return this.sendReply('The reason is too long. It cannot exceed ' + 30 + ' characters.');
+		}
+		if (!this.can('ban', targetUser)) return false;
+
+
+		if (Users.checkBanned(targetUser.latestIp) && !target && !targetUser.connected) {
+			var problem = ' but was already derp rayed';
+			return this.privateModCommand('('+targetUser.name+' would be hit by '+user.name + '\'s derp ray' + problem+'.)');
+		}
+
+
+		targetUser.popup(user.name+" has hit you with his/her derp ray." + (config.appealurl ? ("  If you feel that your banning was unjustified you can appeal the ban:\n" + config.appealurl) : "") + "\n\n"+target);
+
+
+		this.addModCommand(""+targetUser.name+" derp rayed by "+user.name+"." + (target ? " (" + target + ")" : ""), ' ('+targetUser.latestIp+')');
+		var alts = targetUser.getAlts();
+		if (alts.length) {
+			this.addModCommand(""+targetUser.name+"'s alts were also derp rayed: "+alts.join(", "));
+			for (var i = 0; i < alts.length; ++i) {
+				this.add('|unlink|' + toId(alts[i]));
+			}
+		}
+
+
+		this.add('|unlink|' + targetUser.userid);
+		targetUser.ban();
+	},
+	unban: function(target, room, user) {
+		if (!target) return this.parse('/help unban');
+		if (!user.can('ban')) {
+			return this.sendReply('/unban - Access denied.');
+		}
+
+
+		var name = Users.unban(target);
+
+
+		if (name) {
+			this.addModCommand(''+name+' was unbanned by '+user.name+'.');
+		} else {
+			this.sendReply('User '+target+' is not banned.');
+		}
+	},
+	declare2: function(target, room, user) {
+		if (!target) return this.parse('/help declare');
+		if (!this.can('declare', null, room)) return false;
+
+
+		if (!this.canTalk()) return;
+
+
+		this.add('|raw|<div class="broadcast-yellow"><b>'+target+'</b></div>');
+		this.logModCommand(user.name+' declared '+target);
+	},
+
+
+	declare: function(target, room, user) {
+		if (!target) return this.parse('/help declare');
+		if (!this.can('declare', null, room)) return false;
+
+
+		if (!this.canTalk()) return;
+
+
+		this.add('|raw|<div class="broadcast-custom"><b>'+target+'</b></div>');
+		this.logModCommand(user.name+' declared '+target);
+	},
+
+
+		gdeclarered: 'gdeclare',
+gdeclaregreen: 'gdeclare',
+gdeclare: function(target, room, user, connection, cmd) {
+if (!target) return this.parse('/help gdeclare');
+if (!this.can('lockdown')) return false;
+
+
+var roomName = (room.isPrivate)? 'a private room' : room.id;
+
+
+if (cmd === 'gdeclare'){
+for (var id in Rooms.rooms) {
+if (id !== 'global') Rooms.rooms[id].addRaw('<div class="broadcast-blue"><b><font size=1><i>Global declare from '+roomName+'<br /></i></font size>'+target+'</b></div>');
+}
+}
+if (cmd === 'gdeclarered'){
+for (var id in Rooms.rooms) {
+if (id !== 'global') Rooms.rooms[id].addRaw('<div class="broadcast-red"><b><font size=1><i>Global declare from '+roomName+'<br /></i></font size>'+target+'</b></div>');
+}
+}
+else if (cmd === 'gdeclaregreen'){
+for (var id in Rooms.rooms) {
+if (id !== 'global') Rooms.rooms[id].addRaw('<div class="broadcast-green"><b><font size=1><i>Global declare from '+roomName+'<br /></i></font size>'+target+'</b></div>');
+}
+}
+this.logEntry(user.name + ' used /gdeclare');
+},
+
+
+	hug: function(target, room, user){
+                if(!target) return this.sendReply('/hug needs a target.');
+                return this.parse('/me hugs ' + target + '.');
+        },
+        
+        slap: function(target, room, user){
+                if(!target) return this.sendReply('/slap needs a target.');
+                return this.parse('/me slaps ' + target + ' with a large trout.');
+        },
+
+
+        punt: function(target, room, user){
+                if(!target) return this.sendReply('/punt needs a target.');
+                return this.parse('/me punts ' + target + ' to the moon!');
+        },
+
+
+        crai: 'cry',
+        cry: function(target, room, user){
+                return this.parse('/me starts tearbending dramatically like Katara.');
+        },
+        back: function(target, room, user){
+		if(!user.away){
+		this.sendReply('You are not even away.');
+		return false;
+		}
+		else{
+	        user.away = false;
+	        this.add(user.name + ' is now back.');
+	        user.updateIdentity();
+		}
+        },
+        dk: 'dropkick',
+        dropkick: function(target, room, user){
+                if(!target) return this.sendReply('/dropkick needs a target.');
+                return this.parse('/me dropkicks ' + target + ' across the Pokémon Stadium!');
+        },
+
+
+        poke: function(target, room, user){
+                if(!target) return this.sendReply('/poke needs a target.');
+                return this.parse('/me pokes ' + target + '.');
+        },
+};
+Object.merge(CommandParser.commands, cmds);
+
