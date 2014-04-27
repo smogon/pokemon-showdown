33362bbc09ad7b7a961e8e1b94b0fc3feb9309c4
小太 <nospam@xiaotai.org>
2014-04-28 01:01:54 +1000

    Add /customavatar

--- app.js
+++ app.js
@@ -92,6 +92,32 @@ if (!fs.existsSync('./config/config.js')) {
 
 global.Config = require('./config/config.js');
 
+global.reloadCustomAvatars = function() {
+	var path = require('path');
+	var newCustomAvatars = {};
+	fs.readdirSync('./config/avatars').forEach(function (file) {
+		var ext = path.extname(file);
+		if (ext !== '.png' && ext !== '.gif')
+			return;
+
+		var user = toUserid(path.basename(file, ext));
+		newCustomAvatars[user] = file;
+		delete Config.customAvatars[user];
+	});
+
+	// Make sure the manually entered avatars exist
+	for (var a in Config.customAvatars)
+		if (typeof Config.customAvatars[a] === 'number')
+			newCustomAvatars[a] = Config.customAvatars[a];
+		else
+			fs.exists('./config/avatars/' + Config.customAvatars[a], (function (user, file, isExists) {
+				if (isExists)
+					Config.customAvatars[user] = file;
+			}).bind(null, a, Config.customAvatars[a]));
+
+	Config.customAvatars = newCustomAvatars;
+}
+
 var watchFile = function () {
 	try {
 		return fs.watchFile.apply(fs, arguments);
@@ -106,6 +132,7 @@ if (Config.watchConfig) {
 		try {
 			delete require.cache[require.resolve('./config/config.js')];
 			Config = require('./config/config.js');
+			reloadCustomAvatars();
 			console.log('Reloaded config/config.js');
 		} catch (e) {}
 	});
@@ -416,3 +443,5 @@ fs.readFile('./config/ipbans.txt', function (err, data) {
 	}
 	Users.checkRangeBanned = Cidr.checker(rangebans);
 });
+
+reloadCustomAvatars();
