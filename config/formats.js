d6de4af38d30c33e374b97dddbbddacaae708368
The Immortal <the_immortal123@live.com>
2014-04-30 17:39:50 +0400

    Prevent illegal gen 1 movesets
    Hardcoded because there are only a few, and it only affects gen 1 as the
    move relearner didn't exist.
    
    Source: https://smogon.com/forums/threads/78638/

--- config/formats.js
+++ config/formats.js
@@ -781,7 +781,14 @@ exports.Formats = [
 
 		mod: 'gen1',
 		ruleset: ['Pokemon', 'Standard'],
-		banlist: ['Uber']
+		banlist: ['Uber', 
+			'Nidoking + Fury Attack + Thrash',
+			'Exeggutor + Poison Powder + Stomp', 'Exeggutor + Sleep Powder + Stomp', 'Exeggutor + Stun Spore + Stomp',
+			'Eevee + Tackle + Growl',
+			'Vaporeon + Tackle + Growl',
+			'Jolteon + Tackle + Growl', 'Jolteon + Focus Energy + Thunder Shock',
+			'Flareon + Tackle + Growl', 'Flareon + Focus Energy + Ember'
+		]
 	},
 	{
 		name: "[Gen 1] Custom Game",
