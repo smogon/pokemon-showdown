1b42093f301ed50355e6afea8fc07d302cf04a48
Parukia <dragontrainerryan724@yahoo.com>
2014-05-09 16:43:21 -0700

    Update commands.js

--- commands.js
+++ commands.js
@@ -1282,6 +1282,28 @@ var commands = exports.commands = {
 			}
 		});
 	},
+	
+	frt: 'forcerenameto',
+	forcerenameto: function(target, room, user) {
+		if (!target) return this.parse('/help forcerenameto');
+		target = this.splitTarget(target);
+		var targetUser = this.targetUser;
+		if (!targetUser) {
+			return this.sendReply("User " + this.targetUsername + " not found.");
+		}
+		if (!target) {
+			return this.sendReply("No new name was specified.");
+		}
+		if (!this.can('forcerenameto', targetUser)) return false;
+
+		if (targetUser.userid === toUserid(this.targetUser)) {
+			var entry = targetUser.name + " was forcibly renamed to " + target + " by " + user.name + ".";
+			this.privateModCommand("(" + entry + ")");
+			targetUser.forceRename(target, null, true);
+		} else {
+			this.sendReply("User " + targetUser.name + " is no longer using that name.");
+		}
+	},
 
 	bw: 'banword',
 	banword: function (target, room, user) {
