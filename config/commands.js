8d4c7bff10f149df5298f4ec984ab54ecfb28e8b
Kenny- <umbi98@excite.it>
2014-05-23 18:07:25 +0200

    Aggiunto poll

--- config/commands.js
+++ config/commands.js
@@ -143,6 +143,10 @@
  * @license MIT license
  */
 
+var question = undefined;
+var answerList = new Array();
+var answers = new Object();
+
 var commands = exports.commands = {
 
 	ip: 'whois',
@@ -1127,6 +1131,82 @@ var commands = exports.commands = {
 		if (!this.canBroadcast('!htmlbox')) return;
 
 		this.sendReplyBox(target);
+	},
+	
+	survey: 'poll',
+	poll: function(target, room, user) {
+		if (!this.can('ban')) return this.sendReply('You do not have enough authority to use this command.');
+		if (tour[room.id].question) return this.sendReply('There is currently a poll going on already.');
+		var separacion = "&nbsp;&nbsp;";
+		var answers = tour.splint(target);
+		formats = ''; 
+		for (var u in Tools.data.Formats) {
+			if (Tools.data.Formats[u].name && Tools.data.Formats[u].challengeShow) formats = formats+','+Tools.data.Formats[u].name;
+		}
+		formats = 'Tournament'+formats;
+		if (answers[0] == 'tournament' || answers[0] == 'tour') answers = tour.splint(formats);
+		if (answers.length < 3) return this.sendReply('Correct syntax for this command is /poll question, option, option...');
+		var question = answers[0];
+		question = clean(question);
+		answers.splice(0, 1);
+		var answers = answers.join(',').toLowerCase().split(',');
+		tour[room.id].question = question;
+		tour[room.id].answerList = answers;
+		tour[room.id].usergroup = config.groupsranking.indexOf(user.group);
+		room.addRaw('<div class="infobox"><h2>' + tour[room.id].question + separacion + '<font size=2 color = "#939393"><small>/vote OPTION<br /><i><font size=1>Poll started by '+user.name+'</font size></i></small></font></h2><hr />' + separacion + separacion + " &bull; " + tour[room.id].answerList.join(' &bull; ') + '</div>');
+	},
+
+	vote: function(target, room, user) {
+		var ips = JSON.stringify(user.ips);
+		if (!tour[room.id].question) return this.sendReply('There is no poll currently going on in this room.');
+		if (!target) return this.parse('/help vote');
+		if (tour[room.id].answerList.indexOf(target.toLowerCase()) == -1) return this.sendReply('\'' + target + '\' is not an option for the current poll.');
+		tour[room.id].answers[ips] = target.toLowerCase();
+		return this.sendReply('You are now voting for ' + target + '.');
+	},
+
+	votes: function(target, room, user) {
+		if (!this.canBroadcast()) return;
+		this.sendReply('NUMBER OF VOTES: ' + Object.keys(tour[room.id].answers).length);
+	},
+
+	endsurvey: 'endpoll',
+	ep: 'endpoll',
+	endpoll: function(target, room, user) {
+		if (!this.can('ban')) return this.sendReply('You do not have enough authority to use this command.');
+		if (!tour[room.id].question) return this.sendReply('There is no poll to end in this room.');
+		var votes = Object.keys(tour[room.id].answers).length;
+		if (votes == 0) {
+			tour[room.id].question = undefined;
+			tour[room.id].answerList = new Array();
+			tour[room.id].answers = new Object();
+			return room.addRaw("<h3>The poll was canceled because of lack of voters.</h3>");			
+		}
+		var options = new Object();
+		var obj = tour[room.id];
+		for (var i in obj.answerList) options[obj.answerList[i]] = 0;
+		for (var i in obj.answers) options[obj.answers[i]]++;
+		var sortable = new Array();
+		for (var i in options) sortable.push([i, options[i]]);
+		sortable.sort(function(a, b) {return a[1] - b[1]});
+		var html = "";
+		for (var i = sortable.length - 1; i > -1; i--) {
+			//console.log(i);
+			var option = sortable[i][0];
+			var value = sortable[i][1];
+			if (value > 0) html += "&bull; " + option + " - " + Math.floor(value / votes * 100) + "% (" + value + ")<br />";
+		}
+		room.addRaw('<div class="infobox"><h2>Results to "' + obj.question + '"<br /><i><font size=1 color = "#939393">Poll ended by '+user.name+'</font></i></h2><hr />' + html + '</div>');		tour[room.id].question = undefined;
+		tour[room.id].answerList = new Array();
+		tour[room.id].answers = new Object();
+	},
+
+	pollremind: 'pr',
+	pr: function(target, room, user) {
+		var separacion = "&nbsp;&nbsp;";
+		if (!tour[room.id].question) return this.sendReply('There is currently no poll going on.');
+		if (!this.canBroadcast()) return;
+		this.sendReply('|raw|<div class="infobox"><h2>' + tour[room.id].question + separacion + '<font font size=1 color = "#939393"><small>/vote OPTION</small></font></h2><hr />' + separacion + separacion + " &bull; " + tour[room.id].answerList.join(' &bull; ') + '</div>');
 	},
 
 	a: function (target, room, user) {
