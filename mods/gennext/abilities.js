fa0111447da188e8ed949553e745093155a55930
Guangcong Luo <guangcongluo@gmail.com>
2014-05-24 17:53:10 -0400

    NEXT: Fix Compound Eyes accuracy

--- mods/gennext/abilities.js
+++ mods/gennext/abilities.js
@@ -198,7 +198,7 @@ exports.BattleAbilities = {
 		onSourceAccuracy: function (accuracy) {
 			if (typeof accuracy !== 'number') return;
 			this.debug('compoundeyes - enhancing accuracy');
-			return accuracy * 1.3;
+			return accuracy * 1.6;
 		},
 		id: "compoundeyes",
 		name: "Compound Eyes",
