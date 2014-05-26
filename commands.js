d895e7cb3e0ad5c7b25e0c994f6008f312de7abb
Kenny- <umbi98@excite.it>
2014-05-26 21:13:57 +0200

    Update commands.js

--- commands.js
+++ commands.js
@@ -826,7 +826,7 @@ var commands = exports.commands = {
     
     	masspm: 'pmall',
 	pmall: function(target, room, user) {
-		if (!target) return this.parse('|raw|/pmall <em>message</em> - Sends a PM to every user in a room.');
+		if (!target) return this.parse('/pmall message - Sends a PM to every user in a room.');
 		if (!this.can('hotpatch')) return false;
 
 		var pmName = '~Server PM';
