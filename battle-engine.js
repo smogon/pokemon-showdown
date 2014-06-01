79f83f1d2c9d2dc649ad9841312bbb37b329b7fd
Slayer95 <ivojulca@hotmail.com>
2014-05-31 19:40:34 -0500

    Fix prevention of Arceus´ setType

--- battle-engine.js
+++ battle-engine.js
@@ -1120,7 +1120,7 @@ var BattlePokemon = (function () {
 	};
 	BattlePokemon.prototype.setType = function (newType, enforce) {
 		// Arceus first type cannot be normally changed
-		if (!enforce && this.num === 493) return false;
+		if (!enforce && this.template.num === 493) return false;
 
 		this.typesData = [{
 			type: newType,
