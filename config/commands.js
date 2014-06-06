eff59e2135c47fe238aadbf9ed604c187d7fba83
Guangcong Luo <guangcongluo@gmail.com>
2014-06-06 03:45:55 -0400

    Fix bug in /showtan

--- config/commands.js
+++ config/commands.js
@@ -685,9 +685,9 @@ var commands = exports.commands = {
 	showtan: function (target, room, user) {
 		if (room.id !== 'showderp') return this.sendReply("The command '/showtan' was unrecognized. To send a message starting with 'showtan', type '//showtan'.");
 		if (!this.can('modchat', null, room)) return;
-		this.splitTarget(target);
+		target = this.splitTarget(target);
 		if (!this.targetUser) return this.sendReply('user not found');
-		if (!room.users[this.targetUser.id]) return this.sendReply('not a showderper');
+		if (!room.users[this.targetUser.userid]) return this.sendReply('not a showderper');
 		this.targetUser.avatar = '#showtan';
 		this.sendReply('ok');
 	},
