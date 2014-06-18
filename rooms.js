c1185aba04ba9a99ffb25176d87aba392df4711c
Guangcong Luo <guangcongluo@gmail.com>
2014-06-17 22:17:36 -0400

    Roomauth can see private commands

--- rooms.js
+++ rooms.js
@@ -322,7 +322,7 @@ var GlobalRoom = (function () {
 	GlobalRoom.prototype.sendAuth = function (message) {
 		for (var i in this.users) {
 			var user = this.users[i];
-			if (user.connected && user.can('receiveauthmessages')) {
+			if (user.connected && user.can('receiveauthmessages', null, this)) {
 				user.sendTo(this, message);
 			}
 		}
@@ -1336,7 +1336,7 @@ var ChatRoom = (function () {
 	ChatRoom.prototype.sendAuth = function (message) {
 		for (var i in this.users) {
 			var user = this.users[i];
-			if (user.connected && user.can('receiveauthmessages')) {
+			if (user.connected && user.can('receiveauthmessages', null, this)) {
 				user.sendTo(this, message);
 			}
 		}
