72c599ec2d31555651ff491a2a43242cd200ab48
verbiage <kk13777@yahoo.com>
2014-06-17 19:58:41 -0500

    join is now an alias of join
    In recognition of your outstanding acomplishments in the field of Pokémon sciences, we hereby award you the highest of honors; a commemorative alias.

--- commands.js
+++ commands.js
@@ -353,6 +353,7 @@ var commands = exports.commands = {
 		Rooms.global.autojoinRooms(user, connection);
 	},
 
+	joim: 'join',
 	join: function (target, room, user, connection) {
 		if (!target) return false;
 		var targetRoom = Rooms.get(target) || Rooms.get(toId(target));
