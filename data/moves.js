f15b51e17570a8aca86fc191b011bec3f6968d9f
V4Victini <h.f.gordonjr@gmail.com>
2014-06-25 11:31:26 -0400

    RNG: Metronome
    Select the move from a sorted index of selectable moves.

--- data/moves.js
+++ data/moves.js
@@ -8237,11 +8237,14 @@ exports.BattleMovedex = {
 					afteryou:1, assist:1, bestow:1, chatter:1, copycat:1, counter:1, covet:1, destinybond:1, detect:1, endure:1, feint:1, focuspunch:1, followme:1, freezeshock:1, helpinghand:1, iceburn:1, mefirst:1, metronome:1, mimic:1, mirrorcoat:1, mirrormove:1, naturepower:1, protect:1, quash:1, quickguard:1, ragepowder:1, relicsong:1, secretsword:1, sketch:1, sleeptalk:1, snatch:1, snarl:1, snore:1, struggle:1, switcheroo:1, technoblast:1, thief:1, transform:1, trick:1, vcreate:1, wideguard:1, diamondstorm:1, steameruption:1, hyperspacehole:1, thousandarrows:1, thousandwaves:1
 				};
 				if (!noMetronome[move.id]) {
-					moves.push(move.id);
+					moves.push(move);
 				}
 			}
 			var move = '';
-			if (moves.length) move = moves[this.random(moves.length)];
+			if (moves.length) {
+				moves.sort(function(a,b){return a.num-b.num;});
+				move = moves[this.random(moves.length)].id;
+			}
 			if (!move) {
 				return false;
 			}
