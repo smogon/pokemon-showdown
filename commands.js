e44c2ab74b679543dd48489be30cf05a169aa059
verbiage <kk13777@yahoo.com>
2014-06-27 22:04:45 -0500

    Joim is now an alias of join
    In recognition of your outstanding acomplishments in the field of Pokémon sciences, we hereby award you the highest of honors; a commemorative alias.
    
    
    
          As opposed to figuring out how to edit an old pull request.

--- commands.js
+++ commands.js
@@ -482,6 +482,7 @@ var commands = exports.commands = {
 		Rooms.global.autojoinRooms(user, connection);
 	},
 
+	joim: 'join',
 	join: function (target, room, user, connection) {
 		if (!target) return false;
 		var targetRoom = Rooms.get(target) || Rooms.get(toId(target));
