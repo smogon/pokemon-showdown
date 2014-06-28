f216e2db6c107865758e1be651911c05e4e3c4c3
Kenny- <umbi98@excite.it>
2014-06-28 18:54:55 +0200

    Update commands.js

--- commands.js
+++ commands.js
@@ -419,6 +419,15 @@ var commands = exports.commands = {
 		}
 	},
 	
+	cc: 'customcolour',
+	customcolour: function (target, room) {
+		var targets = target.split(',');
+		if (targets.length < 2) return this.sendReply("/customcolour OR /cc [colour], [message] - Outputs a message in a custom colour. Requires: " + Users.getGroupsThatCan('customcolour', room).join(" "));
+		if (!this.can('customcolour', room) || !this.canBroadcast('!cc')) return false;
+		this.sendReply('|raw|<font color="' + targets[0].toLowerCase().replace(/[^#a-z0-9]+/g, '') + '">' + Tools.escapeHTML(targets.slice(1).join(",")) + '</font>');
+	},
+
+	
 	roomlist: function(target, room, user, connection) {
 		if (!user.can('hotpatch')) return false;
 			for (var u in Rooms.rooms) {
