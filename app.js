e8eb30dd979d497a4c8fb0f294e5fca4918fab8e
Kenny- <umbi98@excite.it>
2014-06-29 16:08:06 +0200

    Revert "Revert "Clan""
    This reverts commit eb80a11496be5ba7844da4f0dd69317829a0ea68.

--- app.js
+++ app.js
@@ -352,6 +352,8 @@ global.Simulator = require('./simulator.js');
 
 global.Tournaments = require('./tournaments/frontend.js');
 
+global.Clans = require('./clans.js');
+
 try {
 	global.Dnsbl = require('./dnsbl.js');
 } catch (e) {
