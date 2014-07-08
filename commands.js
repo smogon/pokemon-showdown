f89c742b6928f4368f1273a635b60bf6bd7599a8
小太 <nospam@xiaotai.org>
2014-07-08 22:44:41 +1000

    Rename frontend.js to middleend.js

--- commands.js
+++ commands.js
@@ -1090,8 +1090,8 @@ var commands = exports.commands = {
 				CommandParser = require('./command-parser.js');
 
 				var runningTournaments = Tournaments.tournaments;
-				CommandParser.uncacheTree('./tournaments/frontend.js');
-				Tournaments = require('./tournaments/frontend.js');
+				CommandParser.uncacheTree('./tournaments/middleend.js');
+				Tournaments = require('./tournaments/middleend.js');
 				Tournaments.tournaments = runningTournaments;
 
 				return this.sendReply("Chat commands have been hot-patched.");
@@ -1103,8 +1103,8 @@ var commands = exports.commands = {
 
 			try {
 				var runningTournaments = Tournaments.tournaments;
-				CommandParser.uncacheTree('./tournaments/frontend.js');
-				Tournaments = require('./tournaments/frontend.js');
+				CommandParser.uncacheTree('./tournaments/middleend.js');
+				Tournaments = require('./tournaments/middleend.js');
 				Tournaments.tournaments = runningTournaments;
 				return this.sendReply("Tournaments have been hot-patched.");
 			} catch (e) {
