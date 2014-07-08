300266493beea52f55ce55132f013119ab65be21
小太 <nospam@xiaotai.org>
2014-07-09 01:22:32 +1000

    Minor whitespace fix

--- tournaments/middleend.js
+++ tournaments/middleend.js
@@ -665,7 +665,7 @@ var commands = {
 			if (!targetUser)
 				return this.sendReply("User " + params[0] + " not found.");
 			tournament.disqualifyUser(targetUser, this);
-			this.privateModCommand("("+targetUser.name+" was disqualified from the tournament by "+user.name+")");
+			this.privateModCommand("(" + targetUser.name + " was disqualified from the tournament by " + user.name + ")");
 		},
 		end: 'delete',
 		stop: 'delete',
