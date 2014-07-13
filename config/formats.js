e3667b9c88220d722c0c33ef621ee945773f8144
The Immortal <the_immortal123@live.com>
2014-07-13 19:10:15 +0400

    Update Triples formats
    - Added Random Triples Battle
    - Added Smogon Triples as challenge only
    - Made XY Battle Spot Triples ladderable

--- config/formats.js
+++ config/formats.js
@@ -292,11 +292,30 @@ exports.Formats = [
 	///////////////////////////////////////////////////////////////////
 
 	{
-		name: "XY Battle Spot Triples",
-		section: "XY Triples (beta)",
+		name: "Random Triples Battle",
+		section: "XY Triples",
+
+		gameType: 'triples',
+		team: 'randomDoubles',
+		ruleset: ['PotD', 'Pokemon', 'Sleep Clause Mod', 'HP Percentage Mod']
+	},
+	{
+		name: "Smogon Triples",
+		section: "XY Triples",
 
 		gameType: 'triples',
 		searchShow: false,
+		ruleset: ['Pokemon', 'Species Clause', 'OHKO Clause', 'Moody Clause', 'Endless Battle Clause', 'HP Percentage Mod', 'Team Preview'],
+		banlist: ['Soul Dew', 'Dark Void',
+			'Mewtwo', 'Lugia', 'Ho-Oh', 'Kyogre', 'Groudon', 'Rayquaza', 'Dialga', 'Palkia', 'Giratina', 'Giratina-Origin',
+			'Arceus', 'Reshiram', 'Zekrom', 'Kyurem-White', 'Xerneas', 'Yveltal'
+		]
+	},
+	{
+		name: "XY Battle Spot Triples",
+		section: "XY Triples",
+
+		gameType: 'triples',
 		maxForcedLevel: 50,
 		ruleset: ['Pokemon', 'Standard GBU', 'Team Preview VGC'],
 		validateTeam: function (team, format) {
@@ -305,7 +324,7 @@ exports.Formats = [
 	},
 	{
 		name: "Triples Challenge Cup",
-		section: 'XY Triples (beta)',
+		section: "XY Triples",
 
 		gameType: 'triples',
 		team: 'randomCC',
@@ -314,7 +333,7 @@ exports.Formats = [
 	},
 	{
 		name: "Triples Custom Game",
-		section: "XY Triples (beta)",
+		section: "XY Triples",
 
 		gameType: 'triples',
 		searchShow: false,
