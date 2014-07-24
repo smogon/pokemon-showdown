1702cd394552d5cc1a7e82ac881217b267000d96
Tempoanon <Tempsray@gmail.com>
2014-07-24 16:30:50 -0700

    NEXT: New abilities for lots of Pokemon
    Charizard nerfed to have Flame Body while Typhlosion buffed to get Magma Armor and Feraligatr gets Strong Jaw. Pixies and Hydreigon get abilities, and Crawdaunt gets Tough Claws to make up for the Adaptability change.

--- mods/gennext/scripts.js
+++ mods/gennext/scripts.js
@@ -65,15 +65,15 @@ exports.BattleScripts = {
 		// Venusaur
 		this.modData('Pokedex', 'venusaur').abilities['1'] = 'Leaf Guard';
 		// Charizard
-		this.modData('Pokedex', 'charizard').abilities['1'] = 'Flash Fire';
+		this.modData('Pokedex', 'charizard').abilities['1'] = 'Flame Body';
 		// Blastoise
 		this.modData('Pokedex', 'blastoise').abilities['1'] = 'Shell Armor';
 		// Meganium
 		this.modData('Pokedex', 'meganium').abilities['1'] = 'Harvest';
 		// Typhlosion
-		this.modData('Pokedex', 'typhlosion').abilities['1'] = 'Flame Body';
+		this.modData('Pokedex', 'typhlosion').abilities['1'] = 'Magma Armor';
 		// Feraligatr
-		this.modData('Pokedex', 'feraligatr').abilities['1'] = 'Intimidate';
+		this.modData('Pokedex', 'feraligatr').abilities['1'] = 'Strong Jaw';
 		// Sceptile
 		this.modData('Pokedex', 'sceptile').abilities['1'] = 'Limber';
 		// Blaziken
@@ -109,6 +109,13 @@ exports.BattleScripts = {
 		this.modData('Pokedex', 'gengar').abilities['1'] = 'Cursed Body';
 		this.modData('Pokedex', 'mismagius').abilities['1'] = 'Cursed Body';
 		this.modData('Pokedex', 'cryogonal').abilities['1'] = 'Ice Body';
+		this.modData('Pokedex', 'mesprit').abilities['1'] = 'Serene Grace';
+		this.modData('Pokedex', 'uxie').abilities['1'] = 'Synchronize';
+		this.modData('Pokedex', 'azelf').abilities['1'] = 'Steadfast';
+		this.modData('Pokedex', 'hydreigon').abilities['1'] = 'Sheer Force';
+
+		// Adaptability change
+		this.modData('Pokedex', 'crawdaunt').abilities['H'] = 'Tough Claws';
 
 		// Every hidden ability becomes released
 		for (var i in this.data.FormatsData) {
