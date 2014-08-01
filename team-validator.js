9bf6091d79fdf511ddb84e6e9ec15195d0d15ecc
Ivo Julca <ivojulca@hotmail.com>
2014-08-01 15:33:05 -0500

    Fix crash in 41daa79

--- team-validator.js
+++ team-validator.js
@@ -592,7 +592,7 @@ var Validator = (function () {
 		do {
 			alreadyChecked[template.speciesid] = true;
 			// Stabmons hack to avoid copying all of validateSet to formats.
-			if ('ignorestabmoves' in format.banlistTable && template.types.indexOf(tools.getMove(move).type) > -1) return false;
+			if (format.banlistTable && format.banlistTable['ignorestabmoves'] && template.types.indexOf(tools.getMove(move).type) > -1) return false;
 			// Alphabet Cup hack to do the same
 			if (alphabetCupLetter && alphabetCupLetter === Tools.getMove(move).id.slice(0, 1) && Tools.getMove(move).id !== 'sketch') return false;
 			if (template.learnset) {
