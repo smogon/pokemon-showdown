e706bd9e35264cff169254297f05fca0a3bda689
The Immortal <the_immortal123@live.com>
2014-08-02 17:10:38 +0400

    RU is having a suspect test

--- config/formats.js
+++ config/formats.js
@@ -56,6 +56,15 @@ exports.Formats = [
 		name: "RU",
 		section: "XY Singles",
 
+		searchShow: false,
+		ruleset: ['UU'],
+		banlist: ['UU', 'BL2']
+	},
+	{
+		name: "RU (suspect test)",
+		section: "XY Singles",
+
+		challengeShow: false,
 		ruleset: ['UU'],
 		banlist: ['UU', 'BL2']
 	},
