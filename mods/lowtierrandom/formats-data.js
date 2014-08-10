3606e39789c45fdb6bdcfb51357aa60f4ac69f46
awolffromspace <ewolft@gmail.com>
2014-08-10 17:03:24 -0400

    Update formats-data.js

--- mods/lowtierrandom/formats-data.js
+++ mods/lowtierrandom/formats-data.js
@@ -3,5 +3,5 @@ exports.BattleFormatsData = {
 		viableMoves: {"fakeout":1,"return":1,"suckerpunch":1,"earthquake":1,"wish":1,"poweruppunch":1,"crunch":1},
 		viableDoublesMoves: {"fakeout":1,"return":1,"suckerpunch":1,"earthquake":1,"facade":1,"poweruppunch":1,"crunch":1},
 		tier: "Uber"
-	},
+	}
 };
