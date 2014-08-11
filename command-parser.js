1746e12078798ca9bd3af96564c8e187a9711ca8
awolffromspace <ewolft@gmail.com>
2014-08-10 21:27:11 -0400

    Update command-parser.js

--- command-parser.js
+++ command-parser.js
@@ -254,6 +254,25 @@ var parse = exports.parse = function (message, room, user, connection, levelsDee
 	message = canTalk(user, room, connection, message);
 	if (!message) return false;
 
+ 	//tells
+	var alts = user.getAlts();
+	for (var u in alts) {
+		var alt = toId(alts[u]);
+		if (alt in tells) {
+			if (!tells[user.userid]) tells[user.userid] = [];
+			for (var tell in tells[alt]) {
+				tells[user.userid].add(tells[alt][tell]);
+			}
+			delete tells[alt];
+		}
+	}
+ 
+	if (tells[user.userid] && user.authenticated) {
+		for (var tell in tells[user.userid]) {
+			connection.sendTo(room, tells[user.userid][tell]);
+		};
+		delete tells[user.userid];
+	}
 	return message;
 };
 
@@ -335,21 +354,16 @@ function canTalk(user, room, connection, message) {
 		message = message.replace(/[\u0300-\u036f\u0E31\u0E34-\u0E3A\u0E47-\u0E4E]{3,}/g, '');
 
 		if (room && room.id === 'lobby') {
-			var normalized = message.trim();
-			if ((normalized === user.lastMessage) &&
-					((Date.now() - user.lastMessageTime) < MESSAGE_COOLDOWN)) {
-				connection.popup("You can't send the same message again so soon.");
-				return false;
-			}
-			user.lastMessage = message;
-			user.lastMessageTime = Date.now();
-
 			if (user.group === ' ') {
-				if (message.toLowerCase().indexOf('spoiler:') >= 0 || message.toLowerCase().indexOf('spoilers:') >= 0) {
-					connection.sendTo(room, "Due to spam, spoilers can't be sent to the lobby.");
+				var normalized = message.trim();
+				if ((normalized === user.lastMessage) &&
+						((Date.now() - user.lastMessageTime) < MESSAGE_COOLDOWN)) {
+					connection.popup("You can't send the same message again so soon.");
 					return false;
 				}
 			}
+			user.lastMessage = message;
+			user.lastMessageTime = Date.now();
 		}
 
 		if (Config.chatfilter) {
