b1e9859f1cac291600442946dc442b7f5f68aea8
Guangcong Luo <guangcongluo@gmail.com>
2014-08-10 21:16:14 -0400

    Support `npm test`
    Also made linting synchronous, which makes Gulp's output look
    slightly nicer (and time it, which is cool).

--- gulpfile.js
+++ gulpfile.js
@@ -49,28 +49,24 @@ var jsHintOptions = {
 
 gulp.task('lint', function () {
 	var directories = ['./*.js', './data/*.js', './mods/*/*.js', './tournaments/*.js', './chat-plugins/*.js', './config/*.js'];
-	console.log("\n\n*** Linting JavaScript Files ***\n\n");
 
 	// Replacing `var` with `let` is sort of a hack that stops jsHint from
 	// complaining that I'm using `var` like `let` should be used, but
 	// without having to deal with iffy `let` support.
 
-	gulp.src(directories)
+	return gulp.src(directories)
 		.pipe(replace(/\bvar\b/g, 'let'))
 		.pipe(jshint(jsHintOptions))
 		.pipe(jshint.reporter(jshintStylish));
-
 });
 
 gulp.task('fastlint', function () {
 	var directories = ['./*.js', './tournaments/*.js', './chat-plugins/*.js', './config/*.js'];
-	console.log("\n\n*** Linting JavaScript Files ***\n\n");
 
-	gulp.src(directories)
+	return gulp.src(directories)
 		.pipe(replace(/\bvar\b/g, 'let'))
 		.pipe(jshint(jsHintOptions))
 		.pipe(jshint.reporter(jshintStylish));
-
 });
 
 gulp.task('default', ['lint']);
