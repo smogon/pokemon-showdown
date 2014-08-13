05edfaf7a3d5a927b97dc04dc3441562d2b64482
Guangcong Luo <guangcongluo@gmail.com>
2014-08-12 21:34:30 -0400

    Gen 2: Swagger raises Atk, doesn't confuse against a Sub

--- mods/gen2/moves.js
+++ mods/gen2/moves.js
@@ -191,8 +191,13 @@ exports.BattleMovedex = {
 					var SubBlocked = {
 						leechseed:1, lockon:1, mindreader:1, nightmare:1, painsplit:1, sketch:1
 					};
-					if (move.status || move.boosts || move.volatileStatus === 'confusion' || SubBlocked[move.id]) {
-						return false;
+					if (move.id === 'swagger') {
+						// this is safe, move is a copy
+						delete move.volatileStatus;
+					}
+					if (move.status || (move.boosts && move.id !== 'swagger') || move.volatileStatus === 'confusion' || SubBlocked[move.id] || move.drain) {
+						this.add('-activate', target, 'Substitute', '[block] '+move.name);
+						return null;
 					}
 					return;
 				}
