18b70e718ca554dffa7dada7437cb4222a5bc470
Ivo Julca <ivojulca@hotmail.com>
2014-08-29 01:41:43 -0500

    Fix some pokémon HAs being deleted in recent gens by Gen 3 scripts

--- mods/gen3/pokedex.js
+++ mods/gen3/pokedex.js
@@ -1,14 +1,17 @@
 exports.BattlePokedex = {
 	chansey: {
 		inherit: true,
+		abilities: {0:"Natural Cure",1:"Serene Grace"},
 		prevo: ''
 	},
 	mrmime: {
 		inherit: true,
+		abilities: {0:"Swarm",1:"Technician"},
 		prevo: ''
 	},
 	snorlax: {
 		inherit: true,
+		abilities: {0:"Immunity",1:"Thick Fat"},
 		prevo: ''
 	}
 };
