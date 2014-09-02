10a9f18c01822a515d8bd06a4be2ff6f539ab005
awolffromspace <ewolft@gmail.com>
2014-09-02 16:40:50 -0400

    Update formats-data.js

--- mods/hightierrandom/formats-data.js
+++ mods/hightierrandom/formats-data.js
@@ -1,8 +1,11 @@
 exports.BattleFormatsData = {
 	gengarmega: {
-		randomBattleMoves: {"shadowball","sludgebomb","focusblast","thunderbolt","substitute","disable","painsplit","hypnosis","gigadrain"}
+		randomBattleMoves: ["shadowball","sludgebomb","focusblast","thunderbolt","substitute","disable","painsplit","hypnosis","gigadrain"]
+	},
+	mawilemega: {
+		randomBattleMoves: ["swordsdance","ironhead","firefang","substitute","playrough","suckerpunch","knockoff","focuspunch"]
 	},
 	lucariomega: {
-		randomBattleMoves: {"swordsdance","closecombat","crunch","extremespeed","icepunch","bulletpunch","nastyplot","aurasphere","darkpulse","vacuumwave","flashcannon"}
+		randomBattleMoves: ["swordsdance","closecombat","crunch","extremespeed","icepunch","bulletpunch","nastyplot","aurasphere","darkpulse","vacuumwave","flashcannon"]
 	}
 };
