30936a0918ef7e5269dcc8495023b2869dd16afe
awolffromspace <ewolft@gmail.com>
2014-09-02 16:25:19 -0400

    Update formats-data.js

--- mods/lowtierrandom/formats-data.js
+++ mods/lowtierrandom/formats-data.js
@@ -1,7 +1,6 @@
 exports.BattleFormatsData = {
 	kangaskhanmega: {
-		viableMoves: {"fakeout":1,"return":1,"suckerpunch":1,"earthquake":1,"wish":1,"poweruppunch":1,"crunch":1},
-		viableDoublesMoves: {"fakeout":1,"return":1,"suckerpunch":1,"earthquake":1,"facade":1,"poweruppunch":1,"crunch":1},
+		randomBattleMoves: {"fakeout":1,"return":1,"suckerpunch":1,"earthquake":1,"wish":1,"poweruppunch":1,"crunch":1},
 		tier: "Uber"
 	}
 };
