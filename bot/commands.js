45434b229a45165d49d914fc9741c2152faa7366
awolffromspace <ewolft@gmail.com>
2014-09-13 14:34:33 -0400

    Update commands.js

--- bot/commands.js
+++ bot/commands.js
@@ -440,7 +440,7 @@ exports.commands = {
 		} else {
 			var text = '/pm ' + by + ', ';
 		}
-		text += 'http://sim.smogon.com:8080/Stats/2014-08/';
+		text += 'http://sim.smogon.com:8080/Stats/';
 		this.say(con, room, text);
 	},
 	seen: function(arg, by, room, con) { // this command is still a bit buggy
