d5a25beff6776d2af95b1b6d7a4a81b5b235db81
asgdf <tobias@cehceh.de>
2014-09-13 16:32:47 +0200

    correct /faq star
    The star is in front of the username in battles, not behind it

--- config/commands.js
+++ config/commands.js
@@ -1119,7 +1119,7 @@ var commands = exports.commands = {
 		}
 		if (target === 'all' || target === 'star' || target === 'player') {
 			matched = true;
-			buffer += '<a href="http://www.smogon.com/sim/faq#star">Why is there this star (&starf;) behind my username?</a><br />';
+			buffer += '<a href="http://www.smogon.com/sim/faq#star">Why is there this star (&starf;) in front of my username?</a><br />';
 		}
 		if (target === 'staff') {
 			matched = true;
