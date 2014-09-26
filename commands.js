8e48f9d32a502fa29205bc4f64d96e7820a013dc
Guangcong Luo <guangcongluo@gmail.com>
2014-09-26 05:08:48 -0400

    Fix crashes for vanity groups

--- commands.js
+++ commands.js
@@ -433,9 +433,9 @@ var commands = exports.commands = {
 
 		var buffer = [];
 		Object.keys(rankLists).sort(function (a, b) {
-			return Config.groups[b].rank - Config.groups[a].rank;
+			return (Config.groups[b]||{rank:0}).rank - (Config.groups[a]||{rank:0}).rank;
 		}).forEach(function (r) {
-			buffer.push(Config.groups[r].name + "s (" + r + "):\n" + rankLists[r].sort().join(", "));
+			buffer.push((Config.groups[r] ? Config.groups[r] .name + "s (" + r + ")" : r) + ":\n" + rankLists[r].sort().join(", "));
 		});
 
 		if (!buffer.length) {
