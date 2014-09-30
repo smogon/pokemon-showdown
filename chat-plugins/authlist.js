deb79d221f322d29649c8365ed48cb4ca213dcf2
小太 <nospam@xiaotai.org>
2014-09-30 23:17:33 +1000

    Fix /authlist for non-existant groups

--- chat-plugins/authlist.js
+++ chat-plugins/authlist.js
@@ -11,9 +11,9 @@ exports.commands = {
 
 		var buffer = [];
 		Object.keys(rankLists).sort(function (a, b) {
-			return Config.groups.bySymbol[b].rank - Config.groups.bySymbol[a].rank;
+			return (Config.groups.bySymbol[b] || {rank: 0}).rank - (Config.groups.bySymbol[a] || {rank: 0}).rank;
 		}).forEach(function (r) {
-			buffer.push(Config.groups.bySymbol[r].name + "s (" + r + "):\n" + rankLists[r].sort().join(", "));
+			buffer.push((Config.groups.bySymbol[r] ? Config.groups.bySymbol[r].name + "s (" + r + ")" : r) + ":\n" + rankLists[r].sort().join(", "));
 		});
 
 		if (!buffer.length) {
