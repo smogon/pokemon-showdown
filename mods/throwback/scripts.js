3d21e334e8c12d578322a13c761ee49ca20dec3a
The Immortal <the_immortal123@live.com>
2014-10-01 13:29:33 +0400

    Update OMotM

---
+++ mods/throwback/scripts.js
@@ -0,0 +1,26 @@
+exports.BattleScripts = {
+	inherit: 'gen5',
+	gen: 4,
+	init: function () {
+		for (var i in this.data.Pokedex) {
+			var tier = '';
+			var adjustment = 0;
+			if (this.data.FormatsData[i]) tier = this.data.FormatsData[i].tier || this.data.FormatsData[toId(this.getTemplate(i).baseSpecies)].tier;
+			switch (tier) {
+			case 'BL':
+			case 'UU':
+				adjustment = 5;
+				break;
+			case 'BL2':
+			case 'NU':
+				adjustment = 10;
+			}
+
+			if (adjustment) {
+				for (var j in this.data.Pokedex[i].baseStats) {
+					this.modData('Pokedex', i).baseStats[j] = this.clampIntRange(this.data.Pokedex[i].baseStats[j] + adjustment, 1, 255);
+				}
+			}
+		}
+	}
+};
