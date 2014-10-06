2adf3342cc0eee38cdcbfccada29693c816e81b7
awolffromspace <ewolft@gmail.com>
2014-10-05 22:02:24 -0400

    Add PC Staff Team Random

--- config/formats.js
+++ config/formats.js
@@ -6,13 +6,13 @@ exports.Formats = [
 	// XY Singles
 	///////////////////////////////////////////////////////////////////
 
-	{
+	/*{
 		name: "XY Anniversary Random (BWknd 23)",
 		section: "XY Singles",
 
 		team: 'randomXY',
 		ruleset: ['Random (no PotD)']
-	},
+	},*/
 	{
 		name: "OU",
 		section: "XY Singles",
@@ -240,6 +240,14 @@ exports.Formats = [
 
 		searchShow: false,
 		team: 'randomPCStaff',
+		ruleset: ['Random (no PotD)']
+	},
+	{
+		name: "PC Staff Team Random",
+		section: "Random Battles (aka Randbats)",
+
+		searchShow: false,
+		team: 'randomPCStaffTeam',
 		ruleset: ['Random (no PotD)']
 	},
 	{
