cab60679376c133e1dfedc9d87de5a3c375180ad
unknown <ewolft@gmail.com>
2014-10-16 20:00:38 -0400

    Updates

---
+++ mods/furryrandom/formats-data.js
@@ -0,0 +1,10 @@
+exports.BattleFormatsData = {
+	purrloin: {
+		randomBattleMoves: ["foulplay","swagger","substitute","thunderwave"],
+		tier: "LC"
+	},
+	liepard: {
+		randomBattleMoves: ["foulplay","swagger","substitute","thunderwave"],
+		tier: "NU"
+	}
+};
