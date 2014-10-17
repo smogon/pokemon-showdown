0914d28ef04c4565dd349032f01ffea1e39c9257
awolffromspace <ewolft@gmail.com>
2014-10-17 17:28:11 -0400

    Update users.js

--- users.js
+++ users.js
@@ -1063,6 +1063,7 @@ User = (function () {
 		this.lastConnected = Date.now();
 	};
 	User.prototype.onDisconnect = function (connection) {
+		Core.stdout('lastSeen', this.userid, Date.now());
 		for (var i = 0; i < this.connections.length; i++) {
 			if (this.connections[i] === connection) {
 				// console.log('DISCONNECT: ' + this.userid);
