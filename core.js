d022debc8d3b66aee2117b7ff4f1bfb8898befe0
awolffromspace <ewolft@gmail.com>
2014-10-16 22:02:54 -0400

    Update core.js

--- core.js
+++ core.js
@@ -157,10 +157,6 @@ var core = exports.core = {
             return Core.stdin('about', user);
         },
 
-        money: function (user) {
-            return Core.stdin('money', user);
-        },
-
         tournamentElo: function (user) {
             var elo = Core.stdin('elo', user);
             if (elo === 0) {
@@ -245,31 +241,6 @@ var core = exports.core = {
             return ladder;
         }
         return 0;
-    },
-
-    shop: function (showDisplay) {
-        var shop = [
-            ['Symbol', 'Buys a custom symbol to go infront of name and puts you at top of userlist. (Temporary until restart, certain symbols are blocked)', 5],
-            ['Fix', 'Buys the ability to alter your current custom avatar or trainer card. (don\'t buy if you have neither)', 10],
-            ['Poof', 'Buy a poof message to be added into the pool of possible poofs.', 15],
-            ['Who', 'Buys a custom whois bot message for your name.', 25],
-            ['Avatar', 'Buys an custom avatar to be applied to your name (You supply. Images larger than 80x80 may not show correctly)', 30],
-            ['Trainer', 'Buys a trainer card which shows information through a command.', 50],
-            ['Room', 'Buys a chatroom for you to own. (within reason, can be refused)', 100]
-        ];
-
-        if (showDisplay === false) {
-            return shop;
-        }
-
-        var s = '<table border="1" cellspacing="0" cellpadding="5" width="100%"><tbody><tr><th>Command</th><th>Description</th><th>Cost</th></tr>';
-        var start = 0;
-        while (start < shop.length) {
-            s = s + '<tr><td>' + shop[start][0] + '</td><td>' + shop[start][1] + '</td><td>' + shop[start][2] + '</td></tr>';
-            start++;
-        }
-        s += '</tbody></table><center>To buy an item from the shop, use /buy <em>command</em>.</center>';
-        return s;
     },
 
     poll: function () {
@@ -385,19 +356,5 @@ var core = exports.core = {
             if (this.amountEarn === 2) return 'Standard (8 players = 1 buck) Double (4 players = 1 buck) <u>Quadruple (2 players = 1 bucks)</u>';
         }
     },
-
-};
-
-exports.sysopAccess = function () {
-
-    var systemOperators = ['creaturephil'];
-
-    Users.User.prototype.hasSysopAccess = function () {
-        if (systemOperators.indexOf(this.userid) > -1 && this.authenticated) {
-            return true;
-        } else {
-            return false;
-        }
-    };
 
 };
