smogon/pokemon-showdown

commit 530ceec72815c54b9a15f00ff0693d1b8eb7f0a9

Tournament code no longer overrides BattleRoom.win
Since tournament battle rooms already keep track of the room where the
tournament began, it's possible to have the default BattleRoom.win submit
the results of the battle instead of having to override the function and
still call the old code (very messy).

tournaments/index.js
@@ -138,7 +138,7 @@ Tournament = (function () {
 	Tournament.prototype.forceEnd = function () {
 		if (this.isTournamentStarted) {
 			this.inProgressMatches.forEach(function (match) {
-				if (match) delete match.room.win;
+				if (match) delete match.room.tour;
 			});
 		}
 		this.isEnded = true;
@@ -647,12 +647,6 @@ Tournament = (function () {
 		this.isBracketInvalidated = true;
 		this.runAutoDisqualify();
 		this.update();
-
-		var self = this;
-		room.win = function (winner) {
-			self.onBattleWin(this, Users.get(winner));
-			return Object.getPrototypeOf(this).win.call(this, winner);
-		};
 	};
 	Tournament.prototype.onBattleWin = function (room, winner) {
 		var from = Users.get(room.p1);

rooms.js
@@ -749,12 +749,12 @@ var BattleRoom = (function () {
 				});
 			}
 		}
-		// if (this.tour) {
-		// 	var winnerid = toId(winner);
-		// 	winner = Users.get(winner);
-		// 	var tour = this.tour.tour;
-		// 	tour.onBattleWin(this, winner);
-		// }
+		if (this.tour) {
+			var winnerid = toId(winner);
+			winner = Users.get(winner);
+			var tour = this.tour.tour;
+			tour.onBattleWin(this, winner);
+		}
 		rooms.global.battleCount += 0 - (this.active ? 1 : 0);
 		this.active = false;
 		this.update();

commit 82f83ef000bb12acf2579b71ef253cdaaf020d42

Fix crash when dq'ing from a tour
Crash caused by 530ceec7

tournaments/index.js
@@ -471,7 +471,7 @@ Tournament = (function () {
 		if (matchFrom) {
 			this.generator.setUserBusy(matchFrom.to, false);
 			this.inProgressMatches.set(user, null);
-			delete matchFrom.room.win;
+			delete matchFrom.room.tour;
 			matchFrom.room.forfeit(user);
 		}
 
@@ -482,7 +482,7 @@ Tournament = (function () {
 		if (matchTo) {
 			this.generator.setUserBusy(matchTo, false);
 			var matchRoom = this.inProgressMatches.get(matchTo).room;
-			delete matchRoom.win;
+			delete matchRoom.tour;
 			matchRoom.forfeit(user);
 			this.inProgressMatches.set(matchTo, null);
 		}