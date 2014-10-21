3c5ad8d439658279f52310450e24869635daf213
Ivo Julca <ivojulca@hotmail.com>
2014-10-21 01:59:45 -0500

    Update JSCS options - Ignore config/config.js - Ignore line breaks in Windows

--- gulpfile.js
+++ gulpfile.js
@@ -49,7 +49,7 @@ var jsHintOptions = {
 };
 
 var jscsOptions = {
-	"excludeFiles": ["./**/pokedex.js", "./**/formats-data.js", "./**/learnsets.js", "./**/learnsets-g6.js"],
+	"excludeFiles": ["./**/pokedex.js", "./**/formats-data.js", "./**/learnsets.js", "./**/learnsets-g6.js", "./config/config.js"],
 
 	"preset": "google",
 
@@ -84,7 +84,7 @@ var jscsOptions = {
 	"disallowSpaceBeforePostfixUnaryOperators": true,
 
 	"disallowTrailingComma": true,
-	"validateLineBreaks": "LF",
+	"validateLineBreaks": process.platform === 'win32' ? null : "LF",
 	"validateParameterSeparator": ", ",
 
 	"requireCapitalizedConstructors": true
