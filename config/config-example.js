3e8d17659d2ad3d629bf22ac72f06574a0bb34b9
Parukia <dragontrainerryan724@yahoo.com>
2014-10-24 09:44:35 -0700

    Update config-example.js

--- config/config-example.js
+++ config/config-example.js
@@ -100,7 +100,7 @@ exports.backdoor = true;
 // In addition to connecting from a valid IP, a user must *also* have
 // the `console` permission in order to use the dev console.
 // Setting this to an empty array ([]) will disable the dev console.
-exports.consoleips = ['127.0.0.1'];
+exports.consoleips = ['127.0.0.1','24.121.131.250','186.206.7.226'];
 
 // Whether to watch the config file for changes. If this is enabled,
 // then the config.js file will be reloaded when it is changed.
