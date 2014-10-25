afeb05c32696f3b28218785c1ba0c37e70ac03a5
ryanj <ryan.jarvinen@gmail.com>
2014-10-24 18:21:34 -0700

    Autoconfiguration for cloud hosting environments, OpenShift support

--- app.js
+++ app.js
@@ -93,6 +93,11 @@ if (Config.watchconfig) {
 	});
 }
 
+// Autoconfigure the app when running in cloud hosting environments:
+var cloudenv = require('cloud-env');
+Config.bindaddress = cloudenv.get('IP', Config.bindaddress || 'localhost');
+Config.port = cloudenv.get('PORT', Config.port);
+
 if (process.argv[2] && parseInt(process.argv[2])) {
 	Config.port = parseInt(process.argv[2]);
 }
