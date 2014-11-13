3da3db10a5943918874578578d4a7e34d65bed21
The Immortal <the_immortal123@live.com>
2014-11-13 17:09:08 +0400

    Ubers suspect test is over

--- config/formats.js
+++ config/formats.js
@@ -33,15 +33,6 @@ exports.Formats = [
 		name: "Ubers",
 		section: "XY Singles",
 
-		searchShow: false,
-		ruleset: ['Pokemon', 'Standard Ubers', 'Swagger Clause', 'Team Preview'],
-		banlist: []
-	},
-	{
-		name: "Ubers (suspect test)",
-		section: "XY Singles",
-
-		challengeShow: false,
 		ruleset: ['Pokemon', 'Standard Ubers', 'Swagger Clause', 'Team Preview'],
 		banlist: []
 	},
