64598fa246a2163dc3774f80ec6eae7c40fe38a0
The Immortal <the_immortal123@live.com>
2014-11-27 12:13:08 +0400

    Scolipede is banned from UU

--- data/formats-data.js
+++ data/formats-data.js
@@ -4133,7 +4133,7 @@ exports.BattleFormatsData = {
 	scolipede: {
 		randomBattleMoves: ["spikes","toxicspikes","megahorn","rockslide","earthquake","swordsdance","batonpass","aquatail","superpower"],
 		randomDoubleBattleMoves: ["substitute","protect","megahorn","rockslide","poisonjab","swordsdance","batonpass","aquatail","superpower"],
-		tier: "UU"
+		tier: "BL"
 	},
 	cottonee: {
 		randomBattleMoves: ["encore","taunt","substitute","leechseed","toxic","stunspore"],
