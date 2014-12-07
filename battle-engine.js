7890ca9f1679b0ff45c53ce57af6afb2940f3614
Guangcong Luo <guangcongluo@gmail.com>
2014-12-07 16:33:28 -0600

    Revert "Explosion and Self-Destruct bug-fix"
    It broke faint mechanics. :(
    
    This reverts commit 1f84b4bf8076d5ef0471487dbdd18f4480942515.

--- battle-engine.js
+++ battle-engine.js
@@ -782,6 +782,7 @@ BattlePokemon = (function () {
 		var d = this.hp;
 		this.hp = 0;
 		this.switchFlag = false;
+		this.status = 'fnt';
 		this.battle.faintQueue.push({
 			target: this,
 			source: source,
@@ -2431,7 +2432,6 @@ Battle = (function () {
 			pokemon.position = pos;
 			side.pokemon[pokemon.position] = pokemon;
 			side.pokemon[oldActive.position] = oldActive;
-			if (oldActive.fainted) oldActive.status = 'fnt';
 			this.cancelMove(oldActive);
 			oldActive.clearVolatile();
 		}
