dfec6dc144be080e56de6bc838148dce2cbc095b
Ivo Julca <ivojulca@hotmail.com>
2014-12-19 20:15:53 -0500

    Move some Gen 1 OU rules to Gen 1 mod rulesets - Freeze Clause Mod is standard. - Filler bans are moved higher up.

--- config/formats.js
+++ config/formats.js
@@ -1083,17 +1083,8 @@ exports.Formats = [
 		section: "Past Generations",
 
 		mod: 'gen1',
-		ruleset: ['Pokemon', 'Standard', 'Freeze Clause Mod'],
-		banlist: ['Uber',
-			'Kakuna + Poison Sting + Harden', 'Kakuna + String Shot + Harden',
-			'Beedrill + Poison Sting + Harden', 'Beedrill + String Shot + Harden',
-			'Nidoking + Fury Attack + Thrash',
-			'Exeggutor + Poison Powder + Stomp', 'Exeggutor + Sleep Powder + Stomp', 'Exeggutor + Stun Spore + Stomp',
-			'Eevee + Tackle + Growl',
-			'Vaporeon + Tackle + Growl',
-			'Jolteon + Tackle + Growl', 'Jolteon + Focus Energy + Thunder Shock',
-			'Flareon + Tackle + Growl', 'Flareon + Focus Energy + Ember'
-		]
+		ruleset: ['Pokemon', 'Standard'],
+		banlist: ['Uber']
 	},
 	{
 		name: "[Gen 1] Custom Game",
