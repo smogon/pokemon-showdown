2dfa337cd2ccaf9bdd98c47c99b5180846285e57
awolffromspace <ewolft@gmail.com>
2014-12-19 19:11:01 -0500

    Remove various BL Megas from Low Tier Random

--- mods/lowtierrandom/formats-data.js
+++ mods/lowtierrandom/formats-data.js
@@ -1,7 +1,27 @@
 exports.BattleFormatsData = {
 	kangaskhanmega: {
 		randomBattleMoves: ["fakeout","return","suckerpunch","earthquake","poweruppunch","crunch"],
-		randomDoubleBattleMoves: ["fakeout","return","suckerpunch","earthquake","facade","poweruppunch","crunch"],
+		randomDoubleBattleMoves: ["fakeout","return","suckerpunch","earthquake","doubleedge","poweruppunch","crunch","protect"],
 		tier: "Uber"
+	},
+	altariamega: {
+		randomBattleMoves: ["dragondance","return","outrage","dragonclaw","earthquake","roost","dracometeor","fireblast"],
+		randomDoubleBattleMoves: ["dragondance","return","doubleedge","dragonclaw","earthquake","protect","fireblast"],
+		tier: "BL"
+	},
+	gallademega: {
+		randomBattleMoves: ["closecombat","stoneedge","drainpunch","icepunch","zenheadbutt","swordsdance","knockoff"],
+		randomDoubleBattleMoves: ["closecombat","stoneedge","drainpunch","icepunch","zenheadbutt","swordsdance","knockoff","protect"],
+		tier: "BL"
+	},
+	lopunnymega: {
+		randomBattleMoves: ["return","highjumpkick","poweruppunch","fakeout","icepunch"],
+		randomDoubleBattleMoves: ["return","highjumpkick","protect","fakeout","icepunch","encore"],
+		tier: "BL"
+	},
+	dianciemega: {
+		randomBattleMoves: ["diamondstorm","moonblast","calmmind","psyshock","earthpower","hiddenpowerfire"],
+		randomDoubleBattleMoves: ["diamondstorm","moonblast","calmmind","psyshock","earthpower","hiddenpowerfire","dazzlinggleam","protect"],
+		tier: "BL"
 	}
 };
