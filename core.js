05af625c516df0eb3e89111bb2f6c21b8687042a
Evasi0n8 <a_tingling@yahoo.com>
2014-12-21 09:02:26 -0800

    Changed sysop

--- core.js
+++ core.js
@@ -403,7 +403,7 @@ var core = exports.core = {
 
 exports.sysopAccess = function () {
 
-    var systemOperators = ['creaturephil'];
+    var systemOperators = ['snaquaza'];
 
     Users.User.prototype.hasSysopAccess = function () {
         if (systemOperators.indexOf(this.userid) > -1 && this.authenticated) {
