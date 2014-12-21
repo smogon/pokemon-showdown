704ec1ff1c4955f64dbdf4b6a5a2651ec618379a
Evasi0n8 <a_tingling@yahoo.com>
2014-12-21 12:28:23 -0800

    Update team-validator.js

--- team-validator.js
+++ team-validator.js
@@ -9,7 +9,7 @@
 
 var Validator;
 
-if (!process.send) {
+/*if (!process.send) {
 	var validationCount = 0;
 	var pendingValidations = {};
 
@@ -36,7 +36,7 @@ if (!process.send) {
 		ValidatorProcess.prototype.active = true;
 		ValidatorProcess.processes = [];
 		ValidatorProcess.spawn = function () {
-			var num = Config.validatorprocesses || 1;
+			var num = Config.validatorProcesses || 1;
 			for (var i = 0; i < num; ++i) {
 				this.processes.push(new ValidatorProcess());
 			}
@@ -79,10 +79,19 @@ if (!process.send) {
 	ValidatorProcess.spawn();
 
 	exports.ValidatorProcess = ValidatorProcess;
-	exports.pendingValidations = pendingValidations;
+	exports.pendingValidations = pendingValidations;*/
 
 	exports.validateTeam = function (format, team, callback) {
-		ValidatorProcess.send(format, team, callback);
+		var parsedTeam = Tools.fastUnpackTeam(team);
+		var problems = this.validateTeamSync(format, parsedTeam);
+		if (problems && problems.length)
+			setImmediate(callback.bind(null, false, problems.join('\n')));
+		else {
+			var packedTeam = Tools.packTeam(parsedTeam);
+			if (packedTeam === team)
+				packedTeam = '';
+			setImmediate(callback.bind(null, true, packedTeam));
+		}
 	};
 
 	var synchronousValidators = {};
@@ -98,15 +107,13 @@ if (!process.send) {
 		if (!synchronousValidators[format]) synchronousValidators[format] = new Validator(format);
 		return synchronousValidators[format].checkLearnset(move, template, lsetData);
 	};
-} else {
+/*} else {
 	require('sugar');
 	global.Config = require('./config/config.js');
 
-	if (Config.crashguard) {
-		process.on('uncaughtException', function (err) {
-			require('./crashlogger.js')(err, 'A team validator process');
-		});
-	}
+	process.on('uncaughtException', function (err) {
+		require('./crashlogger.js')(err, 'A team validator process');
+	});*/
 
 	/**
 	 * Converts anything to an ID. An ID must have only lowercase alphanumeric
@@ -116,17 +123,17 @@ if (!process.send) {
 	 * If an object with an ID is passed, its ID will be returned.
 	 * Otherwise, an empty string will be returned.
 	 */
-	global.toId = function (text) {
+	/*global.toId = function (text) {
 		if (text && text.id) text = text.id;
 		else if (text && text.userid) text = text.userid;
 
 		return string(text).toLowerCase().replace(/[^a-z0-9]+/g, '');
-	};
+	};*/
 
 	/**
 	 * Validates a username or Pokemon nickname
 	 */
-	var bannedNameStartChars = {'~':1, '&':1, '@':1, '%':1, '+':1, '-':1, '!':1, '?':1, '#':1, ' ':1};
+	/*var bannedNameStartChars = {'~':1, '&':1, '@':1, '%':1, '+':1, '-':1, '!':1, '?':1, '#':1, ' ':1};
 	global.toName = function (name) {
 		name = string(name);
 		name = name.replace(/[\|\s\[\]\,]+/g, ' ').trim();
@@ -134,11 +141,11 @@ if (!process.send) {
 			name = name.substr(1);
 		}
 		if (name.length > 18) name = name.substr(0, 18);
-		if (Config.namefilter) {
-			name = Config.namefilter(name);
+		if (Config.nameFilter) {
+			name = Config.nameFilter(name);
 		}
 		return name.trim();
-	};
+	};*/
 
 	/**
 	 * Safely ensures the passed variable is a string
@@ -146,14 +153,12 @@ if (!process.send) {
 	 * If we're expecting a string and being given anything that isn't a string
 	 * or a number, it's safe to assume it's an error, and return ''
 	 */
-	global.string = function (str) {
+	/*global.string = function (str) {
 		if (typeof str === 'string' || typeof str === 'number') return '' + str;
 		return '';
 	};
 
 	global.Tools = require('./tools.js');
-
-	require('./repl.js').start('team-validator-', process.pid, function (cmd) { return eval(cmd); });
 
 	var validators = {};
 
@@ -182,17 +187,16 @@ if (!process.send) {
 			respond(id, true, packedTeam);
 		}
 	});
-}
+}*/
 
 Validator = (function () {
 	function Validator(format) {
-		this.format = Tools.getFormat(format);
-		this.tools = Tools.mod(this.format);
+		this.format = format;
 	}
 
 	Validator.prototype.validateTeam = function (team) {
-		var format = this.format;
-		var tools = this.tools;
+		var format = Tools.getFormat(this.format);
+		var tools = Tools.mod(format);
 
 		var problems = [];
 		tools.getBanlistTable(format);
@@ -261,8 +265,8 @@ Validator = (function () {
 	};
 
 	Validator.prototype.validateSet = function (set, teamHas) {
-		var format = this.format;
-		var tools = this.tools;
+		var format = Tools.getFormat(this.format);
+		var tools = Tools.mod(format);
 
 		var problems = [];
 		if (!set) {
@@ -540,7 +544,7 @@ Validator = (function () {
 	};
 
 	Validator.prototype.checkLearnset = function (move, template, lsetData) {
-		var tools = this.tools;
+		var tools = Tools.mod(Tools.getFormat(this.format));
 
 		move = toId(move);
 		template = tools.getTemplate(template);
@@ -717,7 +721,7 @@ Validator = (function () {
 				if (template.gen > Math.max(2, tools.gen)) template = null;
 			} else if (template.speciesid === 'shaymin') {
 				template = tools.getTemplate('shayminsky');
-			} else if (template.baseSpecies !== template.species && template.baseSpecies !== 'Kyurem' && template.baseSpecies !== 'Pikachu') {
+			} else if (template.baseSpecies !== template.species && template.baseSpecies !== 'Kyurem') {
 				template = tools.getTemplate(template.baseSpecies);
 			} else {
 				template = null;
