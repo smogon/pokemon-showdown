a969bddfc57bcf32294e67653402b5cad15f9209
Evasi0n8 <a_tingling@yahoo.com>
2014-12-22 09:42:55 -0800

    Update verifier.js

--- verifier.js
+++ verifier.js
@@ -14,51 +14,48 @@
 
 // Because I don't want two files, we're going to fork ourselves.
 
-var fakeProcess = new (require('./fake-process').FakeProcess)();
-//if (!process.send) {
-
+if (!process.send) {
 	// This is the parent
 
 	var guid = 1;
 	var callbacks = {};
 	var callbackData = {};
 
-	//var child = require('child_process').fork('verifier.js');
+	var child = require('child_process').fork('verifier.js');
 	exports.verify = function (data, signature, callback) {
 		var localGuid = guid++;
 		callbacks[localGuid] = callback;
 		callbackData[localGuid] = data;
-		fakeProcess.server.send({data: data, sig: signature, guid: localGuid});
+		child.send({data: data, sig: signature, guid: localGuid});
 	};
-	fakeProcess.server.on('message', function (response) {
+	child.on('message', function (response) {
 		if (callbacks[response.guid]) {
 			callbacks[response.guid](response.success, callbackData[response.guid]);
 			delete callbacks[response.guid];
 			delete callbackData[response.guid];
 		}
 	});
-
-//} else {
-
+} else {
 	// This is the child
 
 	global.Config = require('./config/config.js');
 	var crypto = require('crypto');
 
-	var keyalgo = Config.loginServer.keyAlgorithm;
-	var pkey = Config.loginServer.publicKey;
+	var keyalgo = Config.loginserverkeyalgo;
+	var pkey = Config.loginserverpublickey;
 
-	fakeProcess.client.on('message', function (message) {
+	process.on('message', function (message) {
 		var verifier = crypto.createVerify(keyalgo);
 		verifier.update(message.data);
 		var success = false;
 		try {
 			success = verifier.verify(pkey, message.sig, 'hex');
 		} catch (e) {}
-		fakeProcess.client.send({
+		process.send({
 			success: success,
 			guid: message.guid
 		});
 	});
 
-//}
+	require('./repl.js').start('verifier', function (cmd) { return eval(cmd); });
+}
