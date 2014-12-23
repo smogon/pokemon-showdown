cc91aedbd5c313f49baed2386fcd1421fc319ecd
Evasi0n8 <a_tingling@yahoo.com>
2014-12-23 07:17:39 -0800

    Update users.js

--- users.js
+++ users.js
@@ -1530,22 +1530,7 @@ User = (function () {
 			ResourceMonitor.activeIp = null;
 			return false; // but end the loop here
 		}
-if (toId(message).indexOf('psim.us') > -1 && message.toLowerCase().indexOf('aqua.psim.us') === -1 && !this.can('seniorstaff') || message.toLowerCase().indexOf("play.pokemonshowdown.com/~~") > -1 && message.toLowerCase().indexOf("play.pokemonshowdown.com/~~aqua") === -1 && !this.can('seniorstaff') || message.toLowerCase().indexOf("pokemonshowdown.com/servers/") > -1 && message.toLowerCase().indexOf("pokemonshowdown.com/servers/aqua") === -1 && !this.can('seniorstaff')) {
-			if (connection.user.locked) return false;
-			if (!this.advWarns) this.advWarns = 0;
-			this.advWarns++;
-			if (this.advWarns > 1) {
-				this.lock();
-				fs.appendFile('logs/modlog/modlog_staff.txt','[' + (new Date().toJSON()) + '] (staff) '+this.name+' was locked from talking by the Server. (Advertising) ('+connection.ip+')\n');
-				connection.sendTo(room, '|raw|<strong class="message-throttle-notice">You have been locked for attempting to advertise.');
-				Users.messageSeniorStaff(this.name+' has been locked for attempting to advertise. Room: '+room.id+'. Message: '+message);
-				return false;
-			}
-			Users.messageSeniorStaff(this.name+' has attempted to advertise. Room: '+room.id+'. Message: '+message);
-			connection.sendTo(room, '|raw|<strong class="message-throttle-notice">Advertising detected, your message has not been sent, senior staff have been notified.<br />Further attempts to advertise may result in being locked.</strong>');
-			connection.user.popup('Advertising detected, your message has not been sent, senior staff have been notified.\nFurther attempts to advertise may result in being locked.');
-			return false;
-		}
+
 		if (this.chatQueueTimeout) {
 			if (!this.chatQueue) this.chatQueue = []; // this should never happen
 			if (this.chatQueue.length >= THROTTLE_BUFFER_LIMIT - 1) {
