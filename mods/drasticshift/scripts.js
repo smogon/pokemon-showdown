e174b1134f6d963547bac381da2282c818dcb962
Gummygamer <aisushin@gmail.com>
2014-12-26 19:05:38 -0300

    Create scripts.js
    The file that defines Drastic Tier Shift

---
+++ mods/drasticshift/scripts.js
@@ -0,0 +1,34 @@
+exports.BattleScripts = {
+	init: function () {
+		for (var i in this.data.Pokedex) {
+			var tier = '';
+			var adjustment = 0;
+			// mega evolutions get the same stat boost as their base forme
+			if (this.data.FormatsData[i]) tier = this.data.FormatsData[i].tier || this.data.FormatsData[toId(this.getTemplate(i).baseSpecies)].tier;
+			switch (tier) {
+			case 'BL':
+			case 'UU':
+				adjustment = 10;
+				break;
+			case 'BL2':
+			case 'RU':
+				adjustment = 20;
+				break;
+			case 'BL3':
+			case 'NU':
+				adjustment = 30;
+			case 'PU':
+			case 'NFE':
+			case 'LC Uber':
+			case 'LC':
+				adjustment = 40;
+			}
+
+			if (adjustment) {
+				for (var j in this.data.Pokedex[i].baseStats) {
+					this.modData('Pokedex', i).baseStats[j] = this.clampIntRange(this.data.Pokedex[i].baseStats[j] + adjustment, 1, 255);
+				}
+			}
+		}
+	}
+};
