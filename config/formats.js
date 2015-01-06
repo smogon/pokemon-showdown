6e72281f399c67c2269e2df67802e6230e1a0251
Joimer <joimpoke@gmail.com>
2015-01-06 12:54:38 +0100

    Gen 1 tradebacks: Change Flareon moveset ban
    Tackle with Growl is legal, Focus Energy with Ember is illegal.

--- config/formats.js
+++ config/formats.js
@@ -976,7 +976,7 @@ exports.Formats = [
 		ruleset: ['Pokemon', 'Sleep Clause Mod', 'Freeze Clause Mod', 'Species Clause', 'OHKO Clause', 'Evasion Moves Clause', 'HP Percentage Mod'],
 		banlist: ['Uber', 'Unreleased', 'Illegal',
 			'Nidoking + Fury Attack + Thrash', 'Exeggutor + Poison Powder + Stomp', 'Exeggutor + Sleep Powder + Stomp',
-			'Exeggutor + Stun Spore + Stomp', 'Jolteon + Focus Energy + Thunder Shock', 'Flareon + Tackle + Growl'
+			'Exeggutor + Stun Spore + Stomp', 'Jolteon + Focus Energy + Thunder Shock', 'Flareon + Focus Energy + Ember'
 		]
 	},
 	{
