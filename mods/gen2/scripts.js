81d7aa49fd416453cd2c1d7cdb1e6e399a599b11
Joimer <joimpoke@gmail.com>
2015-01-08 20:54:37 +0100

    Gens 1 & 2: Fix small bug with recoil and drain damage

--- mods/gen2/scripts.js
+++ mods/gen2/scripts.js
@@ -811,5 +811,67 @@ exports.BattleScripts = {
 		pokemon.update();
 		this.addQueue({pokemon: pokemon, choice: 'runSwitch'});
 		return true;
+	},
+	damage: function (damage, target, source, effect) {
+		if (this.event) {
+			if (!target) target = this.event.target;
+			if (!source) source = this.event.source;
+			if (!effect) effect = this.effect;
+		}
+		if (!target || !target.hp) return 0;
+		effect = this.getEffect(effect);
+		if (!(damage || damage === 0)) return damage;
+		if (damage !== 0) damage = this.clampIntRange(damage, 1);
+
+		if (effect.id !== 'struggle-recoil') { // Struggle recoil is not affected by effects
+			if (effect.effectType === 'Weather' && !target.runImmunity(effect.id)) {
+				this.debug('weather immunity');
+				return 0;
+			}
+			damage = this.runEvent('Damage', target, source, effect, damage);
+			if (!(damage || damage === 0)) {
+				this.debug('damage event failed');
+				return damage;
+			}
+			if (target.illusion && effect && effect.effectType === 'Move') {
+				this.debug('illusion cleared');
+				target.illusion = null;
+				this.add('replace', target, target.getDetails);
+			}
+		}
+		if (damage !== 0) damage = this.clampIntRange(damage, 1);
+		damage = target.damage(damage, source, effect);
+		if (source) source.lastDamage = damage;
+		var name = effect.fullname;
+		if (name === 'tox') name = 'psn';
+		switch (effect.id) {
+		case 'partiallytrapped':
+			this.add('-damage', target, target.getHealth, '[from] ' + this.effectData.sourceEffect.fullname, '[partiallytrapped]');
+			break;
+		default:
+			if (effect.effectType === 'Move') {
+				this.add('-damage', target, target.getHealth);
+			} else if (source && source !== target) {
+				this.add('-damage', target, target.getHealth, '[from] ' + effect.fullname, '[of] ' + source);
+			} else {
+				this.add('-damage', target, target.getHealth, '[from] ' + name);
+			}
+			break;
+		}
+
+		if (effect.drain && source) {
+			this.heal(Math.ceil(damage * effect.drain[0] / effect.drain[1]), source, target, 'drain');
+		}
+
+		if (target.fainted || target.hp <= 0) {
+			this.debug('instafaint: ' + this.faintQueue.map('target').map('name'));
+			this.faintMessages(true);
+			target.faint();
+			this.queue = [];
+		} else {
+			damage = this.runEvent('AfterDamage', target, source, effect, damage);
+		}
+
+		return damage;
 	}
 };
