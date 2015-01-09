defedeb1624b4980c924b30872e75e6a6c9e8c65
Juanma Serrano <joimpoke@gmail.com>
2015-01-09 09:48:13 +0100

    Implement Stadium format
    This commit implements at its full the Stadium format.
    Stadium is a half-generation between Gen 1 and Gen 2, widely regarded as a Gen 1 game without the major gen 1 bugs.
    
    The main changes of this game are:
    Sleep lasts between 1 and 3 turns.
    Hyper Beam does recharge after a faint.
    Critical hits happen way less.
    Substitute now blocks all status ailments and draining.
    It allows tradebacks.
    Partial trapping moves miss and stop their duration upon target switch.
    Focus Energy actually works.
    Stat calculations are done properly, burn and para drop are lost if you lose status.

---
+++ mods/stadium/rulesets.js
@@ -0,0 +1,22 @@
+exports.BattleFormats = {
+	standard: {
+		effectType: 'Banlist',
+		ruleset: ['Sleep Clause Mod', 'Freeze Clause Mod', 'Species Clause', 'OHKO Clause', 'Evasion Moves Clause', 'HP Percentage Mod'],
+		banlist: ['Unreleased', 'Illegal'],
+		validateSet: function (set) {
+			// limit one of each move in Standard
+			var moves = [];
+			if (set.moves) {
+				var hasMove = {};
+				for (var i = 0; i < set.moves.length; i++) {
+					var move = this.getMove(set.moves[i]);
+					var moveid = move.id;
+					if (hasMove[moveid]) continue;
+					hasMove[moveid] = true;
+					moves.push(set.moves[i]);
+				}
+			}
+			set.moves = moves;
+		}
+	}
+};
