746287ac008900440e79f4df796daa76d94cf641
The Immortal <the_immortal123@live.com>
2015-01-26 14:00:20 +0400

    Remove Battle of Hoenn

--- config/formats.js
+++ config/formats.js
@@ -111,24 +111,6 @@ exports.Formats = [
 		}
 	},
 	{
-		name: "Battle of Hoenn",
-		section: "ORAS Singles",
-
-		maxForcedLevel: 50,
-		ruleset: ['Pokemon', 'Standard GBU', 'Hoenn Pokedex', 'Team Preview GBU'],
-		requirePentagon: true,
-		validateTeam: function (team, format) {
-			if (team.length < 3) return ['You must bring at least three Pokémon.'];
-		},
-		onBegin: function () {
-			this.debug('cutting down to 3');
-			this.p1.pokemon = this.p1.pokemon.slice(0, 3);
-			this.p1.pokemonLeft = this.p1.pokemon.length;
-			this.p2.pokemon = this.p2.pokemon.slice(0, 3);
-			this.p2.pokemonLeft = this.p2.pokemon.length;
-		}
-	},
-	{
 		name: "Custom Game",
 		section: "ORAS Singles",
 
