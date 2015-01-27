9a53e3a616cafb5f339397e2d65bb5af99c681e0
awolffromspace <ewolft@gmail.com>
2015-01-26 21:23:22 -0500

    Update poof.js

--- chat-plugins/poof.js
+++ chat-plugins/poof.js
@@ -1,27 +1,28 @@
-const messages = [
-	"didn't get a cool message because none have been added!"
+const wolf = [
+	"howled to the moon.",
+	"ran off into a forest."
 ];
 
 exports.commands = {
-	d: 'poof',
+	dc: 'poof',
+	disconnect: 'poof',
+	disconnected: 'poof',
 	cpoof: 'poof',
 	poof: function (target, room, user) {
 		if (Config.poofOff) return this.sendReply("Poof is currently disabled.");
 		if (target && !this.can('broadcast')) return false;
-		if (room.id !== 'lobby') return false;
-		var message = target || messages[Math.floor(Math.random() * messages.length)];
-		if (message.indexOf('{{user}}') < 0)
-			message = '{{user}} ' + message;
-		message = message.replace(/{{user}}/g, user.name);
 		if (!this.canTalk(message)) return false;
+		if (user.name === 'wolf') {
+			var message = target || wolf[Math.floor(Math.random() * wolf.length)];
+			if (message.indexOf('{{user}}') < 0)
+				message = '{{user}} ' + message;
+			message = message.replace(/{{user}}/g, user.name);
 
-		var colour = '#' + [1, 1, 1].map(function () {
-			var part = Math.floor(Math.random() * 0xaa);
-			return (part < 0x10 ? '0' : '') + part.toString(16);
-		}).join('');
-
-		room.addRaw('<center><strong><font color="' + colour + '">~~ ' + Tools.escapeHTML(message) + ' ~~</font></strong></center>');
-		user.disconnectAll();
+			room.addRaw(Tools.escapeHTML(message));
+			user.disconnectAll();
+		} else {
+			user.disconnectAll();
+		}
 	},
 
 	poofoff: 'nopoof',
