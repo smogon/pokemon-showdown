99c02c35341303c3fc79dcc3c99aa4035d1a4f23
awolffromspace <ewolft@gmail.com>
2015-01-26 21:26:31 -0500

    Poof is now in a separate file

--- commands.js
+++ commands.js
@@ -27,11 +27,6 @@ Users.User.prototype.getIdentity = function (roomid) {
 	return name;
 };
 
-const wolf = [
-	"howled to the moon."
-	"ran off into a forest."
-];
-
 var commands = exports.commands = {
 
 	version: function (target, room, user) {
@@ -745,40 +740,6 @@ var commands = exports.commands = {
 		user.isAway = !user.isAway;
 		user.updateIdentity();
 		this.sendReply("You are " + (user.isAway ? "now" : "no longer") + " away.");
-	},
-
-	dc: 'poof',
-	disconnect: 'poof',
-	disconnected: 'poof',
-	cpoof: 'poof',
-	poof: function (target, room, user) {
-		if (Config.poofOff) return this.sendReply("Poof is currently disabled.");
-		if (target && !this.can('broadcast')) return false;
-		if (!this.canTalk(message)) return false;
-		if (user.name === 'wolf') {
-			var message = target || wolf[Math.floor(Math.random() * wolf.length)];
-			if (message.indexOf('{{user}}') < 0)
-				message = '{{user}} ' + message;
-			message = message.replace(/{{user}}/g, user.name);
-
-			room.addRaw(Tools.escapeHTML(message));
-			user.disconnectAll();
-		} else {
-			user.disconnectAll();
-		}
-	},
-
-	poofoff: 'nopoof',
-	nopoof: function () {
-		if (!this.can('poofoff')) return false;
-		Config.poofOff = true;
-		return this.sendReply("Poof is now disabled.");
-	},
-
-	poofon: function () {
-		if (!this.can('poofoff')) return false;
-		Config.poofOff = false;
-		return this.sendReply("Poof is now enabled.");
 	},
 
 	/*********************************************************
