4927087170f0257f799b4bffd51bd0b33d4b3aee
unknown <ewolft@gmail.com>
2015-01-26 21:12:17 -0500

    Create a separate file for poof

---
+++ poof.js
@@ -0,0 +1,39 @@
+const messages = [
+	"didn't get a cool message because none have been added!"
+];
+
+exports.commands = {
+	d: 'poof',
+	cpoof: 'poof',
+	poof: function (target, room, user) {
+		if (Config.poofOff) return this.sendReply("Poof is currently disabled.");
+		if (target && !this.can('broadcast')) return false;
+		if (room.id !== 'lobby') return false;
+		var message = target || messages[Math.floor(Math.random() * messages.length)];
+		if (message.indexOf('{{user}}') < 0)
+			message = '{{user}} ' + message;
+		message = message.replace(/{{user}}/g, user.name);
+		if (!this.canTalk(message)) return false;
+
+		var colour = '#' + [1, 1, 1].map(function () {
+			var part = Math.floor(Math.random() * 0xaa);
+			return (part < 0x10 ? '0' : '') + part.toString(16);
+		}).join('');
+
+		room.addRaw('<center><strong><font color="' + colour + '">~~ ' + Tools.escapeHTML(message) + ' ~~</font></strong></center>');
+		user.disconnectAll();
+	},
+
+	poofoff: 'nopoof',
+	nopoof: function () {
+		if (!this.can('poofoff')) return false;
+		Config.poofOff = true;
+		return this.sendReply("Poof is now disabled.");
+	},
+
+	poofon: function () {
+		if (!this.can('poofoff')) return false;
+		Config.poofOff = false;
+		return this.sendReply("Poof is now enabled.");
+	}
+};
