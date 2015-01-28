78d0d86f2064fdbd33cf56af25615869b116389c
Bär Halberkamp <fledaway@gmail.com>
2015-01-28 13:12:52 +0100

    Implemented a mtg search function as well, together with fixing some of the concerns in my previous PR

--- chat-plugins/tcg.js
+++ chat-plugins/tcg.js
@@ -1,7 +1,10 @@
 /**
 * TCG: Yugioh wiki plugin
 * This is a command that allows users to search the yugioh wiki for cards. It will display the closest match with a given query, or a separate message if there isn't anything found.
+* By bumbadadabum with help from ascriptmaster, codelegend and the PS development team.
 */
+
+var http = require("http");
 
 exports.commands = {
 	ygo: 'yugioh',
@@ -9,13 +12,12 @@ exports.commands = {
 		if (room.id !== 'tcg') return this.sendReply("This command can only be used in the TCG room.");
 		if (!this.canBroadcast()) return;
 
-		var http = require("http");
-		var self = this;
-
 		var query = Tools.escapeHTML(target);
+		var self = this;
 
 		var Search = http.get(('http://yugioh.wikia.com/api/v1/Search/List/?query=' + query + '&limit=1'), function (a) {
 			var response = '';
+
 			a.on('data', function (data) {
 				response += data;
 			});
@@ -25,7 +27,36 @@ exports.commands = {
 				if (result.exception) {
 					self.sendReply("No articles matching your query found.");
 				} else {
-					self.sendReplyBox("<strong>Best result for " + query + ":</strong><br/><a href= " + result.items[0].url + ">" + result.items[0].title + "</a>");
+					self.sendReplyBox("<strong>Best result for " + query + ":</strong><br/><a href= " + Tools.escapeHTML(result.items[0].url) + ">" + Tools.escapeHTML(result.items[0].title) + "</a>");
+				}
+
+				room.update();
+			});
+		});
+	},
+
+	mtg: 'magic',
+	magic: function (target, room, user) {
+		if (room.id !== 'tcg') return this.sendReply("This command can only be used in the TCG room.");
+		if (!this.canBroadcast()) return;
+
+		var query = Tools.escapeHTML(target);
+
+		var self = this;
+
+		var Search = http.get(('http://mtg.wikia.com/api/v1/Search/List/?query=' + query + '&limit=1'), function (a) {
+			var response = '';
+
+			a.on('data', function (data) {
+				response += data;
+			});
+			a.on('end', function () {
+				var result = JSON.parse(response);
+
+				if (result.exception) {
+					self.sendReply("No articles matching your query found.");
+				} else {
+					self.sendReplyBox("<strong>Best result for " + query + ":</strong><br/><a href= " + Tools.escapeHTML(result.items[0].url) + ">" + Tools.escapeHTML(result.items[0].title) + "</a>");
 				}
 
 				room.update();
