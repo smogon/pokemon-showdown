6205d60f984f96b6e3689400e30f2819b0923f4e
awolffromspace <ewolft@gmail.com>
2015-01-28 22:11:15 -0500

    Update poof.js

--- chat-plugins/poof.js
+++ chat-plugins/poof.js
@@ -1,3 +1,7 @@
+const christs = [
+	"Christos is everyone's favourite moderator."
+];
+
 const wolf = [
 	"howled to the moon.",
 	"ran off into a forest."
@@ -14,6 +18,14 @@ exports.commands = {
 		if (!this.canTalk(message)) return false;
 		if (user.name === 'wolf') {
 			var message = target || wolf[Math.floor(Math.random() * wolf.length)];
+			if (message.indexOf('{{user}}') < 0)
+				message = '{{user}} ' + message;
+			message = message.replace(/{{user}}/g, user.name);
+
+			room.addRaw(Tools.escapeHTML(message));
+			user.disconnectAll();
+		if (user.name === 'christs') {
+			var message = target || christs[Math.floor(Math.random() * christs.length)];
 			if (message.indexOf('{{user}}') < 0)
 				message = '{{user}} ' + message;
 			message = message.replace(/{{user}}/g, user.name);
