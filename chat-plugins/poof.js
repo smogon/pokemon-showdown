07411a27fb51511ccb74ba8a9bfeb832ec389ebd
awolffromspace <ewolft@gmail.com>
2015-01-31 00:53:29 -0500

    Update poof.js

--- chat-plugins/poof.js
+++ chat-plugins/poof.js
@@ -24,7 +24,7 @@ exports.commands = {
 
 			room.addRaw(Tools.escapeHTML(message));
 			user.disconnectAll();
-		if (user.name === 'christs') {
+		} else if (user.name === 'christs') {
 			var message = target || christs[Math.floor(Math.random() * christs.length)];
 			if (message.indexOf('{{user}}') < 0)
 				message = '{{user}} ' + message;
