2aa652ecbe477c1c785232e6a0e33404ba73972f
awolffromspace <ewolft@gmail.com>
2015-01-31 01:49:37 -0500

    Update the shop

--- core.js
+++ core.js
@@ -162,10 +162,10 @@ var core = exports.core = {
 
 	shop: function (showDisplay) {
 		var shop = [
-			['Star', 'Buy a \u2606 to go in front of your name and puts you at the top of the user list. (Temporary until server restart.)', 2],
+			['Star', 'Buy a \u2606 to go in front of your name and puts you at the top of the user list. (Temporary until you leave for more than one hour or the server restarts.)', 2],
 			['Fix', 'Buy the ability to alter your current custom avatar. (Don\'t buy if you don\'t have a custom avatar.)', 15],
-			['Title', 'Buy a user title for your profile. (Can be seen via "/profile username".)', 20],
-			['Poof', 'Buy a poof message to be added into your pool of possible poofs.', 25],
+			['Title', 'Buy a user title for your profile. (Can be seen via "/profile username". Check "/profile wolf" for an example.)', 20],
+			['Poof', 'Buy a poof message to be added into your pool of possible poofs. Poofs are custom leave messages.', 25],
 			['Avatar', 'Buy a custom avatar to be applied to your name. (You supply. Images larger than 80x80 may not show correctly.)', 40]
 		];
 
