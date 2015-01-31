b2cc14f5e221b709511ff49c47e102002c127e3a
Kevin Lau <ascriptmaster@gmail.com>
2015-01-31 14:30:23 -0800

    Random Battle: Reject Quick Attack with Feint

--- data/scripts.js
+++ data/scripts.js
@@ -2294,6 +2294,9 @@ exports.BattleScripts = {
 				case 'hiddenpowerice':
 					if (hasMove['icywind']) rejected = true;
 					break;
+				case 'quickattack':
+					if (hasMove['feint']) rejected = true;
+					break;
 
 				// Status:
 				case 'rest':
