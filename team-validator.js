c7c063b2389f566a84fa50b88096672ecdb36199
The Immortal <the_immortal123@live.com>
2015-02-10 03:22:45 +0400

    Fix Shaymin in STABmons
    It can get Flying moves from Shaymin-Sky.

--- team-validator.js
+++ team-validator.js
@@ -591,7 +591,10 @@ Validator = (function () {
 		do {
 			alreadyChecked[template.speciesid] = true;
 			// STABmons hack to avoid copying all of validateSet to formats
-			if (format.banlistTable && format.banlistTable['ignorestabmoves'] && template.types.indexOf(tools.getMove(move).type) > -1 && move !== 'chatter') return false;
+			if (format.banlistTable && format.banlistTable['ignorestabmoves'] && move !== 'chatter') {
+				if (template.species === 'Shaymin') template.types = tools.getTemplate('shayminsky').types;
+				if (template.types.indexOf(tools.getMove(move).type) > -1) return false;
+			}
 			if (template.learnset) {
 				if (template.learnset[move] || template.learnset['sketch']) {
 					sometimesPossible = true;
