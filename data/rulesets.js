5221c89b398543d5969c02ab49ce66468ad94ab7
The Immortal <the_immortal123@live.com>
2015-02-11 04:12:56 +0400

    Fix Sleep Clause Mod
    In doubles, if a Pokemon asleep was knocked out and in the same turn its
    ally was put to sleep, it would not allow it. Can be seen here:
    http://replay.pokemonshowdown.com/gen5smogondoubles-209911884 at turn
    10.

--- data/rulesets.js
+++ data/rulesets.js
@@ -545,9 +545,8 @@ exports.BattleFormats = {
 			if (status.id === 'slp') {
 				for (var i = 0; i < target.side.pokemon.length; i++) {
 					var pokemon = target.side.pokemon[i];
-					if (pokemon.status === 'slp') {
-						if (!pokemon.statusData.source ||
-							pokemon.statusData.source.side !== pokemon.side) {
+					if (pokemon.hp && pokemon.status === 'slp') {
+						if (!pokemon.statusData.source || pokemon.statusData.source.side !== pokemon.side) {
 							this.add('-message', 'Sleep Clause Mod activated.');
 							return false;
 						}
