f490a125811ccdec48f42cc100941d76c5c9c526
Juanma Serrano <joimpoke@gmail.com>
2015-02-16 10:48:36 +0100

    Gen 1 RandBats: Remove reminder of partial trapping moves

--- mods/gen1/formats-data.js
+++ mods/gen1/formats-data.js
@@ -508,7 +508,7 @@ exports.BattleFormatsData = {
 		tier: "OU"
 	},
 	tangela: {
-		randomBattleMoves: ["bind","hyperbeam","stunspore","megadrain","growth","swordsdance","bodyslam"],
+		randomBattleMoves: ["hyperbeam","stunspore","megadrain","growth","swordsdance","bodyslam"],
 		essentialMove: "sleeppowder",
 		tier: "UU"
 	},
@@ -648,7 +648,7 @@ exports.BattleFormatsData = {
 		tier: "OU"
 	},
 	moltres: {
-		randomBattleMoves: ["agility","fireblast","firespin","hyperbeam","reflect","skyattack","substitute","toxic"],
+		randomBattleMoves: ["agility","fireblast","hyperbeam","reflect","skyattack","substitute","toxic"],
 		tier: "UU"
 	},
 	dratini: {
