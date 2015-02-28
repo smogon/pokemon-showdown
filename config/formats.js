a3fd8991124a0a98a2d1adbcde0f8e7e7959ffc6
Ventillate <firefox.cgomes@gmail.com>
2015-02-28 11:36:04 -0500

    Added Sonarflare's 1% Tier

--- config/formats.js
+++ config/formats.js
@@ -669,6 +669,17 @@ exports.Formats = [
 			}
 		}
 	},
+    {
+                name: "1% Tier",              
+                section: "OM of the Month",
+ 
+               
+                rated: false,
+                maxLevel: 5,
+                ruleset: ['Pokemon', 'Standard', 'Team Preview', 'Little Cup'],
+                banlist: ['LC Uber', 'LC UU', 'Gligar', 'Misdreavus', 'Scyther', 'Sneasel', 'Tangela', 'Aipom', 'Amaura', 'Archen', 'Anorith', 'Aron', 'Blitzle', 'Bunnelby', 'Cacnea', 'Carvanha', 'Charmander', 'Chimchar', 'Chikorita', 'Clauncher', 'Corphish', 'Cranidos', 'Croagunk', 'Cubchoo', 'Cubone', 'Cyndaquil', 'Darumaka', 'Deerling', 'Doduo', 'Deino', 'Onix', 'Nosepass', 'Noibat', 'Minccino', 'Mienfoo', 'Pancham', 'Pumpkaboo', 'Riolu', 'Scraggy', 'Shellder', 'Shieldon', 'Snover', 'Snubbull', 'Swinub', 'Tyrunt', 'Woobat', 'Ponyta', 'Magnemite', 'Machop', 'Voltorb', 'Trapinch', 'Rhyhorn', 'Hippopotas', 'Growlithe', 'Grimer', 'Houndour', 'Honedge', 'Froakie', 'Dwebble', 'Drilbur', 'Sonic Boom', 'Swagger']
+        
+    }
 	{
 		name: "Averagemons",
 		section: "Other Metagames",
