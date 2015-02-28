a292c35bbd4473a0a355bbd3bc16b75546b3118c
awolffromspace <ewolft@gmail.com>
2015-02-27 20:58:58 -0500

    Fix Furry & Metronome Random

--- data/scripts.js
+++ data/scripts.js
@@ -4740,12 +4740,6 @@ exports.BattleScripts = {
 			}
 		}
 
-		// PotD stuff
-		var potd;
-		if (Config.potd && 'Rule:potd' in this.getBanlistTable(this.getFormat())) {
-			potd = this.getTemplate(Config.potd);
-		}
-
 		var typeCount = {};
 		var typeComboCount = {};
 		var baseFormes = {};
@@ -4815,20 +4809,6 @@ exports.BattleScripts = {
 			}
 			if (skip) continue;
 
-			if (potd && potd.exists) {
-				// The Pokemon of the Day belongs in slot 2
-				if (pokemon.length === 1) {
-					template = potd;
-					if (template.species === 'Magikarp') {
-						template.randomBattleMoves = ['magikarpsrevenge', 'splash', 'bounce'];
-					} else if (template.species === 'Delibird') {
-						template.randomBattleMoves = ['present', 'bestow'];
-					}
-				} else if (template.species === potd.species) {
-					continue; // No, thanks, I've already got one
-				}
-			}
-
 			var set = this.randomSet(template, pokemon.length, megaCount);
 
 			// Illusion shouldn't be on the last pokemon of the team
@@ -4846,6 +4826,14 @@ exports.BattleScripts = {
 			var forme = template.otherFormes && this.getTemplate(template.otherFormes[0]);
 			var isMegaSet = this.getItem(set.item).megaStone || (forme && forme.isMega && forme.requiredMove && set.moves.indexOf(toId(forme.requiredMove)) >= 0);
 			if (isMegaSet && megaCount > 0) continue;
+
+			if (template.id === 'purrloin') {
+				set.item = 'leftovers';
+				set.moves = ['foulplay', 'swagger', 'substitute', 'thunderwave'];
+			} else if (template.id === 'liepard') {
+				set.item = 'leftovers';
+				set.moves = ['foulplay', 'swagger', 'substitute', 'thunderwave'];
+			}
 
 			// Okay, the set passes, add it to our team
 			pokemon.push(set);
@@ -4880,9 +4868,6 @@ exports.BattleScripts = {
 		var pokemonLeft = 0;
 		var pokemon = [];
 
-		var pokemonList = [];
-		pokemonList = pokemonList.randomize();
-
 		var excludedTiers = {'LC':1, 'LC Uber':1, 'NFE':1};
 		var allowedNFE = {'Chansey':1, 'Doublade':1, 'Pikachu':1, 'Porygon2':1, 'Scyther':1};
 
@@ -4901,8 +4886,8 @@ exports.BattleScripts = {
 		var puCount = 0;
 		var megaCount = 0;
 
-		for (var i = 0; i < pokemonPool.length && pokemonLeft < 6; i++) {
-			var template = this.getTemplate(pokemonList[i]);
+		while (pokemonPool.length && pokemonLeft < 6) {
+			var template = this.getTemplate(this.sampleNoReplace(pokemonPool));
 			if (!template.exists) continue;
 
 			// Limit to one of each species (Species Clause)
@@ -4910,9 +4895,6 @@ exports.BattleScripts = {
 
 			// Not available on ORAS
 			if (template.species === 'Pichu-Spiky-eared') continue;
-
-			// Ban Shedinja from Metronome Random
-			if (template.species === 'Shedinja') continue;
 
 			// Only certain NFE Pokemon are allowed
 			if (template.evos.length && !allowedNFE[template.species]) continue;
