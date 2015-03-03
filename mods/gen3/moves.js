11b70bace4dbc5ce0db9b131e74264045dba30ac
Marty-D <math_man70@hotmail.com>
2015-03-03 12:59:07 -0500

    Gen III, IV: Fix Focus Punch
    - Loses PP even if the user was hit

--- mods/gen3/moves.js
+++ mods/gen3/moves.js
@@ -327,6 +327,20 @@ exports.BattleMovedex = {
 		inherit: true,
 		basePower: 70
 	},
+	focuspunch: {
+		inherit: true,
+		beforeMoveCallback: function () { },
+		onTry: function (pokemon) {
+			if (!pokemon.removeVolatile('focuspunch')) {
+				return;
+			}
+			if (pokemon.lastAttackedBy && pokemon.lastAttackedBy.damage && pokemon.lastAttackedBy.thisTurn) {
+				this.attrLastMove('[still]');
+				this.add('cant', pokemon, 'Focus Punch', 'Focus Punch');
+				return false;
+			}
+		}
+	},
 	foresight: {
 		inherit: true,
 		isBounceable: false
