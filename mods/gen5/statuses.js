afbf38178e298869c46d82a5e8bbab1c41b307d7
Guangcong Luo <guangcongluo@gmail.com>
2015-03-19 02:51:01 -0400

    Fix Normal Gem Explosion in gen 5
    Backport the gen 6 fix

--- mods/gen5/statuses.js
+++ mods/gen5/statuses.js
@@ -54,6 +54,7 @@ exports.BattleStatuses = {
 	},
 	gem: {
 		duration: 1,
+		affectsFainted: true,
 		onBasePower: function (basePower, user, target, move) {
 			this.debug('Gem Boost');
 			return this.chainModify(1.5);
