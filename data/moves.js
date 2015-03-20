3801c97ed2182ecc514d0733c166cb3b6ca1f146
The Immortal <the_immortal123@live.com>
2015-03-20 22:26:24 +0400

    Add description of flags
    It is unlikely to know what all the flags mean from just their name.

--- data/moves.js
+++ data/moves.js
@@ -1,3 +1,29 @@
+/*
+
+List of flags and their descriptions:
+
+authentic: Ignores a target's substitute.
+bite: Power is multiplied by 1.5 when used by a Pokemon with the Ability Strong Jaw.
+bullet: Has no effect on Pokemon with the Ability Bulletproof.
+charge: The user is unable to make a move between turns.
+contact: Makes contact.
+defrost: Thaws the user if executed successfully while the user is frozen.
+distance: Can target a Pokemon positioned anywhere in a Triple Battle.
+gravity: Prevented from being executed or selected during Gravity's effect.
+heal: Prevented from being executed or selected during Heal Block's effect.
+mirror: Can be copied by Mirror Move.
+nonsky: Prevented from being executed or selected in a Sky Battle.
+powder: Has no effect on Grass-type Pokemon, Pokemon with the Ability Overcoat, and Pokemon holding Safety Goggles.
+protect: Blocked by Detect, Protect, Spiky Shield, and if not a Status move, King's Shield.
+pulse: Power is multiplied by 1.5 when used by a Pokemon with the Ability Mega Launcher.
+punch: Power is multiplied by 1.2 when used by a Pokemon with the Ability Iron Fist.
+recharge: If this move is successful, the user must recharge on the following turn and cannot make a move.
+reflectable: Bounced back to the original user by Magic Coat or the Ability Magic Bounce.
+snatch: Can be stolen from the original user and instead used by another Pokemon using Snatch.
+sound: Has no effect on Pokemon with the Ability Soundproof.
+
+*/
+
 exports.BattleMovedex = {
 	"absorb": {
 		num: 71,
