smogon/pokemon-showdown

commit 046df58e1be19c221b12158d04d8eb552b19e4f7

Omit private rooms from Rooms.global.chatRooms
Rooms.global.chatRooms is a cached list of chat rooms, useful since
Rooms.rooms on Main has thousands of rooms on average, while less
than 100 of them are chat rooms.

A scan through the code reveals that private rooms are never used
(except for .isPrivate === 'voice') when we need .chatRooms, so
we can omit them to make the cache even smaller/faster.

This is a fairly minor optimization (we have fewer private rooms
than public rooms, total) and private rooms are omitted lazily
(only upon server restart) because we have to check .isPrivate
for the === 'voice' case anyway.

commands.js
@@ -247,6 +247,9 @@ var commands = exports.commands = {
 				delete room.chatRoomData.isPrivate;
 				Rooms.global.writeChatRoomData();
 			}
+			if (Rooms.global.chatRooms.indexOf(room) < 0) {
+				Rooms.global.chatRooms.push(room);
+			}
 		} else {
 			room.isPrivate = setting;
 			this.addModCommand("" + user.name + " made this room " + (setting === true ? 'secret' : setting) + ".");

rooms.js
@@ -197,6 +197,8 @@ var GlobalRoom = (function () {
 			}];
 		}
 
+		// cached list of chat rooms for the room list
+		// usually does not contain private rooms, but no guarantees
 		this.chatRooms = [];
 
 		this.autojoin = []; // rooms that users autojoin upon connecting
@@ -214,7 +216,7 @@ var GlobalRoom = (function () {
 					aliases[room.aliases[a]] = room;
 				}
 			}
-			this.chatRooms.push(room);
+			if (!room.isPrivate || room.isPrivate === 'voice') this.chatRooms.push(room);
 			if (room.autojoin) this.autojoin.push(id);
 			if (room.staffAutojoin) this.staffAutojoin.push(id);
 		}

commit 92194d3283b5514af96bc8709fa2c1961861abe9

Fix battle rooms appearing in roomList
Fixes a bug introduced in 046df58e1b

commands.js
@@ -243,11 +243,11 @@ var commands = exports.commands = {
 		if (target === 'off') {
 			delete room.isPrivate;
 			this.addModCommand("" + user.name + " made this room public.");
+			if (room.chatRoomData) {
+				delete room.chatRoomData.isPrivate;
+				Rooms.global.writeChatRoomData();
+			}
 			if (room.type === 'chat') {
-				if (room.chatRoomData) {
-					delete room.chatRoomData.isPrivate;
-					Rooms.global.writeChatRoomData();
-				}
 				if (Rooms.global.chatRooms.indexOf(room) < 0) {
 					Rooms.global.chatRooms.push(room);
 				}