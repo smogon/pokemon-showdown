9d09aff6a9b792ef3f9f9c7f4e2679b2eab559ac
The Immortal <the_immortal123@live.com>
2015-03-23 21:51:09 +0400

    Revert "Fix crash in Substitute with move flag 'sound'"
    This reverts commit d053bc086ca111ddf3e94d68220ce08835c894b4.

--- data/moves.js
+++ data/moves.js
@@ -13705,7 +13705,7 @@ exports.BattleMovedex = {
 					this.debug('sub bypass: self hit');
 					return;
 				}
-				if (move.notSubBlocked || (this.gen >= 6 && move.flags && move.flags['sound'])) {
+				if (move.notSubBlocked || (move.flags['sound'] && this.gen >= 6)) {
 					return;
 				}
 				if (move.category === 'Status') {
