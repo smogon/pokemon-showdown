b7075e19e6afd203fd0093f600e7f65697e99c8b
Nineage <kierenninjo@yahoo.com>
2015-04-01 18:22:05 -0800

    Allow room owners to use /roomdesc

--- commands.js
+++ commands.js
@@ -326,7 +326,7 @@ var commands = exports.commands = {
 			this.sendReplyBox("The room description is: " + room.desc.replace(re, '<a href="$1">$1</a>'));
 			return;
 		}
-		if (!this.can('declare')) return false;
+		if (!this.can('roomdriver', null, room)) return false;
 		if (target.length > 80) return this.sendReply("Error: Room description is too long (must be at most 80 characters).");
 		var normalizedTarget = ' ' + target.toLowerCase().replace('[^a-zA-Z0-9]+', ' ').trim() + ' ';
 
