8b59cca3614294610f75fca4b9081971c3ae52b5
Irraquated <tehpwnzer150@hotmail.com>
2015-04-02 17:46:52 +1000

    Update commands.js

--- config/commands.js
+++ config/commands.js
@@ -1246,13 +1246,13 @@ var commands = exports.commands = {
 	},
 
 	bmfrocket: function (target, room, user) {
-		if (room.id !== 'bmf') return this.sendReply("The command '/bofrocket' was unrecognized. To send a message starting with '/bofrocket', type '//bofrocket'.");
+		if (room.id !== 'bmf') return this.sendReply("The command '/bmfrocket' was unrecognized. To send a message starting with '/bofrocket', type '//bofrocket'.");
 		if (!this.can('modchat', null, room)) return;
 		target = this.splitTarget(target);
 		if (!this.targetUser) return this.sendReply("User not found");
 		if (!room.users[this.targetUser.userid]) return this.sendReply("Not in bmf");
 		this.targetUser.avatar = '#bofrocket';
-		room.add("" + user.name + " applied bofrocket to " + this.targetUser.name);
+		room.add("" + user.name + " applied bmfrocket to " + this.targetUser.name);
 	},
 
 	showtan: function (target, room, user) {
