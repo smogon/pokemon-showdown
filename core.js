a40c69aa3b2a4960639c29e3899df06d586853a4
awolffromspace <ewolft@gmail.com>
2015-04-02 20:31:19 -0400

    Increase BP requirements

--- core.js
+++ core.js
@@ -162,11 +162,11 @@ var core = exports.core = {
 
 	shop: function (showDisplay) {
 		var shop = [
-			['Star', 'Buy a \u2606 to go in front of your name and puts you at the top of the user list. (Goes away if you leave for more than one hour or the server restarts.)', 2],
-			['Fix', 'Buy the ability to alter your current custom avatar. (Don\'t buy this if you don\'t have a custom avatar. If you have a custom avatar and would like to apply it to other usernames, contact the admin "wolf" and don\'t buy this.)', 15],
-			['Title', 'Buy a user title for your profile. (Can be seen via "/profile username". Check "/profile wolf" for an example.)', 20],
-			['Poof', 'Buy a poof message to be added into your pool of possible poofs. Poofs are custom leave messages.', 25],
-			['Avatar', 'Buy a custom avatar to be applied to your name. (You supply. Images larger than 80x80 may not show correctly.)', 40]
+			['Star', 'Buy a \u2606 to go in front of your name and puts you at the top of the user list. (Goes away if you leave for more than one hour or the server restarts.)', 5],
+			['Fix', 'Buy the ability to alter your current custom avatar. (Don\'t buy this if you don\'t have a custom avatar. If you have a custom avatar and would like to apply it to other usernames, contact the admin "wolf" and don\'t buy this.)', 25],
+			['Title', 'Buy a user title for your profile. (Can be seen via "/profile username". Check "/profile wolf" for an example.)', 30],
+			['Poof', 'Buy a poof message to be added into your pool of possible poofs. Poofs are custom leave messages.', 35],
+			['Avatar', 'Buy a custom avatar to be applied to your name. (You supply. Images larger than 80x80 may not show correctly.)', 50]
 		];
 
 		if (showDisplay === false) {
