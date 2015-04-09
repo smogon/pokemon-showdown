47cdc2fd2e59f746a516f79f95784045c1c841b9
Marty-D <math_man70@hotmail.com>
2015-04-09 17:08:45 -0400

    Gen III: Fix Roar/Whirlwind against Protect

--- mods/gen3/moves.js
+++ mods/gen3/moves.js
@@ -527,7 +527,7 @@ exports.BattleMovedex = {
 	},
 	roar: {
 		inherit: true,
-		flags: {mirror: 1, sound: 1, authentic: 1}
+		flags: {protect: 1, mirror: 1, sound: 1, authentic: 1}
 	},
 	rockblast: {
 		inherit: true,
@@ -682,7 +682,7 @@ exports.BattleMovedex = {
 	},
 	whirlwind: {
 		inherit: true,
-		flags: {mirror: 1, authentic: 1}
+		flags: {protect: 1, mirror: 1, authentic: 1}
 	},
 	wish: {
 		inherit: true,
