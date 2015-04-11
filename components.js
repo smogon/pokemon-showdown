c44eb320e97f9222fc389474d8645dde20b8f78d
awolffromspace <ewolft@gmail.com>
2015-04-11 05:44:42 -0400

    Add Gen 2 Random to polls

--- components.js
+++ components.js
@@ -570,7 +570,7 @@ var components = exports.components = {
 	tourneypoll: 'tourpoll',
 	tourpoll: function (target, room, user) {
 		if (!this.can('poll', null, room)) return false;
-		this.parse('/poll Tournament format?, ou, ubers, uu, ru, nu, lc, vgc, monotype, [gen 4] ou, random, 1v1 random, uber random, high tier random, low tier random, lc random, monotype random, generational random, inverse random, hoenn random, hoenn weather random, super smash bros. random, winter wonderland, community random, furry random, metronome 3v3 random, metronome 6v6 random, doubles random, triples random, [seasonal] random, [gen 1] random');
+		this.parse('/poll Tournament format?, ou, ubers, uu, ru, nu, lc, vgc, monotype, [gen 4] ou, random, 1v1 random, uber random, high tier random, low tier random, lc random, monotype random, generational random, inverse random, hoenn random, hoenn weather random, super smash bros. random, winter wonderland, community random, furry random, metronome 3v3 random, metronome 6v6 random, doubles random, triples random, [seasonal] random, [gen 2] random, [gen 1] random');
 	},
 
 	teampoll: function (target, room, user) {
@@ -583,7 +583,7 @@ var components = exports.components = {
 	randpoll: 'randompoll',
 	randompoll: function (target, room, user) {
 		if (!this.can('poll', null, room)) return false;
-		this.parse('/poll Tournament format?, random, 1v1 random, uber random, high tier random, low tier random, lc random, monotype random, generational random, inverse random, hoenn random, hoenn weather random, super smash bros. random, winter wonderland, community random, furry random, doubles random, triples random, [seasonal] random, [gen 1] random');
+		this.parse('/poll Tournament format?, random, 1v1 random, uber random, high tier random, low tier random, lc random, monotype random, generational random, inverse random, hoenn random, hoenn weather random, super smash bros. random, winter wonderland, community random, furry random, doubles random, triples random, [seasonal] random, [gen 2] random, [gen 1] random');
 	},
 
 	endpoll: function (target, room, user) {
