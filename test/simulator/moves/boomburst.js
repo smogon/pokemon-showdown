eca4bdf7a798b0a129b77f79988a7470a0205ade
Ivo Julca <ivojulca@hotmail.com>
2015-04-13 01:13:56 -0500

    Add regression tests for damaging moves affected by 5318835

---
+++ test/simulator/moves/boomburst.js
@@ -0,0 +1,34 @@
+var assert = require('assert');
+var battle;
+
+describe('Boomburst', function () {
+	afterEach(function () {
+		battle.destroy();
+	});
+
+	it('should pierce through substitutes', function () {
+		battle = BattleEngine.Battle.construct();
+		battle.join('p1', 'Guest 1', 1, [{species: "Deoxys-Attack", ability: 'victorystar', item: 'laggingtail', moves: ['splash', 'boomburst']}]);
+		battle.join('p2', 'Guest 2', 1, [{species: "Caterpie", level: 2, ability: 'naturalcure', item: 'focussash', moves: ['substitute', 'rest']}]);
+		battle.commitDecisions();
+		battle.choose('p1', 'move 2');
+		battle.choose('p2', 'move 2');
+		assert.strictEqual(battle.p2.active[0].item, '');
+	});
+});
+
+describe('Boomburst - BW', function () {
+	afterEach(function () {
+		battle.destroy();
+	});
+
+	it('should not pierce through substitutes', function () {
+		battle = BattleEngine.Battle.construct('battle-boomburst-bw', 'gen5customgame');
+		battle.join('p1', 'Guest 1', 1, [{species: "Deoxys-Attack", ability: 'victorystar', item: 'laggingtail', moves: ['healpulse', 'boomburst']}]);
+		battle.join('p2', 'Guest 2', 1, [{species: "Caterpie", level: 2, ability: 'naturalcure', item: 'focussash', moves: ['substitute']}]);
+		battle.commitDecisions();
+		battle.choose('p1', 'move 2');
+		battle.choose('p2', 'move 2');
+		assert.strictEqual(battle.p2.active[0].item, 'focussash');
+	});
+});
