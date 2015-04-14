0b43420727b25e4b3ddaa119f8572f691a93847f
Joimer <joimpoke@gmail.com>
2015-04-15 00:28:24 +0200

    Fix Thunder Wave hitting Ground-types Since move.ignoreImmunity was defined as `false` for Thunder Wave, the statement !move.ignoreImmunity would always evaluate to true. This would lead to move.ignoreImmunity being true for all status moves. This commit fixes that problem, checking if the property was not defined as opposed to anything JavaScript deems `false`.
    Related: 2e56120ad993de42c5b42e9287ead3bd3fd10c03

--- tools.js
+++ tools.js
@@ -316,7 +316,7 @@ module.exports = (function () {
 				else move.gen = 0;
 			}
 			if (!move.priority) move.priority = 0;
-			if (!move.ignoreImmunity) move.ignoreImmunity = (move.category === 'Status');
+			if (move.ignoreImmunity === undefined) move.ignoreImmunity = (move.category === 'Status');
 			if (!move.flags) move.flags = {};
 		}
 		return move;
