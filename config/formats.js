bf21b347df43efc0312516b9a6922105b209c748
The Immortal <the_immortal123@live.com>
2015-04-18 18:58:48 +0400

    Remove NU suspect test

--- config/formats.js
+++ config/formats.js
@@ -70,15 +70,6 @@ exports.Formats = [
 		name: "NU",
 		section: "ORAS Singles",
 
-		searchShow: false,
-		ruleset: ['RU'],
-		banlist: ['RU', 'BL3', 'Glalitite', 'Steelixite']
-	},
-	{
-		name: "NU (suspect test)",
-		section: "ORAS Singles",
-
-		challengeShow: false,
 		ruleset: ['RU'],
 		banlist: ['RU', 'BL3', 'Glalitite', 'Steelixite']
 	},
