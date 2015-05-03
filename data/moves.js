6356f1ab33b8c3b7f0bdc510080244c83c25951a
Marty-D <math_man70@hotmail.com>
2015-05-03 13:26:20 -0400

    Fix Rock Head against substitutes

--- data/moves.js
+++ data/moves.js
@@ -13467,7 +13467,7 @@ exports.BattleMovedex = {
 				} else {
 					this.add('-activate', target, 'Substitute', '[damage]');
 				}
-				if (move.recoil) {
+				if (move.recoil && (!source.hasAbility('rockhead') || move.id === 'struggle')) {
 					this.damage(Math.round(damage * move.recoil[0] / move.recoil[1]), source, target, 'recoil');
 				}
 				if (move.drain) {
