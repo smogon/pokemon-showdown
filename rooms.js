e9c772693c5bd15ce0c2b48099cc5821dea11ef3
awolffromspace <ewolft@gmail.com>
2015-05-03 21:20:14 -0400

    Revert room intro height limit

--- rooms.js
+++ rooms.js
@@ -1487,21 +1487,15 @@ var ChatRoom = (function () {
 		this.send(update);
 	};
 	ChatRoom.prototype.getIntroMessage = function () {
-		if (this.modchat && this.introMessage) {
-			return '\n|raw|<div class="infobox"><div' + (!this.isOfficial ? ' class="infobox-limited"' : '') + '>' + this.introMessage + '</div>' +
-				'<br />' +
-				'<div class="broadcast-red">' +
-				'Must be rank ' + this.modchat + ' or higher to talk right now.' +
-				'</div></div>';
-		}
-
+		var html = this.introMessage || '';
 		if (this.modchat) {
-			return '\n|raw|<div class="infobox"><div class="broadcast-red">' +
-				'Must be rank ' + this.modchat + ' or higher to talk right now.' +
-				'</div></div>';
+			if (html) html += '<br /><br />';
+			html += '<div class="broadcast-red">';
+			html += 'Must be rank ' + this.modchat + ' or higher to talk right now.';
+			html += '</div>';
 		}
 
-		if (this.introMessage) return '\n|raw|<div class="infobox infobox-limited">' + this.introMessage + '</div>';
+		if (html) return '\n|raw|<div class="infobox">' + html + '</div>';
 
 		return '';
 	};
