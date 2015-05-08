4fad4b8bf37c7c723bb7c29885681fdf20750fa0
Marty-D <math_man70@hotmail.com>
2015-05-08 12:20:51 -0400

    Gen II: Fix critical hit ratios
    - High crit moves start at stage 2 (stage 0 being normal moves)
    - Focus Energy adds 1 stage
    - Lucky Punch and Stick set the stage at 2 and skip the other effects
    - Sky Attack is not a high crit move
    - Also fix Razor Wind's accuracy

--- mods/gen2/moves.js
+++ mods/gen2/moves.js
@@ -2,6 +2,10 @@
  * Gen 2 moves
  */
 exports.BattleMovedex = {
+	aeroblast: {
+		inherit: true,
+		critRatio: 3
+	},
 	bellydrum: {
 		inherit: true,
 		onHit: function (target) {
@@ -28,6 +32,14 @@ exports.BattleMovedex = {
 		beforeTurnCallback: function () {},
 		onTryHit: function () {},
 		effect: {}
+	},
+	crabhammer: {
+		inherit: true,
+		critRatio: 3
+	},
+	crosschop: {
+		inherit: true,
+		critRatio: 3
 	},
 	dig: {
 		inherit: true,
@@ -119,6 +131,18 @@ exports.BattleMovedex = {
 			}
 		}
 	},
+	focusenergy: {
+		inherit: true,
+		effect: {
+			onStart: function (pokemon) {
+				this.add('-start', pokemon, 'move: Focus Energy');
+			},
+			onModifyMovePriority: 1,
+			onModifyMove: function (move) {
+				move.critRatio += 1;
+			}
+		}
+	},
 	highjumpkick: {
 		inherit: true,
 		onMoveFail: function (target, source, move) {
@@ -136,6 +160,10 @@ exports.BattleMovedex = {
 				this.damage(this.clampIntRange(damage / 8, 1), source, source, 'jumpkick');
 			}
 		}
+	},
+	karatechop: {
+		inherit: true,
+		critRatio: 3
 	},
 	leechseed: {
 		inherit: true,
@@ -264,6 +292,15 @@ exports.BattleMovedex = {
 		// Disable does not build
 		inherit: true
 	},
+	razorleaf: {
+		inherit: true,
+		critRatio: 3
+	},
+	razorwind: {
+		inherit: true,
+		accuracy: 75,
+		critRatio: 3
+	},
 	reflect: {
 		inherit: true,
 		effect: {
@@ -316,7 +353,12 @@ exports.BattleMovedex = {
 	},
 	skyattack: {
 		inherit: true,
+		critRatio: 1,
 		secondary: {}
+	},
+	slash: {
+		inherit: true,
+		critRatio: 3
 	},
 	sleeptalk: {
 		inherit: true,
