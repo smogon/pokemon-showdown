c069f3143a85d1ea9f60c4ba7761f25f9bced15e
Kevin Lau <ascriptmaster@gmail.com>
2015-05-10 01:33:25 -0700

    Upgrade tests
    - Improve the wording on tests for status moves to be more clear
    - Add a test for Stealth Rocks

--- test/simulator/moves/stealthrock.js
+++ test/simulator/moves/stealthrock.js
@@ -13,4 +13,26 @@ describe('Stealth Rock', function () {
 		battle.commitDecisions();
 		assert(battle.p2.sideConditions['stealthrock']);
 	});
+
+	it('should deal damage to Pokemon switching in based on their type effectiveness against Rock-type', function () {
+		battle = BattleEngine.Battle.construct();
+		battle.join('p1', 'Guest 1', 1, [{species: "Smeargle", moves: ['splash', 'stealthrock']}]);
+		battle.join('p2', 'Guest 2', 1, [
+			{species: "Ninjask", moves: ['protect']},
+			{species: "Volcarona", moves: ['roost']},
+			{species: "Staraptor", moves: ['roost']},
+			{species: "Chansey", moves: ['wish']},
+			{species: "Hitmonchan", moves: ['rest']},
+			{species: "Steelix", moves: ['rest']}
+		]);
+		battle.choose('p1', 'move 2');
+		battle.commitDecisions();
+		var pokemon;
+		for (var i = 2; i <= 6; i++) {
+			battle.choose('p2', 'switch ' + i);
+			battle.commitDecisions();
+			pokemon = battle.p2.active[0];
+			assert.strictEqual(pokemon.maxhp - pokemon.hp, Math.floor(pokemon.maxhp * Math.pow(0.5, i - 1)));
+		}
+	});
 });
