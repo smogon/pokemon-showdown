21b1ee7107a3da61fada5550ba6c26e01c26b43b
Juanma Serrano <joimpoke@gmail.com>
2015-05-12 09:32:12 +0200

    Stadium: Update Standard Clauses

--- config/formats.js
+++ config/formats.js
@@ -932,7 +932,7 @@ exports.Formats = [
 
 		mod: 'stadium',
 		searchShow: false,
-		ruleset: ['Pokemon', 'Standard'],
+		ruleset: ['Pokemon', 'Standard', 'Team Preview'],
 		banlist: ['Uber',
 			'Nidoking + Fury Attack + Thrash', 'Exeggutor + Poison Powder + Stomp', 'Exeggutor + Sleep Powder + Stomp',
 			'Exeggutor + Stun Spore + Stomp', 'Jolteon + Focus Energy + Thunder Shock', 'Flareon + Focus Energy + Ember'
