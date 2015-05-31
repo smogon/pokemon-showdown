ce8fa81cc800f2dd9ce6fba419e4c76348ec6133
Kevin Lau <ascriptmaster@gmail.com>
2015-05-31 05:05:07 -0700

    Add Multiscale test

---
+++ test/simulator/abilities/multiscale.js
@@ -0,0 +1,38 @@
+var assert = require('assert');
+var battle;
+
+describe('Multiscale', function () {
+	afterEach(function () {
+		battle.destroy();
+	});
+
+	it('should halve damage when it is at full health', function () {
+		battle = BattleEngine.Battle.construct();
+		battle.join('p1', 'Guest 1', 1, [{species: "Dragonite", ability: 'multiscale', moves: ['splash']}]);
+		battle.join('p2', 'Guest 2', 1, [{species: "Gyarados", ability: 'moxie', moves: ['incinerate']}]);
+		var damage, curhp;
+		var pokemon = battle.p1.active[0];
+		battle.seed = [0, 0, 0, 1];
+		battle.commitDecisions();
+		damage = pokemon.maxhp - pokemon.hp;
+		curhp = pokemon.hp;
+		battle.seed = [0, 0, 0, 1];
+		battle.commitDecisions();
+		assert.strictEqual(damage, battle.modify(curhp - pokemon.hp, 0.5));
+	});
+
+	it('should be ignored by Mold Breaker', function () {
+		battle = BattleEngine.Battle.construct();
+		battle.join('p1', 'Guest 1', 1, [{species: "Dragonite", ability: 'multiscale', moves: ['splash']}]);
+		battle.join('p2', 'Guest 2', 1, [{species: "Gyarados", ability: 'moldbreaker', moves: ['incinerate']}]);
+		var damage, curhp;
+		var pokemon = battle.p1.active[0];
+		battle.seed = [0, 0, 0, 1];
+		battle.commitDecisions();
+		damage = pokemon.maxhp - pokemon.hp;
+		curhp = pokemon.hp;
+		battle.seed = [0, 0, 0, 1];
+		battle.commitDecisions();
+		assert.strictEqual(curhp - pokemon.hp, damage);
+	});
+});
