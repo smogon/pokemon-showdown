ba35604c5896515bdd28b9f9fd259553fc0cacbe
Kevin Lau <ascriptmaster@gmail.com>
2015-06-17 14:47:08 -0700

    Add Sticky Hold and Knock Off tests

--- test/simulator/moves/knockoff.js
+++ test/simulator/moves/knockoff.js
@@ -14,6 +14,22 @@ describe('Knock Off', function () {
 		assert.strictEqual(battle.p2.active[0].item, '');
 	});
 
+	it('should not remove plates from Arceus', function () {
+		battle = BattleEngine.Battle.construct();
+		battle.join('p1', 'Guest 1', 1, [{species: "Mew", ability: 'synchronize', moves: ['knockoff']}]);
+		battle.join('p2', 'Guest 2', 1, [{species: "Arceus", ability: 'download', item: 'flameplate', moves: ['swordsdance']}]);
+		battle.commitDecisions();
+		assert.strictEqual(battle.p2.active[0].item, 'flameplate');
+	});
+
+	it('should not remove drives from Genesect', function () {
+		battle = BattleEngine.Battle.construct();
+		battle.join('p1', 'Guest 1', 1, [{species: "Mew", ability: 'synchronize', moves: ['knockoff']}]);
+		battle.join('p2', 'Guest 2', 1, [{species: "Genesect", ability: 'download', item: 'dousedrive', moves: ['shiftgear']}]);
+		battle.commitDecisions();
+		assert.strictEqual(battle.p2.active[0].item, 'dousedrive');
+	});
+
 	it('should not remove correctly held mega stones', function () {
 		battle = BattleEngine.Battle.construct();
 		battle.join('p1', 'Guest 1', 1, [{species: "Mew", ability: 'synchronize', moves: ['knockoff']}]);
@@ -28,5 +44,13 @@ describe('Knock Off', function () {
 		battle.join('p2', 'Guest 2', 1, [{species: "Scizor", ability: 'technician', item: 'audinite', moves: ['swordsdance']}]);
 		battle.commitDecisions();
 		assert.strictEqual(battle.p2.active[0].item, '');
+	});
+
+	it('should not remove items if the user faints mid-move', function () {
+		battle = BattleEngine.Battle.construct();
+		battle.join('p1', 'Guest 1', 1, [{species: "Shedinja", ability: 'wonderguard', moves: ['knockoff']}]);
+		battle.join('p2', 'Guest 2', 1, [{species: "Ferrothorn", ability: 'ironbarbs', item: 'rockyhelmet', moves: ['curse']}]);
+		battle.commitDecisions();
+		assert.strictEqual(battle.p2.active[0].item, 'rockyhelmet');
 	});
 });
