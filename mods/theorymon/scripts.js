90baf74c6c3058fb8c33388fe7650f423bc9f9eb
The Immortal <the_immortal123@live.com>
2015-07-08 05:17:44 +0400

    Theorymon: Add new changes

--- mods/theorymon/scripts.js
+++ mods/theorymon/scripts.js
@@ -33,5 +33,7 @@ exports.BattleScripts = {
 		this.modData('Learnsets', 'braviary').learnset.extremespeed = ['6T'];
 		this.modData('Pokedex', 'cacturne').abilities['1'] = 'Sand Rush';
 		this.modData('Pokedex', 'beedrillmega').types = ['Bug', 'Ground'];
+		this.modData('Pokedex', 'swanna').abilities['1'] = 'Gale Wings';
+		this.modData('Pokedex', 'kyurem').abilities['1'] = 'Sheer Force';
 	}
 };
