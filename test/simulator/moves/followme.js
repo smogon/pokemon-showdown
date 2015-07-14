3ed6fa64398969a39848a75df090505049b268e5
Kevin Lau <ascriptmaster@gmail.com>
2015-07-14 04:00:25 -0700

    Upgrade Follow Me test to check for self-targetting moves

--- test/simulator/moves/followme.js
+++ test/simulator/moves/followme.js
@@ -18,14 +18,32 @@ describe('Follow Me', function () {
 			{species: 'Kadabra', ability: 'synchronize', moves: ['lowkick']},
 			{species: 'Alakazam', ability: 'synchronize', moves: ['lowkick']}
 		]);
-		battle.commitDecisions();
+		battle.commitDecisions(); // Team Preview
 		var hitCount = 0;
-		battle.p1.active[0].damage = function () {
-			hitCount++;
-			return BattleEngine.BattlePokemon.prototype.damage.apply(this, arguments);
-		};
+		battle.on('Damage', battle.getFormat(), function (damage, pokemon) {
+			if (pokemon.template.speciesid === 'clefable') {
+				hitCount++;
+			}
+		});
 		battle.choose('p2', 'move 1 2, move 1 2, move 1 2');
 		battle.commitDecisions();
 		assert.strictEqual(hitCount, 2);
+	});
+
+	it('should not redirect self-targetting moves', function () {
+		battle = BattleEngine.Battle.construct('battle-followme-self', 'doublescustomgame');
+		battle.join('p1', 'Guest 1', 1, [
+			{species: 'Clefable', ability: 'unaware', moves: ['followme']},
+			{species: 'Clefairy', ability: 'unaware', moves: ['softboiled']}
+		]);
+		battle.join('p2', 'Guest 2', 1, [
+			{species: 'Alakazam', ability: 'synchronize', moves: ['howl']},
+			{species: 'Kadabra', ability: 'synchronize', moves: ['howl']}
+		]);
+		battle.commitDecisions(); // Team Preview
+		battle.commitDecisions();
+		assert.strictEqual(battle.p1.active[0].boosts['atk'], 0);
+		assert.strictEqual(battle.p2.active[0].boosts['atk'], 1);
+		assert.strictEqual(battle.p2.active[1].boosts['atk'], 1);
 	});
 });
