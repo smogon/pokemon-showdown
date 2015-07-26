f29e35ea9dc9a533817577320484a877011bc6c0
The Immortal <the_immortal123@live.com>
2015-07-26 15:59:29 +0400

    Move Pidgeot to PU
    And it’s custom Random Battle level isn’t needed anymore because it’s
    unlikely (Mega) Pidgeot will go to OU.

--- data/scripts.js
+++ data/scripts.js
@@ -1836,7 +1836,7 @@ exports.BattleScripts = {
 			Aegislash: 74, Blaziken: 74, 'Blaziken-Mega': 74, Genesect: 74, 'Genesect-Burn': 74, 'Genesect-Chill': 74, 'Genesect-Douse': 74, 'Genesect-Shock': 74, Greninja: 74, 'Kangaskhan-Mega': 74, 'Lucario-Mega': 74, 'Mawile-Mega': 74,
 
 			// Not holding Mega Stone
-			Banette: 83, Beedrill: 83, Glalie: 83, Lopunny: 83, Pidgeot: 83,
+			Banette: 83, Beedrill: 83, Glalie: 83, Lopunny: 83,
 			Altaria: 81, Ampharos: 81, Charizard: 81,
 			Aerodactyl: 79, Aggron: 79, Blastoise: 79, Gardevoir: 79, Manectric: 79, Sceptile: 79, Venusaur: 79,
 			Diancie: 77, Metagross: 77, Sableye: 77,
