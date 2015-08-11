fa72faf696543823f1dc380357b23ff3f9684e16
Flareninja <flareninja98@gmail.com>
2015-08-11 19:23:41 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -9,7 +9,7 @@ exports.port = 8000;
 exports.proxyip = ['127.0.0.0/8'];
 
 exports.serverid = 'galaxy';
-exports.servertoken = 'vWxhVw/l2i+U';
+exports.servertoken = 'drwkNrgqcxyY';
 
 // Pokemon of the Day - put a pokemon's name here to make it Pokemon of the Day
 //   The PotD will always be in the #2 slot (not #1 so it won't be a lead)
