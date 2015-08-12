9a228aab2a5a075851ac44229bf588f95b607df5
Flareninja <flareninja98@gmail.com>
2015-08-12 11:14:18 +0200

    Create misc.js

---
+++ chat-plugins/misc.js
@@ -0,0 +1,259 @@
+/**
+ * Miscellaneous commands
+ */
+
+var fs = require('fs');
+var moment = require('moment');
+var request = require('request');
+
+var messages = [
+	"has vanished into nothingness!",
+	"used Explosion!",
+	"fell into the void.",
+	"went into a cave without a repel!",
+	"has left the building.",
+	"was forced to give StevoDuhHero's mom an oil massage!",
+	"was hit by Magikarp's Revenge!",
+	"ate a bomb!",
+	"is blasting off again!",
+	"(Quit: oh god how did this get here i am not good with computer)",
+	"was unfortunate and didn't get a cool message.",
+	"{{user}}'s mama accidently kicked {{user}} from the server!"
+];
+
+exports.commands = {
+	stafflist: 'authority',
+	auth: 'authority',
+	authlist: 'authority',
+	authority: function (target, room, user, connection) {
+		var rankLists = {};
+		var ranks = Object.keys(Config.groups);
+		for (var u in Users.usergroups) {
+			var rank = Users.usergroups[u].charAt(0);
+			// In case the usergroups.csv file is not proper, we check for the server ranks.
+			if (ranks.indexOf(rank) > -1) {
+				var name = Users.usergroups[u].substr(1);
+				if (!rankLists[rank]) rankLists[rank] = [];
+				if (name) rankLists[rank].push(((Users.getExact(name) && Users.getExact(name).connected) ? '**' + name + '**' : name));
+			}
+		}
+
+		var buffer = [];
+		Object.keys(rankLists).sort(function (a, b) {
+			return (Config.groups[b] || {rank: 0}).rank - (Config.groups[a] || {rank: 0}).rank;
+		}).forEach(function (r) {
+			buffer.push((Config.groups[r] ? r + Config.groups[r].name + "s (" + rankLists[r].length + ")" : r) + ":\n" + rankLists[r].sort().join(", "));
+		});
+
+		if (!buffer.length) buffer = "This server has no auth.";
+		connection.popup(buffer.join("\n\n"));
+	},
+
+	clearall: function (target, room, user) {
+		if (!this.can('declare')) return false;
+		if (room.battle) return this.sendReply("You cannot clearall in battle rooms.");
+
+		var len = room.log.length;
+		var users = [];
+		while (len--) {
+			room.log[len] = '';
+		}
+		for (var u in room.users) {
+			users.push(u);
+			Users.get(u).leaveRoom(room, Users.get(u).connections[0]);
+		}
+		len = users.length;
+		setTimeout(function () {
+			while (len--) {
+				Users.get(users[len]).joinRoom(room, Users.get(users[len]).connections[0]);
+			}
+		}, 1000);
+	},
+
+	hide: function (target, room, user) {
+		if (!this.can('lock')) return false;
+		user.hiding = true;
+		user.updateIdentity();
+		this.sendReply("You have hidden your staff symbol.");
+	},
+
+	rk: 'kick',
+	roomkick: 'kick',
+	kick: function (target, room, user) {
+		if (!target) return this.parse('/help kick');
+		if (user.locked && !user.can('bypassall')) {
+			return this.sendReply("You cannot do this while unable to talk.");
+		}
+
+		target = this.splitTarget(target);
+		var targetUser = this.targetUser;
+		if (!targetUser || !targetUser.connected) return this.sendReply("User \"" + this.targetUsername + "\" not found.");
+		if (!this.can('mute', targetUser, room)) return false;
+
+		this.addModCommand(targetUser.name + " was kicked from the room by " + user.name + ".");
+		targetUser.popup("You were kicked from " + room.id + " by " + user.name + ".");
+		targetUser.leaveRoom(room.id);
+	},
+	kickhelp: ["/kick - Kick a user out of a room. Requires: % @ # & ~"],
+
+	masspm: 'pmall',
+	pmall: function (target, room, user) {
+		if (!this.can('pmall')) return false;
+		if (!target) return this.parse('/help pmall');
+
+		var pmName = ' Server PM [Do not reply]';
+
+		for (var i in Users.users) {
+			var message = '|pm|' + pmName + '|' + Users.users[i].getIdentity() + '|' + target;
+			Users.users[i].send(message);
+		}
+	},
+	pmallhelp: ["/pmall [message] - PM all users in the server."],
+
+	staffpm: 'pmallstaff',
+	pmstaff: 'pmallstaff',
+	pmallstaff: function (target, room, user) {
+		if (!this.can('forcewin')) return false;
+		if (!target) return this.parse('/help pmallstaff');
+
+		var pmName = ' Staff PM [Do not reply]';
+
+		for (var i in Users.users) {
+			if (Users.users[i].isStaff) {
+				Users.users[i].send('|pm|' + pmName + '|' + Users.users[i].group + Users.users[i].name + '|' + target);
+			}
+		}
+	},
+	pmallstaffhelp: ["/pmallstaff [message] - Sends a PM to every staff member online."],
+
+	d: 'poof',
+	cpoof: 'poof',
+	poof: function (target, room, user) {
+		if (Config.poofOff) return this.sendReply("Poof is currently disabled.");
+		if (target && !this.can('broadcast')) return false;
+		if (room.id !== 'lobby') return false;
+		var message = target || messages[Math.floor(Math.random() * messages.length)];
+		if (message.indexOf('{{user}}') < 0) message = '{{user}} ' + message;
+		message = message.replace(/{{user}}/g, user.name);
+		if (!this.canTalk(message)) return false;
+
+		var colour = '#' + [1, 1, 1].map(function () {
+			var part = Math.floor(Math.random() * 0xaa);
+			return (part < 0x10 ? '0' : '') + part.toString(16);
+		}).join('');
+
+		room.addRaw("<strong><font color=\"" + colour + "\">~~ " + Tools.escapeHTML(message) + " ~~</font></strong>");
+		user.disconnectAll();
+	},
+	poofhelp: ["/poof - Disconnects the user and leaves a message in the room."],
+
+	poofon: function () {
+		if (!this.can('poofoff')) return false;
+		Config.poofOff = false;
+		return this.sendReply("Poof is now enabled.");
+	},
+	poofonhelp: ["/poofon - Enable the use /poof command."],
+
+	nopoof: 'poofoff',
+	poofoff: function () {
+		if (!this.can('poofoff')) return false;
+		Config.poofOff = true;
+		return this.sendReply("Poof is now disabled.");
+	},
+	poofoffhelp: ["/poofoff - Disable the use of the /poof command."],
+
+	regdate: function (target, room, user) {
+		if (!this.canBroadcast()) return;
+		if (!target || target === "0") target = toId(user.userid);
+		if (!target || target === "." || target === "," || target === "'") return this.parse('/help regdate');
+		var username = toId(target);
+		target = target.replace(/\s+/g, '');
+		var self = this, data;
+		request('http://pokemonshowdown.com/users/~' + target, function (error, response, content) {
+			if (!(!error && response.statusCode === 200)) return;
+			content = content + '';
+			content = content.split("<em");
+			if (content[1]) {
+				content = content[1].split("</p>");
+				if (content[0]) {
+					content = content[0].split("</em>");
+					if (content[1]) {
+						var regdate = content[1].split('</small>')[0] + '.';
+						data = Tools.escapeHTML(username) + " was registered on" + regdate;
+					}
+				}
+			} else {
+				data = Tools.escapeHTML(username) + " is not registered.";
+			}
+			self.sendReplyBox(Tools.escapeHTML(data));
+			room.update();
+		});
+	},
+	regdatehelp: ["/regdate - Please specify a valid username."],
+
+	show: function (target, room, user) {
+		if (!this.can('lock')) return false;
+		user.hiding = false;
+		user.updateIdentity();
+		this.sendReply("You have revealed your staff symbol.");
+	},
+
+	sb: 'showdownboilerplate',
+	showdownboilerplate: function (target, room, user) {
+		if (!this.canBroadcast()) return;
+		this.sendReply("|raw|This server uses <a href='https://github.com/CreaturePhil/Showdown-Boilerplate'>Showdown-Boilerplate</a>.");
+	},
+	showdownboilerplatehelp: ["/showdownboilerplate - Links to the Showdown-Boilerplate repository on Github."],
+
+	seen: function (target, room, user) {
+		if (!this.canBroadcast()) return;
+		if (!target) return this.parse('/help seen');
+		var targetUser = Users.get(target);
+		if (targetUser && targetUser.connected) return this.sendReplyBox(targetUser.name + " is <b>currently online</b>.");
+		target = Tools.escapeHTML(target);
+		var seen = Seen[toId(target)];
+		if (!seen) return this.sendReplyBox(target + " has never been online on this server.");
+		this.sendReplyBox(target + " was last seen <b>" + moment(seen).fromNow() + "</b>.");
+	},
+	seenhelp: ["/seen - Shows when the user last connected on the server."],
+
+	tell: function (target, room, user, connection) {
+		if (!target) return this.parse('/help tell');
+		target = this.splitTarget(target);
+		var targetUser = this.targetUser;
+		if (!target) {
+			this.sendReply("You forgot the comma.");
+			return this.parse('/help tell');
+		}
+
+		if (targetUser && targetUser.connected) {
+			return this.parse('/pm ' + this.targetUsername + ', ' + target);
+		}
+
+		if (user.locked) return this.popupReply("You may not send offline messages when locked.");
+		if (target.length > 255) return this.popupReply("Your message is too long to be sent as an offline message (>255 characters).");
+
+		if (Config.tellrank === 'autoconfirmed' && !user.autoconfirmed) {
+			return this.popupReply("You must be autoconfirmed to send an offline message.");
+		} else if (!Config.tellrank || Config.groupsranking.indexOf(user.group) < Config.groupsranking.indexOf(Config.tellrank)) {
+			return this.popupReply("You cannot send an offline message because offline messaging is " +
+				(!Config.tellrank ? "disabled" : "only available to users of rank " + Config.tellrank + " and above") + ".");
+		}
+
+		var userid = toId(this.targetUsername);
+		if (userid.length > 18) return this.popupReply("\"" + this.targetUsername + "\" is not a legal username.");
+
+		var sendSuccess = Tells.addTell(user, userid, target);
+		if (!sendSuccess) {
+			if (sendSuccess === false) {
+				return this.popupReply("User " + this.targetUsername + " has too many offline messages queued.");
+			} else {
+				return this.popupReply("You have too many outgoing offline messages queued. Please wait until some have been received or have expired.");
+			}
+		}
+		return connection.send('|pm|' + user.getIdentity() + '|' +
+			(targetUser ? targetUser.getIdentity() : ' ' + this.targetUsername) +
+			"|/text This user is currently offline. Your message will be delivered when they are next online.");
+	},
+	tellhelp: ["/tell [username], [message] - Send a message to an offline user that will be received when they log in."]
+};
