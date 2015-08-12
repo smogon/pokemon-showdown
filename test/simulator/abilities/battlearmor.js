e405866e3d2d75306cae44df1ef2a6d57b5051cc
Kevin Lau <ascriptmaster@gmail.com>
2015-08-12 16:02:12 -0700

    Fix Battle Armor and Shell Armor tests to no longer use the battle log
    Instead, they can use Battle#on to check for the move dealing a critical
    hit within the ModifyDamage event instead.

--- test/simulator/abilities/battlearmor.js
+++ test/simulator/abilities/battlearmor.js
@@ -7,18 +7,34 @@ describe('Battle Armor', function () {
 	});
 
 	it('should prevent moves from dealing critical hits', function () {
-		battle = BattleEngine.Battle.construct();
+		battle = BattleEngine.Battle.construct('battle-battlearmor', 'customgame');
 		battle.join('p1', 'Guest 1', 1, [{species: 'Slowbro', ability: 'battlearmor', moves: ['quickattack']}]);
 		battle.join('p2', 'Guest 2', 1, [{species: 'Cyrogonal', ability: 'noguard', moves: ['frostbreath']}]);
+		battle.commitDecisions(); // Team Preview
+		var successfulEvent = false;
+		battle.on('ModifyDamage', battle.getFormat(), function (damage, attacker, defender, move) {
+			if (move.id === 'frostbreath') {
+				successfulEvent = true;
+				assert.ok(!move.crit);
+			}
+		});
 		battle.commitDecisions();
-		assert.ok(!battle.log[battle.lastMoveLine + 1].startsWith('|-crit'));
+		assert.ok(successfulEvent);
 	});
 
 	it('should be suppressed by Mold Breaker', function () {
-		battle = BattleEngine.Battle.construct();
+		battle = BattleEngine.Battle.construct('battle-battlearmor-moldbreaker', 'customgame');
 		battle.join('p1', 'Guest 1', 1, [{species: 'Slowbro', ability: 'battlearmor', moves: ['quickattack']}]);
 		battle.join('p2', 'Guest 2', 1, [{species: 'Cyrogonal', ability: 'moldbreaker', item: 'zoomlens', moves: ['frostbreath']}]);
+		battle.commitDecisions(); // Team Preview
+		var successfulEvent = false;
+		battle.on('ModifyDamage', battle.getFormat(), function (damage, attacker, defender, move) {
+			if (move.id === 'frostbreath') {
+				successfulEvent = true;
+				assert.ok(move.crit);
+			}
+		});
 		battle.commitDecisions();
-		assert.ok(battle.log[battle.lastMoveLine + 1].startsWith('|-crit'));
+		assert.ok(successfulEvent);
 	});
 });
--- test/simulator/abilities/shellarmor.js
+++ test/simulator/abilities/shellarmor.js
@@ -7,18 +7,34 @@ describe('Shell Armor', function () {
 	});
 
 	it('should prevent moves from dealing critical hits', function () {
-		battle = BattleEngine.Battle.construct();
+		battle = BattleEngine.Battle.construct('battle-shellarmor', 'customgame');
 		battle.join('p1', 'Guest 1', 1, [{species: 'Slowbro', ability: 'shellarmor', moves: ['quickattack']}]);
 		battle.join('p2', 'Guest 2', 1, [{species: 'Cyrogonal', ability: 'noguard', moves: ['frostbreath']}]);
+		battle.commitDecisions(); // Team Preview
+		var successfulEvent = false;
+		battle.on('ModifyDamage', battle.getFormat(), function (damage, attacker, defender, move) {
+			if (move.id === 'frostbreath') {
+				successfulEvent = true;
+				assert.ok(!move.crit);
+			}
+		});
 		battle.commitDecisions();
-		assert.ok(!battle.log[battle.lastMoveLine + 1].startsWith('|-crit'));
+		assert.ok(successfulEvent);
 	});
 
 	it('should be suppressed by Mold Breaker', function () {
-		battle = BattleEngine.Battle.construct();
+		battle = BattleEngine.Battle.construct('battle-shellarmor-moldbreaker', 'customgame');
 		battle.join('p1', 'Guest 1', 1, [{species: 'Slowbro', ability: 'shellarmor', moves: ['quickattack']}]);
 		battle.join('p2', 'Guest 2', 1, [{species: 'Cyrogonal', ability: 'moldbreaker', item: 'zoomlens', moves: ['frostbreath']}]);
+		battle.commitDecisions(); // Team Preview
+		var successfulEvent = false;
+		battle.on('ModifyDamage', battle.getFormat(), function (damage, attacker, defender, move) {
+			if (move.id === 'frostbreath') {
+				successfulEvent = true;
+				assert.ok(move.crit);
+			}
+		});
 		battle.commitDecisions();
-		assert.ok(battle.log[battle.lastMoveLine + 1].startsWith('|-crit'));
+		assert.ok(successfulEvent);
 	});
 });
