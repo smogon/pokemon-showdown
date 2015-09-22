b7797ea7257ddaeef53a1b170039ba20e48ef596
Guangcong Luo <guangcongluo@gmail.com>
2015-09-22 04:52:25 -0500

    Add basic local ladder support
    This initial local ladder support is really basic. Just Elo/W/L/T in
    .tsv files. No support for /rank yet.
    
    Servers are encouraged to write and share their own ladder
    implementations. The protocol is designed to allow basically any kind
    of ladder you want.

--- app.js
+++ app.js
@@ -313,7 +313,7 @@ global.Tools = require('./tools.js').includeFormats();
 
 global.LoginServer = require('./loginserver.js');
 
-global.Ladders = require('./ladders-remote.js');
+global.Ladders = require(Config.remoteladder ? './ladders-remote.js' : './ladders.js');
 
 global.Users = require('./users.js');
 
---
+++ config/ladders/README.md
@@ -0,0 +1,3 @@
+Ladders are stored here in files named `FORMATID.tsv`.
+
+TSV files are spreadsheets and are best opened with spreadsheet programs such as Excel. Text editors can also handle them, although not as easily.
