c027fe6e4520b9deb6615198ddd703edc5cdacea
Guangcong Luo <guangcongluo@gmail.com>
2015-09-22 03:34:56 -0400

    Disallow /roomauth staff
    Specifically, treat staff room as a modjoin room for the purposes
    of /roomauth, which means it can only be used if you're in the
    room. Bypassing this is now done with 'makeroom' instead of
    'bypassall', as is the new policy with room permissions.

--- commands.js
+++ commands.js
@@ -620,7 +620,8 @@ var commands = exports.commands = {
 	roomauth: function (target, room, user, connection) {
 		var targetRoom = room;
 		if (target) targetRoom = Rooms.search(target);
-		if (!targetRoom || (targetRoom !== room && targetRoom.modjoin && !user.can('bypassall'))) return this.sendReply("The room '" + target + "' does not exist.");
+		var unavailableRoom = (targetRoom !== room && (targetRoom.modjoin || targetRoom.staffRoom) && !user.can('makeroom'));
+		if (!targetRoom || unavailableRoom) return this.sendReply("The room '" + target + "' does not exist.");
 		if (!targetRoom.auth) return this.sendReply("/roomauth - The room '" + (targetRoom.title ? targetRoom.title : target) + "' isn't designed for per-room moderation and therefore has no auth list.");
 
 		var rankLists = {};
