325465c9e4f1bec5d0e7c4374446567bf8fb57bc
Bryan AA <abrayan87@yahoo.com>
2015-09-27 12:16:31 -0600

    Replace 'this.sendReply' with 'this.errorReply' in some cases

--- chat-plugins/thestudio.js
+++ chat-plugins/thestudio.js
@@ -29,9 +29,9 @@ if (theStudio && !theStudio.plugin) {
 
 var commands = {
 	start: function (target, room, user) {
-		if (room.id !== 'thestudio') return this.sendReply('This command can only be used in The Studio.');
+		if (room.id !== 'thestudio') return this.errorReply('This command can only be used in The Studio.');
 		if (!room.chatRoomData || !this.can('mute', null, room)) return false;
-		if ((user.locked || room.isMuted(user)) && !user.can('bypassall')) return this.sendReply("You cannot do this while unable to talk.");
+		if ((user.locked || room.isMuted(user)) && !user.can('bypassall')) return this.errorReply("You cannot do this while unable to talk.");
 		if (artistOfTheDay.pendingNominations) return this.sendReply("Nominations for the Artist of the Day are already in progress.");
 
 		var nominations = artistOfTheDay.nominations;
@@ -55,9 +55,9 @@ var commands = {
 	starthelp: ["/aotd start - Start nominations for the Artist of the Day. Requires: % @ # & ~"],
 
 	end: function (target, room, user) {
-		if (room.id !== 'thestudio') return this.sendReply('This command can only be used in The Studio.');
+		if (room.id !== 'thestudio') return this.errorReply('This command can only be used in The Studio.');
 		if (!room.chatRoomData || !this.can('mute', null, room)) return false;
-		if ((user.locked || room.isMuted(user)) && !user.can('bypassall')) return this.sendReply("You cannot do this while unable to talk.");
+		if ((user.locked || room.isMuted(user)) && !user.can('bypassall')) return this.errorReply("You cannot do this while unable to talk.");
 		if (!artistOfTheDay.pendingNominations) return this.sendReply("Nominations for the Artist of the Day are not in progress.");
 		if (!artistOfTheDay.nominations.size) return this.sendReply("No nominations have been submitted yet.");
 
@@ -77,10 +77,10 @@ var commands = {
 	endhelp: ["/aotd end - End nominations for the Artist of the Day and set it to a randomly selected artist. Requires: % @ # & ~"],
 
 	prenom: function (target, room, user) {
-		if (room.id !== 'thestudio') return this.sendReply('This command can only be used in The Studio.');
+		if (room.id !== 'thestudio') return this.errorReply('This command can only be used in The Studio.');
 		if (!target) this.parse('/help aotd prenom');
 		if (!room.chatRoomData || !target) return false;
-		if ((user.locked || room.isMuted(user)) && !user.can('bypassall')) return this.sendReply("You cannot do this while unable to talk.");
+		if ((user.locked || room.isMuted(user)) && !user.can('bypassall')) return this.errorReply("You cannot do this while unable to talk.");
 		if (artistOfTheDay.pendingNominations) return this.sendReply("Nominations for the Artist of the Day are in progress.");
 		if (!room.chatRoomData.prenominations) room.chatRoomData.prenominations = [];
 
@@ -118,10 +118,10 @@ var commands = {
 	prenomhelp: ["/aotd prenom [artist] - Nominate an artist for the Artist of the Day between nomination periods."],
 
 	nom: function (target, room, user) {
-		if (room.id !== 'thestudio') return this.sendReply('This command can only be used in The Studio.');
+		if (room.id !== 'thestudio') return this.errorReply('This command can only be used in The Studio.');
 		if (!target) this.parse('/help aotd nom');
 		if (!room.chatRoomData || !target) return false;
-		if ((user.locked || room.isMuted(user)) && !user.can('bypassall')) return this.sendReply("You cannot do this while unable to talk.");
+		if ((user.locked || room.isMuted(user)) && !user.can('bypassall')) return this.errorReply("You cannot do this while unable to talk.");
 		if (!artistOfTheDay.pendingNominations) return this.sendReply("Nominations for the Artist of the Day are not in progress.");
 
 		var removedNominators = artistOfTheDay.removedNominators;
@@ -150,7 +150,7 @@ var commands = {
 	nomhelp: ["/aotd nom [artist] - Nominate an artist for the Artist of the Day."],
 
 	viewnoms: function (target, room, user) {
-		if (room.id !== 'thestudio') return this.sendReply('This command can only be used in The Studio.');
+		if (room.id !== 'thestudio') return this.errorReply('This command can only be used in The Studio.');
 		if (!room.chatRoomData) return false;
 
 		var buffer = "";
@@ -194,17 +194,17 @@ var commands = {
 	viewnomshelp: ["/aotd viewnoms - View the current nominations for the Artist of the Day. Requires: % @ # & ~"],
 
 	removenom: function (target, room, user) {
-		if (room.id !== 'thestudio') return this.sendReply('This command can only be used in The Studio.');
+		if (room.id !== 'thestudio') return this.errorReply('This command can only be used in The Studio.');
 		if (!target) this.parse('/help aotd removenom');
 		if (!room.chatRoomData || !target || !this.can('mute', null, room)) return false;
-		if ((user.locked || room.isMuted(user)) && !user.can('bypassall')) return this.sendReply("You cannot do this while unable to talk.");
+		if ((user.locked || room.isMuted(user)) && !user.can('bypassall')) return this.errorReply("You cannot do this while unable to talk.");
 		if (!artistOfTheDay.pendingNominations) return this.sendReply("Nominations for the Artist of the Day are not in progress.");
 		if (!artistOfTheDay.nominations.size) return this.sendReply("No nominations have been submitted yet.");
 
 		target = this.splitTarget(target);
 		var name = this.targetUsername;
 		var userid = toId(name);
-		if (!userid) return this.sendReply("'" + name + "' is not a valid username.");
+		if (!userid) return this.errorReply("'" + name + "' is not a valid username.");
 
 		for (var nominator, nominatorsIterator = artistOfTheDay.nominations.keys(); !!(nominator = nominatorsIterator.next().value);) { // replace with for-of loop once available
 			if (nominator.userid === userid) {
@@ -219,11 +219,11 @@ var commands = {
 	removenomhelp: ["/aotd removenom [username] - Remove a user\'s nomination for the Artist of the Day and prevent them from voting again until the next round. Requires: % @ # & ~"],
 
 	set: function (target, room, user) {
-		if (room.id !== 'thestudio') return this.sendReply('This command can only be used in The Studio.');
+		if (room.id !== 'thestudio') return this.errorReply('This command can only be used in The Studio.');
 		if (!target) this.parse('/help aotd set');
 		if (!room.chatRoomData || !this.can('mute', null, room)) return false;
-		if ((user.locked || room.isMuted(user)) && !user.can('bypassall')) return this.sendReply("You cannot do this while unable to talk.");
-		if (!toId(target)) return this.sendReply("No valid artist was specified.");
+		if ((user.locked || room.isMuted(user)) && !user.can('bypassall')) return this.errorReply("You cannot do this while unable to talk.");
+		if (!toId(target)) return this.errorReply("No valid artist was specified.");
 		if (artistOfTheDay.pendingNominations) return this.sendReply("The Artist of the Day cannot be set while nominations are in progress.");
 
 		room.chatRoomData.artistOfTheDay = target;
@@ -233,7 +233,7 @@ var commands = {
 	sethelp: ["/aotd set [artist] - Set the Artist of the Day. Requires: % @ # & ~"],
 
 	quote: function (target, room, user) {
-		if (room.id !== 'thestudio') return this.sendReply('This command can only be used in The Studio.');
+		if (room.id !== 'thestudio') return this.errorReply('This command can only be used in The Studio.');
 		if (!room.chatRoomData) return false;
 		if (!target) {
 			if (!this.canBroadcast()) return;
@@ -266,13 +266,13 @@ var commands = {
 	],
 
 	'': function (target, room) {
-		if (room.id !== 'thestudio') return this.sendReply('This command can only be used in The Studio.');
+		if (room.id !== 'thestudio') return this.errorReply('This command can only be used in The Studio.');
 		if (!room.chatRoomData || !this.canBroadcast()) return false;
 		this.sendReplyBox("The Artist of the Day " + (room.chatRoomData.artistOfTheDay ? "is " + room.chatRoomData.artistOfTheDay + "." : "has not been set yet."));
 	},
 
 	help: function (target, room) {
-		if (room.id !== 'thestudio') return this.sendReply('This command can only be used in The Studio.');
+		if (room.id !== 'thestudio') return this.errorReply('This command can only be used in The Studio.');
 		if (!room.chatRoomData || !this.canBroadcast()) return false;
 		this.sendReply("Use /help aotd to view help for all commands, or /help aotd [command] for help on a specific command.");
 	}
--- logs/repl/README.md
+++ logs/repl/README.md
@@ -6,4 +6,4 @@ This directory is used by default to store the REPL sockets to all of showdown's
 
 The intended uses of these REPL sockets are for debugging (especially when the server is seemingly frozen) and scripting.
 
-You can use any tool capable of talking to Unix sockets such as `nc`. e.g., `nc -U app`
+You can use any tool capable of talking to Unix sockets such as `nc`. e.g. `nc -U app`
