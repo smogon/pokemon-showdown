7de1330286a35a004a83a8bb31e91d93b0e7bad8
Guangcong Luo <guangcongluo@gmail.com>
2015-10-10 02:22:20 -0400

    Ban buttons in private rooms

--- command-parser.js
+++ command-parser.js
@@ -332,7 +332,7 @@ var Context = exports.Context = (function () {
 				}
 			}
 		}
-		if (this.room.isPersonal && !this.user.can('lock') && html.match(/<button /)) {
+		if ((this.room.isPersonal || this.room.isPrivate === true) && !this.user.can('lock') && html.match(/<button /)) {
 			this.errorReply('You do not have permission to use buttons in HTML.');
 			return false;
 		}
