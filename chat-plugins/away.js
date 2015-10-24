808bb8dd19d19fbf67d3c3d870602078a684ffbb
Wando94 <daniel.wilson11@outlook.com>
2015-10-24 08:42:11 +0100

    Create away.js

---
+++ chat-plugins/away.js
@@ -0,0 +1,16 @@
+if (!Users.User.prototype._getIdentity_away) Users.User.prototype._getIdentity_away = Users.User.prototype.getIdentity;
+Users.User.prototype.getIdentity = function (roomid) {
+	var name = this._getIdentity_away(roomid);
+	if (this.isAway) name += " - \u0410\u051d\u0430\u0443";
+	return name;
+};
+
+exports.commands = {
+	back: 'away',
+	idle: 'away',
+	away: function (target, room, user) {
+		user.isAway = !user.isAway;
+		user.updateIdentity();
+		this.sendReply("You are " + (user.isAway ? "now" : "no longer") + " away.");
+	}
+};
