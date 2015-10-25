1efb510d5ce20cdad115056d82eea757f7284c29
Wando94 <daniel.wilson11@outlook.com>
2015-10-25 20:48:23 +0000

    Update away.js

--- chat-plugins/away.js
+++ chat-plugins/away.js
@@ -31,3 +31,37 @@ exports.commands = {
 		this.sendReply("You are " + (user.isCoding ? "now" : "no longer") + " Coding.");
 	}
 };
+
+if (!Users.User.prototype._getIdentity_Dead) Users.User.prototype._getIdentity_Dead = Users.User.prototype.getIdentity;
+Users.User.prototype.getIdentity = function (roomid) {
+	var name = this._getIdentity_Dead(roomid);
+	if (this.isDead) name += " - \u24B9\u24D4\u24D0\u24B9";
+	return name;
+};
+
+exports.commands = {
+	back: 'Dead',
+	idle: 'Dead',
+	away: function (target, room, user) {
+		user.isDead = !user.isDead;
+		user.updateIdentity();
+		this.sendReply("You are " + (user.isDead ? "now" : "no longer") + " Dead.");
+	}
+};
+
+if (!Users.User.prototype._getIdentity_Drunk) Users.User.prototype._getIdentity_Drunk = Users.User.prototype.getIdentity;
+Users.User.prototype.getIdentity = function (roomid) {
+	var name = this._getIdentity_Drunk(roomid);
+	if (this.isDrunk) name += " - \u24B8\u24E1\u24E4\u24DD\u24C0";
+	return name;
+};
+
+exports.commands = {
+	back: 'Drunk',
+	idle: 'Drunk',
+	away: function (target, room, user) {
+		user.isDrunk = !user.isDrunk;
+		user.updateIdentity();
+		this.sendReply("You are " + (user.isDrunk ? "now" : "no longer") + " Drunk.");
+	}
+};
