163af1a5c9c2abfed6afec170a19b0bd915b9351
Wando94 <daniel.wilson11@outlook.com>
2015-10-25 20:52:03 +0000

    Update away.js

--- chat-plugins/away.js
+++ chat-plugins/away.js
@@ -52,7 +52,7 @@ exports.commands = {
 if (!Users.User.prototype._getIdentity_Drunk) Users.User.prototype._getIdentity_Drunk = Users.User.prototype.getIdentity;
 Users.User.prototype.getIdentity = function (roomid) {
 	var name = this._getIdentity_Drunk(roomid);
-	if (this.isDrunk) name += " - \u24B8\u24E1\u24E4\u24DD\u24C0";
+	if (this.isDrunk) name += " - \u24B9\u24E1\u24E4\u24DD\u24C0";
 	return name;
 };
 
