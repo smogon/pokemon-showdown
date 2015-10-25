fd41ebf6550b4e38c4f164add115c6f9120a7664
Wando94 <daniel.wilson11@outlook.com>
2015-10-25 21:11:34 +0000

    Update away.js

--- chat-plugins/away.js
+++ chat-plugins/away.js
@@ -14,3 +14,20 @@ exports.commands = {
 		this.sendReply("You are " + (user.isAway ? "now" : "no longer") + " away.");
 	}
 };
+
+if (!Users.User.prototype._getIdentity_Coding) Users.User.prototype._getIdentity_Coding = Users.User.prototype.getIdentity;
+Users.User.prototype.getIdentity = function (roomid) {
+	var name = this._getIdentity_Coding(roomid);
+	if (this.isCoding) name += " - \u24B8\u24DE\u24D3\u24D8\u24DD\u24D6";
+	return name;
+};
+
+exports.commands = {
+	back: 'coding',
+	idle: 'coding',
+	away: function (target, room, user) {
+		user.isCoding = !user.isCoding;
+		user.updateIdentity();
+		this.sendReply("You are " + (user.isCoding ? "now" : "no longer") + " Coding.");
+	}
+};
