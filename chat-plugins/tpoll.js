d06a961402af45d75ec16884fe9536dcb1ce54c3
Wando94 <daniel.wilson11@outlook.com>
2015-11-03 17:31:29 +0000

    Create tpoll.js

---
+++ chat-plugins/tpoll.js
@@ -0,0 +1,4 @@
+tierpoll: 'tpoll',
+	tpoll: function(target, room, user) {
+	return this.parse('/poll new Tournament Tier? [Anything Goes', 'Challenge Cup 1v1', 'Monotype', 'OU', 'Random Battle', 'Random Monotype Battle', 'Tier Shift', 'UU']');
+}
