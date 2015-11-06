f2010a620e566dc6be70a8b5c80e8ea4a18457b0
Ivo Julca <ivojulca@hotmail.com>
2015-11-05 23:16:37 -0500

    Gen 4: implement Arceus EV restrictions
    Though they will not actually be used since Arceus is banned.

--- mods/gen4/rulesets.js
+++ mods/gen4/rulesets.js
@@ -8,6 +8,11 @@ exports.BattleFormats = {
 			if (item && item.id === 'griseousorb' && template.num !== 487) {
 				return ['Griseous Orb can only be held by Giratina in Generation 4.'];
 			}
+			if (template.num === 493 && set.evs) {
+				for (var stat in set.evs) {
+					if (set.evs[stat] > 100) return ["Arceus may not have more than 100 of any EVs in Generation 4."];
+				}
+			}
 		}
 	}
 };
