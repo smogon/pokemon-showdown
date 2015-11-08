86dd9f819d33b24b0c7bf6967a1887e16e64c717
Konrad Borowski <x.fix@o2.pl>
2015-11-08 08:57:50 +0100

    Prefer ES6 octal literals to string form

--- config/config-example.js
+++ config/config-example.js
@@ -182,7 +182,7 @@ exports.appealurl = '';
 // replsocketprefix - the prefix for the repl sockets to be listening on
 // replsocketmode - the file mode bits to use for the repl sockets
 exports.replsocketprefix = './logs/repl/';
-exports.replsocketmode = '0600';
+exports.replsocketmode = 0o600;
 
 // permissions and groups:
 //   Each entry in `grouplist' is a seperate group. Some of the members are "special"
