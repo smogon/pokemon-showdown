e5eecf515ad40c7dfdaf2b0e8e11a3cfa1da097f
Guangcong Luo <guangcongluo@gmail.com>
2015-11-07 23:12:04 -0600

    Fix duplicate cases in random set generators

--- dev-tools/eslint/config-base.js
+++ dev-tools/eslint/config-base.js
@@ -19,7 +19,7 @@ module.exports = {
 		"no-debugger": 2,
 		"no-dupe-args": 2,
 		"no-dupe-keys": 2,
-		"no-duplicate-case": 1,
+		"no-duplicate-case": 2,
 		"no-empty-character-class": 2,
 		"no-empty": 0,
 		"no-ex-assign": 2,
