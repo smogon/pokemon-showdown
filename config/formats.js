662c32cc92131fbc640d76449ac0ae0a62c0904e
The Immortal <the_immortal123@live.com>
2015-11-09 03:40:08 +0400

    Update Tier Shift thread link
    The metagame and viability threads have been merged.

--- config/formats.js
+++ config/formats.js
@@ -671,8 +671,7 @@ exports.Formats = [
 		name: "Tier Shift",
 		desc: [
 			"Pok&eacute;mon below OU/BL get all their stats boosted. UU/BL2 get +5, RU/BL3 get +10, NU/Bl4 get +15, and PU or lower get +20.",
-			"&bullet; <a href=\"https://www.smogon.com/forums/threads/3532973/\">Tier Shift</a>",
-			"&bullet; <a href=\"https://www.smogon.com/forums/threads/3554765/\">Tier Shift Viability Ranking</a>"
+			"&bullet; <a href=\"https://www.smogon.com/forums/threads/3554765/\">Tier Shift</a>"
 		],
 		section: "Other Metagames",
 
