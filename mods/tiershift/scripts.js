821e4d2659905a716fcc2073dcde7ab23fc6f10f
The Immortal <the_immortal123@live.com>
2015-11-12 03:41:06 +0400

    Fix Tier Shift
    - Use the Mega’s tier when relevant
    - Add HP boost

--- mods/tiershift/scripts.js
+++ mods/tiershift/scripts.js
@@ -31,19 +31,25 @@ exports.BattleScripts = {
 					'LC Uber': 20,
 					'LC': 20
 				};
+				let tier = template.tier;
+				if (this.set.item) {
+					var item = this.battle.getItem(this.set.item);
+					if (item.megaEvolves === template.species) tier = this.battle.getTemplate(item.megaStone).tier;
+				}
+				if (tier.charAt(0) === '(') tier = tier.slice(1, -1);
+				let boost = (tier in boosts) ? boosts[tier] : 0;
+				if (this.set.ability in {'Drizzle': 1, 'Drought': 1, 'Shadow Tag': 1}) {
+					boost = 0;
+				} else if (this.set.moves.indexOf('chatter') >= 0) {
+					boost = 15;
+				}
+
+				var boostedHP = Math.floor(Math.floor(2 * this.template.baseStats['hp'] + boost + this.set.ivs['hp'] + Math.floor(this.set.evs['hp'] / 4) + 100) * this.level / 100 + 10);
+				if (this.maxhp > 1 && this.maxhp < boostedHP) this.hp = this.maxhp = boostedHP;
+
 				for (let statName in this.stats) {
 					let stat = this.template.baseStats[statName];
-
-					let tier = template.tier;
-					if (tier.charAt(0) === '(') tier = tier.slice(1, -1);
-					let boost = (tier in boosts) ? boosts[tier] : 0;
-					if (this.set.ability in {'Drizzle': 1, 'Drought': 1, 'Shadow Tag': 1}) {
-						boost = 0;
-					} else if (this.set.moves.indexOf('chatter') >= 0) {
-						boost = 15;
-					}
 					stat = this.battle.clampIntRange(stat + boost, 1, 255);
-
 					stat = Math.floor(Math.floor(2 * stat + this.set.ivs[statName] + Math.floor(this.set.evs[statName] / 4)) * this.level / 100 + 5);
 
 					let nature = this.battle.getNature(this.set.nature);
