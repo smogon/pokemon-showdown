1daa833c66105dba00b63ce29b96c5c77152599d
Marty-D <math_man70@hotmail.com>
2015-11-15 21:05:07 -0500

    Fix Gooey activation

--- data/abilities.js
+++ data/abilities.js
@@ -951,7 +951,10 @@ exports.BattleAbilities = {
 	"gooey": {
 		shortDesc: "Pokemon making contact with this Pokemon have their Speed lowered by 1 stage.",
 		onAfterDamage: function (damage, target, source, effect) {
-			if (effect && effect.flags['contact']) this.boost({spe: -1}, source, target);
+			if (effect && effect.flags['contact']) {
+				this.add('-ability', target, 'Gooey');
+				this.boost({spe: -1}, source, target);
+			}
 		},
 		id: "gooey",
 		name: "Gooey",
