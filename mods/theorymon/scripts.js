675adc719784144b049e7e963ee550f21dabd27d
The Immortal <the_immortal123@live.com>
2015-11-20 13:17:16 +0400

    Add new Theorymons

--- mods/theorymon/scripts.js
+++ mods/theorymon/scripts.js
@@ -55,5 +55,7 @@ exports.BattleScripts = {
 		this.modData('Pokedex', 'cobalion').abilities['1'] = 'Magnet Pull';
 		this.modData('Learnsets', 'sceptile').learnset.seedflare = ['6T'];
 		this.modData('Pokedex', 'gogoat').abilities['1'] = 'Thick Fat';
+		this.modData('Pokedex', 'houndoommega').abilities['0'] = 'Rattled';
+		this.modData('Learnsets', 'houndoom').learnset.moonblast = ['6T'];
 	}
 };
