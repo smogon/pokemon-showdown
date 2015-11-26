4d2eda196801fab2a933a5b40f513f7a387b2282
Marty-D <math_man70@hotmail.com>
2015-11-26 11:39:24 -0500

    Fix Serene Grace's interaction with Secret Power

--- mods/gen3/abilities.js
+++ mods/gen3/abilities.js
@@ -93,6 +93,17 @@ exports.BattleAbilities = {
 			}
 		}
 	},
+	"serenegrace": {
+		inherit: true,
+		onModifyMove: function (move) {
+			if (move.secondaries) {
+				this.debug('doubling secondary chance');
+				for (var i = 0; i < move.secondaries.length; i++) {
+					move.secondaries[i].chance *= 2;
+				}
+			}
+		}
+	},
 	"shadowtag": {
 		inherit: true,
 		onFoeModifyPokemon: function (pokemon) {
