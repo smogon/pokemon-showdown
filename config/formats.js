41d2f26c395ebf5ae8f9747a0aa4fa9c7fe42552
The Immortal <the_immortal123@live.com>
2015-12-03 20:39:32 +0400

    Remove PU suspect test

--- config/formats.js
+++ config/formats.js
@@ -103,7 +103,7 @@ exports.Formats = [
 		banlist: ['RU', 'BL3']
 	},
 	{
-		name: "PU (suspect test)",
+		name: "PU",
 		desc: [
 			"&bullet; <a href=\"https://www.smogon.com/forums/threads/3554725/\">np: PU Stage 4</a>",
 			"&bullet; <a href=\"https://www.smogon.com/forums/threads/3528743/\">PU Viability Ranking</a>"
