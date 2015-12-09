01ca17e5ce160ddf517efd2a8523982de9ba74ab
Konrad Borowski <x.fix@o2.pl>
2015-12-09 19:54:40 +0100

    Implement VGC 2016

--- config/formats.js
+++ config/formats.js
@@ -260,6 +260,31 @@ exports.Formats = [
 		requirePentagon: true
 	},
 	{
+		name: "VGC 2016",
+		section: "ORAS Doubles",
+
+		gameType: 'doubles',
+		maxForcedLevel: 50,
+		teamLength: {
+			validate: [4, 6],
+			battle: 4
+		},
+		ruleset: ['Pokemon', 'Nickname Clause', 'Species Clause', 'Item Clause', 'Cancel Mod', 'Team Preview'],
+		banlist: ['Unreleased', 'Illegal', 'Mew', 'Celebi', 'Jirachi', 'Deoxys', 'Deoxys-Attack', 'Deoxys-Defense', 'Deoxys-Speed', 'Phione',
+			'Manaphy', 'Darkrai', 'Shaymin', 'Shaymin-Sky', 'Arceus', 'Victini', 'Keldeo', 'Meloetta', 'Genesect', 'Diancie', 'Hoopa', 'Hoopa-Unbound',
+			'Soul Dew', 'Tornadus + Defiant', 'Thundurus + Defiant', 'Landorus + Sheer Force'
+		],
+		onValidateTeam: function (team, format) {
+			const legends = {'Mewtwo':1, 'Lugia':1, 'Ho-Oh':1, 'Kyogre':1, 'Groudon':1, 'Rayquaza':1, 'Dialga':1, 'Palkia':1, 'Giratina':1, 'Reshiram':1, 'Zekrom':1, 'Kyurem':1, 'Xerneas':1, 'Yveltal':1, 'Zygarde':1};
+			let n = 0;
+			for (let i = 0; i < team.length; i++) {
+				const template = this.getTemplate(team[i].species).baseSpecies;
+				if (template in legends) n++;
+				if (n > 2) return ["You can only use up to two legendary Pokémon."];
+			}
+		}
+	},
+	{
 		name: "Battle Spot Special 13",
 		desc: ["&bullet; <a href=\"https://www.smogon.com/forums/threads/3555571/\">Battle Spot Special Season 13</a>"],
 		section: "ORAS Doubles",
