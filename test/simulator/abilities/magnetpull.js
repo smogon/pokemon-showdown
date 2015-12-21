a372f7f69b995f54a3dd438e19656c44bec8e11a
Kevin Lau <ascriptmaster@gmail.com>
2015-12-21 12:21:38 -0800

    Add test suites for trapping moves and abilities

---
+++ test/simulator/abilities/magnetpull.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const assert = require('assert');
+let battle;
+
+describe('Magnet Pull', function () {
+	afterEach(function () {
+		battle.destroy();
+	});
+
+	it('should prevent Steel-type Pokemon from switching out normally', function () {
+		battle = BattleEngine.Battle.construct();
+		battle.join('p1', 'Guest 1', 1, [{species: "Magnezone", ability: 'magnetpull', moves: ['soak', 'charge']}]);
+		battle.join('p2', 'Guest 2', 1, [
+			{species: "Heatran", ability: 'flashfire', moves: ['curse']},
+			{species: "Starmie", ability: 'illuminate', moves: ['reflecttype']}
+		]);
+		battle.choose('p2', 'switch 2');
+		battle.commitDecisions();
+		assert.strictEqual(battle.p2.active[0].template.speciesid, 'heatran');
+		battle.choose('p2', 'switch 2');
+		battle.commitDecisions();
+		assert.strictEqual(battle.p2.active[0].template.speciesid, 'starmie');
+		battle.choose('p1', 'move 2');
+		battle.commitDecisions(); // Reflect Type makes Starmie part Steel
+		battle.choose('p2', 'switch 2');
+		battle.commitDecisions();
+		assert.strictEqual(battle.p2.active[0].template.speciesid, 'starmie');
+	});
+
+	it('should not prevent Steel-type Pokemon from switching out using moves', function () {
+		battle = BattleEngine.Battle.construct();
+		battle.join('p1', 'Guest 1', 1, [{species: "Magnezone", ability: 'magnetpull', moves: ['toxic']}]);
+		battle.join('p2', 'Guest 2', 1, [
+			{species: "Heatran", ability: 'flashfire', moves: ['batonpass']},
+			{species: "Tentacruel", ability: 'clearbody', moves: ['rapidspin']}
+		]);
+		battle.commitDecisions();
+		battle.choose('p2', 'switch 2');
+		assert.strictEqual(battle.p2.active[0].template.speciesid, 'tentacruel');
+	});
+
+	it('should not prevent Pokemon immune to trapping from switching out', function () {
+		battle = BattleEngine.Battle.construct();
+		battle.join('p1', 'Guest 1', 1, [{species: "Magnezone", ability: 'magnetpull', moves: ['substitute']}]);
+		battle.join('p2', 'Guest 2', 1, [
+			{species: "Aegislash", ability: 'stancechange', moves: ['swordsdance']},
+			{species: "Arcanine", ability: 'flashfire', moves: ['roar']}
+		]);
+		battle.choose('p2', 'switch 2');
+		battle.commitDecisions();
+		assert.strictEqual(battle.p2.active[0].template.speciesid, 'arcanine');
+	});
+});
---
+++ test/simulator/misc/trapmoves.js
@@ -0,0 +1,158 @@
+'use strict';
+
+const assert = require('assert');
+let battle;
+let trappers = ['Block', 'Mean Look', 'Spider Web', 'Thousand Waves'];
+let partialtrappers = ['Bind', 'Clamp', 'Fire Spin', 'Infestation', 'Magma Storm', 'Sand Tomb', 'Whirlpool', 'Wrap'];
+
+describe('Trapping Moves', function () {
+	afterEach(function () {
+		battle.destroy();
+	});
+
+	for (let i = 0; i < trappers.length; i++) {
+		it('should prevent Pokemon from switching out normally', function () {
+			battle = BattleEngine.Battle.construct();
+			battle.join('p1', 'Guest 1', 1, [{species: "Smeargle", ability: 'prankster', moves: [toId(trappers[i])]}]);
+			battle.join('p2', 'Guest 2', 1, [
+				{species: "Tangrowth", ability: 'leafguard', moves: ['swordsdance']},
+				{species: "Starmie", ability: 'illuminate', moves: ['reflecttype']}
+			]);
+			battle.commitDecisions();
+			battle.choose('p2', 'switch 2');
+			battle.commitDecisions();
+			assert.strictEqual(battle.p2.active[0].template.speciesid, 'tangrowth');
+		});
+
+		it('should not prevent Pokemon from switching out using moves', function () {
+			battle = BattleEngine.Battle.construct();
+			battle.join('p1', 'Guest 1', 1, [{species: "Smeargle", ability: 'prankster', moves: [toId(trappers[i])]}]);
+			battle.join('p2', 'Guest 2', 1, [
+				{species: "Tangrowth", ability: 'leafguard', moves: ['batonpass']},
+				{species: "Starmie", ability: 'illuminate', moves: ['reflecttype']}
+			]);
+			battle.commitDecisions();
+			battle.choose('p2', 'switch 2');
+			assert.strictEqual(battle.p2.active[0].template.speciesid, 'starmie');
+		});
+
+		it('should not prevent Pokemon immune to trapping from switching out', function () {
+			battle = BattleEngine.Battle.construct();
+			battle.join('p1', 'Guest 1', 1, [{species: "Smeargle", ability: 'prankster', moves: [toId(trappers[i])]}]);
+			battle.join('p2', 'Guest 2', 1, [
+				{species: "Gourgeist", ability: 'insomnia', moves: ['synthesis']},
+				{species: "Starmie", ability: 'illuminate', moves: ['reflecttype']}
+			]);
+			battle.commitDecisions();
+			battle.choose('p2', 'switch 2');
+			battle.commitDecisions();
+			assert.strictEqual(battle.p2.active[0].template.speciesid, 'starmie');
+		});
+
+		it('should stop trapping the Pokemon if the user is no longer active', function () {
+			battle = BattleEngine.Battle.construct();
+			battle.join('p1', 'Guest 1', 1, [
+				{species: "Smeargle", ability: 'prankster', moves: [toId(trappers[i])]},
+				{species: "Kyurem", ability: 'pressure', moves: ['rest']}
+			]);
+			battle.join('p2', 'Guest 2', 1, [
+				{species: "Tangrowth", ability: 'leafguard', moves: ['roar']},
+				{species: "Starmie", ability: 'illuminate', moves: ['reflecttype']}
+			]);
+			battle.commitDecisions();
+			battle.choose('p2', 'switch 2');
+			battle.commitDecisions();
+			assert.strictEqual(battle.p2.active[0].template.speciesid, 'starmie');
+		});
+	}
+});
+
+describe('Partial Trapping Moves', function () {
+	afterEach(function () {
+		battle.destroy();
+	});
+
+	for (let i = 0; i < partialtrappers.length; i++) {
+		it('should deal 1/8 HP per turn', function () {
+			battle = BattleEngine.Battle.construct();
+			battle.join('p1', 'Guest 1', 1, [{species: "Smeargle", ability: 'noguard', moves: [toId(partialtrappers[i]), 'rest']}]);
+			battle.join('p2', 'Guest 2', 1, [{species: "Blissey", ability: 'naturalcure', moves: ['healbell']}]);
+			battle.commitDecisions();
+			let pokemon = battle.p2.active[0];
+			pokemon.heal(pokemon.maxhp);
+			battle.choose('p1', 'move 2');
+			battle.commitDecisions();
+			assert.strictEqual(pokemon.maxhp - pokemon.hp, battle.modify(pokemon.maxhp, 1 / 8));
+		});
+
+		it('should prevent Pokemon from switching out normally', function () {
+			battle = BattleEngine.Battle.construct();
+			battle.join('p1', 'Guest 1', 1, [{species: "Smeargle", ability: 'noguard', moves: [toId(partialtrappers[i])]}]);
+			battle.join('p2', 'Guest 2', 1, [
+				{species: "Blissey", ability: 'naturalcure', moves: ['healbell']},
+				{species: "Starmie", ability: 'illuminate', moves: ['reflecttype']}
+			]);
+			battle.commitDecisions();
+			battle.choose('p2', 'switch 2');
+			battle.commitDecisions();
+			assert.strictEqual(battle.p2.active[0].template.speciesid, 'blissey');
+		});
+
+		it('should not prevent Pokemon from switching out using moves', function () {
+			battle = BattleEngine.Battle.construct();
+			battle.join('p1', 'Guest 1', 1, [{species: "Smeargle", ability: 'noguard', moves: [toId(partialtrappers[i])]}]);
+			battle.join('p2', 'Guest 2', 1, [
+				{species: "Blissey", ability: 'naturalcure', moves: ['batonpass']},
+				{species: "Starmie", ability: 'illuminate', moves: ['reflecttype']}
+			]);
+			battle.commitDecisions();
+			battle.choose('p2', 'switch 2');
+			assert.strictEqual(battle.p2.active[0].template.speciesid, 'starmie');
+		});
+
+		it('should not prevent Pokemon immune to trapping from switching out', function () {
+			battle = BattleEngine.Battle.construct();
+			battle.join('p1', 'Guest 1', 1, [{species: "Smeargle", ability: 'noguard', moves: [toId(partialtrappers[i])]}]);
+			battle.join('p2', 'Guest 2', 1, [
+				{species: "Dusknoir", ability: 'frisk', moves: ['sleeptalk']},
+				{species: "Starmie", ability: 'illuminate', moves: ['reflecttype']}
+			]);
+			battle.commitDecisions();
+			battle.choose('p2', 'switch 2');
+			battle.commitDecisions();
+			assert.strictEqual(battle.p2.active[0].template.speciesid, 'starmie');
+		});
+
+		it('should stop trapping the Pokemon if the user is no longer active', function () {
+			battle = BattleEngine.Battle.construct();
+			battle.join('p1', 'Guest 1', 1, [
+				{species: "Smeargle", ability: 'noguard', moves: [toId(partialtrappers[i])]},
+				{species: "Kyurem", ability: 'pressure', moves: ['rest']}
+			]);
+			battle.join('p2', 'Guest 2', 1, [
+				{species: "Blissey", ability: 'naturalcure', moves: ['roar']},
+				{species: "Starmie", ability: 'illuminate', moves: ['reflecttype']}
+			]);
+			battle.commitDecisions();
+			battle.choose('p2', 'switch 2');
+			battle.commitDecisions();
+			assert.strictEqual(battle.p2.active[0].template.speciesid, 'starmie');
+		});
+
+		it('should stop trapping the Pokemon if the target uses Rapid Spin', function () {
+			battle = BattleEngine.Battle.construct();
+			battle.join('p1', 'Guest 1', 1, [
+				{species: "Smeargle", ability: 'noguard', moves: [toId(partialtrappers[i])]},
+				{species: "Kyurem", ability: 'pressure', moves: ['rest']}
+			]);
+			battle.join('p2', 'Guest 2', 1, [
+				{species: "Blissey", ability: 'naturalcure', moves: ['rapidspin']},
+				{species: "Starmie", ability: 'illuminate', moves: ['reflecttype']}
+			]);
+			battle.commitDecisions();
+			battle.choose('p2', 'switch 2');
+			battle.commitDecisions();
+			assert.strictEqual(battle.p2.active[0].template.speciesid, 'starmie');
+		});
+	}
+});
