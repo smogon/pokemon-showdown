7c491ad06b454899a82de431bba1d60bcfe8401d
Kevin Lau <ascriptmaster@gmail.com>
2015-12-21 12:21:38 -0800

    Add test suites for trapping moves and abilities

---
+++ test/simulator/abilities/shadowtag.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const assert = require('assert');
+let battle;
+
+describe('Shadow Tag', function () {
+	afterEach(function () {
+		battle.destroy();
+	});
+
+	it('should prevent most Pokemon from switching out normally', function () {
+		battle = BattleEngine.Battle.construct();
+		battle.join('p1', 'Guest 1', 1, [{species: "Wobbuffet", ability: 'shadowtag', moves: ['counter']}]);
+		battle.join('p2', 'Guest 2', 1, [
+			{species: "Tornadus", ability: 'defiant', moves: ['tailwind']},
+			{species: "Heatran", ability: 'flashfire', moves: ['roar']}
+		]);
+		battle.choose('p2', 'switch 2');
+		battle.commitDecisions();
+		assert.strictEqual(battle.p2.active[0].template.speciesid, 'tornadus');
+	});
+
+	it('should not prevent Pokemon from switching out using moves', function () {
+		battle = BattleEngine.Battle.construct();
+		battle.join('p1', 'Guest 1', 1, [{species: "Wobbuffet", ability: 'shadowtag', moves: ['counter']}]);
+		battle.join('p2', 'Guest 2', 1, [
+			{species: "Tornadus", ability: 'defiant', moves: ['uturn']},
+			{species: "Heatran", ability: 'flashfire', moves: ['roar']}
+		]);
+		battle.commitDecisions();
+		battle.choose('p2', 'switch 2');
+		assert.strictEqual(battle.p2.active[0].template.speciesid, 'heatran');
+	});
+
+	it('should not prevent other Pokemon with Shadow Tag from switching out', function () {
+		battle = BattleEngine.Battle.construct();
+		battle.join('p1', 'Guest 1', 1, [{species: "Wobbuffet", ability: 'shadowtag', moves: ['counter']}]);
+		battle.join('p2', 'Guest 2', 1, [
+			{species: "Gothitelle", ability: 'shadowtag', moves: ['psychic']},
+			{species: "Heatran", ability: 'flashfire', moves: ['roar']}
+		]);
+		battle.choose('p2', 'switch 2');
+		battle.commitDecisions();
+		assert.strictEqual(battle.p2.active[0].template.speciesid, 'heatran');
+	});
+
+	it('should not prevent Pokemon immune to trapping from switching out', function () {
+		battle = BattleEngine.Battle.construct();
+		battle.join('p1', 'Guest 1', 1, [{species: "Wobbuffet", ability: 'shadowtag', moves: ['counter']}]);
+		battle.join('p2', 'Guest 2', 1, [
+			{species: "Gengar", ability: 'levitate', moves: ['curse']},
+			{species: "Heatran", ability: 'flashfire', moves: ['roar']}
+		]);
+		battle.choose('p2', 'switch 2');
+		battle.commitDecisions();
+		assert.strictEqual(battle.p2.active[0].template.speciesid, 'heatran');
+	});
+});
---
+++ test/simulator/items/shedshell.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const assert = require('assert');
+let battle;
+
+describe('Shed Shell', function () {
+	afterEach(function () {
+		battle.destroy();
+	});
+
+	it('should allow a Pokemon to escape a trapping abilities', function () {
+		battle = BattleEngine.Battle.construct();
+		battle.join('p1', 'Guest 1', 1, [{species: "Gothitelle", ability: 'shadowtag', moves: ['calmmind']}]);
+		battle.join('p2', 'Guest 2', 1, [
+			{species: "Starmie", ability: 'naturalcure', item: 'shedshell', moves: ['recover']},
+			{species: "Heatran", ability: 'flashfire',  moves: ['rest']}
+		]);
+		battle.choose('p2', 'switch 2');
+		battle.commitDecisions();
+		assert.strictEqual(battle.p2.active[0].speciesid, 'heatran');
+	});
+
+	it('should allow a Pokemon to escape from most moves that would trap them', function () {
+		battle = BattleEngine.Battle.construct();
+		battle.join('p1', 'Guest 1', 1, [{species: "Gengar", ability: 'levitate', moves: ['meanlook']}]);
+		battle.join('p2', 'Guest 2', 1, [
+			{species: "Venusaur", ability: 'overgrow', item: 'shedshell', moves: ['ingrain']},
+			{species: "Heatran", ability: 'flashfire',  moves: ['rest']}
+		]);
+		battle.commitDecisions();
+		battle.choose('p2', 'switch 2');
+		battle.commitDecisions();
+		assert.strictEqual(battle.p2.active[0].speciesid, 'heatran');
+	});
+
+	it('should not allow a Pokemon to escape from Sky Drop', function () {
+		battle = BattleEngine.Battle.construct();
+		battle.join('p1', 'Guest 1', 1, [{species: "Dragonite", ability: 'multiscale', moves: ['skydrop']}]);
+		battle.join('p2', 'Guest 2', 1, [
+			{species: "Magnezone", ability: 'sturdy', item: 'shedshell', moves: ['sleeptalk']},
+			{species: "Heatran", ability: 'flashfire',  moves: ['rest']}
+		]);
+		battle.commitDecisions();
+		battle.choose('p2', 'switch 2');
+		battle.commitDecisions();
+		assert.strictEqual(battle.p2.active[0].speciesid, 'magnezone');
+	});
+});
