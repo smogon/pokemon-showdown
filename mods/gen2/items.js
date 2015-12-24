8d2c022c1466af53f73c26ebc764b271c2331991
Marty-D <math_man70@hotmail.com>
2015-12-24 12:36:57 -0500

    Gen II: Remove now unneeded items.
    And fix up the real Gen 2 items.

--- mods/gen2/items.js
+++ mods/gen2/items.js
@@ -1,78 +1,16 @@
 'use strict';
 
 exports.BattleItems = {
-	amuletcoin: {
-		id: "amuletcoin",
-		name: "Amulet Coin",
-		num: -1,
-		gen: 2,
-		desc: "Doubles the amount of money received in trainer battles."
-	},
-	aspearberry: {
-		inherit: true,
-		gen: 2
-	},
-	berserkgene: {
-		id: "berserkgene",
-		name: "Berserk Gene",
-		onUpdate: function (pokemon) {
-			this.boost({atk: 2});
-			pokemon.addVolatile('confusion');
-			pokemon.setItem('');
-		},
-		gen: 2
-	},
-	cheriberry: {
-		inherit: true,
-		gen: 2
-	},
-	chestoberry: {
-		inherit: true,
-		gen: 2
-	},
 	dragonfang: {
 		inherit: true,
 		onBasePower: function () {},
 		desc: "No competitive use."
-	},
-	dragonscale: {
-		id: "dragonscale",
-		name: "Dragon Scale",
-		num: -3,
-		onBasePower: function (basePower, user, target, move) {
-			if (move.type === 'Dragon') {
-				return basePower * 1.1;
-			}
-		},
-		gen: 2
 	},
 	metalpowder: {
 		inherit: true,
 		// On Gen 2 this happens in stat calculation directly.
 		onModifyDef: function () {},
 		onModifySpD: function () {}
-	},
-	leppaberry: {
-		inherit: true,
-		onEat: function (pokemon) {
-			var move;
-			if (pokemon.volatiles['leppaberry']) {
-				move = pokemon.volatiles['leppaberry'].move;
-				pokemon.removeVolatile('leppaberry');
-			} else {
-				var pp = 99;
-				for (var i in pokemon.moveset) {
-					if (pokemon.moveset[i].pp < pp) {
-						move = pokemon.moveset[i];
-						pp = move.pp;
-					}
-				}
-			}
-			move.pp += 5;
-			if (move.pp > move.maxpp) move.pp = move.maxpp;
-			this.add('-activate', pokemon, 'item: Leppa Berry', move.move);
-		},
-		gen: 2
 	},
 	lightball: {
 		inherit: true,
@@ -87,50 +25,6 @@ exports.BattleItems = {
 				move.critRatio = 3;
 			}
 		}
-	},
-	lumberry: {
-		inherit: true,
-		gen: 2
-	},
-	oranberry: {
-		inherit: true,
-		gen: 2
-	},
-	pechaberry: {
-		inherit: true,
-		gen: 2
-	},
-	persimberry: {
-		inherit: true,
-		gen: 2
-	},
-	pinkbow: {
-		id: "pinkbow",
-		name: "Pink Bow",
-		onBasePower: function (basePower, user, target, move) {
-			if (move.type === 'Normal') {
-				return basePower * 1.1;
-			}
-		},
-		gen: 2
-	},
-	polkadotbow: {
-		id: "polkadotbow",
-		name: "Polkadot Bow",
-		onBasePower: function (basePower, user, target, move) {
-			if (move.type === 'Normal') {
-				return basePower * 1.1;
-			}
-		},
-		gen: 2
-	},
-	rawstberry: {
-		inherit: true,
-		gen: 2
-	},
-	sitrusberry: {
-		inherit: true,
-		gen: 2
 	},
 	stick: {
 		inherit: true,
