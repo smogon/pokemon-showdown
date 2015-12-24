f12c70e6d9270e9f0a1ef3dd1d725fb7b0c3b95c
Guangcong Luo <guangcongluo@gmail.com>
2015-12-24 04:36:41 -0600

    Remove gen 2 berries from Aliases
    We now have them properly implemented.

--- data/aliases.js
+++ data/aliases.js
@@ -219,18 +219,6 @@ exports.BattleAliases = {
 	"specs": "Choice Specs",
 	"yache": "Yache Berry",
 
-	// gen 1-2 berries
-	"berry": "Oran Berry",
-	"bitterberry": "Persim Berry",
-	"burntberry": "Aspear Berry",
-	"goldberry": "Sitrus Berry",
-	"iceberry": "Rawst Berry",
-	"mintberry": "Chesto Berry",
-	"miracleberry": "Lum Berry",
-	"mysteryberry": "Leppa Berry",
-	"przcureberry": "Cheri Berry",
-	"psncureberry": "Pecha Berry",
-
 	// pokemon
 	"aboma": "Abomasnow",
 	"aegi": "Aegislash",
