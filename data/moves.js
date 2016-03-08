919ca7f5cb3b4b9df34e48d894ef4beb391fdde7
Marty-D <math_man70@hotmail.com>
2016-03-07 21:45:29 -0500

    Avoid all potential Thousand Arrows crashes from info commands

--- data/moves.js
+++ data/moves.js
@@ -14140,6 +14140,7 @@ exports.BattleMovedex = {
 		onEffectiveness: function (typeMod, type, move) {
 			if (move.type !== 'Ground') return;
 			let target = this.activeTarget;
+			if (!target) return; // avoid crashing when called from a chat plugin
 			// ignore effectiveness if the target is Flying type and immune to Ground
 			if (!target.runImmunity('Ground')) {
 				if (target.hasType('Flying')) return 0;
