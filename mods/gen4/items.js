9d496e00dcc23ff4a689ebcf0eb36b0e523d59fb
Marty-D <Marty-D@users.noreply.github.com>
2016-03-10 12:29:39 -0500

    Gen IV: Fix Focus Sash properly

--- mods/gen4/items.js
+++ mods/gen4/items.js
@@ -90,11 +90,12 @@ exports.BattleItems = {
 			duration: 1,
 			onDamage: function (damage, target, source, effect) {
 				if (effect && effect.effectType === 'Move' && damage >= target.hp) {
+					this.effectData.activated = true;
 					return target.hp - 1;
 				}
 			},
 			onAfterMoveSecondary: function (target) {
-				target.useItem();
+				if (this.effectData.activated) target.useItem();
 				target.removeVolatile('focussash');
 			},
 		},
