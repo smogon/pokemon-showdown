79747aa8b42ff46438d0c2259d86f57d2c1a53a7
Quite Quiet <femtontusen@gmail.com>
2016-03-14 12:52:25 +0100

    Modify target sent when redirected

--- data/moves.js
+++ data/moves.js
@@ -4665,6 +4665,7 @@ exports.BattleMovedex = {
 			onFoeRedirectTarget: function (target, source, source2, move) {
 				if (this.validTarget(this.effectData.target, source, move.target)) {
 					this.debug("Follow Me redirected target of move");
+					this.retargetLastMove(this.effectData.target);
 					return this.effectData.target;
 				}
 			},
@@ -10645,6 +10646,7 @@ exports.BattleMovedex = {
 			onFoeRedirectTarget: function (target, source, source2, move) {
 				if (source.runStatusImmunity('powder') && this.validTarget(this.effectData.target, source, move.target)) {
 					this.debug("Rage Powder redirected target of move");
+					this.retargetLastMove(this.effectData.target);
 					return this.effectData.target;
 				}
 			},
