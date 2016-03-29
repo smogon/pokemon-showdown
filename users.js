1acdee2e0f5d88930eff131da4f7e657da3b5a6a
Guangcong Luo <guangcongluo@gmail.com>
2016-03-29 19:44:13 -0400

    Limit users to 4 games at a time
    This is mostly targeted at the AG bot that's everywhere. Playing a
    huge number of battles at once makes it way too common to be matched
    with it over and over again, which is undesirable.

--- users.js
+++ users.js
@@ -1413,6 +1413,14 @@ class User {
 			connection.popup(message);
 			return Promise.resolve(false);
 		}
+		let gameCount = 0;
+		for (let i in this.games) { // eslint-disable-line no-unused-vars
+			gameCount++;
+			if (gameCount > 4) {
+				connection.popup("Due to high load, you are limited to 4 games at the same time.");
+				return Promise.resolve(false);
+			}
+		}
 		if (Monitor.countPrepBattle(connection.ip || connection.latestIp, this.name)) {
 			connection.popup("Due to high load, you are limited to 6 battles every 3 minutes.");
 			return Promise.resolve(false);
