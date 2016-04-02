915715b0cb6c670376a6b294e7c59fb6380d9c7e
The Immortal <the_immortal123@live.com>
2016-04-02 23:17:07 +0400

    Update Ability Unity

--- config/formats.js
+++ config/formats.js
@@ -413,7 +413,7 @@ exports.Formats = [
 		onValidateTeam: function (team) {
 			let problems = [];
 			let pokedex = Object.keys(Tools.data.Pokedex);
-			let bannedAbilities = {'Aerilate': 1, 'Arena Trap': 1, 'Fur Coat': 1, 'Huge Power': 1, 'Imposter': 1, 'Parental Bond': 1, 'Prankster': 1, 'Pure Power': 1, 'Shadow Tag': 1, 'Simple':1, 'Speed Boost': 1};
+			let usedPokemon = [];
 			for (let i = 0; i < team.length; i++) {
 				let template = this.getTemplate(team[i].species);
 				let ability = team[i].ability;
@@ -421,18 +421,22 @@ exports.Formats = [
 					problems.push(template.species + " needs to have an ability.");
 					continue;
 				}
-				let sources = pokedex.filter(pokemon => Tools.data.Pokedex[pokemon].num > 0 && template.types.sort().toString() === Tools.data.Pokedex[pokemon].types.sort().toString() && Object.values(Tools.data.Pokedex[pokemon].abilities).indexOf(ability) >= 0);
+				let sources = pokedex.filter(pokemon => usedPokemon.indexOf(pokemon) < 0 && Tools.data.Pokedex[pokemon].num > 0 && template.types.sort().toString() === Tools.data.Pokedex[pokemon].types.sort().toString() && Object.values(Tools.data.Pokedex[pokemon].abilities).indexOf(ability) >= 0);
 				if (!sources.length) {
 					problems.push(template.species + " cannot obtain the ability " + ability + ".");
 					continue;
 				}
-				if (ability in bannedAbilities) {
+				if (ability in {'Aerilate': 1, 'Arena Trap': 1, 'Fur Coat': 1, 'Huge Power': 1, 'Imposter': 1, 'Parental Bond': 1, 'Pure Power': 1, 'Shadow Tag': 1, 'Simple':1, 'Speed Boost': 1}) {
 					let legalAbility = false;
 					for (let i in template.abilities) {
 						if (ability === template.abilities[i]) legalAbility = true;
 					}
-					if (!legalAbility) problems.push('The ability ' + ability + ' is banned on Pok\u00e9mon that do not naturally have it.');
+					if (!legalAbility) {
+						problems.push("The ability " + ability + " is banned on Pok\u00e9mon that do not naturally have it.");
+						continue;
+					}
 				}
+				usedPokemon.push(sources[0]);
 			}
 			return problems;
 		},
