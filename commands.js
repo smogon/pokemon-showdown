60dd8f5feae3ac4e17218d8c854e90e8275c746b
Guangcong Luo <guangcongluo@gmail.com>
2016-04-13 01:16:01 -0400

    New hidden avatar: zinnia

--- commands.js
+++ commands.js
@@ -92,7 +92,7 @@ exports.commands = {
 		if (!target) return this.parse('/avatars');
 		let parts = target.split(',');
 		let avatar = parseInt(parts[0]);
-		if (parts[0] === '#bw2elesa' || parts[0] === '#teamrocket' || parts[0] === '#yellow') {
+		if (parts[0] === '#bw2elesa' || parts[0] === '#teamrocket' || parts[0] === '#yellow' || parts[0] === '#zinnia') {
 			avatar = parts[0];
 		}
 		if (typeof avatar === 'number' && (!avatar || avatar > 294 || avatar < 1)) {
