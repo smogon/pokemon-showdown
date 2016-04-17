952cefbad4b8f5ffe129e651d3cd3640ccea7b37
kotarou3 <kotarou3@users.noreply.github.com>
2016-04-17 19:15:17 +1000

    Minor crash guard changes (#2470)
    - Remove unused crash guard comment
    - Update Config.crashguard comment a bit
    - Use Config.crashguard to disable it in tests

--- battle-engine.js
+++ battle-engine.js
@@ -18,12 +18,6 @@ if (Config.crashguard) {
 	// graceful crash - allow current battles to finish before restarting
 	process.on('uncaughtException', err => {
 		require('./crashlogger.js')(err, 'A simulator process');
-		/* let stack = Tools.escapeHTML(err.stack).split("\n").slice(0, 2).join("<br />");
-		if (Rooms.lobby) {
-			Rooms.lobby.addRaw('<div><b>THE SERVER HAS CRASHED:</b> ' + stack + '<br />Please restart the server.</div>');
-			Rooms.lobby.addRaw('<div>You will not be able to talk in the lobby or start new battles until the server restarts.</div>');
-		}
-		Rooms.global.lockdown = true; */
 	});
 }
 
