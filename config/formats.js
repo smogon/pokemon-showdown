24d3d37b6e76fc04ec955f48a8006e9fad3242c0
The Immortal <the_immortal123@live.com>
2016-04-24 22:29:37 +0400

    Ability Unity: Check for non-existing Pokemon/Abilities

--- config/formats.js
+++ config/formats.js
@@ -398,23 +398,25 @@ exports.Formats = [
 			let usedPokemon = [];
 			for (let i = 0; i < team.length; i++) {
 				let template = this.getTemplate(team[i].species);
-				let ability = team[i].ability;
-				if (!ability) {
+				if (!template.exists) continue;
+				let ability = this.getAbility(team[i].ability);
+				if (!ability.name) {
 					problems.push(template.species + " needs to have an ability.");
 					continue;
 				}
-				let sources = pokedex.filter(pokemon => usedPokemon.indexOf(pokemon) < 0 && Tools.data.Pokedex[pokemon].num > 0 && template.types.sort().toString() === Tools.data.Pokedex[pokemon].types.sort().toString() && Object.values(Tools.data.Pokedex[pokemon].abilities).indexOf(ability) >= 0);
+				if (!ability.exists) continue;
+				let sources = pokedex.filter(pokemon => usedPokemon.indexOf(pokemon) < 0 && Tools.data.Pokedex[pokemon].num > 0 && template.types.sort().toString() === Tools.data.Pokedex[pokemon].types.sort().toString() && Object.values(Tools.data.Pokedex[pokemon].abilities).indexOf(ability.name) >= 0);
 				if (!sources.length) {
-					problems.push(template.species + " cannot obtain the ability " + ability + ".");
+					problems.push(template.species + " cannot obtain the ability " + ability.name + ".");
 					continue;
 				}
-				if (ability in {'Aerilate': 1, 'Arena Trap': 1, 'Fur Coat': 1, 'Huge Power': 1, 'Imposter': 1, 'Parental Bond': 1, 'Pure Power': 1, 'Simple':1, 'Speed Boost': 1}) {
+				if (ability.id in {aerilate:1, arenatrap:1, furcoat:1, hugepower:1, imposter:1, parentalbond:1, purepower:1, simple:1, speedboost:1}) {
 					let legalAbility = false;
 					for (let i in template.abilities) {
-						if (ability === template.abilities[i]) legalAbility = true;
+						if (ability.name === template.abilities[i]) legalAbility = true;
 					}
 					if (!legalAbility) {
-						problems.push("The ability " + ability + " is banned on Pok\u00e9mon that do not naturally have it.");
+						problems.push("The ability " + ability.name + " is banned on Pok\u00e9mon that do not naturally have it.");
 						continue;
 					}
 				}
