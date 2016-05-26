442ee6f5434533830ecff17f0b5b70b5665f19d1
jd4564 <jd4656@gmail.com>
2016-05-25 21:15:25 -0300

    Give room owners /roomdesc

--- commands.js
+++ commands.js
@@ -610,7 +610,7 @@ exports.commands = {
 			this.sendReplyBox("The room description is: " + Tools.escapeHTML(room.desc));
 			return;
 		}
-		if (!this.can('roomdesc')) return false;
+		if (!this.can('roomdesc', null, room)) return false;
 		if (target.length > 80) return this.errorReply("Error: Room description is too long (must be at most 80 characters).");
 		let normalizedTarget = ' ' + target.toLowerCase().replace('[^a-zA-Z0-9]+', ' ').trim() + ' ';
 
