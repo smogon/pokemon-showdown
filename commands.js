0a60b18e1723a6e861b94befc23c411c15945719
Jana-Brahimi <janaholden1@icloud.com>
2016-06-05 11:13:05 +0100

    Rank: Allow to broadcast

--- commands.js
+++ commands.js
@@ -283,6 +283,7 @@ exports.commands = {
 	backhelp: ["/back - Unblocks challenges and/or private messages, if either are blocked."],
 
 	rank: function (target, room, user) {
+	        if (!this.runBroadcast()) return;
 		if (!target) target = user.name;
 
 		Ladders.visualizeAll(target).then(values => {
