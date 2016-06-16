18768f9a3944788a01652a6fb1212d182ab6bb01
asgdf <asgdf@users.noreply.github.com>
2016-06-16 06:19:19 +0200

    Allow non-broadcasted use of /roomhelp in lobby/battles (#2587)

--- chat-plugins/info.js
+++ chat-plugins/info.js
@@ -1133,8 +1133,8 @@ exports.commands = {
 	},
 
 	roomhelp: function (target, room, user) {
-		if (room.id === 'lobby' || room.battle) return this.sendReply("This command is too spammy for lobby/battles.");
 		if (!this.runBroadcast()) return;
+		if (this.broadcasting && (room.id === 'lobby' || room.battle)) return this.sendReply("This command is too spammy for lobby/battles.");
 		this.sendReplyBox(
 			"Room drivers (%) can use:<br />" +
 			"- /warn OR /k <em>username</em>: warn a user and show the Pok&eacute;mon Showdown rules<br />" +
