93871c516152a04f27239180d4cb834649022b39
Kevin Lau <ascriptmaster@gmail.com>
2016-06-24 11:18:33 -0700

    Tests: Update Weather tests for clarity and readability
    Updated remaining Primal Weathers to the new APIs.
    
    Updated misleading test cases relating to base power of moves in
    Desolate Land / Primordial Sea.
    
    Increased coverage on weather tests.

--- test/simulator/abilities/desolateland.js
+++ test/simulator/abilities/desolateland.js
@@ -17,21 +17,25 @@ describe('Desolate Land', function () {
 		assert.ok(battle.isWeather('desolateland'));
 	});
 
-	it('should not increase the base power of Fire-type attacks', function () {
+	it('should increase the damage (not the basePower) of Fire-type attacks', function () {
 		battle = common.createBattle();
-		battle.join('p1', 'Guest 1', 1, [{species: "Groudon", ability: 'desolateland', moves: ['helpinghand']}]);
-		battle.join('p2', 'Guest 2', 1, [{species: "Charizard", ability: 'blaze', moves: ['firepledge']}]);
-		battle.commitDecisions();
-		let move = Tools.getMove('firepledge');
-		let basePower = battle.runEvent('BasePower', battle.p2.active[0], battle.p1.active[0], move, move.basePower, true);
+		battle.randomizer = dmg => dmg; // max damage
+		battle.join('p1', 'Guest 1', 1, [{species: 'Ninetales', ability: 'desolateland', moves: ['incinerate']}]);
+		battle.join('p2', 'Guest 2', 1, [{species: 'Cryogonal', ability: 'levitate', moves: ['splash']}]);
+		const attacker = battle.p1.active[0];
+		const defender = battle.p2.active[0];
+		assert.hurtsBy(defender, 152, () => battle.commitDecisions());
+		const move = Tools.getMove('incinerate');
+		const basePower = battle.runEvent('BasePower', attacker, defender, move, move.basePower, true);
 		assert.strictEqual(basePower, move.basePower);
 	});
 
 	it('should cause Water-type attacks to fail', function () {
 		battle = common.createBattle();
-		const p1 = battle.join('p1', 'Guest 1', 1, [{species: "Groudon", ability: 'desolateland', moves: ['helpinghand']}]);
+		battle.join('p1', 'Guest 1', 1, [{species: "Groudon", ability: 'desolateland', moves: ['helpinghand']}]);
 		battle.join('p2', 'Guest 2', 1, [{species: "Blastoise", ability: 'torrent', moves: ['surf']}]);
-		assert.false.hurts(p1.active[0], () => battle.commitDecisions());
+		battle.commitDecisions();
+		assert.fullHP(battle.p1.active[0]);
 	});
 
 	it('should not cause Water-type Status moves to fail', function () {
@@ -53,8 +57,7 @@ describe('Desolate Land', function () {
 			{species: "Abomasnow", ability: 'snowwarning', moves: ['hail']},
 		]);
 		for (let i = 2; i <= 5; i++) {
-			battle.choose('p1', 'switch ' + i);
-			battle.commitDecisions();
+			battle.p2.chooseSwitch(i).foe.chooseDefault();
 			assert.ok(battle.isWeather('desolateland'));
 			battle.commitDecisions();
 			assert.ok(battle.isWeather('desolateland'));
@@ -71,12 +74,11 @@ describe('Desolate Land', function () {
 			{species: "Venusaur", ability: 'chlorophyll', moves: ['growth']},
 			{species: "Toxicroak", ability: 'dryskin', moves: ['bulkup']},
 		]);
-		p1.active[0].damage = function () {
-			if (battle.activeMove.id === 'weatherball') {
-				assert.strictEqual(battle.activeMove.type, 'Fire');
+		battle.on('Hit', battle.getFormat(), (target, pokemon, move) => {
+			if (move.id === 'weatherball') {
+				assert.strictEqual(move.type, 'Fire');
 			}
-			return BattleEngine.BattlePokemon.prototype.damage.apply(this, arguments);
-		};
+		});
 		battle.commitDecisions();
 		assert.species(p2.active[0], 'Castform-Sunny');
 		p2.chooseSwitch(2).foe.chooseDefault();
