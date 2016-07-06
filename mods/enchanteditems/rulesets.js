0a5cf9a037480c94cd1d3bdaf80e37e2350a54c8
Spandan7 <spandan.punwatkar@gmail.com>
2016-07-06 16:39:59 +0530

    Update rulesets.js

--- mods/enchanteditems/rulesets.js
+++ mods/enchanteditems/rulesets.js
@@ -32,7 +32,7 @@ exports.BattleFormats = {
 			}
 			let boostSpeed = ['flamecharge', 'geomancy', 'motordrive', 'rattled', 'speedboost', 'steadfast', 'weakarmor'];
 			let boostSpeedEI = ['cheriberry', 'duskball', 'friendball', 'skullfossil', 'weaknesspolicy'];
-			boostSpeed=boostSpeed.concat(boostSpeedEI);
+			boostSpeed = boostSpeed.concat (boostSpeedEI);
 			if (!speedBoosted) {
 				for (let i = 0; i < boostSpeed.length; i++) {
 					if (boostSpeed[i] in setHas) {
@@ -54,7 +54,7 @@ exports.BattleFormats = {
 			}
 			let boostNonSpeed = ['acupressure', 'curse', 'metalclaw', 'meteormash', 'poweruppunch', 'rage', 'rototiller', 'fellstinger', 'bellydrum', 'download', 'justified', 'moxie', 'sapsipper', 'defiant', 'angerpoint', 'diamondstorm', 'flowershield', 'skullbash', 'steelwing', 'stockpile', 'cottonguard', 'chargebeam', 'fierydance', 'geomancy', 'lightningrod', 'stormdrain', 'competitive', 'charge'];
 			let boostNonSpeedEI = ['habanberry', 'salacberry', 'whiteherb', 'rawstberry', 'ganlonberry', 'shucaberry', 'heavyball', 'damprock'];
-			boostNonSpeed = boostNonSpeed.concat(boostNonSpeedEI);
+			boostNonSpeed = boostNonSpeed.concat (boostNonSpeedEI);
 			if (!nonSpeedBoosted) {
 				for (let i = 0; i < boostNonSpeed.length; i++) {
 					if (boostNonSpeed[i] in setHas) {
