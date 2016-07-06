8d3f4441e6234bfd0bf1b2ffbb7005701fe5911f
Spandan7 <spandan.punwatkar@gmail.com>
2016-07-06 16:34:22 +0530

    Update Baton Pass Clause for Enchanted Items

--- mods/enchanteditems/rulesets.js
+++ mods/enchanteditems/rulesets.js
@@ -30,7 +30,9 @@ exports.BattleFormats = {
 					break;
 				}
 			}
-			let boostSpeed = ['flamecharge', 'geomancy', 'motordrive', 'rattled', 'speedboost', 'steadfast', 'weakarmor', 'cheriberry', 'duskball', 'friendball', 'skullfossil', 'weaknesspolicy'];
+			let boostSpeed = ['flamecharge', 'geomancy', 'motordrive', 'rattled', 'speedboost', 'steadfast', 'weakarmor'];
+			let boostSpeedEI = ['cheriberry', 'duskball', 'friendball', 'skullfossil', 'weaknesspolicy'];
+			boostSpeed=boostSpeed.concat(boostSpeedEI);
 			if (!speedBoosted) {
 				for (let i = 0; i < boostSpeed.length; i++) {
 					if (boostSpeed[i] in setHas) {
@@ -50,7 +52,9 @@ exports.BattleFormats = {
 					break;
 				}
 			}
-			let boostNonSpeed = ['acupressure', 'curse', 'metalclaw', 'meteormash', 'poweruppunch', 'rage', 'rototiller', 'fellstinger', 'bellydrum', 'download', 'justified', 'moxie', 'sapsipper', 'defiant', 'angerpoint', 'diamondstorm', 'flowershield', 'skullbash', 'steelwing', 'stockpile', 'cottonguard', 'chargebeam', 'fierydance', 'geomancy', 'lightningrod', 'stormdrain', 'competitive', 'charge', 'habanberry', 'salacberry', 'whiteherb', 'rawstberry', 'ganlonberry', 'shucaberry', 'heavyball', 'damprock'];
+			let boostNonSpeed = ['acupressure', 'curse', 'metalclaw', 'meteormash', 'poweruppunch', 'rage', 'rototiller', 'fellstinger', 'bellydrum', 'download', 'justified', 'moxie', 'sapsipper', 'defiant', 'angerpoint', 'diamondstorm', 'flowershield', 'skullbash', 'steelwing', 'stockpile', 'cottonguard', 'chargebeam', 'fierydance', 'geomancy', 'lightningrod', 'stormdrain', 'competitive', 'charge'];
+			let boostNonSpeedEI = ['habanberry', 'salacberry', 'whiteherb', 'rawstberry', 'ganlonberry', 'shucaberry', 'heavyball', 'damprock'];
+			boostNonSpeed = boostNonSpeed.concat(boostNonSpeedEI);
 			if (!nonSpeedBoosted) {
 				for (let i = 0; i < boostNonSpeed.length; i++) {
 					if (boostNonSpeed[i] in setHas) {
