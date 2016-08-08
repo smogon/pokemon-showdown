bd231476e4d495ffd5f7b179fbe4cdfd0849392e
panpawn <thebestintheworld12321@gmail.com>
2016-08-08 19:10:34 -0400

    Ban Swagger from Doubles OU (#2679)
    %kamikaze: http://www.smogon.com/forums/threads/np-doubles-ou-stage-4-infamous-jirachi-stays-in-dou-swagger-is-banned.3569913/page-10#post-6935868
    %kamikaze: Swagger has been banned from Doubles OU
    %kamikaze: so it would be great if someone can add Swagger Clause to it

--- config/formats.js
+++ config/formats.js
@@ -190,7 +190,7 @@ exports.Formats = [
 		section: "ORAS Doubles",
 
 		gameType: 'doubles',
-		ruleset: ['Pokemon', 'Standard Doubles', 'Team Preview'],
+		ruleset: ['Pokemon', 'Standard Doubles', 'Swagger Clause', 'Team Preview'],
 		banlist: ['Arceus', 'Dialga', 'Giratina', 'Giratina-Origin', 'Groudon', 'Ho-Oh', 'Kyogre', 'Kyurem-White', 'Lugia',
 			'Mewtwo', 'Palkia', 'Rayquaza', 'Reshiram', 'Salamence-Mega', 'Salamencite', 'Shaymin-Sky', 'Xerneas', 'Yveltal', 'Zekrom',
 			'Soul Dew', 'Dark Void', 'Gravity ++ Grass Whistle', 'Gravity ++ Hypnosis', 'Gravity ++ Lovely Kiss', 'Gravity ++ Sing', 'Gravity ++ Sleep Powder', 'Gravity ++ Spore',
