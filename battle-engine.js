293bcbff8941e01336cf37dcc6c6c146576ba980
asdfghjklohhnhn <ethan.bartiromo@yahoo.com>
2016-08-08 20:11:39 -0400

    Update battle-engine.js
    This should fix the Mega Evolution and Transform Issue

--- battle-engine.js
+++ battle-engine.js
@@ -601,6 +601,22 @@ BattlePokemon = (() => {
 	};
 	BattlePokemon.prototype.transformInto = function (pokemon, user, effect) {
 		let template = pokemon.template;
+	
+		if ((user && user.transformed && this.battle.gen >= 5)&&(BattleSide.prototype.chooseMove(willMega))) {
+			this.transformed = false;
+			this.types = this.types
+			this.addedType = this.addedType;
+			this.knownType = this.side === this.side && this.knownType;
+			for (let statName in this.stats) {
+				this.stats[statName] = this.stats[statName];
+			}
+			this.moveset = [];
+			this.moves = [];
+			this.set.ivs = (this.battle.gen >= 5 ? this.set.ivs : this.set.ivs);
+			this.hpType = (this.battle.gen >= 5 ? this.hpType : this.hpType);
+			this.hpPower = (this.battle.gen >= 5 ? this.hpPower : this.hpPower);
+			
+		}
 		if (pokemon.fainted || pokemon.illusion || (pokemon.volatiles['substitute'] && this.battle.gen >= 5)) {
 			return false;
 		}
@@ -675,7 +691,6 @@ BattlePokemon = (() => {
 				}
 			}
 		}
-
 		return true;
 	};
 	BattlePokemon.prototype.formeChange = function (template, dontRecalculateStats) {
