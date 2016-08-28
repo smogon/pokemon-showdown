209171b0a9e37b63a237df02f2985ef0fb8338d3
urkerab <urkerab@users.noreply.github.com>
2016-08-28 18:35:15 +0100

    -activate should include item: or move: prefix (#2724)

--- data/items.js
+++ data/items.js
@@ -959,7 +959,7 @@ exports.BattleItems = {
 		onModifyPriority: function (priority, pokemon) {
 			if (pokemon.hp <= pokemon.maxhp / 4 || (pokemon.hp <= pokemon.maxhp / 2 && pokemon.hasAbility('gluttony'))) {
 				if (pokemon.eatItem()) {
-					this.add('-activate', pokemon, 'Custap Berry');
+					this.add('-activate', pokemon, 'item: Custap Berry');
 					pokemon.removeVolatile('custapberry');
 					return Math.round(priority) + 0.1;
 				}
@@ -3895,7 +3895,7 @@ exports.BattleItems = {
 		},
 		onTryHit: function (pokemon, source, move) {
 			if (move.flags['powder'] && pokemon !== source) {
-				this.add('-activate', pokemon, 'Safety Goggles', move.name);
+				this.add('-activate', pokemon, 'item: Safety Goggles', move.name);
 				return null;
 			}
 		},
