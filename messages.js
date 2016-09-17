31768c40c914eac1b121d4800d06cdf8240ffee8
Guangcong Luo <guangcongluo@gmail.com>
2016-09-17 08:13:59 -0700

    Fix /ignore message in PMs

--- messages.js
+++ messages.js
@@ -65,8 +65,8 @@ Messages.send = function (target, context) {
 			break;
 		case 'ignore':
 		case 'unignore':
-			this.errorReply("This command can only be used by itself to ignore the person you're talking to: /" + innerCmd);
-			break;
+			context.errorReply(`This command can only be used by itself to ignore the person you're talking to: "/${innerCmd}", not "/${innerCmd} ${innerTarget}"`);
+			return;
 		case 'invite':
 		case 'inv': {
 			let targetRoom = Rooms.search(innerTarget);
