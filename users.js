e87c072fe81bdd83ebedbd5d267d15899585e95b
Guangcong Luo <guangcongluo@gmail.com>
2016-09-27 04:23:40 -0400

    Suppress crash in roomgame

--- users.js
+++ users.js
@@ -1413,7 +1413,13 @@ class User {
 	destroy() {
 		// deallocate user
 		this.games.forEach(roomid => {
-			let game = Rooms(roomid).game;
+			let room = Rooms(roomid);
+			if (!room) {
+				Monitor.warn(`while deallocating, room ${roomid} did not exist for ${this.userid} in rooms ${[...this.inRooms]} and games ${[...this.games]}`);
+				this.games.delete(roomid);
+				return;
+			}
+			let game = room.game;
 			if (!game) {
 				Monitor.warn(`while deallocating, room ${roomid} did not have a game for ${this.userid} in rooms ${[...this.inRooms]} and games ${[...this.games]}`);
 				this.games.delete(roomid);
