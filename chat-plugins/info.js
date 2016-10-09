8ab309ae740bd12688aee8b630a6f2875cffaf78
panpawn <panpawn@users.noreply.github.com>
2016-10-09 19:21:54 -0400

    Add missing linebreak in /opensource (#2833)

--- chat-plugins/info.js
+++ chat-plugins/info.js
@@ -972,7 +972,7 @@ exports.commands = {
 			"- Language: JavaScript (Node.js)<br />" +
 			"- <a href=\"https://github.com/Zarel/Pokemon-Showdown/commits/master\">What's new?</a><br />" +
 			"- <a href=\"https://github.com/Zarel/Pokemon-Showdown\">Server source code</a><br />" +
-			"- <a href=\"https://github.com/Zarel/Pokemon-Showdown-Client\">Client source code</a>" +
+			"- <a href=\"https://github.com/Zarel/Pokemon-Showdown-Client\">Client source code</a><br />" +
 			"- <a href=\"https://github.com/Zarel/Pokemon-Showdown-Dex\">Dex source code</a>"
 		);
 	},
