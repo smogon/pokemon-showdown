bd98c44c72b7c3232f7ab6634a92cc73dd105449
panpawn <panpawn@users.noreply.github.com>
2016-10-12 22:29:46 -0400

    Bold room notifications on room/global promotions
    [16:27:41] ~The Immortal: what if promotion messages were bold?? right now they don't stand out at all but promotions are pretty important.

--- chat-commands.js
+++ chat-commands.js
@@ -1025,7 +1025,7 @@ exports.commands = {
 		if (!room.auth) room.auth = room.chatRoomData.auth = {};
 
 		room.auth[userid] = '#';
-		this.addModCommand(`${name} was appointed Room Owner by ${user.name}.`);
+		this.addModCommand(`**${name} was appointed Room Owner by ${user.name}.**`);
 		if (targetUser) {
 			targetUser.popup(`You were appointed Room Owner by ${user.name} in ${room.id}.`);
 			room.onUpdateIdentity(targetUser);
@@ -1111,10 +1111,10 @@ exports.commands = {
 			this.privateModCommand("(" + name + " was demoted to Room " + groupName + " by " + user.name + ".)");
 			if (needsPopup) targetUser.popup("You were demoted to Room " + groupName + " by " + user.name + " in " + room.id + ".");
 		} else if (nextGroup === '#') {
-			this.addModCommand("" + name + " was promoted to " + groupName + " by " + user.name + ".");
+			this.addModCommand("**" + name + " was promoted to " + groupName + " by " + user.name + ".**");
 			if (needsPopup) targetUser.popup("You were promoted to " + groupName + " by " + user.name + " in " + room.id + ".");
 		} else {
-			this.addModCommand("" + name + " was promoted to Room " + groupName + " by " + user.name + ".");
+			this.addModCommand("**" + name + " was promoted to Room " + groupName + " by " + user.name + ".**");
 			if (needsPopup) targetUser.popup("You were promoted to Room " + groupName + " by " + user.name + " in " + room.id + ".");
 		}
 
@@ -1847,7 +1847,7 @@ exports.commands = {
 			this.privateModCommand("(" + name + " was demoted to " + groupName + " by " + user.name + ".)");
 			if (targetUser) targetUser.popup("You were demoted to " + groupName + " by " + user.name + ".");
 		} else {
-			this.addModCommand("" + name + " was promoted to " + groupName + " by " + user.name + ".");
+			this.addModCommand("**" + name + " was promoted to " + groupName + " by " + user.name + ".**");
 			if (targetUser) targetUser.popup("You were promoted to " + groupName + " by " + user.name + ".");
 		}
 
