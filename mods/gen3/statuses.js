71249f48fe2b7440542f29f440c8e85a70e8854c
Guangcong Luo <guangcongluo@gmail.com>
2016-11-14 09:16:40 -0700

    Make Gen 3 inherit from Gen 4
    Due to a quirk of history (namely, programmers having a poor
    understanding of the mod inheritance system), Gen 3 inherited from
    Gen 5 for a long time, resulting in a lot of duplicate code.
    
    This commit changes up the inheritance system and removes some
    duplicate code (specifically, statuses.js and moves.js besides
    Metronome and Mirror Move which need more investigation), the rest is
    reserved for a future commit.

--- mods/gen3/statuses.js
+++ mods/gen3/statuses.js
@@ -35,44 +35,14 @@ exports.BattleStatuses = {
 	},
 	frz: {
 		inherit: true,
-		onBeforeMove: function (pokemon, target, move) {
-			if (this.random(5) === 0) {
-				pokemon.cureStatus();
-				return;
-			}
-			if (move.flags['defrost']) return;
-			this.add('cant', pokemon, 'frz');
-			return false;
-		},
 		onHit: function (target, source, move) {
 			if (move.thawsTarget || move.type === 'Fire' && move.category !== 'Status' && move.id !== 'hiddenpower' && move.id !== 'weatherball') {
 				target.cureStatus();
 			}
 		},
 	},
-	trapped: {
-		inherit: true,
-		noCopy: false,
-	},
-	trapper: {
-		inherit: true,
-		noCopy: false,
-	},
-	partiallytrapped: {
-		inherit: true,
-		durationCallback: function (target, source) {
-			if (source.hasItem('gripclaw')) return 6;
-			return this.random(3, 7);
-		},
-	},
 	sandstorm: {
 		inherit: true,
 		onModifySpD: function () { },
-	},
-	stall: {
-		// In gen 3, the chance of protect succeeding does not fall below 1/8.
-		// See http://upokecenter.dreamhosters.com/dex/?lang=en&move=182
-		inherit: true,
-		counterMax: 8,
 	},
 };
