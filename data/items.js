e97ccb3d4874742b14f85514402377c41a8e2e76
Ivo Julca <ivojulca@hotmail.com>
2016-11-18 13:55:49 -0500

    Emergency Exit activation quirks are similar to Eject Button and Red Card
    In particular, it only activates after all hits in a multi-hit move are performed, and
    it won't activate if the move is boosted by Sheer Force.
    
    This commit also implements the interactions of Emergency Exit
    with Eject Button and Red Card, and adds tests.

--- data/items.js
+++ data/items.js
@@ -1383,6 +1383,7 @@ exports.BattleItems = {
 		fling: {
 			basePower: 30,
 		},
+		onAfterMoveSecondaryPriority: 2,
 		onAfterMoveSecondary: function (target, source, move) {
 			if (source && source !== target && target.hp && move && move.category !== 'Status') {
 				if (!this.canSwitch(target.side) || target.forceSwitchFlag) return;
