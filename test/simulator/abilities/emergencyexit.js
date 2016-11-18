ac05470d0d3eaae6a562e331950d7646337cb8d9
Ivo Julca <ivojulca@hotmail.com>
2016-11-18 15:10:51 -0500

    Fix Emergency Exit: It should block Volt Switch effect

--- test/simulator/abilities/emergencyexit.js
+++ test/simulator/abilities/emergencyexit.js
@@ -34,6 +34,23 @@ describe(`Emergency Exit`, function () {
 		assert.strictEqual(battle.currentRequest, 'move');
 	});
 
+	it(`should prevent Volt Switch after-switches`, function () {
+		battle = common.createBattle([
+			[{species: "Golisopod", ability: 'emergencyexit', moves: ['sleeptalk'], ivs: EMPTY_IVS}, {species: "Clefable", ability: 'Unaware', moves: ['metronome']}],
+			[{species: "Zekrom", ability: 'pressure', moves: ['voltswitch']}, {species: "Clefable", ability: 'Unaware', moves: ['metronome']}],
+		]);
+		const eePokemon = battle.p1.active[0];
+		battle.commitDecisions();
+		assert.atMost(eePokemon.hp, eePokemon.maxhp / 2);
+
+		assert.false.holdsItem(eePokemon);
+		assert.strictEqual(battle.currentRequest, 'switch');
+
+		battle.commitDecisions();
+		assert.species(battle.p1.active[0], 'Clefable');
+		assert.species(battle.p2.active[0], 'Zekrom');
+	});
+
 	it(`should not prevent Red Card's activation`, function () {
 		battle = common.createBattle([
 			[{species: "Golisopod", ability: 'emergencyexit', item: 'redcard', moves: ['sleeptalk'], ivs: EMPTY_IVS}, {species: "Clefable", ability: 'Unaware', moves: ['metronome']}],
