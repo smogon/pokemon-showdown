32412e5524f06783b9d65a2d276f8fa7a183385f
Ivo Julca <ivojulca@hotmail.com>
2016-11-17 20:50:34 -0500

    Refactor EV/IV/Nature stat modifiers to Tools#spreadModify

--- tools.js
+++ tools.js
@@ -673,6 +673,18 @@ class BattleDex {
 		}
 		return nature;
 	}
+	spreadModify(stats, set) {
+		const modStats = {atk:10, def:10, spa:10, spd:10, spe:10};
+		for (let statName in modStats) {
+			let stat = stats[statName];
+			modStats[statName] = Math.floor(Math.floor(2 * stat + set.ivs[statName] + Math.floor(set.evs[statName] / 4)) * set.level / 100 + 5);
+		}
+		if ('hp' in stats) {
+			let stat = stats['hp'];
+			modStats['hp'] = Math.floor(Math.floor(2 * stat + set.ivs['hp'] + Math.floor(set.evs['hp'] / 4) + 100) * set.level / 100 + 10);
+		}
+		return this.natureModify(modStats, set.nature);
+	}
 	natureModify(stats, nature) {
 		nature = this.getNature(nature);
 		if (nature.plus) stats[nature.plus] *= 1.1;
