1ad403807033cda2294706006a6fbab24fdc0d28
Marty-D <Marty-D@users.noreply.github.com>
2016-11-19 15:05:47 -0500

    Fix Battery modifier

--- data/abilities.js
+++ data/abilities.js
@@ -229,7 +229,7 @@ exports.BattleAbilities = {
 		onAllyBasePower: function (basePower, attacker, defender, move) {
 			if (attacker !== this.effectData.target && move.category === 'Special') {
 				this.debug('Battery boost');
-				return this.chainModify(1.3);
+				return this.chainModify([0x14CD, 0x1000]);
 			}
 		},
 		id: "battery",
