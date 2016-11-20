d7d121b9e575a68ddfdc00906e1cd6af0c222326
urkerab <urkerab@users.noreply.github.com>
2016-11-20 15:14:37 +0000

    Ash Greninja and Zygarde 10% and Complete should be Gen 7

--- data/formats-data.js
+++ data/formats-data.js
@@ -5296,6 +5296,7 @@ exports.BattleFormatsData = {
 		tier: "OU",
 	},
 	greninjaash: {
+		gen: 7,
 		requiredAbility: "Battle Bond",
 		battleOnly: true,
 	},
@@ -5680,9 +5681,11 @@ exports.BattleFormatsData = {
 			{"generation": 7, "level": 30, "moves":["safeguard", "dig", "bind", "landswrath"]},
 		],
 		eventOnly: true,
+		gen: 7,
 		tier: "OU",
 	},
 	zygardecomplete: {
+		gen: 7,
 		requiredAbility: "Power Construct",
 		battleOnly: true,
 	},
