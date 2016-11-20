2ea40170f6cf11040f8e958d61e31c8d9ac44e07
Marty-D <Marty-D@users.noreply.github.com>
2016-11-20 08:58:32 -0500

    Fix Assist check

--- data/moves.js
+++ data/moves.js
@@ -639,7 +639,7 @@ exports.BattleMovedex = {
 					let noAssist = {
 						assist:1, belch:1, bestow:1, bounce:1, chatter:1, circlethrow:1, copycat:1, counter:1, covet:1, destinybond:1, detect:1, dig:1, dive:1, dragontail:1, endure:1, feint:1, fly:1, focuspunch:1, followme:1, helpinghand:1, kingsshield:1, matblock:1, mefirst:1, metronome:1, mimic:1, mirrorcoat:1, mirrormove:1, naturepower:1, phantomforce:1, protect:1, ragepowder:1, roar:1, shadowforce:1, sketch:1, skydrop:1, sleeptalk:1, snatch:1, spikyshield:1, struggle:1, switcheroo:1, thief:1, transform:1, trick:1, whirlwind:1,
 					};
-					if (!noAssist[move] || !move.isZ) {
+					if (!noAssist[move] && !move.isZ) {
 						moves.push(move);
 					}
 				}
