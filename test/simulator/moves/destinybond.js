dd5fb4f76fc28d3f454138264b96df51e7969e74
Ivo Julca <ivojulca@hotmail.com>
2016-11-20 02:59:32 -0500

    Test: Fix Destiny Bond specification
    It should fail deterministically every second time it's used.

--- test/simulator/moves/destinybond.js
+++ test/simulator/moves/destinybond.js
@@ -6,11 +6,9 @@ const common = require('./../../common');
 let battle;
 
 describe(`Destiny Bond`, function () {
-	afterEach(function () {
-		battle.destroy();
-	});
+	afterEach(() => battle.destroy());
 
-	it(`should have a chance to succeed on repeated usage`, function () {
+	it.skip(`should fail if used consecutively`, function () {
 		battle = common.createBattle([
 			[{species: "Gastly", ability: 'levitate', moves: ['destinybond']}, {species: "Clefable", ability: 'unaware', moves: ['calmmind']}],
 			[{species: "Metagross", ability: 'clearbody', moves: ['psychic', 'calmmind']}, {species: "Clefable", ability: 'unaware', moves: ['calmmind']}],
@@ -19,15 +17,13 @@ describe(`Destiny Bond`, function () {
 		battle.seed = common.minRollSeed;
 		battle.p1.chooseMove('destinybond').foe.chooseMove('psychic');
 		assert.fainted(battle.p1.active[0]);
-		assert.fainted(battle.p2.active[0]);
-	});
+		assert.false.fainted(battle.p2.active[0]);
 
-	it(`should have a chance to fail on repeated usage`, function () {
+		battle.destroy();
 		battle = common.createBattle([
 			[{species: "Gastly", ability: 'levitate', moves: ['destinybond']}, {species: "Clefable", ability: 'unaware', moves: ['calmmind']}],
 			[{species: "Metagross", ability: 'clearbody', moves: ['psychic', 'calmmind']}, {species: "Clefable", ability: 'unaware', moves: ['calmmind']}],
 		]);
-		assert.equal(battle.turn, 1);
 		battle.p1.chooseMove('destinybond').foe.chooseMove('calmmind');
 		battle.seed = common.maxRollSeed;
 		battle.p1.chooseMove('destinybond').foe.chooseMove('psychic');
@@ -41,6 +37,33 @@ describe(`Destiny Bond`, function () {
 			[{species: "Metagross", ability: 'clearbody', moves: ['psychic', 'calmmind']}, {species: "Clefable", ability: 'unaware', moves: ['calmmind']}],
 		]);
 		battle.p1.chooseMove('protect').foe.chooseMove('calmmind');
+		battle.seed = common.maxRollSeed;
+		battle.p1.chooseMove('destinybond').foe.chooseMove('psychic');
+		assert.fainted(battle.p1.active[0]);
+		assert.fainted(battle.p2.active[0]);
+	});
+});
+
+describe(`Destiny Bond [Gen 6]`, function () {
+	afterEach(() => battle.destroy());
+
+	it(`should not fail if used consecutively`, function () {
+		battle = common.gen(6).createBattle([
+			[{species: "Gastly", ability: 'levitate', moves: ['destinybond']}, {species: "Clefable", ability: 'unaware', moves: ['calmmind']}],
+			[{species: "Metagross", ability: 'clearbody', moves: ['psychic', 'calmmind']}, {species: "Clefable", ability: 'unaware', moves: ['calmmind']}],
+		]);
+		battle.p1.chooseMove('destinybond').foe.chooseMove('calmmind');
+		battle.seed = common.minRollSeed;
+		battle.p1.chooseMove('destinybond').foe.chooseMove('psychic');
+		assert.fainted(battle.p1.active[0]);
+		assert.fainted(battle.p2.active[0]);
+
+		battle.destroy();
+		battle = common.gen(6).createBattle([
+			[{species: "Gastly", ability: 'levitate', moves: ['destinybond']}, {species: "Clefable", ability: 'unaware', moves: ['calmmind']}],
+			[{species: "Metagross", ability: 'clearbody', moves: ['psychic', 'calmmind']}, {species: "Clefable", ability: 'unaware', moves: ['calmmind']}],
+		]);
+		battle.p1.chooseMove('destinybond').foe.chooseMove('calmmind');
 		battle.seed = common.maxRollSeed;
 		battle.p1.chooseMove('destinybond').foe.chooseMove('psychic');
 		assert.fainted(battle.p1.active[0]);
