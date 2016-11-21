f53f6132aa62e734341cf177754e3c46a64c50dd
Bär Halberkamp <fledaway@gmail.com>
2016-11-22 00:45:57 +0100

    Fix gen 7 mons in giveaways

--- chat-plugins/wifi.js
+++ chat-plugins/wifi.js
@@ -108,6 +108,7 @@ class Giveaway {
 		text = toId(text);
 		if (mons.size) {
 			mons.forEach(function (value, key) {
+				let useBWSprites = value.gen === 7;
 				let spriteid = value.spriteid;
 				if (value.otherForms) {
 					for (let i = 0; i < value.otherForms.length; i++) {
@@ -119,6 +120,14 @@ class Giveaway {
 				}
 				if (value.otherFormes) {
 					for (let i = 0; i < value.otherFormes.length; i++) {
+						// Hardcore alola formes.
+						if (value.otherFormes[i].endsWith('alola')) {
+							if (/alolan?/.test(text)) {
+								spriteid += '-alolan';
+								useBWSprites = true;
+								break;
+							}
+						}
 						if (text.includes(value.otherFormes[i])) {
 							spriteid += '-' + value.otherFormes[i].substr(key.length);
 							break; // We don't want to end up with landorus-therian-therian
@@ -131,7 +140,7 @@ class Giveaway {
 					output += `<div style="display:inline-block;width:40px;height:30px;background:transparent url('/sprites/xyicons-sheet.png?a1') no-repeat scroll -${left}px -${top}px'"></div>`;
 				} else {
 					let shiny = (text.includes("shiny") && !text.includes("shinystone") ? '-shiny' : '');
-					output += `<img src="/sprites/xyani${shiny}/${spriteid}.gif">`;
+					output += `<img src="/sprites/${useBWSprites ? 'bw' : 'xyani'}${shiny}/${spriteid}.${useBWSprites ? 'png' : 'gif'}">`;
 				}
 			});
 		}
