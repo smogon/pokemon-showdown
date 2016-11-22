d9846e4043aef398739e58f5fd9435536ea9aeed
The Immortal <the_immortal123@live.com>
2016-11-22 15:35:44 +0400

    Ban Power Construct from OU

--- config/formats.js
+++ config/formats.js
@@ -32,7 +32,7 @@ exports.Formats = [
 
 		mod: 'gen7',
 		ruleset: ['Pokemon', 'Standard', 'Team Preview', 'Baton Pass Clause'],
-		banlist: ['Uber', 'Shadow Tag'],
+		banlist: ['Uber', 'Power Construct', 'Shadow Tag'],
 		requirePentagon: true,
 	},
 	{
@@ -44,7 +44,7 @@ exports.Formats = [
 
 		mod: 'gen7',
 		ruleset: ['Pokemon', 'Standard', 'Team Preview', 'Baton Pass Clause'],
-		banlist: ['Uber', 'Bank-Uber', 'Shadow Tag'],
+		banlist: ['Uber', 'Bank-Uber', 'Power Construct', 'Shadow Tag'],
 	},
 	{
 		name: "[Gen 7] Pokebank Ubers",
