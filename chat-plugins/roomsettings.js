848fcd2c2b88685cdcbb6dee4e8e226129ca745d
Lionyx <choloux3@gmail.com>
2016-11-23 00:01:25 +0100

    Roomsettings: Change variable spelling (#2935)
    minor syntaxic change "atuh" → "auth"

--- chat-plugins/roomsettings.js
+++ chat-plugins/roomsettings.js
@@ -45,10 +45,10 @@ class RoomSettings {
 			} else if (RANKS[i]) {
 				let rankIndex = RANKS.indexOf(RANKS[i]);
 				let roomAuth = (this.room.auth && this.room.auth[this.user.userid] ? this.room.auth[this.user.userid] : false);
-				let roomAtuhIndex = (roomAuth ? RANKS.indexOf(roomAuth) : false);
+				let roomAuthIndex = (roomAuth ? RANKS.indexOf(roomAuth) : false);
 				if (rankIndex > 1 && !this.user.can('modchatall', null, this.room)) continue;
 				if (roomAuth && !this.user.can('bypassall')) {
-					if (rankIndex > roomAtuhIndex) continue;
+					if (rankIndex > roomAuthIndex) continue;
 				}
 				modchatOutput.push(this.button(RANKS[i], null, `modchat ${RANKS[i]}`));
 			}
