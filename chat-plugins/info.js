29df34d2587044c5da4c50d1a5c43588d09aa255
InactiveUser <anakin.10@hotmail.com>
2016-11-22 14:01:58 -0500

    Reword htmlbox help (#2910)
    * Reword htmlbox help
    
    I think the way it was before kind of confusing, especially with the not rank part, hopefully this helps?
    
    * Removed *

--- chat-plugins/info.js
+++ chat-plugins/info.js
@@ -1836,7 +1836,7 @@ exports.commands = {
 	},
 	htmlboxhelp: [
 		"/htmlbox [message] - Displays a message, parsing HTML code contained.",
-		"!htmlbox [message] - Shows everyone a message, parsing HTML code contained. Requires: ~ & * with global authority OR # * with room authority",
+		"!htmlbox [message] - Shows everyone a message, parsing HTML code contained. Requires: ~ & #",
 	],
 };
 
