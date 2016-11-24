0876a18cdfc9b0a63874dfd0d55e5d5a9b474f6d
Marty-D <Marty-D@users.noreply.github.com>
2016-11-23 22:37:25 -0500

    Update Big Root with Strength Sap

--- data/items.js
+++ data/items.js
@@ -419,7 +419,7 @@ exports.BattleItems = {
 		},
 		onTryHealPriority: 1,
 		onTryHeal: function (damage, target, source, effect) {
-			let heals = {drain: 1, leechseed: 1, ingrain: 1, aquaring: 1};
+			let heals = {drain: 1, leechseed: 1, ingrain: 1, aquaring: 1, strengthsap: 1};
 			if (heals[effect.id]) {
 				return Math.ceil((damage * 1.3) - 0.5); // Big Root rounds half down
 			}
