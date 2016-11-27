94e42efe726e115a8365f25b287013a16dc7400f
Ivo Julca <ivojulca@hotmail.com>
2016-11-27 00:23:19 -0500

    Trim megaOrZ argument of BattleSide#chooseMove()

--- test/simulator/moves/zmove.js
+++ test/simulator/moves/zmove.js
@@ -10,11 +10,11 @@ describe('Z-Moves', function () {
 		battle.destroy();
 	});
 
-	it('should not use the base move\'s priority if it is a damaging move', function () {
+	it(`should not use the base move's priority if it is a damaging move`, function () {
 		battle = common.createBattle();
 		battle.join('p1', 'Guest 1', 1, [{species: 'Kecleon', ability: 'colorchange', item: 'ghostiumz', moves: ['shadowsneak']}]);
 		battle.join('p2', 'Guest 2', 1, [{species: 'Starmie', ability: 'naturalcure', moves: ['reflecttype']}]);
-		battle.p1.chooseMove('shadowsneak', 0, ' zmove').foe.chooseDefault();
+		battle.p1.chooseMove('shadowsneak', 0, 'zmove').foe.chooseDefault();
 		assert.fullHP(battle.p2.active[0]);
 	});
 });
