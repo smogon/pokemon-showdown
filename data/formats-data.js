8a34ecf14fdb83ebe905557401c7377661420245
MacChaeger <msaimrkon@gmail.com>
2016-11-30 09:37:24 -0600

    Update formats-data.js

--- data/formats-data.js
+++ data/formats-data.js
@@ -5679,7 +5679,6 @@ exports.BattleFormatsData = {
 			{"generation": 6, "level": 100, "moves":["landswrath", "extremespeed", "glare", "outrage"], "pokeball": "cherishball"},
 			{"generation": 7, "level": 50, "moves":["bind", "landswrath", "sandstorm", "haze"]},
 			{"generation": 7, "level": 50, "isHidden": true, "moves":["bind", "landswrath", "sandstorm", "haze"]},
-			{"generation": 7, "level": 50, "moves":["bind", "landswrath", "sandstorm", "haze"]},
 		],
 		eventOnly: true,
 		tier: "New",
@@ -5687,13 +5686,8 @@ exports.BattleFormatsData = {
 	zygarde10: {
 		randomBattleMoves: ["dragondance", "thousandarrows", "outrage", "extremespeed", "irontail", "substitute"],
 		eventPokemon: [
-<<<<<<< HEAD
 			{"generation": 7, "level": 30, "moves":["safeguard", "dig", "bind", "landswrath"]},
 			{"generation": 7, "level": 50, "isHidden": true, "moves":["safeguard", "dig", "bind", "landswrath"]},
-=======
-			{"generation": 7, "level": 50, "isHidden": true, "moves":["bind", "landswrath", "sandstorm", "haze"]},
-			{"generation": 7, "level": 30, "moves":["safeguard", "dig", "bind", "landswrath"]},
->>>>>>> origin/master
 		],
 		eventOnly: true,
 		gen: 7,
