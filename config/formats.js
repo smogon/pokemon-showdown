9282967ec25533cea405736506929fdb3c05ee9a
The Immortal <the_immortal123@live.com>
2016-12-01 10:08:32 +0400

    Add Doubles Ubers ladder

--- config/formats.js
+++ config/formats.js
@@ -181,7 +181,6 @@ exports.Formats = [
 
 		mod: 'gen7',
 		gameType: 'doubles',
-		searchShow: false,
 		ruleset: ['Pokemon', 'Species Clause', 'Moody Clause', 'OHKO Clause', 'Evasion Moves Clause', 'Evasion Abilities Clause', 'Endless Battle Clause', 'HP Percentage Mod', 'Cancel Mod', 'Team Preview'],
 		banlist: ['Illegal', 'Unreleased'],
 	},
