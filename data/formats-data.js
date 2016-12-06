7c74eca02c9c02fcaf159043966ac6c1c0322254
Kevin Lau <ascriptmaster@gmail.com>
2016-12-06 11:27:35 -0800

    Random Battle: Update Pyukumuku

--- data/formats-data.js
+++ data/formats-data.js
@@ -5943,7 +5943,7 @@ exports.BattleFormatsData = {
 		tier: "New",
 	},
 	pyukumuku: {
-		randomBattleMoves: ["curse", "batonpass", "recover", "counter", "reflect", "lightscreen"],
+		randomBattleMoves: ["toxic", "recover", "counter", "reflect", "lightscreen"],
 		tier: "New",
 	},
 	typenull: {
