4106821db73a92e7fd3fef1fa22953d67b036bbd
MacChaeger <msaimrkon@gmail.com>
2016-12-08 21:52:04 -0600

    Random Battle updates
    Fix levels for unreleased Ubers and Power Construct Zygarde

--- data/scripts.js
+++ data/scripts.js
@@ -2197,6 +2197,7 @@ exports.BattleScripts = {
 			// Blaziken: 74, 'Blaziken-Mega': 74, 'Lucario-Mega': 74,
 
 			// Banned Ability
+			Zygarde: 73
 			// Gothitelle: 74, Wobbuffet: 74,
 
 			// Holistic judgement
@@ -2207,8 +2208,11 @@ exports.BattleScripts = {
 			tier = tier.slice(1, -1);
 		}
 		let level = levelScale[tier] || 75;
+		if (tier === 'Unreleased') {
+			let baseSpeciesTemplate = Tools.getTemplate(template.basespecies);
+			level = levelScale[baseSpeciesTemplate.tier] || 75;
 		if (customScale[template.name]) level = customScale[template.name];
-
+		
 		// if (template.name === 'Slurpuff' && !counter.setupType) level = 81;
 		// if (template.name === 'Xerneas' && hasMove['geomancy']) level = 71;
 
