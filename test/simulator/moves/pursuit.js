d1de1e0c3947ce30ee630b116bca1c727887a349
Guangcong Luo <guangcongluo@gmail.com>
2016-12-21 19:36:23 -0800

    Fix Mega Pursuit
    Previously, if a pokemon mega evolved and used Pursuit, and the foe
    switched on the next turn, it would use Pursuit again on the next turn,
    in addition to its usual decision.
    
    This is a hack to fix it; a full fix is pending a rewrite of the
    decision structure.

--- test/simulator/moves/pursuit.js
+++ test/simulator/moves/pursuit.js
@@ -17,4 +17,17 @@ describe(`Pursuit`, function () {
 		assert.species(battle.p1.active[0], "Beedrill-Mega");
 		assert.fainted(battle.p2.active[0]);
 	});
+
+	it(`should not repeat`, function () {
+		battle = common.createBattle();
+		battle.join('p1', 'Guest 1', 1, [{species: "Beedrill", ability: 'swarm', item: 'beedrillite', moves: ['pursuit']}, {species: "Clefable", ability: 'unaware', moves: ['calmmind']}]);
+		battle.join('p2', 'Guest 2', 1, [{species: "Clefable", ability: 'magicguard', moves: ['calmmind']}, {species: "Alakazam", ability: 'unaware', moves: ['calmmind']}]);
+		battle.p1.chooseMove('pursuit', null, true);
+		battle.p2.chooseMove('calmmind');
+		let clefable = battle.p2.pokemon[0];
+		let hpBeforeSwitch = clefable.hp;
+		battle.p1.chooseSwitch(2);
+		battle.p2.chooseSwitch(2);
+		assert.strictEqual(hpBeforeSwitch, clefable.hp);
+	});
 });
