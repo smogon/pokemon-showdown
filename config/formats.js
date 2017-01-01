d57688a28bcf92e5024f01ca2457a03d2ecf22b5
The Immortal <the_immortal123@live.com>
2017-01-01 18:00:48 +0400

    Update formats.js

--- config/formats.js
+++ config/formats.js
@@ -262,8 +262,7 @@ exports.Formats = [
 						if (toId(template.abilities[a]) === pokemon.ability) continue;
 						if (this.statusability.includes(toId(template.abilities[a]))) {
 							this.sides[p].pokemon[i].innates.push("other" + toId(template.abilities[a]));
-						}
-						else {
+						} else {
 							this.sides[p].pokemon[i].innates.push(toId(template.abilities[a]));
 						}
 					}
