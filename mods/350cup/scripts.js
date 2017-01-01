1659993dc0b388099534872c67d6100b86473a0b
Spandan <spandan.punwatkar@gmail.com>
2017-01-01 20:16:33 +0530

    Add new OMs of the month (#3062)
    * Add new OMs of the month

---
+++ mods/350cup/scripts.js
@@ -0,0 +1,14 @@
+'use strict';
+
+exports.BattleScripts = {
+	init: function () {
+		for (let i in this.data.Pokedex) {
+			if (Object.values(this.data.Pokedex[i].baseStats).sum() <= 350) {
+				for (let j in this.data.Pokedex[i].baseStats) {
+					this.modData('Pokedex', i).baseStats[j] = this.data.Pokedex[i].baseStats[j] * 2;
+				}
+			}
+		}
+		this.modData('Pokedex', 'shedinja').baseStats['hp'] = 1;
+	},
+};
