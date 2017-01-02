e532c25d16e58f9039b8f6eb8583545959132875
The Immortal <the_immortal123@live.com>
2017-01-02 05:54:16 +0400

    Fix 350 Cup

--- mods/350cup/scripts.js
+++ mods/350cup/scripts.js
@@ -3,7 +3,7 @@
 exports.BattleScripts = {
 	init: function () {
 		for (let i in this.data.Pokedex) {
-			if (Object.values(this.data.Pokedex[i].baseStats).sum() <= 350) {
+			if (Object.values(this.data.Pokedex[i].baseStats).reduce((x, y) => x + y) <= 350) {
 				for (let j in this.data.Pokedex[i].baseStats) {
 					this.modData('Pokedex', i).baseStats[j] = this.data.Pokedex[i].baseStats[j] * 2;
 				}
