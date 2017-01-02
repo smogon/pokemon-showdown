9417073c2499464abd87ad8cfe784eb899838cbc
Tigerstar <Tigerball890@users.noreply.github.com>
2017-01-02 14:39:26 -0500

    Update Embargo's short desc (#3065)

--- data/moves.js
+++ data/moves.js
@@ -4315,7 +4315,7 @@ exports.BattleMovedex = {
 		basePower: 0,
 		category: "Status",
 		desc: "For 5 turns, the target's held item has no effect. An item's effect of causing forme changes is unaffected, but any other effects from such items are negated. During the effect, Fling and Natural Gift are prevented from being used by the target. Items thrown at the target with Fling will still activate for it. If the target uses Baton Pass, the replacement will remain unable to use items.",
-		shortDesc: "For 5 turns, the target can't use any items.",
+		shortDesc: "For 5 turns, the target's item has no effect.",
 		id: "embargo",
 		name: "Embargo",
 		pp: 15,
