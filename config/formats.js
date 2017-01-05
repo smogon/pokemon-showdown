54ab129eff8184f4c974e868fb7f1d204f70d464
The Immortal <the_immortal123@live.com>
2017-01-05 22:23:48 +0400

    RU: Ban correct Mega Charizard

--- config/formats.js
+++ config/formats.js
@@ -82,7 +82,7 @@ exports.Formats = [
 			'Infernape', 'Jirachi', 'Keldeo', 'Krookodile', 'Kyurem-Black', 'Latias', 'Magneton', 'Mamoswine', 'Mandibuzz', 'Mew', 'Muk-Alola',
 			'Necrozma', 'Porygon-Z', 'Primarina', 'Raikou', 'Salamence', 'Scizor', 'Serperior', 'Starmie', 'Staraptor', 'Sylveon', 'Tentacruel',
 			'Terrakion', 'Thundurus', 'Tornadus-Therian', 'Tsareena', 'Victini', 'Volcanion', 'Volcarona', 'Weavile', 'Zygarde-10%',
-			'Aerodactylite', 'Blastoisinite', 'Charizardite X', 'Sharpedonite', 'Slowbronite',
+			'Aerodactylite', 'Blastoisinite', 'Charizardite Y', 'Sharpedonite', 'Slowbronite',
 		],
 	},
 	{
