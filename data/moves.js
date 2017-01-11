f2f89836b58938e69d3937cde6f753056961bf52
Marty-D <Marty-D@users.noreply.github.com>
2017-01-10 23:13:15 -0500

    Update Quick Guard bypass check

--- data/moves.js
+++ data/moves.js
@@ -12725,11 +12725,9 @@ exports.BattleMovedex = {
 			onTryHit: function (target, source, move) {
 				// Quick Guard blocks moves with positive priority, even those given increased priority by Prankster or Gale Wings.
 				// (e.g. it blocks 0 priority moves boosted by Prankster or Gale Wings; Quick Claw/Custap Berry do not count)
-				if (move.id === 'feint' || move.priority <= 0.1 || move.target === 'self') {
-					return;
-				}
-				if (move.isZ) {
-					move.zBrokeProtect = true;
+				if (move.priority <= 0.1) return;
+				if (!move.flags['protect']) {
+					if (move.isZ) move.zBrokeProtect = true;
 					return;
 				}
 				this.add('-activate', target, 'move: Quick Guard');
