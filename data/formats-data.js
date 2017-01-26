4e46b1f08b2a5c4dd355c71e600d0ccc0e4f3958
The Immortal <the_immortal123@live.com>
2017-01-26 23:59:45 +0400

    Random Battle: Update Pyukumuku

--- data/formats-data.js
+++ data/formats-data.js
@@ -5951,7 +5951,7 @@ exports.BattleFormatsData = {
 		tier: "New",
 	},
 	pyukumuku: {
-		randomBattleMoves: ["curse", "batonpass", "recover", "counter", "reflect", "lightscreen"],
+		randomBattleMoves: ["batonpass", "counter", "reflect", "lightscreen"],
 		tier: "New",
 	},
 	typenull: {
