40663070779bcf1ce13bb229ca9fe129f131217b
spekss <speksss1@gmail.com>
2017-02-16 06:37:56 +0100

    Add bold in /punishments (#3236)

--- chat-plugins/info.js
+++ chat-plugins/info.js
@@ -1083,18 +1083,18 @@ exports.commands = {
 	punishments: function (target, room, user) {
 		if (!this.runBroadcast()) return;
 		this.sendReplyBox(
-			"<b>Room punishments</b>:<br />" +
-			"warn - Displays a popup with the rules.<br />" +
-			"mute - Mutes a user (makes them unable to talk) for 7 minutes.<br />" +
-			"hourmute - Mutes a user for 60 minutes.<br />" +
-			"ban - Bans a user (makes them unable to join the room) for 2 days.<br />" +
-			"blacklist - Bans a user for a year.<br />" +
+			"<strong>Room punishments</strong>:<br />" +
+			"<strong>warn</strong> - Displays a popup with the rules.<br />" +
+			"<strong>mute</strong> - Mutes a user (makes them unable to talk) for 7 minutes.<br />" +
+			"<strong>hourmute</strong> - Mutes a user for 60 minutes.<br />" +
+			"<strong>ban</strong> - Bans a user (makes them unable to join the room) for 2 days.<br />" +
+			"<strong>blacklist</strong> - Bans a user for a year.<br />" +
 			"<br />" +
-			"<b>Global punishments</b>:<br />" +
-			"lock - Locks a user (makes them unable to talk in any rooms or PM non-staff) for 2 days.<br />" +
-			"weeklock - Locks a user for a week.<br />" +
-			"namelock - Locks a user and prevents them from having a username for 2 days.<br />" +
-			"globalban - Globally bans (makes them unable to connect and play games) for a week."
+			"<strong>Global punishments</strong>:<br />" +
+			"<strong>lock</strong> - Locks a user (makes them unable to talk in any rooms or PM non-staff) for 2 days.<br />" +
+			"<strong>weeklock</strong> - Locks a user for a week.<br />" +
+			"<strong>namelock</strong> - Locks a user and prevents them from having a username for 2 days.<br />" +
+			"<strong>globalban</strong> - Globally bans (makes them unable to connect and play games) for a week."
 		);
 	},
 	punishmentshelp: ["/punishments - Explains punishments.",
