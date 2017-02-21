0f8241953b3e77ab8ca70e20ba15c336a7d95da9
Bär Halberkamp <fledaway@gmail.com>
2017-02-21 14:48:54 +0100

    FAQs: sort /roomfaq topic list alphabetically

--- chat-plugins/room-faqs.js
+++ chat-plugins/room-faqs.js
@@ -91,7 +91,7 @@ exports.commands = {
 		if (!roomFaqs[room.id]) return this.errorReply("This room has no FAQ topics.");
 		let topic = toId(target);
 		if (topic === 'constructor') return false;
-		if (!topic) return this.sendReplyBox(`List of topics in this room: ${Object.keys(roomFaqs[room.id]).filter(val => !getAlias(room.id, val)).join(', ')}`);
+		if (!topic) return this.sendReplyBox(`List of topics in this room: ${Object.keys(roomFaqs[room.id]).filter(val => !getAlias(room.id, val)).sort((a, b) => a.localeCompare(b)).join(', ')}`);
 		if (!roomFaqs[room.id][topic]) return this.errorReply("Invalid topic.");
 		topic = getAlias(room.id, topic) || topic;
 
