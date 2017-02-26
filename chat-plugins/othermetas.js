5692206b3e5f2946f56d5adce9809095a128ac5c
Spandan Punwatkar <spandan.punwatkar@gmail.com>
2017-02-26 15:38:24 +0530

    Update othermetas.js

--- chat-plugins/othermetas.js
+++ chat-plugins/othermetas.js
@@ -79,25 +79,26 @@ exports.commands = {
 	mixandmegahelp: ["/mnm <pokemon> @ <mega stone> - Shows the mix and mega evolved Pokemon's type and stats."],
 	
 	'350' : 'cup350',
-	cup350 : function(target, room, user) {
+	cup350 : function (target, room, user) {
 		if (!this.runBroadcast()) return;
 		if(!Tools.data.Pokedex[toId(target)]) {
 			return this.errorReply("Error: Pokemon not found.")
 		}
-		var bst = 0, pokeobj = Tools.data.Pokedex[toId(arg)];
-		for(var i in pokeobj.baseStats) {
+		let bst = 0, pokeobj = Tools.data.Pokedex[toId(target)];
+		for(let i in pokeobj.baseStats) {
 			bst += pokeobj.baseStats[i];
 		}
 		if(bst>350) {
-			return this.parse('/dt '+arg);
+			return this.parse('/dt '+target);
 		}
-		var newStats = {};
-		for(var i in pokeobj.baseStats) {
+		let newStats = {};
+		for( i in pokeobj.baseStats) {
 			newStats[i] = pokeobj.baseStats[i]*2;
 		}
-		var text = `${pokeobj.species} in 350 Cup: <br />`;
-		for(var i in newStats) text = `${text}newStats[i]/`;
+		let text = `${pokeobj.species} in 350 Cup: <br />`;
+		for(let i in newStats) text = `${text}newStats[i]/`;
 		text = text.substring(0,text.length-1);
 		this.sendReplyBox(text);
 	},
+	cup350help: ["/350 <pokemon> - Shows the base stats that a Pokemon would have in 350 cup."],
 };
