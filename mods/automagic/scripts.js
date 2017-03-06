bac159fe79976d0dc6a15472c77b0f05a30d0edc
Spandan Punwatkar <spandan.punwatkar@gmail.com>
2017-03-06 17:31:30 +0530

    Fix Status in Automagic (#3335)
    * Las Automagic Bugfix
    
    * Update Automagic
    
    * Update Automagic
    
    * Travis
    
    * Update scripts.js
    
    * Update scripts.js

--- mods/automagic/scripts.js
+++ mods/automagic/scripts.js
@@ -197,7 +197,31 @@ exports.BattleScripts = {
 				if (typeof secondaries[i].chance === 'undefined' || secondaryRoll < secondaries[i].chance) {
 					// mod for automagic start
 					let flag = true;
-					if (moveData.secondary.status) flag = moveData.secondary.status !== target.status;
+					let canSetStatus = function (status, target, pokemon) {
+						if (target.status) return false;
+						let cantStatus = {
+							brn: ['Fire', 'comatose', 'waterveil', 'waterbubble'],
+							frz: ['Ice', 'comatose', 'magmaarmor'],
+							par: ['Electric', 'comatose', 'limber'],
+							psn: ['comatose', 'immunity'],
+							slp: ['comatose', 'insomnia', 'vitalspirit'],
+							tox: ['comatose', 'immunity'],
+						};
+						if (target.hasType(['Poison', 'Steel']) && (status === 'psn' || status === 'tox')) {
+							if (pokemon.hasAbility('corrosion')) {
+								return true;
+							} else {
+								return false;
+							}
+						}
+						if (target.hasType(cantStatus[status][0])) return false;
+						if (move.ignoreAbility) return true;
+						if (target.hasAbility('leafguard') && this.isWeather(['sunnyday', 'desolateland'])) return false;
+						if (target.hasAbility('shieldsdown') && target.template.speciesid === 'miniormeteor') return false;
+						if (target.hasAbility(cantStatus[status])) return false;
+						return true;
+					};
+					if (moveData.secondary.status) flag = canSetStatus(moveData.secondary.status, target, pokemon);
 					if (moveData.secondary.volatileStatus) flag = !(moveData.secondary.volatileStatus in target.volatiles);
 					if (moveData.secondary.volatileStatus === 'flinch') flag = flag && target.activeTurns && !target.moveThisTurn;
 					this.moveHit(target, pokemon, move, secondaries[i], true, isSelf);
@@ -208,7 +232,7 @@ exports.BattleScripts = {
 					} else {
 						flag = flag && !(target.hp === undefined || target.hp <= 0);
 					}
-					if (moveData.target === 'Normal' && moveData.secondary.boosts) {
+					if (moveData.target !== 'self' && moveData.secondary.boosts) {
 						let cantLower = {
 							'atk': ['clearbody', 'fullmetalbody', 'hypercutter', 'whitesmoke'],
 							'def': ['bigpecks', 'clearbody', 'fullmetalbody', 'whitesmoke'],
@@ -222,13 +246,9 @@ exports.BattleScripts = {
 								flag = false;
 								continue;
 							}
-							if (moveData.secondary.boosts[k] < 0) {
-								for (let j = 0; j < cantLower[k].length; j++) {
-									if (target.hasAbility(cantLower[k][j])) {
-										flag = false;
-										break;
-									}
-								}
+							if (moveData.secondary.boosts[k] < 0 && target.hasAbility(cantLower[k]) && !move.ignoreAbility) {
+								flag = false;
+								break;
 							}
 						}
 					}
