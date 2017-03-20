f1a11c4f8bfd08a95aab8ccd69ea6c555473b75d
Kris Johnson <KrisOnline@users.noreply.github.com>
2017-03-19 22:53:34 -0600

    oops

--- chat-plugins/othermetas.js
+++ chat-plugins/othermetas.js
@@ -25,7 +25,7 @@ exports.commands = {
 			return this.errorReply(`You cannot use ${stone.name} on anything besides ${stone.megaEvolves} in Mix and Mega.`);
 		}
 		for (let i in Tools.mod.("mixandmega").data.BattleFormatsData) { // Separate messages because there's a difference between being already mega evolved / NFE and being banned from mega evolving
-			let mnmTemplate = Tools.mod.("mixandmega").data.BattleFormatsData[i];
+			let mnmTemplate = Tools.mod.("mixandmega").data.BattleFormatsData;
 			if (mnmTemplate[i] && mnmTemplate[i].tier === "Uber") {
 				return this.errorReply(`${template.name} is banned from mega evolving in Mix and Mega.`);
 			}
