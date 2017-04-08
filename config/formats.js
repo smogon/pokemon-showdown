7d0ac60268166083b9830a32137e5cf04f5dc4fd
The Immortal <the_immortal123@live.com>
2017-04-08 06:54:51 +0400

    Remove OU suspect

--- config/formats.js
+++ config/formats.js
@@ -36,18 +36,8 @@ exports.Formats = [
 		],
 
 		mod: 'gen7',
-		searchShow: false,
 		ruleset: ['Pokemon', 'Standard', 'Team Preview', 'Baton Pass Clause'],
 		banlist: ['Uber', 'Power Construct', 'Shadow Tag'],
-	},
-	{
-		name: "[Gen 7] OU (suspect test)",
-		desc: ["&bullet; <a href=\"https://www.smogon.com/forums/threads/3599206/\">OU Suspect Discussion</a>"],
-
-		mod: 'gen7',
-		challengeShow: false,
-		ruleset: ['[Gen 7] OU'],
-		banlist: ['Pheromosa'],
 	},
 	{
 		name: "[Gen 7] Ubers",
