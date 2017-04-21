28093e9c55cb8bed7bb6cc0d7e59b47701eec1db
Quite Quiet <femtontusen@gmail.com>
2017-04-21 09:37:04 +0200

    Gen 2: Once again combine SpA and SpD DVs into the same value In commit e8b60c1e908b2ea27a120caa8b1325d22ac7b1e4 this line was apparently lost which made sure Special Attack and Special Defense IVs were the same in Gen 2.
    This closes #3459 I believe.

--- team-validator.js
+++ team-validator.js
@@ -396,6 +396,9 @@ class Validator {
 				if (expectedHpDV !== hpDV) {
 					problems.push(`${name} has an HP DV of ${hpDV}, but its Atk, Def, Spe, and Spc DVs give it an HP DV of ${expectedHpDV}.`);
 				}
+				if (set.ivs.spa !== set.ivs.spd) {
+					problems.push(`${name} has differnt SpA and SpD DVs (its SpA and SpD DVs has to be equal).`);
+				}
 				if (tools.gen > 1 && !template.gender) {
 					// Gen 2 gender is calculated from the Atk DV.
 					// High Atk DV <-> M. The meaning of "high" depends on the gender ratio.
