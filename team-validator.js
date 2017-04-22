4b1857568585cc2f2b7379b7e03fe5787a72d57f
Guangcong Luo <guangcongluo@gmail.com>
2017-04-22 11:04:49 -0400

    Don't warn for incorrect SpD IV in Gen 1
    Also fix grammar

--- team-validator.js
+++ team-validator.js
@@ -397,7 +397,11 @@ class Validator {
 					problems.push(`${name} has an HP DV of ${hpDV}, but its Atk, Def, Spe, and Spc DVs give it an HP DV of ${expectedHpDV}.`);
 				}
 				if (set.ivs.spa !== set.ivs.spd) {
-					problems.push(`${name} has differnt SpA and SpD DVs (its SpA and SpD DVs has to be equal).`);
+					if (tools.gen === 2) {
+						problems.push(`${name} has different SpA and SpD DVs, which is not possible in Gen 2.`);
+					} else {
+						set.ivs.spd = set.ivs.spa;
+					}
 				}
 				if (tools.gen > 1 && !template.gender) {
 					// Gen 2 gender is calculated from the Atk DV.
