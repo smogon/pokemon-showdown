4b61a48dba270b52a6b9773eb23cd074149e9c01
The Immortal <the_immortal123@live.com>
2017-04-26 14:43:14 +0400

    Random Battle: Fix Linoone

--- data/scripts.js
+++ data/scripts.js
@@ -2328,7 +2328,7 @@ exports.BattleScripts = {
 				evs.hp -= 4;
 				hp = Math.floor(Math.floor(2 * template.baseStats.hp + ivs.hp + Math.floor(evs.hp / 4) + 100) * level / 100 + 10);
 			}
-		} else if (hasMove['bellydrum'] && item === 'Sitrus Berry') {
+		} else if (hasMove['bellydrum'] && (item === 'Sitrus Berry' || ability === 'Gluttony')) {
 			// Belly Drum should activate Sitrus Berry
 			if (hp % 2 > 0) evs.hp -= 4;
 		} else if (hasMove['substitute'] && hasMove['reversal']) {
