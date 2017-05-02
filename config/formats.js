7e1e41e48f6bb06d057f22b06c88d8476fa5bc99
MacChaeger <msaimrkon@gmail.com>
2017-05-02 00:03:30 -0500

    Fix Arceus/Silvally related crash

--- config/formats.js
+++ config/formats.js
@@ -376,9 +376,9 @@ exports.Formats = [
 			return this.validateSet(set, teamHas, mixedTemplate);
 		},
 		onModifyTemplate: function (template, pokemon) {
-			if (pokemon.crossEvolved || pokemon.set.name === pokemon.set.species) return template;
+			if (pokemon.crossEvolved || !pokemon.set.name) return template;
 			let crossTemplate = this.getTemplate(pokemon.name);
-			if (!crossTemplate.exists) return template;
+			if (!crossTemplate.exists || crossTemplate.num === template.num) return template;
 			let crossPrevoTemplate = this.getTemplate(crossTemplate.prevo);
 			let mixedTemplate = Object.assign({}, template);
 			mixedTemplate.baseSpecies = mixedTemplate.species = template.species + '-' + crossTemplate.species;
