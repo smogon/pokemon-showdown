b6434de6a115a436532c69f86c2fbfd05d099fb5
Spandan Punwatkar <spandan.punwatkar@gmail.com>
2017-05-09 22:07:47 +0530

    Update github.js

--- chat-plugins/github.js
+++ chat-plugins/github.js
@@ -62,9 +62,9 @@ git.on('push', (repo, ref, result) => {
 		}
 		message = "";
 		message += `<font color='FF00FF'>${Chat.escapeHTML(repo)}</font>/`;
-		message += `<font color='800080'>${Chat.escapeHTML(branch)}</font> `;
-		message += `<a href="${Chat.escapeHTML(commit.url)}">`;
-		message += `<font color='606060'>${Chat.escapeHTML(commit.id.substring(0, 6))}</font></a> `;
+		message += Chat.html`<font color='800080'>${branch}</font> `;
+		message += Chat.html`<a href="${commit.url}">`;
+		message += Chat.html`<font color='606060'>${commit.id.substring(0, 6)}</font></a> `;
 		message += Chat.html`<font color='909090'>${commit.author.name}</font>: ${shortCommit}`;
 		messages.push(message);
 	});
