b779f1ec92edbebca34112eb3a20c1fcee6212b8
Insist <DeathlyPlays@users.noreply.github.com>
2017-05-09 09:44:14 -0700

    Simplify /helix

--- chat-plugins/info.js
+++ chat-plugins/info.js
@@ -1880,7 +1880,7 @@ exports.commands = {
 	m8b: function (target, room, user) {
 		if (!this.runBroadcast()) return;
 		let results = ['Signs point to yes.', 'Yes.', 'Reply hazy, try again.', 'Without a doubt.', 'My sources say no.', 'As I see it, yes.', 'You may rely on it.', 'Concentrate and ask again.', 'Outlook not so good.', 'It is decidedly so.', 'Better not tell you now.', 'Very doubtful.', 'Yes - definitely.', 'It is certain.', 'Cannot predict now.', 'Most likely.', 'Ask again later.', 'My reply is no.', 'Outlook good.', 'Don\'t count on it.'];
-		return this.sendReplyBox(results[Math.floor(20 * Math.random())]);
+		return this.sendReplyBox(results.length);
 	},
 
 	showimage: function (target, room, user) {
