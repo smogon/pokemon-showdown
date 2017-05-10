925b40361d816ac0875471e1816b2e0c6769bed2
Jacob McLemore <jmclemo6@vols.utk.edu>
2017-05-10 14:54:37 -0400

    Add /damagecalc as alias of /calc (#3517)

--- chat-plugins/info.js
+++ chat-plugins/info.js
@@ -1234,6 +1234,8 @@ exports.commands = {
 
 	'!calc': true,
 	calculator: 'calc',
+	damagecalculator: 'calc',
+	damagecalc: 'calc',
 	calc: function (target, room, user) {
 		if (!this.runBroadcast()) return;
 		this.sendReplyBox(
