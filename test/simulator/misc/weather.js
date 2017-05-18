00b05be0327f2bffa42de744256243af2be627fe
Guangcong Luo <guangcongluo@gmail.com>
2017-05-18 06:29:25 -0500

    Fix Hail/Sandstorm damage regression
    (Also add a test for it since this isn't even the first time this
    exact regression has happened.)

--- test/simulator/misc/weather.js
+++ test/simulator/misc/weather.js
@@ -35,4 +35,16 @@ describe('Weather damage calculation', function () {
 		const basePower = battle.runEvent('BasePower', attacker, defender, move, move.basePower, true);
 		assert.strictEqual(basePower, move.basePower);
 	});
+
+	it('should make Hail/Sandstorm damage some pokemon but not others', function () {
+		battle = common.createBattle();
+		battle.randomizer = dmg => dmg; // max damage
+		battle.join('p1', 'Guest 1', 1, [{species: 'Abomasnow', ability: 'snowwarning', moves: ['protect']}]);
+		battle.join('p2', 'Guest 2', 1, [{species: 'Sandslash', ability: 'sandveil', moves: ['protect']}]);
+		battle.commitDecisions();
+		const p1active = battle.p1.active[0];
+		const p2active = battle.p2.active[0];
+		assert.strictEqual(p1active.hp, p1active.maxhp);
+		assert.notEqual(p2active.hp, p2active.maxhp);
+	});
 });
