9fc27500aa382c75dfadd84037ba4562cb7d50a4
Lionyx <choloux3@gmail.com>
2017-05-25 03:23:35 +0200

    /forcepromote: Prevent from promoting to Global Room Owner (#3544)

--- chat-commands.js
+++ chat-commands.js
@@ -1962,6 +1962,7 @@ exports.commands = {
 		let name = this.targetUsername;
 		let nextGroup = target;
 		if (!Config.groups[nextGroup]) return this.errorReply("Group '" + nextGroup + "' does not exist.");
+		if (Config.groups[nextGroup].roomonly || Config.groups[nextGroup].battleonly) return this.errorReply(`Group '${nextGroup}' does not exist as a global rank.`);
 
 		if (Users.isUsernameKnown(name)) {
 			return this.errorReply("/forcepromote - Don't forcepromote unless you have to.");
