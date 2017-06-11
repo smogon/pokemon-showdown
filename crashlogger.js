1b656dbc06a598330be601a4eb0f5603bb4329d9
Ben Davies <morfentdoge@gmail.com>
2017-06-11 13:36:46 -0300

    Typescript: fix a couple of REPL and crashlogger errors I missed

--- crashlogger.js
+++ crashlogger.js
@@ -25,7 +25,7 @@ let hadException = false;
  *
  * @param {Error} err
  * @param {string} description
- * @param {?Object} data
+ * @param {?Object} [data = null]
  * @return {?string}
  */
 module.exports = function crashLogger(err, description, data = null) {
@@ -41,10 +41,10 @@ module.exports = function crashLogger(err, description, data = null) {
 
 	console.error(`\nCRASH: ${stack}\n`);
 	let out = require('fs').createWriteStream(logPath, {'flags': 'a'});
-	out.on("open", fd => {
+	out.on('open', fd => {
 		out.write(`\n${stack}\n`);
 		out.end();
-	}).on("error", /** @param {Error} err */ err => {
+	}).on('error', /** @param {Error} err */ err => {
 		console.error(`\nSUBCRASH: ${err.stack}\n`);
 	});
 
@@ -60,8 +60,8 @@ module.exports = function crashLogger(err, description, data = null) {
 				from: Config.crashguardemail.from,
 				to: Config.crashguardemail.to,
 				subject: Config.crashguardemail.subject,
-				text: `${description} crashed ${hadException ? "again " : ""}with this stack trace:\n${stack}`,
-			}, /** @param {Error?} err */ err => {
+				text: `${description} crashed ${hadException ? 'again ' : ''}with this stack trace:\n${stack}`,
+			}, /** @param {?Error} err */ err => {
 				if (err) console.error(`Error sending email: ${err}`);
 			});
 		}
@@ -72,5 +72,6 @@ module.exports = function crashLogger(err, description, data = null) {
 		// lock down the server
 		return 'lockdown';
 	}
+
 	return null;
 };
