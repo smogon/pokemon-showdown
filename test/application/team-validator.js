7812a907218a6f08051db25f12f4f0cbf6683ac9
Konrad Borowski <konrad@borowski.pw>
2017-06-24 10:51:06 +0200

    Rename Gen6 formats to have [Gen 6] in name (#3683)
    * Rename Gen6 formats to have [Gen 6] in name
    
    * Add Gen 6 format aliases for bots
    
    Those will be removed later when bots will be updated to know
    about new format names.

--- test/application/team-validator.js
+++ test/application/team-validator.js
@@ -6,61 +6,61 @@ describe('Team Validator features', function () {
 	describe('TeamValidator', function () {
 		it('should reject non-existent Pokemon', function () {
 			let team = [{species:'nonexistentPokemon', moves:['thunderbolt']}];
-			let illegal = TeamValidator('customgame').validateTeam(team);
+			let illegal = TeamValidator('gen7customgame').validateTeam(team);
 			assert(illegal);
 		});
 
 		it('should reject non-existent items', function () {
 			let team = [{species:'pikachu', moves:['thunderbolt'], ability:'static', item:'nonexistentItem'}];
-			let illegal = TeamValidator('customgame').validateTeam(team);
+			let illegal = TeamValidator('gen7customgame').validateTeam(team);
 			assert(illegal);
 		});
 
 		it('should reject non-existent abilities', function () {
 			let team = [{species:'pikachu', moves:['thunderbolt'], ability:'nonexistentAbility'}];
-			let illegal = TeamValidator('customgame').validateTeam(team);
+			let illegal = TeamValidator('gen7customgame').validateTeam(team);
 			assert(illegal);
 		});
 
 		it('should reject non-existent moves', function () {
 			let team = [{species:'pikachu', ability:'static', moves:['nonexistentMove']}];
-			let illegal = TeamValidator('customgame').validateTeam(team);
+			let illegal = TeamValidator('gen7customgame').validateTeam(team);
 			assert(illegal);
 		});
 
 		it('should reject non-existent natures', function () {
 			let team = [{species:'pikachu', ability:'static', moves:['thunderbolt'], nature:'nonexistentNature'}];
-			let illegal = TeamValidator('customgame').validateTeam(team);
+			let illegal = TeamValidator('gen7customgame').validateTeam(team);
 			assert(illegal);
 		});
 
 		it('should reject invalid happiness values', function () {
 			let team = [{species:'pikachu', ability:'static', moves:['thunderbolt'], happiness:'invalidHappinessValue'}];
-			let illegal = TeamValidator('customgame').validateTeam(team);
+			let illegal = TeamValidator('gen7customgame').validateTeam(team);
 			assert(illegal);
 		});
 
 		it('should accept legal movesets', function () {
 			let team = [{species:'pikachu', ability:'static', moves:['agility', 'protect', 'thunder', 'thunderbolt']}];
-			let illegal = TeamValidator('anythinggoes').validateTeam(team);
+			let illegal = TeamValidator('gen7anythinggoes').validateTeam(team);
 			assert(!illegal);
 		});
 
 		it('should reject illegal movesets', function () {
 			let team = [{species:'pikachu', ability:'static', moves:['blastburn', 'frenzyplant', 'hydrocannon', 'dragonascent']}];
-			let illegal = TeamValidator('anythinggoes').validateTeam(team);
+			let illegal = TeamValidator('gen7anythinggoes').validateTeam(team);
 			assert(illegal);
 		});
 
 		it('should accept both ability types for Mega Evolutions', function () {
 			// base forme ability
 			let team = [{species:'gyaradosmega', item:'gyaradosite', ability:'intimidate', moves:['dragondance', 'crunch', 'waterfall', 'icefang']}];
-			let illegal = TeamValidator('anythinggoes').validateTeam(team);
+			let illegal = TeamValidator('gen7anythinggoes').validateTeam(team);
 			assert(!illegal);
 
 			// mega forme ability
 			team = [{species:'gyaradosmega', item:'gyaradosite', ability:'moldbreaker', moves:['dragondance', 'crunch', 'waterfall', 'icefang']}];
-			illegal = TeamValidator('anythinggoes').validateTeam(team);
+			illegal = TeamValidator('gen7anythinggoes').validateTeam(team);
 			assert(!illegal);
 		});
 
