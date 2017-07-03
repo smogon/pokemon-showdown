b8d3b278b8bff49bf4724b591b1439ef235e0dce
sparktrain <sparktrain@users.noreply.github.com>
2017-07-03 15:24:16 -0400

    CAP: Update Persistent (#3722)
    Persistent now affects Room moves introduced after Gen 4 as discussed here: http://www.smogon.com/forums/threads/cap-updates-fidgit-discussion-complete.3602772/

--- data/moves.js
+++ data/moves.js
@@ -9611,13 +9611,12 @@ exports.BattleMovedex = {
 		},
 		effect: {
 			duration: 5,
-			/*durationCallback: function (source, effect) {
-				// Persistent isn't updated for BW moves
-				if (source && source.hasAbility('Persistent')) {
+			durationCallback: function (source, effect) {
+				if (source && source.hasAbility('persistent')) {
 					return 7;
 				}
 				return 5;
-			},*/
+			},
 			onStart: function (target, source) {
 				this.add('-fieldstart', 'move: Magic Room', '[of] ' + source);
 			},
@@ -18693,6 +18692,12 @@ exports.BattleMovedex = {
 		},
 		effect: {
 			duration: 5,
+			durationCallback: function (source, effect) {
+				if (source && source.hasAbility('persistent')) {
+					return 7;
+				}
+				return 5;
+			},
 			onStart: function (side, source) {
 				this.add('-fieldstart', 'move: Wonder Room', '[of] ' + source);
 			},
