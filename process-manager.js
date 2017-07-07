c42c40e40b4de6315c6100ef589f12b4b4272f85
Ben Davies <morfentdoge@gmail.com>
2017-07-06 23:16:20 -0700

    Typescript: process-manager.js, verifier.js (#3717)

--- process-manager.js
+++ process-manager.js
@@ -11,28 +11,48 @@
 
 const EventEmitter = require('events');
 
+/**
+ * @type {Map<any, string>}
+ */
 const processManagers = new Map();
 
+/**
+ * @param {any} str
+ * @return {string}
+ */
 function serialize(str) {
 	if (typeof str === 'string') return str;
 	return JSON.stringify(str);
 }
 
+/**
+ * @class ProcessWrapper
+ * @extends NodeJS.EventEmitter
+ * @property {any} PM
+ * @property {boolean} active
+ * @property {Map<number, any>} pendingTasks
+ * @property {any} process
+ */
 class ProcessWrapper extends EventEmitter {
+	/**
+	 * @param {any} PM
+	 */
 	constructor(PM) {
 		super();
 
 		this.PM = PM;
 		this.active = true;
 		this.pendingTasks = new Map();
-		this.process = require('child_process').fork(PM.execFile, {cwd: __dirname});
+		this.process = require('child_process').fork(PM.execFile, [], {cwd: __dirname});
 
 		// Allow events to bubble-up to the wrapper
 		this.process.on('message', message => this.emit('message', message));
-
 		this.on('message', PM.onMessageUpstream);
 	}
 
+	/**
+	 * @param {string} data
+	 */
 	send(data) {
 		return this.process.send(data);
 	}
@@ -44,31 +64,48 @@ class ProcessWrapper extends EventEmitter {
 		this.process.disconnect();
 	}
 
+	/**
+	 * @return {number}
+	 */
 	get load() {
 		return this.pendingTasks.size;
 	}
 }
 
-// execFile - the path to the file to spawn the child process(es) from
-// maxProcesses - the maximum number of child processes to spawn
-// isChatBased - the process manager handles some chat functionality
+/**
+ * @typedef {Object} PMOptions
+ * The path to the file to spawn the child process(es) from.
+ * @property {string} [execFile]
+ * The maximum number of child processes to spawn.
+ * @property {number} [maxProcesses]
+ * Whether or not the process manager handles a chat commands module.
+ * @property {boolean} [isChatBased]
+ */
+
+/**
+ * @class ProcessManager
+ * @property {ProcessWrapper[]} processes
+ * @property {number} taskId
+ * @property {string} execFile
+ * @property {number} maxProcesses
+ * @property {boolean} isChatBased
+ */
 class ProcessManager {
+	/**
+	 * @param {PMOptions} options
+	 */
 	constructor(options) {
-		if (!('execFile' in options)) {
-			Monitor.debug(
-				"No execFile property was missing form the options object to be " +
-				"given to the ProcessManager constructor!"
-			);
-		} else if (!('maxProcesses' in options) || !('isChatBased' in options)) {
-			Monitor.debug(
-				"An options object given to the ProcessManager constructor is " +
-				"missing required properties! The filename given is: " + (options.execFile || '""') + "."
+		if (!('execFile' in options) || !('maxProcesses' in options) || !('isChatBased' in options)) {
+			throw new Error(
+				"An options object given to the ProcessManager constructor is missing required properties! " +
+				`The execFile given is: ${options.execFile || ''}`
 			);
 		}
 
+		/** @type {ProcessWrapper[]} */
 		this.processes = [];
 		this.taskId = 0;
-		this.execFile = options.execFile;
+		this.execFile = '' + options.execFile;
 		this.maxProcesses = (typeof options.maxProcesses === 'number') ? options.maxProcesses : 1;
 		this.isChatBased = !!options.isChatBased;
 
@@ -76,7 +113,7 @@ class ProcessManager {
 	}
 
 	spawn() {
-		for (let i = this.processes.length; i < this.maxProcesses; ++i) {
+		for (let i = this.processes.length; i < this.maxProcesses; i++) {
 			this.processes.push(new ProcessWrapper(this));
 		}
 	}
@@ -93,9 +130,12 @@ class ProcessManager {
 		this.spawn();
 	}
 
+	/**
+	 * @return {ProcessWrapper}
+	 */
 	acquire() {
 		let process = this.processes[0];
-		for (let i = 1; i < this.processes.length; ++i) {
+		for (let i = 1; i < this.processes.length; i++) {
 			if (this.processes[i].load < process.load) {
 				process = this.processes[i];
 			}
@@ -103,47 +143,78 @@ class ProcessManager {
 		return process;
 	}
 
+	/**
+	 * @param {ProcessWrapper} process
+	 */
 	release(process) {
 		process.release();
 	}
 
+	/**
+	 * @param {...any} args
+	 * @return {Promise<any>}
+	 */
 	send(...args) {
 		if (!this.processes.length) {
-			return Promise.resolve(this.receive.apply(this, args));
+			return Promise.resolve(this.receive(...args));
 		}
 
-		let serializedArgs = args.map(serialize).join('|');
 		return new Promise((resolve, reject) => {
 			let process = this.acquire();
 			process.pendingTasks.set(this.taskId, resolve);
 			try {
-				process.process.send(`${this.taskId}|${serializedArgs}`);
+				let serializedArgs = args.map(serialize).join('|');
+				process.send(`${this.taskId++}|${serializedArgs}`);
 			} catch (e) {}
-			++this.taskId;
 		});
 	}
 
+	/**
+	 * @param {...any} args
+	 * @return {any}
+	 */
 	sendSync(...args) {
 		// synchronously!
-		return this.receive.apply(this, args);
+		return this.receive(...args);
 	}
 
-	onMessageUpstream() {
+	/**
+	 * @param {string} message
+	 */
+	onMessageUpstream(message) {
 		// Expected to resolve the pending task completed.
 	}
 
-	onMessageDownstream() {
+	/**
+	 * @param {string} message
+	 */
+	onMessageDownstream(message) {
 		// Expected to call `receive()` at some point,
 		// and send the result to the parent process.
 	}
 
-	receive() {
+	/**
+	 * @param {...any} args
+	 * @return {any}
+	 */
+	receive(...args) {
 		// This is where the child process actually does stuff
 		// To be overriden by specific implementations.
-		return '';
+	}
+
+	/**
+	 * @return {Map<ProcessManager, string>}
+	 */
+	static get cache() {
+		return processManagers;
+	}
+
+	/**
+	 * @return {{new(PM: ProcessManager): ProcessWrapper}}
+	 */
+	static get ProcessWrapper() {
+		return ProcessWrapper;
 	}
 }
 
-ProcessManager.cache = processManagers;
-ProcessManager.ProcessWrapper = ProcessWrapper;
 module.exports = ProcessManager;
