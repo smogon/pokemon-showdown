978c1b84f72db701b6c0f0d2386790b5570a80ad
Ben Davies <morfentdoge@gmail.com>
2017-07-08 02:55:27 -0300

    Typescript: declare all globals, fix FS error
    Typescript doesn't care whether or not the globals' modules are
    actually valid Typescript or not since globals.ts isn't required by any
    modules. This makes it much easier to get more modules moved to
    Typescript since most of the remaining ones are dependent on users.js
    and rooms.js, which are also dependent on them.

--- dev-tools/globals.ts
+++ dev-tools/globals.ts
@@ -1,5 +1,28 @@
 interface AnyObject {[k: string]: any}
 
-let Config = require('./config/config');
-let Dex = require('./sim/dex');
-// let Sim = require('./sim');
+let Config = require('../config/config');
+
+let Monitor = require('../monitor');
+
+let Dex = require('../sim/dex');
+let toId = Dex.toId;
+// let Sim = require('../sim');
+
+let LoginServer = require('../loginserver');
+let Ladders = (Config.remoteladder ? '../ladders-remote' : '../ladders');
+let Users = require('../users');
+type Connection = any;
+type User = any;
+
+let Punishments = require('../punishments');
+let Chat = require('../chat');
+let Rooms = require('../rooms');
+type Room = any;
+type GlobalRoom = any;
+type BattleRoom = any;
+type ChatRoom = any;
+
+let Verifier = require('../verifier');
+let Dnsbl = require('../dnsbl');
+let Sockets = require('../sockets');
+let TeamValidator = require('../team-validator');
