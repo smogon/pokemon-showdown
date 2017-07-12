13ad6dcd2976ee43d0520ceb63d8fa83a03ad7f3
Ivo Julca <ivojulca@hotmail.com>
2017-07-12 04:02:05 -0500

    Gen3-4 Randbats: Fix crashlogger path

--- mods/gen3/scripts.js
+++ mods/gen3/scripts.js
@@ -33,7 +33,7 @@ exports.BattleScripts = {
 			template = this.getTemplate('unown');
 
 			let err = new Error('Template incompatible with random battles: ' + species);
-			require('../crashlogger')(err, 'The gen 3 randbat set generator');
+			require('../../crashlogger')(err, 'The gen 3 randbat set generator');
 		}
 
 		if (template.battleOnly) species = template.baseSpecies;
