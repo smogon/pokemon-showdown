88204d5ad9a9043270a30bf8cc78225afcd54826
Quinton Lee <quivistis@gmail.com>
2017-07-14 15:55:25 -0500

    Check for name changes in /checkchallenges

--- chat-plugins/info.js
+++ chat-plugins/info.js
@@ -321,10 +321,10 @@ exports.commands = {
 			return this.errorReply(`Both users must be in this room.`);
 		}
 		let challenges = [];
-		if (user1.challengeTo && user1.challengeTo.to === user2.userid) {
+		if (user1.challengeTo && Users.get(user1.challengeTo.to) === user2) {
 			challenges.push(Chat.html`${user1.name} is challenging ${user2.name} in ${Dex.getFormat(user1.challengeTo.format).name}.`);
 		}
-		if (user2.challengeTo && user2.challengeTo.to === user1.userid) {
+		if (user2.challengeTo && Users.get(user2.challengeTo.to) === user1) {
 			challenges.push(Chat.html`${user2.name} is challenging ${user1.name} in ${Dex.getFormat(user2.challengeTo.format).name}.`);
 		}
 		if (!challenges.length) {
