a0aec690c81f32a103c0eeec648cc3e7eecf9edb
panpawn <jeremypiemonte@gmail.com>
2017-07-15 06:46:48 -0400

    Modlog: Make popup modal
    This will prevent the classic waiting for modlog and accidentally clicking it away

--- chat-plugins/modlog.js
+++ chat-plugins/modlog.js
@@ -304,7 +304,7 @@ exports.commands = {
 		}
 
 		PM.send(roomIdList.join(','), searchString, exactSearch, lines).then(response => {
-			connection.popup(prettifyResults(response, roomId, searchString, exactSearch, addModlogLinks, hideIps));
+			connection.popup(`|modal|${prettifyResults(response, roomId, searchString, exactSearch, addModlogLinks, hideIps)}`);
 			if (cmd === 'timedmodlog') this.sendReply(`The modlog query took ${Date.now() - startTime} ms to complete.`);
 		});
 	},
