9a746efc95a287e9134dfc7d949e7c71fcef07c4
Guangcong Luo <guangcongluo@gmail.com>
2017-07-20 17:00:28 -0500

    Make /roomevents add/edit messages consistent

--- chat-plugins/room-events.js
+++ chat-plugins/room-events.js
@@ -48,11 +48,11 @@ exports.commands = {
 			if (!eventId) return this.errorReply("Event names must contain at least one alphanumerical character.");
 
 			if (room.events[eventId] && this.cmd === 'add') {
-				this.errorReply(`There's already an event named '${eventId}'. If you want to replace that event, use /roomevents edit.`);
+				this.errorReply(`There's already an event named '${eventId}'; to replace it, use /roomevents edit`);
 				this.sendReplyBox(Chat.html`<code>/roomevents edit ${room.events[eventId].eventName} | ${room.events[eventId].date} | ${room.events[eventId].desc}</code>`);
 				return;
 			} else if (this.cmd === 'edit' && !room.events[eventId]) {
-				this.errorReply(`There's no event with the name '${eventId}'; use /roomevents add.`);
+				this.errorReply(`There's no event named '${eventId}'; to add one, use /roomevents add`);
 				this.sendReplyBox(Chat.html`<code>/roomevents add ${eventName} | ${date} | ${desc}</code>`);
 				return;
 			}
