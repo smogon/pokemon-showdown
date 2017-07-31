dddd603d252feaca93c599d5f9e468e00a0c87a9
EeveeGeneral <derek_conrad87@yahoo.com>
2017-07-31 16:28:43 -0700

    STABmons: Ban Lovely Kiss
    Reasoning http://www.smogon.com/forums/threads/stabmons.3587949/page-19#post-7452442

--- team-validator.js
+++ team-validator.js
@@ -837,7 +837,7 @@ class Validator {
 			alreadyChecked[template.speciesid] = true;
 			if (dex.gen === 2 && template.gen === 1) tradebackEligible = true;
 			// STABmons hack
-			if (ruleTable.has('ignorestabmoves') && !(moveid in {'acupressure':1, 'bellydrum':1, 'chatter':1, 'geomancy':1, 'shellsmash':1, 'shiftgear':1, 'thousandarrows':1}) && !move.isZ) {
+			if (ruleTable.has('ignorestabmoves') && !(moveid in {'acupressure':1, 'bellydrum':1, 'chatter':1, 'geomancy':1, 'lovelykiss':1, 'shellsmash':1, 'shiftgear':1, 'thousandarrows':1}) && !move.isZ) {
 				let types = template.types;
 				if (template.baseSpecies === 'Rotom') types = ['Electric', 'Ghost', 'Fire', 'Water', 'Ice', 'Flying', 'Grass'];
 				if (template.baseSpecies === 'Shaymin') types = ['Grass', 'Flying'];
