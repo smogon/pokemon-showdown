e811f43a59eb128d74b103407aca7d11cb3cdd2e
Ivo Julca <ivojulca@hotmail.com>
2017-08-07 07:37:43 -0500

    Fix file descriptor leak in chat logs

--- rooms.js
+++ rooms.js
@@ -1178,6 +1178,8 @@ class ChatRoom extends Room {
 		// hour ago isn't done yet. But if that's the case, we have bigger
 		// problems anyway.
 		if (this.rollLogTimer) clearTimeout(this.rollLogTimer);
+
+		if (this.destroyingLog) return;
 		this.rollLogTimer = setTimeout(() => this.rollLogFile(), nextHour - currentTime);
 
 		if (relpath + filename === this.logFilename) return;
@@ -1208,8 +1210,8 @@ class ChatRoom extends Room {
 			this.rollLogTimer = null;
 			this.logEntry = function () { };
 			this.logFile.end(finalCallback);
-		} else {
-			finalCallback();
+		} else if (typeof finalCallback === 'function') {
+			setImmediate(finalCallback);
 		}
 	}
 	logUserStats() {
@@ -1412,6 +1414,8 @@ class ChatRoom extends Room {
 			clearInterval(this.logUserStatsInterval);
 		}
 		this.logUserStatsInterval = null;
+
+		this.destroyLog();
 
 		if (!this.isPersonal) {
 			this.modlogStream.removeAllListeners('finish');
