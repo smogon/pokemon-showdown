49015cac1818a60cef14db67558adc207af67a87
Insist <DeathlyPlays@users.noreply.github.com>
2017-08-17 05:51:41 -0500

    Fix desc

--- mods/gen2/items.js
+++ mods/gen2/items.js
@@ -30,7 +30,7 @@ exports.BattleItems = {
 				return 3;
 			}
 		},
-		desc: "If holder is a Chansey, it overrides all crit boosting effects.",
+		desc: "If holder is a Chansey, it overrides all crit boosting effects, and sets Chansey's crit ratio to 3 (crit chance = 1/3).",
 	},
 	stick: {
 		inherit: true,
@@ -40,7 +40,7 @@ exports.BattleItems = {
 				return 3;
 			}
 		},
-		desc: "If holder is a Farfetch'd, it overrides all crit boosting effects.",
+		desc: "If holder is a Farfetch'd, it overrides all crit boosting effects, and sets Farfetch'd's crit ratio to 3 (crit chance = 1/3).",
 	},
 	thickclub: {
 		inherit: true,
