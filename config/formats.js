6196203b4fd0b9e39cb3c1dd213ddebb7cc6fb46
The Immortal <the_immortal123@live.com>
2017-09-01 11:17:28 +0400

    Fix Tier Shift

--- config/formats.js
+++ config/formats.js
@@ -332,7 +332,7 @@ exports.Formats = [
 		onModifyTemplate: function (template, pokemon) {
 			if (pokemon.tierShifted) return template;
 			let tsTemplate = Object.assign({}, template);
-			const boosts = {'UU': 5, 'BL2': 5, 'RU': 10, 'BL3': 10, 'NU': 15, 'BL4': 15, 'PU': 20, 'NFE': 20, 'LC Uber': 20, 'LC': 20};
+			const boosts = {'UU': 10, 'BL2': 10, 'RU': 20, 'BL3': 20, 'NU': 30, 'BL4': 30, 'PU': 40, 'NFE': 40, 'LC Uber': 40, 'LC': 40};
 			let tier = template.tier;
 			if (pokemon.set.item) {
 				let item = this.getItem(pokemon.set.item);
@@ -340,6 +340,8 @@ exports.Formats = [
 			}
 			if (tier.charAt(0) === '(') tier = tier.slice(1, -1);
 			let boost = (tier in boosts) ? boosts[tier] : 0;
+			if (boost > 0 && (pokemon.set.ability === 'Drizzle' || pokemon.set.item === 'Mewnium Z')) boost = 0;
+			if (boost > 20 && pokemon.set.ability === 'Drought') boost = 20;
 			for (let statName in template.baseStats) {
 				tsTemplate.baseStats[statName] = this.clampIntRange(template.baseStats[statName] + boost, 1, 255);
 			}
