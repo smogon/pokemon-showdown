31e14aa22448a9b0b436ce845e5d1932cb89aa3b
XnadrojX <jordanlow.jinyi@gmail.com>
2017-09-05 23:56:52 +0800

    Update scripts.js

--- mods/gen2/scripts.js
+++ mods/gen2/scripts.js
@@ -154,7 +154,7 @@ exports.BattleScripts = {
 			}
 			if (!move.ignoreEvasion) {
 				if (target.boosts.evasion > 0 && !move.ignorePositiveEvasion) {
-					accuracy *= negativeBoostTable[target.boosts.evasion]);
+					accuracy *= negativeBoostTable[target.boosts.evasion];
 				} else if (target.boosts.evasion < 0) {
 					accuracy *= positiveBoostTable[-target.boosts.evasion];
 				}
