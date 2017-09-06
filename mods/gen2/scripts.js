b7af0b66e18f88de42a4e2546f9fd5ea90573577
XnadrojX <jordanlow.jinyi@gmail.com>
2017-09-06 10:07:18 +0800

    Update scripts.js

--- mods/gen2/scripts.js
+++ mods/gen2/scripts.js
@@ -163,7 +163,7 @@ exports.BattleScripts = {
 			accuracy = Math.max(accuracy, 1);
 		}
 		accuracy = this.runEvent('ModifyAccuracy', target, pokemon, move, accuracy);
-		accuracy = Math.max(accuracy, 0);
+		if (accuracy !== true) accuracy = Math.max(accuracy, 0);
 
 		if (accuracy !== true && accuracy !== 255 && this.random(255) >= accuracy) {
 			this.attrLastMove('[miss]');
