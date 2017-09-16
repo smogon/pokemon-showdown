977af547d6b79d1abcd1dce57dfc68b54a28bdff
Marty-D <Marty-D@users.noreply.github.com>
2017-09-15 20:47:14 -0400

    Fix Aurora Veil

--- data/moves.js
+++ data/moves.js
@@ -868,6 +868,10 @@ exports.BattleMovedex = {
 			},
 			onAnyModifyDamage: function (damage, source, target, move) {
 				if (target !== source && target.side === this.effectData.target) {
+					if ((target.side.sideConditions['reflect'] && this.getCategory(move) === 'Physical') ||
+							(target.side.sideConditions['lightscreen'] && this.getCategory(move) === 'Special')) {
+						return;
+					}
 					if (!move.crit && !move.infiltrates) {
 						this.debug('Aurora Veil weaken');
 						if (target.side.active.length > 1) return this.chainModify([0xAAC, 0x1000]);
