b72ed1ca19cd83bfb09a414fa1d9938b1f9f9e98
Kris Johnson <KrisOnline@users.noreply.github.com>
2017-09-25 15:50:13 -0600

    Add NU suspect test (#4019)

--- config/formats.js
+++ config/formats.js
@@ -89,14 +89,24 @@ exports.Formats = [
 	{
 		name: "[Gen 7] NU",
 		desc: [
-			"&bullet; <a href=\"https://www.smogon.com/forums/threads/3611511/\">NU Metagame Discussion</a>",
+			"&bullet; <a href=\"https://www.smogon.com/forums/threads/3616091/\">NU Metagame Discussion</a>",
 			"&bullet; <a href=\"https://www.smogon.com/forums/threads/3607629/\">NU Viability Rankings</a>",
 			"&bullet; <a href=\"https://www.smogon.com/forums/threads/3606112/\">NU Sample Teams</a>",
 		],
 
 		mod: 'gen7',
+		searchShow: false,
 		ruleset: ['[Gen 7] RU'],
 		banlist: ['RU', 'BL3', 'Drought'],
+	},
+	{
+		name: "[Gen 7] NU (suspect test)",
+		desc: ["&bullet; <a href=\"https://www.smogon.com/forums/threads/3616091/\">NU Suspect Test</a>"],
+
+		mod: 'gen7',
+		challengeShow: false,
+		ruleset: ['[Gen 7] NU'],
+		banlist: [],
 	},
 	{
 		name: "[Gen 7] PU",
