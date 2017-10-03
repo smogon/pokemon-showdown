a71b82073f12e8fe2c2b18d43b73d30154ec9e35
Ridaz <ridaz@live.com.pt>
2017-10-04 00:57:05 +0100

    Update Encore & Fake Out move animations (#4035)

--- data/moves.js
+++ data/moves.js
@@ -4756,6 +4756,7 @@ exports.BattleMovedex = {
 		flags: {contact: 1, protect: 1, mirror: 1},
 		onTry: function (pokemon, target) {
 			if (pokemon.activeTurns > 1) {
+				this.attrLastMove('[still]');
 				this.add('-fail', pokemon);
 				this.add('-hint', "Fake Out only works on your first turn out.");
 				return null;
