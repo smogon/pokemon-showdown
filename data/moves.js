650a77e290e97f24de4de4578843219557e9bcec
MacChaeger <msaimrkon@gmail.com>
2017-10-06 15:24:04 -0500

    Fix Reflect Type vs. Pokemon with the "???" type (#4039)

--- data/moves.js
+++ data/moves.js
@@ -13039,7 +13039,15 @@ exports.BattleMovedex = {
 		onHit: function (target, source) {
 			if (source.template && (source.template.num === 493 || source.template.num === 773)) return false;
 			this.add('-start', source, 'typechange', '[from] move: Reflect Type', '[of] ' + target);
-			source.types = target.getTypes(true);
+			let newBaseTypes = target.getTypes(true).filter(type => type !== '???');
+			if (!newBaseTypes.length) {
+				if (target.addedType) {
+					newBaseTypes = ['Normal'];
+				} else {
+					return false;
+				}
+			}
+			source.types = newBaseTypes;
 			source.addedType = target.addedType;
 			source.knownType = target.side === source.side && target.knownType;
 		},
