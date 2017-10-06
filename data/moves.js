6985b66f8074dafa88eefb8d5cb716aecc648445
MacChaeger <msaimrkon@gmail.com>
2017-10-05 20:33:15 -0500

    Actually Fix Reflect type + add test (#62)

--- data/moves.js
+++ data/moves.js
@@ -13038,13 +13038,16 @@ exports.BattleMovedex = {
 		flags: {protect: 1, authentic: 1, mystery: 1},
 		onHit: function (target, source) {
 			if (source.template && (source.template.num === 493 || source.template.num === 773)) return false;
-			if (target.getTypes() === ['???']) return false;
 			this.add('-start', source, 'typechange', '[from] move: Reflect Type', '[of] ' + target);
-			if (target.addedType) {
-				source.types = target.getTypes(true).map(type => type === '???' ? 'Normal' : type);
-			} else {
-				source.types = target.getTypes(true).filter(type => type !== '???');
+			let newBaseTypes = target.getTypes(true).filter(type => type !== '???');
+			if (!newBaseTypes.length) {
+				if (target.addedType) {
+					newBaseTypes = ['Normal'];
+				} else {
+					return false;
+				}
 			}
+			source.types = newBaseTypes;
 			source.addedType = target.addedType;
 			source.knownType = target.side === source.side && target.knownType;
 		},
---
+++ test/simulator/moves/reflecttype.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const assert = require('./../../assert');
+const common = require('./../../common');
+
+let battle;
+
+describe('Reflect Type', function () {
+	afterEach(function () {
+		battle.destroy();
+	});
+
+	it('should fail when used against a Pokemon whose type is "???"', function () {
+		battle = common.createBattle();
+		battle.join('p1', 'Guest 1', 1, [{species: 'Arcanine', ability: 'intimidate', moves: ['burnup']}]);
+		battle.join('p2', 'Guest 2', 1, [{species: 'Latias', ability: 'levitate', item: 'laggingtail', moves: ['reflecttype']}]);
+		assert.constant(() => battle.p2.active[0].getTypes(), () => battle.commitDecisions());
+	});
+
+	it('should ignore the "???" type when used against a Pokemon whose type contains "???" and a non-added type', function () {
+		battle = common.createBattle();
+		const p1 = battle.join('p1', 'Guest 1', 1, [{species: 'Latias', ability: 'levitate', item: 'laggingtail', moves: ['reflecttype', 'trickortreat']}]);
+		battle.join('p2', 'Guest 2', 1, [{species: 'Moltres', ability: 'intimidate', moves: ['burnup']}]);
+		battle.commitDecisions();
+		assert.strictEqual(battle.p1.active[0].getTypes().length, 1);
+		assert.strictEqual(battle.p1.active[0].getTypes()[0], 'Flying');
+		p1.chooseMove(2, 1).foe.chooseDefault();
+		battle.commitDecisions();
+		assert.strictEqual(battle.p1.active[0].getTypes().length, 2);
+		assert.strictEqual(battle.p1.active[0].getTypes()[0], 'Flying');
+		assert.strictEqual(battle.p1.active[0].getTypes()[1], 'Ghost');
+	});
+
+	it('should turn the "???" type into "Normal" when used against a Pokemon whose type is only "???" and an added type', function () {
+		battle = common.createBattle();
+		const p1 = battle.join('p1', 'Guest 1', 1, [{species: 'Latias', ability: 'levitate', item: 'laggingtail', moves: ['reflecttype', 'trickortreat']}]);
+		battle.join('p2', 'Guest 2', 1, [{species: 'Arcanine', ability: 'intimidate', moves: ['burnup']}]);
+		p1.chooseMove(2, 1).foe.chooseDefault();
+		battle.commitDecisions();
+		assert.strictEqual(battle.p1.active[0].getTypes().length, 2);
+		assert.strictEqual(battle.p1.active[0].getTypes()[0], 'Normal');
+		assert.strictEqual(battle.p1.active[0].getTypes()[1], 'Ghost');
+	});
+});
