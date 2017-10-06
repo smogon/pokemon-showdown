7c700ca20bfd543392f21e88adfe836cfefeb54d
The Immortal <the_immortal123@live.com>
2017-10-06 22:40:46 +0400

    Ultimate Z: Add new bans

--- config/formats.js
+++ config/formats.js
@@ -356,7 +356,7 @@ exports.Formats = [
 
 		mod: 'ultimatez',
 		ruleset: ['[Gen 7] OU'],
-		banlist: ['Celebrate', 'Happy Hour', 'Hold Hands'],
+		banlist: ['Kyurem-Black', 'Celebrate', 'Conversion', 'Happy Hour', 'Hold Hands'],
 	},
 	{
 		name: "[Gen 6] Balanced Hackmons",
