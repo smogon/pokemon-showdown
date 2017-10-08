a4710c3c8dbbbd3d9b49b313ea25cc80966b99b5
Guangcong Luo <guangcongluo@gmail.com>
2017-10-08 03:39:58 -0500

    Implement tiebreaker
    This is the tiebreaker used for timeouts in Gen 6-7 (I think), but it's
    not currently used anywhere.

--- sim/battle.js
+++ sim/battle.js
@@ -1004,6 +1004,45 @@ class Battle extends Dex.ModdedDex {
 			throw new Error(`Choices are done immediately after a request`);
 		}
 	}
+	tiebreak() {
+		this.add('message', "Time's up! Going to tiebreaker...");
+		const notFainted = this.sides.map(side => (
+			side.pokemon.filter(pokemon => !pokemon.fainted).length
+		));
+		this.add('-message', this.sides.map((side, i) => (
+			`${side.name}: ${notFainted[i]} Pokemon left`
+		)).join('; '));
+		const maxNotFainted = Math.max(...notFainted);
+		let tiedSides = this.sides.filter((side, i) => notFainted[i] === maxNotFainted);
+		if (tiedSides.length <= 1) {
+			return this.win(tiedSides[0]);
+		}
+
+		const hpPercentage = tiedSides.map(side => (
+			side.pokemon.map(pokemon => pokemon.hp / pokemon.maxhp).reduce((a, b) => a + b) * 100 / 6
+		));
+		this.add('-message', tiedSides.map((side, i) => (
+			`${side.name}: ${Math.round(hpPercentage[i])}% total HP left`
+		)).join('; '));
+		const maxPercentage = Math.max(...hpPercentage);
+		tiedSides = tiedSides.filter((side, i) => hpPercentage[i] === maxPercentage);
+		if (tiedSides.length <= 1) {
+			return this.win(tiedSides[0]);
+		}
+
+		const hpTotal = tiedSides.map(side => (
+			side.pokemon.map(pokemon => pokemon.hp).reduce((a, b) => a + b)
+		));
+		this.add('-message', tiedSides.map((side, i) => (
+			`${side.name}: ${Math.round(hpTotal[i])} total HP left`
+		)).join('; '));
+		const maxTotal = Math.max(...hpTotal);
+		tiedSides = tiedSides.filter((side, i) => hpTotal[i] === maxTotal);
+		if (tiedSides.length <= 1) {
+			return this.win(tiedSides[0]);
+		}
+		this.tie();
+	}
 	tie() {
 		this.win();
 	}
@@ -2671,6 +2710,10 @@ class Battle extends Dex.ModdedDex {
 		case 'win':
 		case 'tie':
 			this.win(data[2]);
+			break;
+
+		case 'tiebreak':
+			this.tiebreak();
 			break;
 
 		case 'choose':
