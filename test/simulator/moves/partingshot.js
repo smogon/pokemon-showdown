d4cfdaf8882c75e6a3496677e89a7bdd6836bf0e
MacChaeger <msaimrkon@gmail.com>
2017-10-31 12:19:06 -0500

    Add test for Parting Shot

---
+++ test/simulator/moves/partingshot.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const assert = require('./../../assert');
+const common = require('./../../common');
+
+let battle;
+
+describe(`Parting Shot`, function () {
+	afterEach(() => battle.destroy());
+
+	it(`should not switch the user out if the target's stats are not changed`, function () {
+		battle = common.createBattle([
+			[{species: "Silvally", ability: 'prankster', moves: ['partingshot', 'splash']},
+				{species: "Type: Null", ability: 'battlearmor', moves: ['return']}],
+			[{species: "Registeel", ability: 'clearbody', moves: ['splash']},
+				{species: "Solgaleo", ability: 'fullmetalbody', moves: ['splash']},
+				{species: "Torkoal", ability: 'whitesmoke', moves: ['splash']},
+				{species: "Shaymin", ability: 'flowerveil', moves: ['splash']},
+				{species: "Kingler", ability: 'hypercutter', moves: ['splash']},
+				{species: "Spinda", ability: 'contrary', moves: ['splash', 'partingshot']}],
+		]);
+		const p1 = battle.p1;
+		const p2 = battle.p2;
+		battle.commitDecisions();
+		assert.strictEqual(battle.currentRequest, 'move');
+		p1.chooseMove(1).foe.chooseSwitch(2); // Solgaleo
+		assert.strictEqual(battle.currentRequest, 'move');
+		p1.chooseMove(1).foe.chooseSwitch(3); // Torkoal
+		assert.strictEqual(battle.currentRequest, 'move');
+		p1.chooseMove(1).foe.chooseSwitch(4); // Shaymin
+		assert.strictEqual(battle.currentRequest, 'move');
+		p1.chooseMove(2).foe.chooseSwitch(5); // Kingler
+		p2.active[0].boostBy({spa: -6});
+		battle.commitDecisions();
+		assert.strictEqual(battle.currentRequest, 'move');
+		p1.chooseMove(2).foe.chooseSwitch(6); // Spinda
+		p2.active[0].boostBy({atk: 6, spa: 6});
+		battle.commitDecisions();
+		assert.strictEqual(battle.currentRequest, 'move');
+		p1.active[0].boostBy({atk: -6, spa: -6});
+		p1.chooseMove(2).foe.chooseMove(2); // Parting Shot against Silvally this time
+		assert.strictEqual(battle.currentRequest, 'move');
+	});
+});
