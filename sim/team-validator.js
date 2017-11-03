d03d4856b5cc16b7a1656e17f3e4e4a0769ff91e
Kris Johnson <KrisOnline@users.noreply.github.com>
2017-11-03 01:42:37 -0600

    STABmons: Fix move validation error (#4121)

--- sim/team-validator.js
+++ sim/team-validator.js
@@ -901,7 +901,8 @@ class Validator {
 			alreadyChecked[template.speciesid] = true;
 			if (dex.gen === 2 && template.gen === 1) tradebackEligible = true;
 			// STABmons hack to avoid copying all of validateSet to formats
-			if (ruleTable.has('ignorestabmoves') && format.noLearn.indexOf(moveid) < 0 && !move.isZ) {
+			let noLearn = format.noLearn || dex.getFormat('gen7stabmons').noLearn;
+			if (ruleTable.has('ignorestabmoves') && noLearn.indexOf(move.name) < 0 && !move.isZ) {
 				let types = template.types;
 				if (template.baseSpecies === 'Rotom') types = ['Electric', 'Ghost', 'Fire', 'Water', 'Ice', 'Flying', 'Grass'];
 				if (template.baseSpecies === 'Shaymin') types = ['Grass', 'Flying'];
