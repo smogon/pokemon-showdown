25ccd242eba132efde57897f18b65facc06cab0a
Kris Johnson <kris.langur@gmail.com>
2017-11-02 23:15:36 -0600

    Revert formats.js

--- config/formats.js
+++ config/formats.js
@@ -548,7 +548,7 @@ exports.Formats = [
 		searchShow: false,
 		ruleset: ['Gen 7] OU', 'Ignore STAB Moves'],
 		banlist: ['Kartana', 'Komala', 'Kyurem-Black', 'Silvally-Ghost', 'Tapu Koko', 'Tapu Lele', 'Aerodactylite', 'King\'s Rock', 'Metagrossite', 'Razor Fang'],
-		noLearn: ['acupressure', 'bellydrum', 'chatter', 'geomancy', 'shellsmash', 'shiftgear', 'thousandarrows'],
+		noLearn: ['Acupressure', 'Belly Drum', 'Chatter', 'Geomancy', 'Shell Smash', 'Shift Gear', 'Thousand Arrows'],
 	},
 	{
 		name: "[Gen 7] 2v2 Doubles",
