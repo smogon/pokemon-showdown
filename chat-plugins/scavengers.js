d54acfa638abd1bd74830d5d5e8e317abc9dc53f
Charlie Kobayashi <sparkychildcharlie@gmail.com>
2017-11-10 00:10:56 -0500

    Scavengers: improve status display

--- chat-plugins/scavengers.js
+++ chat-plugins/scavengers.js
@@ -788,6 +788,14 @@ let commands = {
 		if (!room.game || !room.game.scavGame) return this.errorReply(`There is no scavenger game currently running.`);
 
 		let game = room.game.childGame || room.game;
+
+		const elapsedMsg = Chat.toDurationString(Date.now() - game.startTime, {hhmmss: true});
+		const gameTypeMsg = game.gameType ? `<em>${game.gameType}</em> ` : '';
+		const hostersMsg = Chat.toListString(game.hosts.map(h => h.name));
+		const hostMsg = game.hosts.some(h => h.userid === game.staffHostId) ? '' : Chat.html` (started by - ${game.staffHostName})`;
+		const finishers = Chat.html`${game.completed.map(u => u.name).join(', ')}`;
+		const buffer = `<div class="infobox" style="margin-top: 0px;">The current ${gameTypeMsg}scavenger hunt by <em>${hostersMsg}${hostMsg}</em> has been up for: ${elapsedMsg}<br />Completed (${game.completed.length}): ${finishers}<br /></div>`;
+
 		if (game.hosts.some(h => h.userid === user.userid) || game.staffHostId === user.userid) {
 			let str = `<div class="ladder" style="overflow-y: scroll; max-height: 300px;"><table style="width: 100%"><th><b>Question</b></th><th><b>Users on this Question</b></th>`;
 			for (let i = 0; i < game.questions.length; i++) {
@@ -800,15 +808,9 @@ let commands = {
 				}
 			}
 			str += Chat.html`<tr><td>Completed</td><td>${game.completed.length ? game.completed.map(pl => pl.name).join(", ") : 'None'}`;
-			this.sendReply(`|raw|${str}</table></div>`);
-		} else {
-			const elapsedMsg = Chat.toDurationString(Date.now() - game.startTime, {hhmmss: true});
-			const gameTypeMsg = game.gameType ? `<em>${game.gameType}</em> ` : '';
-			const hostersMsg = Chat.toListString(game.hosts.map(h => h.name));
-			const hostMsg = game.hosts.some(h => h.userid === game.staffHostId) ? '' : Chat.html` (started by - ${game.staffHostName})`;
-			const finishers = Chat.html`${game.completed.map(u => u.name).join(', ')}`;
-			this.sendReplyBox(`The current ${gameTypeMsg}scavenger hunt by <em>${hostersMsg}${hostMsg}</em> has been up for: ${elapsedMsg}<br />Completed (${game.completed.length}): ${finishers}<br />`);
+			return this.sendReply(`|raw|${str}</table></div>${buffer}`);
 		}
+		this.sendReply(`|raw|${buffer}`);
 	},
 
 	hint: function (target, room, user) {
