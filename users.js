a61d8b8ecdfea18a38ec28c7a873260c4247131f
Guangcong Luo <guangcongluo@gmail.com>
2017-11-15 04:01:01 -0600

    Support loginfilter in chat plugins

--- users.js
+++ users.js
@@ -762,14 +762,14 @@ class User {
 			if (user.namelocked) user.named = true;
 
 			Rooms.global.checkAutojoin(user);
-			if (Config.loginfilter) Config.loginfilter(user, this, userType);
+			Chat.loginfilter(user, this, userType);
 			return true;
 		}
 
 		// rename success
 		if (this.forceRename(name, registered)) {
 			Rooms.global.checkAutojoin(this);
-			if (Config.loginfilter) Config.loginfilter(this, null, userType);
+			Chat.loginfilter(this, null, userType);
 			return true;
 		}
 		return false;
