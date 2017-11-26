cf16bd480da6fc1120efa93bdd327e145dec8822
Ben Davies <morfentdoge@gmail.com>
2017-11-26 02:46:55 -0400

    Trivia: fix Trivia#formatPlayerList

--- chat-plugins/trivia.js
+++ chat-plugins/trivia.js
@@ -429,11 +429,11 @@ class Trivia extends Rooms.RoomGame {
 	 * @return {string}
 	 */
 	formatPlayerList(count) {
-		let playerCount = Object.keys(this.players).length;
-		let from = (!isNaN(count) && count >= 0 && count < playerCount) ? (playerCount - count) : 0;
-		return Object.values(this.players)
+		let players = Object.values(this.players);
+		let to = (isNaN(count) || count < 0 || count > players.length) ? players.length : count;
+		return players
 			.sort((p1, p2) => p2.points - p1.points)
-			.slice(from)
+			.slice(0, to)
 			.map(player => {
 				const buf = Chat.html`${player.name} (${player.points})`;
 				return player.isAbsent ? `<span style="color: #444444">${buf}</span>` : buf;
