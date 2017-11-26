d0f9d41d7fac9a752904b01a60c4e9fc59d15818
Kris Johnson <KrisOnline@users.noreply.github.com>
2017-11-25 23:33:12 -0700

    othermetas.js: Add /stone (#4182)
    I couldn't use `Chat.getDataPokemonHTML`, `Chat.getDataItemHTML`, or
    `Chat.getDataMoveHTML`, as this required elements that none of those
    fully provide.

--- chat-plugins/othermetas.js
+++ chat-plugins/othermetas.js
@@ -147,6 +147,109 @@ exports.commands = {
 	},
 	mixandmegahelp: ["/mnm <pokemon> @ <mega stone> - Shows the Mix and Mega evolved Pokemon's type and stats."],
 
+	'!stone': true,
+	move: 'stone',
+	orb: 'stone',
+	megastone: 'stone',
+	stone: function (target) {
+		if (!this.runBroadcast()) return;
+		let targetid = toId(target);
+		if (!targetid) return this.parse('/help stone');
+		let stone;
+		if (targetid === 'dragonascent') {
+			stone = {
+				megaStone: "Rayquaza-Mega",
+				megaEvolves: "Rayquaza",
+			};
+		} else {
+			stone = Dex.getItem(target);
+		}
+		if (!stone.megaEvolves && !stone.onPrimal) return this.errorReply(`Error: Mega Stone not found.`);
+		let bannedStones = Dex.getFormat('gen7mixandmega').bannedStones;
+		if (bannedStones.includes(stone.name)) {
+			this.errorReply(`Warning: ${stone.name} is restricted to ${stone.megaEvolves} in Mix and Mega.`);
+		}
+		if (stone.isUnreleased) {
+			this.errorReply(`Warning: ${stone.name} is unreleased and is not usable in current Mix and Mega.`);
+		}
+		if (targetid === 'dragonascent') {
+			this.errorReply(`Warning: Only Pokemon with access to Dragon Ascent can mega evolve with Mega Rayquaza's traits.`);
+		}
+		// Fake Mega Stones
+		if (targetid === 'crucibellite') {
+			this.errorReply(`Warning: Crucibellite is a fake mega stone created by the CAP Project and is restricted to the CAP Crucibelle.`);
+		}
+		let baseTemplate = Dex.getTemplate(stone.megaEvolves);
+		let megaTemplate = Dex.getTemplate(stone.megaStone);
+		if (stone.id === 'redorb') { // Orbs do not have 'Item.megaStone' or 'Item.megaEvolves' properties.
+			baseTemplate = Dex.getTemplate("Groudon");
+			megaTemplate = Dex.getTemplate("Groudon-Primal");
+		} else if (stone.id === 'blueorb') {
+			baseTemplate = Dex.getTemplate("Kyogre");
+			megaTemplate = Dex.getTemplate("Kyogre-Primal");
+		}
+		let deltas = {
+			baseStats: {},
+			weightkg: megaTemplate.weightkg - baseTemplate.weightkg,
+		};
+		for (let statId in megaTemplate.baseStats) {
+			deltas.baseStats[statId] = megaTemplate.baseStats[statId] - baseTemplate.baseStats[statId];
+		}
+		if (megaTemplate.types.length > baseTemplate.types.length) {
+			deltas.type = megaTemplate.types[1];
+		} else if (megaTemplate.types.length < baseTemplate.types.length) {
+			deltas.type = baseTemplate.types[0];
+		} else if (megaTemplate.types[1] !== baseTemplate.types[1]) {
+			deltas.type = megaTemplate.types[1];
+		}
+		let details = {
+			"Gen": 6,
+			"Weight": (JSON.stringify(deltas.weightkg).startsWith("-") ? "" : "+") + Math.round(deltas.weightkg * 100) / 100 + " kg",
+		};
+		let tier;
+		if (['redorb', 'blueorb'].includes(stone.id)) {
+			tier = "Orb";
+		} else if (targetid === "dragonascent") {
+			tier = "Move";
+		} else {
+			tier = "Stone";
+		}
+		let buf = `<li class="result">`;
+		buf += `<span class="col numcol">${tier}</span> `;
+		if (targetid === "dragonascent") {
+			buf += `<span class="col itemiconcol"></span>`;
+		} else {
+			buf += `<span class="col itemiconcol"><psicon item="${targetid}"/></span> `;
+		}
+		if (targetid === "dragonascent") {
+			buf += `<span class="col movenamecol" style="white-space:nowrap"><a href="https://pokemonshowdown.com/dex/moves/${targetid}" target="_blank">Dragon Ascent</a></span> `;
+		} else {
+			buf += `<span class="col pokemonnamecol" style="white-space:nowrap"><a href="https://pokemonshowdown.com/dex/items/${stone.id}" target="_blank">${stone.name}</a></span> `;
+		}
+		if (deltas.type) {
+			buf += `<span class="col typecol"><img src="https://play.pokemonshowdown.com/sprites/types/${deltas.type}.png" alt="${deltas.type}" height="14" width="32"></span> `;
+		} else {
+			buf += `<span class="col typecol"></span>`;
+		}
+		buf += `<span style="float:left;min-height:26px">`;
+		buf += `<span class="col abilitycol">${megaTemplate.abilities['0']}</span>`;
+		buf += `<span class="col abilitycol"></span>`;
+		buf += `</span>`;
+		buf += `<span style="float:left;min-height:26px">`;
+		buf += `<span class="col statcol"><em>HP</em><br />0</span> `;
+		buf += `<span class="col statcol"><em>Atk</em><br />${deltas.baseStats.atk}</span> `;
+		buf += `<span class="col statcol"><em>Def</em><br />${deltas.baseStats.def}</span> `;
+		buf += `<span class="col statcol"><em>SpA</em><br />${deltas.baseStats.spa}</span> `;
+		buf += `<span class="col statcol"><em>SpD</em><br />${deltas.baseStats.spd}</span> `;
+		buf += `<span class="col statcol"><em>Spe</em><br />${deltas.baseStats.spe}</span> `;
+		buf += `<span class="col bstcol"><em>BST<br />100</em></span> `;
+		buf += `</span>`;
+		buf += `</li>`;
+		this.sendReply(`|raw|<div class="message"><ul class="utilichart">${buf}<li style="clear:both"></li></ul></div>`);
+		this.sendReply(`|raw|<font size="1"><font color="#686868">Gen:</font> ${details["Gen"]}&nbsp;|&ThickSpace;<font color="#686868">Weight:</font> ${details["Weight"]}</font>`);
+	},
+	stonehelp: ["/stone <mega stone> - Shows the changes that a mega stone/orb applies to a Pokemon."],
+
 	'!350cup': true,
 	'350': '350cup',
 	'350cup': function (target, room, user) {
