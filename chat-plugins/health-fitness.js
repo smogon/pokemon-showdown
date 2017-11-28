08ade60b59367696d011c8cf4c458e4e6e4a4404
Bär Halberkamp <fledaway@gmail.com>
2017-11-28 16:39:57 +0100

    Set isTrusted to true for the appropriate uses of Chat.formatText

--- chat-plugins/health-fitness.js
+++ chat-plugins/health-fitness.js
@@ -55,9 +55,9 @@ exports.commands = {
 		'': function (target, room, user) {
 			if (room.id !== 'healthfitness') return this.errorReply("This command can only be used in Health & Fitness.");
 			if (!this.runBroadcast()) return;
-			const cardio = Chat.formatText(HF.hfData.cardio || NOT_SET);
-			const gym = Chat.formatText(HF.hfData.gym || NOT_SET);
-			const quote = Chat.formatText(HF.hfData.quote || NOT_SET);
+			const cardio = Chat.formatText(HF.hfData.cardio || NOT_SET, true);
+			const gym = Chat.formatText(HF.hfData.gym || NOT_SET, true);
+			const quote = Chat.formatText(HF.hfData.quote || NOT_SET, true);
 			return this.sendReplyBox(
 				`<strong>Cardio Challenge:</strong> ${cardio}<br />` +
 				`<strong>Gym Challenge:</strong> ${gym}<br />` +
@@ -72,7 +72,7 @@ exports.commands = {
 
 				if (!this.runBroadcast('!healthfitness cardio')) return;
 
-				const cardio = Chat.formatText(HF.hfData.cardio || NOT_SET);
+				const cardio = Chat.formatText(HF.hfData.cardio || NOT_SET, true);
 				return this.sendReplyBox(`<strong>Cardio Challenge:</strong> ${cardio}`);
 			} else {
 				if (!this.can('broadcast', null, room)) return;
@@ -95,7 +95,7 @@ exports.commands = {
 
 				if (!this.runBroadcast('!healthfitness gym')) return;
 
-				const gym = Chat.formatText(HF.hfData.gym || NOT_SET);
+				const gym = Chat.formatText(HF.hfData.gym || NOT_SET, true);
 				return this.sendReplyBox(`<strong>Gym Challenge:</strong> ${gym}`);
 			} else {
 				if (!this.can('broadcast', null, room)) return;
@@ -118,7 +118,7 @@ exports.commands = {
 
 				if (!this.runBroadcast('!healthfitness quote')) return;
 
-				const quote = Chat.formatText(HF.hfData.quote || NOT_SET);
+				const quote = Chat.formatText(HF.hfData.quote || NOT_SET, true);
 				return this.sendReplyBox(`<strong>Quote of the Day:</strong> ${quote}`);
 			} else {
 				if (!this.can('broadcast', null, room)) return;
