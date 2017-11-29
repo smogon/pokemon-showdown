4af3c97dd87ea6d097ad034432f4bc431708e8ab
Nathaniel Harris <Nathaniel@ZanMgt.com>
2017-11-28 19:10:56 -0500

    More trailing space fixes

--- mods/gen2/scripts.js
+++ mods/gen2/scripts.js
@@ -428,10 +428,10 @@ exports.BattleScripts = {
 		if (move.id === 'present') {
 			const type_index = {"Normal":0, "Fighting":1, "Flying":2, "Poison":3, "Ground":4, "Rock":5, "Bug":7, "Ghost":8, "Steel":9, "Fire":20, "Water":21, "Grass": 22, "Electric":23, "Psychic":24, "Ice":25, "Dragon":26, "Dark":27};
 			attack = 10;
-			
+
 			let attacker_types = attacker.getTypes();
 			let defender_types = defender.getTypes();
-			
+
 			defense = Math.max(type_index[attacker_types[attacker_types.length - 1]], 1);
 			level = Math.max(type_index[defender_types[defender_types.length - 1]], 1);
 			if (move.crit) {
