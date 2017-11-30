8473c3f4fa3c78785fb51535857d1139bd7687fd
Guangcong Luo <guangcongluo@gmail.com>
2017-11-30 10:44:02 -0600

    Improve test engine
    New functions:
    
    battle.makeChoices([side 1], [side 2]);
    
    Intended to be a replacement for the previous .choose/.commitDecisions
    API.
    
    If we can get all the test code on it, we can maybe finally actually
    deprecate LEGACY_API_DO_NOT_USE.
    
    It's now used in Healing Wish, where I think it makes a very readable
    test.

--- test/simulator/moves/healingwish.js
+++ test/simulator/moves/healingwish.js
@@ -10,48 +10,59 @@ describe('Healing Wish', function () {
 		battle.destroy();
 	});
 
-	it('should heal a switch-in for full', function () {
+	it('should heal a switch-in for full at end of turn', function () {
 		battle = common.createBattle();
 		battle.join('p1', 'Guest 1', 1, [
-			{species: 'Pikachu', ability: 'static', moves: ['growl']},
-			{species: 'Jirachi', ability: 'serenegrace', moves: ['healingwish']},
+			{species: 'Caterpie', ability: 'shielddust', moves: ['stringshot']},
+			{species: 'Jirachi', ability: 'serenegrace', moves: ['healingwish', 'protect']},
 		]);
 		battle.join('p2', 'Guest 2', 1, [
-			{species: 'Miltank', ability: 'thickfat', moves: ['seismictoss']},
+			{species: 'Shedinja', ability: 'wonderguard', moves: ['endeavor']},
+			{species: 'Tyranitar', ability: 'sandstream', moves: ['seismictoss', 'stealthrock']},
 		]);
 
-		battle.commitDecisions(); // Pikachu takes damage
+		battle.makeChoices('move String Shot', 'move Endeavor'); // set Caterpie to 1hp
 
-		battle.choose('p1', 'switch 2');
-		battle.commitDecisions(); // Jirachi switches in
+		battle.makeChoices('switch Jirachi', 'switch Tyranitar'); // set up Sand
 
-		battle.commitDecisions(); // Jirachi uses Healing Wish
+		battle.makeChoices('move Protect', 'move Stealth Rock');
 
-		battle.commitDecisions(); // Pikachu switches in and fully heals
+		battle.makeChoices('move Healing Wish', 'move Seismic Toss');
 
-		assert.strictEqual(battle.p1.active[0].hp, battle.p1.active[0].maxhp);
+		// sand happens after Jirachi faints and before any switch-in
+		battle.makeChoices('switch Caterpie', ''); // Caterpie heals before taking SR damage
+		assert.strictEqual(battle.p1.active[0].hp, 174);
 		assert.strictEqual(battle.p1.active[0].moveset[0].pp, 63);
 	});
 
-	it('[Gen 4] should heal a switch-in for full', function () {
+	it('[Gen 4] should heal a switch-in for full mid-turn', function () {
 		battle = common.gen(4).createBattle();
 		battle.join('p1', 'Guest 1', 1, [
-			{species: 'Pikachu', ability: 'static', moves: ['growl']},
+			{species: 'Caterpie', ability: 'shielddust', moves: ['stringshot']},
+			{species: 'Raichu', ability: 'lightningrod', moves: ['growl']},
 			{species: 'Jirachi', ability: 'serenegrace', moves: ['healingwish']},
 		]);
 		battle.join('p2', 'Guest 2', 1, [
-			{species: 'Miltank', ability: 'thickfat', moves: ['seismictoss']},
+			{species: 'Shedinja', ability: 'wonderguard', moves: ['endeavor']},
+			{species: 'Tyranitar', ability: 'sandstream', moves: ['seismictoss', 'stealthrock']},
 		]);
 
-		battle.commitDecisions(); // Pikachu takes damage
+		battle.makeChoices('move String Shot', 'move Endeavor'); // set Caterpie to 1hp
 
-		battle.choose('p1', 'switch 2');
-		battle.commitDecisions(); // Jirachi switches in
+		battle.makeChoices('switch Raichu', 'switch Tyranitar'); // set up Sand
 
-		battle.commitDecisions(); // Jirachi uses Healing Wish
-		battle.commitDecisions(); // Pikachu switches in and fully heals
+		battle.makeChoices('move Growl', 'move Stealth Rock');
 
-		assert.strictEqual(battle.p1.active[0].hp, battle.p1.active[0].maxhp);
+		battle.makeChoices('switch Jirachi', 'move Seismic Toss');
+
+		battle.makeChoices('move Healing Wish', 'move Seismic Toss');
+
+		battle.makeChoices('switch Caterpie', ''); // Caterpie faints from hazards
+		assert.strictEqual(battle.p1.active[0].hp, 0);
+
+		battle.makeChoices('switch Raichu', ''); // Raichu fully heals and takes stoss + Sandstorm damage
+		assert.strictEqual(battle.turn, 6);
+		assert.strictEqual(battle.p1.active[0].hp, 145); // after stoss + Sandstorm
 		assert.strictEqual(battle.p1.active[0].moveset[0].pp, 63);
 	});
 });
