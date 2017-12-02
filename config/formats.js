84fb6e32a4796aa32312bc22d3ee3e175ffff3db
Spandamn <spandan.punwatkar@gmail.com>
2017-12-02 15:07:19 +0530

    Fix Disabling in PiC

--- config/formats.js
+++ config/formats.js
@@ -401,17 +401,16 @@ exports.Formats = [
 		gameType: 'doubles',
 		ruleset: ['[Gen 7] Doubles OU'],
 		banlist: ['Huge Power', 'Imposter', 'Parental Bond', 'Pure Power', 'Wonder Guard', 'Kangaskhanite', 'Mawilite', 'Medichamite', 'Mimic', 'Sketch', 'Transform'],
-		onDisableMovePriority: -1,
-		onDisableMove: function (pokemon) {
-			let ally = pokemon.side.active.find(ally => ally && ally !== pokemon && !ally.fainted);
-			if (!ally) ally = {baseMoveset: []};
-			pokemon.moveset = pokemon.baseMoveset.concat(ally.baseMoveset);
-		},
-		onResidual: function () {
-			this.eachEvent('ResetMoveset');
-		},
-		onResetMoveset: function (pokemon) {
-			pokemon.moveset = pokemon.baseMoveset;
+		onBegin: function () {
+			for (let k = 0; k < this.sides.length; k++) {
+				for (let i = 0; i < this.sides[k].pokemon.length; i++) {
+					let pokemon = this.sides[k].pokemon[i];
+					pokemon.originalBaseMoveset = [];
+					for (let j = 0; j < pokemon.baseMoveset.length; j++) {
+						pokemon.originalBaseMoveset.push(Object.assign({}, pokemon.baseMoveset[j]));
+					}
+				}
+			}
 		},
 		onSwitchInPriority: 2,
 		onSwitchIn: function (pokemon) {
@@ -434,7 +433,18 @@ exports.Formats = [
 				}
 			}
 			let ally = pokemon.side.active.find(ally => ally && ally !== pokemon && !ally.fainted);
-			if (ally && ally.ability !== pokemon.ability) {
+			if (!ally) return;
+			let addedMoves = [];
+			for (let i in pokemon.originalBaseMoveset) {
+				addedMoves[i] = Object.assign({}, pokemon.originalBaseMoveset[i]);
+			}
+			ally.moveset = ally.baseMoveset.concat(addedMoves);
+			addedMoves = [];
+			for (let i in ally.originalBaseMoveset) {
+				addedMoves[i] = Object.assign({}, ally.originalBaseMoveset[i]);
+			}
+			pokemon.moveset = pokemon.baseMoveset.concat(addedMoves);
+			if (ally.ability !== pokemon.ability) {
 				if (!pokemon.innate) {
 					pokemon.innate = 'ability' + ally.ability;
 					delete pokemon.volatiles[pokemon.innate];
@@ -446,28 +456,35 @@ exports.Formats = [
 					ally.addVolatile(ally.innate);
 				}
 			}
+			[ally, pokemon].forEach((mon => {
+				this.runEvent("DisableMove", mon);
+			}).bind(this));
 		},
 		onSwitchOut: function (pokemon) {
 			if (pokemon.innate) {
 				pokemon.removeVolatile(pokemon.innate);
 				delete pokemon.innate;
 			}
+			pokemon.moveset.length = pokemon.originalBaseMoveset.length;
 			let ally = pokemon.side.active.find(ally => ally && ally !== pokemon && !ally.fainted);
 			if (ally && ally.innate) {
 				ally.removeVolatile(ally.innate);
 				delete ally.innate;
 			}
+			ally.moveset.length = ally.originalBaseMoveset.length;
 		},
 		onFaint: function (pokemon) {
 			if (pokemon.innate) {
 				pokemon.removeVolatile(pokemon.innate);
 				delete pokemon.innate;
 			}
+			pokemon.moveset.length = pokemon.originalBaseMoveset.length;
 			let ally = pokemon.side.active.find(ally => ally && ally !== pokemon && !ally.fainted);
 			if (ally && ally.innate) {
 				ally.removeVolatile(ally.innate);
 				delete ally.innate;
 			}
+			ally.moveset.length = ally.originalBaseMoveset.length;
 		},
 	},
 	{
