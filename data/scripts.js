1eb361592a17b7444a590d92df7932a387626cb1
Guangcong Luo <guangcongluo@gmail.com>
2017-12-06 20:09:13 -0600

    Fix Mega Rayquaza mega evolution

--- data/scripts.js
+++ data/scripts.js
@@ -875,7 +875,7 @@ exports.BattleScripts = {
 	canMegaEvo: function (pokemon) {
 		let altForme = pokemon.baseTemplate.otherFormes && this.getTemplate(pokemon.baseTemplate.otherFormes[0]);
 		let item = pokemon.getItem();
-		if (altForme && altForme.isMega && altForme.requiredMove && pokemon.set.moves.includes(toId(altForme.requiredMove)) && !item.zMove) return altForme.species;
+		if (altForme && altForme.isMega && altForme.requiredMove && pokemon.baseMoves.includes(toId(altForme.requiredMove)) && !item.zMove) return altForme.species;
 		if (item.megaEvolves !== pokemon.baseTemplate.baseSpecies || item.megaStone === pokemon.species) {
 			return null;
 		}
