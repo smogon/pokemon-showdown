fd71b8df2a68854c9860507762e3c505d589e7dc
MacChaeger <msaimrkon@gmail.com>
2017-12-13 15:44:57 -0600

    Add MnM test & update the old one

--- test/simulator/misc/megaevolution.js
+++ test/simulator/misc/megaevolution.js
@@ -3,8 +3,6 @@
 const assert = require('./../../assert');
 const common = require('./../../common');
 
-let strongAbilities = ['battlebond', 'comatose', 'disguise', 'multitype', 'powerconstruct', 'rkssystem', 'schooling', 'shieldsdown', 'stancechange'];
-
 let battle;
 
 describe('Mega Evolution', function () {
@@ -12,13 +10,11 @@ describe('Mega Evolution', function () {
 		battle.destroy();
 	});
 
-	for (const strongAbility of strongAbilities) {
-		it('should overwrite normally immutable abilities', function () {
-			battle = common.createBattle();
-			const p1 = battle.join('p1', 'Guest 1', 1, [{species: "Metagross", ability: strongAbility, item: 'metagrossite', moves: ['metalclaw']}]);
-			battle.join('p2', 'Guest 2', 1, [{species: "Wishiwashi", ability: strongAbility, moves: ['uturn']}]);
-			battle.makeChoices('move metalclaw mega', 'move uturn');
-			assert.equal(p1.active[0].ability, 'toughclaws');
-		});
-	}
+	it('should overwrite normally immutable abilities', function () {
+		battle = common.createBattle();
+		const p1 = battle.join('p1', 'Guest 1', 1, [{species: "Metagross", ability: 'zenmode', item: 'metagrossite', moves: ['metalclaw']}]);
+		battle.join('p2', 'Guest 2', 1, [{species: "Wishiwashi", ability: 'schooling', moves: ['uturn']}]);
+		battle.makeChoices('move metalclaw mega', 'move uturn');
+		assert.equal(p1.active[0].ability, 'toughclaws');
+	});
 });
