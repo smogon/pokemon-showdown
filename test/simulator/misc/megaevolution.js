9efff36556e8c24011f41f84b33bfefa0d35bafc
MacChaeger <msaimrkon@gmail.com>
2017-12-12 19:24:15 -0600

    Improve test a bit

--- test/simulator/misc/megaevolution.js
+++ test/simulator/misc/megaevolution.js
@@ -3,7 +3,7 @@
 const assert = require('./../../assert');
 const common = require('./../../common');
 
-let strongAbilities = ['illusion', 'battlebond', 'comatose', 'disguise', 'multitype', 'powerconstruct', 'rkssystem', 'schooling', 'shieldsdown', 'stancechange'];
+let strongAbilities = ['battlebond', 'comatose', 'disguise', 'multitype', 'powerconstruct', 'rkssystem', 'schooling', 'shieldsdown', 'stancechange'];
 
 let battle;
 
@@ -14,7 +14,7 @@ describe('Mega Evolution', function () {
 
 	for (const strongAbility of strongAbilities) {
 		battle = common.createBattle();
-		it('should overwrite forme-change abilities on Mega Evolution', function () {
+		it('should overwrite normally immutable abilities', function () {
 			const p1 = battle.join('p1', 'Guest 1', 1, [{species: "Metagross", ability: strongAbility, item: 'metagrossite', moves: ['metalclaw']}]);
 			battle.join('p2', 'Guest 2', 1, [{species: "Wishiwashi", ability: strongAbility, moves: ['uturn']}]);
 			battle.makeChoices('move metalclaw mega', 'move uturn');
