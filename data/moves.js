6c6750b5b94383f553e4a06d7e4f059efc4b61d4
Marty-D <math_man70@hotmail.com>
2017-12-14 11:32:39 -0500

    Fix moves for USUM Ver 1.1

--- data/moves.js
+++ data/moves.js
@@ -8643,10 +8643,6 @@ exports.BattleMovedex = {
 		priority: 1,
 		flags: {},
 		pseudoWeather: 'iondeluge',
-		onTryHitField: function () {
-			this.add('-hint', "Ion Deluge does not work in Ultra Sun and Ultra Moon.");
-			return false;
-		},
 		effect: {
 			duration: 1,
 			onStart: function (target) {
@@ -18706,13 +18702,12 @@ exports.BattleMovedex = {
 			},
 			onTryHitPriority: 4,
 			onTryHit: function (target, source, move) {
-				// USUM bug
-				if (move.isZ) {
-					move.zBrokeProtect = true;
-					return;
-				}
 				// Wide Guard blocks all spread moves
 				if (move && move.target !== 'allAdjacent' && move.target !== 'allAdjacentFoes') {
+					return;
+				}
+				if (move.isZ) {
+					move.zBrokeProtect = true;
 					return;
 				}
 				this.add('-activate', target, 'move: Wide Guard');
