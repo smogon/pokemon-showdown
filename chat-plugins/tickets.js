965b7f618b19f3a35da5ffa867174d3b8128ebcb
Jeremy Piemonte <jeremypiemonte@gmail.com>
2017-12-14 15:11:57 -0500

    Help Tickets: Remove redundant |html| (#4252)

--- chat-plugins/tickets.js
+++ chat-plugins/tickets.js
@@ -322,7 +322,7 @@ exports.commands = {
 		create: function (target, room, user, connection) {
 			if (!this.runBroadcast()) return;
 			if (this.broadcasting) {
-				return this.sendReplyBox('|html|<button name="joinRoom" value="view-help-request" class="button"><strong>Request help</strong></button>');
+				return this.sendReplyBox('<button name="joinRoom" value="view-help-request" class="button"><strong>Request help</strong></button>');
 			}
 			if (user.can('lock')) return this.parse('/join view-help-request'); // Globals automatically get the form for reference.
 			if (!user.named) return this.errorReply(`You need to choose a username before doing this.`);
