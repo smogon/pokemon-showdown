02caf4794234b5bd32a74006fd28b42a4e3ae7d8
Becki Srofe <blf_23@yahoo.com>
2018-01-09 10:19:35 -0700

    Refactor belch to use makeChoices

--- test/simulator/moves/belch.js
+++ test/simulator/moves/belch.js
@@ -14,9 +14,9 @@ describe('Belch', function () {
 		battle = common.createBattle();
 		battle.join('p1', 'Guest 1', 1, [{species: 'Swalot', ability: 'gluttony', item: 'lumberry', moves: ['belch', 'stockpile']}]);
 		battle.join('p2', 'Guest 2', 1, [{species: 'Registeel', ability: 'clearbody', item: 'laggingtail', moves: ['thunderwave']}]);
-		battle.commitDecisions();
+		battle.makeChoices('move Stockpile', 'move Thunderwave');
 		assert.strictEqual(battle.p1.active[0].lastMove.id, 'stockpile');
-		battle.commitDecisions();
+		battle.makeChoices('move Belch', 'move Thunderwave');
 		assert.strictEqual(battle.p1.active[0].lastMove.id, 'belch');
 	});
 
@@ -24,8 +24,8 @@ describe('Belch', function () {
 		battle = common.createBattle();
 		battle.join('p1', 'Guest 1', 1, [{species: 'Swalot', ability: 'gluttony', item: 'salacberry', moves: ['belch', 'bugbite']}]);
 		battle.join('p2', 'Guest 2', 1, [{species: 'Swalot', ability: 'gluttony', item: 'salacberry', moves: ['belch', 'pluck']}]);
-		battle.commitDecisions();
-		battle.commitDecisions();
+		battle.makeChoices('move Bugbite', 'move Pluck');
+		battle.makeChoices('move Belch', 'move Belch');
 		assert.strictEqual(battle.p1.active[0].lastMove.id, 'belch');
 		assert.strictEqual(battle.p2.active[0].lastMove.id, 'belch');
 	});
@@ -34,8 +34,8 @@ describe('Belch', function () {
 		battle = common.createBattle();
 		battle.join('p1', 'Guest 1', 1, [{species: 'Swalot', ability: 'gluttony', moves: ['belch', 'stockpile']}]);
 		battle.join('p2', 'Guest 2', 1, [{species: 'Machamp', ability: 'noguard', item: 'salacberry', moves: ['fling']}]);
-		battle.commitDecisions();
-		battle.commitDecisions();
+		battle.makeChoices('move Stockpile', 'move Fling');
+		battle.makeChoices('move Belch', 'move Fling');
 		assert.strictEqual(battle.p1.active[0].lastMove.id, 'belch');
 	});
 
@@ -46,12 +46,10 @@ describe('Belch', function () {
 			{species: 'Swalot', ability: 'gluttony', moves: ['toxic']},
 		]);
 		battle.join('p2', 'Guest 2', 1, [{species: 'Registeel', ability: 'prankster', moves: ['rest', 'thunderwave']}]);
-		battle.choose('p2', 'move 2');
-		battle.commitDecisions();
-		battle.choose('p1', 'switch 2'); // For U-Turn
-		battle.choose('p1', 'switch 2');
-		battle.commitDecisions();
-		battle.commitDecisions();
+		battle.makeChoices('move Uturn', 'move Thunderwave');
+		battle.makeChoices('switch 2', 'move Thunderwave');//For U-Turn
+		battle.makeChoices('switch 2', 'move Thunderwave');
+		battle.makeChoices('move Belch', 'move Thunderwave');
 		assert.strictEqual(battle.p1.active[0].lastMove.id, 'belch');
 	});
 });
