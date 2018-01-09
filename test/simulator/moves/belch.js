290ee368cbbe1ae7d8aeb18b0405563df4775c3d
Becki Srofe <blf_23@yahoo.com>
2018-01-09 10:37:08 -0700

    Take out thunderwave

--- test/simulator/moves/belch.js
+++ test/simulator/moves/belch.js
@@ -47,7 +47,7 @@ describe('Belch', function () {
 		]);
 		battle.join('p2', 'Guest 2', 1, [{species: 'Registeel', ability: 'prankster', moves: ['rest', 'thunderwave']}]);
 		battle.makeChoices('move Uturn', 'move Thunderwave');
-		battle.makeChoices('switch 2', 'move Thunderwave');//For U-Turn
+		battle.makeChoices('switch 2');//For U-Turn
 		battle.makeChoices('switch 2', 'move Thunderwave');
 		battle.makeChoices('move Belch', 'move Thunderwave');
 		assert.strictEqual(battle.p1.active[0].lastMove.id, 'belch');
