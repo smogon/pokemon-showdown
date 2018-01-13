4969fb07903d05dc18459df9a2c38cea3fe0bedf
Guangcong Luo <guangcongluo@gmail.com>
2018-01-12 22:17:34 -0600

    Add Streams library
    I've finally been sufficiently frustrated by Node's Streams API to
    straight-up write my own.
    
    This is what a Streams API looks like when you don't try to pretend
    Promises don't exist. So much easier to use.

--- loginserver.js
+++ loginserver.js
@@ -16,6 +16,7 @@ const http = Config.loginserver.startsWith('http:') ? require("http") : require(
 const url = require('url');
 
 const FS = require('./lib/fs');
+const Streams = require('./lib/streams');
 
 /**
  * A custom error type used when requests to the login server take too long.
@@ -75,14 +76,7 @@ class LoginServerInstance {
 		return new Promise((resolve, reject) => {
 			// @ts-ignore TypeScript bug: http.get signature
 			let req = http.get(urlObject, res => {
-				let buffer = '';
-				res.setEncoding('utf8');
-
-				res.on('data', (/** @type {string} */ chunk) => {
-					buffer += chunk;
-				});
-
-				res.on('end', () => {
+				Streams.readAll(res).then(buffer => {
 					let data = parseJSON(buffer).json || null;
 					resolve([data, res.statusCode, null]);
 					this.openRequests--;
@@ -153,53 +147,20 @@ class LoginServerInstance {
 			'&servertoken=' + encodeURIComponent(Config.servertoken) +
 			'&nocache=' + new Date().getTime() +
 			'&json=' + encodeURIComponent(JSON.stringify(dataList)) + '\n';
+		/** @type {any} */
 		let requestOptions = url.parse(this.uri + 'action.php');
-		// @ts-ignore
 		requestOptions.method = 'post';
-		// @ts-ignore
 		requestOptions.headers = {
 			'Content-Type': 'application/x-www-form-urlencoded',
 			'Content-Length': postData.length,
 		};
 
-		let req = /** @type {any} */ (null);
-
-		let hadError = false;
-		let onReqError = (/** @type {Error} */ error) => {
-			if (hadError) return;
-			hadError = true;
-			if (this.requestTimeoutTimer) {
-				clearTimeout(this.requestTimeoutTimer);
-				this.requestTimeoutTimer = null;
-			}
-			req.abort();
-			for (const resolve of resolvers) {
-				resolve([null, 0, error]);
-			}
-			this.requestEnd(error);
-		};
-
-		// @ts-ignore TypeScript bug: http.get signature
-		req = http.request(requestOptions, res => {
-			if (this.requestTimeoutTimer) {
-				clearTimeout(this.requestTimeoutTimer);
-				this.requestTimeoutTimer = null;
-			}
-			let buffer = '';
-			res.setEncoding('utf8');
-
-			res.on('data', (/** @type {string} */ chunk) => {
-				buffer += chunk;
-			});
-
-			let requestEnded = false;
-			let endReq = () => {
-				if (requestEnded) return;
-				requestEnded = true;
-				if (this.requestTimeoutTimer) {
-					clearTimeout(this.requestTimeoutTimer);
-					this.requestTimeoutTimer = null;
-				}
+		/** @type {any} */
+		let response = null;
+		// @ts-ignore
+		let req = http.request(requestOptions, res => {
+			response = res;
+			Streams.readAll(res).then(buffer => {
 				//console.log('RESPONSE: ' + buffer);
 				let data = parseJSON(buffer).json;
 				for (const [i, resolve] of resolvers.entries()) {
@@ -210,20 +171,20 @@ class LoginServerInstance {
 					}
 				}
 				this.requestEnd();
-			};
-			res.on('end', endReq);
-			res.on('close', endReq);
-
-			this.requestTimeoutTimer = setTimeout(() => {
-				if (res.connection) res.connection.destroy();
-				endReq();
-			}, LOGIN_SERVER_TIMEOUT);
+			});
 		});
 
-		req.on('error', onReqError);
+		req.on('close', () => {
+			if (response) return;
+			const error = new TimeoutError("Response not received");
+			for (const resolve of resolvers) {
+				resolve([null, 0, error]);
+			}
+			this.requestEnd(error);
+		});
 
 		req.setTimeout(LOGIN_SERVER_TIMEOUT, () => {
-			onReqError(new TimeoutError("Response not received"));
+			req.abort();
 		});
 
 		req.write(postData);
