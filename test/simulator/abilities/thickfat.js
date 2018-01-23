4923a3df50a294dfd5cd92992af95e620d038d8b
Dan Huang <github@yuzeh.com>
2018-01-22 18:51:31 -0800

    Replace mods with resetRNG changes

--- test/simulator/abilities/thickfat.js
+++ test/simulator/abilities/thickfat.js
@@ -12,24 +12,26 @@ describe('Thick Fat', function () {
 
 	it('should halve damage from Fire- or Ice-type attacks', function () {
 		battle = common.createBattle();
-		battle.join('p1', 'Guest 1', 1, [{species: "Hariyama", gender: 'F', ability: 'thickfat', moves: ['splash']}]);
+		battle.join('p1', 'Guest 1', 1, [{species: "Hariyama", ability: 'thickfat', moves: ['splash']}]);
 		battle.join('p2', 'Guest 2', 1, [{species: "Nidoking", ability: 'sheerforce', moves: ['incinerate', 'icebeam']}]);
 		const target = battle.p1.active[0];
 		battle.p2.chooseMove('incinerate').foe.chooseDefault();
 		assert.bounded(target.maxhp - target.hp, [29, 35]);
 		battle.heal(target.maxhp, target, target, battle.getFormat());
+		battle.resetRNG();
 		battle.p2.chooseMove('icebeam').foe.chooseDefault();
 		assert.bounded(target.maxhp - target.hp, [56, 66]);
 	});
 
 	it('should be suppressed by Mold Breaker', function () {
 		battle = common.createBattle();
-		battle.join('p1', 'Guest 1', 1, [{species: "Hariyama", gender: 'F', ability: 'thickfat', moves: ['splash']}]);
+		battle.join('p1', 'Guest 1', 1, [{species: "Hariyama", ability: 'thickfat', moves: ['splash']}]);
 		battle.join('p2', 'Guest 2', 1, [{species: "Nidoking", ability: 'moldbreaker', moves: ['incinerate', 'icebeam']}]);
 		const target = battle.p1.active[0];
 		battle.p2.chooseMove('incinerate').foe.chooseDefault();
 		assert.bounded(target.maxhp - target.hp, [57, 68]);
 		battle.heal(target.maxhp, target, target, battle.getFormat());
+		battle.resetRNG();
 		battle.p2.chooseMove('icebeam').foe.chooseDefault();
 		assert.bounded(target.maxhp - target.hp, [85, 101]);
 	});
