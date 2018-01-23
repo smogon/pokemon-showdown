3e0b7a70994761b2d49c07267c841091ec8f0c43
Kris Johnson <kris.langur@gmail.com>
2018-01-22 22:57:32 -0700

    Fix crash

--- chat-plugins/info.js
+++ chat-plugins/info.js
@@ -426,8 +426,8 @@ exports.commands = {
 					}
 					return this.sendReply(buffer);
 				case 'pokemon':
-					let template = Object.assign({}, mod.getTemplate(newTarget.name));
-					if ((room && room.id === 'smogondoubles') || (room && ['gen7doublesou', 'gen7doublesubers', 'gen7doublesuu'].includes(toId(Dex.getFormat(room.battle.name))))) template.tier = template.doublesTier;
+					let template = mod.getTemplate(newTarget.name);
+					if (room.id === 'smogondoubles' || ['gen7doublesou', 'gen7doublesubers', 'gen7doublesuu'].includes(Dex.getFormat(room.battle).id)) template.tier = template.doublesTier;
 					buffer += `|raw|${Chat.getDataPokemonHTML(template, mod.gen)}\n`;
 					break;
 				case 'item':
