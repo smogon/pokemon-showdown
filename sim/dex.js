6b20a86d1da73ab63d0130dd3e3275b733a85e6e
Marty-D <Marty-D@users.noreply.github.com>
2018-01-27 04:22:50 +0000

    Fix forme tiering in past gens

--- sim/dex.js
+++ sim/dex.js
@@ -369,7 +369,7 @@ class ModdedDex {
 		}
 		if (id && this.data.Pokedex.hasOwnProperty(id)) {
 			template = new Data.Template({name}, this.data.Pokedex[id], this.data.FormatsData[id], this.data.Learnsets[id]);
-			if ((!template.tier || !template.doublesTier) && template.baseSpecies !== template.species) {
+			if (!template.tier && !template.doublesTier && template.baseSpecies !== template.species) {
 				if (template.baseSpecies === 'Mimikyu') {
 					template.tier = this.data.FormatsData[toId(template.baseSpecies)].tier;
 					template.doublesTier = this.data.FormatsData[toId(template.baseSpecies)].doublesTier;
