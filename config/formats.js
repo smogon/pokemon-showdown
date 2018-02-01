7a5f65743efae99d44ecf699285d7cea62bb0b4f
cant say <jake.c.king@live.com.au>
2018-02-01 14:05:58 +1100

    Add Ultra Sinnoh Classic (#4391)

--- config/formats.js
+++ config/formats.js
@@ -227,6 +227,27 @@ exports.Formats = [
 		},
 	},
 	{
+		name: "[Gen 7] Ultra Sinnoh Classic",
+		desc: [`&bullet; <a href="http://www.smogon.com/forums/threads/3627404/">Ultra Sinnoh Classic Discussion</a>`],
+
+		mod: 'gen7',
+		maxForcedLevel: 50,
+		teamLength: {
+			validate: [3, 6],
+			battle: 3,
+		},
+		ruleset: ['Pokemon', 'Standard GBU', 'Sinnoh Pokedex'],
+		onValidateSet: function (set) {
+			const item = this.getItem(set.item);
+			if (item.megaStone) {
+				return [`(${set.name} is holding ${item.name})`, `Mega Stones are banned in Ultra Sinnoh Classic.`];
+			}
+			if (item.zMove) {
+				return [`(${set.name} is holding ${item.name})`, `Z-Crystals are banned in Ultra Sinnoh Classic.`];
+			}
+		},
+	},
+	{
 		name: "[Gen 7] Custom Game",
 
 		mod: 'gen7',
