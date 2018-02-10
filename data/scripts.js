bb2548a77144d404f3d6655aa73b13d74ca1f619
Jordan Low <jordanlow.jinyi@gmail.com>
2018-02-10 23:28:09 +0800

    Update scripts.js

--- data/scripts.js
+++ data/scripts.js
@@ -317,6 +317,58 @@ exports.BattleScripts = {
 		this.setActiveMove(move, pokemon, target);
 		move.zBrokeProtect = false;
 		let hitResult = true;
+		
+		if (this.gen == 4) {
+			//Gen 4's Accuracy Checks go before Immunity Checks
+			let accuracy = move.accuracy;
+			let boosts, boost;
+			if (accuracy !== true) {
+				if (!move.ignoreAccuracy) {
+					boosts = this.runEvent('ModifyBoost', pokemon, null, null, Object.assign({}, pokemon.boosts));
+					boost = this.clampIntRange(boosts['accuracy'], -6, 6);
+					if (boost > 0) {
+						accuracy *= boostTable[boost];
+					} else {
+						accuracy /= boostTable[-boost];
+					}
+				}
+				if (!move.ignoreEvasion) {
+					boosts = this.runEvent('ModifyBoost', target, null, null, Object.assign({}, target.boosts));
+					boost = this.clampIntRange(boosts['evasion'], -6, 6);
+					if (boost > 0) {
+						accuracy /= boostTable[boost];
+					} else if (boost < 0) {
+						accuracy *= boostTable[-boost];
+					}
+				}
+			}
+			if (move.ohko) { // bypasses accuracy modifiers
+				if (!target.isSemiInvulnerable()) {
+					accuracy = 30;
+					if (move.ohko === 'Ice' && this.gen >= 7 && !pokemon.hasType('Ice')) {
+						accuracy = 20;
+					}
+					if (pokemon.level >= target.level && (move.ohko === true || !target.hasType(move.ohko))) {
+						accuracy += (pokemon.level - target.level);
+					} else {
+						this.add('-immune', target, '[ohko]');
+						return false;
+					}
+				}
+			} else {
+				accuracy = this.runEvent('ModifyAccuracy', target, pokemon, move, accuracy);
+			}
+			if (move.alwaysHit || (move.id === 'toxic' && this.gen >= 6 && pokemon.hasType('Poison'))) {
+				accuracy = true; // bypasses ohko accuracy modifiers
+			} else {
+				accuracy = this.runEvent('Accuracy', target, pokemon, move, accuracy);
+			}
+			if (accuracy !== true && this.random(100) >= accuracy) {
+				if (!move.spreadHit) this.attrLastMove('[miss]');
+				this.add('-miss', pokemon, target);
+				return false;
+			}
+		}
 
 		hitResult = this.singleEvent('PrepareHit', move, {}, target, pokemon, move);
 		if (!hitResult) {
@@ -372,55 +424,57 @@ exports.BattleScripts = {
 		}
 
 		let boostTable = [1, 4 / 3, 5 / 3, 2, 7 / 3, 8 / 3, 3];
-
-		// calculate true accuracy
-		let accuracy = move.accuracy;
-		let boosts, boost;
-		if (accuracy !== true) {
-			if (!move.ignoreAccuracy) {
-				boosts = this.runEvent('ModifyBoost', pokemon, null, null, Object.assign({}, pokemon.boosts));
-				boost = this.clampIntRange(boosts['accuracy'], -6, 6);
-				if (boost > 0) {
-					accuracy *= boostTable[boost];
-				} else {
-					accuracy /= boostTable[-boost];
+		
+		if (this.gen !=4) {
+			// calculate true accuracy
+			let accuracy = move.accuracy;
+			let boosts, boost;
+			if (accuracy !== true) {
+				if (!move.ignoreAccuracy) {
+					boosts = this.runEvent('ModifyBoost', pokemon, null, null, Object.assign({}, pokemon.boosts));
+					boost = this.clampIntRange(boosts['accuracy'], -6, 6);
+					if (boost > 0) {
+						accuracy *= boostTable[boost];
+					} else {
+						accuracy /= boostTable[-boost];
+					}
+				}
+				if (!move.ignoreEvasion) {
+					boosts = this.runEvent('ModifyBoost', target, null, null, Object.assign({}, target.boosts));
+					boost = this.clampIntRange(boosts['evasion'], -6, 6);
+					if (boost > 0) {
+						accuracy /= boostTable[boost];
+					} else if (boost < 0) {
+						accuracy *= boostTable[-boost];
+					}
 				}
 			}
-			if (!move.ignoreEvasion) {
-				boosts = this.runEvent('ModifyBoost', target, null, null, Object.assign({}, target.boosts));
-				boost = this.clampIntRange(boosts['evasion'], -6, 6);
-				if (boost > 0) {
-					accuracy /= boostTable[boost];
-				} else if (boost < 0) {
-					accuracy *= boostTable[-boost];
+			if (move.ohko) { // bypasses accuracy modifiers
+				if (!target.isSemiInvulnerable()) {
+					accuracy = 30;
+					if (move.ohko === 'Ice' && this.gen >= 7 && !pokemon.hasType('Ice')) {
+						accuracy = 20;
+					}
+					if (pokemon.level >= target.level && (move.ohko === true || !target.hasType(move.ohko))) {
+						accuracy += (pokemon.level - target.level);
+					} else {
+						this.add('-immune', target, '[ohko]');
+						return false;
+					}
 				}
+			} else {
+				accuracy = this.runEvent('ModifyAccuracy', target, pokemon, move, accuracy);
 			}
-		}
-		if (move.ohko) { // bypasses accuracy modifiers
-			if (!target.isSemiInvulnerable()) {
-				accuracy = 30;
-				if (move.ohko === 'Ice' && this.gen >= 7 && !pokemon.hasType('Ice')) {
-					accuracy = 20;
-				}
-				if (pokemon.level >= target.level && (move.ohko === true || !target.hasType(move.ohko))) {
-					accuracy += (pokemon.level - target.level);
-				} else {
-					this.add('-immune', target, '[ohko]');
-					return false;
-				}
+			if (move.alwaysHit || (move.id === 'toxic' && this.gen >= 6 && pokemon.hasType('Poison'))) {
+				accuracy = true; // bypasses ohko accuracy modifiers
+			} else {
+				accuracy = this.runEvent('Accuracy', target, pokemon, move, accuracy);
 			}
-		} else {
-			accuracy = this.runEvent('ModifyAccuracy', target, pokemon, move, accuracy);
-		}
-		if (move.alwaysHit || (move.id === 'toxic' && this.gen >= 6 && pokemon.hasType('Poison'))) {
-			accuracy = true; // bypasses ohko accuracy modifiers
-		} else {
-			accuracy = this.runEvent('Accuracy', target, pokemon, move, accuracy);
-		}
-		if (accuracy !== true && this.random(100) >= accuracy) {
-			if (!move.spreadHit) this.attrLastMove('[miss]');
-			this.add('-miss', pokemon, target);
-			return false;
+			if (accuracy !== true && this.random(100) >= accuracy) {
+				if (!move.spreadHit) this.attrLastMove('[miss]');
+				this.add('-miss', pokemon, target);
+				return false;
+			}
 		}
 		
 		if (this.gen == 4 && move.ignoreImmunity !== true && !move.ignoreImmunity[move.type] && !target.runImmunity(move.type, true)) {
