699391bcc2c5e02722a614bbf8160c832cd7fbbe
EienSeiryuu <wong.kev001@gmail.com>
2018-02-12 00:22:41 -0800

    ORAS Monotype: Ban Damp Rock (#4414)
    It's always been banned: http://www.smogon.com/dex/xy/formats/monotype/

--- config/formats.js
+++ config/formats.js
@@ -990,7 +990,7 @@ exports.Formats = [
 			'Aegislash', 'Altaria-Mega', 'Arceus', 'Blaziken', 'Charizard-Mega-X', 'Darkrai', 'Deoxys-Base', 'Deoxys-Attack', 'Dialga', 'Genesect', 'Gengar-Mega',
 			'Giratina', 'Greninja', 'Groudon', 'Ho-Oh', 'Hoopa-Unbound', 'Kangaskhan-Mega', 'Kyogre', 'Kyurem-White', 'Lucario-Mega', 'Lugia', 'Mawile-Mega', 'Metagross-Mega',
 			'Mewtwo', 'Palkia', 'Rayquaza', 'Reshiram', 'Sableye-Mega', 'Salamence-Mega', 'Shaymin-Sky', 'Slowbro-Mega', 'Talonflame', 'Xerneas', 'Yveltal', 'Zekrom',
-			'Smooth Rock', 'Soul Dew',
+			'Damp Rock', 'Smooth Rock', 'Soul Dew',
 		],
 	},
 	{
