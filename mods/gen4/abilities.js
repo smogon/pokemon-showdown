c618855c8a57a5f3538d2f7c8667b4a8713c0091
KrisXV <11083252+KrisXV@users.noreply.github.com>
2018-03-02 07:24:18 -0700

    Gen III-IV: Fix Flash Fire + Freeze (#4449)

--- mods/gen4/abilities.js
+++ mods/gen4/abilities.js
@@ -59,6 +59,17 @@ exports.BattleAbilities = {
 	},
 	"flashfire": {
 		inherit: true,
+		onTryHit: function (target, source, move) {
+			if (target !== source && move.type === 'Fire') {
+				if (target.status === 'frz') {
+					return;
+				}
+				if (!target.addVolatile('flashfire')) {
+					this.add('-immune', target, '[msg]', '[from] ability: Flash Fire');
+				}
+				return null;
+			}
+		},
 		effect: {
 			noCopy: true, // doesn't get copied by Baton Pass
 			onStart: function (target) {
