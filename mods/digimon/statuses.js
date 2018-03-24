62fc4c52b71906cf0ba2f12146024111f5b70e6f
Lycanium Z <28816776+Lycanium-Z@users.noreply.github.com>
2018-03-23 22:54:53 -0400

    Fix Crash
    @volco saves my ass once again! :^D

--- mods/digimon/statuses.js
+++ mods/digimon/statuses.js
@@ -73,27 +73,29 @@ exports.BattleStatuses = {
 		},
 	},
 	sidestall: {
-	    //this is a side condition
-	    duration: 2,
-	    counterMax: 729,
-	    onStart: function (side, source) {
-			 if (source.volatiles['stall']) {
-				 this.effectData.counter = source.volatiles['stall'].counter * 3;
-			 } else {
-				 this.effectData.counter = 3;
-			 }
-		 },
-	    onStallMove: function (target) {
-			 let counter = this.effectData.counter || 1;
-			 this.debug("Success chance: " + Math.round(100 / counter) + "%");
-			 return !this.random(counter);
-	    },
-	    onRestart: function () {
-			 if (this.effectData.counter < this.effect.counterMax) {
-				 this.effectData.counter *= 3;
-			 }
-			 this.effectData.duration = 2;
-		 },
+		//this is a side condition
+		duration: 2,
+		counterMax: 729,
+		onStart: function (side) {
+			for (let u in side.active) {
+				if (side.active[u].volatiles['stall']) {
+					this.effectData.counter = side.active[u].volatiles['stall'].counter * 3;
+				} else {
+					this.effectData.counter = 3;
+				}
+			}
+		},
+		onStallMove: function (target) {
+			let counter = this.effectData.counter || 1;
+			this.debug("Success chance: " + Math.round(100 / counter) + "%");
+			return !this.random(counter);
+		},
+		onRestart: function () {
+			if (this.effectData.counter < this.effect.counterMax) {
+				this.effectData.counter *= 3;
+			}
+			this.effectData.duration = 2;
+		},
 	},
 
 	raindance: {
