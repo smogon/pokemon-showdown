891d563a96df5838f000e8a3b6f8cae77437bc93
HoeenHero <HoeenCoder@users.noreply.github.com>
2018-04-05 18:14:29 -0400

    Allow Leaders to block pms (#4552)

--- chat.js
+++ chat.js
@@ -839,8 +839,8 @@ class CommandContext {
 				if (targetUser.ignorePMs && targetUser.ignorePMs !== user.group && !user.can('lock')) {
 					if (!targetUser.can('lock')) {
 						return this.errorReply(`This user is blocking private messages right now.`);
-					} else if (targetUser.can('bypassall')) {
-						return this.errorReply(`This admin is too busy to answer private messages right now. Please contact a different staff member.`);
+					} else if (targetUser.can('declare')) {
+						return this.errorReply(`This ${Config.groups[targetUser.group].name} is too busy to answer private messages right now. Please contact a different staff member.`);
 					}
 				}
 				if (user.ignorePMs && user.ignorePMs !== targetUser.group && !targetUser.can('lock')) {
