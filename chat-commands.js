f864fe53b69e0ae3b3ae047e2e47b49e9bfd5bb3
Ben Davies <34197615+Kaiepi@users.noreply.github.com>
2018-04-21 00:40:18 -0300

    Limit /code characters to 8KiB (#4610)
    Being able to send 100KiB messages whenever you want this easily
    isn't a good idea. Limiting it to 8KiB makes it harder for workers to
    block.

--- chat-commands.js
+++ chat-commands.js
@@ -134,6 +134,7 @@ exports.commands = {
 		if (!target) return this.parse('/help code');
 		if (!this.canTalk()) return;
 		if (target.startsWith('\n')) target = target.slice(1);
+		if (target.length >= 8192) return this.errorReply("Your code must be under 8192 characters long!");
 		const separator = '\n';
 		if (target.includes(separator)) {
 			const params = target.split(separator);
