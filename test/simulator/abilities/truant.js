cccee906103f767001693775115b32dabb0e8373
Kevin Lau <ascriptmaster@gmail.com>
2018-05-21 12:02:57 -0700

    Add additional Truant sleep check

--- test/simulator/abilities/truant.js
+++ test/simulator/abilities/truant.js
@@ -31,7 +31,7 @@ describe('Truant', function () {
 		assert.hurts(pokemon, () => battle.commitDecisions());
 	});
 
-	it('should not allow the user to act the turn it wakes up', function () {
+	it('should not allow the user to act the turn it wakes up, if it moved the turn it fell asleep', function () {
 		battle = common.createBattle();
 		battle.join('p1', 'Guest 1', 1, [{species: "Slaking", ability: 'truant', moves: ['scratch', 'rest']}]);
 		battle.join('p2', 'Guest 2', 1, [{species: "Steelix", ability: 'sturdy', moves: ['endure', 'quickattack']}]);
@@ -41,6 +41,22 @@ describe('Truant', function () {
 		assert.false.hurts(pokemon, () => battle.makeChoices('move scratch', 'move endure'), 'Attacked on turn 1 of sleep');
 		assert.false.hurts(pokemon, () => battle.makeChoices('move scratch', 'move endure'), 'Attacked on turn 2 of sleep');
 		assert.false.hurts(pokemon, () => battle.makeChoices('move scratch', 'move endure'), 'Attacked after waking up');
+	});
+
+	it('should allow the user to act the turn it wakes up, if it was loafing the turn it fell asleep', function () {
+		battle = common.createBattle();
+		battle.join('p1', 'Guest 1', 1, [{species: "Slaking", ability: 'truant', moves: ['scratch', 'irondefense']}]);
+		battle.join('p2', 'Guest 2', 1, [{species: "Steelix", ability: 'sturdy', moves: ['endure', 'spore']}]);
+		let user = battle.p1.active[0];
+		let pokemon = battle.p2.active[0];
+
+		battle.makeChoices('move irondefense', 'move endure');
+		battle.makeChoices('move irondefense', 'move spore');
+		while (user.status === 'slp') {
+			assert.fullHP(pokemon);
+			battle.makeChoices('move scratch', 'move endure');
+		}
+		assert.false.fullHP(pokemon);
 	});
 
 	it('should cause two-turn moves to fail', function () {
