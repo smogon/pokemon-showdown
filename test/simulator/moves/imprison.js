12acbd9b719f33141e4f0e0658705d0292450b8b
MacChaeger <msaimrkon@gmail.com>
2018-05-23 14:40:36 -0500

    Imprison shouldn't stop z-status moves (#4686)

--- test/simulator/moves/imprison.js
+++ test/simulator/moves/imprison.js
@@ -20,4 +20,13 @@ describe('Imprison', function () {
 		battle.makeChoices('move imprison', 'move calmmind');
 		assert.strictEqual(battle.p2.active[0].lastMove.id, 'confusion');
 	});
+
+	it('should not prevent foes from using Z-Powered Status moves', function () {
+		battle = common.createBattle();
+		battle.join('p1', 'Guest 1', 1, [{species: 'Sableye', ability: 'prankster', moves: ['imprison', 'sunnyday']}]);
+		battle.join('p2', 'Guest 2', 1, [{species: 'Charmander', ability: 'blaze', item: 'firiumz', moves: ['sunnyday']}]);
+		battle.makeChoices('move imprison', 'move sunnyday zmove');
+		assert.statStage(battle.p2.active[0], 'spe', 1);
+		assert.ok(battle.isWeather('sunnyday'));
+	});
 });
