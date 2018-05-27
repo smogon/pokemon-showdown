a3dc96ea5b28c0300f35e3289d8c4130abcdaba9
urkerab <urkerab@users.noreply.github.com>
2018-05-27 18:34:56 +0100

    Psycho Shift should fail verbosely

--- data/moves.js
+++ data/moves.js
@@ -12589,12 +12589,14 @@ let BattleMovedex = {
 		pp: 10,
 		priority: 0,
 		flags: {protect: 1, mirror: 1},
-		onHit: function (target, pokemon) {
-			if (pokemon.status && !target.status && target.trySetStatus(pokemon.status)) {
+		onPrepareHit: function (target, source, move) {
+			if (!source.status) return false;
+			move.status = source.status;
+		},
+		self: {
+			onHit: function (pokemon) {
 				pokemon.cureStatus();
-			} else {
-				return false;
-			}
+			},
 		},
 		secondary: false,
 		target: "normal",
