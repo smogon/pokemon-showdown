7b391bfb43acac00c88820a90651eb65b52214da
Spandan Punwatkar <spandan.punwatkar@gmail.com>
2018-07-01 15:03:40 +0530

    Update formats.js

--- config/formats.js
+++ config/formats.js
@@ -458,7 +458,7 @@ let Formats = [
 	},
 	{
 		name: "[Gen 7] Reversed",
-		desc: `Every Pok&eacute;mon has ALL of it's base Physical and Special stats swapped.`,
+		desc: `Every Pok&eacute;mon has its base Physical and Special stats swapped.`,
 		threads: [
 			`&bullet; <a href="https://www.smogon.com/forums/threads/3623871/">Reversed</a>`,
 		],
