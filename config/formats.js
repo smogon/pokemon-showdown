dcfb70ac05436798d15fbd8b59749614d10f13ce
The Immortal <the_immortal123@live.com>
2018-07-03 18:48:29 +0800

    Remove NU suspect test

--- config/formats.js
+++ config/formats.js
@@ -100,17 +100,8 @@ let Formats = [
 		],
 
 		mod: 'gen7',
-		searchShow: false,
 		ruleset: ['[Gen 7] RU'],
 		banlist: ['RU', 'NUBL', 'Drought'],
-	},
-	{
-		name: "[Gen 7] NU (suspect test)",
-		threads: [`&bullet; <a href="https://www.smogon.com/forums/threads/3637061/">NU Suspect Test</a>`],
-
-		mod: 'gen7',
-		challengeShow: false,
-		ruleset: ['[Gen 7] NU'],
 	},
 	{
 		name: "[Gen 7] PU",
