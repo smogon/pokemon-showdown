0e923e01be6b628e2169e45801ef32c3b5377daa
Harman <harmansinghgwalior1@gmail.com>
2018-07-04 23:24:02 +0530

    Specify seconds and UNO game

--- chat-plugins/uno.js
+++ chat-plugins/uno.js
@@ -609,11 +609,11 @@ exports.commands = {
 				return;
 			}
 			const amount = parseInt(target);
-			if (!amount || amount < 30 || amount > 600) return this.errorReply("The amount must be a number between 30 and 600.");
+			if (!amount || amount < 30 || amount > 600) return this.errorReply("The amount must be a number between 30 and 600 seconds.");
 			if (room.game.state !== 'signups') return this.errorReply("The game of UNO has already started.");
 			if (room.game.autostartTimer) clearTimeout(room.game.autostartTimer);
 			room.game.autostartTimer = setTimeout(room.game.onStart, amount * 1000);
-			this.addModAction(`${user.name} has set the autostart timer to ${amount} seconds.`);
+			this.addModAction(`${user.name} has set the UNO autostart timer to ${amount} seconds.`);
 		},
 
 		dq: 'disqualify',
