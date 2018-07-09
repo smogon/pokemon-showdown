b20c7afa4fe71d1eb517ed83940a8e3b9d88a5f3
The Immortal <the_immortal123@live.com>
2018-07-10 02:09:28 +0800

    Remove Ubers suspect test

--- config/formats.js
+++ config/formats.js
@@ -53,16 +53,6 @@ let Formats = [
 		],
 
 		mod: 'gen7',
-		searchShow: false,
-		ruleset: ['Pokemon', 'Standard', 'Team Preview', 'Mega Rayquaza Clause'],
-		banlist: ['Baton Pass'],
-	},
-	{
-		name: "[Gen 7] Ubers (suspect test)",
-		threads: [`&bullet; <a href="https://www.smogon.com/forums/threads/3637068/">Ubers Suspect Test</a>`],
-
-		mod: 'gen7',
-		challengeShow: false,
 		ruleset: ['Pokemon', 'Standard', 'Team Preview', 'Mega Rayquaza Clause'],
 		banlist: ['Baton Pass'],
 	},
