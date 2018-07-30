b2f36aac26dc27ffc9386bf79cd68815992f22f9
whales <jumbowhales@gmail.com>
2018-07-30 20:17:17 +0930

    Scavs: Point system updates

--- chat-plugins/scavengers.js
+++ chat-plugins/scavengers.js
@@ -12,8 +12,12 @@
 
 const fs = require('fs');
 
-const DEFAULT_POINTS = [20, 15, 10, 5, 1];
-const DEFAULT_BLITZ_POINTS = 10;
+const DEFAULT_POINTS = {
+	official: [20, 15, 10, 5, 1],
+};
+const DEFAULT_BLITZ_POINTS = {
+	official: 10,
+};
 const DEFAULT_TIMER_DURATION = 120;
 
 const path = require('path');
@@ -27,6 +31,18 @@ const FILTER_LENIENCY = 7;
 const HISTORY_PERIOD = 6; // months
 
 const ScavengerGames = require("./scavenger-games");
+
+// convert points stored in the old format
+const scavsRoom = Rooms('scavengers');
+if (scavsRoom && Array.isArray(scavsRoom.winPoints)) {
+	scavsRoom.winPoints = {official: scavsRoom.winPoints.slice()};
+	scavsRoom.blitzPoints = {official: scavsRoom.blitzPoints};
+	if (scavsRoom.chatRoomData) {
+		scavsRoom.chatRoomData.winPoints = scavsRoom.winPoints;
+		scavsRoom.chatRoomData.blitzPoints = scavsRoom.blitzPoints;
+		scavsRoom.global.writeChatRoomData();
+	}
+}
 
 class Ladder {
 	constructor(file) {
@@ -366,7 +382,7 @@ class ScavengerHunt extends Rooms.RoomGame {
 		let now = Date.now();
 		let time = Chat.toDurationString(now - this.startTime, {hhmmss: true});
 
-		let blitz = this.gameType === 'official' && now - this.startTime <= 60000;
+		let blitz = (((this.room.blitzPoints && this.room.blitzPoints[this.gameType]) || DEFAULT_BLITZ_POINTS[this.gameType]) && now - this.startTime <= 60000);
 
 		player.completed = true;
 		this.completed.push({name: player.name, time: time, blitz: blitz});
@@ -385,15 +401,15 @@ class ScavengerHunt extends Rooms.RoomGame {
 		if (!reset) {
 			// give points for winning and blitzes in official games
 
-			let winPoints = this.room.winPoints || DEFAULT_POINTS;
-			let blitzPoints = this.room.blitzPoints || DEFAULT_BLITZ_POINTS;
+			let winPoints = (this.room.winPoints && this.room.winPoints[this.gameType]) || DEFAULT_POINTS[this.gameType];
+			let blitzPoints = (this.room.blitzPoints && this.room.blitzPoints[this.gameType]) || DEFAULT_BLITZ_POINTS[this.gameType];
 
-			if (this.gameType === 'official') {
+			if (winPoints || blitzPoints) {
 				for (const [i, completed] of this.completed.entries()) {
 					if (!completed.blitz && i >= winPoints.length) break; // there won't be any more need to keep going
 					let name = completed.name;
 					if (winPoints[i]) Leaderboard.addPoints(name, 'points', winPoints[i]);
-					if (completed.blitz) Leaderboard.addPoints(name, 'points', blitzPoints);
+					if (blitzPoints && completed.blitz) Leaderboard.addPoints(name, 'points', blitzPoints);
 				}
 				Leaderboard.write();
 			}
@@ -807,12 +823,13 @@ let commands = {
 	 */
 	createpractice: 'create',
 	createofficial: 'create',
+	createunrated: 'create',
 	create: function (target, room, user, connection, cmd) {
 		if (room.id !== 'scavengers') return this.errorReply("Scavenger hunts can only be created in the scavengers room.");
 		if (!this.can('mute', null, room)) return false;
 		if (room.game && !room.game.scavParentGame) return this.errorReply(`There is already a game in this room - ${room.game.title}.`);
-		if (!cmd.includes('official') && !cmd.includes('practice') && room.scavQueue.length && !(room.game && room.scavParentGame)) return this.errorReply("There are currently hunts in the queue! If you would like to start the hunt anyways, use /forcestarthunt.");
-		let gameType = cmd.includes('official') ? 'official' : cmd.includes('practice') ? 'practice' : null;
+		if (!cmd.includes('official') && !cmd.includes('practice') && room.scavQueue && room.scavQueue.length && !(room.game && room.scavParentGame)) return this.errorReply("There are currently hunts in the queue! If you would like to start the hunt anyways, use /forcestarthunt.");
+		let gameType = cmd.includes('official') ? 'official' : cmd.includes('practice') ? 'practice' : cmd.includes('unrated') ? 'unrated' : 'regular';
 
 		let [hostsArray, ...params] = target.split('|');
 		let hosts = ScavengerHunt.parseHosts(hostsArray.split(/[,;]/), room, gameType === 'official');
@@ -1127,12 +1144,15 @@ let commands = {
 		if (room.id !== 'scavengers') return this.errorReply("This command can only be used in the scavengers room.");
 		if (!this.runBroadcast()) return false;
 
-		Leaderboard.visualize('points').then(ladder => {
-			this.sendReply(`|raw|<div class="ladder" style="overflow-y: scroll; max-height: 300px;"><table style="width: 100%"><tr><th>Rank</th><th>Name</th><th>Points</th></tr>${ladder.map(entry => {
-				let isStaff = room.auth && room.auth[toId(entry.name)];
+		const isChange = (!this.broadcasting && target);
+		const hideStaff = (!this.broadcasting && this.meansNo(target));
 
+		Leaderboard.visualize('points').then(ladder => {
+			this.sendReply(`|uhtml${isChange ? 'change' : ''}|scavladder|<div class="ladder" style="overflow-y: scroll; max-height: 300px;"><table style="width: 100%"><tr><th>Rank</th><th>Name</th><th>Points</th></tr>${ladder.map(entry => {
+				let isStaff = room.auth && room.auth[toId(entry.name)];
+				if (isStaff && hideStaff) return '';
 				return `<tr><td>${entry.rank}</td><td>${(isStaff ? `<em>${Chat.escapeHTML(entry.name)}</em>` : (entry.rank <= 5 ? `<strong>${Chat.escapeHTML(entry.name)}</strong>` : Chat.escapeHTML(entry.name)))}</td><td>${entry.points}</td></tr>`;
-			}).join('')}</table></div>`);
+			}).join('')}</table></div><div style="text-align: center"><button class="button" name="send" value="/scav top ${hideStaff ? 'yes' : 'no'}">${hideStaff ? "Show" : "Hide"} Auth</button></div>`);
 			if (this.broadcasting) setImmediate(() => room.update()); // make sure the room updates for broadcasting since this is async.
 		});
 	},
@@ -1157,43 +1177,65 @@ let commands = {
 	setblitz: function (target, room, user) {
 		if (room.id !== 'scavengers') return this.errorReply("This command can only be used in the scavengers room.");
 		if (!this.can('mute', null, room)) return false; // perms for viewing only
-		if (!target) return this.sendReply(`The points rewarded for winning official hunts is: ${(room.blitzPoints || DEFAULT_BLITZ_POINTS)}`);
+
+		if (!target) {
+			let points = [];
+			const source = Object.entries(Object.assign(DEFAULT_BLITZ_POINTS, room.blitzPoints || {}));
+			for (const entry of source) {
+				points.push(`${entry[0]}: ${entry[1]}`);
+			}
+			return this.sendReplyBox(`The points rewarded for winning hunts is:<br />${points.join('<br />')}`);
+		}
 
 		if (!this.can('declare', null, room)) return false; // perms for editing
 
-		let blitzPoints = parseInt(target);
-		if (isNaN(blitzPoints) || blitzPoints < 0 || blitzPoints > 1000) return this.errorReply("The points value awarded for blitz must be an integer bewteen 0 and 1000.");
+		let [type, blitzPoints] = target.split(',');
+		blitzPoints = parseInt(blitzPoints);
+		type = toId(type);
 
-		room.blitzPoints = blitzPoints;
+		if (isNaN(blitzPoints) || blitzPoints < 0 || blitzPoints > 1000) return this.errorReply("The points value awarded for blitz must be an integer bewteen 0 and 1000.");
+		if (!room.blitzPoints) room.blitzPoints = {};
+		room.blitzPoints[type] = blitzPoints;
 
 		if (room.chatRoomData) {
 			room.chatRoomData.blitzPoints = room.blitzPoints;
 			Rooms.global.writeChatRoomData();
 		}
-		this.privateModAction(`(${user.name} has set the points awarded for blitz to ${blitzPoints}.)`);
-		this.modlog('SCAV BLITZ', null, blitzPoints);
+		this.privateModAction(`(${user.name} has set the points awarded for blitz for ${type} hunts to ${blitzPoints}.)`);
+		this.modlog('SCAV BLITZ', null, `${type}: ${blitzPoints}`);
 	},
 
 	setpoints: function (target, room, user) {
 		if (room.id !== 'scavengers') return this.errorReply("This command can only be used in the scavengers room.");
 		if (!this.can('mute', null, room)) return false; // perms for viewing only
-		if (!target) return this.sendReply(`The points rewarded for winning official hunts is: ${(room.winPoints || DEFAULT_POINTS).map((p, i) => `(${(i + 1)}) ${p}`).join(', ')}`);
+
+		if (!target) {
+			let points = [];
+			const source = Object.entries(Object.assign(DEFAULT_POINTS, room.winPoints || {}));
+			for (const entry of source) {
+				points.push(`${entry[0]}: ${entry[1].map((p, i) => `(${(i + 1)}) ${p}`).join(', ')}`);
+			}
+			return this.sendReplyBox(`The points rewarded for winning hunts is:<br />${points.join('<br />')}`);
+		}
 
 		if (!this.can('declare', null, room)) return false; // perms for editting
 
-		let winPoints = target.split(',').map(p => parseInt(p));
+		let [type, ...pointsSet] = target.split(',');
+		type = toId(type);
+		let winPoints = pointsSet.map(p => parseInt(p));
 
 		if (winPoints.some(p => isNaN(p) || p < 0 || p > 1000) || !winPoints.length) return this.errorReply("The points value awarded for winning a scavenger hunt must be an integer between 0 and 1000.");
 
-		room.winPoints = winPoints;
+		if (!room.winPoints) room.winPoints = {};
+		room.winPoints[type] = winPoints;
 
 		if (room.chatRoomData) {
 			room.chatRoomData.winPoints = room.winPoints;
 			Rooms.global.writeChatRoomData();
 		}
 		const pointsDisplay = winPoints.map((p, i) => `(${(i + 1)}) ${p}`).join(', ');
-		this.privateModAction(`(${user.name} has set the points awarded for winning an official scavenger hunt to - ${pointsDisplay})`);
-		this.modlog('SCAV SETPOINTS', null, pointsDisplay);
+		this.privateModAction(`(${user.name} has set the points awarded for winning ${type} scavenger hunts to - ${pointsDisplay})`);
+		this.modlog('SCAV SETPOINTS', null, `${type}: ${pointsDisplay}`);
 	},
 
 	/**
