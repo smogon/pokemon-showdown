b3bc805adca238985fff3a4f3a1be1b730ffcac7
buck20two <buck.20.two@gmail.com>
2018-08-06 23:15:36 -0600

    return actual last move target instead of true on moves like rollout

--- sim/battle.js
+++ sim/battle.js
@@ -769,6 +769,9 @@ class Battle extends Dex.ModdedDex {
 		effect = this.getEffect(effect);
 		let args = [target, source, effect];
 		//console.log('Event: ' + eventid + ' (depth ' + this.eventDepth + ') t:' + target.id + ' s:' + (!source || source.id) + ' e:' + effect.id);
+		if (eventid == 'LockMoveTarget') {
+			relayVar = target.lastMoveTargetLoc;
+		}
 		if (relayVar === undefined || relayVar === null) {
 			relayVar = true;
 			hasRelayVar = false;
@@ -869,7 +872,6 @@ class Battle extends Dex.ModdedDex {
 			relayVar = this.modify(relayVar, this.event.modifier);
 		}
 		this.event = parentEvent;
-
 		return relayVar;
 	}
 
