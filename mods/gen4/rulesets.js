8da6628bd8c30a1a0d5f1d37e02d055571f2bba8
Marty-D <Marty-D@users.noreply.github.com>
2018-08-25 15:19:43 -0400

    Gen IV: Allow Arceus with max EVs by default
    https://www.smogon.com/forums/threads/changes-to-arceuss-legality-dpp-ubers-revisited.3593683/
    This lets people play whichever ruleset they want for custom Ubers tournaments.

--- mods/gen4/rulesets.js
+++ mods/gen4/rulesets.js
@@ -11,10 +11,10 @@ let BattleFormats = {
 			if (item && item.id === 'griseousorb' && template.num !== 487) {
 				return ['Griseous Orb can only be held by Giratina in Generation 4.'];
 			}
-			if (template.num === 493 && set.evs) {
+			if (template.num === 493 && set.evs && (set.moves.includes('roaroftime') || set.moves.includes('shadowforce') || set.moves.includes('spacialrend'))) {
 				for (let stat in set.evs) {
 					// @ts-ignore
-					if (set.evs[stat] > 100) return ["Arceus may not have more than 100 of any EVs in Generation 4."];
+					if (set.evs[stat] > 100) return ["Event Arceus may not have more than 100 of any EVs in Generation 4."];
 				}
 			}
 		},
