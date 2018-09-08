7b58e11613fe424f7030f96a0fae975bbb730417
HoeenHero <HoeenCoder@users.noreply.github.com>
2018-09-08 12:11:24 -0400

    Uno: Don't delete properties

--- chat-plugins/uno.js
+++ chat-plugins/uno.js
@@ -128,6 +128,10 @@ class UnoGame extends Rooms.RoomGame {
 		this.discards = [];
 		/** @type {Card?} */
 		this.topCard = null;
+		/** @type {string?} */
+		this.awaitUno = null;
+		/** @type {string?} */
+		this.unoId = null;
 
 		this.direction = 1;
 
@@ -266,7 +270,7 @@ class UnoGame extends Rooms.RoomGame {
 			if (this.timer) clearTimeout(this.timer);
 			this.nextTurn();
 		}
-		if (this.awaitUno === userid) delete this.awaitUno;
+		if (this.awaitUno === userid) this.awaitUno = null;
 
 		// put that player's cards into the discard pile to prevent cards from being permanently lost
 		this.discards.push(...this.players[userid].hand);
@@ -341,7 +345,7 @@ class UnoGame extends Rooms.RoomGame {
 
 				this.sendToRoom(`|c:|${(Math.floor(Date.now() / 1000))}|~|${player.name}'s turn.`);
 				this.state = 'play';
-				if (player.cardLock) delete player.cardLock;
+				if (player.cardLock) player.cardLock = null;
 				player.sendDisplay();
 
 				this.timer = setTimeout(() => {
@@ -559,8 +563,8 @@ class UnoGame extends Rooms.RoomGame {
 		// uno id makes spamming /uno uno impossible
 		if (this.unoId !== unoId || player.userid !== this.awaitUno) return false;
 		this.sendToRoom(Chat.html`|raw|<strong>UNO!</strong> ${player.name} is down to their last card!`);
-		delete this.awaitUno;
-		delete this.unoId;
+		this.awaitUno = null;
+		this.unoId = null;
 	}
 
 	onCheckUno() {
@@ -570,8 +574,8 @@ class UnoGame extends Rooms.RoomGame {
 				this.sendToRoom(`${this.players[this.awaitUno].name} forgot to say UNO! and is forced to draw 2 cards.`);
 				this.onDrawCard(this.players[this.awaitUno], 2);
 			}
-			delete this.awaitUno;
-			delete this.unoId;
+			this.awaitUno = null;
+			this.unoId = null;
 		}
 	}
 
@@ -615,7 +619,8 @@ class UnoGamePlayer extends Rooms.RoomGamePlayer {
 		super(user, game);
 		this.hand = /** @type {Card[]} */ ([]);
 		this.game = game;
-		this.cardLock = '';
+		/** @type {string?} */
+		this.cardLock = null;
 	}
 
 	/**
