fac9e369e570b420f4deae18a0c3efa02ccee1d2
Bär Halberkamp <fledaway@gmail.com>
2018-09-19 15:34:32 +0200

    Chat monitor: use message to parse wordfilter regex to avoid failing the replace

--- chat-plugins/chat-monitor.js
+++ chat-plugins/chat-monitor.js
@@ -150,12 +150,12 @@ let chatfilter = function (message, user, room) {
 	for (let line of filterWords.wordfilter) {
 		const regex = line[0];
 		if (typeof regex === 'string') continue;
-		let match = regex.exec(lcMessage);
+		let match = regex.exec(message);
 		while (match) {
 			message = message.replace(match[0], line[2] || '');
 			line[3]++;
 			saveFilters();
-			match = regex.exec(lcMessage);
+			match = regex.exec(message);
 		}
 	}
 
