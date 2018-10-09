smogon/pokemon-showdown

commit 9d5b0968518713c5e3bf82ed814458a62fe1b4e3

Trim target in /declare [22:30] +Zyg: is there a reason why you can declare a blank message? it's nothing major but you can do /declare with two spaces after it and it declares a blank message but gives the HL

chat-commands.js
@@ -2332,6 +2332,7 @@ const commands = {
 	},
 
 	declare: function (target, room, user) {
+		target = target.trim();
 		if (!target) return this.parse('/help declare');
 		if (!this.can('declare', null, room)) return false;
 		if (!this.canTalk()) return;

commit 0007e7865fd4de83ba8df56a6c6d30807704893a

Declare: Fix a potential crash
Introduced in https://github.com/Zarel/Pokemon-Showdown/commit/9d5b0968518713c5e3bf82ed814458a62fe1b4e3

chat-commands.js
@@ -2332,7 +2332,7 @@ const commands = {
 	},
 
 	declare: function (target, room, user) {
-		target = target.trim();
+		if (target) target = target.trim();
 		if (!target) return this.parse('/help declare');
 		if (!this.can('declare', null, room)) return false;
 		if (!this.canTalk()) return;