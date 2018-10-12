2c5a77af96a626ea8a0ad19ae83044c810b5e787
Guangcong Luo <guangcongluo@gmail.com>
2018-10-12 16:58:05 -0500

    Fix crash in Gen 1 Bide

--- mods/gen1/moves.js
+++ mods/gen1/moves.js
@@ -98,7 +98,7 @@ let BattleMovedex = {
 					}
 				}
 			},
-			onBeforeMove: function (pokemon) {
+			onBeforeMove: function (pokemon, target, move) {
 				if (this.effectData.duration === 1) {
 					if (!this.effectData.totalDamage) {
 						this.add('-fail', pokemon);
@@ -106,7 +106,7 @@ let BattleMovedex = {
 					}
 					this.add('-end', pokemon, 'Bide');
 					let target = this.effectData.sourceSide.active[this.effectData.sourcePosition];
-					this.moveHit(target, pokemon, 'bide', /** @type {ActiveMove} */ ({damage: this.effectData.totalDamage * 2}));
+					this.moveHit(target, pokemon, move, /** @type {ActiveMove} */ ({damage: this.effectData.totalDamage * 2}));
 					return false;
 				}
 				this.add('-activate', pokemon, 'Bide');
