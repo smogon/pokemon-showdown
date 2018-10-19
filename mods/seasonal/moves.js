2b1c75a82250537dbfbc965aef59287e0f1be19c
ImLycan <worstdigiroommod@gmail.com>
2018-10-19 17:05:36 -0400

    SSB: Fix Prismatic Terrain interaction with Pilfer (#136)

--- mods/seasonal/moves.js
+++ mods/seasonal/moves.js
@@ -2648,8 +2648,8 @@ let BattleMovedex = {
 				}
 				return 5;
 			},
-			onBeforeMovePriority: 2,
-			onBeforeMove: function (target, source, move) {
+			onTryHitPriority: 2,
+			onTryHit: function (target, source, move) {
 				let hazardMoves = ['reflect', 'lightscreen', 'auroraveil', 'safeguard', 'mist', 'spikes', 'toxicspikes', 'stealthrock', 'stickyweb', 'hazardpass', 'beskyttelsesnet', 'bringerofdarkness', 'soulbend', 'smokebomb', 'hurl'];
 				if (hazardMoves.includes(move.id)) {
 					this.add('-message', `${source.name} couldn't use ${move.name} because of the prismatic terrain!`);
