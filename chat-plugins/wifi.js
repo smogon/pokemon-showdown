d0c02da6c95427d9f6973da2c8f7ccc3e5adbe32
Bär Halberkamp <fledaway@gmail.com>
2018-10-20 20:42:48 +0200

    Wifi: modlog when setting left to more than 1 lower

--- chat-plugins/wifi.js
+++ chat-plugins/wifi.js
@@ -825,7 +825,9 @@ let commands = {
 			if (isNaN(newamount)) return this.errorReply("Please enter a valid amount.");
 			// @ts-ignore
 			if (newamount > room.gtsga.left) return this.errorReply("The new amount must be lower than the old amount.");
-
+			// @ts-ignore
+			if (newamount < room.gtsga.left - 1) this.modlog(`GTS GIVEAWAY`, null, `set to ${newamount} left by ${user.name}`);
+			
 			// @ts-ignore
 			room.gtsga.updateLeft(newamount);
 		},
