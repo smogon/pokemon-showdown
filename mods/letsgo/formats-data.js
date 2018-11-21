9d06871b070e8cc6c5be0d049b1a45ab0c4d67fa
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2018-11-21 00:07:31 -0700

    Update Let's Go tiers (#4986)

--- mods/letsgo/formats-data.js
+++ mods/letsgo/formats-data.js
@@ -7,112 +7,852 @@ let BattleFormatsData = {
 		eventPokemon: [
 			{"generation": 7, "level": 12, "shiny": 1, "ivs": {hp: 31, atk: 25, def: 30, spa: 25, spd: 30, spe: 25}, "moves": ["leechseed", "vinewhip", "growl", "tackle"], "pokeball": "pokeball"},
 		],
+		tier: "LC",
+	},
+	ivysaur: {
+		inherit: true,
+		tier: "NFE",
+	},
+	venusaur: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	venusaurmega: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
 	},
 	charmander: {
 		inherit: true,
 		eventPokemon: [
 			{"generation": 7, "level": 14, "shiny": 1, "ivs": {hp: 25, atk: 30, def: 25, spa: 30, spd: 25, spe: 31}, "moves": ["ember", "smokescreen", "growl", "scratch"], "pokeball": "pokeball"},
 		],
+		tier: "LC",
+	},
+	charmeleon: {
+		inherit: true,
+		tier: "NFE",
+	},
+	charizard: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	charizardmegax: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	charizardmegay: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
 	},
 	squirtle: {
 		inherit: true,
 		eventPokemon: [
 			{"generation": 7, "level": 16, "shiny": 1, "ivs": {hp: 25, atk: 25, def: 30, spa: 31, spd: 30, spe: 25}, "moves": ["withdraw", "bubble", "tailwhip", "tackle"], "pokeball": "pokeball"},
 		],
+		tier: "LC",
+	},
+	wartortle: {
+		inherit: true,
+		tier: "NFE",
+	},
+	blastoise: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	blastoisemega: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	caterpie: {
+		inherit: true,
+		tier: "LC",
+	},
+	metapod: {
+		inherit: true,
+		tier: "NFE",
+	},
+	butterfree: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	weedle: {
+		inherit: true,
+		tier: "LC",
+	},
+	kakuna: {
+		inherit: true,
+		tier: "NFE",
+	},
+	beedrill: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	beedrillmega: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	pidgey: {
+		inherit: true,
+		tier: "LC",
+	},
+	pidgeotto: {
+		inherit: true,
+		tier: "NFE",
+	},
+	pidgeot: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	pidgeotmega: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	rattata: {
+		inherit: true,
+		tier: "LC",
+	},
+	rattataalola: {
+		inherit: true,
+		tier: "LC",
+	},
+	raticate: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	raticatealola: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	spearow: {
+		inherit: true,
+		tier: "LC",
+	},
+	fearow: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	ekans: {
+		inherit: true,
+		tier: "LC",
+	},
+	arbok: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	pikachu: {
+		inherit: true,
+		tier: "LC",
 	},
 	pikachustarter: {
 		inherit: true,
 		isNonstandard: false,
-		tier: 'PU',
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	raichu: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	raichualola: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	sandshrew: {
+		inherit: true,
+		tier: "LC",
+	},
+	sandshrewalola: {
+		inherit: true,
+		tier: "LC",
+	},
+	sandslash: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	sandslashalola: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	nidoranf: {
+		inherit: true,
+		tier: "LC",
+	},
+	nidorina: {
+		inherit: true,
+		tier: "NFE",
+	},
+	nidoqueen: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	nidoranm: {
+		inherit: true,
+		tier: "LC",
+	},
+	nidorino: {
+		inherit: true,
+		tier: "NFE",
+	},
+	nidoking: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	clefairy: {
+		inherit: true,
+		tier: "LC",
+	},
+	clefable: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	vulpix: {
+		inherit: true,
+		tier: "LC",
+	},
+	vulpixalola: {
+		inherit: true,
+		tier: "LC",
+	},
+	ninetales: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	ninetalesalola: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	jigglypuff: {
+		inherit: true,
+		tier: "LC",
+	},
+	wigglytuff: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	zubat: {
+		inherit: true,
+		tier: "LC",
+	},
+	golbat: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	oddish: {
+		inherit: true,
+		tier: "LC",
+	},
+	gloom: {
+		inherit: true,
+		tier: "NFE",
+	},
+	vileplume: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	paras: {
+		inherit: true,
+		tier: "LC",
+	},
+	parasect: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	venonat: {
+		inherit: true,
+		tier: "LC",
+	},
+	venomoth: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	diglett: {
+		inherit: true,
+		tier: "LC",
+	},
+	diglettalola: {
+		inherit: true,
+		tier: "LC",
+	},
+	dugtrio: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	dugtrioalola: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	meowth: {
+		inherit: true,
+		tier: "LC",
+	},
+	meowthalola: {
+		inherit: true,
+		tier: "LC",
 	},
 	persian: {
 		inherit: true,
 		eventPokemon: [
 			{"generation": 7, "level": 16, "shiny": 1, "ivs": {hp: 30, atk: 30, def: 25, spa: 25, spd: 25, spe: 31}, "moves": ["feint", "payday", "taunt", "fakeout"], "pokeball": "pokeball"},
 		],
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	persianalola: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	psyduck: {
+		inherit: true,
+		tier: "LC",
+	},
+	golduck: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	mankey: {
+		inherit: true,
+		tier: "LC",
+	},
+	primeape: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	growlithe: {
+		inherit: true,
+		tier: "LC",
 	},
 	arcanine: {
 		inherit: true,
 		eventPokemon: [
 			{"generation": 7, "level": 16, "shiny": 1, "ivs": {hp: 25, atk: 30, def: 25, spa: 30, spd: 25, spe: 31}, "moves": ["roar", "leer", "ember", "doubleedge"], "pokeball": "pokeball"},
 		],
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	poliwag: {
+		inherit: true,
+		tier: "LC",
+	},
+	poliwhirl: {
+		inherit: true,
+		tier: "NFE",
+	},
+	poliwrath: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	abra: {
+		inherit: true,
+		tier: "LC",
+	},
+	kadabra: {
+		inherit: true,
+		tier: "NFE",
+	},
+	alakazam: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	alakazammega: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	machop: {
+		inherit: true,
+		tier: "LC",
+	},
+	machoke: {
+		inherit: true,
+		tier: "NFE",
+	},
+	machamp: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	bellsprout: {
+		inherit: true,
+		tier: "LC",
+	},
+	weepinbell: {
+		inherit: true,
+		tier: "NFE",
+	},
+	victreebel: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	tentacool: {
+		inherit: true,
+		tier: "LC",
+	},
+	tentacruel: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	geodude: {
+		inherit: true,
+		tier: "LC",
+	},
+	geodudealola: {
+		inherit: true,
+		tier: "LC",
+	},
+	graveler: {
+		inherit: true,
+		tier: "NFE",
+	},
+	graveleralola: {
+		inherit: true,
+		tier: "NFE",
+	},
+	golem: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	golemalola: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	ponyta: {
+		inherit: true,
+		tier: "LC",
+	},
+	rapidash: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	slowpoke: {
+		inherit: true,
+		tier: "LC",
+	},
+	slowbro: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	slowbromega: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	magnemite: {
+		inherit: true,
+		tier: "LC",
+	},
+	magneton: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	farfetchd: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	doduo: {
+		inherit: true,
+		tier: "LC",
+	},
+	dodrio: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	seel: {
+		inherit: true,
+		tier: "LC",
+	},
+	dewgong: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	grimer: {
+		inherit: true,
+		tier: "LC",
+	},
+	grimeralola: {
+		inherit: true,
+		tier: "LC",
+	},
+	muk: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	mukalola: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	shellder: {
+		inherit: true,
+		tier: "LC",
+	},
+	cloyster: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	gastly: {
+		inherit: true,
+		tier: "LC",
+	},
+	haunter: {
+		inherit: true,
+		tier: "NFE",
+	},
+	gengar: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	gengarmega: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	onix: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	drowzee: {
+		inherit: true,
+		tier: "LC",
+	},
+	hypno: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	krabby: {
+		inherit: true,
+		tier: "LC",
+	},
+	kingler: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
 	},
 	voltorb: {
 		inherit: true,
 		eventPokemon: [
 			{"generation": 7, "level": 42, "shiny": 1, "perfectIVs": 3, "moves": ["mirrorcoat", "thunderbolt", "swift", "selfdestruct"]},
 		],
+		tier: "LC",
 	},
 	electrode: {
 		inherit: true,
 		eventPokemon: [
 			{"generation": 7, "level": 42, "shiny": 1, "perfectIVs": 3, "moves": ["thunderbolt", "screech", "selfdestruct", "swift"]},
 		],
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	exeggcute: {
+		inherit: true,
+		tier: "LC",
+	},
+	exeggutor: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	exeggutoralola: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	cubone: {
+		inherit: true,
+		tier: "LC",
+	},
+	marowak: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	marowakalola: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
 	},
 	hitmonlee: {
 		inherit: true,
 		eventPokemon: [
 			{"generation": 7, "level": 30, "shiny": 1, "ivs": {hp: 25, atk: 30, def: 25, spa: 25, spd: 30, spe: 31}, "moves": ["jumpkick", "facade", "brickbreak", "feint"], "pokeball": "pokeball"},
 		],
+		tier: "OU",
+		doublesTier: "DOU",
 	},
 	hitmonchan: {
 		inherit: true,
 		eventPokemon: [
 			{"generation": 7, "level": 30, "shiny": 1, "ivs": {hp: 25, atk: 31, def: 30, spa: 25, spd: 30, spe: 25}, "moves": ["firepunch", "icepunch", "thunderpunch", "dizzypunch"], "pokeball": "pokeball"},
 		],
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	lickitung: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	koffing: {
+		inherit: true,
+		tier: "LC",
+	},
+	weezing: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	rhyhorn: {
+		inherit: true,
+		tier: "LC",
+	},
+	rhydon: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
 	},
 	chansey: {
 		inherit: true,
 		eventPokemon: [
 			{"generation": 7, "level": 1, "gender": "F", "nature": "Mild", "moves": ["celebrate", "pound"], "pokeball": "cherishball"},
 		],
+		tier: "OU",
+		doublesTier: "DOU",
 	},
-	lapras: {
+	tangela: {
 		inherit: true,
-		eventPokemon: [
-			{"generation": 7, "level": 34, "shiny": 1, "ivs": {hp: 31, atk: 25, def: 25, spa: 30, spd: 30, spe: 25}, "moves": ["bodyslam", "confuseray", "iceshard", "mist"], "pokeball": "pokeball"},
-		],
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	kangaskhan: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	kangaskhanmega: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	horsea: {
+		inherit: true,
+		tier: "LC",
+	},
+	seadra: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	goldeen: {
+		inherit: true,
+		tier: "LC",
+	},
+	seaking: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	staryu: {
+		inherit: true,
+		tier: "LC",
+	},
+	starmie: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	mrmime: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	scyther: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	jynx: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	electabuzz: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	magmar: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	pinsir: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	pinsirmega: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	tauros: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
 	},
 	magikarp: {
 		inherit: true,
 		eventPokemon: [
 			{"generation": 7, "level": 5, "shiny": 1, "ivs": {hp: 30, atk: 31, def: 25, spa: 25, spd: 25, spe: 31}, "moves": ["splash"], "pokeball": "pokeball"},
 		],
+		tier: "LC",
+	},
+	gyarados: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	gyaradosmega: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	lapras: {
+		inherit: true,
+		eventPokemon: [
+			{"generation": 7, "level": 34, "shiny": 1, "ivs": {hp: 31, atk: 25, def: 25, spa: 30, spd: 30, spe: 25}, "moves": ["bodyslam", "confuseray", "iceshard", "mist"], "pokeball": "pokeball"},
+		],
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	ditto: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
 	},
 	eevee: {
 		inherit: true,
 		eventPokemon: [
 			{"generation": 7, "level": 30, "shiny": 1, "moves": ["doubleedge", "takedown", "swift", "bite"], "pokeball": "pokeball"},
 		],
+		tier: "LC",
 	},
 	eeveestarter: {
 		inherit: true,
 		isNonstandard: false,
-		tier: 'PU',
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	vaporeon: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	jolteon: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	flareon: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
 	},
 	porygon: {
 		inherit: true,
 		eventPokemon: [
 			{"generation": 7, "level": 34, "shiny": 1, "ivs": {hp: 25, atk: 25, def: 30, spa: 31, spd: 30, spe: 25}, "moves": ["conversion", "thunderwave", "triattack", "barrier"], "pokeball": "pokeball"},
 		],
+		tier: "OU",
+		doublesTier: "DOU",
 	},
 	omanyte: {
 		inherit: true,
 		eventPokemon: [
 			{"generation": 7, "level": 44, "shiny": 1, "perfectIVs": 3, "moves": ["hydropump", "rockslide", "protect", "rockthrow"], "pokeball": "pokeball"},
 		],
+		tier: "LC",
+	},
+	omastar: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
 	},
 	kabuto: {
 		inherit: true,
 		eventPokemon: [
 			{"generation": 7, "level": 44, "shiny": 1, "perfectIVs": 3, "moves": ["rockslide", "sandattack", "rockthrow", "aquajet"], "pokeball": "pokeball"},
 		],
+		tier: "LC",
+	},
+	kabutops: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
 	},
 	aerodactyl: {
 		inherit: true,
 		eventPokemon: [
 			{"generation": 7, "level": 44, "shiny": 1, "perfectIVs": 3, "moves": ["rockslide", "crunch", "rockthrow", "agility"], "pokeball": "pokeball"},
 		],
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	aerodactylmega: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
 	},
 	snorlax: {
 		inherit: true,
@@ -120,6 +860,8 @@ let BattleFormatsData = {
 			{"generation": 7, "level": 34, "shiny": 1, "perfectIVs": 3, "moves": ["rest", "headbutt", "lick", "yawn"]},
 			{"generation": 7, "level": 34, "shiny": 1, "perfectIVs": 3, "moves": ["rest", "headbutt", "lick", "yawn"]},
 		],
+		tier: "OU",
+		doublesTier: "DOU",
 	},
 	articuno: {
 		inherit: true,
@@ -127,6 +869,8 @@ let BattleFormatsData = {
 			{"generation": 7, "level": 50, "shiny": 1, "perfectIVs": 3, "moves": ["reflect", "agility", "icebeam", "mirrorcoat"]},
 		],
 		eventOnly: false,
+		tier: "OU",
+		doublesTier: "DOU",
 	},
 	zapdos: {
 		inherit: true,
@@ -134,6 +878,8 @@ let BattleFormatsData = {
 			{"generation": 7, "level": 50, "shiny": 1, "perfectIVs": 3, "moves": ["lightscreen", "agility", "thunderbolt", "drillpeck"]},
 		],
 		eventOnly: false,
+		tier: "OU",
+		doublesTier: "DOU",
 	},
 	moltres: {
 		inherit: true,
@@ -141,28 +887,59 @@ let BattleFormatsData = {
 			{"generation": 7, "level": 50, "shiny": 1, "perfectIVs": 3, "moves": ["heatwave", "agility", "flamethrower", "airslash"]},
 		],
 		eventOnly: false,
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+	dratini: {
+		inherit: true,
+		tier: "LC",
+	},
+	dragonair: {
+		inherit: true,
+		tier: "NFE",
+	},
+	dragonite: {
+		inherit: true,
+		tier: "OU",
+		doublesTier: "DOU",
 	},
 	mewtwo: {
 		inherit: true,
 		eventPokemon: [
 			{"generation": 7, "level": 70, "shiny": 1, "perfectIVs": 3, "moves": ["psychic", "recover", "amnesia", "swift"]},
 		],
+		tier: "Uber",
+		doublesTier: "DUber",
+	},
+	mewtwomegax: {
+		inherit: true,
+		tier: "Uber",
+		doublesTier: "DUber",
+	},
+	mewtwomegay: {
+		inherit: true,
+		tier: "Uber",
+		doublesTier: "DUber",
 	},
 	mew: {
 		inherit: true,
 		eventPokemon: [
 			{"generation": 7, "level": 1, "perfectIVs": 3, "moves": ["pound"], "pokeball": "pokeball"},
 		],
+		tier: "OU",
+		doublesTier: "DOU",
 	},
 	meltan: {
 		inherit: true,
 		isNonstandard: false,
-		tier: 'PU',
+		tier: "OU",
+		doublesTier: "DOU",
 	},
 	melmetal: {
 		inherit: true,
 		isNonstandard: false,
-		tier: 'OU',
+		tier: "OU",
+		doublesTier: "DOU",
 	},
 };
 
