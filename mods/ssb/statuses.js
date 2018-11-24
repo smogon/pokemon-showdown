3e7a2cbde56516345a42af0e32823e702e6ed386
Lycanium Z <worstdigiroommod@gmail.com>
2018-11-24 16:05:56 -0500

    SSB: Snaquaza is G% now

--- mods/ssb/statuses.js
+++ mods/ssb/statuses.js
@@ -1143,15 +1143,15 @@ let BattleStatuses = {
 	snaquaza: {
 		noCopy: true,
 		onStart: function () {
-			this.add(`c|+Snaquaza|Snaq is baq... with a vengeance!`);
+			this.add(`c|%Snaquaza|Snaq is baq... with a vengeance!`);
 		},
 		onSwitchOut: function (pokemon) {
-			this.add(`c|+Snaquaza|Lynch Hoeen while I'm away...`);
+			this.add(`c|%Snaquaza|Lynch Hoeen while I'm away...`);
 			// @ts-ignore Hack for Snaquaza's Z move
 			if (pokemon.claimHP) delete pokemon.claimHP;
 		},
 		onFaint: function () {
-			this.add(`c|+Snaquaza|How did you know I was scum?`);
+			this.add(`c|%Snaquaza|How did you know I was scum?`);
 		},
 		onDamage: function (damage, pokemon) {
 			// @ts-ignore Hack for Snaquaza's Z move
