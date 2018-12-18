a4d6905aaa4f4c5800e7a833a7eb23ace6bce559
Kris Johnson <kris.langur@gmail.com>
2018-12-18 15:34:28 -0700

    Let's Go: Happiness is always 70 on wifi battles

--- mods/letsgo/scripts.js
+++ mods/letsgo/scripts.js
@@ -47,7 +47,7 @@ let BattleScripts = {
 		if (nature.plus) stats[nature.plus] = Math.floor(stats[nature.plus] * 1.1);
 		// @ts-ignore
 		if (nature.minus) stats[nature.minus] = Math.floor(stats[nature.minus] * 0.9);
-		set.happiness = (set.happiness === 0 ? 0 : (set.happiness || 255));
+		set.happiness = 70;
 		let friendshipValue = Math.floor((set.happiness / 255 / 10 + 1) * 100);
 		for (const stat in stats) {
 			if (stat !== 'hp') {
