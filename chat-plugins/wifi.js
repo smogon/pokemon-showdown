9a0f1c8f0041f8b040f67241d20e01f3968371ed
Bär Halberkamp <fledaway@gmail.com>
2018-12-19 03:20:09 +0100

    Wifi: show old amount in /gts left

--- chat-plugins/wifi.js
+++ chat-plugins/wifi.js
@@ -827,7 +827,7 @@ let commands = {
 			// @ts-ignore
 			if (newamount > room.gtsga.left) return this.errorReply("The new amount must be lower than the old amount.");
 			// @ts-ignore
-			if (newamount < room.gtsga.left - 1) this.modlog(`GTS GIVEAWAY`, null, `set to ${newamount} left`);
+			if (newamount < room.gtsga.left - 1) this.modlog(`GTS GIVEAWAY`, null, `set from ${room.gtsga.left} to ${newamount} left`);
 
 			// @ts-ignore
 			room.gtsga.updateLeft(newamount);
