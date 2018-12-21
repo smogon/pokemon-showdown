1e6d5fa8b198e2786a4ad9adf6e1ca7b6c814673
Necrozmas <crystalfranks18@hotmail.com>
2018-12-21 02:44:33 -0500

    Uploade it

---
+++ express.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
