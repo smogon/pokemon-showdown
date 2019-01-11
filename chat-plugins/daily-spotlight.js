f4af921d4aa89a0e9a44701fa6914ca0d6241f5b
Alexander B <mathfreak231@users.noreply.github.com>
2019-01-11 13:40:39 -0600

    more ugh

--- chat-plugins/daily-spotlight.js
+++ chat-plugins/daily-spotlight.js
@@ -153,7 +153,7 @@ const commands = {
 	daily: async function (target, room, user) {
 		let key = toId(target);
 		if (!key) return this.parse('/help daily');
-		
+
 		if (!spotlights[room.id] || !spotlights[room.id][key]) {
 			return this.errorReply(`Cannot find a daily spotlight with name '${key}'`);
 		}
