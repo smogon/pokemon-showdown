7c9adb72f218d0623fe9c8d54390cbc5ee291ca0
Lycanium Z <worstdigiroommod@gmail.com>
2019-01-19 21:53:39 -0500

    SSB: update promotions

--- mods/ssb/statuses.js
+++ mods/ssb/statuses.js
@@ -355,13 +355,13 @@ let BattleStatuses = {
 	cleo: {
 		noCopy: true,
 		onStart: function () {
-			this.add(`c|+Cleo|Cleo! Cleo! Your friendly neighborhood Sea Leo!`);
+			this.add(`c|%Cleo|Cleo! Cleo! Your friendly neighborhood Sea Leo!`);
 		},
 		onSwitchOut: function () {
-			this.add(`c|+Cleo|bbl~`);
+			this.add(`c|%Cleo|bbl~`);
 		},
 		onFaint: function () {
-			this.add(`c|+Cleo|n.n`);
+			this.add(`c|%Cleo|n.n`);
 		},
 	},
 	deg: {
@@ -652,13 +652,13 @@ let BattleStatuses = {
 	kaijubunny: {
 		noCopy: true,
 		onStart: function () {
-			this.add(`c|+Kaiju Bunny|Hey there! Good luck!`);
+			this.add(`c|%Kaiju Bunny|Hey there! Good luck!`);
 		},
 		onSwitchOut: function () {
-			this.add(`c|+Kaiju Bunny|Don't keep her from battling for too long!`);
+			this.add(`c|%Kaiju Bunny|Don't keep her from battling for too long!`);
 		},
 		onFaint: function () {
-			this.add(`c|+Kaiju Bunny|She tried her best... ;;`);
+			this.add(`c|%Kaiju Bunny|She tried her best... ;;`);
 		},
 		// Kaiju Rage Innate
 		// onUpdate so toxic orb can activate after. Code mainly copied from Power Construct.
@@ -881,13 +881,13 @@ let BattleStatuses = {
 	mitsuki: {
 		noCopy: true,
 		onStart: function () {
-			this.add(`c|+Mitsuki|SSSSSSSSSSSSS`);
+			this.add(`c|%Mitsuki|SSSSSSSSSSSSS`);
 		},
 		onSwitchOut: function () {
-			this.add(`c|+Mitsuki|sssssssssssss`);
+			this.add(`c|%Mitsuki|sssssssssssss`);
 		},
 		onFaint: function () {
-			this.add(`c|+Mitsuki|sss`);
+			this.add(`c|%Mitsuki|sss`);
 		},
 	},
 	moo: {
