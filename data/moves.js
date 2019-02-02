008dfbdd8a59721a49bf1d78a0d23046001b6a09
Marty-D <Marty-D@users.noreply.github.com>
2019-02-02 16:13:03 -0500

    Update Me First

--- data/moves.js
+++ data/moves.js
@@ -10098,10 +10098,10 @@ let BattleMovedex = {
 			let action = this.willMove(target);
 			if (action) {
 				let noMeFirst = [
-					'chatter', 'counter', 'covet', 'focuspunch', 'mefirst', 'metalburst', 'mirrorcoat', 'struggle', 'thief',
+					'beakblast', 'chatter', 'counter', 'covet', 'focuspunch', 'mefirst', 'metalburst', 'mirrorcoat', 'shelltrap', 'struggle', 'thief',
 				];
 				let move = this.getActiveMove(action.move.id);
-				if (move.category !== 'Status' && !noMeFirst.includes(move.id)) {
+				if (!action.zmove && !move.isZ && move.category !== 'Status' && !noMeFirst.includes(move.id)) {
 					pokemon.addVolatile('mefirst');
 					this.useMove(move, pokemon, target);
 					return null;
