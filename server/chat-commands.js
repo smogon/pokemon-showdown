20999820aa601e333e11e35eedb491436ec1abb9
Guangcong Luo <guangcongluo@gmail.com>
2019-02-05 02:34:37 +0100

    Update server/chat-commands.js
    Co-Authored-By: DubbleClick <marc@henderkes.com>

--- server/chat-commands.js
+++ server/chat-commands.js
@@ -2723,7 +2723,7 @@ const commands = {
 		if (!reason) {
 			return this.errorReply(`Battle bans require a reason.`);
 		}
-		if (!room.battle && (!reason.includes('.pokemonshowdown.com/') && cmd !== 'forcebattleban')) { // lgtm[js/incomplete-url-substring-sanitization]
+		if (!room.battle && (!reason.includes('.pokemonshowdown.com/') && cmd !== 'forcebattleban')) {
 			return this.errorReply(`Battle bans require a battle replay if used outside of a battle; if the battle has expired, use /forcebattleban.`);
 		}
 		if (!this.can('rangeban', targetUser)) {
