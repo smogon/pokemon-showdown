6a243ae8fb46a0b53cc1ccc198b4731be854f568
Raj Patel <raj.p.patel@gmail.com>
2019-02-05 15:49:40 -0500

    begin creating test AI

---
+++ test.js
@@ -0,0 +1,33 @@
+var readline = require('readline'),
+    rl = readline.createInterface(process.stdin, process.stdout);
+
+const Sim = require('./sim');
+stream = new Sim.BattleStream();
+
+(async () => {
+    let output;
+    while ((output = await stream.read())) {
+        console.log(output);
+    }
+})();
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+stream.write(`>start {"formatid":"gen7randombattle"}`);
+stream.write(`>player p1 {"name":"Me"}`);
+stream.write(`>player p2 {"name":"AI"}`);
+
+
+rl.setPrompt('');
+rl.prompt();
+
+rl.on('line', function(line) {
+    stream.write(line);
+    stream.write(`>p2 move ` + getRndInteger(1,4));
+    rl.prompt();
+}).on('close', function() {
+    console.log('Have a great day!');
+    process.exit(0);
+});
