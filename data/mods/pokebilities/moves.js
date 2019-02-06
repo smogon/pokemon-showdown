a70d13080c6f359005e718226642dfe4ed47dd2d
MacChaeger <msaimrkon@gmail.com>
2019-02-06 06:30:14 -0600

    Pokebilities: fix special abilities as well as Gastro Acid (#5140)

---
+++ data/mods/pokebilities/moves.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**@type {{[k: string]: ModdedMoveData}} */
+exports.BattleMovedex = {
+	"gastroacid": {
+		inherit: true,
+		effect: {
+			// Ability suppression implemented in Pokemon.ignoringAbility() within sim/pokemon.js
+			onStart: function (pokemon) {
+				this.add('-endability', pokemon);
+				this.singleEvent('End', this.getAbility(pokemon.ability), pokemon.abilityData, pokemon, pokemon, 'gastroacid');
+				if (pokemon.innates) pokemon.innates.forEach(innate => pokemon.removeVolatile("ability" + innate));
+			},
+		},
+	},
+};
