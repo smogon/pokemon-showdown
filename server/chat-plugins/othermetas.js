9271c809ca0d8d3b284905096c765fca09c26af7
uzelbot <47611030+uzelbot@users.noreply.github.com>
2019-02-20 06:59:50 +0000

    Remove * from most permissions shown in /help (#5168)

--- server/chat-plugins/othermetas.js
+++ server/chat-plugins/othermetas.js
@@ -61,7 +61,7 @@ const commands = {
 	},
 	othermetashelp: [
 		`/om - Provides links to information on the Other Metagames.`,
-		`!om - Show everyone that information. Requires: + % @ * # & ~`,
+		`!om - Show everyone that information. Requires: + % @ # & ~`,
 	],
 
 	'!mixandmega': true,
