cc1b7c352ad0c1e045aff5c0bfef20a50e3b4756
MacChaeger <msaimrkon@gmail.com>
2019-02-25 12:03:31 -0600

    Use runEvent

--- sim/pokemon.ts
+++ sim/pokemon.ts
@@ -953,12 +953,16 @@ export class Pokemon {
 		let template = this.battle.singleEvent('ModifyTemplate', this.battle.getFormat(), null, this, source, null, rawTemplate);
 =======
 	setTemplate(rawTemplate, source = this.battle.effect) {
+<<<<<<< HEAD:sim/pokemon.ts
 		let template = this.battle.singleEvent('ModifyTemplate', this.battle.getFormat(), null, this, null, source, rawTemplate);
 		for (const rule of this.battle.getRuleTable(this.battle.getFormat()).keys()) {
 			let subFormat = this.battle.getFormat(rule);
 			if (subFormat && subFormat.onModifyTemplate) template = this.battle.singleEvent('ModifyTemplate', subFormat, null, this, null, source, template);
 		}
 >>>>>>> Run onModifyTemplate on rulesets for custom formats:sim/pokemon.js
+=======
+		let template = this.battle.runEvent('ModifyTemplate', this, null, source, rawTemplate);
+>>>>>>> Use runEvent:sim/pokemon.js
 
 		if (!template) return null;
 
