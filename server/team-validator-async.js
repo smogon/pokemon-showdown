ffd4b21bb9bca2c85efc978ecbf9aec1fe67d988
Guangcong Luo <guangcongluo@gmail.com>
2019-02-26 08:57:18 -0800

    Update server/team-validator-async.js to remove parens
    Co-Authored-By: scheibo <scheibo@users.noreply.github.com>

--- server/team-validator-async.js
+++ server/team-validator-async.js
@@ -89,7 +89,7 @@ if (!PM.isParentProcess) {
 		});
 	}
 
-	global.Dex = (require(/** @type {any} */ ('../.sim-dist/dex'))).includeData();
+	global.Dex = require(/** @type {any} */ ('../.sim-dist/dex')).includeData();
 	global.toId = Dex.getId;
 	global.Chat = require('./chat');
 
