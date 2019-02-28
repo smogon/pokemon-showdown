6a65937e7b1cff48e3cc36f24c75a6abdedbd7c6
Kirk Scheibelhut <kjs@google.com>
2019-02-28 15:26:21 -0800

    Add fix for gen4 Encore

--- data/mods/gen4/moves.js
+++ data/mods/gen4/moves.js
@@ -523,7 +523,9 @@ let BattleMovedex = {
 			onStart(target, source) {
 				let noEncore = ['encore', 'mimic', 'mirrormove', 'sketch', 'struggle', 'transform'];
 				let moveIndex = target.lastMove ? target.moves.indexOf(target.lastMove.id) : -1;
-				if (!target.lastMove || noEncore.includes(target.lastMove.id) || !target.moveSlots[moveIndex] || target.moveSlots[moveIndex].pp <= 0) {
+				if (!target.lastMove || noEncore.includes(target.lastMove.id) ||
+					!target.moveSlots[moveIndex] || target.moveSlots[moveIndex].pp <= 0 ||
+					(!target.moveThisTurnResult && !target.moveLastTurnResult)) {
 					// it failed
 					this.add('-fail', source);
 					this.attrLastMove('[still]');
