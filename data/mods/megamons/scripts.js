1e32766830c7859348ea80616cbb2769da1e6a72
Kris Johnson <kris.langur@gmail.com>
2019-03-02 12:45:56 -0700

    Move Crucibelle-Mega check

--- data/mods/megamons/scripts.js
+++ data/mods/megamons/scripts.js
@@ -7,6 +7,7 @@ let BattleScripts = {
 			let pokemon = this.data.Pokedex[i];
 			switch (pokemon.species) {
 			case 'Blaziken-Mega':
+			case 'Crucibelle Mega':
 			case 'Gengar-Mega':
 			case 'Mewtwo-Mega-X':
 			case 'Mewtwo-Mega-Y':
@@ -17,7 +18,6 @@ let BattleScripts = {
 				case 'Mega':
 				case 'Mega-X':
 				case 'Mega-Y':
-					if (this.modData('Pokedex', i).species === 'Crucibelle-Mega') break;
 					this.modData('FormatsData', i).isMega = true;
 					this.modData('FormatsData', i).battleOnly = false;
 					this.modData('FormatsData', i).requiredItem = undefined;
