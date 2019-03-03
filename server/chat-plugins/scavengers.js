a654a179f0b8ff1a51a339c2db9237f0a42937bb
Konrad Borowski <konrad@borowski.pw>
2019-03-03 23:59:33 +0100

    Use FS when loading Scavengers' data (#5238)
    This prevents issues with loading old state while hotpatching.

--- server/chat-plugins/scavengers.js
+++ server/chat-plugins/scavengers.js
@@ -54,11 +54,8 @@ class Ladder {
 	}
 
 	load() {
-		try {
-			this.data = require(`../../${this.file}`);
-		} catch (e) {
-			if (e.code !== 'MODULE_NOT_FOUND' && e.code !== 'ENOENT') throw e;
-		}
+		const json = FS(this.file).readIfExistsSync();
+		if (json) this.data = JSON.parse(json);
 	}
 
 	addPoints(name, aspect, points, noUpdate) {
