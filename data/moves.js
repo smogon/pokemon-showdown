33a872e0293aa535408635ca85844076f696eed6
MacChaeger <msaimrkon@gmail.com>
2019-03-04 17:59:44 -0600

    Gen 7: Stockpile tracks individual stat alterations (#5246)

--- data/moves.js
+++ data/moves.js
@@ -16438,20 +16438,30 @@ let BattleMovedex = {
 			noCopy: true,
 			onStart(target) {
 				this.effectData.layers = 1;
+				this.effectData.def = 0;
+				this.effectData.spd = 0;
 				this.add('-start', target, 'stockpile' + this.effectData.layers);
+				let [curDef, curSpD] = [target.boosts.def, target.boosts.spd];
 				this.boost({def: 1, spd: 1}, target, target);
+				if (curDef !== target.boosts.def) this.effectData.def--;
+				if (curSpD !== target.boosts.spd) this.effectData.spd--;
 			},
 			onRestart(target) {
 				if (this.effectData.layers >= 3) return false;
 				this.effectData.layers++;
 				this.add('-start', target, 'stockpile' + this.effectData.layers);
+				const curDef = target.boosts.def;
+				const curSpD = target.boosts.spd;
 				this.boost({def: 1, spd: 1}, target, target);
+				if (curDef !== target.boosts.def) this.effectData.def--;
+				if (curSpD !== target.boosts.spd) this.effectData.spd--;
 			},
 			onEnd(target) {
-				let layers = this.effectData.layers * -1;
-				this.effectData.layers = 0;
-				this.boost({def: layers, spd: layers}, target, target);
+				this.boost({def: this.effectData.def, spd: this.effectData.spd}, target, target);
 				this.add('-end', target, 'Stockpile');
+				if (this.effectData.def !== this.effectData.layers * -1 || this.effectData.spd !== this.effectData.layers * -1) {
+					this.add('-hint', "In Gen 7, Stockpile keeps track of how many times it successfully altered each stat individually.");
+				}
 			},
 		},
 		secondary: null,
