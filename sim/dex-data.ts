a8f7211808992285b47e595823f948dca65e643a
Kirk Scheibelhut <kjs@google.com>
2019-03-05 12:29:24 -0800

    Fix minor Typescript bugs in dex-data.ts
    - BasicEffect#weather's type got messed up during the rewrite in .ts
    - Add Template#originalMega? to match the declaration in globals.ts

--- sim/dex-data.ts
+++ sim/dex-data.ts
@@ -602,6 +602,7 @@ export class Template extends BasicEffect implements Readonly<BasicEffect & Temp
 	readonly isPrimal?: boolean;
 	/** True if a pokemon is a forme that is only accessible in battle. */
 	readonly battleOnly?: boolean;
+	readonly originalMega?: string;
 
 	constructor(data: AnyObject, ...moreData: (AnyObject | null)[]) {
 		super(data, ...moreData);
