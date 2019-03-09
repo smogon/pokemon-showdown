dd136c301829771776858e13410056a230ce7785
Kirk Scheibelhut <kjs@scheibo.com>
2019-03-09 12:48:35 -0800

    WIP

--- dev-tools/harness.js
+++ dev-tools/harness.js
@@ -8,52 +8,17 @@
 
 'use strict';
 
-const child_process = require('child_process');
-
-function shell(cmd) {
-	child_process.execSync(cmd, {stdio: 'inherit', cwd: __dirname});
-}
-
-// `require('../build')` is not safe because `replace` is async.
-shell('node ../build');
-
-try {
-	require.resolve('minimist');
-	//require.resolve('trakr');
-} catch (e) {
-	if (e.code !== 'MODULE_NOT_FOUND') throw e;
-	console.log('Installing dependencies...');
-	shell('npm install minimist');
-	//child_process.execSync('npm install minimist trakr', {stdio: 'inherit', cwd: __dirname});
-}
-
-const argv = require('minimist')(process.argv.slice(2));
-//const trakr = require('trakr');
-
 const BattleStreams = require('../.sim-dist/battle-stream');
 const Dex = require('../.sim-dist/dex');
 const RandomPlayerAI = require('../.sim-dist/examples/random-player-ai').RandomPlayerAI;
 const PRNG = require('../.sim-dist/prng').PRNG;
 
 const DEFAULT_SEED = [0x09917, 0x06924, 0x0e1c8, 0x06af0];
-const SEED = argv.seed ? argv.seed.split(',').map(s => Number(s)) : DEFAULT_SEED;
-const NUM_GAMES = Number(argv._[0] || argv.num) || 100;
-const LOGS = !!argv.logs;
-const SILENT = !LOGS || !!argv.silent;
-const ALL = !!argv.all;
-const ASYNC = !!argv.async;
-const FORMAT = argv.format;
-
-const AI_OPTIONS = {move: 0.7, mega: 0.6};
 
 const FORMATS = [
-	'gen7randombattle', 'gen7randomdoublesbattle',
-	'gen7battlefactory', 'gen6randombattle', 'gen6battlefactory',
-	'gen5randombattle', 'gen4randombattle', 'gen3randombattle',
-	'gen2randombattle', 'gen1randombattle',
+	'gen7randombattle', 'gen7randomdoublesbattle', 'gen7battlefactory', 'gen6randombattle', 'gen6battlefactory',
+	'gen5randombattle', 'gen4randombattle', 'gen3randombattle', 'gen2randombattle', 'gen1randombattle',
 ];
-
-const prng = new PRNG(SEED);
 
 let formatIndex = 0;
 let numGames = 0;
@@ -72,15 +37,15 @@ function getNextFormat() {
 	}
 }
 
-function generateTeam(format) {
+function generateTeam(format, prng) {
 	return Dex.packTeam(Dex.generateTeam(format, prng));
 }
 
-async function runGame(format) {
+async function runGame(format,) {
 	const streams = BattleStreams.getPlayerStreams(new BattleStreams.BattleStream());
 	const spec = {formatid: format, seed: prng.nextFrame(prng.seed)};
-	const p1spec = {name: "Bot 1", team: generateTeam(format)};
-	const p2spec = {name: "Bot 2", team: generateTeam(format)};
+	const p1spec = {name: "Bot 1", team: generateTeam(format, prng)};
+	const p2spec = {name: "Bot 2", team: generateTeam(format, prng)};
 
 	/* eslint-disable no-unused-vars */
 	const p1 = new RandomPlayerAI(streams.p1,
@@ -96,12 +61,12 @@ async function runGame(format) {
 	let chunk;
 	while ((chunk = await streams.omniscient.read())) {
 		if (SILENT || !LOGS) continue;
-		// Logged to error for easier redirection.
-		console.error(chunk);
+		console.log(chunk);
 	}
 }
 
 (async () => {
+	const prng = new PRNG(SEED);
 	let games = [];
 
 	let format, lastFormat;
@@ -114,9 +79,14 @@ async function runGame(format) {
 			games = [];
 		}
 
-		const game = runGame(format);
-		if (!ASYNC) await game;
-		games.push(game);
+		let seed = prng.seed;
+		try {
+			const game = runGame(format, prng);
+			if (!ASYNC) await game;
+			games.push(game);
+		} catch (e) {
+			console.error(`Error running game with seed: [${seed}]\n`, e);
+		}
 		lastFormat = format;
 	}
 
@@ -126,4 +96,43 @@ async function runGame(format) {
 		// TODO timing
 	}
 })();
+
+
+if (require.main === module) {
+	const child_process = require('child_process');
+
+	function shell(cmd) {
+		child_process.execSync(cmd, {stdio: 'inherit', cwd: __dirname});
+	}
+
+	// `require('../build')` is not safe because `replace` is async.
+	shell('node ../build');
+
+	try {
+		require.resolve('minimist');
+		//require.resolve('trakr');
+	} catch (e) {
+		if (e.code !== 'MODULE_NOT_FOUND') throw e;
+		console.log('Installing dependencies...');
+		shell('npm install minimist');
+		//child_process.execSync('npm install minimist trakr', {stdio: 'inherit', cwd: __dirname});
+	}
+
+	const argv = require('minimist')(process.argv.slice(2));
+	//const trakr = require('trakr');
+
+	const SEED = argv.seed ? argv.seed.split(',').map(s => Number(s)) : DEFAULT_SEED;
+const NUM_GAMES = Number(argv._[0] || argv.num) || 100;
+const LOGS = !!argv.logs;
+const SILENT = !LOGS || !!argv.silent;
+const ALL = !!argv.all;
+const ASYNC = !!argv.async;
+const FORMAT = argv.format;
+
+const AI_OPTIONS = {move: 0.7, mega: 0.6};
+
+} else {
+
+
+}
 
