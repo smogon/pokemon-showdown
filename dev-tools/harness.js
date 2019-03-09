16bbe2e2e40af3bde8544bf975baa3157b280bf9
Kirk Scheibelhut <kjs@google.com>
2019-03-08 16:53:21 -0800

    Improve random AI, fix formatting

--- dev-tools/harness.js
+++ dev-tools/harness.js
@@ -15,17 +15,17 @@ function shell(cmd) {
 	child_process.execSync(cmd, {stdio: 'inherit', cwd: __dirname});
 }
 
-// NB: `require('../build')` is not safe because `replace` is async.
+// `require('../build')` is not safe because `replace` is async.
 shell('node ../build');
 
 try {
-  require.resolve('minimist');
-  //require.resolve('trakr');
+	require.resolve('minimist');
+	//require.resolve('trakr');
 } catch (e) {
-  if (e.code !== 'MODULE_NOT_FOUND') throw e;
+	if (e.code !== 'MODULE_NOT_FOUND') throw e;
 	console.log('Installing dependencies...');
-  shell('npm install minimist');
-  //child_process.execSync('npm install minimist trakr', {stdio: 'inherit', cwd: __dirname});
+	shell('npm install minimist');
+	//child_process.execSync('npm install minimist trakr', {stdio: 'inherit', cwd: __dirname});
 }
 
 const argv = require('minimist')(process.argv.slice(2));
@@ -34,86 +34,91 @@ const argv = require('minimist')(process.argv.slice(2));
 const BattleStreams = require('../.sim-dist/battle-stream');
 const Dex = require('../.sim-dist/dex');
 const RandomPlayerAI = require('../.sim-dist/examples/random-player-ai').RandomPlayerAI;
-const PRNG = new require('../.sim-dist/prng').PRNG;
+const PRNG = new (require('../.sim-dist/prng').PRNG)();
 
 const NUM_GAMES = Number(argv._[0] || argv.num) || 100;
 const LOGS = !!argv.logs;
 const SILENT = !LOGS || !!argv.silent;
-const RANDOM = !!argv.random;
+const ALL = !!argv.all;
 const ASYNC = !!argv.async;
+const FORMAT = argv.format;
+
+const AI_OPTIONS = {move: 0.7, mega: 0.3, zmove: 0.3};
 
 const FORMATS = [
-  'gen7randombattle', //'gen7randomdoublesbattle',
-  'gen7battlefactory', 'gen6randombattle', 'gen6battlefactory',
-  'gen5randombattle', 'gen4randombattle', 'gen3randombattle',
-  'gen2randombattle', 'gen1randombattle' ];
+	'gen7randombattle', 'gen7randomdoublesbattle',
+	'gen7battlefactory', 'gen6randombattle', 'gen6battlefactory',
+	'gen5randombattle', 'gen4randombattle', 'gen3randombattle',
+	'gen2randombattle', 'gen1randombattle',
+];
 
 let formatIndex = 0;
 let numGames = 0;
 function getNextFormat() {
-  if (formatIndex > FORMATS.length) {
-    return false;
-  } else if (numGames++ < NUM_GAMES) {
-    return RANDOM ? PRNG.sample(FORMATS) : FORMATS[formatIndex];
-  } else if (RANDOM) {
-    return false;
-  } else {
-    numGames = 1;
-    formatIndex++;
-    return FORMATS[formatIndex];
-  }
+	if (FORMAT) return (numGames++ < NUM_GAMES) && FORMAT;
+	if (formatIndex > FORMATS.length) {
+		return false;
+	} else if (numGames++ < NUM_GAMES) {
+		return ALL ? FORMATS[formatIndex] : PRNG.sample(FORMATS);
+	} else if (!ALL) {
+		return false;
+	} else {
+		numGames = 1;
+		formatIndex++;
+		return FORMATS[formatIndex];
+	}
 }
 
 function generateTeam(format) {
-  return Dex.packTeam(Dex.generateTeam(format));
+	return Dex.packTeam(Dex.generateTeam(format));
 }
 
 async function runGame(format) {
-  const streams = BattleStreams.getPlayerStreams(new BattleStreams.BattleStream());
-  const spec = { formatid: format };
-  const p1spec = { name: "Bot 1", team: generateTeam(format) };
-  const p2spec = { name: "Bot 2", team: generateTeam(format) };
+	const streams = BattleStreams.getPlayerStreams(new BattleStreams.BattleStream());
+	const spec = {formatid: format};
+	const p1spec = {name: "Bot 1", team: generateTeam(format)};
+	const p2spec = {name: "Bot 2", team: generateTeam(format)};
 
-  const p1 = new RandomPlayerAI(streams.p1);
-  const p2 = new RandomPlayerAI(streams.p2);
+	/* eslint-disable no-unused-vars */
+	const p1 = new RandomPlayerAI(streams.p1, AI_OPTIONS);
+	const p2 = new RandomPlayerAI(streams.p2, AI_OPTIONS);
+	/* eslint-enable no-unused-vars */
 
-  streams.omniscient.write(`>start ${JSON.stringify(spec)}
+	streams.omniscient.write(`>start ${JSON.stringify(spec)}
 >player p1 ${JSON.stringify(p1spec)}
 >player p2 ${JSON.stringify(p2spec)}`);
 
-  let chunk;
-  while ((chunk = await streams.omniscient.read())) {
-    if (SILENT || !LOGS) continue;
-    // Logged to error for easier redirection.
-    console.error(chunk);
-  }
+	let chunk;
+	while ((chunk = await streams.omniscient.read())) {
+		if (SILENT || !LOGS) continue;
+		// Logged to error for easier redirection.
+		console.error(chunk);
+	}
 }
 
 (async () => {
-  let games = [];
+	let games = [];
 
-  let format, lastFormat;
-  while ((format = getNextFormat())) {
-    if (!RANDOM && lastFormat && format != lastFormat) {
-      await Promise.all(games);
+	let format, lastFormat;
+	while ((format = getNextFormat())) {
+		if (ALL && lastFormat && format !== lastFormat) {
+			await Promise.all(games);
+			if (!SILENT) {
+				// TODO timing
+			}
+			games = [];
+		}
 
-      if (!SILENT) {
-        // TODO
-      }
+		const game = runGame(format);
+		if (!ASYNC) await game;
+		games.push(game);
+		lastFormat = format;
+	}
 
-      games = [];
-    }
+	await Promise.all(games);
 
-    const game = runGame(format);
-    if (!ASYNC) await game;
-    games.push(game);
-    lastFormat = format;
-  }
-
-  await Promise.all(games);
-
-  if (!SILENT) {
-    // TODO
-  }
+	if (!SILENT) {
+		// TODO timing
+	}
 })();
 
--- sim/examples/random-player-ai.ts
+++ sim/examples/random-player-ai.ts
@@ -8,13 +8,29 @@
  * @author Guangcong Luo <guangcongluo@gmail.com>
  */
 
+import {ObjectReadWriteStream} from '../../lib/streams';
 import {BattlePlayer} from '../battle-stream';
 
-function randomElem(array: number[]) {
+function randomElem<T>(array: T[]): T {
 	return array[Math.floor(Math.random() * array.length)];
 }
 
 export class RandomPlayerAI extends BattlePlayer {
+	readonly move: number;
+	readonly mega: number;
+	readonly zmove: number;
+
+	constructor(
+		playerStream: ObjectReadWriteStream,
+		options: {move?: number, mega?: number, zmove?: number} = {},
+		debug: boolean = false
+	) {
+		super(playerStream, debug);
+		this.move = options.move || 1.0;
+		this.mega = options.mega || 0;
+		this.zmove = options.zmove || 0;
+	}
+
 	receiveRequest(request: AnyObject) {
 		if (request.wait) {
 			// wait request
@@ -25,6 +41,7 @@ export class RandomPlayerAI extends BattlePlayer {
 			const chosen: number[] = [];
 			const choices = request.forceSwitch.map((mustSwitch: AnyObject) => {
 				if (!mustSwitch) return `pass`;
+
 				const canSwitch = [1, 2, 3, 4, 5, 6].filter(i => (
 					// not active
 					i > request.forceSwitch.length &&
@@ -37,19 +54,59 @@ export class RandomPlayerAI extends BattlePlayer {
 				chosen.push(target);
 				return `switch ${target}`;
 			});
+
 			this.choose(choices.join(`, `));
 		} else if (request.active) {
 			// move request
-			const choices = request.active.map((pokemon: AnyObject, i: number) => {
-				if (request.side.pokemon[i].condition.endsWith(` fnt`)) return `pass`;
-				const canMove = [1, 2, 3, 4].slice(0, pokemon.moves.length).filter(j => (
+			let canMegaEvo = true;
+			const pokemon = request.side.pokemon;
+			const chosen: number[] = [];
+			const choices = request.active.map((active: AnyObject, i: number) => {
+				if (pokemon[i].condition.endsWith(` fnt`)) return `pass`;
+
+				canMegaEvo = canMegaEvo && active.canMegaEvo;
+
+				const canMove = [1, 2, 3, 4].slice(0, active.moves.length).filter(j => (
 					// not disabled
-					!pokemon.moves[j - 1].disabled
+					!active.moves[j - 1].disabled
 				));
-				const move = randomElem(canMove);
-				const targetable = request.active.length > 1 && ['normal', 'any'].includes(pokemon.moves[move - 1].target);
-				const target = targetable ? ` ${1 + Math.floor(Math.random() * 2)}` : ``;
-				return `move ${move}${target}`;
+
+				// TODO zmove?
+				const moves = canMove.map(j => {
+					// NOTE: We don't generate all posible targeting combinations.
+					if (request.active.length > 1) {
+						const target = active.moves[j - 1].target;
+						if (['normal', 'any'].includes(target)) {
+							return `move ${j} ${1 + Math.floor(Math.random() * 2)}`;
+						}
+						// TODO targetting adjacentAlly etc
+					}
+					return `move ${j}`;
+				});
+
+				const canSwitch = [1, 2, 3, 4, 5, 6].filter(j => (
+					// not active
+					!pokemon[j - 1].active &&
+					// not chosen for a simultaneous switch
+					!chosen.includes(j) &&
+					// not fainted
+					!pokemon[j - 1].condition.endsWith(` fnt`)
+				));
+				const switches = (active.trapped || active.maybeTrapped) ? [] : canSwitch;
+
+				if (Math.random() > this.move && switches.length) {
+					const target = randomElem(switches);
+					chosen.push(target);
+					return `switch ${target}`;
+				} else {
+					let move = randomElem(moves);
+					if (Math.random() < this.mega && canMegaEvo) {
+						canMegaEvo = false;
+						return `${move} mega`;
+					} else {
+						return move;
+					}
+				}
 			});
 			this.choose(choices.join(`, `));
 		} else {
