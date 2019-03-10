1b2aeb696b37f79da61aa74b484f15923157a9cc
Kirk Scheibelhut <scheibo@users.noreply.github.com>
2019-03-09 21:23:36 -0800

    Reject `move` commands with multiple flags (#5290)

--- sim/side.ts
+++ sim/side.ts
@@ -629,19 +629,29 @@ export class Side {
 
 			switch (choiceType) {
 			case 'move':
-				let targetLoc = 0;
+				const original = data;
+				const error = () => this.emitChoiceError(`Conflicting arguments for "move": ${original}`);
+				let targetLoc: number | undefined;
 				let megaOrZ = '';
 				while (true) {
-					if (/\s-?[1-3]$/.test(data)) {
+					// If data ends with a number, treat it as a target location.
+					// We need to special case 'Conversion 2' so it doesn't get
+					// confused with 'Conversion' erroneously sent with the target
+					// '2' (since Conversion targets 'self', targetLoc can't be 2).
+					if (/\s(?:-|\+)?[1-3]$/.test(data) && toId(data) !== 'conversion2') {
+						if (targetLoc !== undefined) return error();
 						targetLoc = parseInt(data.slice(-2), 10);
 						data = data.slice(0, -2).trim();
 					} else if (data.endsWith(' mega')) {
+						if (megaOrZ) return error();
 						megaOrZ = 'mega';
 						data = data.slice(0, -5);
 					} else if (data.endsWith(' zmove')) {
+						if (megaOrZ) return error();
 						megaOrZ = 'zmove';
 						data = data.slice(0, -6);
 					} else if (data.endsWith(' ultra')) {
+						if (megaOrZ) return error();
 						megaOrZ = 'ultra';
 						data = data.slice(0, -6);
 					} else {
@@ -665,6 +675,7 @@ export class Side {
 				if (data) return this.emitChoiceError(`Unrecognized data after "pass": ${data}`);
 				this.choosePass();
 				break;
+			case 'auto':
 			case 'default':
 				this.autoChoose();
 				break;
