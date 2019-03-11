488ca68b4a9d603de79796a72df3e7e383feae1f
e088764 <christopher.buckner@mastercard.com>
2019-03-11 16:10:28 -0400

    add singles test remove doubles check

--- test/simulator/items/whiteherb.js
+++ test/simulator/items/whiteherb.js
@@ -120,7 +120,7 @@ describe('White Herb', function () {
 		assert.false.holdsItem(holder);
 		assert.statStage(holder, 'atk', -1);
 	});
-	it('should use white herb after two intimidate switch in after faint', function () {
+	it('should use white herb after two intimidate switch in', function () {
 		battle = common.createBattle({gameType: 'doubles'});
 
 		battle.join('p1', 'Guest 1', 1, [
@@ -185,6 +185,72 @@ describe('White Herb', function () {
 		battle.makeChoices('move stealthrock, move rest', 'move gyroball, move airslash');
 
 		battle.makeChoices('switch Arcanine, move rest', 'move gyroball, move airslash');
+
+		const holder = battle.p2.active[0];
+
+		assert.false.holdsItem(holder);
+		assert.statStage(holder, 'atk', 0);
+	});
+
+	it('should use white herb after one intimidate switch in [singles]', function () {
+		battle = common.createBattle();
+
+		battle.join('p1', 'Guest 1', 1, [
+			{species: "Rotom", ability: 'levitate', item: 'ironball', moves: ['rest']},
+			{species: "Arcanine", ability: 'intimidate', moves: ['bodyslam']},
+			{species: "Incineroar", ability: 'intimidate', moves: ['agility']}]);
+		battle.join('p2', 'Guest 2', 2, [
+			{species: "aegislash", ability: 'stancechange', item: 'whiteherb', moves: ['autotomize']},
+			{species: "pelipper", ability: 'sandveil', item: 'lifeorb', moves: ['airslash']},
+			{species: "Thundurus", ability: 'prankster', item: 'ironball', moves: ['electricterrain']},
+			{species: 'Klefki', ability: 'prankster', moves: ['confuseray']}]);
+
+		battle.makeChoices('move rest', 'move autotomize');
+
+		battle.makeChoices('switch Arcanine', 'move autotomize');
+
+		const holder = battle.p2.active[0];
+
+		assert.false.holdsItem(holder);
+		assert.statStage(holder, 'atk', 0);
+	});
+	it('should use white herb after one intimidate [singles]', function () {
+		battle = common.createBattle();
+
+		battle.join('p1', 'Guest 1', 1, [
+			{species: "Arcanine", ability: 'intimidate', moves: ['bodyslam']},
+			{species: "Incineroar", ability: 'intimidate', moves: ['agility']}]);
+		battle.join('p2', 'Guest 2', 2, [
+			{species: "aegislash", ability: 'stancechange', item: 'whiteherb', moves: ['autotomize']},
+			{species: "pelipper", ability: 'sandveil', item: 'lifeorb', moves: ['airslash']},
+			{species: 'Klefki', ability: 'prankster', moves: ['confuseray']}]);
+
+		battle.makeChoices('move rest', 'move autotomize');
+
+		battle.makeChoices('switch Arcanine', 'move autotomize');
+
+		const holder = battle.p2.active[0];
+
+		assert.false.holdsItem(holder);
+		assert.statStage(holder, 'atk', 0);
+	});
+	it('should use white herb after pokemon switch in after faint [singles]', function () {
+		battle = common.createBattle();
+
+		battle.join('p1', 'Guest 1', 1, [
+			{species: "oddish", ability: 'stancechange', item: 'ironball', moves: ['storedpower']},
+			{species: "oddish", ability: 'stancechange', item: 'ironball', moves: ['storedpower']},
+			{species: "Arcanine", ability: 'intimidate', moves: ['bodyslam']},
+			{species: "Incineroar", ability: 'intimidate', moves: ['agility']}]);
+		battle.join('p2', 'Guest 2', 2, [
+			{species: "Rotom", ability: 'levitate', item: 'whiteherb', moves: ['searingshot']},
+			{species: "Rotom", ability: 'levitate', item: 'whiteherb', moves: ['searingshot']},
+			{species: "Thundurus", ability: 'prankster', item: 'whiteherb', moves: ['electricterrain']},
+			{species: 'Klefki', ability: 'prankster', moves: ['confuseray']}]);
+
+		battle.makeChoices('move storedpower', 'move searingshot');
+
+		battle.makeChoices('switch Arcanine', 'switch searingshot');
 
 		const holder = battle.p2.active[0];
 
