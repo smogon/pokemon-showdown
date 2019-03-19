e61965eaf59c42fc2da70b6e6e9a7d58fa80fe58
Kirk Scheibelhut <kjs@google.com>
2019-03-19 15:45:09 -0700

    Pull out RandomPlayerAI from example and enhance

--- sim/examples/battle-stream-example.ts
+++ sim/examples/battle-stream-example.ts
@@ -3,77 +3,22 @@
  * Pokemon Showdown - http://pokemonshowdown.com/
  *
  * Example of how to create AIs battling against each other.
+ * Run this using `node build && node .sim-dist/examples/battle-stream-example`.
  *
  * @license MIT
  * @author Guangcong Luo <guangcongluo@gmail.com>
  */
 
-import * as BattleStreams from '../battle-stream';
+import {BattleStream, getPlayerStreams} from '../battle-stream';
 import Dex = require('../dex');
-
-/*********************************************************************
- * Helper functions
- *********************************************************************/
-
-function randomElem(array: number[]) {
-	return array[Math.floor(Math.random() * array.length)];
-}
-
-/*********************************************************************
- * Define AI
- *********************************************************************/
-
-class RandomPlayerAI extends BattleStreams.BattlePlayer {
-	receiveRequest(request: AnyObject) {
-		if (request.wait) {
-			// wait request
-			// do nothing
-		} else if (request.forceSwitch) {
-			// switch request
-			const pokemon = request.side.pokemon;
-			const chosen: number[] = [];
-			const choices = request.forceSwitch.map((mustSwitch: AnyObject) => {
-				if (!mustSwitch) return `pass`;
-				const canSwitch = [1, 2, 3, 4, 5, 6].filter(i => (
-					// not active
-					i > request.forceSwitch.length &&
-					// not chosen for a simultaneous switch
-					!chosen.includes(i) &&
-					// not fainted
-					!pokemon[i - 1].condition.endsWith(` fnt`)
-				));
-				const target = randomElem(canSwitch);
-				chosen.push(target);
-				return `switch ${target}`;
-			});
-			this.choose(choices.join(`, `));
-		} else if (request.active) {
-			// move request
-			const choices = request.active.map((pokemon: AnyObject, i: number) => {
-				if (request.side.pokemon[i].condition.endsWith(` fnt`)) return `pass`;
-				const canMove = [1, 2, 3, 4].slice(0, pokemon.moves.length).filter(j => (
-					// not disabled
-					!pokemon.moves[j - 1].disabled
-				));
-				const move = randomElem(canMove);
-				const targetable = request.active.length > 1 && ['normal', 'any'].includes(pokemon.moves[move - 1].target);
-				const target = targetable ? ` ${1 + Math.floor(Math.random() * 2)}` : ``;
-				return `move ${move}${target}`;
-			});
-			this.choose(choices.join(`, `));
-		} else {
-			// team preview?
-			this.choose(`default`);
-		}
-	}
-}
+import {RandomPlayerAI} from './random-player-ai';
 
 /*********************************************************************
  * Run AI
  *********************************************************************/
 // tslint:disable:no-floating-promises
 
-const streams = BattleStreams.getPlayerStreams(new BattleStreams.BattleStream());
+const streams = getPlayerStreams(new BattleStream());
 
 const spec = {
 	formatid: "gen7customgame",
