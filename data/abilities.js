e195a9fb2748f24c1498917bef0a380b9b6b8538
Ivo Julca <ivojulca@hotmail.com>
2019-03-26 19:33:16 -0500

    Sim: Refactor to use Battle#getAllPokemon() and Battle#getAllActive()

--- data/abilities.js
+++ data/abilities.js
@@ -109,8 +109,7 @@ let BattleAbilities = {
 		onBasePowerPriority: 8,
 		onBasePower(basePower, pokemon) {
 			let boosted = true;
-			let allActives = pokemon.side.active.concat(pokemon.side.foe.active);
-			for (const target of allActives) {
+			for (const target of this.getAllActive()) {
 				if (target === pokemon) continue;
 				if (this.willMove(target)) {
 					boosted = false;
@@ -662,13 +661,11 @@ let BattleAbilities = {
 		},
 		onEnd(pokemon) {
 			if (this.field.weatherData.source !== pokemon) return;
-			for (const side of this.sides) {
-				for (const target of side.active) {
-					if (target === pokemon) continue;
-					if (target && target.hp && target.hasAbility('deltastream')) {
-						this.field.weatherData.source = target;
-						return;
-					}
+			for (const target of this.getAllActive()) {
+				if (target === pokemon) continue;
+				if (target.hasAbility('deltastream')) {
+					this.field.weatherData.source = target;
+					return;
 				}
 			}
 			this.field.clearWeather();
@@ -689,13 +686,11 @@ let BattleAbilities = {
 		},
 		onEnd(pokemon) {
 			if (this.field.weatherData.source !== pokemon) return;
-			for (const side of this.sides) {
-				for (const target of side.active) {
-					if (target === pokemon) continue;
-					if (target && target.hp && target.hasAbility('desolateland')) {
-						this.field.weatherData.source = target;
-						return;
-					}
+			for (const target of this.getAllActive()) {
+				if (target === pokemon) continue;
+				if (target.hasAbility('desolateland')) {
+					this.field.weatherData.source = target;
+					return;
 				}
 			}
 			this.field.clearWeather();
@@ -2307,7 +2302,7 @@ let BattleAbilities = {
 		onResidual(pokemon) {
 			if (pokemon.item) return;
 			let pickupTargets = [];
-			for (const target of pokemon.side.active.concat(pokemon.side.foe.active)) {
+			for (const target of this.getAllActive()) {
 				if (target.lastItem && target.usedItemThisTurn && this.isAdjacent(pokemon, target)) {
 					pickupTargets.push(target);
 				}
@@ -2509,13 +2504,11 @@ let BattleAbilities = {
 		},
 		onEnd(pokemon) {
 			if (this.field.weatherData.source !== pokemon) return;
-			for (const side of this.sides) {
-				for (const target of side.active) {
-					if (target === pokemon) continue;
-					if (target && target.hp && target.hasAbility('primordialsea')) {
-						this.field.weatherData.source = target;
-						return;
-					}
+			for (const target of this.getAllActive()) {
+				if (target === pokemon) continue;
+				if (target.hasAbility('primordialsea')) {
+					this.field.weatherData.source = target;
+					return;
 				}
 			}
 			this.field.clearWeather();
