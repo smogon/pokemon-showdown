a53d6a8af762e6bc98382ac8f090c92ef2a57d82
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2019-04-02 16:12:11 -0600

    Remove LC suspect notice (#5398)

--- config/formats.js
+++ config/formats.js
@@ -122,9 +122,6 @@ let Formats = [
 			'Scyther', 'Sneasel', 'Swirlix', 'Tangela', 'Vulpix-Base', 'Wingull', 'Yanma',
 			'Eevium Z', 'Baton Pass', 'Dragon Rage', 'Sonic Boom',
 		],
-		onBegin() {
-			if (this.rated && this.format === 'gen7lc') this.add('-html', `<div class="broadcast-red"><strong>LC is currently suspecting Trapinch! For information on how to participate check out the <a href="https://www.smogon.com/forums/threads/3648309/">suspect thread</a>.</strong></div>`);
-		},
 	},
 	{
 		name: "[Gen 7] Monotype",
