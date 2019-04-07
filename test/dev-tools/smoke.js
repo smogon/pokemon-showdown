a8eb9800f23f11c24289b90cf1492ede2a092bfd
Kirk Scheibelhut <scheibo@users.noreply.github.com>
2019-04-07 08:34:43 -0700

    Add a smoke test runner to dev-tools (#5405)

---
+++ test/dev-tools/smoke.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const assert = require('assert');
+
+const {SmokeRunner} = require('./../../dev-tools/smoke');
+
+describe('SmokeRunner', async () => {
+	it('should run successfully', async () => {
+		const opts = {format: 'gen7doublescustomgame', maxGames: 1, prng: [1, 2, 3, 4]};
+		assert.strictEqual(await (new SmokeRunner(opts).run()), 0);
+	});
+});
