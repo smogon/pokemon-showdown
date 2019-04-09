d29c08a12e08e41d1f1cf425420801a1275f7f44
Kirk Scheibelhut <kjs@scheibo.com>
2019-04-08 19:14:04 -0700

    Disable slow harness tests to avoid timeouts
    Poor @KrisXV can't run these. Flip them to pending while we sort
    out a better story for slower tests.

--- test/dev-tools/harness.js
+++ test/dev-tools/harness.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 
 const {MultiRunner} = require('./../../dev-tools/harness');
 
-describe('MultiRunner', async () => {
+describe.skip('MultiRunner', async () => {
 	it('should run successfully', async () => {
 		const opts = {totalGames: 1, prng: [1, 2, 3, 4]};
 		assert.strictEqual(await (new MultiRunner(opts).run()), 0);
