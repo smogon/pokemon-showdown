e8ad90504966c4dcfcae5c43113e5362bc4135ad
Kris Johnson <kris.langur@gmail.com>
2019-04-08 19:30:26 -0600

    yep

--- test/simulator/misc/random-teams.js
+++ test/simulator/misc/random-teams.js
@@ -105,7 +105,7 @@ describe(`Hackmons Cup Team generator`, function () {
 describe(`Factory sets`, function () {
 	for (const filename of ['bss-factory-sets', 'factory-sets']) {
 		it(`should have valid sets in ${filename}.json`, function () {
-			this.timeout(5000);
+			this.timeout(0);
 			const setsJSON = require(`../../../data/${filename}.json`);
 
 			for (const type in setsJSON) {
