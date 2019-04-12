174fd5745f041b65860fba995ef2927802c2aa0e
Slayer95 <ivojulca@hotmail.com>
2019-04-12 14:08:32 -0500

    Test: Implement `npm run fulltest` (#5437)
    - Tests flagged with (slow) will no longer be run in `npm test`.
    - Travis CI will now run `fulltest`.
    - Unskip slow dev-tools tests and flag as such.

--- test/dev-tools/harness.js
+++ test/dev-tools/harness.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 
 const {MultiRunner} = require('./../../dev-tools/harness');
 
-describe.skip('MultiRunner', async () => {
+describe('MultiRunner (slow)', async () => {
 	it('should run successfully', async () => {
 		const opts = {totalGames: 1, prng: [1, 2, 3, 4]};
 		assert.strictEqual(await (new MultiRunner(opts).run()), 0);
