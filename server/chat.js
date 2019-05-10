9a7ce703f2abe391d73fb2872a503e7d20427e6b
jetou <31071116+TheJetOU@users.noreply.github.com>
2019-05-10 19:23:46 +0300

    TypeScript server/chat formatter.js (#5495)

--- server/chat.js
+++ server/chat.js
@@ -1895,8 +1895,10 @@ Chat.stringify = function (value, depth = 0) {
 	return `${constructor}{${buf}}`;
 };
 
-Chat.formatText = require('./chat-formatter').formatText;
-Chat.linkRegex = require('./chat-formatter').linkRegex;
+/** @type {typeof import('./chat-formatter').formatText} */
+Chat.formatText = require(/** @type {any} */('../.server-dist/chat-formatter')).formatText;
+/** @type {typeof import('./chat-formatter').linkRegex} */
+Chat.linkRegex = require(/** @type {any} */('../.server-dist/chat-formatter')).linkRegex;
 Chat.updateServerLock = false;
 
 /**
