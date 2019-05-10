203d8c0692d23b99d0e633fc3a960af4443bf9b0
TheJetOU <waleed.2004.hassan@gmail.com>
2019-05-10 19:57:46 +0400

    Correctly type chat-formatter require calls

--- server/chat.js
+++ server/chat.js
@@ -1895,7 +1895,9 @@ Chat.stringify = function (value, depth = 0) {
 	return `${constructor}{${buf}}`;
 };
 
+/** @type {typeof import('./chat-formatter').formatText} */
 Chat.formatText = require(/** @type {any} */('../.server-dist/chat-formatter')).formatText;
+/** @type {typeof import('./chat-formatter').formatText} */
 Chat.linkRegex = require(/** @type {any} */('../.server-dist/chat-formatter')).linkRegex;
 Chat.updateServerLock = false;
 
