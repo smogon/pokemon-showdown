654931d92af0a44d46143ed25565311341334e9f
urkerab <urkerab@users.noreply.github.com>
2019-06-05 22:20:16 +0100

    Don't share self-replacing abilities

--- data/mods/pic/scripts.js
+++ data/mods/pic/scripts.js
@@ -7,7 +7,7 @@ exports.BattleScripts = {
 			return name;
 		}
 		let id = toID(name);
-		if (id.startsWith('ability')) return Object.assign(Object.create(this.getAbility(id.slice(7))), {id});
+		if (id.startsWith('ability') && !['abilitypowerofalchemy', 'abilityreceiver', 'abilitytrace'].includes(id)) return Object.assign(Object.create(this.getAbility(id.slice(7))), {id});
 		return Object.getPrototypeOf(this).getEffect.call(this, name);
 	},
 	pokemon: {
