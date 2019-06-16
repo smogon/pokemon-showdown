75159ffcd5cb96996e01e588b2ebd2fcb537138c
TapoCollino <44491367+TapoCollino@users.noreply.github.com>
2019-06-16 14:35:16 +0800

    Add juniper avatar
    hopefully

--- server/chat-commands.js
+++ server/chat-commands.js
@@ -172,6 +172,7 @@ const avatarTable = new Set([
 	'juan-gen3',
 	'juan',
 	'juggler-gen1', 'juggler-gen1rb', 'juggler-gen2', 'juggler-gen3', 'juggler',
+	'juniper',
 	'jupiter',
 	'karen-gen2', 'karen',
 	'kimonogirl',
