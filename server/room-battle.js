48c9304a4a33499453e2e05222c0c5a9b3dc66a5
Bär Halberkamp <fledaway@gmail.com>
2019-07-02 13:57:57 +0200

    Fix a bug with clearing non-afk statuses

--- server/room-battle.js
+++ server/room-battle.js
@@ -544,7 +544,7 @@ class RoomBattle extends RoomGames.RoomGame {
 		const player = this.playerTable[user.userid];
 		const [choice, rqid] = data.split('|', 2);
 		if (!player) return;
-		user.clearStatus();
+		if (user.isAway()) user.clearStatus();
 		let request = player.request;
 		if (request.isWait !== false && request.isWait !== true) {
 			player.sendRoom(`|error|[Invalid choice] There's nothing to choose`);
