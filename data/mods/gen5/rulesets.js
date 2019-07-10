74306fc97ce597a7c8c67d24b890ec1808fa35bb
Marty-D <Marty-D@users.noreply.github.com>
2019-07-09 21:23:07 -0400

    Gen V: Prevent illegal Huntail move combination

---
+++ data/mods/gen5/rulesets.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**@type {{[k: string]: ModdedFormatsData}} */
+let BattleFormats = {
+	pokemon: {
+		inherit: true,
+		banlist: [
+			'Chansey + Charm + Seismic Toss',
+			'Blissey + Charm + Seismic Toss',
+			'Huntail + Shell Smash + Sucker Punch',
+		],
+	},
+};
+
+exports.BattleFormats = BattleFormats;
