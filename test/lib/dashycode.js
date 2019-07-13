1375d653f9e0d5832e53618553f12b04728263e0
Ben Davies <kaiepi@outlook.com>
2019-07-13 02:42:57 -0300

    Unit test Dashycode

---
+++ test/lib/dashycode.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const assert = require('assert');
+const Dashycode = require('./../../.lib-dist/dashycode');
+
+describe('Dashycode', function () {
+	const codepoints = Array.from({length: 0x10000}, (v, k) => k);
+	const encoded = new Map();
+
+	const encode = (codepoint, allowCaps) => {
+		const character = String.fromCodePoint(codepoint);
+		const dashycode = Dashycode.encode(character);
+		assert.strictEqual(encoded.has(dashycode), false);
+		encoded.set(dashycode, character);
+	};
+
+	const decode = (dashycode) => {
+		const character = Dashycode.decode(dashycode);
+		assert.strictEqual(encoded.get(dashycode), character);
+	};
+
+	it('should encode all codepoints uniquely', function () {
+		return codepoints.reduce((p, codepoint) => (
+			p.then(v => encode(codepoint))
+		), Promise.resolve());
+	});
+
+	it('should decode all codepoints accurately', function () {
+		return [...encoded.keys()].reduce((p, dashycode) => (
+			p.then(v => decode(dashycode))
+		), Promise.resolve());
+	});
+
+	after(function () {
+		encoded.clear();
+	});
+});
