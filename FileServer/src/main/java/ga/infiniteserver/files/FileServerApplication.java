abdfa42e12cd927704f3d730a62e73fbb96f9951
josephjustn <josephjustn@gmail.com>
2019-07-19 22:38:15 -0600

    File Server

---
+++ FileServer/src/main/java/ga/infiniteserver/files/FileServerApplication.java
@@ -0,0 +1,13 @@
+package ga.infiniteserver.files;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileServerApplication.class, args);
+	}
+
+}
---
+++ FileServer/src/test/java/ga/infiniteserver/files/FileServerApplicationTests.java
@@ -0,0 +1,16 @@
+package ga.infiniteserver.files;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FileServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
