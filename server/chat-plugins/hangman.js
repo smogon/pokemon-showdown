7067f3144b9a6a68248b51297186354e30a6c7f1
maxalexandderpi <49593536+maxalexandderpi@users.noreply.github.com>
2019-08-05 23:59:59 -0500

    Let users see who made a hangman/poll (#5680)

--- server/chat-plugins/hangman.js
+++ server/chat-plugins/hangman.js
@@ -250,7 +250,7 @@ const commands = {
 			game.display(user, true);
 
 			this.modlog('HANGMAN');
-			return this.privateModAction(`(A game of hangman was started by ${user.name}.)`);
+			return this.addModAction(`A game of hangman was started by ${user.name}.`);
 		},
 		createhelp: ["/hangman create [word], [hint] - Makes a new hangman game. Requires: % @ # & ~"],
 
