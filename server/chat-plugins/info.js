065dd2b47f051b5626659ae38217ab326eeb050f
RyotaMitaraiWeb <51201292+RyotaMitaraiWeb@users.noreply.github.com>
2019-08-12 23:09:30 +0300

    Update a link in the /intro command (#5697)

--- server/chat-plugins/info.js
+++ server/chat-plugins/info.js
@@ -1511,7 +1511,7 @@ const commands = {
 			`New to competitive Pok&eacute;mon?<br />` +
 			`- <a href="https://www.smogon.com/forums/threads/3496279/">Beginner's Guide to Pok&eacute;mon Showdown</a><br />` +
 			`- <a href="https://www.smogon.com/dp/articles/intro_comp_pokemon">An introduction to competitive Pok&eacute;mon</a><br />` +
-			`- <a href="https://www.smogon.com/bw/articles/bw_tiers">What do 'OU', 'UU', etc mean?</a><br />` +
+			`- <a href="https://www.smogon.com/sm/articles/sm_tiers">What do 'OU', 'UU', etc mean?</a><br />` +
 			`- <a href="https://www.smogon.com/xyhub/tiers">What are the rules for each format? What is 'Sleep Clause'?</a>`
 		);
 	},
