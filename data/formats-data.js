bd3449820480e7e485cd7cb473ca1ee347cc63f3
fart <tyler.m.sciara@gmail.com>
2019-08-16 22:16:14 -0400

    Add Celebrate Aerodactyl event (#5711)

--- data/formats-data.js
+++ data/formats-data.js
@@ -2256,6 +2256,7 @@ let BattleFormatsData = {
 		randomDoubleBattleMoves: ["wideguard", "taunt", "stoneedge", "rockslide", "earthquake", "aquatail", "protect", "icefang", "skydrop", "tailwind"],
 		eventPokemon: [
 			{"generation": 5, "level": 15, "gender": "M", "isHidden": false, "abilities": ["pressure"], "moves": ["steelwing", "icefang", "firefang", "thunderfang"], "pokeball": "cherishball"},
+			{"generation": 7, "level": 50, "isHidden": true, "moves": ["ancientpower", "rockpolish", "wideguard", "celebrate"], "pokeball": "cherishball"},
 		],
 		encounters: [
 			{"generation": 1, "level": 30},
