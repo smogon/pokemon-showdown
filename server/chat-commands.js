8238cfeb6674c58e66da83f3c22c691c8e6ab662
whales <jumbowhales@gmail.com>
2019-08-24 17:20:07 +0930

    generic trusted check

--- server/chat-commands.js
+++ server/chat-commands.js
@@ -2806,17 +2806,15 @@ const commands = {
 		if (!targetUser && !room.log.hasUsername(target)) return this.errorReply(`User ${target} not found or has no roomlogs.`);
 		let userid = toID(this.inputUsername);
 
-		if (!user.can('mute', targetUser, room)) {
-			if (!this.can('mute', null, room)) return;
-			if (targetUser !== user) {
-				if (!cmd.includes('force')) {
-					return this.errorReply(`${target} is a higher rank than you, are you sure you want to hide their messages? Use /forcehidetext if you're sure.`);
-				}
-				// Notify staff when someone's hiding text outside of their jurisdiction
-				if (!room.isPrivate) {
-					const staffRoom = Rooms('staff');
-					if (staffRoom) staffRoom.addByUser(user, `<<${room.id}>> ${user.getIdentity(room)} hid ${targetUser ? targetUser.getIdentity(room) : target}'s messages`);
-				}
+		if (!this.can('mute', null, room)) return;
+		if (targetUser && targetUser.trusted && targetUser !== user) {
+			if (!cmd.includes('force')) {
+				return this.errorReply(`${target} is a trusted user, are you sure you want to hide their messages? Use /forcehidetext if you're sure.`);
+			}
+			// Notify staff when a trusted user gets their messages hidden
+			if (!room.isPrivate) {
+				const staffRoom = Rooms('staff');
+				if (staffRoom) staffRoom.addByUser(user, `<<${room.id}>> ${user.name} hid trusted user ${targetUser.name}'s messages.`);
 			}
 		}
 
