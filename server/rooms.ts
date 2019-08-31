e76bd2da12cfad9b801eed66b5d0fdbcb55efd27
Spandan Punwatkar <spandan.punwatkar@gmail.com>
2019-08-31 15:49:59 +0530

    Update rooms.ts

--- server/rooms.ts
+++ server/rooms.ts
@@ -1281,7 +1281,7 @@ export class BasicChatRoom extends BasicRoom {
 		} else {
 			this.reportJoin('n', user.getIdentityWithStatus(this.id) + '|' + oldid, user);
 		}
-		if (typeof this.pollOrAnnouncement === Poll && user.userid in this.pollOrAnnouncement.voters) this.pollOrAnnouncement.updateFor(user);
+		if (this.pollOrAnnouncement instanceof Poll && user.userid in this.pollOrAnnouncement.voters) this.pollOrAnnouncement.updateFor(user);
 		return true;
 	}
 	/**
