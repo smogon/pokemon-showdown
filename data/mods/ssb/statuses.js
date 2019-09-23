87e902cc144f8c61b1c140bb72e1ceb64c13dfa4
Lusamine <30205550+Lusamine@users.noreply.github.com>
2019-09-23 10:27:34 -0500

    Shorten descs, add speech (#5809)

--- data/mods/ssb/statuses.js
+++ data/mods/ssb/statuses.js
@@ -1211,6 +1211,7 @@ let BattleStatuses = {
 				this.add(`c|&Anubis|ohey it's pre`);
 				this.add(`c|&Anubis|!showimage https://pokemonshowdown.com/images/ssbkitten.jpg`);
 				this.add(`raw|<img src="https://pokemonshowdown.com/images/ssbkitten.jpg" style="width: 300px; height: 400px" />`);
+				this.add(`c|~pre|<3`);
 			}
 		},
 		onSwitchOut() {
