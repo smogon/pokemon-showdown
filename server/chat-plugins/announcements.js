b2f9ebe99f57df14e1c9e5c03dd5c84baeeadc14
Spandan Punwatkar <spandan.punwatkar@gmail.com>
2019-09-23 20:22:06 +0530

    pollOrAnnouncement -> minorActivity

--- server/chat-plugins/announcements.js
+++ server/chat-plugins/announcements.js
@@ -82,14 +82,14 @@ const commands = {
 			if (!this.can('minigame', null, room)) return false;
 			if (supportHTML && !this.can('declare', null, room)) return false;
 			if (!this.canTalk()) return;
-			if (room.pollOrAnnouncement) return this.errorReply("There is already a poll or announcement in progress in this room.");
+			if (room.minorActivity) return this.errorReply("There is already a poll or announcement in progress in this room.");
 
 			// @ts-ignore In the case that any of these are null, the function is terminated, and the result never used.
 			if (supportHTML) target = this.canHTML(target);
 			if (!target) return;
 
-			room.pollOrAnnouncement = new Announcement(room, {source: target, supportHTML: supportHTML});
-			room.pollOrAnnouncement.display();
+			room.minorActivity = new Announcement(room, {source: target, supportHTML: supportHTML});
+			room.minorActivity.display();
 
 			this.roomlog(`${user.name} used ${message}`);
 			this.modlog('ANNOUNCEMENT');
@@ -98,32 +98,32 @@ const commands = {
 		newhelp: [`/announcement create [announcement] - Creates an announcement. Requires: % @ # & ~`],
 
 		timer(target, room, user) {
-			if (!room.pollOrAnnouncement || !(room.pollOrAnnouncement instanceof Announcement)) return this.errorReply("There is no announcement running in this room.");
+			if (!room.minorActivity || !(room.minorActivity instanceof Announcement)) return this.errorReply("There is no announcement running in this room.");
 
 			if (target) {
 				if (!this.can('minigame', null, room)) return false;
 				if (target === 'clear') {
-					if (!room.pollOrAnnouncement.timeout) return this.errorReply("There is no timer to clear.");
-					clearTimeout(room.pollOrAnnouncement.timeout);
-					room.pollOrAnnouncement.timeout = null;
-					room.pollOrAnnouncement.timeoutMins = 0;
+					if (!room.minorActivity.timeout) return this.errorReply("There is no timer to clear.");
+					clearTimeout(room.minorActivity.timeout);
+					room.minorActivity.timeout = null;
+					room.minorActivity.timeoutMins = 0;
 					return this.add("The announcement timer was turned off.");
 				}
 				const timeout = parseFloat(target);
 				if (isNaN(timeout) || timeout <= 0 || timeout > 0x7FFFFFFF) return this.errorReply("Invalid time given.");
-				if (room.pollOrAnnouncement.timeout) clearTimeout(room.pollOrAnnouncement.timeout);
-				room.pollOrAnnouncement.timeoutMins = timeout;
-				room.pollOrAnnouncement.timeout = setTimeout(() => {
-					if (room.pollOrAnnouncement) room.pollOrAnnouncement.end();
-					room.pollOrAnnouncement = null;
+				if (room.minorActivity.timeout) clearTimeout(room.minorActivity.timeout);
+				room.minorActivity.timeoutMins = timeout;
+				room.minorActivity.timeout = setTimeout(() => {
+					if (room.minorActivity) room.minorActivity.end();
+					room.minorActivity = null;
 				}, (timeout * 60000));
 				room.add(`The announcement timer was turned on: the announcement will end in ${timeout} minute${Chat.plural(timeout)}.`);
 				this.modlog('announcement TIMER', null, `${timeout} minutes`);
 				return this.privateModAction(`(The announcement timer was set to ${timeout} minute${Chat.plural(timeout)} by ${user.name}.)`);
 			} else {
 				if (!this.runBroadcast()) return;
-				if (room.pollOrAnnouncement.timeout) {
-					return this.sendReply(`The announcement timer is on and will end in ${room.pollOrAnnouncement.timeoutMins} minute${Chat.plural(room.pollOrAnnouncement.timeoutMins)}.`);
+				if (room.minorActivity.timeout) {
+					return this.sendReply(`The announcement timer is on and will end in ${room.minorActivity.timeoutMins} minute${Chat.plural(room.minorActivity.timeoutMins)}.`);
 				} else {
 					return this.sendReply("The announcement timer is off.");
 				}
@@ -139,11 +139,11 @@ const commands = {
 		end(target, room, user) {
 			if (!this.can('minigame', null, room)) return false;
 			if (!this.canTalk()) return;
-			if (!room.pollOrAnnouncement || !(room.pollOrAnnouncement instanceof Announcement)) return this.errorReply("There is no announcement running in this room.");
-			if (room.pollOrAnnouncement.timeout) clearTimeout(room.pollOrAnnouncement.timeout);
+			if (!room.minorActivity || !(room.minorActivity instanceof Announcement)) return this.errorReply("There is no announcement running in this room.");
+			if (room.minorActivity.timeout) clearTimeout(room.minorActivity.timeout);
 
-			room.pollOrAnnouncement.end();
-			delete room.pollOrAnnouncement;
+			room.minorActivity.end();
+			delete room.minorActivity;
 			this.modlog('ANNOUNCEMENT END');
 			return this.privateModAction(`(The announcement was ended by ${user.name}.)`);
 		},
@@ -151,14 +151,14 @@ const commands = {
 
 		show: 'display',
 		display(target, room, user, connection) {
-			if (!room.pollOrAnnouncement || !(room.pollOrAnnouncement instanceof Announcement)) return this.errorReply("There is no announcement running in this room.");
+			if (!room.minorActivity || !(room.minorActivity instanceof Announcement)) return this.errorReply("There is no announcement running in this room.");
 			if (!this.runBroadcast()) return;
 			room.update();
 
 			if (this.broadcasting) {
-				room.pollOrAnnouncement.display();
+				room.minorActivity.display();
 			} else {
-				room.pollOrAnnouncement.displayTo(user, connection);
+				room.minorActivity.displayTo(user, connection);
 			}
 		},
 		displayhelp: [`/announcement display - Displays the announcement`],
