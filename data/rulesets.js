71498d451db099e2aecca9470d6bed396245e1c0
Guangcong Luo <guangcongluo@gmail.com>
2019-10-06 04:21:30 +1100

    New validator (#5840)
    * Refactor validator
    
    This is a major refactor intended to make the default rules easier to
    understand, and also easier for OMs to bypass.
    
    Removed rules:
    - `Pokemon`: This is now `-Nonexistent`. Its previous name was intended
      to be interpreted as "simulate the Pokémon games exactly, and only
      allow what they allow". The new name should make it clearer that it
      mainly bans CAPs and other nonexistent Pokémon and functionality.
    - `-Illegal`: This is now `Obtainable` (see below).
    - `Allow CAP`: This is now `+CAP`. Instead of having a hardcoded rule,
      OMs can now be manually whitelist any pokemon/item/etc or group of
      them, overriding `-Nonexistent`.
    - `Ignore Illegal Abilities`: This is now `!Obtainable Abilities` (see
      below).
    
    `Obtainable` was previously `-Illegal`, and does the same thing: Makes
    sure you have a regular Pokémon game with only Pokémon that can be
    obtained without hacking.
    
    But instead of being a ban, it's now a rule that does nothing by
    itself, except contain more rules:
    - `Obtainable Moves`
    - `Obtainable Abilities`
    - `Obtainable Formes`
    - `Obtainable Misc`
    - `-Nonexistent`
    - `-Unreleased`
    
    This allows OMs to piecemeal repeal and unban any of these individual
    rules, instead of the previous approach of unbanning them all and
    manually reimplementing every single validation you wanted to keep.
    
    * Refactor PokemonSources into a class
    
    This mostly just makes a lot of the weirder checks in the validator
    substantially more readable.
    
    This also renames `lsetData` to `setSources`, which should also help
    readability.
    
    * Validate Bottle Cap HP types
    
    Fixes an issue reported here:
    
    https://github.com/Zarel/Pokemon-Showdown/issues/5742#issuecomment-533850288
    
    * Fix several move validation issues
    
    Fixes #5742
    
    We have a new MoveSource type: R for Restricted. R moves work like
    level-up/tutor/TM moves, except you're limited to one R move.
    
    - Shedinja move stolen from Ninjask in Gen 3-4 are now R moves instead
      of event moves. This allows them to coexist with Nincada egg moves.
    
    - Necrozma-DW/DM now inherit moves/events from Necrozma (like Rotom,
      but with event validation). This allows them to be shiny.
    
    - Pokemon can now get egg moves from their own evolutions. This fixes
      some Tyrogue, Charmander, and Treecko sets mentioned in #5742
    
    - Some more C moves were added, fixing some Hitmontop and Chatot sets
      mentioned in #5742
    
    * Improve ability/move compatibility validator

--- data/rulesets.js
+++ data/rulesets.js
@@ -14,28 +14,26 @@ let BattleFormats = {
 		name: 'Standard',
 		desc: "The standard ruleset for all offical Smogon singles tiers (Ubers, OU, etc.)",
 		ruleset: ['Sleep Clause Mod', 'Species Clause', 'Nickname Clause', 'OHKO Clause', 'Moody Clause', 'Evasion Moves Clause', 'Endless Battle Clause', 'HP Percentage Mod', 'Cancel Mod'],
-		banlist: ['Unreleased', 'Illegal'],
 	},
 	standardnext: {
 		effectType: 'ValidatorRule',
 		name: 'Standard NEXT',
 		desc: "The standard ruleset for the NEXT mod",
-		ruleset: ['Sleep Clause Mod', 'Species Clause', 'Nickname Clause', 'OHKO Clause', 'HP Percentage Mod', 'Cancel Mod'],
-		banlist: ['Illegal', 'Soul Dew'],
+		ruleset: ['+Unreleased', 'Sleep Clause Mod', 'Species Clause', 'Nickname Clause', 'OHKO Clause', 'HP Percentage Mod', 'Cancel Mod'],
+		banlist: ['Soul Dew'],
 	},
 	standardubers: {
 		effectType: 'ValidatorRule',
 		name: 'Standard Ubers',
 		desc: "The standard ruleset for [Gen 5] Ubers",
 		ruleset: ['Sleep Clause Mod', 'Species Clause', 'Nickname Clause', 'Moody Clause', 'OHKO Clause', 'Endless Battle Clause', 'HP Percentage Mod', 'Cancel Mod'],
-		banlist: ['Unreleased', 'Illegal'],
 	},
 	standardgbu: {
 		effectType: 'ValidatorRule',
 		name: 'Standard GBU',
 		desc: "The standard ruleset for all official in-game Pok&eacute;mon tournaments and Battle Spot",
 		ruleset: ['Species Clause', 'Nickname Clause', 'Item Clause', 'Team Preview', 'Cancel Mod'],
-		banlist: ['Unreleased', 'Illegal', 'Battle Bond',
+		banlist: ['Battle Bond',
 			'Mewtwo', 'Mew',
 			'Lugia', 'Ho-Oh', 'Celebi',
 			'Kyogre', 'Groudon', 'Rayquaza', 'Jirachi', 'Deoxys',
@@ -55,7 +53,7 @@ let BattleFormats = {
 		name: 'Minimal GBU',
 		desc: "The standard ruleset for official tournaments, but without Restricted Legendary bans",
 		ruleset: ['Species Clause', 'Nickname Clause', 'Item Clause', 'Team Preview', 'Cancel Mod'],
-		banlist: ['Unreleased', 'Illegal', 'Battle Bond',
+		banlist: ['Battle Bond',
 			'Mew',
 			'Celebi',
 			'Jirachi', 'Deoxys',
@@ -75,137 +73,70 @@ let BattleFormats = {
 		name: 'Standard Doubles',
 		desc: "The standard ruleset for all official Smogon doubles tiers",
 		ruleset: ['Species Clause', 'Nickname Clause', 'OHKO Clause', 'Moody Clause', 'Evasion Abilities Clause', 'Evasion Moves Clause', 'Endless Battle Clause', 'HP Percentage Mod', 'Cancel Mod'],
-		banlist: ['Unreleased', 'Illegal'],
 	},
-	pokemon: {
+	validate: {
 		effectType: 'ValidatorRule',
-		name: 'Pokemon',
-		desc: "Applies the basic limitations of pokemon games: level 100, 6 pokemon, 4 moves, no CAP, no future-gen pokemon/moves/etc - but does not include illegal move/ability validation",
-		onValidateTeam(team, format) {
-			let problems = [];
-			if (team.length > 6) problems.push('Your team has more than six Pok\u00E9mon.');
-			// ----------- legality line ------------------------------------------
-			if (!format || !this.getRuleTable(format).has('-illegal')) return problems;
-			// everything after this line only happens if we're doing legality enforcement
-			let kyurems = 0;
-			let ndm = 0;
-			let ndw = 0;
+		name: 'Obtainable',
+		desc: "Makes sure the team is possible to obtain in-game.",
+		ruleset: ['Obtainable Moves', 'Obtainable Abilities', 'Obtainable Formes', 'Obtainable Misc'],
+		banlist: ['Unreleased', 'Nonexistent'],
+	},
+	validatemoves: {
+		effectType: 'ValidatorRule',
+		name: 'Obtainable Moves',
+		desc: "Makes sure moves are learnable by the species.",
+		banlist: [
+			'Chansey + Charm + Seismic Toss', 'Chansey + Charm + Psywave',
+			'Blissey + Charm + Seismic Toss', 'Blissey + Charm + Psywave',
+			'Shiftry + Leaf Blade + Sucker Punch',
+		],
+		// Mostly hardcoded in team-validator.ts
+	},
+	validateabilities: {
+		effectType: 'ValidatorRule',
+		name: 'Obtainable Abilities',
+		desc: "Makes sure abilities match the species.",
+		// Hardcoded in team-validator.ts
+	},
+	validateformes: {
+		effectType: 'ValidatorRule',
+		name: 'Obtainable Formes',
+		desc: "Makes sure in-battle formes only appear in-battle.",
+		// Mostly hardcoded in team-validator.ts
+		onValidateTeam(team) {
+			let kyuremCount = 0;
+			let necrozmaDMCount = 0;
+			let necrozmaDWCount = 0;
 			for (const set of team) {
 				if (set.species === 'Kyurem-White' || set.species === 'Kyurem-Black') {
-					if (kyurems > 0) {
-						problems.push('You cannot have more than one Kyurem-Black/Kyurem-White.');
-						break;
+					if (kyuremCount > 0) {
+						return ['You cannot have more than one Kyurem-Black/Kyurem-White.'];
 					}
-					kyurems++;
+					kyuremCount++;
 				}
 				if (set.species === 'Necrozma-Dusk-Mane') {
-					if (ndm > 0) {
-						problems.push('You cannot have more than one Necrozma-Dusk-Mane.');
-						break;
+					if (necrozmaDMCount > 0) {
+						return ['You cannot have more than one Necrozma-Dusk-Mane.'];
 					}
-					ndm++;
+					necrozmaDMCount++;
 				}
 				if (set.species === 'Necrozma-Dawn-Wings') {
-					if (ndw > 0) {
-						problems.push('You cannot have more than one Necrozma-Dawn-Wings.');
-						break;
+					if (necrozmaDWCount > 0) {
+						return ['You cannot have more than one Necrozma-Dawn-Wings.'];
 					}
-					ndw++;
+					necrozmaDWCount++;
 				}
 			}
-			return problems;
+			return [];
 		},
-		onChangeSet(set, format) {
-			let item = this.getItem(set.item);
+	},
+	validatemisc: {
+		effectType: 'ValidatorRule',
+		name: 'Obtainable Misc',
+		desc: "Validate all obtainability things that aren't moves/abilities (Hidden Power type, gender, stats, etc).",
+		// Mostly hardcoded in team-validator.ts
+		onChangeSet(set) {
 			let template = this.getTemplate(set.species);
-			let problems = [];
-			let totalEV = 0;
-			let allowCAP = !!(format && this.getRuleTable(format).has('allowcap'));
-
-			if (set.species === set.name) delete set.name;
-			if (template.gen > this.gen) {
-				problems.push(set.species + ' does not exist in gen ' + this.gen + '.');
-			}
-			if (template.gen && template.gen !== this.gen && template.tier === 'Illegal') {
-				problems.push(set.species + ' does not exist outside of gen ' + template.gen + '.');
-			}
-			/**@type {Ability} */
-			// @ts-ignore
-			let ability = {};
-			if (set.ability) {
-				ability = this.getAbility(set.ability);
-				if (ability.gen > this.gen) {
-					problems.push(ability.name + ' does not exist in gen ' + this.gen + '.');
-				}
-			}
-			if (set.moves) {
-				for (const moveid of set.moves) {
-					let move = this.getMove(moveid);
-					if (move.gen > this.gen) {
-						problems.push(move.name + ' does not exist in gen ' + this.gen + '.');
-					} else if (!allowCAP && move.isNonstandard) {
-						problems.push(move.name + ' does not exist.');
-					}
-				}
-			}
-			if (item.gen > this.gen) {
-				problems.push(item.name + ' does not exist in gen ' + this.gen + '.');
-			}
-			if (set.moves && set.moves.length > 4) {
-				problems.push((set.name || set.species) + ' has more than four moves.');
-			}
-			if (set.level && set.level > 100) {
-				problems.push((set.name || set.species) + ' is higher than level 100.');
-			}
-
-			if (!allowCAP || !template.tier.startsWith('CAP')) {
-				if (template.isNonstandard && template.num > -5000) {
-					problems.push(set.species + ' does not exist.');
-				}
-			}
-
-			if (!allowCAP && ability.isNonstandard) {
-				problems.push(ability.name + ' does not exist.');
-			}
-
-			if (item.isNonstandard) {
-				if (item.isNonstandard === 'Past' || item.isNonstandard === 'Future') {
-					problems.push(item.name + ' does not exist in this generation.');
-				} else if (!allowCAP) {
-					problems.push(item.name + ' does not exist.');
-				}
-			}
-
-			if (set.evs) {
-				for (let k in set.evs) {
-					// @ts-ignore
-					if (typeof set.evs[k] !== 'number' || set.evs[k] < 0) {
-						// @ts-ignore
-						set.evs[k] = 0;
-					}
-					// @ts-ignore
-					totalEV += set.evs[k];
-				}
-			}
-
-			// In gen 6, it is impossible to battle other players with pokemon that break the EV limit
-			if (totalEV > 510 && this.gen === 6) {
-				problems.push((set.name || set.species) + " has more than 510 total EVs.");
-			}
-
-			// ----------- legality line ------------------------------------------
-			if (!this.getRuleTable(format).has('-illegal')) return problems;
-			// everything after this line only happens if we're doing legality enforcement
-
-			// Pokestar studios
-			if (template.num <= -5000 && template.isNonstandard) {
-				problems.push(`${set.species} cannot be obtained by legal means.`);
-			}
-
-			// only in gen 1 and 2 it was legal to max out all EVs
-			if (this.gen >= 3 && totalEV > 510) {
-				problems.push((set.name || set.species) + " has more than 510 total EVs.");
-			}
 
 			if (template.gender) {
 				if (set.gender !== template.gender) {
@@ -215,20 +146,6 @@ let BattleFormats = {
 				if (set.gender !== 'M' && set.gender !== 'F') {
 					set.gender = '';
 				}
-			}
-
-			// Legendary Pokemon must have at least 3 perfect IVs in gen 6
-			let baseTemplate = this.getTemplate(template.baseSpecies);
-			if (set.ivs && this.gen >= 6 && (baseTemplate.gen >= 6 || format.requirePentagon) && (template.eggGroups[0] === 'Undiscovered' || template.species === 'Manaphy') && !template.prevo && !template.nfe &&
-				// exceptions
-				template.species !== 'Unown' && template.baseSpecies !== 'Pikachu' && (template.baseSpecies !== 'Diancie' || !set.shiny)) {
-				let perfectIVs = 0;
-				for (let i in set.ivs) {
-					// @ts-ignore
-					if (set.ivs[i] >= 31) perfectIVs++;
-				}
-				let reason = (format.requirePentagon ? " and this format requires gen " + this.gen + " Pokémon" : " in gen 6");
-				if (perfectIVs < 3) problems.push((set.name || set.species) + " must have at least three perfect IVs because it's a legendary" + reason + ".");
 			}
 
 			// limit one of each move
@@ -245,78 +162,7 @@ let BattleFormats = {
 				}
 			}
 			set.moves = moves;
-
-			let battleForme = template.battleOnly && template.species;
-			if (battleForme) {
-				if (template.requiredAbility && set.ability !== template.requiredAbility) {
-					problems.push("" + template.species + " transforms in-battle with " + template.requiredAbility + "."); // Darmanitan-Zen, Zygarde-Complete
-				}
-				if (template.requiredItems) {
-					if (template.species === 'Necrozma-Ultra') {
-						problems.push(`Necrozma-Ultra must start the battle as Necrozma-Dawn-Wings or Necrozma-Dusk-Mane holding Ultranecrozium Z.`); // Necrozma-Ultra transforms from one of two formes, and neither one is the base forme
-					} else if (!template.requiredItems.includes(item.name)) {
-						problems.push(`${template.species} transforms in-battle with ${Chat.plural(template.requiredItems.length, "either ") + template.requiredItems.join(" or ")}.`); // Mega or Primal
-					}
-				}
-				if (template.requiredMove && set.moves.indexOf(toID(template.requiredMove)) < 0) {
-					problems.push(`${template.species} transforms in-battle with ${template.requiredMove}.`); // Meloetta-Pirouette, Rayquaza-Mega
-				}
-				if (!format.noChangeForme) set.species = template.baseSpecies; // Fix battle-only forme
-			} else {
-				if (template.requiredAbility && set.ability !== template.requiredAbility) {
-					problems.push(`${(set.name || set.species)} needs the ability ${template.requiredAbility}.`); // No cases currently.
-				}
-				if (template.requiredItems && !template.requiredItems.includes(item.name)) {
-					problems.push(`${(set.name || set.species)} needs to hold ${Chat.plural(template.requiredItems.length, "either ") + template.requiredItems.join(" or ")}.`); // Memory/Drive/Griseous Orb/Plate/Z-Crystal - Forme mismatch
-				}
-				if (template.requiredMove && set.moves.indexOf(toID(template.requiredMove)) < 0) {
-					problems.push(`${(set.name || set.species)} needs to have the move ${template.requiredMove}.`); // Keldeo-Resolute
-				}
-
-				// Mismatches between the set forme (if not base) and the item signature forme will have been rejected already.
-				// It only remains to assign the right forme to a set with the base species (Arceus/Genesect/Giratina/Silvally).
-				if (item.forcedForme && template.species === this.getTemplate(item.forcedForme).baseSpecies && !format.noChangeForme) {
-					set.species = item.forcedForme;
-				}
-			}
-
-			if (template.species === 'Pikachu-Cosplay') {
-				/**@type {{[k: string]: string}} */
-				let cosplay = {meteormash: 'Pikachu-Rock-Star', iciclecrash: 'Pikachu-Belle', drainingkiss: 'Pikachu-Pop-Star', electricterrain: 'Pikachu-PhD', flyingpress: 'Pikachu-Libre'};
-				for (const moveid of set.moves) {
-					if (moveid in cosplay) {
-						set.species = cosplay[moveid];
-						break;
-					}
-				}
-			}
-
-			if (set.species !== template.species) {
-				// Autofixed forme.
-				template = this.getTemplate(set.species);
-
-				if (!this.getRuleTable(format).has('ignoreillegalabilities') && !format.noChangeAbility) {
-					// Ensure that the ability is (still) legal.
-					let legalAbility = false;
-					for (let i in template.abilities) {
-						// @ts-ignore
-						if (template.abilities[i] !== set.ability) continue;
-						legalAbility = true;
-						break;
-					}
-					if (!legalAbility) { // Default to first ability.
-						set.ability = template.abilities['0'];
-					}
-				}
-			}
-
-			return problems;
 		},
-		banlist: [
-			'Chansey + Charm + Seismic Toss', 'Chansey + Charm + Psywave',
-			'Blissey + Charm + Seismic Toss', 'Blissey + Charm + Psywave',
-			'Shiftry + Leaf Blade + Sucker Punch',
-		],
 	},
 	hoennpokedex: {
 		effectType: 'ValidatorRule',
@@ -852,7 +698,7 @@ let BattleFormats = {
 		effectType: 'ValidatorRule',
 		name: 'STABmons Move Legality',
 		desc: "Allows Pok&eacute;mon to use any move that they or a previous evolution/out-of-battle forme share a type with",
-		checkLearnset(move, template, lsetData, set) {
+		checkLearnset(move, template, setSources, set) {
 			const restrictedMoves = this.format.restrictedMoves || [];
 			if (!move.isZ && !restrictedMoves.includes(move.name)) {
 				let dex = this.dex;
@@ -871,15 +717,9 @@ let BattleFormats = {
 				}
 				if (types.includes(move.type)) return null;
 			}
-			return this.checkLearnset(move, template, lsetData, set);
+			return this.checkLearnset(move, template, setSources, set);
 		},
 		unbanlist: ['Shiftry + Leaf Blade + Sucker Punch'],
-	},
-	allowcap: {
-		effectType: 'ValidatorRule',
-		name: 'Allow CAP',
-		desc: "Allows the use of Pok&eacute;mon, abilities, moves, and items made by the Create-A-Pok&eacute;mon project",
-		// Implemented in the 'pokemon' ruleset
 	},
 	allowtradeback: {
 		effectType: 'ValidatorRule',
--- sim/globals.ts
+++ sim/globals.ts
@@ -5,6 +5,7 @@ type Pokemon = import('./pokemon').Pokemon
 type PRNGSeed = import('./prng').PRNGSeed;
 type Side = import('./side').Side
 type TeamValidator = import('./team-validator').TeamValidator
+type PokemonSources = import('./team-validator').PokemonSources
 
 type ID = '' | string & {__isID: true};
 interface AnyObject {[k: string]: any}
@@ -46,6 +47,7 @@ type PokemonSet = {
  * - L = start or level-up, 3rd char+ is the level
  * - M = TM/HM
  * - T = tutor
+ * - R = restricted (special moves like Rotom moves)
  * - E = egg
  * - S = event, 3rd char+ is the index in .eventPokemon
  * - D = Dream World, only 5D is valid
@@ -59,48 +61,6 @@ type PokemonSet = {
  * check prevos for compatibility).
  */
 type MoveSource = string;
-
-/**
- * Describes a possible way to get a pokemon. Is not exhaustive!
- * sourcesBefore covers all sources that do not have exclusive
- * moves (like catching wild pokemon).
- *
- * First character is a generation number, 1-7.
- * Second character is a source ID, one of:
- *
- * - E = egg, 3rd char+ is the father in gen 2-5, empty in gen 6-7
- *   because egg moves aren't restricted to fathers anymore
- * - S = event, 3rd char+ is the index in .eventPokemon
- * - D = Dream World, only 5D is valid
- * - V = Virtual Console transfer, only 7V is valid
- *
- * Designed to match MoveSource where possible.
- */
-type PokemonSource = string;
-
-/**
- * Keeps track of how a pokemon with a given set might be obtained.
- *
- * `sources` is a list of possible PokemonSources, and a nonzero
- * sourcesBefore means the Pokemon is compatible with all possible
- * PokemonSources from that gen or earlier.
- *
- * `limitedEgg` tracks moves that can only be obtained from an egg with
- * another father in gen 2-5. If there are multiple such moves,
- * potential fathers need to be checked to see if they can actually
- * learn the move combination in question.
- */
-type PokemonSources = {
-	sources: PokemonSource[]
-	sourcesBefore: number
-	babyOnly?: string
-	sketchMove?: string
-	hm?: string
-	restrictiveMoves?: string[]
-	limitedEgg?: (string | 'self')[]
-	isHidden?: boolean
-	fastCheck?: true
-}
 
 type EventInfo = {
 	generation: number,
@@ -1015,8 +975,6 @@ interface FormatsData extends EventMethods {
 	maxForcedLevel?: number
 	maxLevel?: number
 	mod?: string
-	noChangeAbility?: boolean
-	noChangeForme?: boolean
 	onBasePowerPriority?: number
 	onModifyMovePriority?: number
 	onSwitchInPriority?: number
@@ -1035,7 +993,7 @@ interface FormatsData extends EventMethods {
 	timer?: Partial<GameTimerSettings>
 	tournamentShow?: boolean
 	unbanlist?: string[]
-	checkLearnset?: (this: TeamValidator, move: Move, template: Template, lsetData: PokemonSources, set: PokemonSet) => {type: string, [any: string]: any} | null
+	checkLearnset?: (this: TeamValidator, move: Move, template: Template, setSources: PokemonSources, set: PokemonSet) => {type: string, [any: string]: any} | null
 	onAfterMega?: (this: Battle, pokemon: Pokemon) => void
 	onBegin?: (this: Battle) => void
 	onChangeSet?: (this: ModdedDex, set: PokemonSet, format: Format, setHas?: AnyObject, teamHas?: AnyObject) => string[] | void
