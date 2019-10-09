d6752954efaf54e0c1b8cf9bdf384d688578df5d
Guangcong Luo <guangcongluo@gmail.com>
2019-10-10 06:33:33 +1100

    Further improve validator
    - Tyranitar egg moves are fixed
    
    - Staravia egg moves are now properly supported
      (9742ecf62f was a hack that caused the Tyranitar problem)
    
    - Some Pokemon evolve by having a move in their learnset (like Piloswine
      with Ancient Power). These can only carry three other moves from their
      prevo, because the fourth move must be the evo move. This restriction
      doesn't apply to gen 6+ eggs, which can get around the restriction with
      the relearner.
    
    Fixes #2287
    
    (Except Shiftry and friends, but those are already tracked in
    `rulesets:obtainablemoves`.)

--- sim/dex-data.ts
+++ sim/dex-data.ts
@@ -543,9 +543,11 @@ export class Template extends BasicEffect implements Readonly<BasicEffect & Temp
 	/** Added type (used in OMs). */
 	readonly addedType?: string;
 	/** Pre-evolution. '' if nothing evolves into this Pokemon. */
-	readonly prevo: string;
+	readonly prevo: ID;
 	/** Evolutions. Array because many Pokemon have multiple evolutions. */
-	readonly evos: string[];
+	readonly evos: ID[];
+	readonly evoType?: 'trade' | 'stone' | 'levelMove' | 'levelExtra' | 'levelFriendship' | 'levelHold';
+	readonly evoMove?: string;
 	/** Evolution level. falsy if doesn't evolve. */
 	readonly evoLevel?: number;
 	/** Is NFE? True if this Pokemon can evolve (Mega evolution doesn't count). */
@@ -646,6 +648,8 @@ export class Template extends BasicEffect implements Readonly<BasicEffect & Temp
 		this.tier = data.tier || '';
 		this.doublesTier = data.doublesTier || '';
 		this.evos = data.evos || [];
+		this.evoType = data.evoType || undefined;
+		this.evoMove = data.evoMove || undefined;
 		this.evoLevel = data.evoLevel || undefined;
 		this.nfe = !!this.evos.length;
 		this.eggGroups = data.eggGroups || [];
