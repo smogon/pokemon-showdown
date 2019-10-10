75aea9cbe816920377b6b82e6aa2cdd01864825c
Aurolux <36804393+Aurolux@users.noreply.github.com>
2019-10-10 00:05:46 -0500

    Simplify modnote to just player count

--- server/tournaments/index.ts
+++ server/tournaments/index.ts
@@ -607,8 +607,7 @@ export class Tournament extends Rooms.RoomGame {
 		}
 		this.isBracketInvalidated = true;
 		this.room.add(`|tournament|start|${this.players.length}`);
-		this.privateModAction(`(The ${tour.baseFormat} tournament by ${user.name} has started and has a current total of ${this.players.length} users.)`);
-		this.modlog('TOUR START', null, `${tour.baseFormat} with ${this.players.length} users.`);	// Modnote format and number of players when tour starts
+		this.modlog('TOUR START', null, `The tournament has started with ${this.players.length} users.`);	// Modnote number of players and when a tour starts
 		this.room.send('|tournament|update|{"isStarted":true}');
 		this.update();
 		return true;
