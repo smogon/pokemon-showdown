eec8c6c938e074c3418050d06e53cf1f24f8fc5f
urkerab <urkerab@users.noreply.github.com>
2019-10-12 16:12:24 +0100

    Maybe just onInvulnerability?

--- data/mods/gen1/moves.js
+++ data/mods/gen1/moves.js
@@ -276,7 +276,7 @@ let BattleMovedex = {
 		effect: {
 			duration: 2,
 			onLockMove: 'dig',
-			onTryInvulnerability(target, source, move) {
+			onInvulnerability(target, source, move) {
 				if (move.id === 'swift') return true;
 				this.add('-message', 'The foe ' + target.name + ' can\'t be hit underground!');
 				return null;
@@ -413,7 +413,7 @@ let BattleMovedex = {
 		effect: {
 			duration: 2,
 			onLockMove: 'fly',
-			onTryInvulnerability(target, source, move) {
+			onInvulnerability(target, source, move) {
 				if (move.id === 'swift') return true;
 				this.add('-message', 'The foe ' + target.name + ' can\'t be hit while flying!');
 				return null;
