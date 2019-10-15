c7248b6b8743f9df4295941e398168a87c98efe6
whales <jumbowhales@gmail.com>
2019-10-15 17:57:59 +1030

    Fix a few more userid -> id changes

--- server/chat-plugins/info.js
+++ server/chat-plugins/info.js
@@ -281,7 +281,7 @@ const commands = {
 			}
 		}
 
-		let punishments = Punishments.getRoomPunishments(targetUser || {userid});
+		let punishments = Punishments.getRoomPunishments(targetUser || {id: userid});
 
 		if (punishments && punishments.length) {
 			buf += `<br />Room punishments: `;
