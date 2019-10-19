8d6860bf0fc3f0af6e9c98854f833fbbd15788b4
TheJetOU <waleed.hassan.aidid@gmail.com>
2019-10-19 08:57:35 +0300

    Server: reorganize TypeScript globals This commit keeps modules in `server/global.d.ts` and moves types to `globals.ts` (a la `sim/`'s structure).

--- server/global.d.ts
+++ server/global.d.ts
@@ -1,94 +1,41 @@
-import * as child_process from 'child_process';
-
+import * as ChatType from './chat';
 import * as RoomsType from './rooms';
 import * as RoomlogsType from './roomlogs';
-import * as RoomGameType from './room-game';
-import * as RoomBattleType from './room-battle';
-import {LadderStore as LadderStoreType} from './ladders-remote';
-import {Ladders as LaddersType} from './ladders';
-import {Punishments as PunishmentsType} from './punishments';
+import * as SocketsType from './sockets';
+import * as TeamValidatorAsyncType from './team-validator-async';
+import * as UsersType from './users';
+import * as VerifierType from './verifier';
 
 import * as StreamsType from './../lib/streams';
-import * as UsersType from './users';
-import * as ChatType from './chat'
+
+import {IPTools as IPToolsType} from './ip-tools';
+import {LadderStore as LadderStoreType} from './ladders-remote';
+import {Ladders as LaddersType} from './ladders';
+import {LoginServer as LoginServerType} from './loginserver';
+import {Monitor as MonitorType} from './monitor';
+import {Punishments as PunishmentsType} from './punishments';
 
 declare global {
 	namespace NodeJS {
 		interface Global {
-			IPTools: any
-			Config: any
-			Chat: any
-			__version: {head: string, origin?: string, tree?: string}
+			IPTools: any;
+			Config: any;
+			Chat: any;
+			__version: {head: string, origin?: string, tree?: string};
 		}
 	}
-	// modules
-	const Punishments: typeof PunishmentsType
+	const Config: AnyObject;
+	const Chat: typeof ChatType.Chat;
+	const IPTools: typeof IPToolsType;
 	const Ladders: typeof LaddersType
-	const LadderStoreT: typeof LadderStoreType
-
-	namespace Streams {
-		export type WriteStream = StreamsType.WriteStream
-		export type ReadStream = StreamsType.ReadStream
-		export type ReadWriteStream = StreamsType.ReadWriteStream
-		export type ObjectWriteStream<T> = StreamsType.ObjectWriteStream<T>
-		export type ObjectReadStream<T> = StreamsType.ObjectReadStream<T>
-		export type ObjectReadWriteStream<T> = StreamsType.ObjectReadWriteStream<T>
-	}
-
-	type ChildProcess = child_process.ChildProcess
-
-	// rooms
-	type GlobalRoom = RoomsType.GlobalRoom
-	type ChatRoom = RoomsType.ChatRoom
-	type GameRoom = RoomsType.GameRoom
-	type BasicRoom = RoomsType.BasicRoom
-	type BasicChatRoom = RoomsType.BasicChatRoom
-	type RoomGame = RoomGameType.RoomGame
-	type RoomBattle = RoomBattleType.RoomBattle
-	const Rooms: typeof RoomsType.Rooms
-	const Roomlogs: typeof RoomlogsType.Roomlogs
-	type Roomlog = RoomlogsType.Roomlog
-	type Room = RoomsType.Room
-	type RoomID = string & {__isRoomID: true};
-	namespace Rooms {
-		export type GlobalRoom = RoomsType.GlobalRoom
-		export type ChatRoom = RoomsType.ChatRoom
-		export type GameRoom = RoomsType.GameRoom
-		export type BasicRoom = RoomsType.BasicRoom
-		export type BasicChatRoom = RoomsType.BasicChatRoom
-		export type RoomGame = RoomGameType.RoomGame
-		export type RoomBattle = RoomBattleType.RoomBattle
-		export type Roomlog = RoomlogsType.Roomlog
-		export type Room = RoomsType.Room
-	}
-
-	// users
+	const LadderStoreT: typeof LadderStoreType;
+	const LoginServer: typeof LoginServerType;
+	const Monitor: typeof MonitorType;
+	const Punishments: typeof PunishmentsType;
+	const Sockets: typeof SocketsType;
+	const TeamValidatorAsync: typeof TeamValidatorAsyncType;
+	const Rooms: typeof RoomsType.Rooms;
+	const Roomlogs: typeof RoomlogsType.Roomlogs;
 	const Users: typeof UsersType.Users
-	type User = UsersType.User
-	type Connection = UsersType.Connection
-	namespace Users {
-		export type User = UsersType.User
-		export type Connection = UsersType.Connection
-	}
-
-	// chat
-	const Chat: typeof ChatType.Chat
-	type CommandContext = ChatType.CommandContext
-	type PageContext = ChatType.PageContext
-	type PageTable = ChatType.PageTable
-	type ChatCommands = ChatType.ChatCommands
-	type ChatFilter = ChatType.ChatFilter
-	type NameFilter = ChatType.NameFilter
-	type StatusFilter = ChatType.StatusFilter
-	type LoginFilter = ChatType.LoginFilter
-	namespace Chat {
-		export type CommandContext = ChatType.CommandContext
-		export type PageContext = ChatType.PageContext
-		export type PageTable = ChatType.PageTable
-		export type ChatCommands = ChatType.ChatCommands
-		export type ChatFilter = ChatType.ChatFilter
-		export type NameFilter = ChatType.NameFilter
-		export type StatusFilter = ChatType.StatusFilter
-		export type LoginFilter = ChatType.LoginFilter
-	}
+	const Verifier: typeof VerifierType;
 }
