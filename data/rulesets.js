af1005dccbc4a36e7aaae802ebd24668e0854ed4
TheMezStrikes <TheMezStrikes@gmail.com>
2019-11-11 05:42:30 +0100

    Generalize NFE bans into validator rule (#5942)

--- data/rulesets.js
+++ data/rulesets.js
@@ -535,6 +535,20 @@ let BattleFormats = {
 			this.add('rule', 'Z-Move Clause: Z-Moves are banned');
 		},
 	},
+	nfeclause: {
+		effectType: 'ValidatorRule',
+		name: 'NFE Clause',
+		desc: "Bans Pok&eacute;mon that are fully evolved or can't evolve",
+		onValidateSet(set) {
+			const template = this.dex.getTemplate(set.species || set.name);
+			if (!template.nfe) {
+				return [set.species + " cannot evolve."];
+			}
+		},
+		onBegin() {
+			this.add('rule', 'NFE Clause: Fully Evolved Pok&eacute;mon are banned');
+		},
+	},
 	hppercentagemod: {
 		effectType: 'Rule',
 		name: 'HP Percentage Mod',
