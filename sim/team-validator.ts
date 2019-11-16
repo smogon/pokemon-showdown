46ca7b7235c01212012bfab00b4d0fd10135c58e
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2019-11-15 17:02:14 -0700

    fix again

--- sim/team-validator.ts
+++ sim/team-validator.ts
@@ -1185,10 +1185,22 @@ export class TeamValidator {
 			if (banReason === '') return null;
 		}
 
-		if (
-			tierTemplate.isNonstandard === 'Unusable' ||
-			tierTemplate.isNonstandard === 'Glitch' && dex.gen === 1
-		) {
+		if (tierTemplate.isNonstandard === 'Unusable') {
+			if (dex.gen === 5) {
+				banReason = ruleTable.check('pokemontag:hackmons', setHas);
+				if (banReason) {
+					return `${tierTemplate.species} is not obtainable without hacking.`;
+				}
+				if (banReason === '') return null;
+			}
+			if (dex.gen === 8) {
+				banReason = ruleTable.check('pokemontag:hackmons', setHas);
+				if (banReason) {
+					return `${tierTemplate.species} is not obtainable without hacking.`;
+				}
+				if (banReason === '') return null;
+			}
+		} else if (tierTemplate.isNonstandard === 'Glitch' && dex.gen === 1) {
 			banReason = ruleTable.check('pokemontag:hackmons', setHas);
 			if (banReason) {
 				return `${tierTemplate.species} is not obtainable without hacking.`;
