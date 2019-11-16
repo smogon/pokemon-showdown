2a225598f828a1449379dd1565c23acfe0a09ba0
The Immortal <the_immortal123@live.com>
2019-11-16 22:52:22 +0400

    Gen 7: Fix items mod
    Sorted it as well.

--- data/mods/gen7/items.js
+++ data/mods/gen7/items.js
@@ -1,6 +1,22 @@
 'use strict';
 /**@type {{[k: string]: ModdedItemData}} */
 let BattleItems = {
+	abomasite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	absolite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	aerodactylite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	aggronite: {
+		inherit: true,
+		isNonstandard: null,
+	},
 	aguavberry: {
 		inherit: true,
 		desc: "Restores 1/2 max HP at 1/4 max HP or less; confuses if -SpD Nature. Single use.",
@@ -11,6 +27,90 @@ let BattleItems = {
 			}
 		},
 	},
+	alakazite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	altarianite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	aloraichiumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	ampharosite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	audinite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	banettite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	beedrillite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	blastoisinite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	blazikenite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	buginiumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	cameruptite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	charizarditex: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	charizarditey: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	darkiniumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	decidiumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	diancite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	dragoniumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	eeviumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	electriumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	fairiumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	fightiniumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
 	figyberry: {
 		inherit: true,
 		desc: "Restores 1/2 max HP at 1/4 max HP or less; confuses if -Atk Nature. Single use.",
@@ -20,6 +120,58 @@ let BattleItems = {
 				pokemon.addVolatile('confusion');
 			}
 		},
+	},
+	firiumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	flyiniumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	galladite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	garchompite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	gardevoirite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	gengarite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	ghostiumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	glalitite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	grassiumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	groundiumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	gyaradosite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	heracronite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	houndoominite: {
+		inherit: true,
+		isNonstandard: null,
 	},
 	iapapaberry: {
 		inherit: true,
@@ -35,9 +187,49 @@ let BattleItems = {
 		inherit: true,
 		desc: "Evolves Alolan Sandshrew into Alolan Sandslash and Alolan Vulpix into Alolan Ninetales when used.",
 	},
+	iciumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	inciniumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	kangaskhanite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	kommoniumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	latiasite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	latiosite: {
+		inherit: true,
+		isNonstandard: null,
+	},
 	leafstone: {
 		inherit: true,
 		desc: "Evolves Gloom into Vileplume, Weepinbell into Victreebel, Exeggcute into Exeggutor or Alolan Exeggutor, Nuzleaf into Shiftry, and Pansage into Simisage when used.",
+	},
+	lopunnite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	lucarionite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	lunaliumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	lycaniumz: {
+		inherit: true,
+		isNonstandard: null,
 	},
 	magoberry: {
 		inherit: true,
@@ -49,349 +241,155 @@ let BattleItems = {
 			}
 		},
 	},
+	manectite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	marshadiumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	mawilite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	medichamite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	metagrossite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	mewniumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	mewtwonitex: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	mewtwonitey: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	mimikiumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	normaliumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	pidgeotite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	pikaniumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	pikashuniumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	pinsirite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	poisoniumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	primariumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	psychiumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	rockiumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	sablenite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	salamencite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	sceptilite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	scizorite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	sharpedonite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	slowbronite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	snorliumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	solganiumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	steeliumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	steelixite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	swampertite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	tapuniumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
 	thunderstone: {
 		inherit: true,
 		desc: "Evolves Pikachu into Raichu or Alolan Raichu, Eevee into Jolteon, and Eelektrik into Eelektross when used.",
 	},
+	tyranitarite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	ultranecroziumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	venusaurite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	wateriumz: {
+		inherit: true,
+		isNonstandard: null,
+	},
 	wikiberry: {
 		inherit: true,
-		desc: "Restores 1/2 max HP at 1/4 max HP or less; confuses if -SpA Nature. Single use.",
+		isNonstandard: null,
 		onEat(pokemon) {
 			this.heal(pokemon.maxhp / 2);
 			if (pokemon.getNature().minus === 'spa') {
 				pokemon.addVolatile('confusion');
 			}
 		},
-	},
-
-	"abomasite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"absolite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"aerodactylite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"aggronite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"alakazite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"altarianite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"ampharosite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"audinite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"banettite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"beedrillite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"blastoisinite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"blazikenite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"cameruptite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"charizarditex": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"charizarditey": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"diancite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"galladite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"garchompite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"gardevoirite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"gengarite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"glalitite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"gyaradosite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"heracronite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"houndoominite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"kangaskhanite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"latiasite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"latiosite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"lopunnite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"lucarionite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"manectite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"mawilite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"medichamite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"metagrossite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"mewtwonitex": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"mewtwonitey": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"pidgeotite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"pinsirite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"sablenite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"salamencite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"sceptilite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"scizorite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"sharpedonite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"slowbronite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"steelixite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"swampertite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"tyranitarite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-	"venusaurite": {
-		inherit: true,
-		isNonstandard: null,
-	},
-
-	"aloraichiumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"buginiumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"darkiniumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"decidiumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"dragoniumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"eeviumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"electriumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"fairiumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"fightiniumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"firiumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"flyiniumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"ghostiumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"grassiumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"groundiumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"iciumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"inciniumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"kommoniumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"lunaliumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"lycaniumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"marshadiumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"mewniumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"mimikiumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"normaliumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"pikaniumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"pikashuniumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"poisoniumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"primariumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"psychiumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"rockiumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"snorliumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"solganiumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"steeliumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"tapuniumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"ultranecroziumz": {
-		inherit: true,
-		isNonstandard: "Past",
-	},
-	"wateriumz": {
-		inherit: true,
-		isNonstandard: "Past",
 	},
 };
 exports.BattleItems = BattleItems;
