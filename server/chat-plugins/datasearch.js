e1feef177a983361b4ebd8ce2b1fe17aac2fb864
TheMezStrikes <TheMezStrikes@gmail.com>
2019-11-16 14:15:29 +0100

    quickfix gen8ou not existing

--- server/chat-plugins/datasearch.js
+++ server/chat-plugins/datasearch.js
@@ -738,7 +738,8 @@ function runDexsearch(target, cmd, canAll, message) {
 			}
 			if (matched) continue;
 
-			const validator = TeamValidator.get(`gen${maxGen}ou`);
+			let validator = TeamValidator.get(`gen${maxGen}ou`);
+			if (maxGen === 8) validator = TeamValidator.get(`gen8oubeta`);
 			let pokemonSource = validator.allSources();
 			for (let move in alts.moves) {
 				if (!validator.checkLearnset(move, mon, pokemonSource) === alts.moves[move]) {
