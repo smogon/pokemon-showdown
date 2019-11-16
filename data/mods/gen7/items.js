bb8e9c4702b999a535cd11f8432166611d130da8
Guangcong Luo <guangcongluo@gmail.com>
2019-11-16 19:27:14 +1300

    Restrict Z-crystals and mega stones to Gen 7

--- data/mods/gen7/items.js
+++ data/mods/gen7/items.js
@@ -63,5 +63,279 @@ let BattleItems = {
 			}
 		},
 	},
+
+	"abomasite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"absolite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"aerodactylite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"aggronite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"alakazite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"altarianite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"ampharosite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"audinite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"banettite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"beedrillite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"blastoisinite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"blazikenite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"cameruptite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"charizarditex": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"charizarditey": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"diancite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"galladite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"garchompite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"gardevoirite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"gengarite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"glalitite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"gyaradosite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"heracronite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"houndoominite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"kangaskhanite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"latiasite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"latiosite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"lopunnite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"lucarionite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"manectite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"mawilite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"medichamite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"metagrossite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"mewtwonitex": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"mewtwonitey": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"pidgeotite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"pinsirite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"sablenite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"salamencite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"sceptilite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"scizorite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"sharpedonite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"slowbronite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"steelixite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"swampertite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"tyranitarite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"venusaurite": {
+		inherit: true,
+		isNonstandard: null,
+	},
+
+	"buginiumz": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"darkiniumz": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"dragoniumz": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"electriumz": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"fairiumz": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"fightiniumz": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"firiumz": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"flyiniumz": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"ghostiumz": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"grassiumz": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"groundiumz": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"iciumz": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"lunaliumz": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"normaliumz": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"poisoniumz": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"psychiumz": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"rockiumz": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"solganiumz": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"steeliumz": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"ultranecroziumz": {
+		inherit: true,
+		isNonstandard: null,
+	},
+	"wateriumz": {
+		inherit: true,
+		isNonstandard: null,
+	},
 };
 exports.BattleItems = BattleItems;
