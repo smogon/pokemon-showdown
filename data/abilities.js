1082258f84e3bf26390279e0168670fc4b705314
Guangcong Luo <guangcongluo@gmail.com>
2019-11-17 07:18:03 +1300

    Clangorous Soul can self-target through Soundproof

--- data/abilities.js
+++ data/abilities.js
@@ -3669,7 +3669,7 @@ let BattleAbilities = {
 	"soundproof": {
 		shortDesc: "This Pokemon is immune to sound-based moves, including Heal Bell.",
 		onTryHit(target, source, move) {
-			if (move.flags['sound']) {
+			if (move.target !== 'self' && move.flags['sound']) {
 				this.add('-immune', target, '[from] ability: Soundproof');
 				return null;
 			}
