e13fa6f0dbd06bf14c0238fc0768804decc70c23
HoeenHero <HoeenCoder@users.noreply.github.com>
2019-11-15 23:46:13 -0500

    Fix issues with Max Guard priority

--- data/scripts.js
+++ data/scripts.js
@@ -1190,6 +1190,7 @@ let BattleScripts = {
 	},
 
 	getMaxMove(move, pokemon) {
+		if (typeof move === 'string') move = this.dex.getMove(move);
 		if (pokemon.canGigantamax) {
 			let gMaxTemplate = this.dex.getTemplate(pokemon.canGigantamax);
 			let gMaxMove = this.dex.getMove(gMaxTemplate.isGigantamax);
@@ -1200,14 +1201,16 @@ let BattleScripts = {
 	},
 
 	getActiveMaxMove(move, pokemon) {
+		if (typeof move === 'string') move = this.dex.getActiveMove(move);
 		let maxMove = this.dex.getActiveMove(this.maxMoveTable[move.category === 'Status' ? move.category : move.type]);
 		if (pokemon.canGigantamax) {
 			let gMaxTemplate = this.dex.getTemplate(pokemon.canGigantamax);
 			let gMaxMove = this.dex.getActiveMove(gMaxTemplate.isGigantamax ? gMaxTemplate.isGigantamax : '');
 			if (gMaxMove.exists && gMaxMove.type === move.type) maxMove = gMaxMove;
 		}
+		// TODO defined a Max Move Power in all moves and use that
 		// @ts-ignore
-		maxMove.basePower = move.zMovePower; // TODO defined a Max Move Power in all moves and use that
+		if (move.category !== 'Status') maxMove.basePower = move.zMovePower;
 		maxMove.maxPowered = true;
 		return maxMove;
 	},
