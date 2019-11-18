61a58eb4e7a91cc57a7083f222b5f5a249e6e84e
Codertogekiss <57895217+Codertogekiss@users.noreply.github.com>
2019-11-18 15:13:34 +0530

    Add Gen 8 LC (Beta)
    Add Gen 8 LC beta, initial banlist is baton pass, swirlix and sneasel.

--- config/formats.js
+++ config/formats.js
@@ -661,6 +661,21 @@ let Formats = [
 		requireGalar: true,
 	},
 	{
+		name: "[Gen 8] LC (Beta) ",
+		threads: [`&bullet; <a href="https://www.smogon.com/forums/threads/metagame-discussion-thread.3656348/">Metagame Discussion</a>`,
+			
+		],
+
+		mod: 'gen8',
+		maxLevel: 5,
+		ruleset: ['Obtainable', 'Standard' , 'Team Preview', 'Little Cup'],
+		banlist: [
+			'Sneasel', 'Swirlix',
+	 'Baton Pass', 'Dragon Rage', 'Sonic Boom',
+		], 
+                 requireGalar: true, 
+	}
+	{
 		name: "[Gen 8] Custom Game",
 
 		mod: 'gen8',
