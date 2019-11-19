68e732d17ac3080608039e267b0f46995a8d20e2
The Immortal <the_immortal123@live.com>
2019-11-20 03:20:00 +0400

    Add Dynamax Clause

--- data/rulesets.js
+++ data/rulesets.js
@@ -674,6 +674,17 @@ let BattleFormats = {
 			}
 		},
 	},
+	dynamaxclause: {
+		effectType: 'Rule',
+		name: 'Dynamax Clause',
+		desc: "Prevents Pok&eacute;mon from dynamaxing",
+		onBegin() {
+			for (const pokemon of this.getAllPokemon()) {
+				pokemon.canDynamax = null;
+			}
+			this.add('rule', 'Dynamax Clause: You cannot dynamax');
+		},
+	},
 	arceusevclause: {
 		effectType: 'ValidatorRule',
 		name: 'Arceus EV Clause',
