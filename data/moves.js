ee5d2c6e250e46c4f9dfb4ec1f30d5e4ebbced68
HoeenHero <HoeenCoder@users.noreply.github.com>
2019-11-21 20:42:24 -0500

    Fishious Rend is boosted by Strong Jaw
    https://www.smogon.com/forums/threads/3655528/page-15#post-8288436

--- data/moves.js
+++ data/moves.js
@@ -5642,7 +5642,7 @@ let BattleMovedex = {
 		name: "Fishious Rend",
 		pp: 10,
 		priority: 0,
-		flags: {contact: 1, protect: 1, mirror: 1},
+		flags: {bite: 1, contact: 1, protect: 1, mirror: 1},
 		secondary: null,
 		target: "normal",
 		type: "Water",
