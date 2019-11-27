3939d140811ce0728e60ca56b261dd0ae2003e27
The Immortal <the_immortal123@live.com>
2019-11-28 02:52:39 +0400

    Remove Moody Clause from Standard ruleset

--- data/mods/gen7/rulesets.js
+++ data/mods/gen7/rulesets.js
@@ -2,6 +2,10 @@
 
 /**@type {{[k: string]: ModdedFormatsData}} */
 let BattleFormats = {
+	standard: {
+		inherit: true,
+		ruleset: ['Sleep Clause Mod', 'Species Clause', 'Nickname Clause', 'OHKO Clause', 'Moody Clause', 'Evasion Moves Clause', 'Endless Battle Clause', 'HP Percentage Mod', 'Cancel Mod'],
+	},
 	standarddoubles: {
 		inherit: true,
 		ruleset: ['Species Clause', 'Nickname Clause', 'OHKO Clause', 'Moody Clause', 'Evasion Abilities Clause', 'Evasion Moves Clause', 'Endless Battle Clause', 'HP Percentage Mod', 'Cancel Mod'],
