smogon/pokemon-showdown

commit 662144419069418db25fa201ef617b821cec8190

Prevent Eternamax Max HP Doubling (#6149)

data/statuses.js
@@ -751,9 +751,7 @@ let BattleStatuses = {
 		},
 		onFlinch: false,
 		onBeforeSwitchOut(pokemon) {
-			if (pokemon.species !== 'Eternatus-Eternamax') {
-				pokemon.removeVolatile('dynamax');
-			}
+			pokemon.removeVolatile('dynamax');
 		},
 		onDragOutPriority: 2,
 		onDragOut(pokemon) {

commit 0111469eb66e29685e14bb2fe7c64f6e9fb0440c

Fix bug caused by #6149
Eternamax was losing dynamax benefits after switching out once. This changes the way eternatus-eternamax interacts with the volatile to completely remove health-related matters from the dynamax volatile, instead being handled by the eternamax volatile right at the beginning. 
I've reviewed videos, and none of them have an indicator of any such thing as "eternamax" starting, so a silent start is the most accurate here. It's intrinsic to the mon itself, except weirdly without an accompanying ability.

data/statuses.js
@@ -743,15 +743,12 @@ let BattleStatuses = {
 		onSwitchIn(pokemon) { // Putting Eternamax in onSwitchIn so it shows up everytime Eternatus switches in.
 			if (pokemon.species !== 'Eternatus-Eternamax') return; // Special for Eternatus' Eternamax forme
 			pokemon.removeVolatile('substitute');
-			this.add('-start', pokemon, 'Eternamax');
+			//this.add('-start', pokemon, 'Eternamax');
 			this.effectData.duration = 0;
-			pokemon.maxhp = Math.floor(pokemon.maxhp * 2);
-			pokemon.hp = Math.floor(pokemon.hp * 2);
-			this.add('-heal', pokemon, pokemon.getHealth, '[silent]');
 		},
 		onFlinch: false,
 		onBeforeSwitchOut(pokemon) {
-			pokemon.removeVolatile('dynamax');
+			if(pokemon.species !== 'Eternatus-Eternamax') pokemon.removeVolatile('dynamax');
 		},
 		onDragOutPriority: 2,
 		onDragOut(pokemon) {
@@ -759,7 +756,7 @@ let BattleStatuses = {
 			return null;
 		},
 		onEnd(pokemon) {
-			if (pokemon.species !== 'Eternatus-Eternamax') this.add('-end', pokemon, 'Dynamax');
+			this.add('-end', pokemon, 'Dynamax');
 			if (pokemon.canGigantamax) pokemon.formeChange(pokemon.baseTemplate.species);
 			if (pokemon.species === 'Shedinja') return;
 			pokemon.hp = pokemon.getUndynamaxedHP();
@@ -781,6 +778,8 @@ let BattleStatuses = {
 		onStart(pokemon) {
 			if (pokemon.transformed) return;
 			pokemon.addVolatile('dynamax');
+			pokemon.maxhp = Math.floor(pokemon.maxhp * 2);
+			pokemon.hp = Math.floor(pokemon.hp * 2);
 		},
 	},
 	arceus: {