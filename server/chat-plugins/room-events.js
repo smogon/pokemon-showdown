47cc496390b09ac47b302654645296db71252205
Dragonmirror27262 <ronak.monga@gmail.com>
2019-12-11 12:20:35 -0800

    Fix roomevents help messages (#5800)

--- server/chat-plugins/room-events.js
+++ server/chat-plugins/room-events.js
@@ -192,9 +192,9 @@ exports.commands = {
 	roomeventshelp: [
 		`/roomevents - Displays a list of upcoming room-specific events.`,
 		`/roomevents add [event name] | [event date/time] | [event description] - Adds a room event. A timestamp in event date/time field like YYYY-MM-DD HH:MM±hh:mm will be displayed in user's timezone. Requires: @ # & ~`,
-		`/roomevents start [event name] - Declares to the room that the event has started.`,
+		`/roomevents start [event name] - Declares to the room that the event has started. Requires: @ # & ~`,
 		`/roomevents remove [event name] - Deletes an event. Requires: @ # & ~`,
+		`/roomevents sortby [column name] | [asc/desc (optional)] - Sorts events table by column name and an optional argument to ascending or descending order. Ascending order is default. Requires: @ # & ~`,
 		`/roomevents view [event name] - Displays information about a specific event.`,
-		`/roomevents sortby [column name] | [asc/desc (optional)] - Sorts events table by column name and an optional argument to ascending or descending order. Ascending order is default`,
 	],
 };
