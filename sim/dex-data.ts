c795f18bbf837543546e52f4012984eee1ebe6bc
Quinton Lee <quivistis@gmail.com>
2019-12-15 01:11:23 -0600

    Dex: fix Darmanitan-Galar-Zen's gen (#6187)

--- sim/dex-data.ts
+++ sim/dex-data.ts
@@ -673,7 +673,7 @@ export class Template extends BasicEffect implements Readonly<BasicEffect & Temp
 		this.inheritsFrom = data.inheritsFrom || undefined;
 
 		if (!this.gen && this.num >= 1) {
-			if (this.num >= 810 || this.forme.endsWith('Galar') || this.forme === 'Gmax') {
+			if (this.num >= 810 || ['Gmax', 'Galar', 'Galar-Zen'].includes(this.forme)) {
 				this.gen = 8;
 			} else if (this.num >= 722 || this.forme.startsWith('Alola') || this.forme === 'Starter') {
 				this.gen = 7;
