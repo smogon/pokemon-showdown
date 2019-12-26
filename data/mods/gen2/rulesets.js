eb6d7d831e46c26b271062a369d89bf0e7a77eb1
Samantha <36967951+SamanthaLola@users.noreply.github.com>
2019-12-26 02:46:40 +0000

    Fix Sunflora Gen 2 move legality (#6223)
    Adding 'Sunflora + Razor Leaf + Synthesis'
    
    Sunflora can only learn Razor Leaf at Level 10, after it evolves. It can only learn Synthesis at Level 31, before it evolves. So it can't have both.

--- data/mods/gen2/rulesets.js
+++ data/mods/gen2/rulesets.js
@@ -17,6 +17,8 @@ let BattleFormats = {
 			'Vaporeon + Tackle + Growl',
 			'Jolteon + Tackle + Growl', 'Jolteon + Focus Energy + Thunder Shock',
 			'Flareon + Tackle + Growl', 'Flareon + Focus Energy + Ember',
+
+			'Sunflora + Razor Leaf + Synthesis',
 		],
 	},
 	standard: {
