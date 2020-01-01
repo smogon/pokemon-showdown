6e68d08e6d0c85f9ed568be027c6a675f1c575e4
Ocean-ey <53575743+Ocean-ey@users.noreply.github.com>
2019-12-31 22:52:31 -0500

    Update !code description (#6248)
    !code no longer requires global voice to use in PM's.

--- server/chat-commands/info.js
+++ server/chat-commands/info.js
@@ -2431,7 +2431,6 @@ const commands = {
 	},
 	codehelp: [
 		`!code [code] - Broadcasts code to a room. Accepts multi-line arguments. Requires: + % @ & # ~`,
-		`In order to use !code in private messages you must be a global voice or higher`,
 		`/code [code] - Shows you code. Accepts multi-line arguments.`,
 	],
 };
