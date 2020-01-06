8c432e6118609addd92db18512b56fa7d035377b
The Immortal <the_immortal123@live.com>
2020-01-07 03:40:08 +0400

    Fix Screen Cleaner messages
    This makes the ability activate first, and also fixes the duplicate screen ending messages.

--- data/abilities.js
+++ data/abilities.js
@@ -3390,19 +3390,19 @@ let BattleAbilities = {
 		onStart(pokemon) {
 			let activated = false;
 			for (const sideCondition of ['reflect', 'lightscreen', 'auroraveil']) {
-				if (pokemon.side.removeSideCondition(sideCondition)) {
+				if (pokemon.side.getSideCondition(sideCondition)) {
 					if (!activated) {
 						this.add('-activate', pokemon, 'ability: Screen Cleaner');
 						activated = true;
 					}
-					this.add('-sideend', pokemon.side, this.dex.getEffect(sideCondition).name);
+					pokemon.side.removeSideCondition(sideCondition);
 				}
-				if (pokemon.side.foe.removeSideCondition(sideCondition)) {
+				if (pokemon.side.foe.getSideCondition(sideCondition)) {
 					if (!activated) {
 						this.add('-activate', pokemon, 'ability: Screen Cleaner');
 						activated = true;
 					}
-					this.add('-sideend', pokemon.side.foe, this.dex.getEffect(sideCondition).name);
+					pokemon.side.foe.removeSideCondition(sideCondition);
 				}
 			}
 		},
