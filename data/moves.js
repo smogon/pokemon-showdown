efe9d547cc056cca49961dff6761e13b9f79b989
urkerab <urkerab@users.noreply.github.com>
2020-01-07 03:12:33 +0000

    Fix interaction between Court Change, Sticky Web, and Defiant or Competitive (#6265)

--- data/moves.js
+++ data/moves.js
@@ -18333,7 +18333,7 @@ let BattleMovedex = {
 				if (!pokemon.isGrounded()) return;
 				if (pokemon.hasItem('heavydutyboots')) return;
 				this.add('-activate', pokemon, 'move: Sticky Web');
-				this.boost({spe: -1}, pokemon, pokemon.side.foe.active[0], this.dex.getActiveMove('stickyweb'));
+				this.boost({spe: -1}, pokemon, this.effectData.source, this.dex.getActiveMove('stickyweb'));
 			},
 		},
 		secondary: null,
