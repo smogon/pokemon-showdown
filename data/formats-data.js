5e1465821dbc14ba99cdd1ff8e4a5636fb0b5b42
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2020-01-09 10:21:50 -0700

    nevermind

--- data/formats-data.js
+++ data/formats-data.js
@@ -1141,9 +1141,6 @@ let BattleFormatsData = {
 		tier: "Unreleased",
 	},
 	slowpokegalar: {
-		eventPokemon: [
-			{"generation": 7, "level": 12, "shiny": false, "isHidden": false, "moves": ["growl", "acid", "yawn", "confusion"]},
-		],
 		unreleasedHidden: true,
 		tier: "OU",
 		doublesTier: "DOU",
