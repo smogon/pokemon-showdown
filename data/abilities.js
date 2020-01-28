37a5008536c87d1fb10169d10931a34c036748e9
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2020-01-27 20:41:36 -0700

    frisk

--- data/abilities.js
+++ data/abilities.js
@@ -1193,7 +1193,9 @@ let BattleAbilities = {
 			for (const target of pokemon.side.foe.active) {
 				if (!target || target.fainted) continue;
 				if (target.item) {
-					this.add('-item', target, target.getItem().name, '[from] ability: Frisk', '[of] ' + pokemon, '[identify]');
+					let itemName = target.getItem().name;
+					if (target.getItem().isTR) itemName = itemName.slice(0, 4);
+					this.add('-item', target, itemName, '[from] ability: Frisk', '[of] ' + pokemon, '[identify]');
 				}
 			}
 		},
