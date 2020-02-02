smogon/pokemon-showdown

commit 11d64af86120b6fcac9960690b6114dc0e54c16a

Update No Retreat mechanics

data/moves.js
@@ -13054,12 +13054,11 @@ let BattleMovedex = {
 		pp: 5,
 		priority: 0,
 		flags: {snatch: 1},
-		volatileStatus: 'noretreat',
-		onTryMove(pokemon, target, move) {
-			if (!pokemon.volatiles['noretreat']) return;
-			this.add('-fail', pokemon, 'move: No Retreat');
-			this.attrLastMove('[still]');
-			return null;
+		onTryHit(pokemon) {
+			if (pokemon.volatiles['noretreat']) return false;
+			if (!pokemon.volatiles['trapped']) {
+				pokemon.addVolatile('noretreat');
+			}
 		},
 		effect: {
 			onStart(pokemon) {

commit 106ca37ef57c4dc621929eae4c702acb57f4c5d4

Fix Last Resort
And the message activation of No Retreat which was broken in 11d64af86.

data/moves.js
@@ -10202,10 +10202,10 @@ let BattleMovedex = {
 		pp: 5,
 		priority: 0,
 		flags: {contact: 1, protect: 1, mirror: 1},
-		onTryHit(target, source) {
-			if (source.moveSlots.length < 2) return false; // Last Resort fails unless the user knows at least 2 moves
+		onTry(pokemon, target) {
+			if (pokemon.moveSlots.length < 2) return false; // Last Resort fails unless the user knows at least 2 moves
 			let hasLastResort = false; // User must actually have Last Resort for it to succeed
-			for (const moveSlot of source.moveSlots) {
+			for (const moveSlot of pokemon.moveSlots) {
 				if (moveSlot.id === 'lastresort') {
 					hasLastResort = true;
 					continue;
@@ -13054,10 +13054,11 @@ let BattleMovedex = {
 		pp: 5,
 		priority: 0,
 		flags: {snatch: 1},
-		onTryHit(pokemon) {
-			if (pokemon.volatiles['noretreat']) return false;
-			if (!pokemon.volatiles['trapped']) {
-				pokemon.addVolatile('noretreat');
+		volatileStatus: 'noretreat',
+		onTryHit(target, source, move) {
+			if (target.volatiles['noretreat']) return false;
+			if (target.volatiles['trapped']) {
+				delete move.volatileStatus;
 			}
 		},
 		effect: {