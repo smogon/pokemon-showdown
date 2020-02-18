5875f1e3ccf4fac5155b37c7651bc64da1fd08e7
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2020-02-18 14:38:19 -0700

    Fix Virtual Console in /learn (#6374)

--- server/chat-plugins/datasearch.js
+++ server/chat-plugins/datasearch.js
@@ -1748,7 +1748,7 @@ function runLearn(target, cmd) {
 	buffer += `${template.name}` + (problems ? ` <span class="message-learn-cannotlearn">can't</span> learn ` : ` <span class="message-learn-canlearn">can</span> learn `) + Chat.toListString(moveNames);
 	if (!problems) {
 		let sourceNames = {
-			E: "", S: "event", D: "dream world", V: "virtual console transfer from gen 1-2", X: "traded-back ", Y: "traded-back event", G: "transferred from Let's Go!",
+			'7V': "virtual console transfer from gen 1-2", '8V': "Pokémon Home transfer from LGPE", E: "", S: "event", D: "dream world", X: "traded-back ", Y: "traded-back event",
 		};
 		let sourcesBefore = setSources.sourcesBefore;
 		if (sources.length || sourcesBefore < gen) buffer += " only when obtained";
@@ -1764,7 +1764,7 @@ function runLearn(target, cmd) {
 				return source;
 			}).sort();
 			for (let source of sources) {
-				buffer += `<li>Gen ${source.charAt(0)} ${sourceNames[source.charAt(1)]}`;
+				buffer += `<li>Gen ${source.charAt(0)} ${sourceNames[source] || sourceNames[source.charAt(1)]}`;
 
 				if (source.charAt(1) === 'E') {
 					let fathers;
