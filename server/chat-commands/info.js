39baf1849086ddfb2432434a46065c6b0ed9779b
Mia <49593536+maxalexandderpi@users.noreply.github.com>
2020-02-17 19:38:33 -0600

    Update info.js

--- server/chat-commands/info.js
+++ server/chat-commands/info.js
@@ -2345,6 +2345,7 @@ const commands = {
 		if (!target.includes(',')) return this.parse('/help shuffle');
 		if (!this.runBroadcast(true)) return false;
 		let args = target.split(',');
+		if (args.length < 3) return this.errorReply('Give at least 3 arguments to shuffle.');
 		const results = Dex.shuffle(args);
 		return this.sendReplyBox(Chat.html`<em>Shuffled:</em><br> ${results.join(', ')}`);
 	},
