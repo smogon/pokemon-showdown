bf2665ca1d1f36df4850c077b348886c1f80eeb5
The Immortal <the_immortal123@live.com>
2020-02-21 05:27:35 +0400

    Add tests for Lum Berry

---
+++ test/sim/items/lumberry.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const assert = require('./../../assert');
+const common = require('./../../common');
+
+let battle;
+
+describe('Lum Berry', function () {
+	afterEach(function () {
+		battle.destroy();
+	});
+
+	it('should heal a major status condition', function () {
+		battle = common.createBattle();
+		battle.setPlayer('p1', {team: [{species: 'Rapidash', moves: ['inferno']}]});
+		battle.setPlayer('p2', {team: [{species: 'Machamp', ability: 'noguard', item: 'lumberry', moves: ['sleeptalk']}]});
+		battle.makeChoices('move inferno', 'move sleeptalk');
+		assert.equal(battle.p2.active[0].status, '');
+	});
+
+	it.skip('should be eaten immediately when the holder gains a status condition', function () {
+		battle = common.createBattle();
+		battle.setPlayer('p1', {team: [{species: 'Charizard', item: 'lumberry', moves: ['outrage']}]});
+		battle.setPlayer('p2', {team: [{species: 'Toxapex', moves: ['recover', 'banefulbunker']}]});
+		const attacker = battle.p1.active[0];
+		battle.makeChoices('move outrage', 'move recover');
+		attacker.volatiles['lockedmove'].duration = 2;
+		battle.makeChoices('move outrage', 'move recover');
+		battle.makeChoices('move outrage', 'move banefulbunker');
+		assert.equal(attacker.status, '');
+		assert.ok(attacker.volatiles['confusion']);
+	});
+});
