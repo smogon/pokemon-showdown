45f0f668ac349639caf0b1a547002869a2f0aa28
whales <jumbowhales@gmail.com>
2020-02-25 18:24:10 +1030

    Fix bugs with renaming rooms (#6392)

--- server/roomlogs.ts
+++ server/roomlogs.ts
@@ -218,11 +218,11 @@ export class Roomlog {
 		if (!this.modlogStream) return;
 		this.modlogStream.write('[' + (new Date().toJSON()) + '] ' + message + '\n');
 	}
-	async rename(newID: RoomID) {
+	async rename(newID: RoomID): Promise<true> {
 		const modlogPath = `logs/modlog`;
 		const roomlogPath = `logs/chat`;
-		const modlogStreamExisted = !!this.modlogStream;
-		const roomlogStreamExisted = !!this.roomlogStream;
+		const modlogStreamExisted = this.modlogStream !== null;
+		const roomlogStreamExisted = this.roomlogStream !== null;
 		await this.destroy();
 		await Promise.all([
 			FS(modlogPath + `/modlog_${this.roomid}.txt`).exists(),
@@ -242,9 +242,12 @@ export class Roomlog {
 		this.roomid = newID;
 		Roomlogs.roomlogs.set(newID, this);
 		if (modlogStreamExisted) {
+			// set modlogStream to undefined (uninitialized) instead of null (disabled)
+			this.modlogStream = undefined;
 			this.setupModlogStream();
 		}
 		if (roomlogStreamExisted) {
+			this.roomlogStream = undefined;
 			await this.setupRoomlogStream(true);
 		}
 		return true;
