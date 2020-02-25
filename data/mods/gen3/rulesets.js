e4b9589d2e4f55164eaab753349195ccae87a5d5
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2020-02-24 22:19:17 -0700

    Correct validation for UU NFE Clause/Little Cup (#6395)

--- data/mods/gen3/rulesets.js
+++ data/mods/gen3/rulesets.js
@@ -8,28 +8,6 @@ let BattleFormats = {
 		desc: "The standard ruleset for all offical Smogon singles tiers (Ubers, OU, etc.)",
 		ruleset: ['Obtainable', 'Sleep Clause Mod', 'Switch Priority Clause Mod', 'Species Clause', 'Nickname Clause', 'OHKO Clause', 'Moody Clause', 'Evasion Moves Clause', 'Endless Battle Clause', 'HP Percentage Mod', 'Cancel Mod'],
 	},
-	uunfeclause: {
-		effectType: 'ValidatorRule',
-		name: 'UU NFE Clause',
-		desc: "Bans all NFE Pokemon, except Scyther.",
-		banlist: [
-			'Abra', 'Anorith', 'Aron', 'Azurill', 'Bagon', 'Baltoy', 'Barboach', 'Bayleef', 'Beldum', 'Bellsprout', 'Bulbasaur',
-			'Cacnea', 'Carvanha', 'Cascoon', 'Caterpie', 'Charmander', 'Charmeleon', 'Chikorita', 'Chinchou', 'Clamperl', 'Clefairy',
-			'Cleffa', 'Combusken', 'Corphish', 'Croconaw', 'Cubone', 'Cyndaquil', 'Diglett', 'Doduo', 'Dragonair', 'Dratini',
-			'Drowzee', 'Duskull', 'Eevee', 'Ekans', 'Electrike', 'Elekid', 'Exeggcute', 'Feebas', 'Flaaffy', 'Gastly', 'Geodude',
-			'Gloom', 'Goldeen', 'Grimer', 'Grovyle', 'Growlithe', 'Gulpin', 'Hoothoot', 'Hoppip', 'Horsea', 'Houndour', 'Igglybuff',
-			'Ivysaur', 'Jigglypuff', 'Kabuto', 'Kakuna', 'Kirlia', 'Koffing', 'Krabby', 'Lairon', 'Larvitar', 'Ledyba', 'Lileep',
-			'Lombre', 'Lotad', 'Loudred', 'Machoke', 'Machop', 'Magby', 'Magikarp', 'Magnemite', 'Makuhita', 'Mankey', 'Mareep', 'Marill',
-			'Marshtomp', 'Meditite', 'Meowth', 'Metang', 'Metapod', 'Mudkip', 'Natu', 'Nidoran-F', 'Nidoran-M', 'Nidorina', 'Nidorino',
-			'Nincada', 'Numel', 'Nuzleaf', 'Oddish', 'Omanyte', 'Onix', 'Paras', 'Phanpy', 'Pichu', 'Pidgeotto', 'Pidgey', 'Pikachu',
-			'Pineco', 'Poliwag', 'Poliwhirl', 'Ponyta', 'Poochyena', 'Porygon', 'Psyduck', 'Pupitar', 'Quilava', 'Ralts', 'Rattata',
-			'Remoraid', 'Rhyhorn', 'Sandshrew', 'Seadra', 'Sealeo', 'Seedot', 'Seel', 'Sentret', 'Shelgon', 'Shellder', 'Shroomish',
-			'Shuppet', 'Silcoon', 'Skiploom', 'Skitty', 'Slakoth', 'Slowpoke', 'Slugma', 'Smoochum', 'Snorunt', 'Snubbull', 'Spearow',
-			'Spheal', 'Spinarak', 'Spoink', 'Squirtle', 'Staryu', 'Sunkern', 'Surskit', 'Swablu', 'Swinub', 'Taillow', 'Teddiursa',
-			'Tentacool', 'Togepi', 'Torchic', 'Totodile', 'Trapinch', 'Treecko', 'Tyrogue', 'Venonat', 'Vibrava', 'Vigoroth', 'Voltorb',
-			'Vulpix', 'Wailmer', 'Wartortle', 'Weedle', 'Weepinbell', 'Whismur', 'Wingull', 'Wooper', 'Wurmple', 'Zigzagoon', 'Zubat',
-		],
-	},
 };
 
 exports.BattleFormats = BattleFormats;
