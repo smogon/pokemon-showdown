5e320c337cdbde245094ada169be07b6a1025b78
Leonard Craft III <leonardcraft64@gmail.com>
2020-03-17 22:38:19 -0500

    Add additional Eject Pack tests (#6453)

--- test/sim/items/ejectpack.js
+++ test/sim/items/ejectpack.js
@@ -35,4 +35,49 @@ describe('Eject Pack', function () {
 		battle.makeChoices();
 		assert.equal(battle.p2.requestState, 'switch');
 	});
+
+	it.skip("should not trigger until after all entrance abilities have resolved during simultaneous switches", function () {
+		battle = common.createBattle({gameType: 'doubles'}, [[
+			{species: 'Hydreigon', ability: 'intimidate', moves: ['sleeptalk']},
+			{species: 'Wynaut', moves: ['sleeptalk']},
+		], [
+			{species: 'Morelull', ability: 'drought', item: 'ejectpack', moves: ['sleeptalk']},
+			{species: 'Mew', level: 1, ability: 'electricsurge', moves: ['sleeptalk']},
+			{species: 'Wynaut', moves: ['sleeptalk']},
+			{species: 'Wynaut', moves: ['sleeptalk']},
+		]]);
+		battle.makeChoices();
+		assert(battle.field.isWeather('sunnyday'));
+		assert(battle.field.isTerrain('electricterrain'));
+		assert.equal(battle.p2.requestState, 'switch');
+	});
+
+	it.skip("should only trigger the fastest Eject Pack when multiple targets with Eject Pack have stats lowered", function () {
+		battle = common.createBattle({gameType: 'doubles'}, [[
+			{species: 'Hydreigon', moves: ['leer']},
+			{species: 'Wynaut', moves: ['sleeptalk']},
+		], [
+			{species: 'Morelull', item: 'ejectpack', moves: ['sleeptalk']},
+			{species: 'Mew', item: 'ejectpack', moves: ['sleeptalk']},
+			{species: 'Wynaut', moves: ['sleeptalk']},
+			{species: 'Wynaut', moves: ['sleeptalk']},
+		]]);
+		battle.makeChoices('move leer, move sleeptalk', 'move sleeptalk, move sleeptalk');
+		battle.makeChoices();
+		assert.equal(battle.p2.active[0].species, "Morelull");
+		assert.equal(battle.p2.active[1].species, "Wynaut");
+	});
+
+	it("should cause Pokemon to switch out during the semi-invulernable state", function () {
+		battle = common.createBattle([[
+			{species: 'Charmeleon', item: 'ejectpack', moves: ['phantomforce']},
+			{species: 'Wynaut', moves: ['sleeptalk']},
+		], [
+			{species: 'Wynaut', moves: ['sleeptalk']},
+			{species: 'Arcanine', ability: 'intimidate', moves: ['sleeptalk']},
+		]]);
+		battle.makeChoices('move phantomforce', 'move sleeptalk');
+		battle.makeChoices('move phantomforce', 'switch 2');
+		assert.equal(battle.p1.requestState, 'switch');
+	});
 });
