80cfacc2b0ac29f711e6ce5ffee70fad885d9b83
fart <24866009+tmagicturtle@users.noreply.github.com>
2020-03-22 18:53:40 -0400

    Support /hidetext [line count] (#6471)

--- server/rooms.ts
+++ server/rooms.ts
@@ -1159,10 +1159,10 @@ export class BasicChatRoom extends BasicRoom {
 		this.log.modlog(message);
 		return this;
 	}
-	hideText(userids: ID[]) {
-		const cleared = this.log.clearText(userids);
+	hideText(userids: ID[], lineCount = 0) {
+		const cleared = this.log.clearText(userids, lineCount);
 		for (const userid of cleared) {
-			this.send(`|unlink|hide|${userid}`);
+			this.send(`|unlink|hide|${userid}|${lineCount}`);
 		}
 		this.update();
 	}
