1628258371334315f1a29238dbb00889df2bfc0d
Mia <49593536+mia-pi-git@users.noreply.github.com>
2020-03-24 01:50:35 -0500

    Typescript chat-commands/core (#6484)
    Also fix TS line numbers in GitHub Actions

--- server/room-game.ts
+++ server/room-game.ts
@@ -91,6 +91,11 @@ export class RoomGame {
 	playerCount: number;
 	playerCap: number;
 	ended: boolean;
+	/**
+	 * We should really resolve this collision at _some_ point, but it will have
+	 * to be later. The /timer command is written to be resilient to this.
+	 */
+	timer?: {timerRequesters?: Set<ID>, start: (force?: User) => void, stop: (force?: User) => void} | NodeJS.Timer | null;
 	constructor(room: ChatRoom | GameRoom) {
 		this.roomid = room.roomid;
 		this.room = room;
@@ -198,29 +203,29 @@ export class RoomGame {
 	 * extremely unlikely to keep playing after this function is
 	 * called.
 	 */
-	forfeit?(user: User) {}
+	forfeit?(user: User): void;
 
 	/**
 	 * Called when a user uses /choose [text]
 	 * If you have buttons, you are recommended to use this interface
 	 * instead of making your own commands.
 	 */
-	choose?(user: User, text: string) {}
+	choose?(user: User, text: string): void;
 
 	/**
 	 * Called when a user uses /undo [text]
 	 */
-	undo?(user: User, text: string) {}
+	undo?(user: User, text: string): void;
 
 	/**
 	 * Called when a user uses /joingame [text]
 	 */
-	joinGame?(user: User, text?: string) {}
+	joinGame?(user: User, text?: string): void;
 
 	/**
 	 * Called when a user uses /leavegame [text]
 	 */
-	leaveGame?(user: User, text?: string) {}
+	leaveGame?(user: User, text?: string): void;
 
 	// Events:
 
