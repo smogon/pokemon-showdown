0e234539b9e2913148c0ab9ab09683731a30f7a1
The Immortal <the_immortal123@live.com>
2020-03-26 22:12:35 +0400

    Fix MissingNo.'s learnset

--- data/learnsets.js
+++ data/learnsets.js
@@ -2,35 +2,6 @@
 
 /**@type {{[k: string]: LearnsetData}} */
 let BattleLearnsets = {
-	missingno: {
-		learnset: {
-			blizzard: ["5L1"],
-			bubblebeam: ["5L1"],
-			cut: ["5L1"],
-			doubleedge: ["5L1"],
-			earthquake: ["5L1"],
-			fissure: ["5L1"],
-			fly: ["5L1"],
-			icebeam: ["5L1"],
-			megakick: ["5L1"],
-			megapunch: ["5L1"],
-			psychic: ["5L1"],
-			rage: ["5L1"],
-			razorwind: ["5L1"],
-			rest: ["5L1"],
-			seismictoss: ["5L1"],
-			skyattack: ["5L1"],
-			submission: ["5L1"],
-			swordsdance: ["5L1"],
-			takedown: ["5L1"],
-			teleport: ["5L1"],
-			thunder: ["5L1"],
-			thunderwave: ["5L1"],
-			toxic: ["5L1"],
-			triattack: ["5L1"],
-			watergun: ["5L1"],
-		},
-	},
 	bulbasaur: {
 		learnset: {
 			amnesia: ["8M", "7E", "6E", "5E", "4E"],
