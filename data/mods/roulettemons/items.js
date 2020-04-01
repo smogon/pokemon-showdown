119d6c5648f81fbd30f568050ac6d601c55aba79
charizard8888 <omkumr082001@outlook.com>
2020-04-01 16:31:08 +0530

    Update items.js

--- data/mods/roulettemons/items.js
+++ data/mods/roulettemons/items.js
@@ -10,11 +10,12 @@ let BattleItems = {
 		megaEvolves: "Chillyte",
 		itemUser: ["Chillyte"],
 		onTakeItem(item, source) {
-			if (item.megaEvolves === source.baseTemplate.baseSpecies) return false;
+			if (item.megaEvolves === source.baseSpecies.baseSpecies) return false;
 			return true;
 		},
 		num: 673,
 		gen: 8,
+		isNonstandard: "Past",
 		desc: "If held by a Chillyte, this item allows it to Mega Evolve in battle.",
 	},
 };
