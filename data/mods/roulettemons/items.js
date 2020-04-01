bd4fab013a9e24a4bc46c1edad93c6fb2daac78d
charizard8888 <omkumr082001@outlook.com>
2020-04-01 15:45:47 +0530

    Create items.js

---
+++ data/mods/roulettemons/items.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**@type {{[k: string]: ModdedItemData}} */
+let BattleItems = {
+	"chillytite": {
+		id: "chillytite",
+		name: "Chillytite",
+		spritenum: 594,
+		megaStone: "Chillyte-Mega",
+		megaEvolves: "Chillyte",
+		itemUser: ["Chillyte"],
+		onTakeItem(item, source) {
+			if (item.megaEvolves === source.baseTemplate.baseSpecies) return false;
+			return true;
+		},
+		num: 673,
+		gen: 8,
+		desc: "If held by a Chillyte, this item allows it to Mega Evolve in battle.",
+	},
+};
+
+exports.BattleItems = BattleItems;
