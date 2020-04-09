db0b0d0acfe1dee9abc52566a4e84638d79f0ba8
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2020-04-08 21:08:08 -0600

    Shared Power: Fix banlist and ban crash-causing ability (#6545)
    Mirror Armor is a temporary ban because it is causing stack overflow errors; Regenerator and Emergency Exit were intended to be '++' to begin with

--- config/formats.js
+++ config/formats.js
@@ -463,10 +463,10 @@ let Formats = [
 			'Darmanitan-Galar', 'Eternatus', 'Kyurem-Black', 'Kyurem-White', 'Lunala', 'Marshadow', 'Melmetal', 'Mewtwo',
 			'Necrozma-Dawn-Wings', 'Necrozma-Dusk-Mane', 'Reshiram', 'Shedinja', 'Solgaleo', 'Zacian', 'Zamazenta', 'Zekrom',
 			'Arena Trap', 'Drizzle ++ Swift Swim', 'Drought ++ Chlorophyll', 'Electric Surge ++ Surge Surfer', 'Harvest',
-			'Huge Power', 'Imposter', 'Innards Out', 'Magic Bounce', 'Magic Guard', 'Mold Breaker', 'Moody', 'Neutralizing Gas',
-			'Regenerator + Emergency Exit', 'Regenerator ++ Wimp Out', 'Sand Stream ++ Sand Rush', 'Sand Veil', 'Shadow Tag',
-			'Simple', 'Snow Cloak', 'Snow Warning ++ Slush Rush', 'Speed Boost', 'Steelworker ++ Steely Spirit', 'Tinted Lens',
-			'Trace', 'Unaware', 'Water Bubble', 'Baton Pass',
+			'Huge Power', 'Imposter', 'Innards Out', 'Magic Bounce', 'Magic Guard', 'Mirror Armor', 'Mold Breaker', 'Moody',
+			'Neutralizing Gas', 'Regenerator ++ Emergency Exit', 'Regenerator ++ Wimp Out', 'Sand Stream ++ Sand Rush',
+			'Sand Veil', 'Shadow Tag', 'Simple', 'Snow Cloak', 'Snow Warning ++ Slush Rush', 'Speed Boost',
+			'Steelworker ++ Steely Spirit', 'Tinted Lens', 'Trace', 'Unaware', 'Water Bubble', 'Baton Pass',
 		],
 		/** @param {Pokemon} pokemon */
 		// @ts-ignore
