f860d0640d12a6242200b1aee9b1e2ad70066c95
Leonard Craft <leonardcraft64@gmail.com>
2020-04-12 21:48:36 -0500

    Improve queue-like test

--- test/sim/moves/gmaxvolcalith.js
+++ test/sim/moves/gmaxvolcalith.js
@@ -42,32 +42,26 @@ describe('G-Max Volcalith', function () {
 	});
 
 	it.skip('should deal damage alongside Sea of Fire or G-Max Wildfire in the order those field effects were set', function () {
-		battle = common.createBattle({gameType: 'doubles'});
-		battle.setPlayer('p1', {team: [
+		battle = common.createBattle({gameType: 'doubles'}, [[
 			{species: 'Coalossal-Gmax', item: 'Eject Button', moves: ['rockthrow', 'sleeptalk']},
-			{species: 'Charmander', moves: ['firepledge', 'sleeptalk']},
-			{species: 'Wynaut', level: 72, moves: ['grasspledge', 'nightshade', 'sleeptalk']},
-		]});
-		battle.setPlayer('p2', {team: [
-			{species: 'Roggenrola', ability: 'Receiver', item: 'Focus Sash', level: 1, moves: ['sleeptalk', 'tackle']},
-			{species: 'Boldore', ability: 'Receiver', item: 'Sitrus Berry', evs: {hp: 28}, moves: ['sleeptalk']},
-			{species: 'Torracat', ability: 'Cheek Pouch', item: 'Focus Sash', level: 1, moves: ['sleeptalk']},
-		]});
+			{species: 'Wynaut', moves: ['sleeptalk', 'grasspledge']},
+			{species: 'Wynaut', moves: ['sleeptalk', 'firepledge']},
+		], [
+			{species: 'Blissey', ability: "noguard", moves: ['superfang', 'softboiled']},
+			{species: 'Blissey', ability: "noguard", moves: ['superfang', 'softboiled']},
+			{species: 'Golisopod', moves: ['sleeptalk']},
+		]]);
+
 		// Set up Volcalith first, then Sea of Fire
-		battle.makeChoices('move rockthrow 1 dynamax, move sleeptalk', 'move tackle 1, move sleeptalk');
+		battle.makeChoices('move rockthrow 1 dynamax, move grasspledge -1', 'move softboiled, move softboiled');
 		battle.makeChoices('switch 3');
-		battle.makeChoices('move grasspledge 1, move firepledge 1', 'move sleeptalk, move sleeptalk');
-		battle.makeChoices('', 'switch 3');
+		battle.makeChoices('move firepledge 1, move grasspledge 1', 'move softboiled, move softboiled');
 
-		// Bring Cheek Pouch down to Sash so it will faint and set Receiver's HP to exactly 5/8 of max
-		battle.makeChoices('move nightshade 2, move firepledge 1', 'move sleeptalk, move sleeptalk');
-		let expectedHP = battle.p2.active[1].maxhp;
-
-		/* TODO: this passes when it shouldn't because Sitrus Berry isn't triggering between Sea of Fire and G-Max Volcalith like it should;
-		 * Sitrus Berry should be able to activate in between those damaging effects. */
-
-		// If Sea of Fire triggered first, Sitrus Berry would activate without Cheek Pouch, missing the timing for extra health
-		// Failure HP should be 216
-		assert.strictEqual(battle.p2.active[1].hp, expectedHP);
+		// Switch into Golisopod; Emergency Exit should trigger and only take Volcalith damage then switch, avoiding Sea of Fire
+		// Note that Emergency Exit is also bugged here; it's actually not switching out at all lol, so it takes both Volcalith and Sea of Fire
+		battle.makeChoices('move sleeptalk, move sleeptalk', 'switch 3, move superfang -1');
+		const maxHP = battle.p2.active[0].maxhp;
+		const expectedHP = maxHP - Math.floor(maxHP / 2) - Math.floor(maxHP / 6);
+		assert.strictEqual(battle.p2.active[0].hp, expectedHP);
 	});
 });
