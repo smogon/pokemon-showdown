3b641d0b8c079e76c62dfd4c9364694ba68941f6
Petuuuhhh <peterswanton191@bellsouth.net>
2020-04-14 16:45:52 -0400

    fix_tiers2

--- data/mods/roulettemons/formats-data.js
+++ data/mods/roulettemons/formats-data.js
@@ -3,190 +3,237 @@
 /**@type {{[k: string]: ModdedSpeciesFormatsData}} */
 let BattleFormatsData = {
 	koatric: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	aquazelle: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	salamalix: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	brawnkey: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	stuneleon: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	chillyte: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	eartharoo: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	crazefly: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	electritar: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	aquatopus: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	scorpita: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	baloon: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	kinesel: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	glacida: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	pidgeotine: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	gorilax: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	albatrygon: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	chillvark: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	komodith: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	giranium: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	flamyle: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	voltecta: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	ostria: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	ninjoth: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	herbigator: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	anteros: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	gladiaster: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	hyperoach: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	barracoth: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	toados: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	voltarak: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	mosqung: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	flamepion: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	hyenix: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	rhinolite: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	bellena: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	falcola: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	beanium: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	lemotic: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	biceon: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	skeleray: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	specyte: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	ramron: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	balar: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	panthee: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	blastora: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
 	dropacle: {
+		isNonstandard: "PM",
 		tier: "PM",
 		doublesTier: "PM",
 	},
