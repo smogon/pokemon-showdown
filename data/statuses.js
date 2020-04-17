3068f48418e8e24cbc4f6ac4895d1464a8b46d0b
Guangcong Luo <guangcongluo@gmail.com>
2020-04-16 19:27:34 -0700

    Push Dynamax ending to as late as possible

--- data/statuses.js
+++ data/statuses.js
@@ -762,7 +762,7 @@ let BattleStatuses = {
 			this.add('-block', pokemon, 'Dynamax');
 			return null;
 		},
-		onResidualOrder: 30,
+		onResidualPriority: -100,
 		onEnd(pokemon) {
 			this.add('-end', pokemon, 'Dynamax');
 			if (pokemon.canGigantamax) this.add('-formechange', pokemon, pokemon.species.name);
