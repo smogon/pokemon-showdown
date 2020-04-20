47974e0f06a950ff246bde24a441675b08a08811
Luc Moussiegt <47942311+yghdj@users.noreply.github.com>
2020-04-20 18:14:37 +0200

    Fix Sky Drop Wonder Guard interaction (#6593)

--- data/abilities.js
+++ data/abilities.js
@@ -4659,6 +4659,7 @@ let BattleAbilities = {
 		shortDesc: "This Pokemon can only be damaged by supereffective moves and indirect damage.",
 		onTryHit(target, source, move) {
 			if (target === source || move.category === 'Status' || move.type === '???' || move.id === 'struggle') return;
+			if (move.id === 'skydrop' && !source.volatiles['skydrop']) return;
 			this.debug('Wonder Guard immunity: ' + move.id);
 			if (target.runEffectiveness(move) <= 0) {
 				if (move.smartTarget) {
