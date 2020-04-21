c0afda7fb556bd54db6bdf5c79a3592a845fd1a8
Guangcong Luo <guangcongluo@gmail.com>
2020-04-21 07:16:04 -0700

    Fix port config not being passed to child process
    Fixes #6597

--- server/sockets.ts
+++ server/sockets.ts
@@ -90,6 +90,7 @@ export const Sockets = new class {
 			workerCount = (Config.workers !== undefined ? Config.workers : 1);
 		}
 
+		PM.env = {PSPORT: Config.port, PSBINDADDR: Config.bindaddress || '0.0.0.0', PSNOSSL: Config.ssl ? 0 : 1};
 		PM.subscribeSpawn(worker => void this.onSpawn(worker));
 		PM.subscribeUnspawn(this.onUnspawn);
 
