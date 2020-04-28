d1dcb4e6f28671e830456e1266162fbee4518b3b
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2020-04-28 00:22:30 -0600

    Add more tests

--- test/sim/pokedex.js
+++ test/sim/pokedex.js
@@ -9,25 +9,30 @@ describe('Pokedex', function () {
 			const entry = Pokedex[pokemonid];
 			assert.equal(toID(entry.name), pokemonid, `Mismatched Pokemon key "${pokemonid}" of ${entry.name}`);
 			if (entry.prevo) {
-				assert.equal(entry.prevo, (Pokedex[toID(entry.prevo)] || {}).name, `Mismatched prevo "${entry.prevo}" of ${entry.name}`);
+				const prevoEntry = Pokedex[toID(entry.prevo)] || {};
+				assert.equal(entry.prevo, prevoEntry.name, `Misspelled/nonexistent prevo "${entry.prevo}" of ${entry.name}`);
+				assert.notEqual(entry.num, prevoEntry.num, `Prevo ${entry.prevo} of ${entry.name} should have a different dex number`);
+				assert(prevoEntry.evos.includes(entry.name), `Prevo ${entry.prevo} should have ${entry.name} listed as an evo`);
 			}
 			if (entry.evos) {
 				for (const evo of entry.evos) {
-					assert.equal(evo, (Pokedex[toID(evo)] || {}).name, `Mismatched evo "${evo}" of ${entry.name}`);
+					const evoEntry = Pokedex[toID(evo)] || {};
+					assert.equal(evo, evoEntry.name, `Misspelled/nonexistent evo "${evo}" of ${entry.name}`);
+					assert.notEqual(entry.num, evoEntry.num, `Evo ${evo} of ${entry.name} should have a different dex number`);
+					assert.equal(evoEntry.prevo, entry.name, `Evo ${evo} should have ${entry.name} listed as a prevo`);
 				}
 			}
 			if (entry.otherFormes) {
 				for (const forme of entry.otherFormes) {
-					assert.equal(forme, (Pokedex[toID(forme)] || {}).name, `Mismatched forme "${forme}" of ${entry.name}`);
+					const formeEntry = Pokedex[toID(forme)] || {};
+					assert.equal(forme, formeEntry.name, `Misspelled/nonexistent forme "${forme}" of ${entry.name}`);
+					assert.equal(entry.num, formeEntry.num, `Forme ${formeEntry.name} of ${entry.name} should have the same dex number`);
 				}
 			}
 			if (entry.battleOnly) {
-				if (Array.isArray(entry.battleOnly)) {
-					for (const battleForme of entry.battleOnly) {
-						assert.equal(battleForme, (Pokedex[toID(battleForme)] || {}).name, `Mismatched battle-only forme "${battleForme}" of ${entry.name}`);
-					}
-				} else {
-					assert.equal(entry.battleOnly, (Pokedex[toID(entry.battleOnly)] || {}).name, `Mismatched battle-only forme "${entry.battleOnly}" of ${entry.name}`);
+				const battleOnly = Array.isArray(entry.battleOnly) ? entry.battleOnly : [entry.battleOnly];
+				for (const battleForme of battleOnly) {
+					assert.equal(battleForme, (Pokedex[toID(battleForme)] || {}).name, `Misspelled/nonexistent battle-only forme "${battleForme}" of ${entry.name}`);
 				}
 			}
 			if (entry.changesFrom) {
