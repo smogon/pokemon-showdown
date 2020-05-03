560bc59df971d61fd1263c114d7f128744fa7f13
Leonard Craft <leonardcraft64@gmail.com>
2020-05-03 15:19:32 -0500

    Adjust tests for urkerab's implementation

--- test/sim/moves/gmaxchistrike.js
+++ test/sim/moves/gmaxchistrike.js
@@ -10,19 +10,31 @@ describe('G-Max Chi Strike', function () {
 		battle.destroy();
 	});
 
-	it.skip('should provide a crit boost independent of Focus Energy', function () {
+	it('should boost the user and its ally\'s critical hit rate by 1 stage', function () {
 		// hardcoded RNG seed that doesn't roll a crit at +2 crit rate
-		battle = common.createBattle({gameType: 'doubles', seed: [1, 2, 3, 4]});
-		battle.setPlayer('p1', {team: [
+		battle = common.createBattle({gameType: 'doubles', seed: [1, 2, 3, 4]}, [[
 			{species: 'Machamp-Gmax', moves: ['sleeptalk', 'rocksmash']},
-			{species: 'Wynaut', moves: ['focusenergy', 'tackle']},
-		]});
-		battle.setPlayer('p2', {team: [
+			{species: 'Wynaut', ability: "Super Luck", item: "Scope Lens", moves: ['tackle']},
+		], [
 			{species: 'Wynaut', ability: "Shell Armor", moves: ['sleeptalk']},
 			{species: 'Wynaut', moves: ['sleeptalk']},
-		]});
+		]]);
 
-		battle.resetRNG();
+		// Boosts Wynaut to +3, so this crit is now guaranteed
+		battle.makeChoices('move rocksmash 1 dynamax, move tackle 2', 'auto');
+		assert(battle.log.some(line => line.startsWith('|-crit')));
+	});
+
+	it('should provide a crit boost independent of Focus Energy', function () {
+		// hardcoded RNG seed that doesn't roll a crit at +2 crit rate
+		battle = common.createBattle({gameType: 'doubles', seed: [1, 2, 3, 4]}, [[
+			{species: 'Machamp-Gmax', moves: ['sleeptalk', 'rocksmash']},
+			{species: 'Wynaut', moves: ['focusenergy', 'tackle']},
+		], [
+			{species: 'Wynaut', ability: "Shell Armor", moves: ['sleeptalk']},
+			{species: 'Wynaut', moves: ['sleeptalk']},
+		]]);
+
 		battle.makeChoices();
 		battle.makeChoices('move rocksmash 1 dynamax, move tackle 2', 'auto');
 
@@ -30,33 +42,15 @@ describe('G-Max Chi Strike', function () {
 		assert(battle.log.some(line => line.startsWith('|-crit')));
 	});
 
-	it('should boost the user and its ally\'s critical hit rate by 1 stage', function () {
-		battle = common.createBattle({gameType: 'doubles'});
-		battle.setPlayer('p1', {team: [
-			{species: 'Machamp-Gmax', moves: ['rocksmash']},
-			{species: 'Wynaut', item: 'Scope Lens', ability: "Super Luck", moves: ['tackle']},
-		]});
-		battle.setPlayer('p2', {team: [
-			{species: 'Wynaut', ability: "Shell Armor", moves: ['sleeptalk']},
-			{species: 'Wynaut', moves: ['sleeptalk']},
-		]});
-
-		// Boosts Wynaut to +3, so this crit is now guaranteed
-		battle.makeChoices('move rocksmash 1 dynamax, move tackle 2', 'auto');
-		assert(battle.log.some(line => line.startsWith('|-crit')));
-	});
-
-	it.skip('should be copied by Psych Up', function () {
-		battle = common.createBattle({gameType: 'doubles'});
-		battle.setPlayer('p1', {team: [
+	it('should be copied by Psych Up', function () {
+		battle = common.createBattle({gameType: 'doubles'}, [[
 			{species: 'Machamp-Gmax', moves: ['rocksmash', 'sleeptalk']},
 			{species: 'Wynaut', moves: ['sleeptalk']},
 			{species: 'Magikarp', moves: ['psychup', 'tackle']},
-		]});
-		battle.setPlayer('p2', {team: [
+		], [
 			{species: 'Wynaut', ability: "Shell Armor", moves: ['sleeptalk']},
 			{species: 'Wynaut', moves: ['sleeptalk']},
-		]});
+		]]);
 
 		battle.makeChoices('move rocksmash 1 dynamax, move sleeptalk', 'auto');
 		battle.makeChoices('move rocksmash 1, move sleeptalk', 'auto');
@@ -70,16 +64,14 @@ describe('G-Max Chi Strike', function () {
 	});
 
 	it('should not be passed by Baton Pass', function () {
-		battle = common.createBattle({gameType: 'doubles'});
-		battle.setPlayer('p1', {team: [
+		battle = common.createBattle({gameType: 'doubles'}, [[
 			{species: 'Machamp-Gmax', moves: ['rocksmash', 'batonpass']},
 			{species: 'Wynaut', moves: ['sleeptalk']},
 			{species: 'Magikarp', moves: ['tackle']},
-		]});
-		battle.setPlayer('p2', {team: [
+		], [
 			{species: 'Wynaut', ability: "Shell Armor", moves: ['sleeptalk']},
 			{species: 'Wynaut', moves: ['sleeptalk']},
-		]});
+		]]);
 
 		battle.makeChoices('move rocksmash 1 dynamax, move sleeptalk', 'auto');
 		battle.makeChoices('move rocksmash 1, move sleeptalk', 'auto');
@@ -88,7 +80,7 @@ describe('G-Max Chi Strike', function () {
 		battle.makeChoices('switch 3');
 		battle.makeChoices('move tackle 2, move sleeptalk', 'auto');
 
-		// Magikarp is only +0 so it probably will not crit
+		// Magikarp is only +0 for crit rate, so it probably will not crit
 		assert(battle.log.every(line => !line.startsWith('|-crit')));
 	});
 });
