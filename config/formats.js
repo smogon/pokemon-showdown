a4d3b1eb386d8c86d8d369be34f7f14c8c98ea71
The Immortal <the_immortal123@live.com>
2020-05-05 19:03:48 +0400

    Flipped: Ban Shell Smash

--- config/formats.js
+++ config/formats.js
@@ -464,7 +464,7 @@ const Formats = [
 
 		mod: 'gen8',
 		ruleset: ['[Gen 8] OU'],
-		banlist: ['Psychic Surge', 'Psychic Terrain'],
+		banlist: ['Psychic Surge', 'Psychic Terrain', 'Shell Smash'],
 		unbanlist: ['Darmanitan-Galar'],
 		onModifySpecies(species, target, source, effect) {
 			const newSpecies = this.dex.deepClone(species);
