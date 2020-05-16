4932ed76dc286259b72d3bcfb64fade0df654aea
ZestOfLife <jamescook371@gmail.com>
2020-05-16 10:43:01 +0700

    add something

--- server/chat-plugins/mafia.ts
+++ server/chat-plugins/mafia.ts
@@ -1773,9 +1773,9 @@ export const pages: PageTable = {
 					buf += `<p>${game.theme.desc}</p>`;
 				}
 				if (game.noReveal) {
-					buf += `<p><span style="font-weight:bold; color:${game.closedSetup ? '#ffff00' : ''}">Original Rolelist</span>: ${game.originalRoleString}</p>`;
+					buf += `<p><span style="font-weight:bold; color:${game.closedSetup ? '#ffff00' : 'initial'}">Original Rolelist</span>: ${game.originalRoleString}</p>`;
 				} else {
-					buf += `<p><span style="font-weight:bold; color:${game.closedSetup ? '#ffff00' : ''}">Rolelist</span>: ${game.roleString}</p>`;
+					buf += `<p><span style="font-weight:bold; color:${game.closedSetup ? '#ffff00' : 'initial'}">Rolelist</span>: ${game.roleString}</p>`;
 				}
 			}
 		}
