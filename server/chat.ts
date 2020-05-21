fd8b0eb51b14a83aa4fdd8af01e4c4b6f80e2c7d
PartMan <47669599+PartMan7@users.noreply.github.com>
2020-05-22 02:54:43 +0530

    Update chat.ts
    (audible wincing)

--- server/chat.ts
+++ server/chat.ts
@@ -1110,10 +1110,11 @@ export class CommandContext extends MessageContext {
 				if (tagName === 'button') {
 					if (tagContent.replace(/\s*\bstyle\s*=\s*"[^"]*"\s*/i, '').trim() !== "button" &&
 						(this.room.isPersonal || this.room.isPrivate === true) && !this.user.can('lock')) {
-						if ((tagContent.replace(/\s*\bstyle\s*=\s*"[^"]*"\s*/i, '').replace(/\s*\bname\s*=\s*"\s*send\s*"/i, '')
-							.replace(new RegExp("\\s*\\bvalue\\s*=\\s*\"/(?:msg|pm|w|whisper)\\s+" +
-							this.user.id.split('').join('[^a-z0-9]*') +
-							"\\s*,[^\"]*\"\\s*", 'i'))) !== 'button') {
+						if (tagContent.replace(/\s*\bstyle\s*=\s*"[^"]*"\s*/i, '').replace(/\s*\bname\s*=\s*"\s*send\s*"/i, '')
+							.replace(new RegExp(
+								("\\s*\\bvalue\\s*=\\s*\"/(?:msg|pm|w|whisper)\\s+" +
+								this.user.id.split('').join('[^a-z0-9]*') +
+								"\\s*,[^\"]*\"\\s*"), 'i')) !== 'button') {
 							this.errorReply(`You do not have permission to use scripted buttons (buttons with attributes other than style="...") in HTML.`);
 							this.errorReply(`If you just want to link to a room, you can do this: <a href="/roomid"><button>button contents</button></a>`);
 							return null;
