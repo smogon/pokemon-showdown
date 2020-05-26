a719ebc4fd1552352e97f408f7b65bf9a0441852
Mia <49593536+mia-pi-git@users.noreply.github.com>
2020-05-25 20:22:51 -0500

    Support searching roomlog entries (#6494)

--- config/config-example.js
+++ config/config-example.js
@@ -421,6 +421,12 @@ exports.forcedpublicprefixes = [];
  */
 exports.startuphook = function () {};
 
+
+/**
+ * chatlogreader - the search method used for searching chatlogs.
+ * @type {'ripgrep' | 'fs'}
+ */
+exports.chatlogreader = 'fs';
 /**
  * permissions and groups:
  *   Each entry in `grouplist` is a seperate group. Some of the members are "special"
