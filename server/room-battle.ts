0264e13699283aed1de095e4191063a076865a7d
Mia <49593536+maxalexandderpi@users.noreply.github.com>
2020-06-07 20:41:24 -0500

    right.

--- server/room-battle.ts
+++ server/room-battle.ts
@@ -13,6 +13,7 @@
 
 import {execSync} from "child_process";
 import {FS} from "../lib/fs";
+import {Utils} from '../lib/utils';
 import {StreamProcessManager} from "../lib/process-manager";
 import {Repl} from "../lib/repl";
 import {BattleStream} from "../sim/battle-stream";
@@ -1172,7 +1173,7 @@ export class RoomBattleStream extends BattleStream {
 
 				if (result?.then) {
 					result.then((unwrappedResult: any) => {
-						unwrappedResult = Chat.stringify(unwrappedResult);
+						unwrappedResult = Utils.visualize(unwrappedResult);
 						battle.add('', 'Promise -> ' + unwrappedResult);
 						battle.sendUpdates();
 					}, (error: Error) => {
@@ -1180,7 +1181,7 @@ export class RoomBattleStream extends BattleStream {
 						battle.sendUpdates();
 					});
 				} else {
-					result = Chat.stringify(result);
+					result = Utils.visualize(result);
 					result = result.replace(/\n/g, '\n||');
 					battle.add('', '<<< ' + result);
 				}
