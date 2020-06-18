7b2ad7777dc2f5afd3092655892818fe0e605e08
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2020-06-17 23:08:03 -0600

    oop

--- data/moves.ts
+++ data/moves.ts
@@ -12228,7 +12228,7 @@ export const BattleMovedex: {[moveid: string]: MoveData} = {
 		category: "Special",
 		// Boost needs research
 		desc: "The user faints after using this move, even if this move fails for having no target. This move is prevented from executing if any active Pokemon has the Damp Ability. If Misty Terrain is active, this move's power is doubled.",
-		shortDesc: "Hits all Pokemon. Stronger in terrain.",
+		shortDesc: "The user explodes. Stronger in Misty Terrain.",
 		name: "Misty Explosion",
 		pp: 5,
 		priority: 0,
