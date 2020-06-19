c6a1bd32e99452f5378c04e16d4cbab921dc0807
Mia <49593536+mia-pi-git@users.noreply.github.com>
2020-06-19 00:46:14 -0500

    Dispossessed, surrendering to the west
    Co-authored-by: Guangcong Luo <guangcongluo@gmail.com>

--- server/chat-commands/admin.ts
+++ server/chat-commands/admin.ts
@@ -952,9 +952,9 @@ export const commands: ChatCommands = {
 			logRoom?.roomlog(`<< ${result}`);
 			this.sendReply(`|html|<table><tr><td>&laquo;</td><td>${Chat.getReadmoreCodeBlock(result)}</td></tr><table>`);
 		} catch (e) {
-			let msg = ('' + e.stack).replace(/\n *at CommandContext\.eval [\s\S]*/m, '');
-			this.sendReply(`|html|<table><tr><td>&laquo;</td><td>${Chat.getReadmoreCodeBlock(msg)}</td></tr><table>`);
-			logRoom?.roomlog(`<< ${msg}`);
+			const message = ('' + e.stack).replace(/\n *at CommandContext\.eval [\s\S]*/m, '');
+			this.sendReply(`|html|<table><tr><td>&laquo;</td><td>${Chat.getReadmoreCodeBlock(message)}</td></tr><table>`);
+			logRoom?.roomlog(`<< ${message}`);
 		}
 	},
 
