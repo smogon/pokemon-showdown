df414bb9334ed4f521063a60822b41c941b48e5e
The Immortal <the_immortal123@live.com>
2020-06-21 16:10:09 +0400

    Hide Urshifu forme in Team Preview
    https://www.smogon.com/forums/threads/sword-shield-battle-mechanics-research.3655528/post-8511700

--- data/rulesets.ts
+++ data/rulesets.ts
@@ -281,7 +281,7 @@ export const BattleFormats: {[k: string]: FormatsData} = {
 		onBegin() {
 			this.add('clearpoke');
 			for (const pokemon of this.getAllPokemon()) {
-				const details = pokemon.details.replace(/(Arceus|Gourgeist|Genesect|Pumpkaboo|Silvally)(-[a-zA-Z?]+)?/g, '$1-*')
+				const details = pokemon.details.replace(/(Arceus|Gourgeist|Genesect|Pumpkaboo|Silvally|Urshifu)(-[a-zA-Z?-]+)?/g, '$1-*')
 					.replace(', shiny', '');
 				this.add('poke', pokemon.side.id, details, this.gen < 8 && pokemon.item ? 'item' : '');
 			}
