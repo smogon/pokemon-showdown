d9c7f418b8b8980b13a1525604b722cf9af5a39f
Austin Couturier <austincouturier@icloud.com>
2020-06-30 13:09:30 -0600

    Fix /tour forcepublic to prevent /hiddenroom (#6932)

--- server/chat-commands/room-settings.ts
+++ server/chat-commands/room-settings.ts
@@ -883,6 +883,9 @@ export const commands: ChatCommands = {
 			if (prefix) {
 				return this.errorReply(`This battle is required to be public due to a player having a name prefixed by '${prefix}'.`);
 			}
+			if (room.tour?.forcePublic) {
+				return this.errorReply(`This battle can't be hidden, because the tournament is set to be forced public.`);
+			}
 		} else {
 			// registered chatrooms show up on the room list and so require
 			// higher permissions to modify privacy settings
