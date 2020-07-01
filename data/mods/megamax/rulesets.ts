7f0812a77ed6d8eac1558611e13164cd0695021b
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2020-07-01 05:54:05 -0600

    Add new PMotS

---
+++ data/mods/megamax/rulesets.ts
@@ -0,0 +1,13 @@
+export const BattleFormats: {[k: string]: FormatsData} = {
+	dynamaxclause: {
+		effectType: 'Rule',
+		name: 'Dynamax Clause',
+		desc: "Prevents Pok&eacute;mon from dynamaxing",
+		onBegin() {
+			for (const pokemon of this.getAllPokemon()) {
+				pokemon.canDynamax = false;
+			}
+			this.add('rule', 'Dynamax Clause: You cannot dynamax');
+		},
+	},
+};
---
+++ data/mods/megamax/scripts.ts
@@ -0,0 +1,51 @@
+export const BattleScripts: ModdedBattleScriptsData = {
+	canMegaEvo(pokemon) {
+		const altForme = pokemon.baseSpecies.otherFormes && this.dex.getSpecies(pokemon.baseSpecies.otherFormes[0]);
+		const item = pokemon.getItem();
+		if (
+			altForme?.isMega && altForme?.requiredMove &&
+			pokemon.baseMoves.includes(toID(altForme.requiredMove)) && !item.zMove
+		) {
+			return altForme.name;
+		}
+		if (item.name === "Urshifusite" && pokemon.baseSpecies.name === "Urshifu-Rapid-Strike") {
+			return "Urshifu-Rapid-Strike-Gmax";
+		}
+		if (item.name === "Toxtricitite" && pokemon.baseSpecies.name === "Toxtricity-Low-Key") {
+			return "Toxtricity-Low-Key-Gmax";
+		}
+		if (item.megaEvolves !== pokemon.baseSpecies.name || item.megaStone === pokemon.species.name) {
+			return null;
+		}
+		return item.megaStone;
+	},
+	runMegaEvo(pokemon) {
+		const speciesid = pokemon.canMegaEvo || pokemon.canUltraBurst;
+		if (!speciesid) return false;
+		const side = pokemon.side;
+
+		// Pokémon affected by Sky Drop cannot mega evolve. Enforce it here for now.
+		for (const foeActive of side.foe.active) {
+			if (foeActive.volatiles['skydrop'] && foeActive.volatiles['skydrop'].source === pokemon) {
+				return false;
+			}
+		}
+
+		pokemon.formeChange(speciesid, pokemon.getItem(), true);
+
+		// Limit one mega evolution
+		const wasMega = pokemon.canMegaEvo;
+		for (const ally of side.pokemon) {
+			if (wasMega) {
+				ally.canMegaEvo = null;
+			} else {
+				ally.canUltraBurst = null;
+			}
+		}
+
+		this.runEvent('AfterMega', pokemon);
+		const baseTypes = this.dex.mod('gen8').getSpecies(pokemon.baseSpecies).types.join('/');
+		if (pokemon.types.join('/') !== baseTypes) this.add('-start', pokemon, 'typechange', pokemon.types.join('/'));
+		return true;
+	},
+};
