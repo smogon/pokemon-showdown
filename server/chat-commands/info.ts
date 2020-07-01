5d72fe2f749261364ecc5ff51a78aad98a420c9f
Mia <49593536+mia-pi-git@users.noreply.github.com>
2020-07-01 01:17:24 -0500

    Fix /viewapprovals (#6935)

--- server/chat-commands/info.ts
+++ server/chat-commands/info.ts
@@ -2663,7 +2663,7 @@ export const pages: PageTable = {
 	},
 	approvals(args) {
 		const room = Rooms.get(args[0]) as ChatRoom | GameRoom;
-		if (!this.can('mute', null, this.room)) return;
+		if (!this.can('mute', null, room)) return;
 		if (!room.pendingApprovals) room.pendingApprovals = new Map();
 		if (room.pendingApprovals.size < 1) return `<h2>No pending approvals on ${room.title}</h2>`;
 		let buf = `<div class="pad"><strong>Pending media requests on ${room.title}</strong><hr />`;
