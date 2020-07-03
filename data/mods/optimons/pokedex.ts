b340ab366b3c7c8ed838f376a29d79af60409e5e
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2020-07-03 16:58:39 -0600

    Start adding other Pet Mods

---
+++ data/mods/optimons/pokedex.ts
@@ -0,0 +1,109 @@
+export const BattlePokedex: {[k: string]: ModdedSpeciesData} = {
+	inteleon: {
+		inherit: true,
+		types: ["Water", "Normal"],
+		baseStats: {hp: 70, atk: 70, def: 75, spa: 125, spd: 85, spe: 120},
+		abilities: {0: "Torrent", H: "Infiltrator"},
+	},
+	inteleongmax: {
+		inherit: true,
+		types: ["Water", "Normal"],
+		baseStats: {hp: 70, atk: 70, def: 75, spa: 125, spd: 85, spe: 120},
+		abilities: {0: "Torrent", H: "Infiltrator"},
+	},
+	falinks: {
+		inherit: true,
+		types: ["Fighting", "Ground"],
+		baseStats: {hp: 95, atk: 120, def: 110, spa: 70, spd: 60, spe: 95},
+		abilities: {0: "Skill Link", 1: "Defiant", H: "Technician"},
+	},
+	cramorant: {
+		inherit: true,
+		baseStats: {hp: 115, atk: 85, def: 75, spa: 85, spd: 95, spe: 80},
+	},
+	cramorantgulping: {
+		inherit: true,
+		baseStats: {hp: 115, atk: 85, def: 75, spa: 85, spd: 95, spe: 80},
+	},
+	cramorantgorging: {
+		inherit: true,
+		baseStats: {hp: 115, atk: 85, def: 75, spa: 85, spd: 95, spe: 80},
+	},
+	eiscue: {
+		inherit: true,
+		types: ["Ice", "Water"],
+		baseStats: {hp: 95, atk: 80, def: 120, spa: 65, spd: 110, spe: 50},
+	},
+	eiscuenoice: {
+		inherit: true,
+		types: ["Ice", "Water"],
+		baseStats: {hp: 95, atk: 90, def: 70, spa: 85, spd: 50, spe: 130},
+		abilities: {0: "Swift Swim"},
+	},
+	perrserker: {
+		inherit: true,
+		baseStats: {hp: 90, atk: 110, def: 120, spa: 50, spd: 80, spe: 80},
+	},
+	mrrime: {
+		inherit: true,
+		baseStats: {hp: 90, atk: 75, def: 95, spa: 110, spd: 100, spe: 70},
+		abilities: {0: "Tangled Feet", 1: "Screen Cleaner", H: "Unaware"},
+	},
+	cursola: {
+		inherit: true,
+		types: ["Ghost", "Dark"],
+		baseStats: {hp: 80, atk: 70, def: 85, spa: 145, spd: 130, spe: 30},
+		abilities: {0: "Regenerator", 1: "Filter", H: "Perish Body"},
+	},
+	rapidashgalar: {
+		inherit: true,
+		baseStats: {hp: 85, atk: 80, def: 75, spa: 100, spd: 80, spe: 105},
+		abilities: {0: "Pastel Veil", 1: "Misty Surge", H: "Psychic Surge"},
+	},
+	weezinggalar: {
+		inherit: true,
+		baseStats: {hp: 95, atk: 80, def: 120, spa: 100, spd: 80, spe: 60},
+	},
+	stunfiskgalar: {
+		inherit: true,
+		baseStats: {hp: 119, atk: 91, def: 99, spa: 66, spd: 84, spe: 32},
+		abilities: {0: "Mimicry", H: "Water Absorb"},
+	},
+	boltund: {
+		inherit: true,
+		types: ["Electric", "Normal"],
+		baseStats: {hp: 89, atk: 105, def: 60, spa: 105, spd: 60, spe: 146},
+		abilities: {0: "Strong Jaw", 1: "Competitive", H: "Scrappy"},
+	},
+	eldegoss: {
+		inherit: true,
+		baseStats: {hp: 90, atk: 50, def: 90, spa: 110, spd: 120, spe: 60},
+		abilities: {0: "Cotton Down", 1: "Regenerator", H: "Fluffy"},
+	},
+	greedent: {
+		inherit: true,
+		baseStats: {hp: 160, atk: 105, def: 105, spa: 55, spd: 75, spe: 20},
+		abilities: {0: "Cheek Pouch", 1: "Ripen", H: "Sticky Hold"},
+	},
+	thievul: {
+		inherit: true,
+		types: ["Dark", "Ground"],
+		baseStats: {hp: 90, atk: 58, def: 78, spa: 107, spd: 92, spe: 95},
+		abilities: {0: "Unburden", 1: "Sheer Force", H: "Stakeout"},
+	},
+	grapploct: {
+		inherit: true,
+		types: ["Fighting", "Water"],
+		baseStats: {hp: 115, atk: 118, def: 90, spa: 70, spd: 80, spe: 42},
+		abilities: {0: "Technician", H: "Poison Heal"},
+	},
+	coalossal: {
+		inherit: true,
+		baseStats: {hp: 110, atk: 100, def: 130, spa: 80, spd: 100, spe: 30},
+	},
+	sandaconda: {
+		inherit: true,
+		types: ["Ground", "Dragon"],
+		baseStats: {hp: 82, atk: 107, def: 125, spa: 65, spd: 90, spe: 75},
+	},
+};
