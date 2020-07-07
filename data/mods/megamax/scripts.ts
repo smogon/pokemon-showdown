7eb6170f40f3ed3c6775c5f566da4a687f71fafe
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2020-07-06 18:15:26 -0600

    Megamax: Add Hone Claws to Grimmsnarl (#6969)
    https://www.smogon.com/forums/threads/megamax-pet-mod-of-the-season.3658623/page-11#post-8525900

--- data/mods/megamax/scripts.ts
+++ data/mods/megamax/scripts.ts
@@ -2,7 +2,6 @@ export const BattleScripts: ModdedBattleScriptsData = {
 	gen: 8,
 	init() {
 		this.modData('FormatsData', 'melmetalgmax').isNonstandard = null;
-		this.modData('FormatsData', 'melmetalgmax').tier = "Uber";
 
 		this.modData('Learnsets', 'venusaur').learnset.gunkshot = ["8M"];
 		this.modData('Learnsets', 'charizard').learnset.lavaplume = ["8M"];
@@ -53,6 +52,7 @@ export const BattleScripts: ModdedBattleScriptsData = {
 		this.modData('Learnsets', 'hatterene').learnset.moonblast = ["8M"];
 		this.modData('Learnsets', 'hatterene').learnset.gravity = ["8M"];
 		this.modData('Learnsets', 'grimmsnarl').learnset.yawn = ["8M"];
+		this.modData('Learnsets', 'grimmsnarl').learnset.honeclaws = ["8M"];
 		this.modData('Learnsets', 'alcremie').learnset.icebeam = ["8M"];
 		this.modData('Learnsets', 'alcremie').learnset.moonblast = ["8M"];
 		this.modData('Learnsets', 'alcremie').learnset.stockpile = ["8M"];
