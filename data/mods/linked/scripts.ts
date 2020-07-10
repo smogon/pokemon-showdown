0afd86221b7ccb5b8d31e39e3967a2fabd544ccd
Slayer95 <ivojulca@hotmail.com>
2020-07-10 01:17:28 -0500

    Linked: Fix priority of linked moves (#6990)
    Second move in a chain ignores Gen 8 dynamic turn-order mechanic.

--- data/mods/linked/scripts.ts
+++ data/mods/linked/scripts.ts
@@ -394,7 +394,8 @@ export const BattleScripts: ModdedBattleScriptsData = {
 
 		if (this.gen < 5) this.eachEvent('Update');
 
-		if (this.gen >= 8 && this.queue.peek()?.choice === 'move') {
+		const nextAction = this.queue.peek();
+		if (this.gen >= 8 && nextAction?.choice === 'move' && nextAction?.pokemon !== action.pokemon) {
 			// In gen 8, speed is updated dynamically so update the queue's speed properties and sort it.
 			this.updateSpeed();
 			for (const queueAction of this.queue) {
