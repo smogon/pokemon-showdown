7558a4707d0a1d8ef5b1fcf9982a7be6cf5a3569
Guangcong Luo <guangcongluo@gmail.com>
2020-07-23 14:18:02 -0700

    Fix bug in /renameroom notifications
    They would not have actually worked.

--- server/chat-commands/room-settings.ts
+++ server/chat-commands/room-settings.ts
@@ -875,13 +875,11 @@ export const commands: ChatCommands = {
 		const privacy = room.settings.isPrivate === true ? "Private" :
 			!room.settings.isPrivate ? "Public" :
 			`${room.settings.isPrivate.charAt(0).toUpperCase()}${room.settings.isPrivate.slice(1)}`;
-		const message = Utils.html`
-			|raw|<div class="broadcast-green">${privacy} chat room <b>${oldTitle}</b> renamed to <b>${target}</b></div>
-		`;
 
-		const toNotify: RoomID[] = ['upperstaff'];
-		if (room.settings.isPrivate !== true) toNotify.push('staff');
-		Rooms.global.notifyRooms(toNotify, message);
+		Rooms.global.notifyRooms(
+			room.settings.isPrivate === true ? ['upperstaff'] : ['upperstaff', 'staff'],
+			Utils.html`|raw|<div class="broadcast-green">${privacy} chat room <b>${oldTitle}</b> renamed to <b>${target}</b></div>`
+		);
 		room.add(Utils.html`|raw|<div class="broadcast-green">The room has been renamed to <b>${target}</b></div>`).update();
 	},
 	renamehelp: [`/renameroom [new title] - Renames the current room to [new title]. Requires &.`],
