e56247d01ea9450a144af2d0b9a0284d5fd07ad5
Alumn <39522426+Alumn2000@users.noreply.github.com>
2020-07-31 02:44:27 +0200

    Update links for Gen 7 OU (#7093)

--- config/formats.ts
+++ config/formats.ts
@@ -1660,8 +1660,8 @@ export const Formats: (FormatsData | {section: string, column?: number})[] = [
 		name: "[Gen 7] OU",
 		threads: [
 			`&bullet; <a href="https://www.smogon.com/dex/sm/tags/ou/">USM OU Banlist</a>`,
-			`&bullet; <a href="https://www.smogon.com/forums/threads/3638845/">USM OU Sample Teams</a>`,
-			`&bullet; <a href="https://www.smogon.com/forums/threads/3621329/">USM OU Viability Rankings</a>`,
+			`&bullet; <a href="https://www.smogon.com/forums/posts/8162240/">USM OU Sample Teams</a>`,
+			`&bullet; <a href="https://www.smogon.com/forums/threads/3667522/">USM OU Viability Rankings</a>`,
 		],
 
 		mod: 'gen7',
