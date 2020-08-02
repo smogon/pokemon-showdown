d9289b5a003d5bb9df70760ee595e38870017535
Volco <13051425+Volco@users.noreply.github.com>
2020-08-02 01:33:20 -0400

    Fix room renaming (yet again) and groupchat creation

--- server/rooms.ts
+++ server/rooms.ts
@@ -671,7 +671,8 @@ export abstract class BasicRoom {
 		// `,` is a delimiter used by a lot of /commands
 		// `|` and `[` are delimiters used by the protocol
 		// `-` has special meaning in roomids
-		if (newTitle.includes(',') || newTitle.includes('|') || newTitle.includes('[') || newTitle.includes('-')) {
+		// groupchats use `[` so allow it to bypass this and run the checks in the commands themselves
+		if (this.persist && (newTitle.includes(',') || newTitle.includes('|') || newTitle.includes('[') || newTitle.includes('-'))) {
 			throw new Chat.ErrorMessage("Room titles can't contain any of: ,|[-");
 		}
 		if (newID.length > MAX_CHATROOM_ID_LENGTH) throw new Chat.ErrorMessage("The given room title is too long.");
