b7b38102ed18e3237a571a32543933f43431cb2c
Marty-D <Marty-D@users.noreply.github.com>
2020-08-06 14:36:54 -0400

    It's fixed!

--- data/mods/gen4/moves.ts
+++ data/mods/gen4/moves.ts
@@ -229,8 +229,6 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 		desc: "The user's type changes based on the battle terrain. Normal type on the regular Wi-Fi terrain. Fails if the user has the Multitype Ability or if the type is one of the user's current types.",
 		shortDesc: "Changes user's type based on terrain. (Normal)",
 		onHit(target) {
-			// includes() is used here over hasType() because for whatever reason Arceus-Water
-			// always returns Normal-type from hasType() which is stupid
 			if (target.hasType('Normal') || !target.setType('Normal')) return false;
 			this.add('-start', target, 'typechange', 'Normal');
 		},
