f6c9165e0b8d0bf9996132f9d0e68ac039ea05ef
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2020-08-07 02:36:12 -0600

    SSB: Remove Flare (#7170)

--- data/mods/ssb/abilities.ts
+++ data/mods/ssb/abilities.ts
@@ -412,56 +412,6 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
 			}
 		},
 	},
-	// Flare
-	superillusion: {
-		desc: "When this Pokemon switches in, it appears as the last unfainted Pokemon in its party until it takes super effective direct damage from another Pokemon's attack. This Pokemon's actual level and HP are displayed instead of those of the mimicked Pokemon.",
-		shortDesc: "Appears as the last Pokemon in the party until it takes a supereffective hit.",
-		name: "Super Illusion",
-		isNonstandard: "Custom",
-		isUnbreakable: true,
-		onBeforeSwitchIn(pokemon) {
-			pokemon.illusion = null;
-			let i;
-			for (i = pokemon.side.pokemon.length - 1; i > pokemon.position; i--) {
-				if (!pokemon.side.pokemon[i]) continue;
-				if (!pokemon.side.pokemon[i].fainted) break;
-			}
-			if (!pokemon.side.pokemon[i]) return;
-			if (pokemon === pokemon.side.pokemon[i]) return;
-			pokemon.illusion = pokemon.side.pokemon[i];
-		},
-		onDamagingHit(damage, target, source, move) {
-			// Illusion that only breaks when hit with a move that is super effective VS dark
-			if (target.illusion && this.dex.getEffectiveness(move.type, target.getTypes()) > 0) {
-				this.singleEvent('End', this.dex.getAbility('Illusion'), target.abilityData, target, source, move);
-			}
-		},
-		onEnd(pokemon) {
-			if (pokemon.illusion) {
-				this.debug('illusion cleared');
-				let disguisedAs = toID(pokemon.illusion.name);
-				pokemon.illusion = null;
-				const details = pokemon.species.name + (pokemon.level === 100 ? '' : ', L' + pokemon.level) +
-					(pokemon.gender === '' ? '' : ', ' + pokemon.gender) + (pokemon.set.shiny ? ', shiny' : '');
-				this.add('replace', pokemon, details);
-				this.add('-end', pokemon, 'Illusion');
-				// Handle hippopotas
-				if (this.dex.getSpecies(disguisedAs).exists) disguisedAs += 'user';
-				if (pokemon.volatiles[disguisedAs]) {
-					pokemon.removeVolatile(disguisedAs);
-				}
-				if (!pokemon.volatiles[toID(pokemon.name)]) {
-					const status = this.dex.getEffect(toID(pokemon.name));
-					if (status?.exists) {
-						pokemon.addVolatile(toID(pokemon.name), pokemon);
-					}
-				}
-			}
-		},
-		onFaint(pokemon) {
-			pokemon.illusion = null;
-		},
-	},
 	// Gallant Spear
 	trombe: {
 		desc: "On switch-in, this Pokemon summons Light Screen or Reflect and is guaranteed to move first for one turn.",
