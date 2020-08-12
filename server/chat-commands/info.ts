47b40d6907d64111d44a2ab661760faa6b1f1fef
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2020-08-12 09:34:04 -0600

    Fix crash with /dt (#7199)

--- server/chat-commands/info.ts
+++ server/chat-commands/info.ts
@@ -549,7 +549,7 @@ export const commands: ChatCommands = {
 			case 'pokemon':
 				let pokemon = dex.getSpecies(newTarget.name);
 				if (format?.onModifySpecies) {
-					pokemon = format.onModifySpecies.call({dex, clampIntRange: Utils.clampIntRange} as Battle, pokemon) || pokemon;
+					pokemon = format.onModifySpecies.call({dex, clampIntRange: Utils.clampIntRange, toID} as Battle, pokemon) || pokemon;
 				}
 				let tierDisplay = room?.settings.dataCommandTierDisplay;
 				if (!tierDisplay && room?.battle) {
