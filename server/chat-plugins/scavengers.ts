4c12f248f2a4b6ea6c368741e3262449e066e4db
Charlie Kobayashi <sparkychildcharlie@gmail.com>
2020-08-19 23:37:40 -0400

    Update scavengers.ts

--- server/chat-plugins/scavengers.ts
+++ server/chat-plugins/scavengers.ts
@@ -1424,7 +1424,7 @@ const ScavengerCommands: ChatCommands = {
 			room,
 			gameType === 'official' || gameType === 'recycled'
 		);
-		if (!hosts) return this.errorReply("The user(s) you specified as the host is not online, or is not in the room.");
+		if (!hosts.length) return this.errorReply("The user(s) you specified as the host is not online, or is not in the room.");
 
 		const res = ScavengerHunt.parseQuestions(params);
 		if (res.err) return this.errorReply(res.err);
@@ -1733,7 +1733,7 @@ const ScavengerCommands: ChatCommands = {
 		} else {
 			const [hostsArray, ...params] = target.split('|');
 			const hosts = ScavengerHunt.parseHosts(hostsArray.split(/[,;]/), room);
-			if (!hosts) return this.errorReply("The user(s) you specified as the host is not online, or is not in the room.");
+			if (!hosts.length) return this.errorReply("The user(s) you specified as the host is not online, or is not in the room.");
 
 			const results = ScavengerHunt.parseQuestions(params);
 			if (results.err) return this.errorReply(results.err);
@@ -2314,7 +2314,7 @@ const ScavengerCommands: ChatCommands = {
 
 			const [hostsArray, ...questions] = target.split('|');
 			const hosts = ScavengerHunt.parseHosts(hostsArray.split(/[,;]/), room, true);
-			if (!hosts) return this.errorReply("You need to specify a host.");
+			if (!hosts.length) return this.errorReply("You need to specify a host.");
 
 			const result = ScavengerHunt.parseQuestions(questions);
 			if (result.err) return this.errorReply(result.err);
