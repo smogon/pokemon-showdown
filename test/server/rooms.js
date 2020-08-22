ba4998350a93e9d2e94786e30a1e1d6a77944961
Guangcong Luo <guangcongluo@gmail.com>
2020-08-22 16:03:10 -0700

    Fix more bugs in /hiderank

--- test/server/rooms.js
+++ test/server/rooms.js
@@ -91,7 +91,7 @@ describe('Rooms features', function () {
 			roomStaff.forceRename("Room auth", true);
 			const administrator = new User();
 			administrator.forceRename("Admin", true);
-			administrator.group = '~';
+			administrator.tempGroup = '~';
 			const options = {
 				p1,
 				p2,
