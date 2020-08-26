70dac61651d1931e7099562a8507e43657af1acd
Instruct <66930476+xInstruct@users.noreply.github.com>
2020-08-26 14:03:47 -0400

    Comment out bugged code for dream

--- data/mods/ssb/abilities.ts
+++ data/mods/ssb/abilities.ts
@@ -560,7 +560,7 @@ export const Abilities: {[k: string]: ModdedAbilityData & {gen?: number}} = {
 		desc: "This Pokemon can only be damaged by direct attacks. On switch-in, this Pokemon's stats are boosted based on the number of hazards on the field. 1 stat is raised if 1-2 hazards are up, and 2 stats are raised if 3 or more hazards are up.",
 		shortDesc: "On switch-in, boosts stats based on the number of hazards up on this Pokemon's side.",
 		name: "Greed Punisher",
-		onSwitchIn(pokemon) {
+		/*onSwitchIn(pokemon) {
 			const side = pokemon.side;
 			const activeCount = Object.keys(side.sideConditions).length;
 			if (activeCount > 0) {
@@ -585,7 +585,7 @@ export const Abilities: {[k: string]: ModdedAbilityData & {gen?: number}} = {
 					return false;
 				}
 			}
-		},
+		},*/
 		onDamage(damage, target, source, effect) {
 			if (effect.id === 'heavyhailstorm') return;
 			if (effect.effectType !== 'Move') {
