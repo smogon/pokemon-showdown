f05e5e2b60d953f0d6ba0674f05016f4b1311acd
TheMezStrikes <TheMezStrikes@gmail.com>
2020-09-01 00:03:49 +0200

    Update CAP samples link to gen 8
    I wasn't sure whether to put SS or SwSh, there's no precedent I can find

--- server/chat-commands/info.ts
+++ server/chat-commands/info.ts
@@ -1730,7 +1730,7 @@ export const commands: ChatCommands = {
 			`- <a href="https://www.smogon.com/cap/">CAP project website and description</a><br />` +
 			`- <a href="https://www.smogon.com/forums/threads/48782/">What Pok&eacute;mon have been made?</a><br />` +
 			`- <a href="https://www.smogon.com/forums/forums/477">Talk about the metagame here</a><br />` +
-			`- <a href="https://www.smogon.com/forums/threads/3648521/">Sample SM CAP teams</a>`
+			`- <a href="https://www.smogon.com/forums/threads/3662655/">Sample SS CAP teams</a>`
 		);
 	},
 	caphelp: [
