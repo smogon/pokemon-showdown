f2bf1a45390d5134bc6f1784a66dd1a5f5cf1137
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2020-09-01 14:35:37 -0600

    350 Cup: Ban Abra and Gastly
    https://www.smogon.com/forums/threads/350-cup-leaders-choice.3656554/#post-8585930

--- config/formats.ts
+++ config/formats.ts
@@ -771,7 +771,7 @@ export const Formats: (FormatsData | {section: string, column?: number})[] = [
 
 		mod: 'gen8',
 		ruleset: ['350 Cup Mod', 'Standard', 'Dynamax Clause'],
-		banlist: ['Pawniard', 'Rufflet', 'Eviolite', 'Light Ball', 'Arena Trap', 'Shadow Tag', 'Baton Pass'],
+		banlist: ['Abra', 'Gastly', 'Pawniard', 'Rufflet', 'Eviolite', 'Light Ball', 'Arena Trap', 'Shadow Tag', 'Baton Pass'],
 	},
 
 	// Other Metagames
