8a4991f8d7bed6e905a27de24eb1e18928668a3d
Mia <49593536+mia-pi-git@users.noreply.github.com>
2020-09-18 18:19:43 -0500

    Add weekban to /punishments (#7388)

--- server/chat-commands/info.ts
+++ server/chat-commands/info.ts
@@ -1542,6 +1542,7 @@ export const commands: ChatCommands = {
 			`<strong>mute</strong> - Mutes a user (makes them unable to talk) for 7 minutes.`,
 			`<strong>hourmute</strong> - Mutes a user for 60 minutes.`,
 			`<strong>ban</strong> - Bans a user (makes them unable to join the room) for 2 days.`,
+			`<strong>weekban</strong> - Bans a user from the room for a week.`,
 			`<strong>blacklist</strong> - Bans a user for a year.`,
 		];
 
