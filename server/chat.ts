05065ccabce45683066dc6442090b616f6e9395e
Guangcong Luo <guangcongluo@gmail.com>
2020-09-28 11:17:10 -0700

    Introduce Chat.oldPlugins
    This provides a new way to persist chat plugin data across hotpatches:
    `Chat.oldPlugins`.
    
    In a plugin, you can now do:
    
    ```
    export const cache = Chat.oldPlugins.pluginname?.cache || {};
    ```
    
    to create a cache that will persist across hotpatches.

--- server/chat.ts
+++ server/chat.ts
@@ -56,6 +56,14 @@ export interface AnnotatedChatCommands {
 	[k: string]: AnnotatedChatHandler | string | string[] | AnnotatedChatCommands;
 }
 
+export interface ChatPlugin {
+	commands?: AnnotatedChatCommands;
+	pages?: PageTable;
+	destroy?: () => void;
+	roomSettings?: SettingsHandler | SettingsHandler[];
+	[k: string]: any;
+}
+
 export type SettingsHandler = (
 	room: Room,
 	user: User,
@@ -1357,7 +1365,9 @@ export const Chat = new class {
 	pages!: PageTable;
 	readonly destroyHandlers: (() => void)[] = [];
 	/** The key is the name of the plugin. */
-	readonly plugins: {[k: string]: AnyObject} = {};
+	readonly plugins: {[k: string]: ChatPlugin} = {};
+	/** Will be empty except during hotpatch */
+	oldPlugins: {[k: string]: ChatPlugin} = {};
 	roomSettings: SettingsHandler[] = [];
 
 	/*********************************************************
@@ -1684,8 +1694,9 @@ export const Chat = new class {
 		if (plugin.statusfilter) Chat.statusfilters.push(plugin.statusfilter);
 		Chat.plugins[name] = plugin;
 	}
-	loadPlugins() {
+	loadPlugins(oldPlugins?: {[k: string]: ChatPlugin}) {
 		if (Chat.commands) return;
+		if (oldPlugins) Chat.oldPlugins = oldPlugins;
 
 		void FS('package.json').readIfExists().then(data => {
 			if (data) Chat.packageData = JSON.parse(data);
@@ -1736,6 +1747,7 @@ export const Chat = new class {
 		for (const file of files) {
 			this.loadPlugin(`chat-plugins/${file}`);
 		}
+		Chat.oldPlugins = {};
 	}
 	destroy() {
 		for (const handler of Chat.destroyHandlers) {
