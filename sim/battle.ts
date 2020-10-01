fa569c7a49c58545365279cc57fbbbbe8c3ab345
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2020-10-01 17:31:45 -0600

    Add properties to see which Pokemon fainted last/this turn (#7452)

--- sim/battle.ts
+++ sim/battle.ts
@@ -1509,7 +1509,7 @@ export class Battle {
 			trappedBySide.push(sideTrapped);
 			stalenessBySide.push(sideStaleness);
 			side.faintedLastTurn = side.faintedThisTurn;
-			side.faintedThisTurn = false;
+			side.faintedThisTurn = null;
 		}
 
 		if (this.maybeTriggerEndlessBattleClause(trappedBySide, stalenessBySide)) return;
@@ -2398,7 +2398,7 @@ export class Battle {
 				pokemon.illusion = null;
 				pokemon.isActive = false;
 				pokemon.isStarted = false;
-				pokemon.side.faintedThisTurn = true;
+				pokemon.side.faintedThisTurn = pokemon;
 			}
 		}
 
