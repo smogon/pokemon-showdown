0fd917faf47582fb66e2f07273f00049095e2544
Mia <49593536+mia-pi-git@users.noreply.github.com>
2020-10-01 11:25:41 -0500

    Quotes: Remove the broadcast cooldown for /randquote

--- server/chat-plugins/quotes.ts
+++ server/chat-plugins/quotes.ts
@@ -38,7 +38,7 @@ export const commands: ChatCommands = {
 		room = this.requireRoom();
 		const roomQuotes = quotes[room.roomid];
 		if (!roomQuotes?.length) return this.errorReply(`This room has no quotes.`);
-		this.runBroadcast();
+		this.runBroadcast(true);
 		const {quote, date, userid} = roomQuotes[Math.floor(Math.random() * roomQuotes.length)];
 		const time = Chat.toTimestamp(new Date(date), {human: true});
 		const attribution = toID(target) === 'showauthor' ? `<br /><hr /><small>Added by ${userid} on ${time}</small>` : '';
