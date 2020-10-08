8e4f6d171f829099f2cfbe34596bdbb6d2ce1672
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2020-10-08 00:42:30 -0600

    Repeats: Refactor storing and deleting repeats, let people repeat room FAQs (#7488)

--- test/server/chat-plugins/repeats.js
+++ test/server/chat-plugins/repeats.js
@@ -15,33 +15,36 @@ describe("Repeats plugin", () => {
 
 	it('should add repeats correctly', () => {
 		assert.ok(!Repeats.repeats.has(this.room));
-		Repeats.addRepeat(this.room, {phrase: "^_^", interval: 1});
+
+		Repeats.addRepeat(this.room, {id: 'happyface', phrase: "^_^", interval: 1});
 
 		assert.ok(Repeats.repeats.has(this.room));
 
-		assert.ok(Repeats.repeats.get(this.room).has("^_^"));
+		assert.ok(Repeats.repeats.get(this.room).has('happyface'));
+
+		assert.ok(Repeats.repeats.get(this.room).get('happyface').has("^_^"));
 
 		assert.ok(this.room.settings.repeats);
 		assert.ok(this.room.settings.repeats.some(repeat => repeat.phrase === "^_^"));
 	});
 
 	it('should remove repeats correctly', () => {
-		Repeats.addRepeat(this.room, {phrase: "^_-", interval: 1});
-		assert.ok(Repeats.repeats.get(this.room).has("^_-"));
+		Repeats.addRepeat(this.room, {id: 'weirdface', phrase: "^_-", interval: 1});
+		assert.ok(Repeats.repeats.get(this.room).get('weirdface').has("^_-"));
 		assert.ok(this.room.settings.repeats.some(repeat => repeat.phrase === "^_-"));
 
-		Repeats.removeRepeat(this.room, "^_-");
-		assert.strictEqual(Repeats.repeats.get(this.room).get("^_-"), undefined);
+		Repeats.removeRepeat(this.room, 'weirdface');
+		assert.strictEqual(Repeats.repeats.get(this.room).get('weirdface'), undefined);
 		assert.ok(!this.room.settings.repeats.some(repeat => repeat.phrase === "^_-"));
 	});
 
 	it('should be able to tell if a repeat exists or not', () => {
-		assert.ok(!Repeats.hasRepeat(this.room, "-_-"));
+		assert.ok(!Repeats.hasRepeat(this.room, 'annoyedface'));
 
-		Repeats.addRepeat(this.room, {phrase: "-_-", interval: 1});
-		assert.ok(Repeats.hasRepeat(this.room, "-_-"));
+		Repeats.addRepeat(this.room, {id: 'annoyedface', phrase: "-_-", interval: 1});
+		assert.ok(Repeats.hasRepeat(this.room, 'annoyedface'));
 
-		Repeats.removeRepeat(this.room, "-_-");
-		assert.ok(!Repeats.hasRepeat(this.room, "-_-"));
+		Repeats.removeRepeat(this.room, 'annoyedface');
+		assert.ok(!Repeats.hasRepeat(this.room, 'annoyedface'));
 	});
 });
