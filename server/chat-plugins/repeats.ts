81288d010413128e536f112c23ddde23af6f68db
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2020-10-07 18:44:57 -0600

    Repeats: Allow people to repeat room FAQs

--- server/chat-plugins/repeats.ts
+++ server/chat-plugins/repeats.ts
@@ -4,10 +4,13 @@
  * @author Annika, Zarel
  */
 
+import {roomFaqs, getAlias} from './room-faqs';
+
 export interface RepeatedPhrase {
 	phrase: string;
 	/** interval in milliseconds */
 	interval: number;
+	faq?: string;
 }
 
 export const Repeats = new class {
@@ -75,7 +78,8 @@ export const Repeats = new class {
 					this.clearRepeats(room);
 					return;
 				}
-				room.add(`|html|<div class="infobox">${repeat.phrase}</div>`);
+				const phrase = !repeat.faq ? repeat.phrase : Chat.formatText(roomFaqs[room.roomid][repeat.faq], true).replace(/\n/g, '<br />');
+				room.add(`|html|<div class="infobox">${phrase}</div>`);
 				room.update();
 			}, repeat.interval)
 		);
@@ -97,17 +101,25 @@ export function destroy() {
 export const pages: PageTable = {
 	repeats(args, user) {
 		const room = this.requireRoom();
-		this.title = `Repeated phrases in ${room.roomid}`;
+		this.title = `[Repeats]`;
 		this.checkCan("mute", null, room);
-		if (!room.settings.repeats) return `<h1>${this.tr`There are no repeated phrases in ${room.roomid}.`}</h1>`;
-		let html = `<div class="ladder pad"><h2>${this.tr`Repeated phrases in ${room.roomid}`}</h2>`;
+		let html = `<div class="ladder pad">`;
+		html += `<button class="button" name="send" value="/join view-repeats-${room.roomid}" style="float: right"><i class="fa fa-refresh"></i> ${this.tr`Refresh`}</button>`;
+		if (!room.settings.repeats?.length) {
+			return `${html}<h1>${this.tr`There are no repeated phrases in ${room.title}.`}</h1></div>`;
+		}
+		html += `<h2>${this.tr`Repeated phrases in ${room.title}`}</h2>`;
 		html += `<table><tr><th>${this.tr`Phrase`}</th><th>${this.tr`Interval`}</th><th>${this.tr`Action`}</th>`;
-		html += `<button class="button" name="send" value="/join view-repeats-${room.roomid}" style="float: right">`;
-		html += `<i class="fa fa-refresh"></i> ${this.tr`Refresh`}</button>`;
 		for (const repeat of room.settings.repeats) {
 			const minutes = repeat.interval / (60 * 1000);
-			html += `<tr><td>${repeat.phrase}</td><td>${this.tr`every ${minutes} minute(s)`}</td>`;
-			html += `<td><button class="button" name="send" value="/removerepeat ${repeat.phrase}">${this.tr`Remove`}</button></td>`;
+			if (!repeat.faq) {
+				html += `<tr><td>${repeat.phrase}</td><td>${this.tr`every ${minutes} minute(s)`}</td>`;
+				html += `<td><button class="button" name="send" value="/removerepeat ${repeat.phrase}[SPLIT]${room.roomid}">${this.tr`Remove`}</button></td>`;
+			} else {
+				const phrase = Chat.formatText(roomFaqs[room.roomid][repeat.faq], true).replace(/\n/g, '<br />');
+				html += `<tr><td>${phrase}</td><td>${this.tr`every ${minutes} minute(s)`}</td>`;
+				html += `<td><button class="button" name="send" value="/removerepeatfaq ${repeat.faq}[SPLIT]${room.roomid}">${this.tr`Remove`}</button></td>`;
+			}
 		}
 		html += `</table>`;
 		if (user.can("editroom", null, room)) {
@@ -146,28 +158,76 @@ export const commands: ChatCommands = {
 		this.runBroadcast();
 		this.sendReplyBox(
 			`<code>/repeat [minutes], [phrase]</code>: repeats a given phrase every [minutes] minutes.<br />` +
+			`<code>/repeatfaq [minutes], [FAQ name/alias]</code>: repeats a given Room FAQ every [minutes] minutes.<br />` +
 			`<code>/removerepeat [phrase]</code>: removes a repeated phrase.<br />` +
-			`/viewrepeats [optional room] - Displays all repeated phrases in a room.<br />` +
+			`<code>/removerepeatfaq [faq]</code>: removes a repeated FAQ.<br />` +
+			`<code>/viewrepeats [optional room]</code>: Displays all repeated phrases in a room.<br />` +
 			`Phrases for <code>/repeat</code> can include normal chat formatting, but not commands. Requires: % @ # &`
 		);
 	},
 
-	deleterepeat: 'removerepeat',
-	removerepeat(target, room, user) {
+	repeatfaq(target, room, user) {
 		room = this.requireRoom();
 		this.checkCan('mute', null, room);
-
-		if (!target) return this.parse('/help repeat');
-		target = target.trim();
-
-		if (!Repeats.hasRepeat(room, target)) {
-			return this.errorReply(this.tr`The phrase "${target}" is not being repeated in this room.`);
+		let [intervalString, topic] = this.splitOne(target);
+		const interval = parseInt(intervalString);
+		if (isNaN(interval) || !/[0-9]{1,}/.test(intervalString) || interval < 1) {
+			throw new Chat.ErrorMessage(this.tr`You must specify a numerical interval of at least 1 minute.`);
+		}
+		if (!roomFaqs[room.roomid]) {
+			throw new Chat.ErrorMessage(`This room has no FAQs.`);
+		}
+		topic = getAlias(room.roomid, topic) || topic;
+		if (!roomFaqs[room.roomid][topic]) {
+			throw new Chat.ErrorMessage(`Invalid topic.`);
 		}
 
-		Repeats.removeRepeat(room, target);
+		if (Repeats.hasRepeat(room, roomFaqs[room.roomid][topic])) {
+			throw new Chat.ErrorMessage(`The text for the Room FAQ '${topic}' is already being repeated.`);
+		}
 
-		this.modlog('REMOVE REPEATPHRASE', null, `"${target}"`);
-		this.privateModAction(room.tr`${user.name} removed the repeated phrase "${target}".`);
+		Repeats.addRepeat(room, {
+			phrase: roomFaqs[room.roomid][topic],
+			interval: interval * 60 * 1000,
+			faq: topic,
+		});
+
+		this.modlog('REPEATPHRASE', null, `every ${interval} minute${Chat.plural(interval)}: the Room FAQ for "${topic}"`);
+		this.privateModAction(
+			room.tr`${user.name} set the Room FAQ "${topic}" to be repeated every ${interval} minute(s).`
+		);
+	},
+
+	deleterepeat: 'removerepeat',
+	removerepeatfaq: 'removerepeat',
+	removerepeat(target, room, user, connection, cmd) {
+		target = target.trim();
+		let [repeat, roomid] = target.split('[SPLIT]');
+		if (!repeat) {
+			return this.parse(`/help repeat`);
+		}
+		const targetRoom = roomid ? Rooms.search(roomid) : room;
+		if (!targetRoom) {
+			return this.errorReply(`Invalid room.`);
+		}
+		this.room = targetRoom;
+		this.checkCan('mute', null, targetRoom);
+		if (cmd === 'removerepeatfaq' &&
+			roomFaqs[targetRoom.roomid][getAlias(targetRoom.roomid, toID(repeat)) || toID(repeat)]) {
+			repeat = roomFaqs[targetRoom.roomid][getAlias(targetRoom.roomid, toID(repeat)) || toID(repeat)];
+		} else if (!roomid) {
+			repeat = Chat.formatText(repeat.trim()).replace(/\n/g, '<br />');
+		}
+
+		if (!Repeats.hasRepeat(targetRoom, repeat)) {
+			return this.errorReply(this.tr`The phrase "${repeat}" is not being repeated in this room.`);
+		}
+
+		Repeats.removeRepeat(targetRoom, repeat);
+
+		this.modlog('REMOVE REPEATPHRASE', null, `"${repeat}"`);
+		this.privateModAction(targetRoom.tr`${user.name} removed the repeated phrase "${repeat}".`);
+		if (roomid) this.parse(`/join view-repeats-${targetRoom.roomid}`);
 	},
 
 	removeallrepeats(target, room, user) {
