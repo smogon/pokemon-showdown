eb0610a4de137ecb22e4486c57d3c73036443755
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2020-10-09 17:58:36 -0600

    Fix /repeatfaq intervals

--- server/chat-plugins/repeats.ts
+++ server/chat-plugins/repeats.ts
@@ -185,8 +185,8 @@ export const commands: ChatCommands = {
 		this.checkCan('mute', null, room);
 		let [intervalString, topic] = target.split(',');
 		const interval = parseInt(intervalString);
-		if (isNaN(interval) || !/[0-9]{1,}/.test(intervalString) || interval < 1) {
-			throw new Chat.ErrorMessage(this.tr`You must specify a numerical interval of at least 1 minute.`);
+		if (isNaN(interval) || !/[0-9]{1,}/.test(intervalString) || interval < 1 || interval > 24 * 60) {
+			throw new Chat.ErrorMessage(this.tr`You must specify a interval as a number of minutes between 1 and 1440.`);
 		}
 		if (!roomFaqs[room.roomid]) {
 			throw new Chat.ErrorMessage(`This room has no FAQs.`);
