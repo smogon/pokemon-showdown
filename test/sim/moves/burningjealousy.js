693a3f0aa37b4a54e8a794dc3e8ecd29e51326cf
Leonard Craft <leonardcraft64@gmail.com>
2020-10-10 11:27:31 -0500

    Further improve Burning Jealousy tests

--- test/sim/moves/burningjealousy.js
+++ test/sim/moves/burningjealousy.js
@@ -12,54 +12,54 @@ describe('Burning Jealousy', function () {
 
 	it(`should burn a target whose stats were raised this turn`, function () {
 		battle = common.createBattle([[
-			{species: "Mew", moves: ['dragondance']},
+			{species: 'Mew', moves: ['dragondance']},
 		], [
-			{species: "Torkoal", moves: ['burningjealousy']},
+			{species: 'Torkoal', moves: ['burningjealousy']},
 		]]);
 		battle.makeChoices();
-		assert(battle.p1.active[0].status === 'brn');
+		assert.strictEqual(battle.p1.active[0].status, 'brn');
 	});
 
 	it(`should not burn a target whose stats were raised after the attack`, function () {
 		battle = common.createBattle([[
-			{species: "Ninetales", moves: ['burningjealousy']},
+			{species: 'Ninetales', moves: ['burningjealousy']},
 		], [
-			{species: "Magearna", item: 'weaknesspolicy', moves: ['imprison']},
+			{species: 'Magearna', item: 'weaknesspolicy', moves: ['imprison']},
 		]]);
 		battle.makeChoices();
-		assert(battle.p2.active[0].status === '');
+		assert.strictEqual(battle.p2.active[0].status, '');
 	});
 
 	it.skip(`should burn a target whose stats were boosted at the start of the match`, function () {
 		battle = common.createBattle([[
-			{species: "Wynaut", moves: ['burningjealousy']},
+			{species: 'Wynaut', moves: ['burningjealousy']},
 		], [
-			{species: "Porygon", ability: 'download', moves: ['sleeptalk']},
+			{species: 'Porygon', ability: 'download', moves: ['sleeptalk']},
 		]]);
 		battle.makeChoices();
-		assert(battle.p2.active[0].status === 'brn');
+		assert.strictEqual(battle.p2.active[0].status, 'brn');
 	});
 
 	it(`should not burn a target whose stats were boosted at a switch after a KO`, function () {
 		battle = common.createBattle([[
-			{species: "Wynaut", moves: ['burningjealousy']},
+			{species: 'Wynaut', moves: ['burningjealousy']},
 		], [
-			{species: "Porygon", ability: 'download', moves: ['memento']},
-			{species: "Porygon2", ability: 'download', moves: ['sleeptalk']},
+			{species: 'Porygon', ability: 'download', moves: ['memento']},
+			{species: 'Porygon2', ability: 'download', moves: ['sleeptalk']},
 		]]);
 		battle.makeChoices();
 		battle.makeChoices();
 		battle.makeChoices();
-		assert(battle.p2.active[0].status === '');
+		assert.strictEqual(battle.p2.active[0].status, '');
 	});
 
 	it(`should be affected by Sheer Force`, function () {
 		battle = common.createBattle([[
-			{species: "Cobalion", moves: ['swordsdance']},
+			{species: 'Cobalion', moves: ['swordsdance']},
 		], [
-			{species: "Darmanitan", ability: 'sheerforce', item: 'kingsrock', moves: ['burningjealousy']},
+			{species: 'Darmanitan', ability: 'sheerforce', item: 'kingsrock', moves: ['burningjealousy']},
 		]]);
 		battle.makeChoices();
-		assert(battle.p1.active[0].status === '');
+		assert.strictEqual(battle.p1.active[0].status, '');
 	});
 });
