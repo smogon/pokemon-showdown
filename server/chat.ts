31bf6b15fa9f5cdfe075e2a0c6dfd6de9717b85c
Guangcong Luo <guangcongluo@gmail.com>
2020-11-01 01:59:57 +0000

    Improve "click here" error message

--- server/chat.ts
+++ server/chat.ts
@@ -1215,7 +1215,7 @@ export class CommandContext extends MessageContext {
 		htmlContent = ('' + (htmlContent || '')).trim();
 		if (!htmlContent) return '';
 		if (/>here.?</i.test(htmlContent) || /click here/i.test(htmlContent)) {
-			throw new Chat.ErrorMessage('Do not use "click here"');
+			throw new Chat.ErrorMessage('Do not use "click here" – See [[Design standard #2 <https://github.com/smogon/pokemon-showdown/blob/master/CONTRIBUTING.md#design-standards>]]');
 		}
 
 		// check for mismatched tags
