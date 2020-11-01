72aac0d719892b0ced57096bc0978b9cf11b95f5
Spandan Punwatkar <spandan.punwatkar@gmail.com>
2020-11-02 03:49:12 +0530

    Update Kev (#428)
    * Update Kev
    
    * Update abilities.ts
    
    * Update abilities.ts

--- data/mods/ssb/abilities.ts
+++ data/mods/ssb/abilities.ts
@@ -1117,23 +1117,11 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
 
 	// Kev
 	kingofatlantis: {
-		shortDesc: "Primordial Sea + Dry Skin.",
+		shortDesc: "Drizzle + Dry Skin; +1 turn of rain for every water type teammate.",
 		onStart(source) {
-			this.field.setWeather('primordialsea');
-		},
-		onAnySetWeather(target, source, weather) {
-			if (this.field.getWeather().id === 'primordialsea' && !STRONG_WEATHERS.includes(weather.id)) return false;
-		},
-		onEnd(pokemon) {
-			if (this.field.weatherData.source !== pokemon) return;
-			for (const target of this.getAllActive()) {
-				if (target === pokemon) continue;
-				if (target.hasAbility(HEAVY_RAIN_ABILITIES)) {
-					this.field.weatherData.source = target;
-					return;
-				}
-			}
-			this.field.clearWeather();
+			const drizzle = this.dex.deepClone(this.dex.deepClone('raindance'));
+			for (const teammate of source.side.pokemon) if (teammate.hasType('Water') && teammate !== source) drizzle.duration++;
+			this.field.setWeather(drizzle);
 		},
 		onTryHit(target, source, move) {
 			if (target !== source && move.type === 'Water') {
