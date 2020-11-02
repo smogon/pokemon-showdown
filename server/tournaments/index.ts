90667e52354fa6cc0695694ffc4a4a162704d0ed
Mia <49593536+mia-pi-git@users.noreply.github.com>
2020-11-02 10:42:33 -0600

    Tournaments: Start timer in all battles when forcetimer is activated (#7624)

--- server/tournaments/index.ts
+++ server/tournaments/index.ts
@@ -1804,6 +1804,15 @@ const commands: ChatCommands = {
 			const option = target ? target.toLowerCase() : 'on';
 			if (this.meansYes(option)) {
 				tournament.forceTimer = true;
+				for (const player of tournament.players) {
+					const curMatch = player.inProgressMatch;
+					if (curMatch) {
+						const battle = curMatch.room.battle;
+						if (battle) {
+							battle.timer.start();
+						}
+					}
+				}
 				room.add('Forcetimer is now on for the tournament.');
 				this.privateModAction(`The timer was turned on for the tournament by ${user.name}`);
 				this.modlog('TOUR FORCETIMER', null, 'ON');
