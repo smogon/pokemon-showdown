001c9c43ff23edff78fb7ac250f1f5e1be9d5da6
May Evans <36418502+PlagueVonKarma@users.noreply.github.com>
2020-11-12 02:56:06 +0000

    Fix Gen 1 Psywave mechanics (#7685)

--- data/mods/stadium/moves.ts
+++ data/mods/stadium/moves.ts
@@ -55,6 +55,13 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 			},
 		},
 	},
+	psywave: {
+		inherit: true,
+		basePower: 1,
+		damageCallback(pokemon) {
+			return this.random(1, this.trunc(1.5 * pokemon.level));
+		},
+	},
 	rage: {
 		inherit: true,
 		self: {
