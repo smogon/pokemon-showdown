0396a231b86f6e13cae7a8cc6907c065f1c7697f
TheMezStrikes <TheMezStrikes@gmail.com>
2020-11-12 16:44:18 +0100

    Update CAP samples link (#7689)

--- server/chat-commands/info.ts
+++ server/chat-commands/info.ts
@@ -1789,7 +1789,7 @@ export const commands: ChatCommands = {
 			`- <a href="https://www.smogon.com/cap/">CAP project website and description</a><br />` +
 			`- <a href="https://www.smogon.com/forums/threads/48782/">What Pok&eacute;mon have been made?</a><br />` +
 			`- <a href="https://www.smogon.com/forums/forums/477">Talk about the metagame here</a><br />` +
-			`- <a href="https://www.smogon.com/forums/threads/3662655/">Sample SS CAP teams</a>`
+			`- <a href="https://www.smogon.com/forums/threads/3671157/">Sample SS CAP teams</a>`
 		);
 	},
 	caphelp: [
