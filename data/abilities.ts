ad70206599b2fa80625718640751171b07386846
Marty-D <Marty-D@users.noreply.github.com>
2020-11-21 14:15:14 -0500

    Update Parental Bond excluded moves

--- data/abilities.ts
+++ data/abilities.ts
@@ -2438,7 +2438,7 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 	parentalbond: {
 		onPrepareHit(source, target, move) {
 			if (move.category === 'Status' || move.selfdestruct || move.multihit) return;
-			if (['iceball', 'rollout'].includes(move.id)) return;
+			if (['endeavor', 'fling', 'iceball', 'rollout'].includes(move.id)) return;
 			if (!move.flags['charge'] && !move.spreadHit && !move.isZ && !move.isMax) {
 				move.multihit = 2;
 				move.multihitType = 'parentalbond';
