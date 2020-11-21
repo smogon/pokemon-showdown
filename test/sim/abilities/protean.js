da11028dedf24ba144625d2fd98f5f962479f580
The Immortal <the_immortal123@live.com>
2020-11-21 11:14:34 +0400

    Add Protean tests

---
+++ test/sim/abilities/protean.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const assert = require('./../../assert');
+const common = require('./../../common');
+
+let battle;
+
+describe('Protean', function () {
+	afterEach(function () {
+		battle.destroy();
+	});
+
+	it('should change the user\'s type when using a move', function () {
+		battle = common.createBattle();
+		battle.setPlayer('p1', {team: [{species: "Cinderace", ability: 'protean', moves: ['highjumpkick']}]});
+		battle.setPlayer('p2', {team: [{species: "Gengar", moves: ['sleeptalk']}]});
+		battle.makeChoices('move highjumpkick', 'move sleeptalk');
+		assert(battle.p1.active[0].hasType('Fighting'));
+	});
+
+	it('should not change the user\'s type using Fling with no item', function () {
+		battle = common.createBattle();
+		battle.setPlayer('p1', {team: [{species: "Kecleon", ability: 'protean', moves: ['fling']}]});
+		battle.setPlayer('p2', {team: [{species: "Wobbuffet", moves: ['counter']}]});
+		battle.makeChoices('move fling', 'move counter');
+		assert(battle.p1.active[0].hasType('Normal'));
+	});
+});
