0e8b68dbd0acdacac414550d2bd7f2c8bab78be8
Adam Tran <aviettran@gmail.com>
2020-11-30 12:59:53 -0500

    Fix Max Guard bypass with multiple targets (#7739)

--- test/sim/moves/maxguard.js
+++ test/sim/moves/maxguard.js
@@ -34,20 +34,27 @@ describe('Max Guard', function () {
 		assert.false.fainted(minun);
 	});
 
-	it.skip('should block certain moves that bypass Protect', function () {
+	it('should block certain moves that bypass Protect', function () {
 		battle = common.createBattle({gameType: 'doubles'}, [[
-			{species: 'minun', ability: 'plus', moves: ['sleeptalk']},
-			{species: 'plusle', moves: ['magneticflux']},
+			{species: 'sunflora', item: 'sitrusberry', ability: 'minus', moves: ['sleeptalk']},
+			{species: 'plusle', ability: 'plus', moves: ['magneticflux', 'gearup']},
 		], [
-			{species: 'wynaut', item: 'powerherb', moves: ['phantomforce']},
-			{species: 'wynaut', ability: 'intrepidsword', moves: ['psychup']},
+			{species: 'ferrothorn', item: 'sitrusberry', moves: ['flowershield', 'teatime']},
+			{species: 'sunflora', ability: 'intrepidsword', moves: ['psychup', 'sleeptalk']},
 		]]);
-		battle.makeChoices('move sleeptalk dynamax, move magneticflux', 'move phantomforce 1, move psychup 1');
 
-		const minun = battle.p1.active[0];
-		assert.statStage(minun, 'def', 0);
-		assert.statStage(minun, 'spd', 0);
-		assert.fullHP(minun);
+		battle.makeChoices('move sleeptalk dynamax, move magneticflux', 'move flowershield, move psychup 1');
+		const sunflora = battle.p1.active[0];
+		assert.statStage(sunflora, 'def', 0, 'should block magneticflux and flowershield');
+		assert.statStage(sunflora, 'spd', 0, 'should block magneticflux');
+		assert.statStage(battle.p2.active[1], 'atk', 1, 'should not have changed from +1 (intrepidsword) to 0 (psychup)');
+		assert.match(battle.log[battle.lastMoveLine + 1], /^|-activate.*move: Max Guard$/, 'should log that maxguard has activated');
+
+		battle.p1.active[0].volatiles.stall.counter = 1;
+		battle.makeChoices('move sleeptalk, move gearup', 'move teatime, move sleeptalk');
+		assert.statStage(sunflora, 'atk', 0, 'should block gearup');
+		assert.statStage(sunflora, 'spa', 0, 'should block gearup');
+		assert.equal(sunflora.item, 'sitrusberry', 'should block teatime');
 		assert.statStage(battle.p2.active[1], 'atk', 1);
 	});
 });
