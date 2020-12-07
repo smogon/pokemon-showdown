f23f565980f1583f6ad5237ab79725d4cbe9c510
Adam Tran <aviettran@gmail.com>
2020-12-06 21:05:56 -0500

    typo

--- test/sim/moves/sleeptalk.js
+++ test/sim/moves/sleeptalk.js
@@ -16,7 +16,7 @@ describe('Sleep Talk', function () {
 		battle.setPlayer('p2', {team: [{species: 'breloom', moves: ['spore', 'gravity']}]});
 		battle.makeChoices('move sleeptalk', 'move gravity');
 		battle.makeChoices('move sleeptalk', 'move spore');
-		assert.fullHP(battle.p1.active[0]);
+		assert.fullHP(battle.p2.active[0]);
 		assert.match(battle.log[battle.lastMoveLine + 1], /^\|cant.*move: Gravity|High Jump Kick$/, 'should log that High Jump Kick failed');
 	});
 });
