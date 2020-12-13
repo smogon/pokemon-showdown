015423703b5e61ba2f2aaf68435fe769cd40ae52
Instruct <66930476+xInstruct@users.noreply.github.com>
2020-12-13 13:21:51 -0500

    oops missed a spot

--- config/formats.ts
+++ config/formats.ts
@@ -1400,7 +1400,7 @@ export const Formats: FormatList = [
 			// Requires client change
 			this.add('raw|SUPER STAFF BROS!!');
 			this.add('message', 'GET READY FOR THE NEXT BATTLE!');
-			this.add(`raw|<div class='broadcast-green'><b>Wondering what all these custom moves, abilities, and items do?<br />Check out the <a href="https://www.smogon.com/articles/super-staff-bros-brawl" target="_blank">Super Staff Bros 4 Guide</a> or use /ssb to find out!</b></div>`);
+			this.add(`raw|<div class='broadcast-green'><b>Wondering what all these custom moves, abilities, and items do?<br />Check out the <a href="https://www.smogon.com/articles/super-staff-bros-4" target="_blank">Super Staff Bros 4 Guide</a> or use /ssb to find out!</b></div>`);
 		},
 		onSwitchInPriority: 100,
 		onSwitchIn(pokemon) {
