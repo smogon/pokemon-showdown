687088519d9e43ecfb8c2e2be84b2330a6571933
Distrib <theodelhay@orange.fr>
2020-12-15 11:58:57 +0100

    Update poll.ts

--- server/chat-plugins/poll.ts
+++ server/chat-plugins/poll.ts
@@ -537,28 +537,28 @@ export const commands: ChatCommands = {
 		},
 		displayhelp: [`/poll display - Displays the poll`],
 	},
-		pollhelp() {
-			this.sendReply(
-				`|html|<details class="readmore"><summary>Poll help:</summary>` +
-				`<p>/poll allows rooms to run their own polls. These polls are limited to one poll at a time per room.<br />` +
-			  	`<p>Polls can be used as quiz questions. To do this, prepend all correct answers with a +.<br />` +
-			  	`<p>Accepts the following commands:<br />` +
-			  	`<p><code>/poll create [question], [option1], [option2], [...]</code> - Creates a poll. Requires: % @ # &<br />` +
-			  	`<p><code>/poll createmulti [question], [option1], [option2], [...]</code> - Creates a poll, allowing for multiple answers to be selected. Requires: % @ # &<br />` +
-			  	`<p><code>/poll htmlcreate(multi) [question], [option1], [option2], [...] </code>- Creates a poll, with HTML allowed in the question and options. Requires: # &<br />` +
-			  	`<p><code>/poll vote [number]</code> - Votes for option [number].<br />` +
-			  	`<p><code>/poll timer [minutes]</code> - Sets the poll to automatically end after [minutes]. Requires: % @ # &.<br />` +
-			  	`<p><code>/poll results </code>- Shows the results of the poll without voting. NOTE: you can't go back and vote after using this.<br />`+
-			  	`<p><code>/poll display</code> - Displays the poll.<br />` +
-			  	`<p><code>/poll end </code>- Ends a poll and displays the results. Requires: % @ # &.<br />` +
-			  	`<p><code>/poll queue [question], [option1], [option2], [...]</code> - Add a poll in queue. Requires: % @ # &<br />` +
-			  	`<p><code>/poll deletequeue [number]</code> - Deletes poll at the corresponding queue slot (1 = next, 2 = the one after that, etc).<br />` +
-				`<p><code>/poll clearqueue </code>- Deletes the queue of polls. Requires: % @ # &.<br />` +
-			  	`<p><code>/poll viewqueue</code> - View the queue of polls in the room. Requires: % @ # &<br />` +
-				`</details>`
-			);
-		},
-	};
+	pollhelp() {
+		this.sendReply(
+			`|html|<details class="readmore"><summary>Poll help:</summary>` +
+			`<p>/poll allows rooms to run their own polls. These polls are limited to one poll at a time per room.<br />` +
+			`<p>Polls can be used as quiz questions. To do this, prepend all correct answers with a +.<br />` +
+			`<p>Accepts the following commands:<br />` +
+			`<p><code>/poll create [question], [option1], [option2], [...]</code> - Creates a poll. Requires: % @ # &<br />` +
+			`<p><code>/poll createmulti [question], [option1], [option2], [...]</code> - Creates a poll, allowing for multiple answers to be selected. Requires: % @ # &<br />` +
+			`<p><code>/poll htmlcreate(multi) [question], [option1], [option2], [...] </code>- Creates a poll, with HTML allowed in the question and options. Requires: # &<br />` +
+			`<p><code>/poll vote [number]</code> - Votes for option [number].<br />` +
+			`<p><code>/poll timer [minutes]</code> - Sets the poll to automatically end after [minutes]. Requires: % @ # &.<br />` +
+			`<p><code>/poll results </code>- Shows the results of the poll without voting. NOTE: you can't go back and vote after using this.<br />` +
+			`<p><code>/poll display</code> - Displays the poll.<br />` +
+			`<p><code>/poll end </code>- Ends a poll and displays the results. Requires: % @ # &.<br />` +
+			`<p><code>/poll queue [question], [option1], [option2], [...]</code> - Add a poll in queue. Requires: % @ # &<br />` +
+			`<p><code>/poll deletequeue [number]</code> - Deletes poll at the corresponding queue slot (1 = next, 2 = the one after that, etc).<br />` +
+			`<p><code>/poll clearqueue </code>- Deletes the queue of polls. Requires: % @ # &.<br />` +
+			`<p><code>/poll viewqueue</code> - View the queue of polls in the room. Requires: % @ # &<br />` +
+			`</details>`
+		);
+	},
+};
 
 export const pages: PageTable = {
 	pollqueue(args, user) {
