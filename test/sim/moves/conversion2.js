17dbb7b114d49d6b934144435b0fd0e0f8888130
Adam Tran <aviettran@gmail.com>
2020-12-15 21:16:09 -0500

    Fix Conversion 2 Interaction with Submoves

---
+++ test/sim/moves/conversion2.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const assert = require('./../../assert');
+const common = require('./../../common');
+
+let battle;
+
+describe('Conversion2', function () {
+	afterEach(function () {
+		battle.destroy();
+	});
+
+	it('should change users type to resist', function () {
+		battle = common.createBattle([
+			[{species: 'porygon2', moves: ['sleeptalk', 'conversion2', 'spore']}],
+			[{species: 'raticate', moves: ['tackle']},
+			 {species: 'zapdos', moves: ['thundershock', 'sleeptalk']}],
+		]);
+
+		battle.makeChoices('move conversion2', 'move tackle');
+		assert(['Rock', 'Ghost', 'Steel'].includes(battle.p1.active[0].getTypes()[0]));
+		battle.makeChoices('move spore', 'switch 2');
+		battle.makeChoices('move conversion2', 'move sleeptalk');
+		assert(['Electric', 'Grass', 'Ground', 'Dragon'].includes(battle.p1.active[0].getTypes()[0]), 'should change type based on submove');
+	});
+});
