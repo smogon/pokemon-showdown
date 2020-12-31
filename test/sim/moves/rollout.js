b000494214ee5c13ac9b931718d5e6204ae57041
Leonard Craft III <leonardcraft64@gmail.com>
2020-12-30 19:27:59 -0600

    Add Rollout Storage tests

--- test/sim/moves/rollout.js
+++ test/sim/moves/rollout.js
@@ -129,5 +129,55 @@ for (const move of moves) {
 			battle.makeChoices('move ' + id, 'move recover');
 			assert.equal(hitCount, 1);
 		});
+
+		describe.skip(`Rollout Storage glitch (Gen 7 / Gen 8DLC1)`, function () {
+			it(`should delay the Rollout multiplier when hitting Disguise or Ice Face`, function () {
+				battle = common.gen(7).createBattle([[
+					{species: 'wynaut', ability: 'compoundeyes', ivs: {atk: '0'}, nature: 'bold', moves: [id, 'watergun']},
+				], [
+					{species: 'mimikyu', ability: 'disguise', evs: {hp: '252', def: '252'}, nature: 'bold', moves: ['gravity']},
+					{species: 'snorlax', moves: ['sleeptalk']},
+				]]);
+
+				for (let i = 0; i < 5; i++) { battle.makeChoices(); }
+				battle.makeChoices('move watergun', 'switch 2');
+				const snorlax = battle.p2.active[0];
+				const damage = snorlax.maxhp - snorlax.hp;
+				assert.bounded(damage, [147, 174]); // 40 * 2^4 BP; would be 40 BP otherwise, range 10-12
+			});
+
+			it(`should delay the Rollout multiplier when hitting multiple Disguise or Ice Face`, function () {
+				battle = common.gen(7).createBattle([[
+					{species: 'wynaut', ability: 'compoundeyes', ivs: {atk: '0'}, nature: 'bold', moves: [id, 'watergun']},
+				], [
+					{species: 'mimikyu', ability: 'disguise', evs: {hp: '252', def: '252'}, nature: 'bold', moves: ['gravity']},
+					{species: 'mimikyu', ability: 'disguise', evs: {hp: '252', def: '252'}, nature: 'bold', moves: ['gravity']},
+					{species: 'snorlax', moves: ['sleeptalk']},
+				]]);
+
+				battle.makeChoices();
+				battle.makeChoices('auto', 'switch 2');
+				for (let i = 0; i < 3; i++) { battle.makeChoices(); }
+				battle.makeChoices('move watergun', 'switch 3');
+				const snorlax = battle.p2.active[0];
+				const damage = snorlax.maxhp - snorlax.hp;
+				assert.bounded(damage, [74, 88]); // 40 * 2^3 BP; would be 40 BP otherwise, range 10-12
+			});
+
+			it(`should use the move's default BP when applying the modifier`, function () {
+				battle = common.gen(7).createBattle([[
+					{species: 'wynaut', ability: 'compoundeyes', ivs: {atk: '0'}, nature: 'bold', moves: [id, 'grassknot']},
+				], [
+					{species: 'mimikyu', ability: 'disguise', evs: {hp: '252', def: '252'}, nature: 'bold', moves: ['gravity']},
+					{species: 'snorlax', moves: ['sleeptalk']},
+				]]);
+
+				for (let i = 0; i < 5; i++) { battle.makeChoices(); }
+				battle.makeChoices('move grassknot', 'switch 2');
+				const snorlax = battle.p2.active[0];
+				const damage = snorlax.maxhp - snorlax.hp;
+				assert.bounded(damage, [5, 6]); // 1 * 2^4 BP; would be 120 BP otherwise, range 28-34
+			});
+		});
 	});
 }
