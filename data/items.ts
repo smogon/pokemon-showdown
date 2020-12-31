8b86eec2ba834c76c1aef9255178b524882fd3f8
Marty-D <Marty-D@users.noreply.github.com>
2020-12-31 15:23:38 -0500

    Fix Eject Button interaction with Pursuit

--- data/items.ts
+++ data/items.ts
@@ -1396,7 +1396,7 @@ export const Items: {[itemid: string]: ItemData} = {
 		onAfterMoveSecondaryPriority: 2,
 		onAfterMoveSecondary(target, source, move) {
 			if (source && source !== target && target.hp && move && move.category !== 'Status') {
-				if (!this.canSwitch(target.side) || target.forceSwitchFlag) return;
+				if (!this.canSwitch(target.side) || target.forceSwitchFlag || target.beingCalledBack) return;
 				for (const pokemon of this.getAllActive()) {
 					if (pokemon.switchFlag === true) return;
 				}
