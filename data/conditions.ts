fbda1e5d92151d565f028502cf426971583ed983
Adam Tran <aviettran@gmail.com>
2021-01-03 09:42:08 -0500

    Fix Protean/Libero on first of two turn moves (#7870)

--- data/conditions.ts
+++ data/conditions.ts
@@ -279,6 +279,8 @@ export const Conditions: {[k: string]: ConditionData} = {
 			// if there are no valid targets, randomly choose one later
 			target.volatiles[effect.id].targetLoc = this.getTargetLoc(moveTarget || target, target);
 			this.attrLastMove('[still]');
+			// Run side-effects normally associated with hitting (e.g., Protean, Libero)
+			this.runEvent('PrepareHit', target, source, effect);
 		},
 		onEnd(target) {
 			target.removeVolatile(this.effectData.move);
