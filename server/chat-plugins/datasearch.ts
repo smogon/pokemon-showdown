6597ef0f15eeb338d89084f343d12afc18b5af4a
PartMan <47669599+PartMan7@users.noreply.github.com>
2021-01-04 01:12:27 +0530

    Add multi-hit as a parameter to /ms (#7855)
    * Add multi-hit as a parameter to /ms
    
    * Revert /help ds, add multihit to /help ms, fix TS
    
    Code linted, let's go
    'Tis coding at midnight
    Save what had been tossed...
    Bring back the older line...
    The older line.
    
    * ;-;
    
      1465 passing (25s)
      53 pending
    
    
    > pokemon-showdown@0.11.4 posttest /home/partman/Documents/Code/pokemon-showdown
    > npm run tsc
    
    
    > pokemon-showdown@0.11.4 tsc /home/partman/Documents/Code/pokemon-showdown
    > tsc
    
    
    No idea why this wasn't caught.
    
    * Fingers crossed

--- server/chat-plugins/datasearch.ts
+++ server/chat-plugins/datasearch.ts
@@ -46,6 +46,7 @@ interface MoveOrGroup {
 	recoil: boolean;
 	skip: boolean;
 	pivot: boolean;
+	multihit: boolean;
 }
 
 type Direction = 'less' | 'greater' | 'equal';
@@ -292,7 +293,7 @@ export const commands: ChatCommands = {
 			`Inequality ranges use the characters <code>></code> and <code><</code>.<br/>` +
 			`Parameters can be excluded through the use of <code>!</code>; e.g., <code>!water type</code> excludes all Water-type moves.<br/>` +
 			`<code>asc</code> or <code>desc</code> following a move property will arrange the names in ascending or descending order of that property, respectively; e.g., <code>basepower asc</code> will arrange moves in ascending order of their base powers.<br/>` +
-			`Valid flags are: authentic (bypasses substitute), bite, bullet, charge, contact, dance, defrost, gravity, mirror (reflected by mirror move), ohko, powder, priority, protect, pulse, punch, recharge, recovery, reflectable, secondary, snatch, sound, zmove and pivot.<br/>` +
+			`Valid flags are: authentic (bypasses substitute), bite, bullet, charge, contact, dance, defrost, gravity, mirror (reflected by mirror move), ohko, powder, priority, protect, pulse, punch, recharge, recovery, reflectable, secondary, snatch, sound, zmove, pivot, and multi-hit.<br/>` +
 			`A search that includes <code>!protect</code> will show all moves that bypass protection.<br/>` +
 			`<code>protection</code> as a parameter will search protection moves like Protect, Detect, etc.<br/>` +
 			`<code>max</code> or <code>gmax</code> as parameters will search for Max Moves and G-Max moves respectively.<br/>` +
@@ -1178,7 +1179,7 @@ function runMovesearch(target: string, cmd: string, canAll: boolean, message: st
 	const allProperties = ['basePower', 'accuracy', 'priority', 'pp'];
 	const allFlags = [
 		'authentic', 'bite', 'bullet', 'charge', 'contact', 'dance', 'defrost', 'gravity', 'highcrit', 'mirror',
-		'ohko', 'powder', 'protect', 'pulse', 'punch', 'recharge', 'reflectable', 'secondary',
+		'multihit', 'ohko', 'powder', 'protect', 'pulse', 'punch', 'recharge', 'reflectable', 'secondary',
 		'snatch', 'sound', 'zmove', 'maxmove', 'gmaxmove', 'protection',
 	];
 	const allStatus = ['psn', 'tox', 'brn', 'par', 'frz', 'slp'];
@@ -1214,7 +1215,8 @@ function runMovesearch(target: string, cmd: string, canAll: boolean, message: st
 	for (const arg of target.split(',')) {
 		const orGroup: MoveOrGroup = {
 			types: {}, categories: {}, contestTypes: {}, flags: {}, gens: {}, recovery: {}, mon: {}, property: {},
-			boost: {}, lower: {}, zboost: {}, status: {}, volatileStatus: {}, targets: {}, recoil: false, skip: false, pivot: false,
+			boost: {}, lower: {}, zboost: {}, status: {}, volatileStatus: {}, targets: {}, recoil: false, skip: false,
+			pivot: false, multihit: false,
 		};
 		const parameters = arg.split("|");
 		if (parameters.length > 3) return {error: "No more than 3 alternatives for each parameter may be used."};
@@ -1288,6 +1290,7 @@ function runMovesearch(target: string, cmd: string, canAll: boolean, message: st
 			if (target === 'z') target = 'zmove';
 			if (target === 'max') target = 'maxmove';
 			if (target === 'gmax') target = 'gmaxmove';
+			if (target === 'multi' || toID(target) === 'multihit') target = 'multihit';
 			if (target === 'crit' || toID(target) === 'highcrit') target = 'highcrit';
 			if (allFlags.includes(target)) {
 				if ((orGroup.flags[target] && isNotSearch) || (orGroup.flags[target] === false && !isNotSearch)) {
@@ -1386,6 +1389,15 @@ function runMovesearch(target: string, cmd: string, canAll: boolean, message: st
 					orGroup.pivot = true;
 				} else if ((orGroup.pivot && isNotSearch) || (!orGroup.pivot && !isNotSearch)) {
 					return {error: 'A search cannot both exclude and include pivot moves.'};
+				}
+				continue;
+			}
+
+			if (target === 'multihit') {
+				if (!orGroup.multihit) {
+					orGroup.multihit = true;
+				} else if ((orGroup.multihit && isNotSearch) || (!orGroup.multihit && !isNotSearch)) {
+					return {error: 'A search cannot both exclude and include multi-hit moves.'};
 				}
 				continue;
 			}
@@ -1675,6 +1687,11 @@ function runMovesearch(target: string, cmd: string, canAll: boolean, message: st
 				} else if (flag === 'highcrit') {
 					const crit = move.willCrit || (move.critRatio && move.critRatio > 1);
 					if (!crit === !alts.flags[flag]) {
+						matched = true;
+						break;
+					}
+				} else if (flag === 'multihit') {
+					if (!move.multihit === !alts.flags[flag]) {
 						matched = true;
 						break;
 					}
