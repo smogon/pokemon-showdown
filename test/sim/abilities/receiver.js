436c60b9f699a2f695ab8569b910605e486c3c88
Leonard Craft III <leonardcraft64@gmail.com>
2021-01-03 20:55:04 -0600

    Add additional Receiver and Soul-Heart tests

--- test/sim/abilities/receiver.js
+++ test/sim/abilities/receiver.js
@@ -22,4 +22,20 @@ describe(`Receiver`, function () {
 		const passimian = battle.p1.active[1];
 		assert.statStage(passimian, 'spa', 1);
 	});
+
+	it.skip(`should do weird stuff with multiple Soul-Heart and multiple Receiver`, function () {
+		battle = common.createBattle({gameType: 'doubles'}, [[
+			{species: 'Passimian', ability: 'receiver', moves: ['earthquake']},
+			{species: 'Magearna', ability: 'soulheart', level: 1, moves: ['sleeptalk']},
+		], [
+			{species: 'Lugia', ability: 'receiver', moves: ['sleeptalk']},
+			{species: 'Wynaut', ability: 'soulheart', level: 1, moves: ['sleeptalk']},
+		]]);
+
+		battle.makeChoices();
+		const passimian = battle.p1.active[0];
+		const lugia = battle.p2.active[0];
+		assert.statStage(passimian, 'spa', 2);
+		assert.statStage(lugia, 'spa', 1);
+	});
 });
