c78c988636c2857504733fd09827376d2393e6de
Leonard Craft III <leonardcraft64@gmail.com>
2021-01-03 22:30:49 -0600

    Add Shell Bell multihit test

--- test/sim/items/shellbell.js
+++ test/sim/items/shellbell.js
@@ -22,4 +22,21 @@ describe('Shell Bell', function () {
 		const landorus = battle.p1.active[1];
 		assert.equal(landorus.hp, landorus.maxhp - Math.floor(landorus.maxhp / 2) + (Math.floor(11 * 2 / 8)));
 	});
+
+	// Also applies to Parental Bond
+	it.skip(`should heal from the damage from all hits of multi-hit moves`, function () {
+		battle = common.createBattle([[
+			{species: 'shelmet', moves: ['finalgambit']},
+			{species: 'landorus', moves: ['sleeptalk']},
+			{species: 'wynaut', moves: ['sleeptalk']},
+		], [
+			{species: 'cloyster', ability: 'skilllink', item: 'shellbell', evs: {hp: 4}, moves: ['sleeptalk', 'iciclespear']},
+		]]);
+		battle.makeChoices();
+		battle.makeChoices('switch 2');
+		battle.makeChoices('auto', 'move iciclespear');
+		const landorus = battle.p1.active[0];
+		const cloyster = battle.p2.active[0];
+		assert.equal(cloyster.hp, 1 + Math.floor(landorus.maxhp / 8));
+	});
 });
