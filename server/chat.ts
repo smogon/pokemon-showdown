5e200392200c6521bff5a7d681d760ed80f27923
Annika <annika0uwu@gmail.com>
2021-01-07 13:10:23 -0800

    Roomlog: Support getting total linecount (#7885)

--- server/chat.ts
+++ server/chat.ts
@@ -1610,11 +1610,11 @@ export const Chat = new class {
 	parse(message: string, room: Room | null | undefined, user: User, connection: Connection) {
 		Chat.loadPlugins();
 
-		const initialRoomlogLength = room?.log.log.length;
+		const initialRoomlogLength = room?.log.getLineCount();
 		const context = new CommandContext({message, room, user, connection});
 		const result = context.parse();
 
-		if (room && room.log.log.length !== initialRoomlogLength) {
+		if (room && room.log.getLineCount() !== initialRoomlogLength) {
 			room.messagesSent++;
 			for (const [handler, numMessages] of room.nthMessageHandlers) {
 				if (room.messagesSent % numMessages === 0) handler(room, message);
