919fff7ee13afdea3dc09d1e1292af69fb108093
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2021-01-08 05:32:28 -0700

    Update rulesets.ts
    Minor fixes

--- data/mods/m4av7/rulesets.ts
+++ data/mods/m4av7/rulesets.ts
@@ -43,12 +43,11 @@ export const Formats: {[k: string]: FormatData} = {
 		desc: 'At the start of a battle, gives each player information about the potential Mega in their party', 
 		onBegin() {
 			this.add('-message', 'Your Mega Evolution this match is:'); 
-			let allPokemon = this.p1.pokemon.concat(this.p2.pokemon); 
-				for (let pokemon of allPokemon) {
-					const item = this.dex.getItem(pokemon.item)
+			const allPokemon = this.p1.pokemon.concat(this.p2.pokemon); 
+				for (const pokemon of allPokemon) {
 					if (pokemon.canMegaEvo) {
 							
-							let mega = this.dex.getSpecies(pokemon.canMegaEvo); 
+							const mega = this.dex.getSpecies(pokemon.canMegaEvo); 
 							const baseStats = mega.baseStats; 	
 							let types = new String(mega.types[0]); 
 							if (mega.types[1]) {
@@ -56,7 +55,9 @@ export const Formats: {[k: string]: FormatData} = {
 							}
 							let msg = new String(" Spe"); 
 							if (mega.name === "Mimikyu-Mega") {
-								msg = msg.concat("; Mega Mimikyu has two forms! If its Disguise is busted, it will Mega Evolve into Mimikyu-Busted-Mega. /dt for info"); 
+								msg = msg.concat(
+									"; Mega Mimikyu has two forms! If its Disguise is busted, it will Mega Evolve into Mimikyu-Busted-Mega. /dt for info"
+								); 
 							}
 							let txt = new String(mega.name + " (" + types +
 							"); Ability: " + mega.abilities[0] + " (" + this.dex.getAbility(mega.abilities[0]).shortDesc +
