e16e3a92ddcd172c8ea5fe43c4a0de476f5d5311
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2021-01-24 03:22:12 -0700

    Update abilities.ts

--- data/mods/megasforall/abilities.ts
+++ data/mods/megasforall/abilities.ts
@@ -1598,8 +1598,9 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 		shortDesc: "This Pokémon moves first in its priority bracket when its target has 1/2 or less HP.",
 		onUpdate(pokemon) {
 			const action = this.queue.willMove(pokemon);
+			if (!action) return;
 			const target = this.getTarget(action.pokemon, action.move, action.targetLoc);
-			if (!action || !target) return;
+			if (!target) return;
 			if (!action.move.spreadHit && target.hp <= target.maxhp / 2) {
 				pokemon.addVolatile('coupdegrass');
 			}
@@ -1629,7 +1630,6 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 		},
 		condition: {
 			onStart(pokemon) {
-				console.log(`Masquerade started on ${pokemon.name}`);
 				pokemon.masquerade = null;
 				let i;
 				for (i = pokemon.side.pokemon.length - 1; i > pokemon.position; i--) {
@@ -1639,36 +1639,30 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 				if (!pokemon.side.pokemon[i]) return;
 				if (pokemon === pokemon.side.pokemon[i]) return;
 				pokemon.masquerade = pokemon.side.pokemon[i];
-				console.log(`${pokemon.name} is going to masquerade as ${pokemon.masquerade.name}`);
 				const additionalBannedAbilities = [
 					'noability', 'flowergift', 'forecast', 'hungerswitch', 'illusion', 'imposter', 'neutralizinggas', 'powerofalchemy', 'receiver', 'trace', 'wonderguard',
 				];
 				if (pokemon.masquerade.getAbility().isPermanent || additionalBannedAbilities.includes(pokemon.masquerade.ability)) {
-					console.log(`${pokemon.name} can't masquerade because ${pokemon.masquerade.name}'s Ability is ${pokemon.masquerade.ability}`);
 					pokemon.setAbility('masquerade');
 					pokemon.removeVolatile('masquerade');
 					return;
 				}
-				console.log(`${pokemon.name} inherited ${pokemon.masquerade.ability}`);
 				pokemon.setAbility(pokemon.masquerade.ability);
 				this.add('-ability', pokemon, 'Masquerade');
 				this.add('-message', `${pokemon.name} inherited ${this.dex.getAbility(pokemon.ability).name} from ${pokemon.masquerade.name}!`);
 				this.add('-ability', pokemon, this.dex.getAbility(pokemon.ability).name);
-				console.log(`${pokemon.name}'s Ability is ${pokemon.ability}`);
 			},
 			onDamagingHit(damage, target, source, move) {
 				target.setAbility('masquerade');
 				target.removeVolatile('masquerade');
 				this.add('-ability', target, 'Masquerade');
 				this.add('-message', `${target.name}'s Masquerade wore off!`);
-				console.log(`${target.name}'s Ability is ${target.ability}`);
 			},
 			onFaint(pokemon) {
 				pokemon.setAbility('masquerade');
 				pokemon.removeVolatile('masquerade');
 				this.add('-ability', pokemon, 'Masquerade');
 				this.add('-message', `${pokemon.name}'s Masquerade wore off!`);
-				console.log(`${pokemon.name}'s Ability is ${pokemon.ability}`);
 			},
 		},
 		name: "Masquerade",
