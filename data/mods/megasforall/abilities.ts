22cfe17221dbe73bbc3b680cf6e17b859642cb18
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2021-01-24 03:36:19 -0700

    Update abilities.ts

--- data/mods/megasforall/abilities.ts
+++ data/mods/megasforall/abilities.ts
@@ -1601,7 +1601,7 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 			if (!action) return;
 			const target = this.getTarget(action.pokemon, action.move, action.targetLoc);
 			if (!target) return;
-			if (!action.move.spreadHit && target.hp <= target.maxhp / 2) {
+			if (target.hp <= target.maxhp / 2) {
 				pokemon.addVolatile('coupdegrass');
 			}
 		},
