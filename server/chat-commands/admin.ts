3c8b6c25a422bf6d5253a61ce841aaa63d88545a
Mia <49593536+mia-pi-git@users.noreply.github.com>
2021-01-28 15:00:05 -0600

    Admin: Allow dev staff to use /processes

--- server/chat-commands/admin.ts
+++ server/chat-commands/admin.ts
@@ -600,7 +600,10 @@ export const commands: ChatCommands = {
 	],
 
 	processes(target, room, user) {
-		this.checkCan('lockdown');
+		const devRoom = Rooms.get('development');
+		if (!(devRoom && Users.Auth.atLeast(devRoom.auth.getDirect(user.id), '%'))) {
+			this.checkCan('lockdown');
+		}
 
 		let buf = `<strong>${process.pid}</strong> - Main<br />`;
 		for (const manager of ProcessManager.processManagers) {
