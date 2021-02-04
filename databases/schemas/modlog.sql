509af19236f0501553bb81ff9cefba59eb80407a
Annika <annika0uwu@gmail.com>
2021-02-04 15:21:39 -0800

    Modlog: Use SQLite for searches

--- databases/schemas/modlog.sql
+++ databases/schemas/modlog.sql
@@ -20,4 +20,9 @@ CREATE TABLE alts (
     FOREIGN KEY (modlog_id) REFERENCES modlog(modlog_id)
 ) WITHOUT ROWID;
 
-PRAGMA journal_mode=WAL;
+CREATE INDEX room_index ON modlog(roomid, timestamp);
+CREATE INDEX all_fields_index ON modlog(roomid, action, userid, autoconfirmed_userid, action_taker_userid, ip, note, timestamp);
+CREATE INDEX userid_index ON modlog(roomid, userid, autoconfirmed_userid, timestamp);
+CREATE INDEX ip_index ON modlog(roomid, ip, timestamp);
+CREATE INDEX note_index ON modlog(roomid, note, timestamp);
+CREATE INDEX action_taker_index ON modlog(roomid, action_taker_userid, timestamp);
