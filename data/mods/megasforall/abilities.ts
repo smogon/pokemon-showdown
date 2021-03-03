9f1a98929154c9172c6510177c31fe8dd90f49e8
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2021-03-02 22:17:08 -0700

    Apply suggestions from code review
    Co-authored-by: Kris Johnson <11083252+KrisXV@users.noreply.github.com>

--- data/mods/megasforall/abilities.ts
+++ data/mods/megasforall/abilities.ts
@@ -1847,7 +1847,7 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 				this.effectData.busted = true;
 			},
 			onUpdate(pokemon) {
-				if (this.effectData.busted === true && pokemon.item !== 'zoroarkite') {
+				if (this.effectData.busted && pokemon.item !== 'zoroarkite') {
 					this.add('-ability', pokemon, 'Forgery');
 					if (pokemon.item) {
 						this.add('-message', `${pokemon.name}'s ${this.dex.getItem(pokemon.item).name} was destroyed!`);
@@ -1980,8 +1980,7 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 		onResidual(pokemon) {
 			if (
 				pokemon.baseSpecies.baseSpecies !== 'Wishiwashi' || pokemon.transformed || !pokemon.hp || !pokemon.activeTurns ||
-				pokemon.species.id === 'wishiwashi' || pokemon.species.id === 'wishiwashischool' ||
-				pokemon.species.id === 'wishiwashimegaschool'
+				['wishiwashi', 'wishiwashischool', 'wishiwashimegaschool'].includes(pokemon.species.id)
 			) return;
 			this.add('-activate', pokemon, 'ability: SOS');
 			this.add('-message', `${pokemon.name} called for help!`);
@@ -2001,7 +2000,12 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 			const abilities = species.abilities;
 			const baseStats = species.baseStats;
 			const type = species.types[0];
-			this.add(`raw|<ul class="utilichart"><li class="result"><span class="col pokemonnamecol" style="white-space: nowrap">` + species.name + `</span> <span class="col typecol"><img src="https://${Config.routes.client}/sprites/types/${type}.png" alt="${type}" height="14" width="32"></span> <span style="float: left ; min-height: 26px"><span class="col abilitycol">` + abilities[0] + `</span><span class="col abilitycol"></span></span><span style="float: left ; min-height: 26px"><span class="col statcol"><em>HP</em><br>` + baseStats.hp + `</span> <span class="col statcol"><em>Atk</em><br>` + baseStats.atk + `</span> <span class="col statcol"><em>Def</em><br>` + baseStats.def + `</span> <span class="col statcol"><em>SpA</em><br>` + baseStats.spa + `</span> <span class="col statcol"><em>SpD</em><br>` + baseStats.spd + `</span> <span class="col statcol"><em>Spe</em><br>` + baseStats.spe + `</span> </span></li><li style="clear: both"></li></ul>`);
+			let buf = `<ul class="utilichart"><li class="result"><span class="col pokemonnamecol" style="white-space: nowrap">${species.name}</span> <span class="col typecol"><psicon type="${type}" /></span> <span style="float: left ; min-height: 26px"><span class="col abilitycol">${abilities[0]}</span><span class="col abilitycol"></span></span><span style="float: left ; min-height: 26px">`;
+			for (const statName of ['HP', 'Atk', 'Def', 'SpA', 'SpD', 'Spe']) {
+				buf += `<span class="col statcol"><em>${statName}</em><br>${baseStats[this.toID(statName) as keyof StatsTable]}</span> `;
+			}
+			buf += `</span></li><li style="clear: both"></li></ul>`;
+			this.add(`raw|${buf}`);
 			pokemon.baseMaxhp = Math.floor(Math.floor(
 				2 * pokemon.species.baseStats['hp'] + pokemon.set.ivs['hp'] + Math.floor(pokemon.set.evs['hp'] / 4) + 100
 			) * pokemon.level / 100 + 10);
@@ -2041,12 +2045,13 @@ export const Abilities: {[abilityid: string]: ModdedAbilityData} = {
 							const abilities = species.abilities;
 							const baseStats = species.baseStats;
 							const type = species.types[0];
-							if (species.types[1]) {
-								const type2 = species.types[1];
-								this.add(`raw|<ul class="utilichart"><li class="result"><span class="col pokemonnamecol" style="white-space: nowrap">` + species.name + `</span> <span class="col typecol"><img src="https://${Config.routes.client}/sprites/types/${type}.png" alt="${type}" height="14" width="32"><img src="https://${Config.routes.client}/sprites/types/${type2}.png" alt="${type2}" height="14" width="32"></span> <span style="float: left ; min-height: 26px"><span class="col abilitycol">` + abilities[0] + `</span><span class="col abilitycol"></span></span><span style="float: left ; min-height: 26px"><span class="col statcol"><em>HP</em><br>` + baseStats.hp + `</span> <span class="col statcol"><em>Atk</em><br>` + baseStats.atk + `</span> <span class="col statcol"><em>Def</em><br>` + baseStats.def + `</span> <span class="col statcol"><em>SpA</em><br>` + baseStats.spa + `</span> <span class="col statcol"><em>SpD</em><br>` + baseStats.spd + `</span> <span class="col statcol"><em>Spe</em><br>` + baseStats.spe + `</span> </span></li><li style="clear: both"></li></ul>`);
-							} else {
-								this.add(`raw|<ul class="utilichart"><li class="result"><span class="col pokemonnamecol" style="white-space: nowrap">` + species.name + `</span> <span class="col typecol"><img src="https://${Config.routes.client}/sprites/types/${type}.png" alt="${type}" height="14" width="32"></span> <span style="float: left ; min-height: 26px"><span class="col abilitycol">` + abilities[0] + `</span><span class="col abilitycol"></span></span><span style="float: left ; min-height: 26px"><span class="col statcol"><em>HP</em><br>` + baseStats.hp + `</span> <span class="col statcol"><em>Atk</em><br>` + baseStats.atk + `</span> <span class="col statcol"><em>Def</em><br>` + baseStats.def + `</span> <span class="col statcol"><em>SpA</em><br>` + baseStats.spa + `</span> <span class="col statcol"><em>SpD</em><br>` + baseStats.spd + `</span> <span class="col statcol"><em>Spe</em><br>` + baseStats.spe + `</span> </span></li><li style="clear: both"></li></ul>`);
+							const type2 = species.types[1];
+							let buf = `<ul class="utilichart"><li class="result"><span class="col pokemonnamecol" style="white-space: nowrap">${species.name}</span> <span class="col typecol"><psicon type="${type1}" />${type2 ? `<psicon type="${type2}" />` : ''}</span> <span style="float: left ; min-height: 26px"><span class="col abilitycol">${abilities[0]}</span><span class="col abilitycol"></span></span><span style="float: left ; min-height: 26px">`;
+							for (const statName of ['HP', 'Atk', 'Def', 'SpA', 'SpD', 'Spe']) {
+								buf += `<span class="col statcol"><em>${statName}</em><br>${baseStats[this.toID(statName) as keyof StatsTable]}</span> `;
 							}
+							buf += `</span></li><li style="clear: both"></li></ul>`;
+							this.add(`raw|${buf}`);
 							this.effectData.busted = true;
 						}
 					}
