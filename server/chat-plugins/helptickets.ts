5b74eb153af5522b446f63213544b875516c9fc4
Mia <49593536+mia-pi-git@users.noreply.github.com>
2021-03-18 02:21:21 -0500

    Helptickets: Automatically save replays linked in tickets (#8120)

--- server/chat-plugins/helptickets.ts
+++ server/chat-plugins/helptickets.ts
@@ -352,6 +352,16 @@ export class HelpTicket extends Rooms.RoomGame {
 		// @ts-ignore
 		this.playerTable = null;
 	}
+	onChatMessage(message: string, user: User) {
+		const roomids = message.match(/battle-(?:[a-z0-9]+)-(?:[0-9]+)(?:-[a-z0-9]+pw)?/g);
+		if (roomids) {
+			for (const roomid of roomids) {
+				const curRoom = Rooms.get(roomid);
+				if (!curRoom || !('uploadReplay' in curRoom) || curRoom?.battle?.replaySaved) continue;
+				void curRoom.uploadReplay(user, user.connections[0], 'forpunishment');
+			}
+		}
+	}
 	static ban(user: User | ID, reason = '') {
 		const userid = toID(user);
 		const userObj = Users.get(user);
