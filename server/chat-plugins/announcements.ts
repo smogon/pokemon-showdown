2e4eaf97e75400a4f871a716c5e48c6bebdb6399
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2021-03-23 12:47:40 -0600

    Announcements: Mark formatted text as trusted

--- server/chat-plugins/announcements.ts
+++ server/chat-plugins/announcements.ts
@@ -90,7 +90,7 @@ export const commands: ChatCommands = {
 				return this.errorReply(this.tr`There is already a poll or announcement in progress in this room.`);
 			}
 
-			const source = supportHTML ? this.checkHTML(target) : Chat.formatText(target);
+			const source = supportHTML ? this.checkHTML(target) : Chat.formatText(target, true);
 
 			room.setMinorActivity(new Announcement(room, {source}));
 
