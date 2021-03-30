6a95ceb415f54597f242591089082702daf0be3a
Instruct <66930476+xInstruct@users.noreply.github.com>
2021-03-30 13:54:50 -0400

    Thanks Freudian Slip

--- data/mods/ssb/moves.ts
+++ data/mods/ssb/moves.ts
@@ -3407,7 +3407,7 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 
 	// Perish Song
 	trickery: {
-		accuracy: 90,
+		accuracy: 95,
 		basePower: 100,
 		category: "Physical",
 		desc: "Changes the target's item to something random.",
