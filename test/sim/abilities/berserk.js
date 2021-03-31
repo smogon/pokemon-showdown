d12957b9351fb87d0fa573d2f00a8e9cb9b8df2d
Marty-D <Marty-D@users.noreply.github.com>
2021-03-31 18:50:27 -0400

    Update berserk.js

--- test/sim/abilities/berserk.js
+++ test/sim/abilities/berserk.js
@@ -22,4 +22,17 @@ describe('Berserk', function () {
 		assert.statStage(drampa, 'spa', 1);
 		assert.equal(drampa.hp, Math.floor(drampa.maxhp / 2) + Math.floor(drampa.maxhp / 4));
 	});
+
+	it(`should not activate prior to healing from Sitrus Berry after a multi-hit move`, function () {
+		battle = common.createBattle([[
+			{species: "Drampa", item: 'sitrusberry', ability: 'berserk', evs: {hp: 4}, moves: ['sleeptalk']},
+		], [
+			{species: "wynaut", ability: 'parentalbond', moves: ['seismictoss']},
+		]]);
+
+		battle.makeChoices();
+		const drampa = battle.p1.active[0];
+		assert.statStage(drampa, 'spa', 0);
+		assert.equal(drampa.hp, drampa.maxhp - 200 + Math.floor(drampa.maxhp / 4));
+	});
 });
