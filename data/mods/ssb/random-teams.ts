e252b3ce00f828e1719c3cb7c498493614e4247b
Instruct <66930476+xInstruct@users.noreply.github.com>
2021-03-31 01:14:32 -0400

    Update data/mods/ssb/random-teams.ts
    Co-authored-by: Kris Johnson <11083252+KrisXV@users.noreply.github.com>

--- data/mods/ssb/random-teams.ts
+++ data/mods/ssb/random-teams.ts
@@ -891,7 +891,8 @@ export class RandomStaffBrosTeams extends RandomTeams {
 			if (depth >= 200) throw new Error(`Infinite loop in Super Staff Bros team generation.`);
 			depth++;
 			const name = this.sampleNoReplace(pool);
-			const ssbSet: SSBSet = this.format.id.includes('wiiu') ? this.dex.deepClone(afdSSBSets[name]) : this.dex.deepClone(ssbSets[name]);
+			const ssbSet: SSBSet = this.format.id.includes('wiiu') ? this.dex.deepClone(afdSSBSets[name]) :
+				this.dex.deepClone(ssbSets[name]);
 			if (ssbSet.skip) continue;
 
 			// Enforce typing limits
