cc50ed49459f01d410628ec14086567efdde1f57
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2021-04-01 03:52:36 -0600

    Add April 2021 OMotM/RoA Spotlights

---
+++ data/mods/pokebilities/abilities.ts
@@ -0,0 +1,136 @@
+export const Abilities: {[k: string]: ModdedAbilityData} = {
+	neutralizinggas: {
+		inherit: true,
+		// Ability suppression implemented in sim/pokemon.ts:Pokemon#ignoringAbility
+		onPreStart(pokemon) {
+			this.add('-ability', pokemon, 'Neutralizing Gas');
+			pokemon.abilityData.ending = false;
+			for (const target of this.getAllActive()) {
+				if (target.illusion) {
+					this.singleEvent('End', this.dex.getAbility('Illusion'), target.abilityData, target, pokemon, 'neutralizinggas');
+				}
+				if (target.volatiles['slowstart']) {
+					delete target.volatiles['slowstart'];
+					this.add('-end', target, 'Slow Start', '[silent]');
+				}
+				if (target.m.innates) {
+					for (const innate of target.m.innates) {
+						if (this.dex.getAbility(innate).isPermanent) continue;
+						target.removeVolatile('ability:' + innate);
+					}
+				}
+			}
+		},
+		onEnd(source) {
+			this.add('-end', source, 'ability: Neutralizing Gas');
+
+			// FIXME this happens before the pokemon switches out, should be the opposite order.
+			// Not an easy fix since we cant use a supported event. Would need some kind of special event that
+			// gathers events to run after the switch and then runs them when the ability is no longer accessible.
+			// (If you're tackling this, do note extreme weathers have the same issue)
+
+			// Mark this pokemon's ability as ending so Pokemon#ignoringAbility skips it
+			if (source.abilityData.ending) return;
+			source.abilityData.ending = true;
+			const sortedActive = this.getAllActive();
+			this.speedSort(sortedActive);
+			for (const pokemon of sortedActive) {
+				if (pokemon !== source) {
+					// Will be suppressed by Pokemon#ignoringAbility if needed
+					this.singleEvent('Start', pokemon.getAbility(), pokemon.abilityData, pokemon);
+					if (pokemon.m.innates) {
+						for (const innate of pokemon.m.innates) {
+							pokemon.addVolatile('ability:' + innate, pokemon);
+						}
+					}
+				}
+			}
+		},
+	},
+	powerofalchemy: {
+		inherit: true,
+		onAllyFaint(ally) {
+			const pokemon = this.effectData.target;
+			if (!pokemon.hp) return;
+			const isAbility = pokemon.ability === 'powerofalchemy';
+			let possibleAbilities = [ally.ability];
+			if (ally.m.innates) possibleAbilities.push(...ally.m.innates);
+			const additionalBannedAbilities = [
+				'noability', 'flowergift', 'forecast', 'hungerswitch', 'illusion', 'imposter', 'neutralizinggas', 'powerofalchemy', 'receiver', 'trace', 'wonderguard', pokemon.ability, ...(pokemon.m.innates || []),
+			];
+			possibleAbilities = possibleAbilities
+				.filter(val => !this.dex.getAbility(val).isPermanent && !additionalBannedAbilities.includes(val));
+			if (!possibleAbilities.length) return;
+			const ability = this.dex.getAbility(possibleAbilities[this.random(possibleAbilities.length)]);
+			this.add('-ability', pokemon, ability, '[from] ability: Power of Alchemy', '[of] ' + ally);
+			if (isAbility) {
+				pokemon.setAbility(ability);
+			} else {
+				pokemon.removeVolatile("ability:powerofalchemy");
+				pokemon.addVolatile("ability:" + ability, pokemon);
+			}
+		},
+	},
+	receiver: {
+		inherit: true,
+		onAllyFaint(ally) {
+			const pokemon = this.effectData.target;
+			if (!pokemon.hp) return;
+			const isAbility = pokemon.ability === 'receiver';
+			let possibleAbilities = [ally.ability];
+			if (ally.m.innates) possibleAbilities.push(...ally.m.innates);
+			const additionalBannedAbilities = [
+				'noability', 'flowergift', 'forecast', 'hungerswitch', 'illusion', 'imposter', 'neutralizinggas', 'powerofalchemy', 'receiver', 'trace', 'wonderguard', pokemon.ability, ...(pokemon.m.innates || []),
+			];
+			possibleAbilities = possibleAbilities
+				.filter(val => !this.dex.getAbility(val).isPermanent && !additionalBannedAbilities.includes(val));
+			if (!possibleAbilities.length) return;
+			const ability = this.dex.getAbility(possibleAbilities[this.random(possibleAbilities.length)]);
+			this.add('-ability', pokemon, ability, '[from] ability: Receiver', '[of] ' + ally);
+			if (isAbility) {
+				pokemon.setAbility(ability);
+			} else {
+				pokemon.removeVolatile("ability:receiver");
+				pokemon.addVolatile("ability:" + ability, pokemon);
+			}
+		},
+	},
+	trace: {
+		inherit: true,
+		onUpdate(pokemon) {
+			if (!pokemon.isStarted) return;
+			const isAbility = pokemon.ability === 'trace';
+			const possibleTargets: Pokemon[] = [];
+			for (const target of pokemon.side.foe.active) {
+				if (target && !target.fainted) {
+					possibleTargets.push(target);
+				}
+			}
+			while (possibleTargets.length) {
+				const rand = this.random(possibleTargets.length);
+				const target = possibleTargets[rand];
+				let possibleAbilities = [target.ability];
+				if (target.m.innates) possibleAbilities.push(...target.m.innates);
+				const additionalBannedAbilities = [
+					// Zen Mode included here for compatability with Gen 5-6
+					'noability', 'flowergift', 'forecast', 'hungerswitch', 'illusion', 'imposter', 'neutralizinggas', 'powerofalchemy', 'receiver', 'trace', 'zenmode', pokemon.ability, ...(pokemon.m.innates || []),
+				];
+				possibleAbilities = possibleAbilities
+					.filter(val => !this.dex.getAbility(val).isPermanent && !additionalBannedAbilities.includes(val));
+				if (!possibleAbilities.length) {
+					possibleTargets.splice(rand, 1);
+					continue;
+				}
+				const ability = this.dex.getAbility(this.sample(possibleAbilities));
+				this.add('-ability', pokemon, ability, '[from] ability: Trace', '[of] ' + target);
+				if (isAbility) {
+					pokemon.setAbility(ability);
+				} else {
+					pokemon.removeVolatile("ability:trace");
+					pokemon.addVolatile("ability:" + ability, pokemon);
+				}
+				return;
+			}
+		},
+	},
+};
