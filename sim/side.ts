47d8bd4d82b503a17015d7a271511ab40ee04d67
HoeenHero <HoeenCoder@users.noreply.github.com>
2021-04-03 18:15:13 -0400

    Fix hazards not applying if the target side is fainted (#8171)
    Co-authored-by: pyuk-bot <msaimrkon@gmail.com>

--- sim/side.ts
+++ sim/side.ts
@@ -243,16 +243,19 @@ export class Side {
 
 		return this.foe.pokemonLeft;
 	}
-	allies() {
+	allies(all?: boolean) {
 		// called during the first switch-in, so `active` can still contain nulls at this point
-		return this.activeTeam().filter(ally => ally && !ally.fainted);
+		let allies = this.activeTeam().filter(ally => ally);
+		if (!all) allies = allies.filter(ally => !ally.fainted);
+
+		return allies;
 	}
-	foes() {
+	foes(all?: boolean) {
 		if (this.battle.gameType === 'freeforall') {
 			return this.battle.sides.map(side => side.active[0])
-				.filter(pokemon => pokemon && pokemon.side !== this && !pokemon.fainted);
+				.filter(pokemon => pokemon && pokemon.side !== this && (all || !pokemon.fainted));
 		}
-		return this.foe.allies();
+		return this.foe.allies(all);
 	}
 	activeTeam() {
 		if (this.battle.gameType !== 'multi') return this.active;
