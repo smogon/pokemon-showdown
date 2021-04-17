2676f6aff7d6ee8f68a0f28dbb6010ccd5fde349
Leonard Craft III <leonardcraft64@gmail.com>
2021-04-17 10:26:01 -0500

    Just remove the endability message altogether

--- sim/pokemon.ts
+++ sim/pokemon.ts
@@ -1699,10 +1699,6 @@ export class Pokemon {
 		}
 		if (!this.battle.runEvent('SetAbility', this, source, this.battle.effect, ability)) return false;
 		this.battle.singleEvent('End', this.battle.dex.abilities.get(oldAbility), this.abilityData, this, source);
-		if (this.battle.effect && this.battle.effect.effectType === 'Move' && !isFromFormeChange) {
-			this.battle.add('-endability', this, this.battle.dex.abilities.get(oldAbility), '[from] move: ' +
-				this.battle.dex.moves.get(this.battle.effect.id));
-		}
 		this.ability = ability.id;
 		this.abilityData = {id: ability.id, target: this};
 		if (ability.id && this.battle.gen > 3) {
