5f6f0a2ceefb82cf95336f0a96e5d88e49fff7f3
Karthik99999 <bandagondak0217@gmail.com>
2021-04-17 11:43:51 -0400

    add test for magician working properly

--- test/sim/abilities/magician.js
+++ test/sim/abilities/magician.js
@@ -10,6 +10,16 @@ describe('Magician', function () {
 		battle.destroy();
 	});
 
+	it(`should steal the opponents item`, function () {
+		battle = common.createBattle([[
+			{species: 'klefki', ability: 'magician', moves: ['flashcannon']},
+		], [
+			{species: 'wynaut', item: 'tr69', moves: ['sleeptalk']},
+		]]);
+		battle.makeChoices();
+		assert.equal(battle.p1.active[0].item, 'tr69');
+	});
+
 	it(`should not steal Weakness Policy on super-effective hits`, function () {
 		battle = common.createBattle([[
 			{species: 'klefki', ability: 'magician', moves: ['flashcannon']},
