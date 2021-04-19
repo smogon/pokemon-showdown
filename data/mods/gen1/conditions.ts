b376756697a24a7dd004ead6476ab342887e11d5
Karthik <32044378+Karthik99999@users.noreply.github.com>
2021-04-19 12:45:44 -0400

    Gen 1: Don't allow Pokemon to recharge if frozen (#8211)

--- data/mods/gen1/conditions.ts
+++ data/mods/gen1/conditions.ts
@@ -88,7 +88,7 @@ export const Conditions: {[id: string]: ModdedConditionData} = {
 		onStart(target) {
 			this.add('-status', target, 'frz');
 		},
-		onBeforeMovePriority: 10,
+		onBeforeMovePriority: 12,
 		onBeforeMove(pokemon, target, move) {
 			this.add('cant', pokemon, 'frz');
 			pokemon.lastMove = null;
@@ -220,6 +220,7 @@ export const Conditions: {[id: string]: ModdedConditionData} = {
 	},
 	mustrecharge: {
 		inherit: true,
+		duration: 0,
 		onStart() {},
 	},
 	lockedmove: {
