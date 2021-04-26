bb711562bd7ff0202e5983e7c234c6dec14373aa
Leonard Craft III <leonardcraft64@gmail.com>
2021-04-25 22:21:50 -0500

    Fix Shell Side Arm information leak (#8233)

--- data/moves.ts
+++ data/moves.ts
@@ -15099,10 +15099,11 @@ export const Moves: {[moveid: string]: MoveData} = {
 			}
 		},
 		onHit(target, source, move) {
-			this.hint(move.category + " Shell Side Arm");
+			// Shell Side Arm normally reveals its category via animation on cart, but doesn't play either custom animation against allies
+			if (!source.isAlly(target)) this.hint(move.category + " Shell Side Arm");
 		},
 		onAfterSubDamage(damage, target, source, move) {
-			this.hint(move.category + " Shell Side Arm");
+			if (!source.isAlly(target)) this.hint(move.category + " Shell Side Arm");
 		},
 		secondary: {
 			chance: 20,
