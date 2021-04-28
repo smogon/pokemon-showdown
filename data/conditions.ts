69bd42c7addf32b66a133a8d9369b5b589b4ca84
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2021-04-28 14:04:31 -0600

    Dynamax: Remove Doubles OU hardcode

--- data/conditions.ts
+++ data/conditions.ts
@@ -691,7 +691,7 @@ export const Conditions: {[k: string]: ConditionData} = {
 			if (pokemon.baseSpecies.name === 'Shedinja') return;
 
 			// Changes based on dynamax level, 2 is max (at LVL 10)
-			const ratio = this.format.id.startsWith('gen8doublesou') ? 1.5 : 2;
+			const ratio = 2; // TODO: Implement Dynamax levels
 
 			pokemon.maxhp = Math.floor(pokemon.maxhp * ratio);
 			pokemon.hp = Math.floor(pokemon.hp * ratio);
