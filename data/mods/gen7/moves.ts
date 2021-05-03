a4aaf64717fce90cd5037fb38673de0d1a94956b
Karthik <32044378+Karthik99999@users.noreply.github.com>
2021-05-02 20:36:52 -0400

    Gen VII: Backport Psychic Terrain priority hint (#8262)

--- data/mods/gen7/moves.ts
+++ data/mods/gen7/moves.ts
@@ -663,8 +663,15 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 			},
 			onTryHitPriority: 4,
 			onTryHit(target, source, effect) {
-				if (!target.isGrounded() || target.isSemiInvulnerable() || target.isAlly(source)) return;
 				if (effect && (effect.priority <= 0.1 || effect.target === 'self')) {
+					return;
+				}
+				if (target.isSemiInvulnerable() || target.isAlly(source)) return;
+				if (!target.isGrounded()) {
+					const baseMove = this.dex.moves.get(effect.id);
+					if (baseMove.priority > 0) {
+						this.hint("Psychic Terrain doesn't affect Pokémon immune to Ground.");
+					}
 					return;
 				}
 				this.add('-activate', target, 'move: Psychic Terrain');
