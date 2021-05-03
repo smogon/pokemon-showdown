7b381bdcf728d961e6964aca4b19ae2675ffc42e
Leonard Craft III <leonardcraft64@gmail.com>
2021-05-02 20:43:43 -0500

    Improve self-hit confusion damage

--- sim/battle-actions.ts
+++ sim/battle-actions.ts
@@ -1711,6 +1711,26 @@ export class BattleActions {
 		return tr(baseDamage, 16);
 	}
 
+	/**
+	 * Confusion damage is unique - most typical modifiers that get run when calculating
+	 * damage (e.g. Huge Power, Life Orb, critical hits) don't apply. It also uses a 16-bit
+	 * context for its damage, unlike the regular damage formula (though this only comes up
+	 * for base damage).
+	 */
+	getConfusionDamage(pokemon: Pokemon, basePower: number) {
+		const tr = this.battle.trunc;
+
+		const attack = pokemon.calculateStat('atk', pokemon.boosts['atk']);
+		const defense = pokemon.calculateStat('def', pokemon.boosts['def']);
+		const level = pokemon.level;
+		const baseDamage = tr(tr(tr(tr(2 * level / 5 + 2) * basePower * attack) / defense) / 50) + 2;
+
+		// Damage is 16-bit context in self-hit confusion damage
+		let damage = tr(baseDamage, 16);
+		damage = this.battle.randomizer(damage);
+		return Math.max(1, damage);
+	}
+
 	// #endregion
 
 	// #region MEGA EVOLUTION
