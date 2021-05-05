16a5ce98d0646be781f5e1c8ee042a750311a4a9
Karthik99999 <bandagondak0217@gmail.com>
2021-05-05 10:31:15 -0400

    delete trailling spaces

--- test/sim/abilities/gluttony.js
+++ test/sim/abilities/gluttony.js
@@ -21,7 +21,7 @@ describe('Gluttony', function () {
 		const wobbuffet = battle.p1.active[0];
 		assert.equal(wobbuffet.hp, Math.floor(wobbuffet.maxhp / 2) + Math.floor(wobbuffet.maxhp * 0.33));
 	});
-  
+
 	it(`should activate after Belly Drum`, function () {
 		battle = common.createBattle([[
 			{species: "snorlax", ability: 'gluttony', item: 'aguavberry', evs: {hp: 4}, moves: ['bellydrum']},
@@ -33,7 +33,7 @@ describe('Gluttony', function () {
 		const snorlax = battle.p1.active[0];
 		assert.equal(snorlax.hp, Math.floor(snorlax.maxhp / 2) + Math.floor(snorlax.maxhp * 0.33));
 	});
-  
+
 	it(`should activate after poison damage`, function () {
 		battle = common.createBattle([[
 			{species: "wobbuffet", ability: 'gluttony', item: 'aguavberry', evs: {hp: 28}, moves: ['sleeptalk']},
