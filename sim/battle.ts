f20a95f3b97a4b2433dddf6e198f8e18d12ccbc1
Marty-D <Marty-D@users.noreply.github.com>
2021-05-12 10:55:56 -0400

    Fix faint check mid-attack
    Only bother checking for a winner if the move's user is also fainted at the time, and check for a winner after the move completes.

--- sim/battle.ts
+++ sim/battle.ts
@@ -2172,7 +2172,7 @@ export class Battle {
 		}
 	}
 
-	faintMessages(lastFirst = false, forceCheck = false) {
+	faintMessages(lastFirst = false, forceCheck = false, checkWin = true) {
 		if (this.ended) return;
 		const length = this.faintQueue.length;
 		if (!length) {
@@ -2219,7 +2219,7 @@ export class Battle {
 			}
 		}
 
-		if (this.checkWin(faintData)) return true;
+		if (checkWin && this.checkWin(faintData)) return true;
 
 		if (faintData && length) {
 			this.runEvent('AfterFaint', faintData.target, faintData.source, faintData.effect, length);
