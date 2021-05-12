44cd13626f46fc020d5a3e9a09405d670fcd277a
Mia <49593536+mia-pi-git@users.noreply.github.com>
2021-05-12 13:26:09 -0500

    Roomsettings: Don't allow locked users to set modchat
    People are spamming with this. See comment for why it's not in the other access denied check.

--- server/chat-commands/room-settings.ts
+++ server/chat-commands/room-settings.ts
@@ -80,7 +80,11 @@ export const commands: Chat.ChatCommands = {
 			const modchatSetting = (room.settings.modchat || "OFF");
 			return this.sendReply(`Moderated chat is currently set to: ${modchatSetting}`);
 		}
-		this.checkCan('modchat', null, room);
+		if (user.locked) { // would put this below but it behaves weird if there's no modchat set
+			return this.errorReply(`/modchat - Access denied.`);
+		} else {
+			this.checkCan('modchat', null, room);
+		}
 
 		if (
 			room.settings.modchat && room.settings.modchat.length <= 1 &&
