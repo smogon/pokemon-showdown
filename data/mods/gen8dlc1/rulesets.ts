6caef4e0d7c974384d7ff8dd6f346f7eab424c1b
Marty-D <Marty-D@users.noreply.github.com>
2021-05-12 13:59:20 -0400

    Update rulesets.ts

--- data/mods/gen8dlc1/rulesets.ts
+++ data/mods/gen8dlc1/rulesets.ts
@@ -1,18 +1,7 @@
 export const Rulesets: {[k: string]: ModdedFormatData} = {
 	teampreview: {
-		effectType: 'Rule',
-		name: 'Team Preview',
-		desc: "Allows each player to see the Pok&eacute;mon on their opponent's team before they choose their lead Pok&eacute;mon",
-		onTeamPreview() {
-			this.add('clearpoke');
-			for (const pokemon of this.getAllPokemon()) {
-				const details = pokemon.details.replace(', shiny', '')
-					.replace(/(Arceus|Gourgeist|Pumpkaboo|Xerneas|Silvally|Zacian|Zamazenta|Urshifu)(-[a-zA-Z?-]+)?/g, '$1-*');
-				this.add('poke', pokemon.side.id, details, '');
-			}
-			this.makeRequest('teampreview');
-		},
-		onFieldStart() {
+		inherit: true,
+		onBattleStart() {
 			// Xerneas isn't in DLC1 but operated this way pre-1.3.2 update
 			const formesToLeak = ["zaciancrowned", "zamazentacrowned", "xerneas"];
 			for (const pokemon of this.getAllPokemon()) {
