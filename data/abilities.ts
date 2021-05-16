63b2c8c4fbbce7c2dfe3e28348238e31cdbe8775
Leonard Craft III <leonardcraft64@gmail.com>
2021-05-16 17:30:37 -0500

    Fix Struggle to be unaffected by Disable

--- data/abilities.ts
+++ data/abilities.ts
@@ -583,7 +583,7 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 	cursedbody: {
 		onDamagingHit(damage, target, source, move) {
 			if (source.volatiles['disable']) return;
-			if (!move.isMax && !move.isFutureMove) {
+			if (!move.isMax && !move.isFutureMove && move.id !== 'struggle') {
 				if (this.randomChance(3, 10)) {
 					source.addVolatile('disable', this.effectState.target);
 				}
