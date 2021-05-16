5ee0bee3e9c738a5018932af5debf086f9be83ad
Mia <49593536+mia-pi-git@users.noreply.github.com>
2021-05-16 18:10:41 -0500

    Minor activities: Don't shift queue twice upon end

--- server/room-minor-activity.ts
+++ server/room-minor-activity.ts
@@ -79,7 +79,6 @@ export abstract class MinorActivity {
 		room.minorActivity?.destroy();
 		if (room.minorActivityQueue?.length) {
 			const pollData = room.minorActivityQueue.shift()!;
-			room.settings.minorActivityQueue!.shift();
 			if (!room.minorActivityQueue.length) room.clearMinorActivityQueue();
 			if (!room.settings.minorActivityQueue?.length) {
 				delete room.settings.minorActivityQueue;
