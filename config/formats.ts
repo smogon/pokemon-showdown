087a309001c796c9ec8efbd6ccffa2fa68008dd1
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2021-05-16 17:24:00 -0600

    ZU: Ban Damp Rock
    https://www.smogon.com/forums/threads/np-zu-stage-5-2-new-drops-at-35-return-of-the-ice.3677885/page-2#post-8858422

--- config/formats.ts
+++ config/formats.ts
@@ -224,7 +224,7 @@ export const Formats: FormatList = [
 
 		mod: 'gen8',
 		ruleset: ['[Gen 8] PU'],
-		banlist: ['PU', 'Drampa', 'Exeggutor-Alola', 'Gallade', 'Haunter', 'Magmortar', 'Magneton', 'Omastar', 'Turtonator', 'Vikavolt', 'Silvally-Dragon', 'Sneasel'],
+		banlist: ['PU', 'Drampa', 'Exeggutor-Alola', 'Gallade', 'Haunter', 'Magmortar', 'Magneton', 'Omastar', 'Turtonator', 'Vikavolt', 'Silvally-Dragon', 'Sneasel', 'Damp Rock'],
 	},
 	{
 		name: "[Gen 8] LC UU",
