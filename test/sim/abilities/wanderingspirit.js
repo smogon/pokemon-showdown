5eb6525ecba5c77e6356a48804bad562883d8c6f
Leonard Craft III <leonardcraft64@gmail.com>
2021-05-18 18:27:00 -0500

    Improve Wandering Spirit tests

--- test/sim/abilities/wanderingspirit.js
+++ test/sim/abilities/wanderingspirit.js
@@ -8,29 +8,36 @@ let battle;
 describe('Wandering Spirit', function () {
 	afterEach(() => battle.destroy());
 
-	it('should exchange abilities with an attacker that makes contact', function () {
-		battle = common.createBattle();
-		battle.setPlayer('p1', {team: [{species: 'Decidueye', ability: 'overgrow', moves: ['shadowsneak']}]});
-		battle.setPlayer('p2', {team: [{species: 'Runerigus', ability: 'wanderingspirit', moves: ['sleeptalk']}]});
-		battle.makeChoices('move shadowsneak', 'move sleeptalk');
+	it(`should exchange abilities with an attacker that makes contact`, function () {
+		battle = common.createBattle([[
+			{species: 'Decidueye', ability: 'overgrow', moves: ['shadowsneak']},
+		], [
+			{species: 'Runerigus', ability: 'wanderingspirit', moves: ['sleeptalk']},
+		]]);
+		battle.makeChoices();
 		assert(battle.p1.active[0].hasAbility('wanderingspirit'));
 		assert(battle.p2.active[0].hasAbility('overgrow'));
 	});
 
-	it('should not activate while Dynamaxed', function () {
+	it(`should not activate while Dynamaxed`, function () {
+		battle = common.createBattle([[
+			{species: 'Decidueye', ability: 'overgrow', moves: ['shadowsneak']},
+		], [
+			{species: 'Runerigus', ability: 'wanderingspirit', moves: ['sleeptalk']},
+		]]);
 		battle = common.createBattle();
-		battle.setPlayer('p1', {team: [{species: 'Decidueye', ability: 'overgrow', moves: ['shadowsneak']}]});
-		battle.setPlayer('p2', {team: [{species: 'Runerigus', ability: 'wanderingspirit', moves: ['bodypress']}]});
-		battle.makeChoices('move shadowsneak', 'move 1 dynamax');
+		battle.makeChoices('auto', 'move 1 dynamax');
 		assert(battle.p1.active[0].hasAbility('overgrow'));
 		assert(battle.p2.active[0].hasAbility('wanderingspirit'));
 	});
 
-	it('should not swap with Wonder Guard', function () {
-		battle = common.createBattle();
-		battle.setPlayer('p1', {team: [{species: 'Shedinja', ability: 'wonderguard', moves: ['shadowsneak']}]});
-		battle.setPlayer('p2', {team: [{species: 'Runerigus', ability: 'wanderingspirit', moves: ['sleeptalk']}]});
-		battle.makeChoices('move shadowsneak', 'move sleeptalk');
+	it(`should not swap with Wonder Guard`, function () {
+		battle = common.createBattle([[
+			{species: 'Shedinja', ability: 'wonderguard', moves: ['shadowsneak']},
+		], [
+			{species: 'Runerigus', ability: 'wanderingspirit', moves: ['sleeptalk']},
+		]]);
+		battle.makeChoices();
 		assert(battle.p1.active[0].hasAbility('wonderguard'));
 		assert(battle.p2.active[0].hasAbility('wanderingspirit'));
 	});
