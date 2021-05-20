smogon/pokemon-showdown

commit fba36165f28eed654d3d2ae73f3cd336da4ff6cb

Run onModifyTemplate on rulesets (#5207)

sim/battle.ts
@@ -164,6 +164,17 @@ export class Battle extends Dex.ModdedDex {
 			this.inputLog.push(`>version ${global.__version}`);
 		}
 		this.inputLog.push(`>start ` + JSON.stringify(inputOptions));
+
+		for (const rule of this.getRuleTable(format).keys()) {
+			if (rule.startsWith('+') || rule.startsWith('-') || rule.startsWith('!')) continue;
+			let subFormat = this.getFormat(rule);
+			if (subFormat.exists) {
+				let hasEventHandler = Object.keys(subFormat).some(val =>
+					val.startsWith('on') && !['onBegin', 'onValidateTeam', 'onChangeSet', 'onValidateSet'].includes(val)
+				);
+				if (hasEventHandler) this.addPseudoWeather(rule);
+			}
+		}
 		if (options.p1) {
 			this.setPlayer('p1', options.p1);
 		}
@@ -861,7 +872,7 @@ export class Battle extends Dex.ModdedDex {
 
 	findEventHandlers(thing: Pokemon | Side | Battle, eventName: string, sourceThing?: Pokemon | null) {
 		let handlers: AnyObject[] = [];
-		if (thing instanceof Pokemon) {
+		if (thing instanceof Pokemon && thing.isActive) {
 			handlers = this.findPokemonEventHandlers(thing, `on${eventName}`);
 			for (const allyActive of thing.side.active) {
 				if (!allyActive || allyActive.fainted) continue;
@@ -1679,10 +1690,6 @@ export class Battle extends Dex.ModdedDex {
 			let subFormat = this.getFormat(rule);
 			if (subFormat.exists) {
 				if (subFormat.onBegin) subFormat.onBegin.call(this);
-				let hasEventHandler = Object.keys(subFormat).some(val =>
-					val.startsWith('on') && !['onBegin', 'onValidateTeam', 'onChangeSet', 'onValidateSet', 'onModifyTemplate'].includes(val)
-				);
-				if (hasEventHandler) this.addPseudoWeather(rule);
 			}
 		}
 

config/formats.js
@@ -737,9 +737,9 @@ let Formats = [
 		searchShow: false,
 		ruleset: ['[Gen 7] OU'],
 		banlist: ['Kartana', 'Kyurem-Black', 'Shedinja'],
-		onModifyTemplate(template, target, source) {
+		onModifyTemplate(template, target, source, effect) {
 			if (!target) return; // Chat command
-			if (source && ['imposter', 'transform'].includes(source.id)) return;
+			if (effect && ['imposter', 'transform'].includes(effect.id)) return;
 			let types = [...new Set(target.baseMoveSlots.slice(0, 2).map(move => this.getMove(move.id).type))];
 			return Object.assign({}, template, {types: types});
 		},

dev-tools/globals.ts
@@ -698,7 +698,7 @@ interface FormatsData extends EventMethods {
 	onAfterMega?: (this: Battle, pokemon: Pokemon) => void
 	onBegin?: (this: Battle) => void
 	onChangeSet?: (this: ModdedDex, set: PokemonSet, format: Format, setHas?: AnyObject, teamHas?: AnyObject) => string[] | void
-	onModifyTemplate?: (this: Battle, template: Template, target: Pokemon, source: Pokemon) => Template | void
+	onModifyTemplate?: (this: Battle, template: Template, target: Pokemon, source: Pokemon, effect: Effect) => Template | void
 	onTeamPreview?: (this: Battle) => void
 	onValidateSet?: (this: ModdedDex, set: PokemonSet, format: Format, setHas: AnyObject, teamHas: AnyObject) => string[] | void
 	onValidateTeam?: (this: ModdedDex, team: PokemonSet[], format: Format, teamHas: AnyObject) => string[] | void

sim/pokemon.ts
@@ -201,7 +201,7 @@ export class Pokemon {
 		this.template = this.baseTemplate;
 		this.moveSlots = [];
 		this.baseMoveSlots = [];
-		// @ts-ignore - null used for this.formeChange(this.baseTemplate)
+		// @ts-ignore - null used for this.setTemplate(this.baseTemplate)
 		this.baseStats = null;
 		this.trapped = false;
 		this.maybeTrapped = false;
@@ -949,7 +949,7 @@ export class Pokemon {
 	 * Use formChange to handle changes to ability and sending client messages.
 	 */
 	setTemplate(rawTemplate: Template, source: Effect | null = this.battle.effect) {
-		let template = this.battle.singleEvent('ModifyTemplate', this.battle.getFormat(), null, this, source, null, rawTemplate);
+		let template = this.battle.runEvent('ModifyTemplate', this, null, source, rawTemplate);
 
 		if (!template) return null;
 

commit cb536900fdefb9c64f7e0135d36bbd7972dd109e

Run events on future moves even while the user is not active
Fixes a regression in https://github.com/smogon/pokemon-showdown/commit/fba36165f28eed654d3d2ae73f3cd336da4ff6cb

sim/battle.ts
@@ -863,7 +863,7 @@ export class Battle {
 			}
 			return handlers;
 		}
-		if (target instanceof Pokemon && target.isActive) {
+		if (target instanceof Pokemon && (target.isActive || source?.isActive)) {
 			handlers = this.findPokemonEventHandlers(target, `on${eventName}`);
 			for (const allyActive of target.alliesAndSelf()) {
 				handlers.push(...this.findPokemonEventHandlers(allyActive, `onAlly${eventName}`));

test/sim/moves/futuresight.js
@@ -198,7 +198,7 @@ describe('Future Sight', function () {
 		assert.bounded(damage, [96, 114]);
 	});
 
-	it.skip(`should not ignore the target's screens, even when the user is not active on the field`, function () {
+	it(`should not ignore the target's screens, even when the user is not active on the field`, function () {
 		battle = common.createBattle([[
 			{species: 'Blissey', ability: 'shellarmor', item: 'lightclay', moves: ['softboiled', 'lightscreen']},
 		], [
@@ -218,5 +218,25 @@ describe('Future Sight', function () {
 		battle.makeChoices('auto', 'switch 2');
 		damage = blissey.maxhp - blissey.hp;
 		assert.bounded(damage, [18, 21]);
+	});
+
+	it(`should not consider the user's item or Ability when the user is not active`, function () {
+		battle = common.createBattle([[
+			{species: 'Blissey', ability: 'shellarmor', moves: ['softboiled']},
+		], [
+			{species: 'Wynaut', ability: 'adaptability', item: 'choicespecs', moves: ['futuresight']},
+			{species: 'Deino', ability: 'powerspot', moves: ['sleeptalk']},
+		]]);
+
+		for (let i = 0; i < 3; i++) battle.makeChoices();
+		const blissey = battle.p1.active[0];
+		let damage = blissey.maxhp - blissey.hp;
+		assert.bounded(damage, [70, 84]); // boosted by Adaptability and Choice Specs
+
+		battle.makeChoices();
+		battle.makeChoices();
+		battle.makeChoices('auto', 'switch 2');
+		damage = blissey.maxhp - blissey.hp;
+		assert.bounded(damage, [46, 55]); // only boosted by Power Spot
 	});
 });