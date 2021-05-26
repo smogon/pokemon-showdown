fd2b4a4f9d63887f526995aae77da33575ac8914
Annika <annika0uwu@gmail.com>
2021-05-26 12:41:56 -0400

    Improve handling of legacy forced-public prefixes

--- server/room-battle.ts
+++ server/room-battle.ts
@@ -1133,7 +1133,9 @@ export class RoomBattle extends RoomGames.RoomGame {
 
 	static battleForcedSetting(user: User, key: 'modchat' | 'privacy') {
 		if (Config.forcedpublicprefixes) {
-			Config.forcedprefixes = {privacy: Config.forcedpublicprefixes};
+			if (!Config.forcedprefixes) Config.forcedprefixes = {};
+			if (!Config.forcedprefixes.privacy) Config.forcedprefixes.privacy = [];
+			Config.forcedprefixes.privacy.push(...Config.forcedpublicprefixes);
 			delete Config.forcedpublicprefixes;
 		}
 		if (!Config.forcedprefixes?.[key]) return null;
