542d9527603edfae65835cfefa797dac09074124
Annika <annika0uwu@gmail.com>
2021-05-29 13:23:31 -0700

    /randbats: Use a `<details>` dropdown

--- server/chat-plugins/random-battles.ts
+++ server/chat-plugins/random-battles.ts
@@ -650,7 +650,11 @@ export const commands: Chat.ChatCommands = {
 				if (!pokemon.randomBattleMoves || pokemon.isNonstandard === 'Future') continue;
 				const randomMoves = pokemon.randomBattleMoves.slice();
 				const m = randomMoves.sort().map(formatMove);
-				movesets.push(`<span style="color:#999999;">Moves for ${pokemon.name} in ${formatName}:</span><br />${m.join(`, `)}`);
+				movesets.push(
+					`<details${!movesets.length ? ' open' : ''}>` +
+					`<summary><span style="color:#999999;">Moves for ${pokemon.name} in ${formatName}:<span style="color:#999999;"></summary>` +
+					`${m.join(`, `)}</details>`
+				);
 			}
 		}
 
