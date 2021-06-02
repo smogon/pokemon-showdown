59377f5a470e3131e03bf2948f8af121ce25ce4f
Mia <49593536+mia-pi-git@users.noreply.github.com>
2021-06-02 13:03:00 -0500

    Info: Fix /show and /requestshow interaction error (#7904)

--- server/chat-commands/info.ts
+++ server/chat-commands/info.ts
@@ -2455,7 +2455,7 @@ export const commands: Chat.ChatCommands = {
 		if (!room.settings.requestShowEnabled) {
 			return this.errorReply(`Media approvals are disabled in this room.`);
 		}
-		if (user.can('showmedia', null, room)) return this.errorReply(`Use !show instead.`);
+		if (user.can('showmedia', null, room, '/show')) return this.errorReply(`Use !show instead.`);
 		if (room.pendingApprovals?.has(user.id)) return this.errorReply('You have a request pending already.');
 		if (!toID(target)) return this.parse(`/help requestshow`);
 
