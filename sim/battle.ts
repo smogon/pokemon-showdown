d48244b328e4e777e28ddefb08096a52160774c0
pacmanboss256 <61611109+pacmanboss256@users.noreply.github.com>
2021-06-07 17:52:27 -0500

    Update battle.ts

--- sim/battle.ts
+++ sim/battle.ts
@@ -1424,7 +1424,7 @@ export class Battle {
 			let sideStaleness: 'internal' | 'external' | undefined;
 			for (const pokemon of side.active) {
 				if (!pokemon) continue;
-				pokemon.hitself = false;
+				pokemon.hitSelf = false;
 				pokemon.moveThisTurn = '';
 				pokemon.newlySwitched = false;
 				pokemon.moveLastTurnResult = pokemon.moveThisTurnResult;
