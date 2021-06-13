c4885f5c07044b35bf163f25439fd399badb8370
Mia <49593536+mia-pi-git@users.noreply.github.com>
2021-06-13 13:10:52 -0500

    Spotlights: Add a refresh button to the dailies page
    https://www.smogon.com/forums/threads/add-a-refresh-button-to-the-daily-spotlights-page.3686101/#post-8886206

--- server/chat-plugins/daily-spotlight.ts
+++ server/chat-plugins/daily-spotlight.ts
@@ -64,7 +64,9 @@ export const pages: Chat.PageTable = {
 		this.title = 'Daily Spotlights';
 		const room = this.requireRoom();
 
-		let buf = `<div class="pad ladder"><h2>Daily Spotlights</h2>`;
+		let buf = `<div class="pad ladder">`;
+		buf += `<div class="pad"><button style="float:right;" class="button" name="send" value="/join view-spotlights-${room.roomid}"><i class="fa fa-refresh"></i> Refresh</button>`;
+		buf += `<h2>Daily Spotlights</h2>`;
 		if (!spotlights[room.roomid]) {
 			buf += `<p>This room has no daily spotlights.</p></div>`;
 		} else {
