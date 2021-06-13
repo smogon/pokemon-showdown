be788410ec8fc71846e041e018a8e9950c62e598
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2021-06-12 22:57:22 -0600

    National Dex UU: Ban Mega Sableye

--- data/tags.ts
+++ data/tags.ts
@@ -196,8 +196,8 @@ export const Tags: {[id: string]: TagData} = {
 		name: "ND UUBL",
 		speciesFilter: species => [
 			'Aerodactyl-Mega', 'Azumarill', 'Blacephalon', 'Diancie-Mega', 'Gallade-Mega', 'Gardevoir-Mega', 'Gengar', 'Gyarados', 'Hawlucha',
-			'Heracross-Mega', 'Hoopa-Unbound', 'Hydreigon', 'Latias', 'Latias-Mega', 'Latios', 'Latios-Mega', 'Manaphy', 'Pinsir-Mega', 'Slowbro-Mega',
-			'Thundurus', 'Thundurus-Therian', 'Venusaur-Mega', 'Xurkitree', 'Zapdos-Galar',
+			'Heracross-Mega', 'Hoopa-Unbound', 'Hydreigon', 'Latias', 'Latias-Mega', 'Latios', 'Latios-Mega', 'Manaphy', 'Pinsir-Mega', 'Sableye-Mega',
+			'Slowbro-Mega', 'Thundurus', 'Thundurus-Therian', 'Venusaur-Mega', 'Xurkitree', 'Zapdos-Galar',
 		].includes(species.name),
 	},
 
