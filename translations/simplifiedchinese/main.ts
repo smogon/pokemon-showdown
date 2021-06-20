3a3be7298dfd4ca0c590ac7003aa9bb2fc345d35
Distrib <theodelhay@orange.fr>
2021-06-21 00:01:36 +0200

    Update main.ts

--- translations/simplifiedchinese/main.ts
+++ translations/simplifiedchinese/main.ts
@@ -127,7 +127,7 @@ export const translations: Translations = {
 		"This ${Config.groups[targetUser.group].name} is too busy to answer private messages right now. Please contact a different staff member.": "",
 		"If you need help, try opening a <a href=\"view-help-request\" class=\"button\">help ticket</a>": "",
 		"You are blocking private messages right now.": "",
-		"You are blocking challenge right now.": "",
+		"You are blocking challenges right now.": "",
 
 		"Your message contained banned words in this room.": "发言内容包含了房间内禁止词汇",
 		"You can't send the same message again so soon.": "同样的句子不能及时发出",
