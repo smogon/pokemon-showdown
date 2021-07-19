f3684a5873bfccbb25241f06e2302b552b0118fc
Mia <49593536+mia-pi-git@users.noreply.github.com>
2021-07-19 17:16:14 -0500

    Friends: Properly update spectator lists on battle end

--- server/chat.ts
+++ server/chat.ts
@@ -64,6 +64,7 @@ export interface Handlers {
 	onRoomClose?: (id: string, user: User, connection: Connection, page: boolean) => any;
 	onRenameRoom?: (oldId: RoomID, newID: RoomID, room: BasicRoom) => void;
 	onBattleStart?: (user: User, room: GameRoom) => void;
+	onBattleLeave?: (user: User, room: GameRoom) => void;
 }
 
 export interface ChatPlugin {
