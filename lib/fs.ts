e9990080d308f33917551bd86059035997db7791
Annika <annika0uwu@gmail.com>
2021-08-04 21:12:37 -0700

    FS: Fix build

--- lib/fs.ts
+++ lib/fs.ts
@@ -56,7 +56,7 @@ export class FSPath {
 		return new FSPath(pathModule.dirname(this.path));
 	}
 
-	read(options: AnyObject | string = 'utf8'): Promise<string> {
+	read(options: AnyObject | BufferEncoding = 'utf8'): Promise<string> {
 		if (typeof options !== 'string' && options.encoding === undefined) {
 			options.encoding = 'utf8';
 		}
@@ -74,7 +74,7 @@ export class FSPath {
 		return fs.readFileSync(this.path, options as {encoding: 'utf8'});
 	}
 
-	readBuffer(options: AnyObject | string = {}): Promise<Buffer> {
+	readBuffer(options: AnyObject | BufferEncoding = {}): Promise<Buffer> {
 		return new Promise((resolve, reject) => {
 			fs.readFile(this.path, options, (err, data) => {
 				err ? reject(err) : resolve(data as Buffer);
