595b2eb0144922c4ce555122435c801e71ebf297
PartMan <47669599+PartMan7@users.noreply.github.com>
2021-09-19 04:24:42 +0530

    /help: Trim commands (#8461)
    ```
    [09:45:39] @PartMan: !h status
    Could not find help for '/status ' - displaying help for '/status' instead
    /status [note] - Sets a short note as your status, visible when users click your username.
    Use /clearstatus to clear your status message.
    ```

--- server/chat-commands/core.ts
+++ server/chat-commands/core.ts
@@ -1774,7 +1774,7 @@ export const commands: Chat.ChatCommands = {
 	man: 'help',
 	help(target, room, user) {
 		if (!this.runBroadcast()) return;
-		target = target.toLowerCase();
+		target = target.toLowerCase().trim();
 		if (target.startsWith('/') || target.startsWith('!')) target = target.slice(1);
 
 		if (!target) {
