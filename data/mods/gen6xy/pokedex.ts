656806f1dc345eb0513f49f48aab4e78767e99c5
Karthik99999 <bandagondak0217@gmail.com>
2021-09-28 22:30:20 -0400

    gen 5 starter HAs

--- data/mods/gen6xy/pokedex.ts
+++ data/mods/gen6xy/pokedex.ts
@@ -15,6 +15,42 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 		inherit: true,
 		unreleasedHidden: true,
 	},
+	snivy: {
+		inherit: true,
+		unreleasedHidden: true,
+	},
+	servine: {
+		inherit: true,
+		unreleasedHidden: true,
+	},
+	serperior: {
+		inherit: true,
+		unreleasedHidden: true,
+	},
+	tepig: {
+		inherit: true,
+		unreleasedHidden: true,
+	},
+	pignite: {
+		inherit: true,
+		unreleasedHidden: true,
+	},
+	emboar: {
+		inherit: true,
+		unreleasedHidden: true,
+	},
+	oshawott: {
+		inherit: true,
+		unreleasedHidden: true,
+	},
+	dewott: {
+		inherit: true,
+		unreleasedHidden: true,
+	},
+	samurott: {
+		inherit: true,
+		unreleasedHidden: true,
+	},
 	skrelp: {
 		inherit: true,
 		unreleasedHidden: true,
