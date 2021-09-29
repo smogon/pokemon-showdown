2e1f6c78d80a1be11d9734d7eedf58cc9db50300
Karthik99999 <bandagondak0217@gmail.com>
2021-09-28 20:40:54 -0400

    Legality stuff

--- data/mods/gen5bw1/formats-data.ts
+++ data/mods/gen5bw1/formats-data.ts
@@ -11,4 +11,28 @@ export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
 		isNonstandard: "Future",
 		tier: "Illegal",
 	},
+	kyuremblack: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+	kyuremwhite: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+	keldeo: {
+		isNonstandard: "Unobtainable",
+		tier: "Unreleased",
+	},
+	keldeoresolute: {
+		isNonstandard: "Unobtainable",
+		tier: "Unreleased",
+	},
+	meloetta: {
+		isNonstandard: "Unobtainable",
+		tier: "Unreleased",
+	},
+	genesect: {
+		isNonstandard: "Unobtainable",
+		tier: "Unreleased",
+	},
 };
--- data/mods/gen6xy/formats-data.ts
+++ data/mods/gen6xy/formats-data.ts
@@ -1,4 +1,12 @@
 export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+	beedrillmega: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+	pidgeotmega: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
 	pikachucosplay: {
 		isNonstandard: "Future",
 		tier: "Illegal",
@@ -22,5 +30,97 @@ export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
 	pikachulibre: {
 		isNonstandard: "Future",
 		tier: "Illegal",
+	},
+	slowbromega: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+	steelixmega: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+	sceptilemega: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+	swampertmega: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+	sableyemega: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+	sharpedomega: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+	cameruptmega: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+	altariamega: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+	glaliemega: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+	salamencemega: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+	metagrossmega: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+	latiasmega: {
+		isNonstandard: "Unobtainable",
+		tier: "Unreleased",
+	},
+	latiosmega: {
+		isNonstandard: "Unobtainable",
+		tier: "Unreleased",
+	},
+	kyogreprimal: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+	groudonprimal: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+	rayquazamega: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+	lopunnymega: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+	gallademega: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+	audinomega: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+	dianciemega: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+	hoopa: {
+		isNonstandard: "Unobtainable",
+		tier: "Unreleased",
+	},
+	hoopaunbound: {
+		isNonstandard: "Future",
+		tier: "Illegal",
+	},
+	volcanion: {
+		isNonstandard: "Unobtainable",
+		tier: "Unreleased",
 	},
 };
