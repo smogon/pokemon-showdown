fe469cd9681bb288006f0f41d9ae3a1e732d77c4
Leonard Craft III <leonardcraft64@gmail.com>
2021-10-21 23:56:54 -0500

    I'm bad

--- test/sim/moves/hyperbeam.js
+++ test/sim/moves/hyperbeam.js
@@ -5,7 +5,7 @@ const common = require('./../../common');
 
 let battle;
 
-describe.only(`Hyper Beam`, function () {
+describe(`Hyper Beam`, function () {
 	afterEach(function () {
 		battle.destroy();
 	});
