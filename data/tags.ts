f65a967ba96d0ec832c50963856449317d87b2fc
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2021-10-28 18:41:55 -0600

    Move Mew to ND UUBL

--- data/tags.ts
+++ data/tags.ts
@@ -196,7 +196,7 @@ export const Tags: {[id: string]: TagData} = {
 		name: "ND UUBL",
 		speciesFilter: species => [
 			'Aerodactyl-Mega', 'Azumarill', 'Blacephalon', 'Diancie-Mega', 'Gallade-Mega', 'Gardevoir-Mega', 'Gengar', 'Gyarados', 'Gyarados-Mega',
-			'Hawlucha', 'Heracross-Mega', 'Hoopa-Unbound', 'Hydreigon', 'Jirachi', 'Latias', 'Latias-Mega', 'Latios', 'Latios-Mega', 'Manaphy',
+			'Hawlucha', 'Heracross-Mega', 'Hoopa-Unbound', 'Hydreigon', 'Jirachi', 'Latias', 'Latias-Mega', 'Latios', 'Latios-Mega', 'Manaphy', 'Mew',
 			'Pinsir-Mega', 'Sableye-Mega', 'Slowbro-Mega', 'Slowking-Galar', 'Thundurus', 'Thundurus-Therian', 'Venusaur-Mega', 'Xurkitree', 'Zapdos-Galar',
 		].includes(species.name),
 	},
