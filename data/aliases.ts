862a9dbc3afbfbc1eeab88459c3e87b06777bc29
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2021-11-01 15:49:11 -0600

    Update OMotM/LCotM aliases

--- data/aliases.ts
+++ data/aliases.ts
@@ -65,8 +65,8 @@ export const Aliases: {[alias: string]: string} = {
 	gen6mono: "[Gen 6] Monotype",
 	gen6ag: "[Gen 6] Anything Goes",
 	crossevo: "[Gen 8] Cross Evolution",
-	omotm: "[Gen 8] Multibility",
-	lcotm: "[Gen 8] 350 Cup",
+	omotm: "[Gen 8] Sketchmons",
+	lcotm: "[Gen 8] AAA Doubles",
 
 	// mega evos
 	fabio: "Ampharos-Mega",
