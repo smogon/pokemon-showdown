40f9fee5fe2c7b174a14bc9ec948fdf4ca2a94bf
Leonard Craft III <leonardcraft64@gmail.com>
2021-11-05 23:15:03 -0500

    Dex: Remove unobtainable Gen IV Abilities

---
+++ data/mods/gen3/pokedex.ts
@@ -0,0 +1,358 @@
+export const Pokedex: {[k: string]: ModdedSpeciesData} = {
+	pidgey: {
+		inherit: true,
+		abilities: {0: "Keen Eye"},
+	},
+	pidgeotto: {
+		inherit: true,
+		abilities: {0: "Keen Eye"},
+	},
+	pidgeot: {
+		inherit: true,
+		abilities: {0: "Keen Eye"},
+	},
+	nidoranf: {
+		inherit: true,
+		abilities: {0: "Poison Point"},
+	},
+	nidorina: {
+		inherit: true,
+		abilities: {0: "Poison Point"},
+	},
+	nidoqueen: {
+		inherit: true,
+		abilities: {0: "Poison Point"},
+	},
+	nidoranm: {
+		inherit: true,
+		abilities: {0: "Poison Point"},
+	},
+	nidorino: {
+		inherit: true,
+		abilities: {0: "Poison Point"},
+	},
+	nidoking: {
+		inherit: true,
+		abilities: {0: "Poison Point"},
+	},
+	clefairy: {
+		inherit: true,
+		abilities: {0: "Cute Charm"},
+	},
+	clefable: {
+		inherit: true,
+		abilities: {0: "Cute Charm"},
+	},
+	paras: {
+		inherit: true,
+		abilities: {0: "Effect Spore"},
+	},
+	parasect: {
+		inherit: true,
+		abilities: {0: "Effect Spore"},
+	},
+	venonat: {
+		inherit: true,
+		abilities: {0: "Compound Eyes"},
+	},
+	venomoth: {
+		inherit: true,
+		abilities: {0: "Shield Dust"},
+	},
+	meowth: {
+		inherit: true,
+		abilities: {0: "Pickup"},
+	},
+	persian: {
+		inherit: true,
+		abilities: {0: "Limber"},
+	},
+	mankey: {
+		inherit: true,
+		abilities: {0: "Vital Spirit"},
+	},
+	primeape: {
+		inherit: true,
+		abilities: {0: "Vital Spirit"},
+	},
+	machop: {
+		inherit: true,
+		abilities: {0: "Guts"},
+	},
+	machoke: {
+		inherit: true,
+		abilities: {0: "Guts"},
+	},
+	machamp: {
+		inherit: true,
+		abilities: {0: "Guts"},
+	},
+	seel: {
+		inherit: true,
+		abilities: {0: "Thick Fat"},
+	},
+	dewgong: {
+		inherit: true,
+		abilities: {0: "Thick Fat"},
+	},
+	shellder: {
+		inherit: true,
+		abilities: {0: "Shell Armor"},
+	},
+	cloyster: {
+		inherit: true,
+		abilities: {0: "Shell Armor"},
+	},
+	drowzee: {
+		inherit: true,
+		abilities: {0: "Insomnia"},
+	},
+	hypno: {
+		inherit: true,
+		abilities: {0: "Insomnia"},
+	},
+	hitmonlee: {
+		inherit: true,
+		abilities: {0: "Limber"},
+	},
+	hitmonchan: {
+		inherit: true,
+		abilities: {0: "Keen Eye"},
+	},
+	tangela: {
+		inherit: true,
+		abilities: {0: "Chlorophyll"},
+	},
+	kangaskhan: {
+		inherit: true,
+		abilities: {0: "Early Bird"},
+	},
+	horsea: {
+		inherit: true,
+		abilities: {0: "Swift Swim"},
+	},
+	seadra: {
+		inherit: true,
+		abilities: {0: "Poison Point"},
+	},
+	mrmmime: {
+		inherit: true,
+		abilities: {0: "Soundproof"},
+	},
+	scyther: {
+		inherit: true,
+		abilities: {0: "Swarm"},
+	},
+	jynx: {
+		inherit: true,
+		abilities: {0: "Oblivious"},
+	},
+	pinsir: {
+		inherit: true,
+		abilities: {0: "Hyper Cutter"},
+	},
+	tauros: {
+		inherit: true,
+		abilities: {0: "Intimidate"},
+	},
+	eevee: {
+		inherit: true,
+		abilities: {0: "Run Away"},
+	},
+	porygon: {
+		inherit: true,
+		abilities: {0: "Trace"},
+	},
+	cleffa: {
+		inherit: true,
+		abilities: {0: "Cute Charm"},
+	},
+	hoppip: {
+		inherit: true,
+		abilities: {0: "Chlorophyll"},
+	},
+	skiploom: {
+		inherit: true,
+		abilities: {0: "Chlorophyll"},
+	},
+	jumpluff: {
+		inherit: true,
+		abilities: {0: "Chlorophyll"},
+	},
+	sunkern: {
+		inherit: true,
+		abilities: {0: "Chlorophyll"},
+	},
+	sunflora: {
+		inherit: true,
+		abilities: {0: "Chlorophyll"},
+	},
+	murkrow: {
+		inherit: true,
+		abilities: {0: "Insomnia"},
+	},
+	granbull: {
+		inherit: true,
+		abilities: {0: "Intimidate"},
+	},
+	scizor: {
+		inherit: true,
+		abilities: {0: "Swarm"},
+	},
+	shuckle: {
+		inherit: true,
+		abilities: {0: "Sturdy"},
+	},
+	teddiursa: {
+		inherit: true,
+		abilities: {0: "Pickup"},
+	},
+	ursaring: {
+		inherit: true,
+		abilities: {0: "Guts"},
+	},
+	swinub: {
+		inherit: true,
+		abilities: {0: "Oblivious"},
+	},
+	piloswine: {
+		inherit: true,
+		abilities: {0: "Oblivious"},
+	},
+	remoraid: {
+		inherit: true,
+		abilities: {0: "Hustle"},
+	},
+	octillery: {
+		inherit: true,
+		abilities: {0: "Suction Cups"},
+	},
+	kingdra: {
+		inherit: true,
+		abilities: {0: "Swift Swim"},
+	},
+	porygon2: {
+		inherit: true,
+		abilities: {0: "Trace"},
+	},
+	stantler: {
+		inherit: true,
+		abilities: {0: "Intimidate"},
+	},
+	smeargle: {
+		inherit: true,
+		abilities: {0: "Own Tempo"},
+	},
+	tyrogue: {
+		inherit: true,
+		abilities: {0: "Guts"},
+	},
+	hitmontop: {
+		inherit: true,
+		abilities: {0: "Intimidate"},
+	},
+	smoochum: {
+		inherit: true,
+		abilities: {0: "Oblivious"},
+	},
+	miltank: {
+		inherit: true,
+		abilities: {0: "Thick Fat"},
+	},
+	poochyena: {
+		inherit: true,
+		abilities: {0: "Run Away"},
+	},
+	mightyena: {
+		inherit: true,
+		abilities: {0: "Intimidate"},
+	},
+	zigzagzoon: {
+		inherit: true,
+		abilities: {0: "Pickup"},
+	},
+	linoone: {
+		inherit: true,
+		abilities: {0: "Pickup"},
+	},
+	shroomish: {
+		inherit: true,
+		abilities: {0: "Effect Spore"},
+	},
+	breloom: {
+		inherit: true,
+		abilities: {0: "Effect Spore"},
+	},
+	skitty: {
+		inherit: true,
+		abilities: {0: "Cute Charm"},
+	},
+	delcatty: {
+		inherit: true,
+		abilities: {0: "Cute Charm"},
+	},
+	sableye: {
+		inherit: true,
+		abilities: {0: "Keen Eye"},
+	},
+	illumise: {
+		inherit: true,
+		abilities: {0: "Oblivious"},
+	},
+	numel: {
+		inherit: true,
+		abilities: {0: "Oblivious"},
+	},
+	camerupt: {
+		inherit: true,
+		abilities: {0: "Magma Armor"},
+	},
+	spinda: {
+		inherit: true,
+		abilities: {0: "Own Tempo"},
+	},
+	barboach: {
+		inherit: true,
+		abilities: {0: "Oblivious"},
+	},
+	whiscash: {
+		inherit: true,
+		abilities: {0: "Oblivious"},
+	},
+	shuppet: {
+		inherit: true,
+		abilities: {0: "Insomnia"},
+	},
+	banette: {
+		inherit: true,
+		abilities: {0: "Insomnia"},
+	},
+	tropius: {
+		inherit: true,
+		abilities: {0: "Chlorophyll"},
+	},
+	absol: {
+		inherit: true,
+		abilities: {0: "Pressure"},
+	},
+	snorunt: {
+		inherit: true,
+		abilities: {0: "Inner Focus"},
+	},
+	glalie: {
+		inherit: true,
+		abilities: {0: "Inner Focus"},
+	},
+	spheal: {
+		inherit: true,
+		abilities: {0: "Thick Fat"},
+	},
+	sealeo: {
+		inherit: true,
+		abilities: {0: "Thick Fat"},
+	},
+	walrein: {
+		inherit: true,
+		abilities: {0: "Thick Fat"},
+	},
+}
