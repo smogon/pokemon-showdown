1fb50fa515c3a80a2f13ec843985c1567c9e85b2
Mia <49593536+mia-pi-git@users.noreply.github.com>
2021-11-09 09:16:59 -0600

    Convert the-cafe to TSX

--- server/chat-plugins/thecafe.tsx
+++ server/chat-plugins/thecafe.tsx
@@ -1,5 +1,6 @@
 import {FS} from '../../lib/fs';
 import {Utils} from '../../lib/utils';
+import preact from 'preact';
 
 const DISHES_FILE = 'config/chat-plugins/thecafe-foodfight.json';
 const FOODFIGHT_COOLDOWN = 5 * 60 * 1000;
@@ -109,7 +110,17 @@ export const commands: Chat.ChatCommands = {
 		if (!targetUser.foodfight) {
 			return this.errorReply(`${self ? `You don't` : `This user doesn't`} have an active Foodfight team.`);
 		}
-		return this.sendReplyBox(`<div class="ladder"><table style="text-align:center;"><tr><th colspan="7" style="font-size:10pt;">${self ? `Your` : `${targetUser.name}'s`} dish is: <u>${targetUser.foodfight.dish}</u></th></tr><tr><th>Team</th>${targetUser.foodfight.generatedTeam.map(mon => `<td><psicon pokemon="${mon}"/> ${mon}</td>`).join('')}</tr><tr><th>Ingredients</th>${targetUser.foodfight.ingredients.map(ingredient => `<td>${ingredient}</td>`).join('')}</tr></table></div>`);
+		return this.sendReplyBox(
+			<div class="ladder">
+				<table style="text-align:center;">
+					<tr><th colSpan={7} style="font-size:10pt;">
+						{self ? `Your` : `${targetUser.name}'s`} dish is: <u>{targetUser.foodfight.dish}</u>
+					</th></tr>
+					<tr><th>Team</th>{targetUser.foodfight.generatedTeam.map(mon => `<td><psicon pokemon="${mon}"/> ${mon}</td>`).join('')}</tr>
+					<tr><th>Ingredients</th>{targetUser.foodfight.ingredients.map(ingredient => `<td>${ingredient}</td>`).join('')}</tr>
+				</table>
+			</div>
+		);
 	},
 	addingredients: 'adddish',
 	adddish(target, room, user, connection, cmd) {
@@ -176,22 +187,22 @@ export const commands: Chat.ChatCommands = {
 export const pages: Chat.PageTable = {
 	foodfight(query, user, connection) {
 		if (!user.named) return Rooms.RETRY_AFTER_LOGIN;
-		let buf = `|title|Foodfight\n|pagehtml|<div class="pad ladder"><h2>Foodfight Dish list</h2>`;
+		this.title = 'Foodfight';
 		const room = Rooms.get('thecafe');
 		if (!room) return this.errorReply(`Room not found.`);
-		if (!user.can('mute', null, room)) {
-			return buf + `<p>Access denied</p></div>`;
-		}
+		this.checkCan('mute', null, room);
 		const content = Object.values(dishes).map(
-			([dish, ...ingredients]) => `<tr><td>${dish}</td><td>${ingredients.join(', ')}</td></tr>`
+			([dish, ...ingredients]) => <tr><td>{dish}</td><td>{ingredients.join(', ')}</td></tr>
 		).join('');
 
-		if (!content) {
-			buf += `<p>There are no dishes in the database.</p>`;
-		} else {
-			buf += `<table><tr><th><h3>Dishes</h3></th><th><h3>Ingredients</h3></th></tr>${content}</table>`;
-		}
-		buf += `</div>`;
-		return buf;
+		return <>
+			<div class="pad ladder">
+				<h2>Foodfight Dish list</h2>
+				{content ?
+					<table><tr><th><h3>Dishes</h3></th><th><h3>Ingredients</h3></th></tr>{content}</table> :
+					<p>There are no dishes in the database.</p>
+				}
+			</div>
+		</>
 	},
 };
