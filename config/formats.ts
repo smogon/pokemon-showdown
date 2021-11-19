474f16e207deb22f8cc9fa9b1e14964d443214ba
Annika <annika0uwu@gmail.com>
2021-11-18 21:59:12 -0800

    Add BDSP random battle format

--- config/formats.ts
+++ config/formats.ts
@@ -1704,6 +1704,14 @@ export const Formats: FormatList = [
 		column: 2,
 	},
 	{
+		name: "[Gen 8] BD/SP Random Battle",
+		desc: `Randomized teams of level-balanced Pok&eacute;mon with sets that are generated to be competitively viable.`,
+
+		mod: 'gen8bdsp',
+		team: 'random',
+		ruleset: ['[Gen 8] Random Battle'],
+	},
+	{
 		name: "[Gen 8] Monotype Random Battle",
 
 		mod: 'gen8',
