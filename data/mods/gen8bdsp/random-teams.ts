59b105d250c5c9497927ce6db959bf635c165d46
Annika <annika0uwu@gmail.com>
2021-11-18 22:13:11 -0800

    Fix BDSP crash

---
+++ data/mods/gen8bdsp/random-teams.ts
@@ -0,0 +1,7 @@
+// BDSP team generation logic is currently largely shared with Swsh
+
+import {RandomTeams} from '../../random-teams';
+
+export class RandomBDSPTeams extends RandomTeams {}
+
+export default RandomBDSPTeams;
