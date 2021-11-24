2e962ff92944451371c1d8300956a75542f9814c
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2021-11-23 20:01:23 -0700

    BDSP: Fix Arceus required items

--- data/mods/gen8bdsp/pokedex.ts
+++ data/mods/gen8bdsp/pokedex.ts
@@ -23,4 +23,72 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 		evoType: "levelExtra",
 		evoCondition: "near an Ice Rock",
 	},
+	arceusbug: {
+		inherit: true,
+		requiredItems: ["Insect Plate"],
+	},
+	arceusdark: {
+		inherit: true,
+		requiredItems: ["Dread Plate"],
+	},
+	arceusdragon: {
+		inherit: true,
+		requiredItems: ["Draco Plate"],
+	},
+	arceuselectric: {
+		inherit: true,
+		requiredItems: ["Zap Plate"],
+	},
+	arceusfairy: {
+		inherit: true,
+		requiredItems: ["Pixie Plate"],
+	},
+	arceusfighting: {
+		inherit: true,
+		requiredItems: ["Fist Plate"],
+	},
+	arceusfire: {
+		inherit: true,
+		requiredItems: ["Flame Plate"],
+	},
+	arceusflying: {
+		inherit: true,
+		requiredItems: ["Sky Plate"],
+	},
+	arceusghost: {
+		inherit: true,
+		requiredItems: ["Spooky Plate"],
+	},
+	arceusgrass: {
+		inherit: true,
+		requiredItems: ["Meadow Plate"],
+	},
+	arceusground: {
+		inherit: true,
+		requiredItems: ["Earth Plate"],
+	},
+	arceusice: {
+		inherit: true,
+		requiredItems: ["Icicle Plate"],
+	},
+	arceuspoison: {
+		inherit: true,
+		requiredItems: ["Toxic Plate"],
+	},
+	arceuspsychic: {
+		inherit: true,
+		requiredItems: ["Mind Plate"],
+	},
+	arceusrock: {
+		inherit: true,
+		requiredItems: ["Stone Plate"],
+	},
+	arceussteel: {
+		inherit: true,
+		requiredItems: ["Iron Plate"],
+	},
+	arceuswater: {
+		inherit: true,
+		requiredItems: ["Splash Plate"],
+	},
 };
