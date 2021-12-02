4f5b5af9c14453598a01dbb1f036aefb5c3dfc72
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2021-12-01 17:29:08 -0700

    BDSP UU: Re-add thread

--- config/formats.ts
+++ config/formats.ts
@@ -876,6 +876,9 @@ export const Formats: FormatList = [
 	},
 	{
 		name: "[Gen 8 BDSP] UU",
+		threads: [
+			`&bullet; <a href="https://www.smogon.com/forums/threads/3694307/">BDSP UU</a>`,
+		],
 
 		mod: 'gen8bdsp',
 		ruleset: ['[Gen 8 BDSP] OU'],
