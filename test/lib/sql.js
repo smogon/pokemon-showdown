c58a1ab2a0410fe449a3ff89a90935975f23b348
Mia <49593536+mia-pi-git@users.noreply.github.com>
2021-12-03 17:52:53 -0600

    Tests: Improve SQL wrapper tests

--- test/lib/sql.js
+++ test/lib/sql.js
@@ -5,29 +5,23 @@ const common = require('../common');
 
 const database = SQL(module, {file: `:memory:`, processes: 1});
 
-(common.hasModule('better-sqlite3') ? describe : describe.skip)(`SQLite worker wrapper`, async () => {
+const test = (common.hasModule('better-sqlite3') ? describe : describe.skip);
+test(`SQLite worker wrapper`, async function () {
 	// prepare statements and set up table
-	await database.exec(`CREATE TABLE IF NOT EXISTS test (col TEXT, col2 TEXT)`);
-	const select = await database.prepare(`SELECT * FROM test`); // 0
-	const insert = await database.prepare(`INSERT INTO test (col, col2) VALUES (?, ?)`); // 1
-	it(`should require you to prepare a statement before running`, async () => {
-		database.get('SELECT col FROM test').then(() => {
-			assert(false, 'expected error');
-		}).catch(() => {
-			assert(true, 'received error');
-		});
+	before(async () => {
+		await database.exec(`CREATE TABLE IF NOT EXISTS test (col TEXT, col2 TEXT)`);
+		this.select = await database.prepare(`SELECT * FROM test`); // 0
+		this.insert = await database.prepare(`INSERT INTO test (col, col2) VALUES (?, ?)`); // 1
 	});
 	it(`should support both statement strings and corresponding statement classes`, async () => {
 		await database.run(`INSERT INTO test (col, col2) VALUES (?, ?)`, ['a', 'b']);
-		await database.run(insert, ['a', 'b']);
+		await database.run(this.insert, ['a', 'b']);
 	});
 	it(`should support both inline and object params`, async () => {
-		const num = await database.prepare(`INSERT INTO test (col, col2) VALUES($col, $col2)`);
-		await database.run(insert, ['a', 'b']);
-		await database.run(num, {col: 'a', col2: 'b'});
+		await database.run(this.insert, ['a', 'b']);
+		await database.run(`INSERT INTO test (col, col2) VALUES($col, $col2)`, {col: 'a', col2: 'b'});
 	});
 	it(`should retrieve one line from Database.get`, async () => {
-		const result = await database.get(select);
-		assert(!!result, 1);
+		assert(!!await database.get(this.select), 1);
 	});
 });
