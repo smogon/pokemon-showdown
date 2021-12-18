8fc527e3fcea70f552aebc039b952c89cf377e9e
Mia <49593536+mia-pi-git@users.noreply.github.com>
2021-12-17 19:31:51 -0600

    Abuse-monitor: Support tracking stats

---
+++ databases/migrations/chat-plugins/v5.sql
@@ -0,0 +1,20 @@
+-- Creates stats tables for the Perspective moderation tool.
+BEGIN TRANSACTION;
+
+CREATE TABLE perspective_stats (
+	staff TEXT NOT NULL,
+	roomid TEXT NOT NULL PRIMARY KEY,
+	result TINYINT(1) NOT NULL,
+	date TEXT NOT NULL, -- YYYY-MM-DD - for easy stat searching.
+	timestamp INTEGER NOT NULL
+);
+
+CREATE INDEX staff_idx ON perspective_stats(staff);
+CREATE INDEX result_idx ON perspective_stats(result);
+CREATE INDEX date_idx ON perspective_stats(date);
+
+-- update database version
+UPDATE db_info SET value = '5' WHERE key = 'version';
+COMMIT;
+
+
