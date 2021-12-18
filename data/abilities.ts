5788ea70c844b1df6393626e3d474ae05ed9bcf4
Leonard Craft III <leonardcraft64@gmail.com>
2021-12-17 21:30:58 -0600

    Fix interaction of Dynamax Cannon and Parental Bond
    Thanks Anubis!

--- data/abilities.ts
+++ data/abilities.ts
@@ -2523,7 +2523,7 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 	parentalbond: {
 		onPrepareHit(source, target, move) {
 			if (move.category === 'Status' || move.selfdestruct || move.multihit) return;
-			if (['endeavor', 'fling', 'iceball', 'rollout'].includes(move.id)) return;
+			if (['dynamaxcannon', 'endeavor', 'fling', 'iceball', 'rollout'].includes(move.id)) return;
 			if (!move.flags['charge'] && !move.spreadHit && !move.isZ && !move.isMax) {
 				move.multihit = 2;
 				move.multihitType = 'parentalbond';
