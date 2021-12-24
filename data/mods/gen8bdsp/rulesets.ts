16ffe60f31d333d41909f9077f2aa2cddb33991f
Leonard Craft III <leonardcraft64@gmail.com>
2021-12-24 14:32:13 -0600

    BDSP: Update orphaned egg move list (#8584)

--- data/mods/gen8bdsp/rulesets.ts
+++ data/mods/gen8bdsp/rulesets.ts
@@ -4,7 +4,13 @@ export const Rulesets: {[k: string]: ModdedFormatData} = {
 		banlist: [
 			// No valid fathers and can't get in Grand Underground
 			'Taillow + Boomburst', 'Swellow + Boomburst',
-			'Chatot + Boomburst',
+			'Plusle + Tearful Look',
+			'Minun + Tearful Look',
+			'Luvdisc + Heal Pulse',
+			'Starly + Detect', 'Staravia + Detect', 'Staraptor + Detect',
+			'Chatot + Boomburst', 'Chatot + Encore',
+			'Spiritomb + Foul Play',
+			'Munchlax + Power-Up Punch', 'Snorlax + Power-Up Punch',
 		],
 	},
 };
