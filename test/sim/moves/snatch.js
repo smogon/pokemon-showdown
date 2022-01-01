fa4944de5ad17ddbb381bc907c7a3d2c4595a396
Karthik <32044378+Karthik99999@users.noreply.github.com>
2022-01-01 18:07:30 -0500

    Gen 3-4: Fix Snatch when used by multiple pokemon (#8592)

--- test/sim/moves/snatch.js
+++ test/sim/moves/snatch.js
@@ -77,3 +77,36 @@ describe('Snatch', function () {
 		assert.match(battle.log[battle.lastMoveLine + 2], /^\|cant.*move: Heal Block\|Recover$/, 'should log that Recover failed');
 	});
 });
+
+describe('Snatch [Gen 4]', function () {
+	afterEach(function () {
+		battle.destroy();
+	});
+
+	it('should Snatch moves that were called by another user of Snatch', function () {
+		battle = common.gen(4).createBattle({gameType: 'doubles'}, [[
+			{species: 'weavile', moves: ['snatch']},
+			{species: 'wynaut', moves: ['howl']},
+		], [
+			{species: 'alakazam', moves: ['snatch']},
+			{species: 'wynaut', moves: ['sleeptalk']},
+		]]);
+
+		battle.makeChoices();
+		assert.statStage(battle.p2.active[0], 'atk', 1);
+	});
+
+	it.skip(`should only deduct additional PP from Snatch if the Snatch was successful`, function () {
+		battle = common.gen(4).createBattle([[
+			{species: 'Palkia', ability: 'pressure', moves: ['watergun', 'calmmind']},
+		], [
+			{species: 'Dialga', moves: ['snatch']},
+		]]);
+		battle.makeChoices();
+		const move = battle.p2.active[0].getMoveData(Dex.moves.get('snatch'));
+		assert.equal(move.pp, move.maxpp - 1, `Snatch should only lose 1 PP because it was not successful.`);
+
+		battle.makeChoices('move calmmind', 'move snatch');
+		assert.equal(move.pp, move.maxpp - 3, `Snatch should be at 13 PP after losing 1 PP earlier and 2 PP this turn`);
+	});
+});
