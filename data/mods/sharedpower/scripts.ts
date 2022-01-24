8a99f0f8bc76163bde6bacab4c8bdd66fa3fbec1
Hisuian Zoroark <96159984+HisuianZoroark@users.noreply.github.com>
2022-01-24 00:13:58 -0500

    Shared Power: Fix sharing weather suppression (#8637)
    * Shared Power: Fix weather suppresion sharing
    
    * Update scripts.ts
    
    Co-authored-by: Kris Johnson <11083252+KrisXV@users.noreply.github.com>

--- data/mods/sharedpower/scripts.ts
+++ data/mods/sharedpower/scripts.ts
@@ -5,7 +5,9 @@ export const Scripts: ModdedBattleScriptsData = {
 			for (const pokemon of this.battle.getAllActive()) {
 				const innates = Object.keys(pokemon.volatiles).filter(x => x.startsWith('ability:'));
 				if (pokemon && !pokemon.ignoringAbility() &&
-					(pokemon.getAbility().suppressWeather || innates.some(x => this.battle.dex.abilities.get(x).suppressWeather))) {
+					(pokemon.getAbility().suppressWeather || innates.some(x => (
+						this.battle.dex.abilities.get(x.replace('ability:', '')).suppressWeather
+					)))) {
 					return true;
 				}
 			}
