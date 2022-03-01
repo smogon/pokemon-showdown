fc1c9d4ec0696e07dbc26eec5cb610cc1198c9ea
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2022-03-01 00:49:30 -0700

    Remove OMotM/LCotM aliases

--- data/aliases.ts
+++ data/aliases.ts
@@ -67,8 +67,6 @@ export const Aliases: {[alias: string]: string} = {
 	gen6mono: "[Gen 6] Monotype",
 	gen6ag: "[Gen 6] Anything Goes",
 	crossevo: "[Gen 8] Cross Evolution",
-	omotm: "[Gen 8] Trademarked",
-	lcotm: "[Gen 8] Linked",
 
 	// mega evos
 	fabio: "Ampharos-Mega",
