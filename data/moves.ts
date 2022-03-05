0697a0eaf5d730a86e61404554c821014e6d854b
Kate Skye N <sjn51406+1@gmail.com>
2022-03-05 16:11:09 -0500

    wind scythe now affected by strong winds

--- data/moves.ts
+++ data/moves.ts
@@ -22525,7 +22525,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 		name: "Wind Scythe",
 		pp: 15,
 		priority: 0,
-		flags: {contact: 1, protect: 1, mirror: 1, blade: 1},
+		flags: {contact: 1, protect: 1, mirror: 1, blade: 1, wind: 1},
 		critRatio: 2,
 		secondary: null,
 		target: "normal",
