2c03a030528f5f808355e89ad07ed9fd983c3009
Marty-D <Marty-D@users.noreply.github.com>
2022-03-05 09:20:35 -0500

    Add Lisia avatar

--- server/chat-commands/avatars.tsx
+++ server/chat-commands/avatars.tsx
@@ -542,7 +542,7 @@ const OFFICIAL_AVATARS_GNOMOWLADNY = new Set([
 
 const OFFICIAL_AVATARS_BRUMIRAGE = new Set([
 	'agatha-lgpe', 'allister', 'archie-gen6', 'avery', 'ballguy', 'bea', 'bede', 'bede-leader', 'brendan-contest',
-	'doctor-gen8', 'elaine', 'gloria', 'gordie', 'hop', 'kabu', 'klara', 'koga-lgpe', 'leon', 'leon-tower',
+	'doctor-gen8', 'elaine', 'gloria', 'gordie', 'hop', 'kabu', 'klara', 'koga-lgpe', 'leon', 'leon-tower', 'lisia',
 	'lorelei-lgpe', 'magnolia', 'marnie', 'may-contest', 'melony', 'milo', 'mustard', 'mustard-master', 'nessa',
 	'oleana', 'opal', 'peonia', 'peony', 'phoebe-gen6', 'piers', 'raihan', 'rose', 'shielbert', 'sonia',
 	'sonia-professor', 'sordward', 'tateandliza-gen6', 'victor', 'victor-dojo', 'yellgrunt', 'yellgruntf',
