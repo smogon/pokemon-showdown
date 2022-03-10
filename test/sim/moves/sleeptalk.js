c9dfdb847bcf8835a697b200fa650d32b7d831be
Leonard Craft III <leonardcraft64@gmail.com>
2022-03-09 20:23:52 -0600

    Fix failing tests using regex on Windows

--- test/sim/moves/sleeptalk.js
+++ test/sim/moves/sleeptalk.js
@@ -11,13 +11,15 @@ describe('Sleep Talk', function () {
 	});
 
 	it('should run conditions for submove', function () {
-		battle = common.createBattle();
-		battle.setPlayer('p1', {team: [{species: 'snorlax', moves: ['sleeptalk', 'highjumpkick']}]});
-		battle.setPlayer('p2', {team: [{species: 'breloom', moves: ['spore', 'gravity']}]});
+		battle = common.createBattle([[
+			{species: 'snorlax', ability: 'noguard', moves: ['sleeptalk', 'highjumpkick']},
+		], [
+			{species: 'breloom', moves: ['spore', 'gravity']},
+		]]);
 		battle.makeChoices('move sleeptalk', 'move gravity');
 		battle.makeChoices('move sleeptalk', 'move spore');
 		assert.fullHP(battle.p2.active[0]);
-		assert.match(battle.log[battle.lastMoveLine + 1], /^\|cant.*move: Gravity|High Jump Kick$/, 'should log that High Jump Kick failed');
+		assert(battle.log[battle.lastMoveLine + 1].startsWith('|cant'), 'should log that High Jump Kick failed');
 	});
 
 	it('should fail and lose PP on subsequent turns while Choice locked, prior to Gen 5', function () {
