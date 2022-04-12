f834b4bcb8211d36100c1fc4e8e863dd01a916b1
Pellevoisindev <bouret.benjamin@gmail.com>
2022-04-12 14:36:20 +0200

    Fixed speedstrike

--- data/moves.ts
+++ data/moves.ts
@@ -20704,7 +20704,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 		pp: 10,
 		priority: 0,
 		flags: {contact: 1, protect: 1, mirror: 1},
-		useSourceSpeedAsOffensive: true,
+		overrideOffensiveStat: 'spe',
 		secondary: null,
 		target: "normal",
 		type: "Normal",
