fa1b13ebc156ec7b2ec2c1631fe37732d939ed7e
Alexander B <4866817+MathyFurret@users.noreply.github.com>
2022-04-24 22:11:29 -0500

    add HDB checks back to SSB

--- data/mods/ssb/conditions.ts
+++ data/mods/ssb/conditions.ts
@@ -2394,6 +2394,7 @@ export const Conditions: {[k: string]: ModdedConditionData & {innateName?: strin
 			this.add('-sidestart', side, 'move: G-Max Steelsurge');
 		},
 		onEntryHazard(pokemon) {
+			if (pokemon.hasItem('heavydutyboots')) return;
 			// Ice Face and Disguise correctly get typed damage from Stealth Rock
 			// because Stealth Rock bypasses Substitute.
 			// They don't get typed damage from Steelsurge because Steelsurge doesn't,
@@ -2420,7 +2421,7 @@ export const Conditions: {[k: string]: ModdedConditionData & {innateName?: strin
 			this.effectState.layers++;
 		},
 		onEntryHazard(pokemon) {
-			if (!pokemon.isGrounded()) return;
+			if (!pokemon.isGrounded() || pokemon.hasItem('heavydutyboots')) return;
 			const damageAmounts = [0, 3, 4, 6]; // 1/8, 1/6, 1/4
 			this.damage(damageAmounts[this.effectState.layers] * pokemon.maxhp / 24);
 		},
@@ -2435,6 +2436,7 @@ export const Conditions: {[k: string]: ModdedConditionData & {innateName?: strin
 			this.add('-sidestart', side, 'move: Stealth Rock');
 		},
 		onEntryHazard(pokemon) {
+			if (pokemon.hasItem('heavydutyboots')) return;
 			const typeMod = this.clampIntRange(pokemon.runEffectiveness(this.dex.getActiveMove('stealthrock')), -6, 6);
 			this.damage(pokemon.maxhp * Math.pow(2, typeMod) / 8);
 		},
@@ -2449,7 +2451,7 @@ export const Conditions: {[k: string]: ModdedConditionData & {innateName?: strin
 			this.add('-sidestart', side, 'move: Sticky Web');
 		},
 		onEntryHazard(pokemon) {
-			if (!pokemon.isGrounded()) return;
+			if (!pokemon.isGrounded() || pokemon.hasItem('heavydutyboots')) return;
 			this.add('-activate', pokemon, 'move: Sticky Web');
 			this.boost({spe: -1}, pokemon, pokemon.side.foe.active[0], this.dex.getActiveMove('stickyweb'));
 		},
@@ -2474,7 +2476,7 @@ export const Conditions: {[k: string]: ModdedConditionData & {innateName?: strin
 			if (pokemon.hasType('Poison')) {
 				this.add('-sideend', pokemon.side, 'move: Toxic Spikes', '[of] ' + pokemon);
 				pokemon.side.removeSideCondition('toxicspikes');
-			} else if (pokemon.hasType('Steel')) {
+			} else if (pokemon.hasType('Steel') || pokemon.hasItem('heavydutyboots')) {
 				return;
 			} else if (this.effectState.layers >= 2) {
 				pokemon.trySetStatus('tox', pokemon.side.foe.active[0]);
