04593b070bc640fbd635c132c9a351a3f017be5f
Karthik <32044378+Karthik99999@users.noreply.github.com>
2022-04-27 10:32:14 -0400

    remove brackets from if statement

--- sim/battle.ts
+++ sim/battle.ts
@@ -2230,9 +2230,7 @@ export class Battle {
 				// even if a move can target an ally, auto-resolution will never make it target an ally
 				// i.e. if both your opponents faint before you use Flamethrower, it will fail instead of targeting your ally
 				const adjacentFoes = pokemon.adjacentFoes();
-				if (adjacentFoes.length) {
-					return this.sample(adjacentFoes);
-				}
+				if (adjacentFoes.length) return this.sample(adjacentFoes);
 				// no valid target at all, return slot directly across for any possible redirection
 				return pokemon.side.foe.active[pokemon.side.foe.active.length - 1 - pokemon.position];
 			}
