63b2b4afd62d945146075e1f143618ef9fa0dc75
Leonard Craft III <leonardcraft64@gmail.com>
2022-04-28 14:03:06 -0500

    Gen VIII: Implement Fly animation target leak (#8724)

--- data/moves.ts
+++ data/moves.ts
@@ -5391,6 +5391,17 @@ export const Moves: {[moveid: string]: MoveData} = {
 			if (!this.runEvent('ChargeMove', attacker, defender, move)) {
 				return;
 			}
+
+			// In SwSh, Fly's animation leaks the initial target through a camera focus
+			// The animation leak target itself isn't "accurate"; the target it reveals is as if Fly weren't a charge movee
+			// (Fly, like all other charge moves, will actually target slots on its charging turn, relevant for things like Follow Me)
+			// We use a generic single-target move to represent this
+			if (this.gameType === 'doubles' || this.gameType === 'multi') {
+				const animatedTarget = attacker.getMoveTargets(this.dex.getActiveMove('aerialace'), defender).targets[0];
+				if (animatedTarget) {
+					this.hint(`${move.name}'s animation targeted ${animatedTarget.name}`);
+				}
+			}
 			attacker.addVolatile('twoturnmove', defender);
 			return null;
 		},
