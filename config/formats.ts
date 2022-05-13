f81de30a7cdcabadd6d9e0bd38f80b755c5bad61
Marty-D <Marty-D@users.noreply.github.com>
2022-05-12 21:32:05 -0400

    Doubles LC: Unban Alolan Vulpix for suspect
    https://www.smogon.com/forums/posts/9209639/

--- config/formats.ts
+++ config/formats.ts
@@ -359,7 +359,7 @@ export const Formats: FormatList = [
 		gameType: 'doubles',
 		searchShow: false,
 		ruleset: ['Standard Doubles', 'Little Cup', 'Dynamax Clause', 'Swagger Clause', 'Sleep Clause Mod'],
-		banlist: ['Corsola-Galar', 'Cutiefly', 'Ponyta-Base', 'Scyther', 'Sneasel', 'Swirlix', 'Tangela', 'Vulpix', 'Vulpix-Alola', 'Ally Switch'],
+		banlist: ['Corsola-Galar', 'Cutiefly', 'Ponyta-Base', 'Scyther', 'Sneasel', 'Swirlix', 'Tangela', 'Vulpix-Base', 'Ally Switch'],
 	},
 	{
 		name: "[Gen 8] VGC 2022",
