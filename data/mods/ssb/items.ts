1f0b4a1c1a0eea9883fed3edac4ce6eb695f42ba
Bamd <105554510+BamdTheBamd@users.noreply.github.com>
2022-05-16 19:36:27 +0600

    items

--- data/mods/ssb/items.ts
+++ data/mods/ssb/items.ts
@@ -1,45 +1,66 @@
 export const Items: {[k: string]: ModdedItemData} = {
-	// Alpha
-	caioniumz: {
-		name: "Caionium Z",
-		onTakeItem: false,
-		zMove: "Blistering Ice Age",
-		zMoveFrom: "Blizzard",
-		itemUser: ["Aurorus"],
+	// Brookeee
+	ramen: {
+		name: "Ramen",
+		spritenum: 22,
+		fling: {
+			basePower: 10,
+		},
+		onAfterSetStatusPriority: -1,
+		onAfterSetStatus(status, pokemon) {
+			pokemon.eatItem();
+		},
+		onUpdate(pokemon) {
+			if (pokemon.status || pokemon.volatiles['confusion'] || pokemon.hp <= pokemon.maxhp < 4 || (pokemon.maxhp <= pokemon.maxhp < 2 && pokemon.hasAbility('gluttony'))) {
+				pokemon.eatItem();
+			}
+		},
+		onEat(pokemon) {
+			this.boost({spe: 1});
+			pokemon.cureStatus();
+			pokemon.removeVolatile('confusion');
+			pokemon.addVolatile('focusenergy');
+		},
+		onSourceModifyDamage(damage, source, target, move) {
+			if (move.type === 'Fairy' || move.type === 'Flying' || move.type === 'Psychic' && target.getMoveHitData(move).typeMod > 0) {
+				const hitSub = target.volatiles['substitute'] && !move.flags['bypasssub'] && !(move.infiltrates && this.gen >= 6);
+				if (hitSub) return;
+
+				if (target.eatItem()) {
+					this.debug('-50% reduction');
+					this.add('-enditem', target, this.effect, '[weaken]');
+					return this.chainModify(0.5);
+				}
+			}
+		},
 		gen: 8,
-		desc: "If held by an Aurorus with Blizzard, it can use Blistering Ice Age.",
+		desc: "Raises Speed and critical hit ratio by 1 stage if at 25% of HP or when it cures status or halves damage taken from a Fairy/Flying/Psychic-type attack.",
 	},
 
-	// A Quag To The Past
-	quagniumz: {
-		name: "Quagnium Z",
+	// Horrific17
+	horrifumz: {
+		name: "Horrifium Z",
+		spritenum: 632,
 		onTakeItem: false,
-		zMove: "Bounty Place",
-		zMoveFrom: "Scorching Sands",
-		itemUser: ["Quagsire"],
+		zMove: "Final Trick",
+		zMoveFrom: "Meteor Charge",
+		itemUser: ["Arcanine"],
 		gen: 8,
-		desc: "If held by a Quagsire with Scorching Sands, it can use Bounty Place.",
+		desc: "If held by an Arcanine with Meteor Charge, it can use Final Trick.",
 	},
 
-	// Kalalokki
-	kalalokkiumz: {
-		name: "Kalalokkium Z",
-		onTakeItem: false,
-		zMove: "Gaelstrom",
-		zMoveFrom: "Blackbird",
-		itemUser: ["Wingull"],
+	// Mayie
+	lunchbox: {
+		spritenum: 242,
+		fling: {
+			basePower: 10,
+		},
+		onResidualOrder: 5,
+		onResidualSubOrder: 4,
+		onResidual(pokemon) {
+			this.heal(pokemon.baseMaxhp / 8);
+		},
 		gen: 8,
-		desc: "If held by a Wingull with Blackbird, it can use Gaelstrom.",
-	},
-
-	// Robb576
-	modium6z: {
-		name: "Modium-6 Z",
-		onTakeItem: false,
-		zMove: "Integer Overflow",
-		zMoveFrom: "Photon Geyser",
-		itemUser: ["Necrozma-Ultra"],
-		gen: 8,
-		desc: "If held by a Robb576 with Photon Geyser, it can use Integer Overflow.",
+		desc: "At the end of every turn, holder restores 1/16 of its max HP.",
 	},
 };
