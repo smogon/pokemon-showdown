7fb23878ed7d5e3f622378191f9994799971bd89
Zakuree <zach.hartin@gmail.com>
2022-05-26 12:34:54 -0400

    Update pokedex.ts

--- data/mods/ssb/pokedex.ts
+++ data/mods/ssb/pokedex.ts
@@ -9,14 +9,14 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 		inherit: true,
 		baseStats: {hp: 100, atk: 100, def: 80, spa: 80, spd: 70, spe: 115},
 	},
+	// Finger
+	reuniclus: {
+		inherit: true,
+		baseStats: {hp: 110, atk: 85, def: 75, spa: 125, spd: 105, spe: 30},
+	},
 	// Genwunner
 	alakazam: {
 		inherit: true,
 		baseStats: {hp: 55, atk: 50, def: 45, spa: 135, spd: 135, spe: 120},
-	},
-	// Minimind
-	clefable: {
-		inherit: true,
-		baseStats: {hp: 95, atk: 95, def: 73, spa: 95, spd: 90, spe: 60},
 	},
 };
