0f87aa91574c6db19644544c248a233c950c8468
Zakuree <zach.hartin@gmail.com>
2022-05-26 23:57:05 -0400

    Update conditions.ts

--- data/mods/ssb/conditions.ts
+++ data/mods/ssb/conditions.ts
@@ -1,2 +1,26 @@
 export const Conditions: {[k: string]: ModdedConditionData & {innateName?: string}} = {
+	badtox: {
+		name: 'badtox',
+		effectType: 'Status',
+		onStart(target, source, sourceEffect) {
+			this.effectState.stage = 0;
+			if (sourceEffect && sourceEffect.id === 'toxicorb') {
+				this.add('-status', target, 'tox', '[from] item: Toxic Orb');
+			} else if (sourceEffect && sourceEffect.effectType === 'Ability') {
+				this.add('-status', target, 'tox', '[from] ability: ' + sourceEffect.name, '[of] ' + source);
+			} else {
+				this.add('-status', target, 'tox');
+			}
+		},
+		onSwitchIn() {
+			this.effectState.stage = 0;
+		},
+		onResidualOrder: 9,
+		onResidual(pokemon) {
+			if (this.effectState.stage < 15) {
+				this.effectState.stage++;
+			}
+			this.damage(this.clampIntRange(pokemon.baseMaxhp / 8, 1) * this.effectState.stage);
+		},
+	},
 };
