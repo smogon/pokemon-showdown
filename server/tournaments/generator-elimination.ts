0cb8dced444d11c8f7ca3bfaccf863943918f1be
Alexander B <4866817+MathyFurret@users.noreply.github.com>
2022-06-01 08:53:35 -0500

    prevent infinite loop(?) on disqualification

--- server/tournaments/generator-elimination.ts
+++ server/tournaments/generator-elimination.ts
@@ -393,7 +393,7 @@ export class Elimination {
 		if (targetNode.parent) {
 			const parent = targetNode.parent;
 
-			if (loser.losses <= winner.losses) {
+			if (loser.losses <= winner.losses && !loser.isDisqualified) {
 				// grand subfinals rematch
 				const newNode = new ElimNode({state: 'available', losersBracketNode: targetNode.losersBracketNode});
 				newNode.setChildren([targetNode, new ElimNode({user: loser})]);
