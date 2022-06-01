31b778d02f8f8f6fd118a724c6a0aa4c37b98735
Zakuree <zach.hartin@gmail.com>
2022-05-31 22:28:02 -0400

    Update abilities.ts

--- data/mods/ssb/abilities.ts
+++ data/mods/ssb/abilities.ts
@@ -575,6 +575,43 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
 		gen: 8,
 	},
 
+	// The Dealer
+	croupier: {
+		desc: "This Pokemon can only be damaged by direct attacks, is unaffected by opposing priority moves, will always hit its moves, and will always be hit by incoming moves.",
+		shortDesc: "Magic Guard + Dazzling + No Guard.",
+		onDamage(damage, target, source, effect) {
+			if (effect.effectType !== 'Move') {
+				if (effect.effectType === 'Ability') this.add('-activate', source, 'ability: ' + effect.name);
+				return false;
+			}
+		},
+		onFoeTryMove(target, source, move) {
+			const targetAllExceptions = ['perishsong', 'flowershield', 'rototiller'];
+			if (move.target === 'foeSide' || (move.target === 'all' && !targetAllExceptions.includes(move.id))) {
+				return;
+			}
+
+			const dazzlingHolder = this.effectState.target;
+			if ((source.isAlly(dazzlingHolder) || move.target === 'all') && move.priority > 0.1) {
+				this.attrLastMove('[still]');
+				this.add('cant', dazzlingHolder, 'ability: Dazzling', move, '[of] ' + target);
+				return false;
+			}
+		},
+		onAnyInvulnerabilityPriority: 1,
+		onAnyInvulnerability(target, source, move) {
+			if (move && (source === this.effectState.target || target === this.effectState.target)) return 0;
+		},
+		onAnyAccuracy(accuracy, target, source, move) {
+			if (move && (source === this.effectState.target || target === this.effectState.target)) {
+				return true;
+			}
+			return accuracy;
+		},
+		name: "Croupier",
+		gen: 8,
+	},
+
 	// Tonberry
 	vindictive: {
 		desc: "This Pokemon deals double damage to the target if an ally fainted last turn.",
