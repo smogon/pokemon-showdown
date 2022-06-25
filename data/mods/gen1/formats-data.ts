54a92af73dbfe65b378b37a963696f5466c032c2
May Evans <36418502+PlagueVonKarma@users.noreply.github.com>
2022-06-25 21:44:12 +0100

    Gen 1 OU tier changes (#8820)

--- data/mods/gen1/formats-data.ts
+++ data/mods/gen1/formats-data.ts
@@ -370,7 +370,7 @@ export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
 		randomBattleMoves: ["bodyslam", "sleeppowder", "stunspore"],
 		essentialMove: "razorleaf",
 		comboMoves: ["hyperbeam", "swordsdance"],
-		tier: "(OU)",
+		tier: "UU",
 	},
 	tentacool: {
 		randomBattleMoves: ["barrier", "hydropump", "surf"],
@@ -414,7 +414,7 @@ export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
 	slowbro: {
 		randomBattleMoves: ["amnesia", "surf", "thunderwave"],
 		exclusiveMoves: ["blizzard", "psychic", "rest", "rest"],
-		tier: "OU",
+		tier: "UUBL",
 	},
 	magnemite: {
 		randomBattleMoves: ["thunder", "thunderbolt", "thunderwave"],
@@ -667,7 +667,7 @@ export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
 		randomBattleMoves: ["bodyslam", "confuseray", "rest", "sing", "surf"],
 		essentialMove: "blizzard",
 		exclusiveMoves: ["thunderbolt", "thunderbolt"],
-		tier: "(OU)",
+		tier: "UUBL",
 	},
 	ditto: {
 		randomBattleMoves: ["transform"],
