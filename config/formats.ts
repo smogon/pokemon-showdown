7891a88766779600982229f941236b141ca70085
Marty-D <Marty-D@users.noreply.github.com>
2022-06-25 19:28:30 -0400

    Fix build

--- config/formats.ts
+++ config/formats.ts
@@ -3284,7 +3284,7 @@ export const Formats: FormatList = [
 
 		mod: 'gen5',
 		searchShow: false,
-		ruleset: ['[Gen 5] NU', '!Sleep Clause Mod', 'Sleep Moves Clause'],
+		ruleset: ['[Gen 5] NU', 'Sleep Moves Clause'],
 		banlist: ['NU', 'PUBL', 'Damp Rock'],
 	},
 	{
