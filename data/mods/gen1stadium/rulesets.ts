961d34cf62aa4c74f5db8a47aeec03b1ceba03c6
May Evans <36418502+PlagueVonKarma@users.noreply.github.com>
2022-07-13 23:30:22 +0100

    .

--- data/mods/gen1stadium/rulesets.ts
+++ data/mods/gen1stadium/rulesets.ts
@@ -4,4 +4,15 @@ export const Rulesets: {[k: string]: ModdedFormatData} = {
 		name: 'Standard',
 		ruleset: ['Obtainable', 'Stadium Sleep Clause', 'Freeze Clause Mod', 'Species Clause', 'OHKO Clause', 'Evasion Moves Clause', 'Exact HP Mod', 'Cancel Mod'],
 	},
+	nintendocup1999movelegality: {
+		effectType: 'ValidatorRule',
+		name: 'Nintendo Cup 1999 Move Legality',
+		onValidateTeam(team) {
+			for (const set of team) {
+				if (eventData.japan) {
+					if (fastReturn) return true;
+				},
+			},
+		},
+	},
 };
