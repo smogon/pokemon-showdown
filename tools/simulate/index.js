1ddfc79b50e08df3d5a0e7ff58820dab66106059
Coppo92 <cartavelina92@icloud.com>
2022-07-22 12:49:16 +0200

    xx

--- tools/simulate/index.js
+++ tools/simulate/index.js
@@ -37,12 +37,12 @@ if (process.argv[2]) {
 }
 
 require('ts-node').register({project: './tsconfig.json', files: true, transpileOnly: true, transpiler: 'ts-node/transpilers/swc-experimental'});
-const Dex = require('../../sim/dex').Dex;
+const Dex = require('../../.sim-dist/dex').Dex;
 global.Config = {allowrequestingties: false};
 Dex.includeModData();
 
-const {ExhaustiveRunner} = require('../../sim/tools/exhaustive-runner');
-const {MultiRandomRunner} = require('../../sim/tools/multi-random-runner');
+const {ExhaustiveRunner} = require('../../.sim-dist/tools/exhaustive-runner');
+const {MultiRandomRunner} = require('../../.sim-dist/tools/multi-random-runner');
 
 // Tracks whether some promises threw errors that weren't caught so we can log
 // and exit with a non-zero status to fail any tests. This "shouldn't happen"
