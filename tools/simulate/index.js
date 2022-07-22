78679301d2af16d8a6c8df2a8bc7c65514fcc875
pacmanboss256 <pacmanboss972@gmail.com>
2022-07-22 16:51:06 -0500

    clean up one last change

--- tools/simulate/index.js
+++ tools/simulate/index.js
@@ -37,7 +37,7 @@ if (process.argv[2]) {
 }
 
 require('ts-node').register({project: './tsconfig.json', files: true, transpileOnly: true, transpiler: 'ts-node/transpilers/swc-experimental'});
-const Dex = require('../../.sim-dist/dex').Dex;
+const Dex = require('../../.sim/dex').Dex;
 global.Config = {allowrequestingties: false};
 Dex.includeModData();
 
