7bd8dfb4cbdc88113bdfc8dc7ad60788457e7c6b
Ace E Emel <acasualmemelord.com>
2022-07-25 03:40:18 -0700

    remove typo

--- config/formats.ts
+++ config/formats.ts
@@ -586,7 +586,6 @@ export const Formats: FormatList = [
 			// Items
 			'Gengarite',
 			// Moves
-			'Chatter', 'Double Iron Bash', 'Octolock',
 			'Chatter', 'Double Iron Bash', 'Octolock', 'Shell Smash',
 			// Other
 			'Comatose + Sleep Talk',
