05c01ac2b307aa9e8dd4dd66ae567d62a5a9b2ec
acasualmemelord <62784122+acasualmemelord@users.noreply.github.com>
2022-07-29 21:10:20 -0700

    Update data/mods/joltemons/conditions.ts
    Co-authored-by: Kris Johnson <11083252+KrisXV@users.noreply.github.com>

--- data/mods/joltemons/conditions.ts
+++ data/mods/joltemons/conditions.ts
@@ -3,7 +3,10 @@ export const Conditions: {[k: string]: ConditionData} = {
 		name: 'Silvally',
 		onTypePriority: 1,
 		onType(types, pokemon) {
-			if (pokemon.transformed || !(pokemon.ability === 'rkssystem' || pokemon.ability === 'powerofalchemysilvally') && this.gen >= 8) return types;
+			if (pokemon.transformed ||
+				!(pokemon.ability === 'rkssystem' || pokemon.ability === 'powerofalchemysilvally') && this.gen >= 8) {
+				return types;
+			}
 			let type: string | undefined = 'Normal';
 			if (pokemon.ability === 'rkssystem' || pokemon.ability === 'powerofalchemysilvally') {
 				type = pokemon.getItem().onMemory;
