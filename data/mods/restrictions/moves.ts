a764fa75e076fede2999289ad3eaa76dfa554a28
Annika <56906084+AnnikaCodes@users.noreply.github.com>
2022-08-07 14:57:55 -0700

    Revert "Add new Pet Mods of the Season (#8848)"
    This reverts commit 2146e2d3033dd1d4f295e79530a8fbe748ff606b.

---
+++ data/mods/restrictions/moves.ts
@@ -0,0 +1,10 @@
+export const Moves: {[k: string]: ModdedMoveData} = {
+	revelationdance: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	toxicthread: {
+		inherit: true,
+		isNonstandard: null,
+	},
+};
