97acb5c81cc720faef5fbbcc47e3f57b91c52eaf
Marty-D <Marty-D@users.noreply.github.com>
2022-08-19 18:10:44 -0400

    Update rulesets.ts

--- data/mods/gen1jpn/rulesets.ts
+++ data/mods/gen1jpn/rulesets.ts
@@ -1,5 +1,5 @@
 export const Rulesets: {[k: string]: ModdedFormatData} = {
-	standard: { // Includes considerations for Japanese event incompatibilities
+	standard: {
 		effectType: 'ValidatorRule',
 		name: 'Standard',
 		ruleset: ['Obtainable', 'Desync Clause Mod', 'Sleep Clause Mod', 'Freeze Clause Mod', 'Species Clause', 'OHKO Clause', 'Evasion Moves Clause', 'Endless Battle Clause', 'HP Percentage Mod', 'Cancel Mod'],
