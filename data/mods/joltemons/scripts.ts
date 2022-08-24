9276af006ce374168fde625bed65edb12abb6fb1
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2022-08-24 17:42:26 -0600

    Simplify function

--- data/mods/joltemons/scripts.ts
+++ data/mods/joltemons/scripts.ts
@@ -106,40 +106,6 @@ export const Scripts: ModdedBattleScriptsData = {
 		},
 	},
 	pokemon: {
-		runImmunity(type: string, message?: string | boolean) {
-			if (!type || type === '???') return true;
-			if (!(type in this.battle.dex.data.TypeChart)) {
-				if (type === 'Fairy' || type === 'Dark' || type === 'Steel') return true;
-				throw new Error("Use runStatusImmunity for " + type);
-			}
-			if (this.fainted) return false;
-
-			const negateResult = this.battle.runEvent('NegateImmunity', this, type);
-			let isGrounded;
-			if (type === 'Ground') {
-				isGrounded = this.isGrounded(!negateResult);
-				if (isGrounded === null) {
-					if (message) {
-						if (this.hasAbility('powerofalchemyweezing')) {
-							this.battle.add('-immune', this, '[from] ability: Power of Alchemy (Weezing)');
-						} else if (this.hasAbility('powerofalchemymismagius')) {
-							this.battle.add('-immune', this, '[from] ability: Power of Alchemy (Mismagius)');
-						} else {
-							this.battle.add('-immune', this, '[from] ability: Levitate');
-						}
-					}
-					return false;
-				}
-			}
-			if (!negateResult) return true;
-			if ((isGrounded === undefined && !this.battle.dex.getImmunity(type, this)) || isGrounded === false) {
-				if (message) {
-					this.battle.add('-immune', this);
-				}
-				return false;
-			}
-			return true;
-		},
 		isGrounded(negateImmunity = false) {
 			if ('gravity' in this.battle.field.pseudoWeather) return true;
 			if ('ingrain' in this.volatiles && this.battle.gen >= 4) return true;
@@ -149,9 +115,7 @@ export const Scripts: ModdedBattleScriptsData = {
 			// If a Fire/Flying type uses Burn Up and Roost, it becomes ???/Flying-type, but it's still grounded.
 			if (!negateImmunity && this.hasType('Flying') && !('roost' in this.volatiles)) return false;
 			if (
-				(this.hasAbility('levitate') ||
-				this.hasAbility('powerofalchemyweezing') ||
-				this.hasAbility('powerofalchemymismagius')) &&
+				this.hasAbility(['levitate', 'powerofalchemyweezing', 'powerofalchemymismagius']) &&
 				!this.battle.suppressingAbility()
 			) return null;
 			if ('magnetrise' in this.volatiles) return false;
