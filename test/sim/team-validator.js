3c2e25949ca8af9e27462780ffb7c8efed182fe2
Karthik99999 <bandagondak0217@gmail.com>
2022-08-25 20:47:18 -0400

    lint

--- test/sim/team-validator.js
+++ test/sim/team-validator.js
@@ -765,11 +765,11 @@ describe('Team Validator', function () {
 	});
 
 	it('should prevent Pokemon that don\'t evolve via level-up and evolve from a Pokemon that does evolve via level-up from being underleveled.', function () {
-		let team = [
+		const team = [
 			{species: 'nidoking', level: 1, ability: 'sheerforce', moves: ['earthpower'], evs: {hp: 1}},
 			{species: 'mamoswine', level: 1, ability: 'oblivious', moves: ['earthquake'], evs: {hp: 1}},
 		];
-		let illegal = TeamValidator.get('gen8anythinggoes').validateTeam(team);
+		const illegal = TeamValidator.get('gen8anythinggoes').validateTeam(team);
 		assert(illegal);
 	});
 
