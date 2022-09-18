965fe251c118aade06c4b3114f26fd2063556c85
Annika <56906084+AnnikaCodes@users.noreply.github.com>
2022-09-18 12:21:14 -0700

    Build: Include Battle Factory sets

--- tools/build-utils.js
+++ tools/build-utils.js
@@ -238,6 +238,7 @@ exports.transpile = (doForce, decl) => {
 	// sucrase doesn't copy JSON over, so we'll have to do it ourselves
 	copyOverDataJSON('bss-factory-sets.json');
 	copyOverDataJSON('cap-1v1-sets.json');
+	copyOverDataJSON('factory-sets.json');
 	copyOverDataJSON('mods/gen7/factory-sets.json');
 	copyOverDataJSON('mods/gen7/bss-factory-sets.json');
 	copyOverDataJSON('mods/gen6/factory-sets.json');
