88d5749e904c9148877929fa066850ce54c63c48
Karthik99999 <bandagondak0217@gmail.com>
2022-10-26 16:09:14 -0400

    i hate tradebacks

--- sim/team-validator.ts
+++ sim/team-validator.ts
@@ -1776,7 +1776,8 @@ export class TeamValidator {
 		// Event-related ability restrictions only matter if we care about illegal abilities
 		const ruleTable = this.ruleTable;
 		if (ruleTable.has('obtainablemoves')) {
-			if (eventData.generation > setSources.maxSourceGen() && !(dex.gen <= 2 && eventSpecies.gen === 1)) {
+			const tradebackEligible = ruleTable.has('allowtradebacks') || (dex.gen === 2 && species.gen === 1);
+			if (eventData.generation > setSources.maxSourceGen() && !tradebackEligible) {
 				if (fastReturn) return true;
 				problems.push(`${name} must not have moves only learnable in gen ${setSources.maxSourceGen()}${etc}.`);
 			}
