b88b3296399a3510c710c756aa03287a6ecbe18c
livid washed <115855253+livid-washed@users.noreply.github.com>
2022-10-28 13:55:31 +1100

    Gen 1: Fix multiple Transform bugs (#8952)
    * Gen 1: Fix multiple Transform bugs
    
    * Modify partners in crime transformInto to be consistent with pokemon.ts changes
    
    * undoing accidental sync of different branch

--- sim/pokemon.ts
+++ sim/pokemon.ts
@@ -1156,6 +1156,7 @@ export class Pokemon {
 		let statName: StatIDExceptHP;
 		for (statName in this.storedStats) {
 			this.storedStats[statName] = pokemon.storedStats[statName];
+			if (this.modifiedStats) this.modifiedStats[statName] = pokemon.modifiedStats![statName]; // Gen 1: Copy modified stats.
 		}
 		this.moveSlots = [];
 		this.set.ivs = (this.battle.gen >= 5 ? this.set.ivs : pokemon.set.ivs);
@@ -1180,14 +1181,6 @@ export class Pokemon {
 		let boostName: BoostID;
 		for (boostName in pokemon.boosts) {
 			this.boosts[boostName] = pokemon.boosts[boostName];
-			if (this.battle.gen <= 1) {
-				if (boostName === 'evasion' || boostName === 'accuracy') continue;
-				if (this.boosts[boostName] >= 0) {
-					this.modifyStat!(boostName, [1, 1.5, 2, 2.5, 3, 3.5, 4][this.boosts[boostName]]);
-				} else {
-					this.modifyStat!(boostName, [100, 66, 50, 40, 33, 28, 25][-this.boosts[boostName]] / 100);
-				}
-			}
 		}
 		if (this.battle.gen >= 6) {
 			const volatilesToCopy = ['focusenergy', 'gmaxchistrike', 'laserfocus'];
