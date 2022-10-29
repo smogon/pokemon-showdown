fad546b038c6aabce5eb1f464ca98d3f49a16c3e
livid washed <liuc.kelvin9901@hotmail.com>
2022-10-29 17:32:46 +1100

    Fix Mimic accuracy in gens 1 and 2
    Mimic accuracy changed from can't miss to 100% for gens 1 and 2

--- data/mods/gen2/moves.ts
+++ data/mods/gen2/moves.ts
@@ -415,7 +415,7 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 	},
 	mimic: {
 		inherit: true,
-		accuracy: true,
+		accuracy: 100,
 		ignoreAccuracy: undefined,
 		ignoreEvasion: undefined,
 		noSketch: true,
