90f367ad0dd8d4f207ea0c3a059e390679c0cd5b
Hisuian Zoroark <96159984+HisuianZoroark@users.noreply.github.com>
2022-11-12 20:21:03 -0500

    I forgot about these items ngl

--- data/mods/partnersincrime/items.ts
+++ data/mods/partnersincrime/items.ts
@@ -1,4 +1,12 @@
 export const Items: {[k: string]: ModdedItemData} = {
+  adrenalineorb: {
+		inherit: true,
+		onAfterBoost(boost, target, source, effect) {
+			if (effect.id === 'intimidate' || effect.id === 'ability:intimidate') {
+				target.useItem();
+			}
+		},
+	},
 	leppaberry: {
 		inherit: true,
     onEat(pokemon) {
@@ -12,5 +20,17 @@ export const Items: {[k: string]: ModdedItemData} = {
       }
 			this.add('-activate', pokemon, 'item: Leppa Berry', moveSlot.move, '[consumed]');
 		},
+    protectivepads: {
+  		inherit: true,
+  		onSetAbility(ability, target, source, effect) {
+  			if (target !== source && target === this.activePokemon && this.activeMove && this.activeMove.flags['contact']) {
+  				if (effect && effect.effectType === 'Ability' && !effect.id.endsWith('wanderingspirit')) {
+  					this.add('-activate', source, effect.fullname);
+  					this.add('-activate', target, 'item: Protective Pads');
+  				}
+  				return false;
+  			}
+  		},
+  	},
 	},
 };
