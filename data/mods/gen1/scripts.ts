e5b0b8a73d8c1609b7167cb668d48c91f1bc99a3
Andrew Werner <HoeenCoder@users.noreply.github.com>
2022-11-17 15:49:17 -0500

    Fix Build

--- data/mods/gen1/scripts.ts
+++ data/mods/gen1/scripts.ts
@@ -619,7 +619,7 @@ export const Scripts: ModdedBattleScriptsData = {
 				}
 			}
 			if (move.selfSwitch && pokemon.hp) {
-				pokemon.switchFlag = move.selfSwitch;
+				pokemon.switchFlag = move.selfSwitch === true ? true : toID(move.selfSwitch);
 			}
 
 			return damage;
