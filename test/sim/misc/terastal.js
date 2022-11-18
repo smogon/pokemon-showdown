e7d2fa35fb72e197e20e57a804c5e0f611805965
Alexander B <4866817+MathyFurret@users.noreply.github.com>
2022-11-18 14:44:26 -0600

    Unit Tests: Transition to Gen 9

--- test/sim/misc/terastal.js
+++ test/sim/misc/terastal.js
@@ -11,8 +11,8 @@ describe("Terastallization", function () {
 	});
 
 	// Don't know why this is failing
-	it.skip('should change the user\'s type to its Tera type after terastallizing', function () {
-		battle = common.createBattle({formatid: 'gen9oubeta'});
+	it('should change the user\'s type to its Tera type after terastallizing', function () {
+		battle = common.createBattle();
 		battle.setPlayer('p1', {team: [
 			{species: 'Ampharos', ability: 'static', moves: ['voltswitch', 'dragonpulse'], teraType: 'Dragon'},
 		]});
@@ -25,8 +25,8 @@ describe("Terastallization", function () {
 
 	// TODO test if actual mechanic
 	// Don't know why this is failing
-	it.skip('should persist the user\'s changed type after switching', function () {
-		battle = common.createBattle({formatid: 'gen9oubeta'});
+	it('should persist the user\'s changed type after switching', function () {
+		battle = common.createBattle();
 		battle.setPlayer('p1', {team: [
 			{species: 'Ampharos', ability: 'static', moves: ['voltswitch', 'dragonpulse'], teraType: 'Dragon'},
 			{species: 'Flaaffy', ability: 'static', moves: ['voltswitch', 'dragonpulse'], teraType: 'Electric'},
@@ -38,5 +38,22 @@ describe("Terastallization", function () {
 		assert.equal(battle.p1.active[0].getTypes().join('/'), 'Dragon');
 		battle.makeChoices('switch 2', 'move voltswitch');
 		assert.equal(battle.p1.pokemon[1].getTypes().join('/'), 'Dragon');
+	});
+
+	it.skip('should give STAB correctly to the user\'s old types', function () {
+		battle = common.createBattle();
+		battle.setPlayer('p1', {team: [
+			{species: 'Ampharos', ability: 'static', moves: ['shockwave', 'swift'], teraType: 'Electric'},
+		]});
+		battle.setPlayer('p2', {team: [
+			{species: 'Ampharos', ability: 'static', moves: ['shockwave', 'swift'], teraType: 'Normal'},
+		]});
+		battle.makeChoices('move shockwave terastallize', 'move shockwave terastallize');
+		const teraDamage = battle.p2.active[0].maxhp - battle.p2.active[0].maxhp;
+		assert.bounded(teraDamage, [54, 64],
+			"Terastralizing into the same type did not boost STAB; actual damage: " + teraDamage);
+		const nonTeraDamage = battle.p1.active[0].maxhp - battle.p1.active[0].maxhp;
+		assert.bounded(nonTeraDamage, [40, 48],
+			"Terastralizing did not keep old type's STAB; actual damage: " + teraDamage);
 	});
 });
