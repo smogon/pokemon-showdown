5842e1d56d9b2a49434ca0f6bec6e40428d42fe9
Leonard Craft III <leonardcraft64@gmail.com>
2022-11-17 20:42:14 -0600

    Fix toID crash in Gen 1 mods

--- data/mods/gen1stadium/scripts.ts
+++ data/mods/gen1stadium/scripts.ts
@@ -521,7 +521,7 @@ export const Scripts: ModdedBattleScriptsData = {
 				}
 			}
 			if (move.selfSwitch && pokemon.hp) {
-				pokemon.switchFlag = move.selfSwitch === true ? true : toID(move.selfSwitch);
+				pokemon.switchFlag = move.selfSwitch === true ? true : this.dex.toID(move.selfSwitch);
 			}
 
 			return damage;
