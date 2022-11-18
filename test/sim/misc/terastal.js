915b10b145816b469cce5c5e7877928c1d927333
Alexander B <4866817+MathyFurret@users.noreply.github.com>
2022-11-18 16:09:11 -0600

    Fix Tera STAB test

--- test/sim/misc/terastal.js
+++ test/sim/misc/terastal.js
@@ -40,7 +40,7 @@ describe("Terastallization", function () {
 		assert.equal(battle.p1.pokemon[1].getTypes().join('/'), 'Dragon');
 	});
 
-	it.skip('should give STAB correctly to the user\'s old types', function () {
+	it('should give STAB correctly to the user\'s old types', function () {
 		battle = common.createBattle();
 		battle.setPlayer('p1', {team: [
 			{species: 'Ampharos', ability: 'static', moves: ['shockwave', 'swift'], teraType: 'Electric'},
@@ -49,10 +49,12 @@ describe("Terastallization", function () {
 			{species: 'Ampharos', ability: 'static', moves: ['shockwave', 'swift'], teraType: 'Normal'},
 		]});
 		battle.makeChoices('move shockwave terastallize', 'move shockwave terastallize');
-		const teraDamage = battle.p2.active[0].maxhp - battle.p2.active[0].maxhp;
-		assert.bounded(teraDamage, [54, 64],
+		const teraDamage = battle.p2.active[0].maxhp - battle.p2.active[0].hp;
+		// 0 SpA Adaptability Ampharos Shock Wave vs. 0 HP / 0 SpD Ampharos: 108-128
+		assert.bounded(teraDamage, [108, 128],
 			"Terastralizing into the same type did not boost STAB; actual damage: " + teraDamage);
-		const nonTeraDamage = battle.p1.active[0].maxhp - battle.p1.active[0].maxhp;
+		const nonTeraDamage = battle.p1.active[0].maxhp - battle.p1.active[0].hp;
+		// 0 SpA Ampharos Shock Wave vs. 0 HP / 0 SpD Ampharos: 40-48
 		assert.bounded(nonTeraDamage, [40, 48],
 			"Terastralizing did not keep old type's STAB; actual damage: " + teraDamage);
 	});
