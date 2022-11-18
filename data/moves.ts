9c52f18d1265694e2958f0ff8ff3db67b2db3661
Annika <annika0uwu@gmail.com>
2022-11-18 00:31:44 -0500

    Fix Last Respects

--- data/moves.ts
+++ data/moves.ts
@@ -10177,7 +10177,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 		accuracy: 100,
 		basePower: 50,
 		basePowerCallback(pokemon, target, move) {
-			return 50 + 50 * pokemon.side.pokemon.map(ally => ally.fainted).length;
+			return 50 + 50 * pokemon.side.pokemon.filter(ally => ally.fainted).length;
 		},
 		category: "Physical",
 		name: "Last Respects",
