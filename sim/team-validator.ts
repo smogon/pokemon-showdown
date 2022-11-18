36c5ca64936ac3995a4df684587bb4af03e6c527
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2022-11-17 20:00:36 -0700

    Prevent Revival Blessing from being used

--- sim/team-validator.ts
+++ sim/team-validator.ts
@@ -1577,6 +1577,9 @@ export class TeamValidator {
 			}
 			if (banReason === '') return null;
 		}
+		if (move.id === 'revivalblessing') {
+			return "Revival Blessing is currently not implemented. It won't be usable until it is.";
+		}
 
 		return null;
 	}
