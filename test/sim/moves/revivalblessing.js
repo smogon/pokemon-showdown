de10fea19bf7454493a803922e5f803fa1723d03
Hisuian Zoroark <96159984+HisuianZoroark@users.noreply.github.com>
2022-11-22 09:38:09 -0500

    add some unit tests

---
+++ test/sim/moves/revivalblessing.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const assert = require('./../../assert');
+const common = require('./../../common');
+
+let battle;
+
+describe('Revival Blessing', function () {
+	afterEach(function () {
+		battle.destroy();
+	});
+
+	it(`should revive allies`, function () {
+		battle = common.createBattle([[
+			{species: 'corviknight', ability: 'runaway', moves: ['memento']},
+			{species: 'zoroark', ability: 'runaway', moves: ['revivalblessing']},
+			{species: 'wynaut', ability: 'runaway', moves: ['splash']},
+		], [
+			{species: 'goodra', ability: 'gooey', moves: ['sleeptalk']},
+		]]);
+		battle.makeChoices('move memento', 'auto');
+		battle.makeChoices('switch zoroark', '');
+		battle.makeChoices('move revivalblessing', 'auto');
+		battle.makeChoices('switch corviknight', '');
+		assert.equal(battle.p1.pokemonLeft, 3);
+	});
+
+	it(`should let you revive even with one Pokemon remaining`, function () {
+		battle = common.createBattle([[
+			{species: 'corviknight', ability: 'runaway', moves: ['memento']},
+			{species: 'zoroark', ability: 'runaway', moves: ['revivalblessing']},
+		], [
+			{species: 'goodra', ability: 'gooey', moves: ['sleeptalk']},
+		]]);
+		battle.makeChoices('move memento', 'auto');
+		battle.makeChoices('switch zoroark', '');
+		battle.makeChoices('move revivalblessing', 'auto');
+		assert.equal(battle.requestState, 'switch');
+		battle.makeChoices('switch corviknight', '');
+		assert.equal(battle.p1.pokemonLeft, 2);
+	});
+});
