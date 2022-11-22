64152423d4f7107079c56bd61eb76e524bec5369
Hisuian Zoroark <96159984+HisuianZoroark@users.noreply.github.com>
2022-11-22 15:44:48 -0500

    Add some client communication

--- sim/pokemon.ts
+++ sim/pokemon.ts
@@ -1080,6 +1080,7 @@ export class Pokemon {
 			baseAbility: this.baseAbility,
 			item: this.item,
 			commanding: !!this.volatiles['commanding'] && !this.fainted,
+			reviving: this.moveThisTurn === 'revivalblessing' && !!this.moveThisTurnResult,
 			pokeball: this.pokeball,
 		};
 		if (this.battle.gen > 6) entry.ability = this.ability;
