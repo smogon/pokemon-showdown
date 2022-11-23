8e0e3749404b20af26b5c2d0b9db8e47f2920896
te <te1@users.noreply.github.com>
2022-11-23 19:39:20 +0100

    Correct Max Move dex generation (#9027)

--- sim/dex-moves.ts
+++ sim/dex-moves.ts
@@ -565,7 +565,8 @@ export class DataMove extends BasicEffect implements Readonly<BasicEffect & Move
 		}
 
 		if (!this.gen) {
-			if (this.num >= 827) {
+			// special handling for gen8 gmax moves (all of them have num 1000 but they are part of gen8)
+			if (this.num >= 827 && !this.isMax) {
 				this.gen = 9;
 			} else if (this.num >= 743) {
 				this.gen = 8;
