017c5557d03f8e2dc20f5601c7ccbd8f45bae80e
livid washed <115855253+livid-washed@users.noreply.github.com>
2022-11-24 07:45:45 +1100

    Gen 1: Fix Confusion/Psybeam confusion chance and substitute interaction (#9044)
    * Gen 1: Confusion/Psybeam can confuse enemies with substitute, and with probability 25/256 (not 26/256)
    
    * Add explanatory hint for secondary effect confusion against Substitute

--- data/mods/gen1/moves.ts
+++ data/mods/gen1/moves.ts
@@ -844,13 +844,23 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 				} else {
 					this.add('-activate', target, 'Substitute', '[damage]');
 				}
-				// Drain/recoil does not happen if the substitute breaks
+				// Drain/recoil/secondary effect confusion do not happen if the substitute breaks
 				if (target.volatiles['substitute']) {
 					if (move.recoil) {
 						this.damage(Math.round(uncappedDamage * move.recoil[0] / move.recoil[1]), source, target, 'recoil');
 					}
 					if (move.drain) {
 						this.heal(Math.ceil(uncappedDamage * move.drain[0] / move.drain[1]), source, target, 'drain');
+					}
+					if (move.secondary?.volatileStatus === 'confusion') {
+						const secondary = move.secondary;
+						if (secondary.chance === undefined || this.randomChance(Math.ceil(secondary.chance * 256 / 100) - 1, 256)) {
+							target.addVolatile(move.secondary.volatileStatus, source, move);
+							this.hint(
+								"In Gen 1, moves that inflict confusion as a secondary effect can confuse targets with a Substitute, " +
+								"as long as the move does not break the Substitute."
+							);
+						}
 					}
 				}
 				this.runEvent('AfterSubDamage', target, source, move, uncappedDamage);
