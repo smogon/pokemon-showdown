a3c0d0a98d1e98c7536601657b2b30e496c714ee
Karthik99999 <bandagondak0217@gmail.com>
2022-11-24 00:57:26 -0500

    remove placeholder comments

--- data/abilities.ts
+++ data/abilities.ts
@@ -4294,14 +4294,12 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 			const faintedAllies = source.side.pokemon.filter(ally => ally.fainted).length;
 			if (faintedAllies < 1) return;
 			this.debug(`Supreme Overlord atk boost for ${faintedAllies} defeated allies.`);
-			// Placeholder 1.1 -> 1.5
 			return this.chainModify(1 + (0.1 * faintedAllies));
 		},
 		onModifySpA(spa, source, target, move) {
 			const faintedAllies = source.side.pokemon.filter(ally => ally.fainted).length;
 			if (faintedAllies < 1) return;
 			this.debug(`Supreme Overlord spa boost for ${faintedAllies} defeated allies.`);
-			// Placeholder 1.1 -> 1.5
 			return this.chainModify(1 + (0.1 * faintedAllies));
 		},
 		onAllyFaint() {
