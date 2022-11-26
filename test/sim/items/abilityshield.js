bb84a750743c576454d20ba253be58b01178ba78
tofa <79044321+im-tofa@users.noreply.github.com>
2022-11-26 23:58:45 +0100

    add references

--- test/sim/items/abilityshield.js
+++ test/sim/items/abilityshield.js
@@ -10,6 +10,7 @@ describe('Ability Shield', function () {
 		battle.destroy();
 	});
 
+	// https://www.smogon.com/forums/threads/scarlet-violet-battle-mechanics-research.3709545/post-9411146
 	it(`should bypass Neutralizing Gas`, function () {
 		battle = common.createBattle([[
 			{species: 'wynaut', ability: 'sturdy', item: 'abilityshield', moves: ['splash'], level: 5},
@@ -24,6 +25,7 @@ describe('Ability Shield', function () {
 		assert.equal(battle.p1.active[0].hp, 1, `Holder should survive from sturdy`);
 	});
 
+	// https://www.smogon.com/forums/threads/scarlet-violet-battle-mechanics-research.3709545/post-9412194
 	it(`should bypass Mold Breaker`, function () {
 		battle = common.createBattle([[
 			{species: 'wynaut', ability: 'sturdy', item: 'abilityshield', moves: ['splash'], level: 5},
@@ -31,10 +33,14 @@ describe('Ability Shield', function () {
 			{species: 'weezinggalar', ability: 'moldbreaker', moves: ['shadowball']},
 		]]);
 
+		// no logs in-game
+		assert(battle.log.every(line => !line.includes('Ability Shield')), `Ability Shield should trigger a block message`);
+
 		battle.makeChoices();
 		assert.equal(battle.p1.active[0].hp, 1, `Holder should survive from sturdy`);
 	});
 
+	// https://www.smogon.com/forums/threads/scarlet-violet-battle-mechanics-research.3709545/post-9403448
 	it(`should block Gastro Acid`, function () {
 		battle = common.createBattle([[
 			{species: 'wynaut', ability: 'sturdy', item: 'abilityshield', moves: ['splash'], level: 5},
@@ -72,51 +78,9 @@ describe('Ability Shield', function () {
 		]]);
 
 		battle.makeChoices();
-
 		assert(battle.log.some(line => line.includes('Ability Shield')), `Ability Shield should trigger a block message`);
 		assert.equal(battle.p1.active[0].ability, 'shadowtag', `Holder should retain ability`);
 	});
 	
 	// TODO skill swap interaction?
-
-	// it(`should not activate on the opponent's moves`, function () {
-	// 	battle = common.createBattle([[
-	// 		{species: 'wynaut', item: 'punchingglove', moves: ['sleeptalk']},
-	// 	], [
-	// 		{species: 'happiny', moves: ['lunge']},
-	// 	]]);
-	// 	battle.makeChoices();
-	// 	assert.statStage(battle.p1.active[0], 'atk', -1, `Attack should be lowered`);
-	// });
-
-	// // https://www.smogon.com/forums/threads/scarlet-violet-battle-mechanics-research.3709545/post-9406865
-	// it(`should stop Pickpocket`, function () {
-	// 	battle = common.createBattle([[
-	// 		{species: 'wynaut', item: 'punchingglove', moves: ['bulletpunch']},
-	// 	], [
-	// 		{species: 'weavile', ability: 'pickpocket', moves: ['sleeptalk']},
-	// 	]]);
-	// 	battle.makeChoices();
-	// 	assert.equal(battle.p1.active[0].item, 'punchingglove', `Attacker should not lose their item`);
-	// 	assert.false.holdsItem(battle.p2.active[0], `Target should not steal Punching Glove`);
-	// });
-
-	// // https://www.smogon.com/forums/threads/scarlet-violet-battle-mechanics-research.3709545/post-9406865
-	// it(`should block against Protecting effects with a contact side effect`, function () {
-	// 	battle = common.createBattle([[
-	// 		{species: 'wynaut', item: 'punchingglove', moves: ['sleeptalk', 'bulletpunch']},
-	// 	], [
-	// 		{species: 'aggron', moves: ['sleeptalk', 'banefulbunker', 'obstruct', 'spikyshield']},
-	// 	]]);
-	// 	battle.makeChoices('move bulletpunch', 'move banefulbunker');
-	// 	battle.makeChoices();
-	// 	battle.makeChoices('move bulletpunch', 'move obstruct');
-	// 	battle.makeChoices();
-	// 	battle.makeChoices('move bulletpunch', 'move spikyshield');
-	// 	battle.makeChoices();
-	// 	const wynaut = battle.p1.active[0];
-	// 	assert.equal(wynaut.status, '', `Wynaut should not have been poisoned by Baneful Bunker`);
-	// 	assert.statStage(wynaut, 'def', 0, `Wynaut's Defense should not have been lowered by Obstruct`);
-	// 	assert.fullHP(wynaut, `Wynaut should not have lost HP from Spiky Shield`);
-	// });
 });
