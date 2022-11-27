9d207ee288e9f8767886da92531e87637241d09b
Leonard Craft III <leonardcraft64@gmail.com>
2022-11-27 12:20:46 -0600

    Mark some Berries as unobtainable

---
+++ data/mods/gen8/items.ts
@@ -0,0 +1,34 @@
+export const Items: {[k: string]: ModdedItemData} = {
+	custapberry: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	enigmaberry: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	jabocaberry: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	keeberry: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	lansatberry: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	marangaberry: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	micleberry: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	rowapberry: {
+		inherit: true,
+		isNonstandard: null,
+	},
+};
