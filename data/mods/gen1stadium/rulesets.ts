151e750c8e23c4a6fdb6aa2015b234398ddb1b51
Leonard Craft III <leonardcraft64@gmail.com>
2022-11-29 21:09:17 -0600

    Add unobtainable Gen 9 Egg moves

--- data/mods/gen1stadium/rulesets.ts
+++ data/mods/gen1stadium/rulesets.ts
@@ -3,5 +3,16 @@ export const Rulesets: {[k: string]: ModdedFormatData} = {
 		effectType: 'ValidatorRule',
 		name: 'Standard',
 		ruleset: ['Obtainable', 'Stadium Sleep Clause', 'Freeze Clause Mod', 'Species Clause', 'OHKO Clause', 'Evasion Moves Clause', 'Exact HP Mod', 'Cancel Mod'],
+		banlist: [
+			// Only obtainable as Egg move, no valid parent
+			'Happiny + Heal Bell', 'Chansey + Heal Bell', 'Blissey + Heal Bell',
+			'Psyduck + Simple Beam', 'Golduck + Simple Beam',
+			'Spoink + Simple Beam', 'Grumpig + Simple Beam',
+			// Only obtainable as Egg move, all parents unobtainable
+			'Qwilfish + Barb Barrage',
+			'Stantler + Psyshield Bash',
+			'Rellor + Cosmic Power', 'Rabsca + Cosmic Power',
+			'Growlithe + Raging Fury', 'Arcanine + Raging Fury',
+		],
 	},
 };
