98f4c8647287f03b62dff1c48540f902114b85c1
Leonard Craft III <leonardcraft64@gmail.com>
2022-12-11 22:26:14 -0600

    Fix coins text for Pay Day and Make it Rain

--- data/text/moves.ts
+++ data/text/moves.ts
@@ -3679,8 +3679,6 @@ export const MovesText: {[k: string]: MoveText} = {
 		name: "Make It Rain",
 		desc: "Lowers the user's Special Attack by 1 stage.",
 		shortDesc: "Lowers the user's Sp. Atk by 1. Hits foe(s).",
-
-		activate: "#payday",
 	},
 	maliciousmoonsault: {
 		name: "Malicious Moonsault",
