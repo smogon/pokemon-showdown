1ebdd74e9cfed0eea3eaebfead58b46a7ae69f61
Leonard Craft III <leonardcraft64@gmail.com>
2022-12-12 18:28:28 -0600

    Don't make old replays weird

--- data/text/moves.ts
+++ data/text/moves.ts
@@ -3679,6 +3679,8 @@ export const MovesText: {[k: string]: MoveText} = {
 		name: "Make It Rain",
 		desc: "Lowers the user's Special Attack by 1 stage.",
 		shortDesc: "Lowers the user's Sp. Atk by 1. Hits foe(s).",
+
+		activate: "#payday",
 	},
 	maliciousmoonsault: {
 		name: "Malicious Moonsault",
