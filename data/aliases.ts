ca867ee289f4a6455f4a9aeeed884ffa1f8db0fb
Mia <49593536+mia-pi-git@users.noreply.github.com>
2022-12-15 13:13:57 -0600

    Update `mayhem` alias to Gen 9 Random Battle Mayhem

--- data/aliases.ts
+++ data/aliases.ts
@@ -71,7 +71,7 @@ export const Aliases: {[alias: string]: string} = {
 	gen6mono: "[Gen 6] Monotype",
 	gen6ag: "[Gen 6] Anything Goes",
 	crossevo: "[Gen 9] Cross Evolution",
-	mayhem: "[Gen 8] Random Battle Mayhem",
+	mayhem: "[Gen 9] Random Battle Mayhem",
 	omotm: "[Gen 9] Shared Power",
 	lcotm: "[Gen 9] Full Potential",
 
