7eaf9febba385a88eaee9d2aefecfad858d1b0ef
Leonard Craft III <leonardcraft64@gmail.com>
2022-12-14 22:47:24 -0600

    Fix Pollen Puff vs Rage Fist

--- test/sim/moves/ragefist.js
+++ test/sim/moves/ragefist.js
@@ -5,7 +5,7 @@ const common = require('./../../common');
 
 let battle;
 
-describe('Rage Fist', function () {
+describe.only('Rage Fist', function () {
 	afterEach(function () {
 		battle.destroy();
 	});
@@ -50,5 +50,22 @@ describe('Rage Fist', function () {
 		battle = common.createBattle([[{species: 'Primeape', ability: 'vitalspirit', moves: ['ragefist']}], [{species: 'Umbreon', moves: ['copycat']}]]);
 		battle.makeChoices();
 		assert.bounded(battle.p1.active[0].maxhp - battle.p1.active[0].hp, [77, 91]);
+	});
+
+	it(`should not increase BP when healed by an ally's Pollen Puff`, function () {
+		battle = common.createBattle({gameType: 'doubles'}, [[
+			{species: 'Wynaut', moves: ['pollenpuff']},
+			{species: 'Annihilape', moves: ['sleeptalk', 'bellydrum']},
+		], [
+			{species: 'Wobbuffet', moves: ['sleeptalk']},
+			{species: 'Lucario', moves: ['sleeptalk']},
+		]]);
+
+		const annihilape = battle.p1.active[1];
+		battle.makeChoices('move pollenpuff -2, move sleeptalk', 'auto');
+		assert.equal(annihilape.timesAttacked, 0, `timesAttacked should not have incremented after a full HP Pollen Puff`);
+
+		battle.makeChoices('move pollenpuff -2, move bellydrum', 'auto');
+		assert.equal(annihilape.timesAttacked, 0, `timesAttacked should not have incremented after a not-full HP Pollen Puff`);
 	});
 });
