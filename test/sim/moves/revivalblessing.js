5b05bd2f6d30a6f750d46468eb57dd29769121bf
Alexander B <4866817+MathyFurret@users.noreply.github.com>
2022-12-22 15:24:19 -0600

    Refactor Revival Blessing to be an action + fixes (#9204)

--- test/sim/moves/revivalblessing.js
+++ test/sim/moves/revivalblessing.js
@@ -55,4 +55,30 @@ describe('Revival Blessing', function () {
 		battle.makeChoices('switch corviknight', '');
 		assert.equal(battle.p1.pokemonLeft, 2);
 	});
+
+	it(`should send the Pokemon back in immediately if in an active slot in Doubles`, () => {
+		battle = common.createBattle({gameType: 'doubles'}, [[
+			{species: 'pawmot', ability: 'naturalcure', moves: ['revivalblessing']},
+			{species: 'shinx', ability: 'intimidate', moves: ['sleeptalk']},
+		], [
+			{species: 'mareep', ability: 'static', moves: ['sleeptalk']},
+			{species: 'chienpao', ability: 'noguard', moves: ['sheercold']},
+		]]);
+		battle.makeChoices('auto', 'move sleeptalk, move sheercold 2');
+		battle.makeChoices('switch 2', '');
+		assert.equal(battle.p2.active[0].boosts.atk, -2, "Intimidate should have activated again");
+	});
+
+	it(`shouldn't allow a fainted Pokemon to make its move the same turn after being revived`, () => {
+		battle = common.createBattle({gameType: 'doubles'}, [[
+			{species: 'pawmot', ability: 'naturalcure', moves: ['revivalblessing']},
+			{species: 'lycanrocmidnight', ability: 'noguard', item: 'laggingtail', moves: ['doubleteam']},
+		], [
+			{species: 'mareep', ability: 'static', moves: ['sleeptalk']},
+			{species: 'chienpao', ability: 'swordofruin', moves: ['sheercold']},
+		]]);
+		battle.makeChoices('auto', 'move sleeptalk, move sheercold 2');
+		battle.makeChoices('switch 2', '');
+		assert.equal(battle.p1.active[1].boosts.evasion, 0, "Lycanroc should not have used Double Team");
+	});
 });
