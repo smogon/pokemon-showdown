65dcedc557567878af2dcc9448dce2869abbbb51
Karthik <32044378+Karthik99999@users.noreply.github.com>
2022-12-26 13:20:44 -0500

    Implement proper Origin forme mechanics (#9227)

--- data/mods/gen8/pokedex.ts
+++ data/mods/gen8/pokedex.ts
@@ -19,6 +19,10 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 		inherit: true,
 		abilities: {0: "Steadfast", H: "Justified"},
 	},
+	giratinaorigin: {
+		inherit: true,
+		requiredItem: "Griseous Orb",
+	},
 	cresselia: {
 		inherit: true,
 		baseStats: {hp: 120, atk: 70, def: 120, spa: 75, spd: 130, spe: 85},
