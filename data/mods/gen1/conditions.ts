94bae23ceb0532c65b234dc4ca1827b3819e8c98
livid washed <115855253+livid-washed@users.noreply.github.com>
2022-12-27 00:14:10 +0100

    Add twoturnmove condition to gen 1

--- data/mods/gen1/conditions.ts
+++ data/mods/gen1/conditions.ts
@@ -235,4 +235,41 @@ export const Conditions: {[id: string]: ModdedConditionData} = {
 			target.addVolatile('confusion');
 		},
 	},
+	twoturnmove: {
+		// Skull Bash, SolarBeam, Sky Drop...
+		name: 'twoturnmove',
+		duration: 2,
+		onStart(attacker, defender, effect) {
+			// ("attacker" is the Pokemon using the two turn move and the Pokemon this condition is being applied to)
+			this.effectState.move = effect.id;
+			attacker.addVolatile(effect.id);
+			// lastMoveTargetLoc is the location of the originally targeted slot before any redirection
+			// note that this is not updated for moves called by other moves
+			// i.e. if Dig is called by Metronome, lastMoveTargetLoc will still be the user's location
+			let moveTargetLoc: number = attacker.lastMoveTargetLoc!;
+			if (effect.sourceEffect && this.dex.moves.get(effect.id).target !== 'self') {
+				// this move was called by another move such as Metronome
+				// and needs a random target to be determined this turn
+				// it will already have one by now if there is any valid target
+				// but if there isn't one we need to choose a random slot now
+				if (defender.fainted) {
+					defender = this.sample(attacker.foes(true));
+				}
+				moveTargetLoc = attacker.getLocOf(defender);
+			}
+			attacker.volatiles[effect.id].targetLoc = moveTargetLoc;
+			this.attrLastMove('[still]');
+			// Run side-effects normally associated with hitting (e.g., Protean, Libero)
+			this.runEvent('PrepareHit', attacker, defender, effect);
+		},
+		onEnd(target) {
+			target.removeVolatile(this.effectState.move);
+		},
+		onLockMove() {
+			return this.effectState.move;
+		},
+		onMoveAborted(pokemon) {
+			pokemon.removeVolatile('twoturnmove');
+		},
+	},
 };
