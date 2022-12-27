827a3570c23b5a2ea9da09edaa3bc50ea7e639aa
livid washed <115855253+livid-washed@users.noreply.github.com>
2022-12-27 11:16:16 +0100

    Gens 1-2: Mirror Move fails immediately after a switch

--- test/sim/moves/mirrormove.js
+++ test/sim/moves/mirrormove.js
@@ -31,4 +31,40 @@ describe('Mirror Move [Gen 1]', function () {
 		battle.choose('p2', 'switch exeggutor');
 		assert.false.cantMove(() => battle.choose('p1', 'move tackle'));
 	});
+
+	it(`[Gen 1] Mirror Move should fail when used by a Pokemon that has not seen the opponent use an attack`, function () {
+		battle = common.gen(1).createBattle({seed: [0, 0, 0, 1]}, [[
+			{species: 'slowbro', moves: ['thunderwave']},
+			{species: 'fearow', moves: ['mirrormove']},
+		], [
+			{species: 'chansey', moves: ['seismictoss']},
+		]]);
+		battle.makeChoices();
+		battle.makeChoices('switch 2', 'auto');
+		// Chansey is fully paralysed
+		assert.fullHP(battle.p1.active[0]);
+		battle.makeChoices();
+		assert.fullHP(battle.p2.active[0]);
+	});
+});
+
+describe('Mirror Move [Gen 2]', function () {
+	afterEach(function () {
+		battle.destroy();
+	});
+
+	it(`[Gen 2] Mirror Move should fail when used by a Pokemon that has not seen the opponent use an attack`, function () {
+		battle = common.gen(2).createBattle({seed: [1, 0, 0, 0]}, [[
+			{species: 'slowbro', moves: ['thunderwave']},
+			{species: 'fearow', moves: ['mirrormove']},
+		], [
+			{species: 'chansey', moves: ['seismictoss']},
+		]]);
+		battle.makeChoices();
+		battle.makeChoices('switch 2', 'auto');
+		// Chansey is fully paralysed
+		assert.fullHP(battle.p1.active[0]);
+		battle.makeChoices();
+		assert.fullHP(battle.p2.active[0]);
+	});
 });
