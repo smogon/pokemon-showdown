3499969288e13423d34ea419940c06b026877669
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2023-01-01 03:12:02 -0700

    Fix OMotM/LCotM aliases

--- data/aliases.ts
+++ data/aliases.ts
@@ -72,8 +72,8 @@ export const Aliases: {[alias: string]: string} = {
 	gen6ag: "[Gen 6] Anything Goes",
 	crossevo: "[Gen 9] Cross Evolution",
 	mayhem: "[Gen 9] Random Battle Mayhem",
-	omotm: "[Gen 9] Shared Power",
-	lcotm: "[Gen 9] Full Potential",
+	omotm: "[Gen 9] VoltTurn Mayhem",
+	lcotm: "[Gen 9] Partners in Crime",
 
 	// mega evos
 	fabio: "Ampharos-Mega",
