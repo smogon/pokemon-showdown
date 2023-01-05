3e3f01a2f8cc471819f1c5e695c18652b080ae8d
Mia <49593536+mia-pi-git@users.noreply.github.com>
2023-01-05 14:15:11 -0600

    Smogtours: Fix newlines in edits

--- server/chat-plugins/smogtours.ts
+++ server/chat-plugins/smogtours.ts
@@ -132,8 +132,8 @@ export const commands: Chat.ChatCommands = {
 				title: Utils.escapeHTML(title),
 				url,
 				image,
-				shortDesc: rawShort,
-				desc: rawDesc,
+				shortDesc: rawShort.replace(/&#13;&#10;/g, '\n'),
+				desc: rawDesc.replace(/&#13;&#10;/g, '\n'),
 				id: tourID || toID(title),
 				date: Date.now(),
 			};
@@ -436,7 +436,7 @@ export const pages: Chat.PageTable = {
 			buf += `Image link (optional): <input name="img" value="${tour.image?.[0] || ""}" /><br />`;
 			buf += `Short description: <br />`;
 			buf += `<textarea name="shortDesc" rows="6" cols="50">${tour.shortDesc}</textarea><br />`;
-			const desc = Utils.escapeHTML(tour.desc).replace(/<br \/>/g, '&#10;');
+			const desc = Utils.escapeHTML(tour.desc).replace(/<br \/>/g, '&#13;&#10;');
 			buf += Utils.html`Full description: <br /><textarea name="desc" rows="20" cols="50">${desc}</textarea><br />`;
 			buf += `<button type="submit" class="button notifying">Update!</button>`;
 			return renderPageChooser('edit', buf, user);
