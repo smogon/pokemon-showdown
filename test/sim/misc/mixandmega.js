e1198d59f4eaed543a7aa96bae77f1467e954087
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2023-01-09 15:36:56 -0700

    Fix build

--- test/sim/misc/mixandmega.js
+++ test/sim/misc/mixandmega.js
@@ -15,7 +15,7 @@ describe('Mix and Mega', function () {
 		battle.setPlayer('p1', {team: [{species: "Mimikyu", ability: 'disguise', item: 'metagrossite', moves: ['shadowclaw']}]});
 		battle.setPlayer('p2', {team: [{species: "Palafin", ability: 'zerotohero', item: 'leftovers', moves: ['jetpunch']}]});
 		battle.makeChoices('default', 'default'); // Switch in
-		battle.makeChoices('move shadowclaw mega', 'move uturn');
+		battle.makeChoices('move shadowclaw mega', 'move jetpunch');
 		assert.equal(battle.p1.active[0].ability, 'toughclaws');
 	});
 });
