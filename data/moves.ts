cdf768c06e607fe2043d9035333c5d29c4f43ac3
Karthik <32044378+Karthik99999@users.noreply.github.com>
2023-01-12 20:45:07 -0500

    Fix Salt Cure effect getting passed by Baton Pass (#9316)

--- data/moves.ts
+++ data/moves.ts
@@ -15916,15 +15916,16 @@ export const Moves: {[moveid: string]: MoveData} = {
 		priority: 0,
 		flags: {protect: 1, mirror: 1},
 		condition: {
-			onStart(pokemon, source) {
-				this.add('-start', pokemon, 'move: Salt Cure', '[of] ' + source);
+			noCopy: true,
+			onStart(pokemon) {
+				this.add('-start', pokemon, 'Salt Cure');
 			},
 			onResidualOrder: 13,
 			onResidual(pokemon) {
 				this.damage(pokemon.baseMaxhp / (pokemon.hasType(['Water', 'Steel']) ? 4 : 8));
 			},
 			onEnd(pokemon) {
-				this.add('-end', pokemon, 'move: Salt Cure');
+				this.add('-end', pokemon, 'Salt Cure');
 			},
 		},
 		secondary: {
