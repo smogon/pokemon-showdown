b136ebad50c2288d52b06baf327608bd933ce681
livid washed <115855253+livid-washed@users.noreply.github.com>
2023-01-23 19:22:28 +0100

    Gen 4 Random Battles updates (#9348)

--- data/mods/gen4/random-teams.ts
+++ data/mods/gen4/random-teams.ts
@@ -22,10 +22,7 @@ export class RandomGen4Teams extends RandomGen5Teams {
 				!counter.get('Bug') && (movePool.includes('bugbuzz') || movePool.includes('megahorn'))
 			),
 			Dark: (movePool, moves, abilities, types, counter) => (
-				(!counter.get('Dark') || (counter.get('Dark') === 1 && moves.has('pursuit'))) &&
-				movePool.includes('suckerpunch') &&
-				counter.setupType !== 'Special'
-			),
+				!counter.get('Dark') || (counter.get('Dark') < 2 && moves.has('pursuit') && movePool.includes('suckerpunch'))),
 			Dragon: (movePool, moves, abilities, types, counter) => !counter.get('Dragon'),
 			Electric: (movePool, moves, abilities, types, counter) => !counter.get('Electric'),
 			Fighting: (movePool, moves, abilities, types, counter) => (
@@ -82,8 +79,26 @@ export class RandomGen4Teams extends RandomGen5Teams {
 			return {cull: counter.get('Physical') + counter.get('Special') < 4};
 		case 'focuspunch':
 			return {cull: !moves.has('substitute') || counter.damagingMoves.size < 2 || moves.has('hammerarm')};
+		case 'lightscreen':
+			if (movePool.length > 1) {
+				const screen = movePool.indexOf('reflect');
+				if (screen >= 0) {
+					this.fastPop(movePool, screen);
+					return {cull: true};
+				}
+			}
+			return {cull: false};
 		case 'raindance':
 			return {cull: abilities.has('Hydration') ? !moves.has('rest') : counter.get('Physical') + counter.get('Special') < 2};
+		case 'reflect':
+			if (movePool.length > 1) {
+				const screen = movePool.indexOf('lightscreen');
+				if (screen >= 0) {
+					this.fastPop(movePool, screen);
+					return {cull: true};
+				}
+			}
+			return {cull: false};
 		case 'refresh':
 			return {cull: !(moves.has('calmmind') && (moves.has('recover') || moves.has('roost')))};
 		case 'rest':
@@ -134,8 +149,9 @@ export class RandomGen4Teams extends RandomGen5Teams {
 			return {cull: !!counter.setupType || moves.has('rest') || moves.has('substitute')};
 		case 'protect':
 			return {cull: (
-				!['Guts', 'Quick Feet', 'Speed Boost'].some(abil => abilities.has(abil)) ||
-				['rest', 'softboiled', 'toxic', 'wish'].some(m => moves.has(m))
+				['rest', 'softboiled'].some(m => moves.has(m)) ||
+				!['Guts', 'Quick Feet', 'Speed Boost'].some(abil => abilities.has(abil)) &&
+				!['toxic', 'wish'].some(m => moves.has(m))
 			)};
 		case 'wish':
 			return {cull: (
@@ -145,7 +161,7 @@ export class RandomGen4Teams extends RandomGen5Teams {
 			)};
 		case 'rapidspin':
 			return {cull: !!teamDetails.rapidSpin || (!!counter.setupType && counter.get('Physical') + counter.get('Special') < 2)};
-		case 'reflect': case 'lightscreen': case 'fakeout':
+		case 'fakeout':
 			return {cull: !!counter.setupType || !!counter.get('speedsetup') || moves.has('substitute')};
 		case 'spikes':
 			return {cull: !!counter.setupType || !!counter.get('speedsetup') || moves.has('substitute')};
@@ -244,10 +260,7 @@ export class RandomGen4Teams extends RandomGen5Teams {
 		case 'brickbreak': case 'closecombat': case 'crosschop': case 'lowkick':
 			return {cull: moves.has('substitute') && moves.has('focuspunch')};
 		case 'machpunch':
-			return {cull: (
-				counter.damagingMoves.size <= counter.get('Fighting') ||
-				(types.has('Fighting') && counter.get('stab') < 2 && !abilities.has('Technician'))
-			)};
+			return {cull: (counter.damagingMoves.size <= counter.get('Fighting'))};
 		case 'seismictoss':
 			return {cull: moves.has('nightshade') || counter.get('Physical') + counter.get('Special') >= 1};
 		case 'superpower':
@@ -262,8 +275,8 @@ export class RandomGen4Teams extends RandomGen5Teams {
 			return {cull: moves.has('psychocut')};
 		case 'rockblast': case 'rockslide':
 			return {cull: moves.has('stoneedge')};
-		case 'shadowclaw':
-			return {cull: moves.has('shadowforce') || moves.has('suckerpunch') && !types.has('Ghost')};
+		case 'shadowclaw': case 'shadowsneak':
+			return {cull: moves.has('suckerpunch') && !types.has('Ghost')};
 		case 'dracometeor':
 			return {cull: moves.has('calmmind') || restTalk || (!!counter.setupType && counter.get('stab') < 2)};
 		case 'dragonclaw':
@@ -271,7 +284,7 @@ export class RandomGen4Teams extends RandomGen5Teams {
 		case 'dragonpulse':
 			return {cull: moves.has('dracometeor') || moves.has('outrage')};
 		case 'crunch': case 'nightslash':
-			return {cull: moves.has('suckerpunch')};
+			return {cull: moves.has('suckerpunch') && !types.has('Dark')};
 		case 'pursuit':
 			return {cull: !!counter.setupType || moves.has('payback')};
 		case 'flashcannon':
@@ -362,7 +375,7 @@ export class RandomGen4Teams extends RandomGen5Teams {
 		case 'Technician':
 			return !counter.get('technician') || moves.has('toxic');
 		case 'Tinted Lens':
-			return counter.get('damage') >= counter.damagingMoves.size;
+			return moves.has('protect');
 		case 'Torrent':
 			return !counter.get('Water');
 		}
@@ -381,8 +394,9 @@ export class RandomGen4Teams extends RandomGen5Teams {
 	): string | undefined {
 		if (species.requiredItem) return species.requiredItem;
 		if (species.requiredItems) return this.sample(species.requiredItems);
-		if (species.name === 'Farfetch\u2019d') return 'Stick';
+		if (species.name === 'Farfetch\u2019d' && moves.has('swordsdance')) return 'Stick';
 		if (species.name === 'Marowak') return 'Thick Club';
+		if (species.name === 'Pikachu') return 'Light Ball';
 		if (species.name === 'Shedinja' || species.name === 'Smeargle') return 'Focus Sash';
 		if (species.name === 'Unown') return 'Choice Specs';
 		if (species.name === 'Wobbuffet') {
@@ -644,7 +658,7 @@ export class RandomGen4Teams extends RandomGen5Teams {
 					(move.category !== 'Status' || !move.flags.heal) &&
 					(move.category === 'Status' || !types.has(move.type) || (move.basePower && move.basePower < 40 && !move.multihit)) &&
 					// These moves cannot be rejected in favor of a forced move
-					!['judgment', 'sleeptalk'].includes(moveid) &&
+					!['judgment', 'lightscreen', 'reflect', 'sleeptalk'].includes(moveid) &&
 					// Setup-supported moves should only be rejected under specific circumstances
 					(counter.get('physicalsetup') + counter.get('specialsetup') < 2 && (
 						!counter.setupType || counter.setupType === 'Mixed' ||
@@ -658,7 +672,7 @@ export class RandomGen4Teams extends RandomGen5Teams {
 					const canRollForcedMoves = (
 						// These moves should always be rolled
 						movePool.includes('spore') || (!Array.from(moves).some(id => recoveryMoves.includes(id)) && (
-							movePool.includes('softboiled') ||
+							movePool.includes('softboiled') && !moves.has('explosion') ||
 							(species.baseSpecies === 'Arceus' && movePool.includes('recover'))
 						))
 					);
