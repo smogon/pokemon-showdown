7137c4b81442382295333d26cb7e110ab5cb30d0
livid washed <115855253+livid-washed@users.noreply.github.com>
2023-01-28 23:46:24 +0100

    Do mirror move in onTryHit

--- data/mods/gen2/moves.ts
+++ data/mods/gen2/moves.ts
@@ -455,11 +455,13 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 	},
 	mirrormove: {
 		inherit: true,
+		onTryHit(target) {
+			if (!target.volatiles['canmirrormove']) return false;
+		},
 		onHit(pokemon) {
 			const noMirror = ['metronome', 'mimic', 'mirrormove', 'sketch', 'sleeptalk', 'transform'];
 			const target = pokemon.side.foe.active[0];
 			const lastMove = target?.lastMove && target?.lastMove.id;
-			if (!target.volatiles['canmirrormove']) return false;
 			if (!lastMove || (!pokemon.activeTurns && !target.moveThisTurn)) {
 				return false;
 			}
