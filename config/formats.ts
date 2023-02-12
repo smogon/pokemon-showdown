1d763bd99c73d6b89d39513f0bf7858b6cc9e3e6
Marty <Marty-D@users.noreply.github.com>
2023-02-11 20:05:55 -0500

    Doubles LC: Add ladder, ban Dunsparce

--- config/formats.ts
+++ config/formats.ts
@@ -233,9 +233,8 @@ export const Formats: FormatList = [
 
 		mod: 'gen9',
 		gameType: 'doubles',
-		searchShow: false,
 		ruleset: ['Standard Doubles', 'Little Cup', 'Sleep Clause Mod'],
-		banlist: ['Murkrow', 'Scyther', 'Sneasel'],
+		banlist: ['Dunsparce', 'Murkrow', 'Scyther', 'Sneasel'],
 	},
 	{
 		name: "[Gen 9] 2v2 Doubles",
