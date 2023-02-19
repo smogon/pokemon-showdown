16c01e1df9675711a18f401e743e3efcf9663eaa
ausmacon <107449244+ausmacon@users.noreply.github.com>
2023-02-19 00:24:27 -0800

    Rumia's Custom Stats (#33)
    Co-authored-by: Kris Johnson <11083252+KrisXV@users.noreply.github.com>

--- data/mods/ssb/pokedex.ts
+++ data/mods/ssb/pokedex.ts
@@ -56,6 +56,13 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 		abilities: {0: 'Hacking'},
 	},
 
+	// Rumia
+	duskull: {
+		inherit: true,
+		baseStats: {hp: 50, atk: 55, def: 90, spa: 90, spd: 55, spe: 55},
+		abilities: {0: 'Youkai of the Dusk'},
+	},
+
 	// sharp_claw
 	sneasel: {
 		inherit: true,
