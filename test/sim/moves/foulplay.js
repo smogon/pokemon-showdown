ff174b695569500137ae77e85b42c09b04e1c0a7
fart <24866009+tmagicturtle@users.noreply.github.com>
2023-02-25 23:22:15 -0700

    Update foulplay.js

--- test/sim/moves/foulplay.js
+++ test/sim/moves/foulplay.js
@@ -8,38 +8,38 @@ describe('Foul Play', function () {
 	afterEach(function () {
 		battle.destroy();
 	});
-    it(`should use target's Attack stat`, function () {
+	it(`should use target's Attack stat`, function () {
 		battle = common.createBattle([[
 			{species: "Chansey", moves: ['foulplay']},
 		], [
 			{species: "Deoxys-Attack", moves: ['splash']},
 		]]);
 		battle.makeChoices('move 1', 'move 1');
-        assert.fainted(battle.p2.active[0]);
+		assert.fainted(battle.p2.active[0]);
 	});
-    it(`should use target's Attack stat against the target's Defense stat`, function () {
+	it(`should use target's Attack stat against the target's Defense stat`, function () {
 		battle = common.createBattle([[
 			{species: "Chansey", moves: ['splash', 'foulplay']},
 		], [
 			{species: "Deoxys-Attack", moves: ['acidarmor']},
 		]]);
 		battle.makeChoices('move 1', 'move 1');
-        battle.makeChoices('move 1', 'move 1');
-        battle.makeChoices('move 1', 'move 1');
-        battle.makeChoices('move 2', 'move 1');
-        assert.false.fainted(battle.p2.active[0]);
+		battle.makeChoices('move 1', 'move 1');
+		battle.makeChoices('move 1', 'move 1');
+		battle.makeChoices('move 2', 'move 1');
+		assert.false.fainted(battle.p2.active[0]);
 	});
-    it(`should use target's Attack stat changes`, function () {
+	it(`should use target's Attack stat changes`, function () {
 		battle = common.createBattle([[
 			{species: "Chansey", moves: ['splash', 'foulplay']},
 		], [
 			{species: "Deoxys-Defense", moves: ['swordsdance']},
 		]]);
 		battle.makeChoices('move 1', 'move 1');
-        battle.makeChoices('move 1', 'move 1');
-        battle.makeChoices('move 1', 'move 1');
-        battle.makeChoices('move 2', 'move 1');
-        assert.fainted(battle.p2.active[0]);
+		battle.makeChoices('move 1', 'move 1');
+		battle.makeChoices('move 1', 'move 1');
+		battle.makeChoices('move 2', 'move 1');
+		assert.fainted(battle.p2.active[0]);
 	});
     it(`should use target's Attack stat changes against the target's Defense stat`, function () {
 		battle = common.createBattle([[
