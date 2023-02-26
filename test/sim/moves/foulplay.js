66f13764744d74f52d3e7d2247c89faf8979293f
fart <24866009+tmagicturtle@users.noreply.github.com>
2023-02-25 23:31:06 -0700

    Update foulplay.js

--- test/sim/moves/foulplay.js
+++ test/sim/moves/foulplay.js
@@ -41,82 +41,82 @@ describe('Foul Play', function () {
 		battle.makeChoices('move 2', 'move 1');
 		assert.fainted(battle.p2.active[0]);
 	});
-    it(`should use target's Attack stat changes against the target's Defense stat`, function () {
+	it(`should use target's Attack stat changes against the target's Defense stat`, function () {
 		battle = common.createBattle([[
 			{species: "Chansey", moves: ['splash', 'foulplay']},
 		], [
 			{species: "Deoxys-Defense", moves: ['acidarmor', 'swordsdance']},
 		]]);
 		battle.makeChoices('move 1', 'move 1');
-        battle.makeChoices('move 1', 'move 1');
-        battle.makeChoices('move 1', 'move 1');
-        battle.makeChoices('move 1', 'move 2');
-        battle.makeChoices('move 1', 'move 2');
-        battle.makeChoices('move 1', 'move 2');
-        battle.makeChoices('move 2', 'move 1');
-        assert.false.fainted(battle.p2.active[0]);
+		battle.makeChoices('move 1', 'move 1');
+		battle.makeChoices('move 1', 'move 1');
+		battle.makeChoices('move 1', 'move 2');
+		battle.makeChoices('move 1', 'move 2');
+		battle.makeChoices('move 1', 'move 2');
+		battle.makeChoices('move 2', 'move 1');
+		assert.false.fainted(battle.p2.active[0]);
 	});
-    it(`should not use user's Attack stat changes`, function () {
+	it(`should not use user's Attack stat changes`, function () {
 		battle = common.createBattle([[
 			{species: "Chansey", moves: ['swordsdance', 'foulplay']},
 		], [
 			{species: "Deoxys-Defense", moves: ['splash']},
 		]]);
 		battle.makeChoices('move 1', 'move 1');
-        battle.makeChoices('move 1', 'move 1');
-        battle.makeChoices('move 1', 'move 1');
-        battle.makeChoices('move 2', 'move 1');
-        assert.false.fainted(battle.p2.active[0]);
+		battle.makeChoices('move 1', 'move 1');
+		battle.makeChoices('move 1', 'move 1');
+		battle.makeChoices('move 2', 'move 1');
+		assert.false.fainted(battle.p2.active[0]);
 	});
-    it(`should not use Unaware target's Attack stat changes`, function () {
+	it(`should not use Unaware target's Attack stat changes`, function () {
 		battle = common.createBattle([[
 			{species: "Chansey", moves: ['splash', 'foulplay']},
 		], [
 			{species: "Deoxys-Defense", moves: ['swordsdance'], ability: 'unaware'},
 		]]);
 		battle.makeChoices('move 1', 'move 1');
-        battle.makeChoices('move 1', 'move 1');
-        battle.makeChoices('move 1', 'move 1');
-        battle.makeChoices('move 2', 'move 1');
-        assert.false.fainted(battle.p2.active[0]);
+		battle.makeChoices('move 1', 'move 1');
+		battle.makeChoices('move 1', 'move 1');
+		battle.makeChoices('move 2', 'move 1');
+		assert.false.fainted(battle.p2.active[0]);
 	});
-    it(`should use target's Attack stat changes even if user is Unaware`, function () {
+	it(`should use target's Attack stat changes even if user is Unaware`, function () {
 		battle = common.createBattle([[
 			{species: "Chansey", moves: ['splash', 'foulplay'], ability: 'unaware'},
 		], [
 			{species: "Deoxys-Defense", moves: ['swordsdance']},
 		]]);
 		battle.makeChoices('move 1', 'move 1');
-        battle.makeChoices('move 1', 'move 1');
-        battle.makeChoices('move 1', 'move 1');
-        battle.makeChoices('move 2', 'move 1');
-        assert.fainted(battle.p2.active[0]);
+		battle.makeChoices('move 1', 'move 1');
+		battle.makeChoices('move 1', 'move 1');
+		battle.makeChoices('move 2', 'move 1');
+		assert.fainted(battle.p2.active[0]);
 	});
-    it(`should use target's Attack stat changes against the target's Defense stat even if user is Unaware`, function () {
+	it(`should use target's Attack stat changes against the target's Defense stat even if user is Unaware`, function () {
 		battle = common.createBattle([[
 			{species: "Chansey", moves: ['splash', 'foulplay'], ability: 'unaware'},
 		], [
 			{species: "Deoxys-Defense", moves: ['acidarmor', 'swordsdance']},
 		]]);
 		battle.makeChoices('move 1', 'move 1');
-        battle.makeChoices('move 1', 'move 1');
-        battle.makeChoices('move 1', 'move 1');
-        battle.makeChoices('move 1', 'move 2');
-        battle.makeChoices('move 1', 'move 2');
-        battle.makeChoices('move 1', 'move 2');
-        battle.makeChoices('move 2', 'move 1');
-        assert.false.fainted(battle.p2.active[0]);
+		battle.makeChoices('move 1', 'move 1');
+		battle.makeChoices('move 1', 'move 1');
+		battle.makeChoices('move 1', 'move 2');
+		battle.makeChoices('move 1', 'move 2');
+		battle.makeChoices('move 1', 'move 2');
+		battle.makeChoices('move 2', 'move 1');
+		assert.false.fainted(battle.p2.active[0]);
 	});
-    it(`should not use user's Attack stat changes even if user is Unaware`, function () {
+	it(`should not use user's Attack stat changes even if user is Unaware`, function () {
 		battle = common.createBattle([[
 			{species: "Chansey", moves: ['swordsdance', 'foulplay'], ability: 'unaware'},
 		], [
 			{species: "Deoxys-Defense", moves: ['splash']},
 	]]);
 		battle.makeChoices('move 1', 'move 1');
-        battle.makeChoices('move 1', 'move 1');
-        battle.makeChoices('move 1', 'move 1');
-        battle.makeChoices('move 2', 'move 1');
-        assert.false.fainted(battle.p2.active[0]);
+		battle.makeChoices('move 1', 'move 1');
+		battle.makeChoices('move 1', 'move 1');
+		battle.makeChoices('move 2', 'move 1');
+		assert.false.fainted(battle.p2.active[0]);
 	});
 });
