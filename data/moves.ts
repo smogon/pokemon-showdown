0ae7c0e778d0924f9df8d8d0ef37d05b16cf82b3
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2023-02-27 22:31:43 -0700

    Flag Hydro Steam as a defrosting and thawing move

--- data/moves.ts
+++ data/moves.ts
@@ -9225,7 +9225,8 @@ export const Moves: {[moveid: string]: MoveData} = {
 		name: "Hydro Steam",
 		pp: 15,
 		priority: 0,
-		flags: {protect: 1, mirror: 1},
+		flags: {protect: 1, mirror: 1, defrost: 1},
+		thawsTarget: true,
 		secondary: null,
 		onBasePower(basePower, source) {
 			if (source.effectiveWeather() === 'sunnyday') {
