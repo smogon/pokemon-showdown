65ac5746ad8e4e2b84047824d3468a71853767b3
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-02-28 21:05:24 +0000

    added all insurgence items to gen 7

--- data/mods/gen7/items.ts
+++ data/mods/gen7/items.ts
@@ -641,4 +641,267 @@ export const Items: {[k: string]: ModdedItemData} = {
 		inherit: true,
 		isNonstandard: null,
 	},
+
+	// Insurgence items
+	poliwrathite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	marowite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	eevite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	meganiumite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	typhlosionite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	feraligatite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	sudowoodite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	politoedite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	sunflorite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	eitgirafarigite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	steelixitefire: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	steelixitesteel: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	macargonite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	donphanite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	miltankite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	shiftrite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	flygonite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	cacturnite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	crawdite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	milotite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	jirachite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	chatotite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	spiritombite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	froslassite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	zebstrikite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	zoronite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	gothitite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	reuniclite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	cryogonite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	haxorite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	stunfiskite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	bisharpite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	hydreigonite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	deltavenusaurite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	deltacharizardite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	deltablastoisinite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	deltabisharpite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	deltagardevoirite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	deltagalladite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	deltasunflorite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	deltascizorite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	deltaglalitite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	deltafroslassite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	deltatyphlosionite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	deltapidgeotite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	deltaetigirafarigite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	deltasablenite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	deltamawilite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	deltamedichamite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	deltacameruptite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	deltamilotite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	deltametagrossitespider: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	deltametagrossiteruin: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	crystalfragment: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	deltalopunnite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	deltalucarionite: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	crystalpiece: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	flygonarmor: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	leavannyarmor: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	mewtwoarmor: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	tyranitararmor: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	volcaronadeltaarmor: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	zekromarmor: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	darkrock: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	trickrock: {
+		inherit: true,
+		isNonstandard: null,
+	},
+
 };
