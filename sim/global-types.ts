bf975a1ffcfa2fe0ac103c607e42c3dee98b1979
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-03-01 13:55:19 +0000

    add morphInto function

--- sim/global-types.ts
+++ sim/global-types.ts
@@ -354,6 +354,7 @@ interface ModdedBattlePokemon {
 	) => boolean;
 	takeItem?: (this: Pokemon, source: Pokemon | undefined) => boolean | Item;
 	transformInto?: (this: Pokemon, pokemon: Pokemon, effect: Effect | null) => boolean;
+	morphInto?: (this: Pokemon, pokemon: Pokemon, effect: Effect | null) => boolean;
 	useItem?: (this: Pokemon, source?: Pokemon, sourceEffect?: Effect) => boolean;
 	ignoringAbility?: (this: Pokemon) => boolean;
 	ignoringItem?: (this: Pokemon) => boolean;
