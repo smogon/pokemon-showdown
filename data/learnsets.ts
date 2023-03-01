a99dc1a07b1e76c7c9b801e7d3113fa4efe16c9e
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-03-01 22:33:30 +0000

    test changes

--- data/learnsets.ts
+++ data/learnsets.ts
@@ -94364,7 +94364,7 @@ export const Learnsets: {[speciesid: string]: LearnsetData} = {
 			worryseed: ["6T"],
 			zenheadbutt: ["6T"],
 			terablast: ["9M"],
-			coreenforcer: ["POA"]
+			coreenforcer: ["INS"]
 		},
 	},
 	ivysaurdelta: {
