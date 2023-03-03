d6be49b65357a897bcc057db31995251502c7736
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-03-03 12:10:41 +0000

    small changes for mega text

--- sim/pokemon.ts
+++ sim/pokemon.ts
@@ -1483,7 +1483,7 @@ export class Pokemon {
 					}
 				} else {
 					if (this.illusion) {
-						this.battle.add('-mega', this.illusion, apparentSpecies, species.requiredItem);
+						this.battle.add('-mega', this.illusion, apparentSpecies, this.illusion.species.requiredItem);
 					} else {
 						this.battle.add('-mega', this, apparentSpecies, species.requiredItem);
 					}
