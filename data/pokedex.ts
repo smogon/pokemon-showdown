7d820f7de80ef5ef0468488793343f8394314f8c
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-03-09 18:25:38 +0000

    Revert "testing something"
    This reverts commit 2d29cc928943bd0b2cd8e791df818d478c11c1cb.

--- data/pokedex.ts
+++ data/pokedex.ts
@@ -2,7 +2,7 @@ export const Pokedex: {[speciesid: string]: SpeciesData} = {
 	bulbasaur: {
 		num: 1,
 		name: "Bulbasaur",
-		types: ["Water", "Poison"],
+		types: ["Grass", "Poison"],
 		genderRatio: {M: 0.875, F: 0.125},
 		baseStats: {hp: 45, atk: 49, def: 49, spa: 65, spd: 65, spe: 45},
 		abilities: {0: "Overgrow", H: "Chlorophyll"},
