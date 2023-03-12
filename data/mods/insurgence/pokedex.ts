86393cac8caf8f619ed106380cdd433495b86b8c
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-03-12 15:20:40 +0000

    vanilla mons w insurgence abilities

---
+++ data/mods/insurgence/pokedex.ts
@@ -0,0 +1,30 @@
+export const Pokedex: {[k: string]: ModdedSpeciesData} = {
+	spiritomb: {
+		inherit: true,
+		abilities: {0: "Pressure", H: "Noctem"},
+	},
+	lunatone: {
+		inherit: true,
+		abilities: {0: "Levitate", H: "Noctem"},
+	},
+	darkrai: {
+		inherit: true,
+		abilities: {0: "Bad Dreams", H: "Absolution"},
+	},
+	whismur: {
+		inherit: true,
+		abilities: {0: "Soundproof", 1: "Amplifier", H: "Rattled"},
+	},
+	loudred: {
+		inherit: true,
+		abilities: {0: "Soundproof", 1: "Amplifier", H: "Scrappy"},
+	},
+	exploud: {
+		inherit: true,
+		abilities: {0: "Soundproof", 1: "Amplifier", H: "Scrappy"},
+	},
+	delibird: {
+		inherit: true,
+		abilities: {0: "Vital Spirit", 1: "Hustle", H: "Winter Joy"},
+	}
+};
