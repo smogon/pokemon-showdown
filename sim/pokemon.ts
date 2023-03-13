4b29518441b68c3a450095c84ae018c93e70c557
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-03-13 20:59:39 +0000

    worth a shot

--- sim/pokemon.ts
+++ sim/pokemon.ts
@@ -1489,8 +1489,8 @@ export class Pokemon {
 						if (this.illusion.canMegaEvo) {
 							const illusionRawSpecies = this.battle.dex.species.get(this.illusion.canMegaEvo);
 							const details = this.illusion.setSpecies(illusionRawSpecies, source).name + (this.level === 100 ? '' : ', L' + this.level) + (this.illusion.gender === '' ? '' : ', ' + this.illusion.gender) + (this.illusion.set.shiny ? ', shiny' : '');
-							this.battle.add('replace', this, details);
 							this.battle.add('-mega', this, apparentSpecies, illusionRawSpecies.requiredItem);
+							this.battle.add('replace', this, details);
 						}
 					} else {
 						this.battle.add('-mega', this, apparentSpecies, species.requiredItem);
