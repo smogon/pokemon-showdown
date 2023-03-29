88075e94dd90674b5bdea2f6d8a3601e660d50e8
PartMan <47669599+PartMan7@users.noreply.github.com>
2023-03-29 06:00:15 +0530

    Use Mia's multi-line HTML
    Co-authored-by: Mia <49593536+mia-pi-git@users.noreply.github.com>

--- server/chat-plugins/scavenger-games.ts
+++ server/chat-plugins/scavenger-games.ts
@@ -539,11 +539,10 @@ const TWISTS: {[k: string]: Twist} = {
 				`${this.completed.slice(0, sliceIndex).map((p, i) => `${Utils.formatOrder(i + 1)} place: <em>${Utils.escapeHTML(p.name)}</em> <span style="color: lightgreen;">[${p.time}]</span>.<br />`).join("")}` +
 				`${this.completed.length > sliceIndex ? `Consolation Prize: ${this.completed.slice(sliceIndex).map(e => `<em>${Utils.escapeHTML(e.name)}</em> <span style="color: lightgreen;">[${e.time}]</span>`).join(', ')}<br />` : ''}<br />` +
 				`<details style="cursor: pointer;"><summary>Solution: </summary><br />` +
-				`${this.questions.map((q, i) => {
-					const questionStr = `${i + 1}) ${Chat.formatText(q.hint)} <span style="color: lightgreen">[<em>${Utils.escapeHTML(q.answer.join(' / '))}</em>]</span><br/>`;
-					const mineStr = `<details style="cursor: pointer;"><summary>Mines: </summary>${mines[i].map(({mine, users}) => Utils.escapeHTML(`${mine}: ${users.join(' / ') || '-'}`)).join('<br />')}</details>`;
-					return questionStr + mineStr;
-				}).join("<br />")}` +
+				`${this.questions.map((q, i) => (
+								`${i + 1}) ${Chat.formatText(q.hint)} <span style="color: lightgreen">[<em>${Utils.escapeHTML(q.answer.join(' / '))}</em>]</span><br/>` + 
+								`<details style="cursor: pointer;"><summary>Mines: </summary>${mines[i].map(({mine, users}) => Utils.escapeHTML(`${mine}: ${users.join(' / ') || '-'}`)).join('<br />')}</details>`
+				)).join("<br />")}` +
 				`</details>`
 			);
 			return true;
