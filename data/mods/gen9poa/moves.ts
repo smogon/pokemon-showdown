b376f93ca70041bca4374a49f11657dbc3459a37
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-04-07 23:54:39 +0100

    make hisuian moves legal

--- data/mods/gen9poa/moves.ts
+++ data/mods/gen9poa/moves.ts
@@ -29,4 +29,88 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 			return success;
 		},
 	},
+	powershift: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	chloroblast: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	ragingfury: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	bleakwindstorm: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	wildboltstorm: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	sandsearstorm: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	springtidestorm: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	mountaingale: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	mysticalpower: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	psyshieldbash: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	ceaselessedge: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	stoneaxe: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	direclaw: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	infernalparade: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	barbbarrage: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	esperwing: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	takeheart: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	triplearrows: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	shelter: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	victorydance: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	lunarblessing: {
+		inherit: true,
+		isNonstandard: null,
+	},
 };
