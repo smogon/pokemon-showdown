5312bb3556df11f8c3eca49b9580bd9bfd6cfafe
livid washed <115855253+livid-washed@users.noreply.github.com>
2023-04-10 23:58:51 +0200

    Old Gens Random Battles: allow battle only formes to generate in Monotype battles (#9490)

--- data/mods/gen8/random-teams.ts
+++ data/mods/gen8/random-teams.ts
@@ -2406,14 +2406,14 @@ export class RandomGen8Teams {
 	) {
 		const exclude = pokemonToExclude.map(p => toID(p.species));
 		const pokemonPool = [];
-		for (let species of this.dex.species.all()) {
+		for (const species of this.dex.species.all()) {
 			if (species.gen > this.gen || exclude.includes(species.id)) continue;
 			if (this.dex.currentMod === 'gen8bdsp' && species.gen > 4) continue;
 			if (isMonotype) {
 				if (!species.types.includes(type)) continue;
 				if (typeof species.battleOnly === 'string') {
-					species = this.dex.species.get(species.battleOnly);
-					if (!species.types.includes(type)) continue;
+					const baseSpecies = this.dex.species.get(species.battleOnly);
+					if (!baseSpecies.types.includes(type)) continue;
 				}
 			}
 			pokemonPool.push(species.id);
