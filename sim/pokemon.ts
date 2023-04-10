a0c656c9d977d9021c3122e870f41061c1ef5a84
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-04-10 22:13:40 +0100

    check weather but more raw

--- sim/pokemon.ts
+++ sim/pokemon.ts
@@ -821,8 +821,7 @@ export class Pokemon {
 	}
 
 	ignoringItem() {
-		if (this.hasItem('Ability Shield') || this.ability === ('neutralizinggas' as ID)) return false;
-		this.battle.add("-message", this.effectiveWeather().toString());
+		this.battle.add("-message", this.battle.field.effectiveWeather().toString());
 		return !!(
 			this.itemState.knockedOff || // Gen 3-4
 			(this.battle.gen >= 5 && !this.isActive) ||
