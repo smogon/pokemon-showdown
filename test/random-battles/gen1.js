06ec143d34ae54c45351a0673981c4795a5761c7
Guangcong Luo <guangcongluo@gmail.com>
2023-04-10 11:34:46 +0900

    Speed up `npm test`
    Randbat tests take up around 2/3 of the `npm test` runtime for me, so
    we're now marking them all as slow.

--- test/random-battles/gen1.js
+++ test/random-battles/gen1.js
@@ -6,7 +6,7 @@
 const assert = require('../assert');
 const {testTeam} = require('./tools');
 
-describe('[Gen 1] Random Battle', () => {
+describe('[Gen 1] Random Battle (slow)', () => {
 	const options = {format: 'gen1randombattle'};
 
 	const badPokemon = ['Magikarp', 'Weedle', 'Kakuna', 'Caterpie', 'Metapod'];
