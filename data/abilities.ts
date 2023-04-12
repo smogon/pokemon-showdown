61a68605f7682e339d276cad4b9f622ba2a6cf83
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-04-12 22:40:07 +0100

    fix atomizate

--- data/abilities.ts
+++ data/abilities.ts
@@ -6411,25 +6411,26 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 		rating: 3.5,
 		num: -109,
 	},
-	// atomizate: {
-	// 	onModifyTypePriority: -1,
-	// 	onModifyType(move, pokemon) {
-	// 		const noModifyType = [
-	// 			'judgment', 'multiattack', 'naturalgift', 'revelationdance', 'technoblast', 'terrainpulse', 'weatherball',
-	// 		];
-	// 		if (move.type === 'Normal' && !noModifyType.includes(move.id) && !(move.isZ && move.category !== 'Status')) {
-	// 			move.type = 'Nuclear';
-	// 			move.atomizateBoosted = true;
-	// 		}
-	// 	},
-	// 	onBasePowerPriority: 23,
-	// 	onBasePower(basePower, pokemon, target, move) {
-	// 		if (move.atomizateBoosted) return this.chainModify([4915, 4096]);
-	// 	},
-	// 	name: "Atomizate",
-	// 	rating: 5,
-	// 	num: -110,
-	// },
+	atomizate: {
+		onModifyTypePriority: -1,
+		onModifyType(move, pokemon) {
+			const noModifyType = [
+				'judgment', 'multiattack', 'naturalgift', 'revelationdance', 'technoblast', 'terrainpulse', 'weatherball',
+			];
+			if (move.type === 'Normal' && !noModifyType.includes(move.id) &&
+				!(move.isZ && move.category !== 'Status') && !(move.name === 'Tera Blast' && pokemon.terastallized)) {
+				move.type = 'Nuclear';
+				move.typeChangerBoosted = this.effect;
+			}
+		},
+		onBasePowerPriority: 23,
+		onBasePower(basePower, pokemon, target, move) {
+			if (move.typeChangerBoosted === this.effect) return this.chainModify([4915, 4096]);
+		},
+		name: "Atomizate",
+		rating: 4,
+		num: -110,
+	},
 	leadskin: {
 		onImmunity(type, pokemon) {
 			if (type === 'fallout') return false;
