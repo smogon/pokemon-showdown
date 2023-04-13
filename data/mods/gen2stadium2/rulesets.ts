e9a00d156fc495fa6af977a379556f6dc88d489e
André Bastos Dias <80102738+andrebastosdias@users.noreply.github.com>
2023-04-13 11:21:48 +0100

    Gen 1 and Gen 2: Nickname Clause (#9505)

--- data/mods/gen2stadium2/rulesets.ts
+++ data/mods/gen2stadium2/rulesets.ts
@@ -2,7 +2,7 @@ export const Rulesets: {[k: string]: ModdedFormatData} = {
 	standard: {
 		effectType: 'ValidatorRule',
 		name: 'Standard',
-		ruleset: ['Obtainable', 'Team Preview', 'Stadium Sleep Clause', 'Freeze Clause Mod', 'Self-KO Clause', 'Species Clause', 'OHKO Clause', 'Evasion Moves Clause', 'Exact HP Mod', 'Cancel Mod', 'Stadium Items Clause'],
+		ruleset: ['Obtainable', 'Team Preview', 'Stadium Sleep Clause', 'Freeze Clause Mod', 'Self-KO Clause', 'Species Clause', 'Nickname Clause', 'OHKO Clause', 'Evasion Moves Clause', 'Exact HP Mod', 'Cancel Mod', 'Stadium Items Clause'],
 	},
 	selfkoclause: {
 		effectType: 'Rule',
