404b086fd3334d67c706ecf8b7b0270f030c4f94
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-04-21 20:59:05 +0100

    add uranium to poa

--- data/mods/gen9poa/formats-data.ts
+++ data/mods/gen9poa/formats-data.ts
@@ -1796,6 +1796,1134 @@ export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
 		natDexTier: "UU",
 	},
 
+	// Uranium
+	orchynx: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	metalynx: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",		
+	},
+	metalynxmega: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",		
+	},
+	raptorch: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",		
+	},
+	archilles: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",		
+	},
+	archillesmega: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "Uber",		
+	},
+	eletux: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",		
+	},
+	electruxo: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",		
+	},
+	electruxomega: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",		
+	},
+	chyinmunk: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",		
+	},
+	chyinmunknuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "NU",		
+	},
+	kinetmunk: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "NU",		
+	},
+	kinetmunknuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",		
+	},
+	birbie: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",		
+	},
+	aveden: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "NU",		
+	},
+	splendifowl: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",		
+	},
+	cubbug: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	cubblfly: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "NU",		
+	},
+	nimflora: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",		
+	},
+	barewl: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",		
+	},
+	dearewl: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",		
+	},
+	gararewl: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",		
+	},
+	grozard: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",		
+	},
+	terlard: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "NU",		
+	},
+	tonemy: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",		
+	},
+	tofurang: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",	
+	},
+	dunseraph: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",	
+	},
+	fortog: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",		
+	},
+	folerog: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "NU",		
+	},
+	blubelrog: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",		
+	},
+	feleng: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",		
+	},
+	felunge: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "NU",
+		
+	},
+	feliger: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",		
+	},
+	empirilla: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "Uber",	
+	},
+	owten: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",		
+	},
+	owtennuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "NU",		
+	},
+	eshouten: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",	
+	},
+	eshoutennuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",	
+	},
+	smore: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",	
+	},
+	firoke: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "NU",	
+	},
+	brailip: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	brainoar: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",
+	},
+	tancoon: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	tancoonnuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "NU",
+	},
+	tanscure: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",
+		
+	},
+	tanscurenuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",	
+	},
+	sponee: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	sponaree: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",
+	},
+	pahar: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",	
+	},
+	paharnuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	palij: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "NU",	
+	},
+	palijnuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",	
+	},
+	pajay: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",	
+	},
+	pajaynuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",
+	},
+	jerbolta: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",
+	},
+	jerboltanuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",
+	},
+	comite: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	cometeor: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "NU",
+	},
+	astronite: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",
+	},
+	baashaun: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	baashaunnuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	baaschaf: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "NU",
+	},
+	baaschafnuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",
+	},
+	baariette: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",
+	},
+	baariettenuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",
+	},
+	baariettemega: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",		
+	},
+	baariettenuclearmega: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",
+	},
+	tricwe: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	harylect: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "NU",
+	},
+	costraw: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	costrawnuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "NU",
+	},
+	trawpint: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",	
+	},
+	trawpintnuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",
+	},
+	lunapup: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	herolune: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",	
+	},
+	minyan: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+		
+	},
+	minyanmystery: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",		
+	},
+	vilucard: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",		
+	},
+	vilucardmystery: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",		
+	},
+	modrille: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	drilgann: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",
+	},
+	drilgannmega: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",
+	},
+	cocaran: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	cocaranmystery: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	cararalm: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",
+	},
+	cararalmmystery: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",
+	},
+	cocancer: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",	
+	},
+	cocancermystery: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",
+	},
+	corsoreef: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",
+	},
+	corsoreefnuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "NU",
+	},
+	tubjaw: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	tubjawnuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "NU",		
+	},
+	tubareel: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",		
+	},
+	tubareelnuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",
+	},
+	cassnail: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	sableau: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",
+	},
+	escartress: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",
+		
+	},
+	nupin: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	nupinnuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "NU",
+	},
+	gellin: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",
+	},
+	gellinnuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",
+	},
+	barand: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",
+	},
+	barandnuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",
+	},
+	glaslug: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	glavinug: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",
+	},
+	s51: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	s51a: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",
+	},
+	s51amega: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",
+	},
+	paraudio: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	paraudionuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "NU",
+	},
+	paraboom: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",
+	},
+	paraboomnuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",
+	},
+	flager: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	inflagetah: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",
+	},
+	inflagetahmega: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "Uber",
+	},
+	chimical: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	chimaconda: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",
+	},
+	tikiki: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	frikitiki: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",
+	},
+	unymph: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	harptera: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",
+	},
+	chicoatl: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	quetzoral: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "NU",
+	},
+	coatlith: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",
+	},
+	tracton: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",
+	},
+	snopach: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	dermafrost: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",
+	},
+	slothohm: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	theriamp: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",
+	},
+	titanice: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "NU",
+	},
+	frynai: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	saidine: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "NU",
+	},
+	daikatuna: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",
+	},
+	selkid: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	syrentide: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",
+	},
+	syrentidemega: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",
+	},
+	miasmedic: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",
+	},
+	jackdeary: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	jackdearymystery: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	winotinger: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",
+	},
+	winotingermystery: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",
+	},
+	duplicat: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "NU",
+	},
+	nucleon: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",
+	},
+	ratsy: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	raffiti: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",
+	},
+	gargryph: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",
+	},
+	masking: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	dramsama: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",
+	},
+	dramsamamega: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",
+	},
+	antarki: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",
+	},
+	chupacho: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	chupachonuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "NU",
+	},
+	luchabra: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",
+	},
+	luchabranuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",
+	},
+	linkite: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	chainite: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",
+	},
+	pufluff: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	alpico: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",
+	},
+	anderind: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",
+	},
+	colarva: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	frosulo: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "NU",
+	},
+	frosthra: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",
+	},
+	fafurr: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	fafninter: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",
+	},
+	shrimputy: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	krilvolver: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",
+	},
+	lavent: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",
+	},
+	swabone: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	skelerogue: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",
+	},
+	navighast: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",
+	},
+	stenowatt: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",
+	},
+	jungore: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	majungold: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",
+	},
+	hagoop: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	hagoopnuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "NU",
+	},
+	haagross: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",
+	},
+	haagrossnuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",
+	},
+	xenomite: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	xenogen: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",
+	},
+	xenoqueen: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",
+	},
+	hazma: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",
+	},
+	geigeroach: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",
+	},
+	minicorn: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	kiricorn: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",
+	},
+	kiricornmega: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",
+	},
+	oblivicorn: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",
+	},
+	luxi: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	luxor: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "NU",
+	},
+	luxelong: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",
+	},
+	praseopunk: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "NU",
+	},
+	neopunk: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",
+	},
+	sheebit: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	terrabbit: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",
+	},
+	laissure: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",
+	},
+	volchik: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	voltasu: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",
+	},
+	yatagaryu: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "Uber",
+	},
+	devimp: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	fallengel: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",
+	},
+	beliaddon: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",
+	},
+	seikamater: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",
+	},
+	garlikid: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",
+	},
+	baitatao: {
+		inherit: true,
+		natDexTier: "Illegal",
+		isNonstandard: "Unobtainable",
+	},
+	leviathao: {
+		inherit: true,
+		natDexTier: "Illegal",
+		isNonstandard: "Unobtainable",
+	},
+	krakanao: {
+		inherit: true,
+		natDexTier: "Illegal",
+		isNonstandard: "Unobtainable",
+	},
+	lanthan: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "Uber",
+	},
+	actan: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "Uber",
+	},
+	actannuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "Illegal",
+	},
+	urayne: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",
+		
+	},
+	uraynebeta: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "Uber",
+		
+	},
+	uraynegamma: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "Illegal",
+	},
+	aotius: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "Illegal",
+	},
+	mutios: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "Illegal",
+	},
+	zephy: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "Illegal",
+	},
+	whimsicottmega: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",
+	},
+	arbokmega: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",
+	},
+	arboknuclearmega: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",
+	},
+	magikarpnuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	gyaradosnuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "OU",
+	},
+	ekansnuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	arboknuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",
+	},
+	gligarnuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "LC",
+	},
+	gliscornuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "UU",
+	},
+	corsolanuclear: {
+		inherit: true,
+		isNonstandard: null,
+		natDexTier: "RU",
+	},
+
 	// Our Additions!
 	susan: {
 		inherit: true,
