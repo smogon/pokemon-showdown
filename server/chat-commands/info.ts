c80a820623ceb872623ad59be785d4cf5d78ba89
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2023-05-01 12:51:55 +0900

    /requestshow: Allow using comma in comments (#9542)
    * /requestshow: Allow using comma in comments
    
    Also refactored #9541, comment should be trimed and stored to variable, not just checking
    
    * Merge requested changes

--- server/chat-commands/info.ts
+++ server/chat-commands/info.ts
@@ -2521,7 +2521,7 @@ export const commands: Chat.ChatCommands = {
 		if (room.pendingApprovals?.has(user.id)) return this.errorReply('You have a request pending already.');
 		if (!toID(target)) return this.parse(`/help requestshow`);
 
-		let [link, comment] = target.split(',');
+		let [link, comment] = this.splitOne(target);
 		if (!/^https?:\/\//.test(link)) link = `https://${link}`;
 		link = encodeURI(link);
 		let dimensions;
@@ -2535,7 +2535,7 @@ export const commands: Chat.ChatCommands = {
 				throw new Chat.ErrorMessage('Invalid link.');
 			}
 		}
-		if (comment && this.checkChat(comment) !== comment.trim()) {
+		if (comment && this.checkChat(comment) !== comment) {
 			return this.errorReply(`You cannot use filtered words in comments.`);
 		}
 		if (!room.pendingApprovals) room.pendingApprovals = new Map();
