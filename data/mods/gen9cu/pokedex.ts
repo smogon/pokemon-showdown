7e6dbc0b38bb9bdcf24231bcf512e4d772422992
Oracle Public Cloud User <opc@pseudos.subnet05271522.vcn05271522.oraclevcn.com>
2023-05-05 14:59:41 +0000

    fix missingno

--- data/mods/gen9cu/pokedex.ts
+++ data/mods/gen9cu/pokedex.ts
@@ -18101,7 +18101,7 @@ export const Pokedex: {[speciesid: string]: SpeciesData} = {
 		name: "MissingNo.",
 		types: ["Bird", "Normal"],
 		baseStats: {hp: 33, atk: 136, def: 0, spa: 6, spd: 6, spe: 29},
-		abilities: {0: "},
+		abilities: {0: ""},
 		heightm: 3,
 		weightkg: 1590.8,
 		color: "Gray",
