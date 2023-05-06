9b62fc13f85f31f49d9e274f487c36730b8e1483
BuddyCornwall <131293169+BuddyCornwall@users.noreply.github.com>
2023-05-06 18:13:43 +0100

    Update moves.ts

--- data/moves.ts
+++ data/moves.ts
@@ -19183,7 +19183,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 		basePower: 40,
 		category: "Physical",
 		name: "Tackle",
-		pp: 35,
+		pp: 10,
 		priority: 0,
 		flags: {contact: 1, protect: 1, mirror: 1},
 		critRatio: 2,
