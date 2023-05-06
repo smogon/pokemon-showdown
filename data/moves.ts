e946ef2db1c373a979b8b87be999cd5fa6522756
BuddyCornwall <131293169+BuddyCornwall@users.noreply.github.com>
2023-05-06 18:15:23 +0100

    Update moves.ts

--- data/moves.ts
+++ data/moves.ts
@@ -19186,7 +19186,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 		pp: 10,
 		priority: 0,
 		flags: {contact: 1, protect: 1, mirror: 1},
-		critRatio: 2,
+		critRatio: 3,
 		secondary: null,
 		target: "normal",
 		type: "Normal",
