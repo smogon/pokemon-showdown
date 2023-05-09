077f1118556b54afe442103dfe69584142e2e8b4
BuddyCornwall <131293169+BuddyCornwall@users.noreply.github.com>
2023-05-09 12:24:35 +0100

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -38,7 +38,7 @@ export const Formats: FormatList = [
 		mod: 'gen9',
 		searchShow: true,
 		gameType: 'doubles',
-		ruleset: ['HP Percentage Mod', 'Cancel Mod', 'Desync Clause Mod', 'Max Team Size = 9', 'Max Move Count = 8', 'Max Level = 100', 'Default Level = 50'],
+		ruleset: ['HP Percentage Mod', 'Cancel Mod', 'Desync Clause Mod', 'Max Team Size = 9', 'Max Move Count = 8', 'Max Level = 100', 'Default Level = 50','Team Preview'],
 	},
 	{
 		name: "[Gen 9] Random Battle",
