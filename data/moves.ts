841e939694273b388e6ac884b5bf1770988fc12a
BuddyCornwall <131293169+BuddyCornwall@users.noreply.github.com>
2023-05-20 22:02:29 +0100

    Update moves.ts

--- data/moves.ts
+++ data/moves.ts
@@ -1042,11 +1042,6 @@ onTry(source) {
 if (source.species.name === 'Hoopa-Unbound') {
 return;
 }
-this.hint("Only a Pokemon whose form is Hoopa Unbound can use this move.");
-if (source.species.name === 'Hoopa') {
-this.attrLastMove('[still]');
-this.add('-fail', source, 'move: Hyperspace Fury', '[forme]');
-return null;
 }
 this.attrLastMove('[still]');
 this.add('-fail', source, 'move: Hyperspace Fury');
