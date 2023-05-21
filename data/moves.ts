ff8061a02176ef145651d7671fb9ab44e2a87ae3
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-05-21 12:57:58 +0100

    een works

--- data/moves.ts
+++ data/moves.ts
@@ -23476,7 +23476,6 @@ export const Moves: {[moveid: string]: MoveData} = {
 		target: "all",
 		onAfterMove(source) {
 			for (const active of this.getAllActive()) {
-				this.add("-message", active.name);
 				if (this.queue.willMove(active)) {
 					this.actions.useMove("Explosion", active, active);
 				}
