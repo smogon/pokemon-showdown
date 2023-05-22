068cc4a52b065285cb25955eae202bafcbc420cf
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2023-05-21 20:01:24 -0600

    Move Indeedee-M to NUBL again

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -5280,7 +5280,7 @@ export const FormatsData: {[k: string]: SpeciesFormatsData} = {
 		natDexTier: "RU",
 	},
 	indeedee: {
-		tier: "NU",
+		tier: "NUBL",
 		doublesTier: "DUU",
 		natDexTier: "RU",
 	},
