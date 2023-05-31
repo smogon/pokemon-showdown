dfca14a1189d3b5e5f6838576056343545da73ef
Kaen <66154904+Seerd@users.noreply.github.com>
2023-05-31 12:47:17 -0400

    Tier Shift: Update bans (#9578)
    * Tier Shift: Update bans
    
    In case there is a hotpatch before the format is removed until it gets OMotM again.
    
    * oops
    
    * Update config/formats.ts
    
    ---------
    
    Co-authored-by: Kris Johnson <11083252+KrisXV@users.noreply.github.com>

--- config/formats.ts
+++ config/formats.ts
@@ -627,10 +627,13 @@ export const Formats: FormatList = [
 		],
 
 		mod: 'gen9',
-		ruleset: ['Standard OMs', 'Tier Shift Mod', 'Sleep Moves Clause', 'Terastal Clause', 'Min Source Gen = 9'],
+		ruleset: ['Standard OMs', 'Evasion Clause', 'Tier Shift Mod', 'Sleep Moves Clause', 'Terastal Clause', 'Min Source Gen = 9'],
 		banlist: [
-			'Chi-Yu', 'Cloyster', 'Espathra', 'Falinks', 'Flutter Mane', 'Houndstone', 'Iron Bundle', 'Koraidon', 'Medicham', 'Miraidon', 'Oricorio-Pom-Pom',
-			'Polteageist', 'Arena Trap', 'Moody', 'Shadow Tag', 'Eviolite', 'Damp Rock', 'Heat Rock', 'Light Clay', 'Baton Pass', 'Shed Tail',
+			'Arceus', 'Calyrex-Ice', 'Calyrex-Shadow', 'Chi-Yu', 'Cloyster', 'Dialga', 'Dialga-Origin', 'Espathra', 'Eternatus', 'Falinks',
+			'Flutter Mane', 'Groudon', 'Giratina', 'Giratina-Origin', 'Iron Bundle', 'Koraidon', 'Kyogre', 'Landorus-Base', 'Medicham',
+			'Mewtwo', 'Miraidon', 'Oricorio-Pom-Pom', 'Palkia', 'Palkia-Origin', 'Polteageist', 'Rayquaza', 'Urshifu-Base', 'Zacian',
+			'Zacian-Crowned', 'Arena Trap', 'Moody', 'Shadow Tag', 'Eviolite', 'Damp Rock', 'Heat Rock', 'Light Clay', 'Baton Pass',
+			'Last Respects', 'Shed Tail',
 		],
 	},
 	{
