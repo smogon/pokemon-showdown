e69b7e117bb540d740175192d7e19551d82d1b0e
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-06-04 13:57:23 +0200

    duped banlist fix

--- config/formats.ts
+++ config/formats.ts
@@ -92,7 +92,6 @@ export const Formats: FormatList = [
 		mod: 'gen9',
 		ruleset: ['Standard'],
 		banlist: ['Uber', 'AG', 'Arena Trap', 'Moody', 'Sand Veil', 'Shadow Tag', 'Snow Cloak', 'King\'s Rock', 'Baton Pass'],
-		banlist: ['Uber', 'AG', 'Arena Trap', 'Moody', 'Sand Veil', 'Shadow Tag', 'Snow Cloak', 'King\'s Rock', 'Baton Pass'],
 	},
 	{
 		name: "[Gen 9] Ubers",
