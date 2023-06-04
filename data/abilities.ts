f3db84da14caadc60428dc6bfc0c76d9d0972e2e
Karthik <32044378+Karthik99999@users.noreply.github.com>
2023-06-04 14:58:48 -0400

    Use correct modifier for Transistor (#9586)

--- data/abilities.ts
+++ data/abilities.ts
@@ -4662,14 +4662,14 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 		onModifyAtk(atk, attacker, defender, move) {
 			if (move.type === 'Electric') {
 				this.debug('Transistor boost');
-				return this.chainModify(1.3);
+				return this.chainModify([5325, 4096]);
 			}
 		},
 		onModifySpAPriority: 5,
 		onModifySpA(atk, attacker, defender, move) {
 			if (move.type === 'Electric') {
 				this.debug('Transistor boost');
-				return this.chainModify(1.3);
+				return this.chainModify([5325, 4096]);
 			}
 		},
 		name: "Transistor",
