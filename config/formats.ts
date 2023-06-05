bb938d36705ae6ccb01a038d5f02a8ccc245c164
Leonard Craft III <leonardcraft64@gmail.com>
2023-06-05 11:33:39 -0500

    Make VGC Reg D ladderable

--- config/formats.ts
+++ config/formats.ts
@@ -334,7 +334,6 @@ export const Formats: FormatList = [
 		name: "[Gen 9] VGC 2023 Regulation D",
 
 		mod: 'gen9',
-		searchShow: false,
 		gameType: 'doubles',
 		ruleset: ['Flat Rules', '!! Adjust Level = 50', 'Min Source Gen = 9', 'VGC Timer', 'Open Team Sheets'],
 		banlist: ['Walking Wake', 'Iron Leaves'],
