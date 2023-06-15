feab85d2a60b42e166c4b43f27c077844ead61ef
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-06-15 21:13:18 +0100

    meltdown fix

--- data/moves.ts
+++ data/moves.ts
@@ -23861,12 +23861,10 @@ export const Moves: {[moveid: string]: MoveData} = {
 		accuracy: 100,
 		basePower: 150,
 		onPrepareHit(target, source, move) {
-			this.add("-message", target.hp);
 			this.effectState.meltdownBurn = false;
 			if (target.hp > target.maxhp / 2) this.effectState.meltdownBurn = true;
 		},
-		onAfterHit(source, target, move) {
-			this.add("-message", target.hp);
+		onAfterHit(target, source, move) {
 			if (this.effectState.meltdownBurn && target.hp <= target.maxhp / 2) {
 				source.trySetStatus('brn', target);
 			}
