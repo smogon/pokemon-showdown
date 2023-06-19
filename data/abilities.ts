76ca082eda7dc70045ff293ad2f620394fde7bec
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-06-20 00:52:03 +0100

    im a hacker

--- data/abilities.ts
+++ data/abilities.ts
@@ -7246,7 +7246,7 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 			for (const active of this.getAllActive()) {
 				let toMove = this.queue.willMove(active);
 				if (toMove && ["Fire", "Electric"].includes(toMove.move.type)) {
-					//this.add('-message', toMove.move.type + active.name);
+					let thing = toMove.move.type + active.name; // wahuh
 					this.effectState.willBoost = true;
 				}
 			}
