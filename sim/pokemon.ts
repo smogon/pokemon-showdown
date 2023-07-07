81769410a5d2bbdedc7d41d259401331cf9c01e9
Karthik99999 <bandagondak0217@gmail.com>
2023-07-07 16:22:21 -0400

    Fix Illusion leak when disguised as Greninja-Bond

--- sim/pokemon.ts
+++ sim/pokemon.ts
@@ -496,7 +496,9 @@ export class Pokemon {
 		const health = this.getHealth();
 		let details = this.details;
 		if (this.illusion) {
-			const illusionDetails = this.illusion.species.name + (this.level === 100 ? '' : ', L' + this.level) +
+			let displayedSpeciesName = this.illusion.species.name;
+			if (displayedSpeciesName === 'Greninja-Bond') displayedSpeciesName = 'Greninja';
+			const illusionDetails = displayedSpeciesName + (this.level === 100 ? '' : ', L' + this.level) +
 				(this.illusion.gender === '' ? '' : ', ' + this.illusion.gender) + (this.illusion.set.shiny ? ', shiny' : '');
 			details = illusionDetails;
 		}
