c32482ffde92d43b2e256b43f88ce1c1fddf8998
dot-Comfey <84290266+dot-Comfey@users.noreply.github.com>
2023-07-09 20:48:11 -0700

    Update misc.js

--- test/sim/team-validator/misc.js
+++ test/sim/team-validator/misc.js
@@ -46,6 +46,14 @@ describe('Team Validator', function () {
 		assert.false.legalTeam(team, 'gen8anythinggoes');
 	});
 
+	it('should prevent Pokemon that don\'t evolve via level-up and evolve from a Pokemon that does evolve via level-up from being underleveled.', function () {
+		const team = [
+			{species: 'nidoking', level: 1, ability: 'sheerforce', moves: ['earthpower'], evs: {hp: 1}},
+			{species: 'mamoswine', level: 1, ability: 'oblivious', moves: ['earthquake'], evs: {hp: 1}},
+		];
+		assert.false.legalTeam(team, 'gen7anythinggoes');
+	});
+
 	it('should require Pokémon transferred from Gens 1 and 2 to be above Level 2', () => {
 		const team = [
 			{species: 'pidgey', level: 1, ability: 'bigpecks', moves: ['curse'], evs: {hp: 1}},
