4bd887e324a435898f2b92ed1c354293afa9e143
dot-Comfey <84290266+dot-Comfey@users.noreply.github.com>
2023-07-11 09:32:35 -0700

    Changed code to something I'm more satisfied with

--- server/chat-plugins/datasearch.ts
+++ server/chat-plugins/datasearch.ts
@@ -2693,16 +2693,14 @@ function runLearn(target: string, cmd: string, canAll: boolean, formatid: string
 		const canUseAbilityPatch = dex.gen >= 8 && format.mod !== 'gen8dlc1';
 		if (!setSources.sourcesBefore && !canUseAbilityPatch) {
 			const learnset = dex.species.getLearnsetData(species.id);
-			for (let sourceIndex = 0; sourceIndex < setSources.sources.length; sourceIndex++) {
-				const source = setSources.sources[sourceIndex];
+			for (const source of setSources.sources) {
 				if (source.charAt(1) === 'S' && learnset.eventData) {
 					const eventData = learnset.eventData[parseInt(source.substr(2))];
 					const hiddenAbilityProblem = validator.validateEventHiddenAbility(
 						species.name, true, eventData, ` because it has a move only available from an event`
 					);
 					if (hiddenAbilityProblem) {
-						setSources.sources.splice(sourceIndex, 1);
-						sourceIndex--;
+						setSources.sources = setSources.sources.filter(invalidSource => source !== invalidSource);
 						if (!setSources.sources.length) {
 							problems.push(hiddenAbilityProblem);
 						}
@@ -2711,11 +2709,9 @@ function runLearn(target: string, cmd: string, canAll: boolean, formatid: string
 			}
 		}
 		if (species.maleOnlyHidden && setSources.sourcesBefore < 5) {
-			for (let sourceIndex = 0; sourceIndex < setSources.sources.length; sourceIndex++) {
-				const source = setSources.sources[sourceIndex];
+			for (const source of setSources.sources) {
 				if (source.charAt(1) === 'E') {
-					setSources.sources.splice(sourceIndex, 1);
-					sourceIndex--;
+					setSources.sources = setSources.sources.filter(invalidSource => source !== invalidSource);
 					if (!setSources.sources.length) {
 						problems.push(`${species.name} has an unbreedable Hidden Ability - it can't use egg moves.`);
 					}
