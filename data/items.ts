0d5670fc063858eb49bd03d8c95fab8ed512dd0d
BuddyCornwall <131293169+BuddyCornwall@users.noreply.github.com>
2023-07-20 15:00:53 +0100

    Update items.ts

--- data/items.ts
+++ data/items.ts
@@ -28,37 +28,17 @@ basePower: 30,
 onResidualOrder: 5,
 onResidualSubOrder: 4,
 onResidual(pokemon) {
-if (pokemon.hasType('Fire')) {
-this.heal(pokemon.baseMaxhp / 20);
-} else {
-this.damage(pokemon.baseMaxhp / 4);
-}
+this.heal(pokemon.baseMaxhp / 50);
+},
+onDamagingHit(damage, target, source, move) {
+},
+boosts: {
+atk: 1,
+spa: 1,
+evasion: -1,
 },
 num: 281,
 gen: 4,
-},
-
-yellowcard: {
-name: "Yellow Card",
-spritenum: 387,
-fling: {
-basePower: 10,
-},
-onAfterMoveSecondary(target, source, move) {
-if (source && source !== target && source.hp && target.hp && move && move.category !== 'Status') {
-if (!source.isActive || !this.canSwitch(source.side) || source.forceSwitchFlag || target.forceSwitchFlag) {
-return;
-}
-// The item is used up even against a pokemon with Ingrain or that otherwise can't be forced out
-if (target.useItem(source)) {
-if (this.runEvent('DragOut', source, target, move)) {
-source.forceSwitchFlag = true;
-}
-}
-}
-},
-num: 542,
-gen: 5,
 },
 
 abilityshield: {
@@ -5040,26 +5020,6 @@ if (target.useItem(source)) {
 if (this.runEvent('DragOut', source, target, move)) {
 source.forceSwitchFlag = true;
 }
-}
-}
-},
-onAfterBoost(boost, target, source, effect) {
-if (this.activeMove?.id === 'partingshot') return;
-let eject = false;
-let i: BoostID;
-for (i in boost) {
-if (boost[i]! < 0) {
-eject = true;
-}
-}
-if (eject) {
-if (target.hp) {
-if (!this.canSwitch(target.side)) return;
-if (target.volatiles['commanding'] || target.volatiles['commanded']) return;
-for (const pokemon of this.getAllActive()) {
-if (pokemon.switchFlag === true) return;
-}
-if (target.useItem()) target.switchFlag = true;
 }
 }
 },
