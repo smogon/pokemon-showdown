bc04178b7f7ef561df64743b55806184b218ceb3
Leonard Craft III <leonardcraft64@gmail.com>
2023-07-31 00:24:51 -0500

    Add tests for upcoming Hackmons changes

--- test/sim/team-validator/misc.js
+++ test/sim/team-validator/misc.js
@@ -91,4 +91,48 @@ describe('Team Validator', function () {
 		];
 		assert.false.legalTeam(team, 'gen8cap');
 	});
+
+	// Based on research by Anubis: https://www.smogon.com/forums/posts/9713378
+	describe.skip(`Hackmons forms`, function () {
+		it(`should reject battle-only forms in Gen 9, even in Hackmons`, function () {
+			const team = [
+				{species: 'palafinhero', ability: 'steadfast', moves: ['watergun'], evs: {hp: 1}},
+				{species: 'zamazentacrowned', ability: 'steadfast', moves: ['watergun'], evs: {hp: 1}},
+			];
+			assert.false.legalTeam(team, 'gen9purehackmons');
+		});
+
+		it(`should also reject battle-only dexited forms in Gen 9 Hackmons`, function () {
+			const team = [
+				{species: 'zygardecomplete', ability: 'steadfast', moves: ['watergun'], evs: {hp: 1}},
+				{species: 'darmanitangalarzen', ability: 'steadfast', moves: ['watergun'], evs: {hp: 1}},
+				{species: 'eternatuseternamax', ability: 'steadfast', moves: ['watergun'], evs: {hp: 1}},
+			];
+			assert.false.legalTeam(team, 'gen9purehackmons');
+		});
+
+		it(`should not allow a Xerneas with a hacked Ability in Gen 9 Hackmons`, function () {
+			const team = [
+				{species: 'xerneas', ability: 'steadfast', moves: ['watergun'], evs: {hp: 1}},
+			];
+			assert.false.legalTeam(team, 'gen9purehackmons');
+		});
+
+		it(`should allow various other hacked forms in Gen 9 Hackmons`, function () {
+			const team = [
+				{species: 'giratinaorigin', ability: 'steadfast', moves: ['watergun'], evs: {hp: 1}},
+				{species: 'calyrexshadow', ability: 'steadfast', moves: ['watergun'], evs: {hp: 1}},
+				{species: 'greninjaash', ability: 'steadfast', moves: ['watergun'], evs: {hp: 1}},
+			];
+			assert.legalTeam(team, 'gen9purehackmons');
+		});
+
+		it(`should allow battle-only forms in Hackmons before Gen 9`, function () {
+			const team = [
+				{species: 'zamazentacrowned', ability: 'steadfast', moves: ['watergun'], evs: {hp: 1}},
+				{species: 'zygardecomplete', ability: 'steadfast', moves: ['watergun'], evs: {hp: 1}},
+			];
+			assert.legalTeam(team, 'gen8purehackmons');
+		});
+	});
 });
