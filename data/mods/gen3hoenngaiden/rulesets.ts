d9ccbaecf9921a692e76fba020e5ec0c3ce72fa6
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-08-01 02:47:31 -0400

    Update rulesets.ts

--- data/mods/gen3hoenngaiden/rulesets.ts
+++ data/mods/gen3hoenngaiden/rulesets.ts
@@ -1,17 +1,17 @@
 export const Rulesets: {[k: string]: ModdedFormatData} = {
 	batonpassmod: {
 		effectType: 'Rule',
-			name: 'Baton Pass Mod',
-			desc: "Positive stat boosts are reset upon using Baton Pass.",
-			onBegin() {
-				this.add('rule', 'Baton Pass Mod: Positive stat boosts are reset upon using Baton Pass');
-			},
-			onHit(source, target, move) {
-				if (source.positiveBoosts() && move.id === 'batonpass') {
-					this.add('-clearpositiveboost', source);
-					this.hint("Baton Pass Mod activated: Stat Boosts cannot be passed");
-				}
-			},
+		name: 'Baton Pass Mod',
+		desc: "Positive stat boosts are reset upon using Baton Pass.",
+		onBegin() {
+			this.add('rule', 'Baton Pass Mod: Positive stat boosts are reset upon using Baton Pass');
+		},
+		onHit(source, target, move) {
+			if (source.positiveBoosts() && move.id === 'batonpass') {
+				this.add('-clearpositiveboost', source);
+				this.hint("Baton Pass Mod activated: Stat Boosts cannot be passed");
+			}
+		},
 	},
 	hoenngaidenmod: {
 		effectType: 'Rule',
