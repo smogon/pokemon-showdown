694b86bbf052f716ac2ceb3763bc0e93d41390f4
Tree69420 <65974506+Tree69420@users.noreply.github.com>
2023-08-25 19:48:44 -0400

    ) {

--- data/mods/gen1stadium/scripts.ts
+++ data/mods/gen1stadium/scripts.ts
@@ -36,7 +36,7 @@ export const Scripts: ModdedBattleScriptsData = {
 						this.modifyStat!(statName, [100, 66, 50, 40, 33, 28, 25][-this.boosts[statName]] / 100);
 					}
 				}
-				if (this.modifiedStats![statName] > 999){
+				if (this.modifiedStats![statName] > 999) {
 					this.modifiedStats![statName] = 999;
 				}
 			}
