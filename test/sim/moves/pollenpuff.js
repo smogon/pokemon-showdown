84bd530423e1a70d5a82c45b90cae27e4580c6d5
Leonard Craft III <leonardcraft64@gmail.com>
2023-09-06 20:13:05 -0500

    Fix interaction of Pollen Puff and Heal Block (#9732)

--- test/sim/moves/pollenpuff.js
+++ test/sim/moves/pollenpuff.js
@@ -39,43 +39,44 @@ describe('Pollen Puff', function () {
 		assert.false.fullHP(battle.p1.active[1]);
 	});
 
-	describe.skip(`interaction of Heal Block and Pollen Puff`, function () {
+	describe(`interaction of Heal Block and Pollen Puff`, function () {
 		it(`should prevent the user from targeting an ally with Pollen Puff while the user is affected by Heal Block`, function () {
 			battle = common.createBattle({gameType: 'doubles'}, [[
-				{species: 'wynaut', moves: ['sleeptalk', 'pollenpuff']},
+				{species: 'bunnelby', moves: ['sleeptalk', 'pollenpuff']},
 				{species: 'roggenrola', ability: 'magicbounce', moves: ['sleeptalk']},
 			], [
-				{species: 'wobbuffet', moves: ['healblock']},
+				{species: 'scolipede', moves: ['healblock']},
 				{species: 'lucario', moves: ['sleeptalk']},
 			]]);
 
-			battle.makeChoices('auto');
-			assert.false.cantMove(() => battle.choose('p1', 'move pollenpuff 1, move sleeptalk'), 'Wynaut', 'Pollen Puff');
-			assert.cantMove(() => battle.choose('p1', 'move pollenpuff -2, move sleeptalk'), 'Wynaut', 'Pollen Puff');
+			battle.makeChoices();
+			assert.cantMove(() => battle.choose('p1', 'move pollenpuff -2, move sleeptalk'));
+			assert.false.cantMove(() => battle.choose('p1', 'move pollenpuff 1, move sleeptalk'));
 		});
 
 		it(`should not prevent the user from targeting an ally with Z-Pollen Puff while the user is affected by Heal Block`, function () {
 			battle = common.createBattle({gameType: 'doubles'}, [[
-				{species: 'wynaut', item: 'buginiumz', moves: ['sleeptalk', 'pollenpuff']},
+				{species: 'bunnelby', item: 'buginiumz', moves: ['sleeptalk', 'pollenpuff']},
 				{species: 'roggenrola', ability: 'magicbounce', moves: ['sleeptalk']},
 			], [
-				{species: 'wobbuffet', moves: ['healblock']},
+				{species: 'scolipede', moves: ['healblock']},
 				{species: 'lucario', moves: ['sleeptalk']},
 			]]);
 
-			battle.makeChoices('auto');
-			assert.false.cantMove(() => battle.choose('p1', 'move pollenpuff -2, move sleeptalk'), 'Wynaut', 'Pollen Puff');
+			battle.makeChoices();
+			assert.false.cantMove(() => battle.choose('p1', 'move pollenpuff zmove -2, move sleeptalk'));
 		});
 
 		it(`should not prevent the user from targeting an ally with Pollen Puff while the target is affected by Heal Block at move selection, but it should fail at move execution`, function () {
 			battle = common.createBattle({gameType: 'doubles'}, [[
-				{species: 'wynaut', ability: 'magicbounce', moves: ['pollenpuff']},
+				{species: 'wynaut', ability: 'magicbounce', moves: ['sleeptalk', 'pollenpuff']},
 				{species: 'roggenrola', moves: ['sleeptalk']},
 			], [
 				{species: 'wobbuffet', moves: ['healblock']},
 				{species: 'lucario', moves: ['falseswipe']},
 			]]);
 
+			battle.makeChoices();
 			battle.makeChoices('move pollenpuff -2, move sleeptalk', 'move healblock, move falseswipe 2');
 			assert.false.fullHP(battle.p1.active[1], `Roggenrola should not have healed from Pollen Puff`);
 		});
@@ -102,7 +103,7 @@ describe('Pollen Puff', function () {
 				{species: 'lucario', moves: ['sleeptalk']},
 			]]);
 
-			battle.makeChoices('move pollenpuff -2, move sleeptalk', 'auto');
+			battle.makeChoices('move pollenpuff zmove -2, move sleeptalk', 'auto');
 			assert.false.fullHP(battle.p1.active[1], `Roggenrola should have taken damage from Z-Pollen Puff`);
 		});
 	});
