2e2e9000b328f2cef5701b8de356f79f4a46ac9a
Hisuian Zoroark <96159984+HisuianZoroark@users.noreply.github.com>
2023-09-06 21:56:01 -0400

    Implement cantusetwice flag in mods (#9752)

--- data/mods/partnersincrime/scripts.ts
+++ data/mods/partnersincrime/scripts.ts
@@ -83,7 +83,11 @@ export const Scripts: ModdedBattleScriptsData = {
 				}
 				this.runEvent('DisableMove', pokemon);
 				for (const moveSlot of pokemon.moveSlots) {
-					this.singleEvent('DisableMove', this.dex.getActiveMove(moveSlot.id), null, pokemon);
+					const activeMove = this.dex.getActiveMove(moveSlot.id);
+					this.singleEvent('DisableMove', activeMove, null, pokemon);
+					if (activeMove.flags['cantusetwice'] && pokemon.lastMove?.id === moveSlot.id) {
+						pokemon.disableMove(pokemon.lastMove.id);
+					}
 				}
 
 				// If it was an illusion, it's not any more
