20f968f9276934079a52cd3890f83e40c580af26
Marty-D <Marty-D@users.noreply.github.com>
2023-09-13 17:07:53 -0400

    Update move descriptions

--- data/text/moves.ts
+++ data/text/moves.ts
@@ -3775,8 +3775,8 @@ export const MovesText: {[k: string]: MoveText} = {
 	},
 	matchagotcha: {
 		name: "Matcha Gotcha",
-		desc: "The user recovers 1/2 the HP lost by the target, rounded half up. If Big Root is held by the user, the HP recovered is 1.3x normal, rounded half down. Has a 30% chance to burn the target.",
-		shortDesc: "User recovers 50% of damage dealt. 30% brn.",
+		desc: "Has a 20% chance to burn the target. The user recovers 1/2 the HP lost by the target, rounded half up. If Big Root is held by the user, the HP recovered is 1.3x normal, rounded half down.",
+		shortDesc: "20% burn. User recovers 50% of damage dealt.",
 	},
 	maxairstream: {
 		name: "Max Airstream",
