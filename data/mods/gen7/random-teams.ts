a28ff5cf05134239beb5abc54dae4dc3bce07f88
ACakeWearingAHat <45981036+ACakeWearingAHat@users.noreply.github.com>
2023-10-28 15:08:57 -0500

    Update data/mods/gen7/random-teams.ts
    Co-authored-by: livid washed <115855253+livid-washed@users.noreply.github.com>

--- data/mods/gen7/random-teams.ts
+++ data/mods/gen7/random-teams.ts
@@ -1036,7 +1036,7 @@ export class RandomGen7Teams extends RandomGen8Teams {
 			) ? 'Choice Scarf' : 'Choice Specs';
 		}
 		if (counter.get('Special') === 3 && moves.has('uturn')) return 'Choice Specs';
-		if (counter.get('Physical') === 4 && species.id !== 'jirachi' && species.id !== 'spinda' &&
+		if (counter.get('Physical') === 4 && species.id !== 'jirachi' &&
 			['dragontail', 'fakeout', 'flamecharge', 'nuzzle', 'rapidspin'].every(m => !moves.has(m))
 		) {
 			return (
