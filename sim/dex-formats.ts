5c78791fa63078bebecddde1848b8fc1604d5f81
Leonard Craft III <leonardcraft64@gmail.com>
2023-10-30 23:57:34 -0500

    Support defining default best-of formats server-side

--- sim/dex-formats.ts
+++ sim/dex-formats.ts
@@ -379,6 +379,7 @@ export class Format extends BasicEffect implements Readonly<BasicEffect> {
 	declare readonly actions?: ModdedBattleActions;
 	declare readonly challengeShow?: boolean;
 	declare readonly searchShow?: boolean;
+	declare readonly bestOfDefault?: boolean;
 	declare readonly threads?: string[];
 	declare readonly timer?: Partial<GameTimerSettings>;
 	declare readonly tournamentShow?: boolean;
@@ -542,6 +543,7 @@ export class DexFormats {
 			if (format.challengeShow === undefined) format.challengeShow = true;
 			if (format.searchShow === undefined) format.searchShow = true;
 			if (format.tournamentShow === undefined) format.tournamentShow = true;
+			if (format.bestOfDefault === undefined) format.bestOfDefault = false;
 			if (format.mod === undefined) format.mod = 'gen9';
 			if (!this.dex.dexes[format.mod]) throw new Error(`Format "${format.name}" requires nonexistent mod: '${format.mod}'`);
 
