1ce14c8f5b593faa6f8b45c45eb9f8b7263d562d
ACakeWearingAHat <45981036+ACakeWearingAHat@users.noreply.github.com>
2023-10-31 11:13:45 -0500

    Randomized format set updates (#9860)
    * Ranodmized format set updates
    
    * oopsie rolenames
    
    * Update data/random-teams.ts
    
    Co-authored-by: livid washed <115855253+livid-washed@users.noreply.github.com>
    
    * Update data/mods/gen7/random-teams.ts
    
    Co-authored-by: livid washed <115855253+livid-washed@users.noreply.github.com>
    
    * Update random-teams.ts
    
    * Update random-teams.ts
    
    * more changes
    
    * fix
    
    * more changes while we wait
    
    ---------
    
    Co-authored-by: livid washed <115855253+livid-washed@users.noreply.github.com>

--- data/mods/gen7/random-teams.ts
+++ data/mods/gen7/random-teams.ts
@@ -362,8 +362,8 @@ export class RandomGen7Teams extends RandomGen8Teams {
 			['destinybond', 'whirlwind'],
 			// Liepard
 			['copycat', 'uturn'],
-			// Spinda and Seviper
-			[['feintattack', 'switcheroo'], 'suckerpunch'],
+			// Seviper
+			['switcheroo', 'suckerpunch'],
 			// Jirachi
 			['bodyslam', 'healingwish'],
 		];
@@ -1380,11 +1380,8 @@ export class RandomGen7Teams extends RandomGen8Teams {
 				// Limit one Z-Move per team
 				if (item.zMove && teamDetails.zMove) continue;
 
-				// Zoroark copies the last Pokemon
-				if (set.ability === 'Illusion') {
-					if (pokemon.length < 1) continue;
-					set.level = pokemon[pokemon.length - 1].level;
-				}
+				// Zoroark copies the last Pokemon and should not be generated in that slot
+				if (set.ability === 'Illusion' && pokemon.length < 1) continue;
 
 				// Okay, the set passes, add it to our team
 				pokemon.unshift(set);
