10f06958b95b66ee391efa9e9f547d12480896a9
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-11-24 16:24:03 -0500

    Update formats-data.ts

--- data/mods/moderngen1/formats-data.ts
+++ data/mods/moderngen1/formats-data.ts
@@ -1,4 +1,4 @@
-export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+export const FormatsData: {[k: string]: SpeciesFormatsData} = {
 	bulbasaur: {
 		inherit: true,
 		tier: "LC",
