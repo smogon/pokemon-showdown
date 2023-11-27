f38f38024a055164782a72512953a224c0d4e19b
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2023-11-27 12:51:46 -0500

    Update scripts.ts

--- data/mods/vaporemons/scripts.ts
+++ data/mods/vaporemons/scripts.ts
@@ -28,9 +28,9 @@ export const Scripts: ModdedBattleScriptsData = {
 			const tr = this.battle.trunc;
 			if (!move.type) move.type = '???';
 			const type = move.type;
-	
+
 			baseDamage += 2;
-	
+
 			if (move.spreadHit) {
 				// multi-target modifier (doubles only)
 				const spreadModifier = move.spreadModifier || (this.battle.gameType === 'freeforall' ? 0.5 : 0.75);
@@ -42,19 +42,19 @@ export const Scripts: ModdedBattleScriptsData = {
 				this.battle.debug(`Parental Bond modifier: ${bondModifier}`);
 				baseDamage = this.battle.modify(baseDamage, bondModifier);
 			}
-	
+
 			// weather modifier
 			baseDamage = this.battle.runEvent('WeatherModifyDamage', pokemon, target, move, baseDamage);
-	
+
 			// crit - not a modifier
 			const isCrit = target.getMoveHitData(move).crit;
 			if (isCrit) {
 				baseDamage = tr(baseDamage * (move.critModifier || (this.battle.gen >= 6 ? 1.5 : 2)));
 			}
-	
+
 			// random factor - also not a modifier
 			baseDamage = this.battle.randomizer(baseDamage);
-	
+
 			// STAB
 			if (move.forceSTAB || (type !== '???' &&
 				(pokemon.hasType(type) || (pokemon.terastallized && pokemon.getTypes(false, true).includes(type))))) {
@@ -62,7 +62,7 @@ export const Scripts: ModdedBattleScriptsData = {
 				// Not even if you Roost in Gen 4 and somehow manage to use
 				// Struggle in the same turn.
 				// (On second thought, it might be easier to get a MissingNo.)
-	
+
 				let stab = move.stab || 1.5;
 				if (type === pokemon.terastallized && pokemon.getTypes(false, true).includes(type)) {
 					// In my defense, the game hardcodes the Adaptability check like this, too.
@@ -72,48 +72,48 @@ export const Scripts: ModdedBattleScriptsData = {
 				}
 				baseDamage = this.battle.modify(baseDamage, stab);
 			}
-	
+
 			// types
 			let typeMod = target.runEffectiveness(move);
 			typeMod = this.battle.clampIntRange(typeMod, -6, 6);
 			target.getMoveHitData(move).typeMod = typeMod;
 			if (typeMod > 0) {
 				if (!suppressMessages) this.battle.add('-supereffective', target);
-	
+
 				for (let i = 0; i < typeMod; i++) {
 					baseDamage *= 2;
 				}
 			}
 			if (typeMod < 0) {
 				if (!suppressMessages) this.battle.add('-resisted', target);
-	
+
 				for (let i = 0; i > typeMod; i--) {
 					baseDamage = tr(baseDamage / 2);
 				}
 			}
-	
+
 			if (isCrit && !suppressMessages) this.battle.add('-crit', target);
-	
+
 			if (pokemon.status === 'brn' && move.category === 'Physical' && !pokemon.hasAbility('guts')) {
 				if (this.battle.gen < 6 || move.id !== 'facade' || move.id !== 'lashout') {
 					baseDamage = this.battle.modify(baseDamage, 0.5);
 				}
 			}
-	
+
 			// Generation 5, but nothing later, sets damage to 1 before the final damage modifiers
 			if (this.battle.gen === 5 && !baseDamage) baseDamage = 1;
-	
+
 			// Final modifier. Modifiers that modify damage after min damage check, such as Life Orb.
 			baseDamage = this.battle.runEvent('ModifyDamage', pokemon, target, move, baseDamage);
-	
+
 			if (move.isZOrMaxPowered && target.getMoveHitData(move).zBrokeProtect) {
 				baseDamage = this.battle.modify(baseDamage, 0.25);
 				this.battle.add('-zbroken', target);
 			}
-	
+
 			// Generation 6-7 moves the check for minimum 1 damage after the final modifier...
 			if (this.battle.gen !== 5 && !baseDamage) return 1;
-	
+
 			// ...but 16-bit truncation happens even later, and can truncate to 0
 			return tr(baseDamage, 16);
 		}
