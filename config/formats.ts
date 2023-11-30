cfb6312acfa7d7e08c46833d5c177a2e8325755a
Hisuian Zoroark <96159984+HisuianZoroark@users.noreply.github.com>
2023-11-29 20:16:00 -0500

    Fix Sharing is Caring

--- config/formats.ts
+++ config/formats.ts
@@ -1750,7 +1750,9 @@ export const Formats: FormatList = [
 		onBeforeSwitchIn(pokemon) {
 			let format = this.format;
 			if (!format.getSharedItems) format = this.dex.formats.get('gen9sharingiscaring');
+			if (!pokemon.m.SharedItemsUsed) pokemon.m.SharedItemsUsed = [];
 			for (const item of format.getSharedItems!(pokemon)) {
+				if (pokemon.m.SharedItemsUsed.includes(item)) continue;
 				const effect = 'item:' + item;
 				pokemon.volatiles[effect] = {id: this.toID(effect), target: pokemon};
 				if (!pokemon.m.items) pokemon.m.items = [];
@@ -1762,6 +1764,7 @@ export const Formats: FormatList = [
 			let format = this.format;
 			if (!format.getSharedItems) format = this.dex.formats.get('gen9sharingiscaring');
 			for (const item of format.getSharedItems!(pokemon)) {
+				if (pokemon.m.SharedItemsUsed.includes(item)) continue;
 				const effect = 'item:' + item;
 				delete pokemon.volatiles[effect];
 				pokemon.addVolatile(effect);
