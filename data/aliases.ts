61722992b1cea9eab3c37b927484bdb0fc0c9be6
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2023-12-01 00:07:24 -0700

    Add December 2023 OMotM/RoA Spotlight

--- data/aliases.ts
+++ data/aliases.ts
@@ -85,8 +85,8 @@ export const Aliases: {[alias: string]: string} = {
 	gen6ag: "[Gen 6] Anything Goes",
 	crossevo: "[Gen 9] Cross Evolution",
 	mayhem: "[Gen 9] Random Battle Mayhem",
-	omotm: "[Gen 9] Shared Power",
-	lcotm: "[Gen 9] Protean Palace",
+	omotm: "[Gen 9] Tier Shift",
+	lcotm: "[Gen 9] Sketchmons",
 
 	// mega evos
 	fabio: "Ampharos-Mega",
--- data/rulesets.ts
+++ data/rulesets.ts
@@ -2113,6 +2113,8 @@ export const Rulesets: {[k: string]: FormatData} = {
 				nu: 25,
 				publ: 25,
 				pu: 30,
+				zubl: 30,
+				zu: 30,
 				nfe: 30,
 				lc: 30,
 			};
@@ -2122,6 +2124,8 @@ export const Rulesets: {[k: string]: FormatData} = {
 			// Non-Pokemon bans in lower tiers
 			if (target) {
 				if (this.toID(target.set.item) === 'lightclay') tier = 'rubl';
+				if (this.toID(target.set.item) === 'damprock') tier = 'publ';
+				if (this.toID(target.set.item) === 'heatrock') tier = 'publ';
 			}
 			const pokemon = this.dex.deepClone(species);
 			pokemon.bst = pokemon.baseStats['hp'];
