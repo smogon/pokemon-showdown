2ed55ecfd27e705c70b6b0bc39d8c33d66c84c2d
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2023-12-01 12:13:18 -0700

    FIx `/tiershift`

--- server/chat-plugins/othermetas.ts
+++ server/chat-plugins/othermetas.ts
@@ -14,7 +14,7 @@ interface StoneDeltas {
 	type?: string;
 }
 
-type TierShiftTiers = 'UU' | 'RUBL' | 'RU' | 'NUBL' | 'NU' | 'PUBL' | 'PU' | 'NFE' | 'LC';
+type TierShiftTiers = 'UU' | 'RUBL' | 'RU' | 'NUBL' | 'NU' | 'PUBL' | 'PU' | 'ZUBL' | 'ZU' | 'NFE' | 'LC';
 
 function getMegaStone(stone: string, mod = 'gen9'): Item | null {
 	let dex = Dex;
@@ -407,6 +407,8 @@ export const commands: Chat.ChatCommands = {
 			NU: 25,
 			PUBL: 25,
 			PU: 30,
+			ZUBL: 30,
+			ZU: 30,
 			NFE: 30,
 			LC: 30,
 		};
