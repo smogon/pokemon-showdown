dc0c2496f083002cfee97a5d6872cb6fe69409cb
Karthik99999 <bandagondak0217@gmail.com>
2023-12-03 22:00:03 -0700

    Fix interaction between Mega Rayquaza/Terastal Clauses

--- data/rulesets.ts
+++ data/rulesets.ts
@@ -1291,7 +1291,9 @@ export const Rulesets: {[k: string]: FormatData} = {
 				if (pokemon.species.id === 'rayquaza') {
 					pokemon.canMegaEvo = null;
 					// ability to terastal was determined before the clause activated, causing incorrect behavior
-					pokemon.canTerastallize = this.actions.canTerastallize(pokemon);
+					if (!this.ruleTable.has('terastalclause')) {
+						pokemon.canTerastallize = this.actions.canTerastallize(pokemon);
+					}
 				}
 			}
 		},
