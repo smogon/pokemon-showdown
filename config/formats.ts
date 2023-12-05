36034fbc4f608424e18556166e0aed1325f764f5
Karthik99999 <bandagondak0217@gmail.com>
2023-12-05 16:34:06 -0700

    Fix issues with and clean up Modern Gen 1

--- config/formats.ts
+++ config/formats.ts
@@ -547,7 +547,7 @@ export const Formats: FormatList = [
 		],
 
 		mod: 'moderngen1',
-		ruleset: ['Standard', 'Partial Trapping Clause', 'Protect Clause', 'Field Effect Clause', 'Sleep Moves Clause', '+No Ability', '-All Abilities', '-All Items', '+Nonexistent', 'MG1 Mod'],
+		ruleset: ['Standard', 'Partial Trapping Clause', 'Protect Clause', 'Field Effect Clause', 'Sleep Moves Clause', 'MG1 Mod'],
 		banlist: ['Uber', 'Fake Out', 'Confuse Ray', 'Supersonic', 'Swagger', 'Sweet Kiss', 'Flatter'],
 	},
 	{
