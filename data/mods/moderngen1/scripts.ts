43c33d270a1fb2cac0c3bcd8a4eb278e6ba5638c
Karthik <32044378+Karthik99999@users.noreply.github.com>
2023-12-05 17:46:33 -0700

    Fix issues with and clean up Modern Gen 1 (#9944)
    * Fix issues with and clean up Modern Gen 1
    
    * Apply suggestions from code review
    
    Co-authored-by: urkerab <urkerab@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Kris Johnson <11083252+KrisXV@users.noreply.github.com>
    Co-authored-by: urkerab <urkerab@users.noreply.github.com>

--- data/mods/moderngen1/scripts.ts
+++ data/mods/moderngen1/scripts.ts
@@ -3,14 +3,15 @@ export const Scripts: ModdedBattleScriptsData = {
 	gen: 1,
 	init() {
 		for (const i in this.data.Pokedex) {
-			(this.data.Pokedex[i] as any).gender = 'N';
-			(this.data.Pokedex[i] as any).eggGroups = null;
+			this.modData('Pokedex', i).gen = 1;
+			this.modData('Pokedex', i).gender = 'N';
+			this.modData('Pokedex', i).eggGroups = null;
 		}
 		const specialTypes = ['Fire', 'Water', 'Grass', 'Ice', 'Electric', 'Dark', 'Psychic', 'Dragon'];
-		let newCategory = '';
 		for (const i in this.data.Moves) {
+			this.modData('Moves', i).gen = 1;
 			if (this.data.Moves[i].category === 'Status') continue;
-			newCategory = specialTypes.includes(this.data.Moves[i].type) ? 'Special' : 'Physical';
+			const newCategory = specialTypes.includes(this.data.Moves[i].type) ? 'Special' : 'Physical';
 			if (newCategory !== this.data.Moves[i].category) {
 				this.modData('Moves', i).category = newCategory;
 			}
