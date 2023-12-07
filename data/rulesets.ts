smogon/pokemon-showdown

commit 1b5ab55ce2a094fd893410f4e72c5a8d8afc4e2d

Add Stadium 2, Nintendo Cup 1997, and Nintendo Cup 2000 (#8195)
An implementation of Nintendo Cup 1997, Nintendo Cup 2000, and Pokemon Stadium 2, for Pokemon Showdown. The only thing which has not been implemented was showing the exact health of both Pokemon in Pokemon Stadium 2, however implementing that requires changes to the showdown client. The Nintendo Cup 1997 code was done by Enigami, and the rest was by myself (Shellnuts).

This should fulfill the requirements asked by the following accepted suggestions:
https://www.smogon.com/forums/threads/please-add-a-pok%C3%A9mon-stadium-2-simulator-in-showdown.3679486/
https://www.smogon.com/forums/threads/add-nintendo-cup-1997-and-2000-as-challenge-only-formats.3653454/

data/rulesets.ts
@@ -1268,4 +1268,82 @@ export const Rulesets: {[k: string]: FormatData} = {
 			}
 		},
 	},
+	eventmovesclause: {
+		effectType: 'ValidatorRule',
+		name: 'Event Moves Clause',
+		desc: "Bans moves only obtainable through events.",
+		onBegin() {
+			this.add('rule', 'Event Moves Clause: Event-only moves are banned');
+		},
+		onValidateSet(set) {
+			const species = this.dex.species.get(set.species);
+			const learnsetData = {...(this.dex.data.Learnsets[species.id]?.learnset || {})};
+			let prevo = species.prevo;
+			while (prevo) {
+				const prevoSpecies = this.dex.species.get(prevo);
+				const prevoLsetData = this.dex.data.Learnsets[prevoSpecies.id]?.learnset || {};
+				for (const moveid in prevoLsetData) {
+					if (!(moveid in learnsetData)) {
+						learnsetData[moveid] = prevoLsetData[moveid];
+					} else {
+						learnsetData[moveid].push(...prevoLsetData[moveid]);
+					}
+				}
+				prevo = prevoSpecies.prevo;
+			}
+			const problems = [];
+			if (set.moves?.length) {
+				for (const move of set.moves) {
+					if (learnsetData[this.toID(move)] && !learnsetData[this.toID(move)].filter(v => !v.includes('S')).length) {
+						problems.push(`${species.name}'s move ${move} is obtainable only through events.`);
+					}
+				}
+			}
+			if (problems.length) problems.push(`(Event-only moves are banned.)`);
+			return problems;
+		},
+	},
+	cuplevellimit: {
+		effectType: 'ValidatorRule',
+		name: 'Cup Level Limit',
+		desc: "Teams are restricted to a total maximum Level limit and Pokemon are restricted to a set range of Levels",
+		onValidateTeam(team, format) {
+			if (!format.teamLength?.battle) return;
+			if (!format.cupLevelLimit) return;
+			const teamLevels = [];
+			for (const set of team) {
+				teamLevels.push(set.level);
+			}
+			teamLevels.sort((a, b) => b - a);
+			let combinedLowestLevels = 0;
+			let i;
+			for (i = 0; i < format.teamLength.battle; i++) {
+				combinedLowestLevels += teamLevels.pop()!;
+			}
+			if (combinedLowestLevels > format.cupLevelLimit.total) {
+				return [
+					`The combined levels of the ${format.teamLength.battle} lowest Leveled Pokemon of your team is ${combinedLowestLevels}, above the format's maximum combined level of ${format.cupLevelLimit.total}.`,
+				];
+			}
+		},
+		onValidateSet(set, format) {
+			if (!format.cupLevelLimit) return;
+			if (set.level < format.cupLevelLimit.range[0]) {
+				return [
+					`${set.name || set.species} is Level ${set.level}, below the format's minimum Level of ${format.cupLevelLimit.range[0]}.`,
+				];
+			}
+			if (set.level > format.cupLevelLimit.range[1]) {
+				return [
+					`${set.name || set.species} is Level ${set.level}, above the format's maximum Level of ${format.cupLevelLimit.range[1]}.`,
+				];
+			}
+		},
+	},
+	stadiumitemsclause: {
+		effectType: 'ValidatorRule',
+		name: 'Stadium Items Clause',
+		desc: "Bans items that are not usable in Pokemon Stadium 2.",
+		banlist: ['Fast Ball', 'Friend Ball', 'Great Ball', 'Heavy Ball', 'Level Ball', 'Love Ball', 'Lure Ball', 'Master Ball', 'Moon Ball', 'Park Ball', 'Poke Ball', 'Safari Ball', 'Ultra Ball', 'Fire Stone', 'Leaf Stone', 'Moon Stone', 'Sun Stone', 'Thunder Stone', 'Upgrade', 'Water Stone', 'Mail'],
+	},
 };

data/mods/gen1jpn/rulesets.ts
@@ -1,0 +1,7 @@
+export const Rulesets: {[k: string]: ModdedFormatData} = {
+	standard: {
+		effectType: 'ValidatorRule',
+		name: 'Standard',
+		ruleset: ['Obtainable', 'Sleep Clause Mod', 'Species Clause', 'Cancel Mod'],
+	},
+};

data/mods/gen2/rulesets.ts
@@ -33,4 +33,99 @@ export const Rulesets: {[k: string]: ModdedFormatData} = {
 			'Spore + Spider Web',
 		],
 	},
+	nintendocup2000movelegality: {
+		effectType: 'ValidatorRule',
+		name: 'Nintendo Cup 2000 Move Legality',
+		desc: "Prevents Pok\u00e9mon from having moves that would only be obtainable in Pok\u00e9mon Crystal.",
+		onValidateSet(set) {
+			const illegalCombos: {[speciesid: string]: {[moveid: string]: 'E' | 'L' | 'S'}} = {
+				arbok: {crunch: 'E'},
+				sandslash: {metalclaw: 'E'},
+				golduck: {crosschop: 'E'},
+				marowak: {swordsdance: 'E'},
+				electabuzz: {crosschop: 'E'},
+				magmar: {crosschop: 'E'},
+				jolteon: {batonpass: 'L'},
+				vaporeon: {batonpass: 'L'},
+				flareon: {batonpass: 'L'},
+				espeon: {batonpass: 'L'},
+				umbreon: {batonpass: 'L'},
+				dragonite: {extremespeed: 'S'},
+				meganium: {swordsdance: 'E'},
+				typhlosion: {submission: 'E'},
+				ariados: {agility: 'L'},
+				yanma: {wingattack: 'L'},
+				murkrow: {skyattack: 'E'},
+				qwilfish: {spikes: 'L'},
+				sneasel: {metalclaw: 'L'},
+				ursaring: {metalclaw: 'E'},
+				piloswine: {amnesia: 'L'},
+				skarmory: {skyattack: 'E'},
+				donphan: {watergun: 'E'},
+				suicune: {aurorabeam: 'L'},
+				dugtrio: {triattack: 'L'},
+				magneton: {triattack: 'L'},
+				cloyster: {spikes: 'L'},
+			};
+			const notUsableAsTM = ['icebeam', 'flamethrower', 'thunderbolt'];
+			const species = this.dex.species.get(set.species || set.name);
+			const learnsetData = {...(this.dex.data.Learnsets[species.id]?.learnset || {})};
+			const legalityList = illegalCombos[species.id];
+			if (!legalityList) return;
+			const problems = [];
+			let prevo = species.prevo;
+			while (prevo) {
+				const prevoSpecies = this.dex.species.get(prevo);
+				const prevoLsetData = this.dex.data.Learnsets[prevoSpecies.id]?.learnset || {};
+				for (const moveid in prevoLsetData) {
+					if (!(moveid in learnsetData)) {
+						learnsetData[moveid] = prevoLsetData[moveid];
+					} else {
+						learnsetData[moveid].push(...prevoLsetData[moveid]);
+					}
+				}
+				prevo = prevoSpecies.prevo;
+			}
+			for (const moveid of set.moves.map(this.toID)) {
+				// Diglett Magnemite Shellder
+				if (!learnsetData[moveid]) continue;
+				const list = learnsetData[moveid].filter(x => !x.includes(legalityList[moveid]));
+				if (!list.length) {
+					switch (legalityList[moveid]) {
+					case 'L':
+						// Converted to a set to remove duplicate entries
+						const levels = new Set(learnsetData[moveid].filter(x => x.includes(legalityList[moveid])).map(x => x.slice(2)));
+						problems.push(
+							`${species.name} can't learn ${this.dex.moves.get(moveid).name}.`,
+							`(It learns ${this.dex.moves.get(moveid).name} in Pok\u00e9mon Crystal at the following levels: ${[...levels].join(', ')})`
+						);
+						break;
+					case 'S':
+						problems.push(
+							`${species.name} can't learn ${this.dex.moves.get(moveid).name}.`,
+							`(It only learns ${this.dex.moves.get(moveid).name} in Pok\u00e9mon Crystal via special in-game events.)`
+						);
+						break;
+					case 'E':
+						problems.push(
+							`${species.name} can't learn ${this.dex.moves.get(moveid).name}.`,
+							`(It only learns ${this.dex.moves.get(moveid).name} as an egg move in Pok\u00e9mon Crystal only.)`
+						);
+						break;
+					}
+				}
+				for (const id of notUsableAsTM) {
+					if (moveid === id && learnsetData[id] && !learnsetData[id].filter(x => !x.includes('2T')).length) {
+						problems.push(`${species.name} can't learn ${this.dex.moves.get(id).name}.`);
+					}
+				}
+			}
+			if (problems.some(x => notUsableAsTM.map(y => this.dex.moves.get(y).name).some(z => x.includes(z)))) {
+				problems.push(
+					`(${notUsableAsTM.map(y => this.dex.moves.get(y).name).join(' / ')} aren't learnable outside of Pok\u00e9mon Crystal.)`
+				);
+			}
+			return problems;
+		},
+	},
 };

data/mods/gen2stadium2/rulesets.ts
@@ -1,0 +1,15 @@
+export const Rulesets: {[k: string]: ModdedFormatData} = {
+	standard: {
+		effectType: 'ValidatorRule',
+		name: 'Standard',
+		ruleset: ['Obtainable', 'Team Preview', 'Stadium Sleep Clause', 'Freeze Clause Mod', 'Self-KO Clause', 'Species Clause', 'OHKO Clause', 'Evasion Moves Clause', 'Exact HP Mod', 'Cancel Mod', 'Stadium Items Clause'],
+	},
+	selfkoclause: {
+		effectType: 'Rule',
+		name: 'Self-KO Clause',
+		desc: "If a player's last Pokemon uses Self-Destruct or Explosion, they automatically lose the battle.",
+		onBegin() {
+			this.add('rule', 'Self-KO Clause: If a player\'s last Pokemon uses Self-Destruct/Explosion, they automatically lose');
+		},
+	},
+};

config/formats.ts
@@ -3227,6 +3227,36 @@ export const Formats: FormatList = [
 		banlist: ['UU', 'NUBL'],
 	},
 	{
+		name: "[Gen 2] Nintendo Cup 2000",
+		threads: [
+			`&bullet; <a href="https://www.smogon.com/forums/threads/3682691/">Nintendo Cup 2000 Resource Hub</a>`,
+			`&bullet; <a href="https://www.smogon.com/forums/threads/3677370/">Differences between Nintendo Cup 2000 and GSC</a>`,
+		],
+
+		mod: 'gen2',
+		cupLevelLimit: {
+			range: [50, 55],
+			total: 155,
+		},
+		teamLength: {
+			validate: [3, 6],
+			battle: 3,
+		},
+		searchShow: false,
+		ruleset: ['Obtainable', 'Stadium Sleep Clause', 'Freeze Clause Mod', 'Species Clause', 'Item Clause', 'Endless Battle Clause', 'Cancel Mod', 'Event Moves Clause', 'Nickname Clause', 'Team Preview', 'Cup Level Limit', 'Nintendo Cup 2000 Move Legality'],
+		banlist: ['Uber'],
+	},
+	{
+		name: "[Gen 2] Stadium OU",
+		threads: [
+			`&bullet; <a href="https://www.smogon.com/forums/threads/3677370/">Placeholder</a>`,
+		],
+
+		mod: 'gen2stadium2',
+		searchShow: false,
+		ruleset: ['Standard'],
+	},
+	{
 		name: "[Gen 2] Custom Game",
 
 		mod: 'gen2',
@@ -3288,6 +3318,90 @@ export const Formats: FormatList = [
 			'Nidoking + Fury Attack + Thrash', 'Exeggutor + Poison Powder + Stomp', 'Exeggutor + Sleep Powder + Stomp',
 			'Exeggutor + Stun Spore + Stomp', 'Jolteon + Focus Energy + Thunder Shock', 'Flareon + Focus Energy + Ember',
 		],
+	},
+	{
+		name: "[Gen 1] Nintendo Cup 1997",
+		threads: [
+			`&bullet; <a href="https://www.smogon.com/forums/threads/3682412/">Nintendo Cup 1997 Discussion &amp; Resources</a>`,
+		],
+
+		mod: 'gen1jpn',
+		cupLevelLimit: {
+			range: [50, 55],
+			total: 155,
+		},
+		teamLength: {
+			validate: [3, 6],
+			battle: 3,
+		},
+		searchShow: false,
+		ruleset: ['Obtainable', 'Team Preview', 'Cup Level Limit', 'Sleep Clause Mod', 'Species Clause', 'Nickname Clause', 'HP Percentage Mod', 'Cancel Mod'],
+		banlist: ['Uber'],
+		onValidateSet(set) {
+			const rgb97Legality: {[speciesid: string]: {[moveid: string]: string | number}} = {
+				charizard: {fly: 'illegal'},
+				butterfree: {
+					confusion: 12, poisonpowder: 15, stunspore: 16, sleeppowder: 17, supersonic: 21,
+					psybeam: 34, flash: 'illegal', gust: 'illegal',
+				},
+				fearow: {payday: 'illegal'},
+				pikachu: {quickattack: 16, tailwhip: 'illegal', slam: 'illegal', lightscreen: 'illegal'},
+				raichu: {quickattack: 16, tailwhip: 'illegal', slam: 'illegal', lightscreen: 'illegal'},
+				nidoranf: {doublekick: 43},
+				nidorina: {doublekick: 43},
+				nidoqueen: {doublekick: 43},
+				nidoranm: {doublekick: 43},
+				nidorino: {doublekick: 43},
+				nidoking: {doublekick: 43},
+				venonat: {poisonpowder: 24, supersonic: 'illegal', confusion: 'illegal'},
+				venomoth: {poisonpowder: 24, supersonic: 'illegal'},
+				diglett: {cut: 'illegal'},
+				dugtrio: {cut: 'illegal'},
+				psyduck: {amnesia: 'illegal'},
+				golduck: {amnesia: 'illegal'},
+				mankey: {lowkick: 'illegal', screech: 'illegal'},
+				primeape: {lowkick: 'illegal', screech: 'illegal'},
+				kadabra: {kinesis: 'illegal'},
+				alakazam: {kinesis: 'illegal'},
+				rapidash: {payday: 'illegal'},
+				cubone: {tailwhip: 'illegal', headbutt: 'illegal'},
+				marowak: {tailwhip: 'illegal', headbutt: 'illegal'},
+				chansey: {tailwhip: 'illegal'},
+				tangela: {absorb: 29, growth: 49, vinewhip: 'illegal'},
+				scyther: {wingattack: 'illegal'},
+				pinsir: {bind: 'illegal'},
+				magikarp: {dragonrage: 'illegal'},
+				eevee: {quickattack: 27, tailwhip: 31, bite: 37, growl: 'illegal', focusenergy: 'illegal'},
+				vaporeon: {
+					quickattack: 27, tailwhip: 31, watergun: 31, bite: 37, acidarmor: 42, haze: 44, mist: 48, hydropump: 54,
+					growl: 'illegal', focusenergy: 'illegal', aurorabeam: 'illegal',
+				},
+				jolteon: {
+					quickattack: 27, tailwhip: 31, thundershock: 31, bite: 37, doublekick: 42, agility: 44,
+					pinmissile: 48, growl: 'illegal', focusenergy: 'illegal',
+				},
+				flareon: {
+					quickattack: 27, tailwhip: 31, ember: 31, bite: 37, leer: 42, firespin: 44, flamethrower: 54,
+					growl: 'illegal', focusenergy: 'illegal', smog: 'illegal',
+				},
+			};
+			const species = this.dex.species.get(set.species || set.name);
+			const legalityList = rgb97Legality[species.id];
+			if (!legalityList) return;
+			const problems = [];
+			if (set.moves) {
+				for (const moveid of set.moves.map(this.toID)) {
+					if (legalityList[moveid]) {
+						if (legalityList[moveid] === 'illegal') {
+							problems.push(`${set.species} can't learn ${this.dex.moves.get(moveid).name} in Japanese versions of the Gen 1 games.`);
+						} else if (set.level < legalityList[moveid]) {
+							problems.push(`${set.species} can't learn ${this.dex.moves.get(moveid).name} before level ${legalityList[moveid]} in 1997.`);
+						}
+					}
+				}
+			}
+			return problems;
+		},
 	},
 	{
 		name: "[Gen 1] Stadium OU",

data/mods/gen1jpn/moves.ts
@@ -1,0 +1,130 @@
+/**
+ * The japanese version of Blizzard in Gen 1 had a 30% chance to freeze
+ */
+
+export const Moves: {[k: string]: ModdedMoveData} = {
+	blizzard: {
+		inherit: true,
+		secondary: {
+			chance: 30,
+			status: 'frz',
+		},
+	},
+	dig: {
+		inherit: true,
+		condition: {
+			duration: 2,
+			onLockMove: 'dig',
+			onInvulnerability(target, source, move) {
+				if (move.id === 'swift' && target.volatiles['substitute']) return true;
+				this.add('-message', `The foe ${target.name} can't be hit underground!`);
+				return false;
+			},
+			onDamage(damage, target, source, move) {
+				if (!move || move.effectType !== 'Move') return;
+				if (!source) return;
+				if (move.id === 'earthquake') {
+					this.add('-message', `The foe ${target.name} can't be hit underground!`);
+					return null;
+				}
+			},
+		},
+	},
+	fly: {
+		inherit: true,
+		condition: {
+			duration: 2,
+			onLockMove: 'fly',
+			onInvulnerability(target, source, move) {
+				if (move.id === 'swift' && target.volatiles['substitute']) return true;
+				this.add('-message', `The foe ${target.name} can't be hit while flying!`);
+				return false;
+			},
+			onDamage(damage, target, source, move) {
+				if (!move || move.effectType !== 'Move') return;
+				if (!source || source.side === target.side) return;
+				if (move.id === 'gust' || move.id === 'thunder') {
+					this.add('-message', `The foe ${target.name} can't be hit while flying!`);
+					return null;
+				}
+			},
+		},
+	},
+	substitute: {
+		inherit: true,
+		condition: {
+			onStart(target) {
+				this.add('-start', target, 'Substitute');
+				this.effectData.hp = Math.floor(target.maxhp / 4) + 1;
+				delete target.volatiles['partiallytrapped'];
+			},
+			onTryHitPriority: -1,
+			onTryHit(target, source, move) {
+				if (move.drain) {
+					this.add('-miss', source);
+					this.hint("In the Japanese versions of Gen 1, draining moves always miss against substitutes.");
+					return null;
+				}
+				if (move.category === 'Status') {
+					// In gen 1 it only blocks:
+					// poison, confusion, secondary effect confusion, stat reducing moves and Leech Seed.
+					const subBlocked = ['lockon', 'meanlook', 'mindreader', 'nightmare'];
+					if ((move.status && ['psn', 'tox'].includes(move.status)) || (move.boosts && target !== source) ||
+						move.volatileStatus === 'confusion' || subBlocked.includes(move.id)) {
+						return false;
+					}
+					return;
+				}
+				if (move.volatileStatus && target === source) return;
+				// NOTE: In future generations the damage is capped to the remaining HP of the
+				// Substitute, here we deliberately use the uncapped damage when tracking lastDamage etc.
+				// Also, multi-hit moves must always deal the same damage as the first hit for any subsequent hits
+				let uncappedDamage = move.hit > 1 ? source.lastDamage : this.actions.getDamage(source, target, move);
+				if (!uncappedDamage) return null;
+				uncappedDamage = this.runEvent('SubDamage', target, source, move, uncappedDamage);
+				if (!uncappedDamage) return uncappedDamage;
+				source.lastDamage = uncappedDamage;
+				target.volatiles['substitute'].hp -= uncappedDamage > target.volatiles['substitute'].hp ?
+					target.volatiles['substitute'].hp : uncappedDamage;
+				if (target.volatiles['substitute'].hp <= 0) {
+					target.removeVolatile('substitute');
+					target.subFainted = true;
+				} else {
+					this.add('-activate', target, 'Substitute', '[damage]');
+				}
+				// Drain/recoil does not happen if the substitute breaks
+				if (target.volatiles['substitute']) {
+					if (move.recoil) {
+						this.damage(Math.round(uncappedDamage * move.recoil[0] / move.recoil[1]), source, target, 'recoil');
+					}
+					if (move.drain) {
+						this.heal(Math.ceil(uncappedDamage * move.drain[0] / move.drain[1]), source, target, 'drain');
+					}
+				}
+				this.runEvent('AfterSubDamage', target, source, move, uncappedDamage);
+				// Add here counter damage
+				const lastAttackedBy = target.getLastAttackedBy();
+				if (!lastAttackedBy) {
+					target.attackedBy.push({source: source, move: move.id, damage: uncappedDamage, thisTurn: true, slot: source.getSlot()});
+				} else {
+					lastAttackedBy.move = move.id;
+					lastAttackedBy.damage = uncappedDamage;
+				}
+				return 0;
+			},
+			onAccuracy(accuracy, target, source, move) {
+				if (move.id === 'swift') {
+					return true;
+				}
+				return accuracy;
+			},
+			onEnd(target) {
+				this.add('-end', target, 'Substitute');
+			},
+		},
+	},
+	swift: {
+		inherit: true,
+		accuracy: 100,
+	},
+};

data/mods/gen1jpn/scripts.ts
@@ -1,0 +1,4 @@
+export const Scripts: ModdedBattleScriptsData = {
+	inherit: 'gen1',
+	gen: 1,
+};

data/mods/gen2stadium2/conditions.ts
@@ -1,0 +1,133 @@
+/**
+ * Since Stadium 2 ignores stat drops while recalculating stats due to boosting moves, haze,
+ * or stat-lowering moves. The status conditions which affect a targets stats (brn and par) have
+ * a volatile along with them to keep track of if their respective stat changes should be factored
+ * in during stat calculations or not.
+ */
+export const Conditions: {[k: string]: ModdedConditionData} = {
+	brn: {
+		name: 'brn',
+		effectType: 'Status',
+		onStart(target) {
+			this.add('-status', target, 'brn');
+			target.addVolatile('brnattackdrop');
+		},
+		onAfterMoveSelfPriority: 3,
+		onAfterMoveSelf(pokemon) {
+			residualdmg(this, pokemon);
+		},
+		onSwitchIn(pokemon) {
+			pokemon.addVolatile('brnattackdrop');
+		},
+		onAfterSwitchInSelf(pokemon) {
+			residualdmg(this, pokemon);
+		},
+	},
+	par: {
+		name: 'par',
+		effectType: 'Status',
+		onStart(target) {
+			this.add('-status', target, 'par');
+			target.addVolatile('parspeeddrop');
+		},
+		onBeforeMovePriority: 2,
+		onBeforeMove(pokemon) {
+			if (this.randomChance(1, 4)) {
+				this.add('cant', pokemon, 'par');
+				return false;
+			}
+		},
+		onSwitchIn(pokemon) {
+			pokemon.addVolatile('parspeeddrop');
+		},
+	},
+	slp: {
+		name: 'slp',
+		effectType: 'Status',
+		onStart(target, source, sourceEffect) {
+			if (sourceEffect && sourceEffect.effectType === 'Move') {
+				this.add('-status', target, 'slp', '[from] move: ' + sourceEffect.name);
+			} else {
+				this.add('-status', target, 'slp');
+			}
+			// 1-4 turns, guaranteed 1 turn of sleep.
+			this.effectData.time = this.random(2, 5);
+		},
+		onBeforeMovePriority: 10,
+		onBeforeMove(pokemon, target, move) {
+			pokemon.statusData.time--;
+			if (pokemon.statusData.time <= 0) {
+				pokemon.cureStatus();
+				return;
+			}
+			this.add('cant', pokemon, 'slp');
+			if (move.sleepUsable) {
+				return;
+			}
+			return false;
+		},
+	},
+	confusion: {
+		inherit: true,
+		// Stadium 2 fixed Berserk Gene afflicting Confusion for 255 turns.
+		onStart(target, source, sourceEffect) {
+			if (sourceEffect && sourceEffect.id === 'lockedmove') {
+				this.add('-start', target, 'confusion', '[silent]');
+			} else {
+				this.add('-start', target, 'confusion');
+			}
+			this.effectData.time = this.random(2, 6);
+		},
+		onBeforeMove(pokemon, target, move) {
+			pokemon.volatiles['confusion'].time--;
+			if (!pokemon.volatiles['confusion'].time) {
+				pokemon.removeVolatile('confusion');
+				return;
+			}
+			this.add('-activate', pokemon, 'confusion');
+			if (this.randomChance(1, 2)) {
+				return;
+			}
+			move = {
+				basePower: 40,
+				type: '???',
+				baseMoveType: move.type,
+				category: 'Physical',
+				willCrit: false,
+				isConfusionSelfHit: true,
+				noDamageVariance: true,
+				flags: {},
+				selfdestruct: move.selfdestruct,
+			} as unknown as ActiveMove;
+			const damage = this.actions.getDamage(pokemon, pokemon, move);
+			if (typeof damage !== 'number') throw new Error("Confusion damage not dealt");
+			this.directDamage(damage);
+			return false;
+		},
+	},
+	residualdmg: {
+		name: 'residualdmg',
+		onStart(target) {
+			target.volatiles['residualdmg'].counter = 0;
+		},
+		onAfterMoveSelfPriority: 100,
+		onAfterMoveSelf(pokemon) {
+			if (['brn', 'psn', 'tox'].includes(pokemon.status)) pokemon.volatiles['residualdmg'].counter++;
+		},
+		onAfterSwitchInSelf(pokemon) {
+			if (['brn', 'psn', 'tox'].includes(pokemon.status)) pokemon.volatiles['residualdmg'].counter++;
+		},
+	},
+};
+
+function residualdmg(battle: Battle, pokemon: Pokemon) {
+	if (pokemon.volatiles['residualdmg']) {
+		const residualDmg = battle.clampIntRange(
+			Math.floor(pokemon.maxhp / 16) * pokemon.volatiles['residualdmg'].counter, 1
+		);
+		battle.damage(residualDmg, pokemon);
+		battle.hint("In Gen 2, Toxic's counter is retained through Baton Pass/Heal Bell and applies to PSN/BRN.", true);
+	} else {
+		battle.damage(battle.clampIntRange(Math.floor(pokemon.maxhp / 8), 1), pokemon);
+	}
+}

commit 448fe4babbd315460ff7eec6f18c401f58f9f301

Fix crash in Rulesets
Crash introduced in #8195 and has been plaguing us with thousands of
crashes per month for the past two years, but e2be1ce49d46059 finally
let us track it down.

Special thanks to everyone who spent so much time and effort trying
to identify the source of this crash over the past few years. It's
over. It's finally over.

data/rulesets.ts
@@ -1,6 +1,7 @@
 // Note: These are the rules that formats use
 
 import {Utils} from "../lib";
+import type {Learnset} from "../sim/dex-species";
 import {Pokemon} from "../sim/pokemon";
 
 // The list of formats is stored in config/formats.js
@@ -1750,24 +1751,19 @@ export const Rulesets: {[k: string]: FormatData} = {
 		},
 		onValidateSet(set) {
 			const species = this.dex.species.get(set.species);
-			const learnsetData = {...(this.dex.data.Learnsets[species.id]?.learnset || {})};
-			let prevo = species.prevo;
-			while (prevo) {
-				const prevoSpecies = this.dex.species.get(prevo);
-				const prevoLsetData = this.dex.data.Learnsets[prevoSpecies.id]?.learnset || {};
-				for (const moveid in prevoLsetData) {
-					if (!(moveid in learnsetData)) {
-						learnsetData[moveid] = prevoLsetData[moveid];
-					} else {
-						learnsetData[moveid].push(...prevoLsetData[moveid]);
-					}
+			const learnset: NonNullable<Learnset['learnset']> = {};
+			let curSpecies: Species | null = species;
+			while (curSpecies) {
+				const curLearnset = this.dex.species.getLearnset(curSpecies.id) || {};
+				for (const moveid in curLearnset) {
+					learnset[moveid] = [...(learnset[moveid] || []), ...curLearnset[moveid]];
 				}
-				prevo = prevoSpecies.prevo;
+				curSpecies = this.learnsetParent(curSpecies);
 			}
 			const problems = [];
 			if (set.moves?.length) {
 				for (const move of set.moves) {
-					if (learnsetData[this.toID(move)] && !learnsetData[this.toID(move)].filter(v => !v.includes('S')).length) {
+					if (learnset[this.toID(move)]?.every(learned => learned.includes('S'))) {
 						problems.push(`${species.name}'s move ${move} is obtainable only through events.`);
 					}
 				}