78887b31a3686a5f577fb2b138f2d94b5974d379
Redspyboy <adamyoung04@gmail.com>
2023-12-13 13:18:32 -0800

    parental bond test

--- sim/battle-actions.ts
+++ sim/battle-actions.ts
@@ -1695,12 +1695,12 @@ export class BattleActions {
 		const type = move.type;
 
 		baseDamage += 2;
-
+		//TODO: Test Parental Bond Rework
 		if (move.spreadHit) {
 			// multi-target modifier (doubles only)
 			const spreadModifier = move.spreadModifier || (this.battle.gameType === 'freeforall' ? 0.5 : 0.75);
 			this.battle.debug('Spread modifier: ' + spreadModifier);
-			baseDamage = this.battle.modify(baseDamage, spreadModifier);
+			baseDamage = move.damage === 'level' ? pokemon.level : this.battle.modify(baseDamage, spreadModifier);
 		} else if (move.multihitType === 'parentalbond' && move.hit > 1) {
 			// Parental Bond modifier
 			const bondModifier = this.battle.gen > 6 ? 0.25 : 0.5;
