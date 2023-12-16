cc977f446cbd13e9fb2466b38f9bf9093fba31dc
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2023-12-15 22:44:35 -0700

    Fix Tera Shell

--- data/abilities.ts
+++ data/abilities.ts
@@ -4681,7 +4681,7 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 			target.abilityState.resisted = true;
 			return -1;
 		},
-		onAfterMove(target, source, move) {
+		onAfterMoveSecondary(target, source, move) {
 			if (target.abilityState.resisted) {
 				delete target.abilityState.resisted;
 			}
