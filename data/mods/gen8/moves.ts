023e941541cc7a3bf9802ee09b8bb5726099ef17
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2023-12-17 10:51:47 -0700

    Mark various moves as unobtainable in Gen 9

--- data/mods/gen8/moves.ts
+++ data/mods/gen8/moves.ts
@@ -54,6 +54,10 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 		inherit: true,
 		isNonstandard: null,
 	},
+	burnup: {
+		inherit: true,
+		isNonstandard: null,
+	},
 	celebrate: {
 		inherit: true,
 		flags: {nosleeptalk: 1, noassist: 1, failcopycat: 1, failinstruct: 1},
@@ -104,6 +108,10 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 		inherit: true,
 		isNonstandard: null,
 	},
+	corrosivegas: {
+		inherit: true,
+		isNonstandard: null,
+	},
 	craftyshield: {
 		inherit: true,
 		isNonstandard: null,
@@ -116,6 +124,10 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 			}
 		},
 		target: "randomNormal",
+	},
+	cut: {
+		inherit: true,
+		isNonstandard: null,
 	},
 	darkvoid: {
 		inherit: true,
@@ -212,8 +224,13 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 		inherit: true,
 		isNonstandard: "Past",
 	},
+	holdback: {
+		inherit: true,
+		isNonstandard: null,
+	},
 	holdhands: {
 		inherit: true,
+		isNonstandard: null,
 		flags: {bypasssub: 1, nosleeptalk: 1, noassist: 1, failcopycat: 1, failinstruct: 1},
 	},
 	hyperspacefury: {
@@ -537,6 +554,10 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 		isNonstandard: "Past",
 	},
 	trickortreat: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	vcreate: {
 		inherit: true,
 		isNonstandard: null,
 	},
