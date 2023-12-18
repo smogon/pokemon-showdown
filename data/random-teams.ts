dcab67139e13c80c8777a5ad2f745a1d12b70318
adrivrie <adriaan.de.vries@hotmail.com>
2023-12-18 01:12:35 +0100

    Challenge Cup: Fix monotype and recurring bug with new Pokemon (#9960)
    * Challenge Cup: Fix monotype and recurring bug with new Pokemon
    
    * missed that monotype gen9 was already fixed

--- data/random-teams.ts
+++ data/random-teams.ts
@@ -2046,9 +2046,6 @@ export class RandomTeams {
 		// Picks `n` random pokemon--no repeats, even among formes
 		// Also need to either normalize for formes or select formes at random
 		// Unreleased are okay but no CAP
-		const last = [0, 151, 251, 386, 493, 649, 721, 807, 898, 1017][this.gen];
-
-		if (n <= 0 || n > last) throw new Error(`n must be a number between 1 and ${last} (got ${n})`);
 		if (requiredType && !this.dex.types.get(requiredType).exists) {
 			throw new Error(`"${requiredType}" is not a valid type.`);
 		}
@@ -2069,7 +2066,6 @@ export class RandomTeams {
 				if (minSourceGen && species.gen < minSourceGen) continue;
 				const num = species.num;
 				if (num <= 0 || pool.includes(num)) continue;
-				if (num > last) break;
 				pool.push(num);
 			}
 		} else {
