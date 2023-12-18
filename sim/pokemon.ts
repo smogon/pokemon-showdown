105c2edc79a31bf58a16f77a4751df0e93f53198
Karthik <32044378+Karthik99999@users.noreply.github.com>
2023-12-17 19:02:46 -0500

    Fix Stomping Tantrum doubling bp after being forced out (#9956)

--- sim/pokemon.ts
+++ sim/pokemon.ts
@@ -1459,6 +1459,8 @@ export class Pokemon {
 		if (this.battle.gen === 2) this.lastMoveEncore = null;
 		this.lastMoveUsed = null;
 		this.moveThisTurn = '';
+		this.moveLastTurnResult = undefined;
+		this.moveThisTurnResult = undefined;
 
 		this.lastDamage = 0;
 		this.attackedBy = [];
