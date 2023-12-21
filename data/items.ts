d4b927bf7644a1b1f5e2e3069a97f73a8c3afa2d
André Bastos Dias <80102738+andrebastosdias@users.noreply.github.com>
2023-12-21 21:20:55 +0000

    Make code more readable

--- data/items.ts
+++ data/items.ts
@@ -629,11 +629,8 @@ export const Items: {[itemid: string]: ItemData} = {
 		},
 		onTakeItem(item, source) {
 			if (source.baseSpecies.tags.includes("Paradox")) {
-				if (['Gouging Fire', 'Raging Bolt', 'Iron Boulder', 'Iron Crown'].includes(source.baseSpecies.baseSpecies)) {
-					this.hint("Gouging Fire, Iron Boulder, Iron Crown and Raging Bolt may lose Booster Energy through item-manipulating moves.");
-				} else {
-					return false;
-				}
+				if (!['Gouging Fire', 'Raging Bolt', 'Iron Boulder', 'Iron Crown'].includes(source.baseSpecies.baseSpecies)) return false;
+				this.hint("Gouging Fire, Iron Boulder, Iron Crown and Raging Bolt may lose Booster Energy through item-manipulating moves.");
 			}
 			return true;
 		},
