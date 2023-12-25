0c1e97bfd96851232515920cd5cb8ac74e2a42dd
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2023-12-25 00:15:03 -0700

    National Dex: Move Darkrai to OU for suspect test

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -2862,7 +2862,7 @@ export const FormatsData: {[k: string]: SpeciesFormatsData} = {
 	darkrai: {
 		tier: "OU",
 		doublesTier: "DUber",
-		natDexTier: "Uber",
+		natDexTier: "OU",
 	},
 	shaymin: {
 		tier: "NU",
