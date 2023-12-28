b60810380622c6f3fa0f7974c68734606e4dd4e9
TomOfTomKat <73961857+TomOfTomKat@users.noreply.github.com>
2023-12-28 00:48:41 -0600

    Lock Tera Shell effect to Terapagos (#10012)
    * Lock Tera Shell effect to Terapagos Stellar
    
    https://www.smogon.com/forums/threads/scarlet-violet-battle-mechanics-research.3709545/page-48#post-9898594
    
    * Update data/abilities.ts
    
    * Update data/abilities.ts
    
    * Update data/abilities.ts
    
    * Resolve null target error
    
    * Apply suggestions from code review
    
    * Update terashell.js
    
    * Update test/sim/abilities/terashell.js
    
    Co-authored-by: André Bastos Dias <80102738+andrebastosdias@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: André Bastos Dias <80102738+andrebastosdias@users.noreply.github.com>
    
    * Fix typo
    
    * Without the debug print
    
    ---------
    
    Co-authored-by: Kris Johnson <11083252+KrisXV@users.noreply.github.com>
    Co-authored-by: André Bastos Dias <80102738+andrebastosdias@users.noreply.github.com>
    Co-authored-by: Leonard Craft III <leonardcraft64@gmail.com>

--- test/sim/abilities/terashell.js
+++ test/sim/abilities/terashell.js
@@ -10,57 +10,57 @@ describe('Tera Shell', function () {
 
 	it(`should take not very effective damage when it is at full health`, function () {
 		battle = common.createBattle([
-			[{species: 'Snorlax', ability: 'terashell', moves: ['sleeptalk']}],
+			[{species: 'Terapagos-Terastal', ability: 'terashell', moves: ['sleeptalk']}],
 			[{species: 'Wynaut', moves: ['wickedblow']}],
 		]);
 
 		battle.makeChoices();
-		const snorlax = battle.p1.active[0];
-		const damage = snorlax.maxhp - snorlax.hp;
-		assert.bounded(damage, [20, 24], `Tera Shell should yield not very effective damage roll, actual damage taken is ${damage}`);
+		const terapagos = battle.p1.active[0];
+		const damage = terapagos.maxhp - terapagos.hp;
+		assert.bounded(damage, [14, 16], `Tera Shell should yield not very effective damage roll, actual damage taken is ${damage}`);
 
 		battle.makeChoices();
-		assert.bounded(snorlax.maxhp - snorlax.hp - damage, [40, 48], `Tera Shell should not reduce damage, because Snorlax was not at full HP`);
+		assert.bounded(terapagos.maxhp - terapagos.hp - damage, [28, 33], `Tera Shell should not reduce damage, because Terapagos-Terastal was not at full HP`);
 	});
 
 	// confirmed here: https://www.smogon.com/forums/threads/scarlet-violet-battle-mechanics-research.3709545/post-9893603
 	it(`should not take precedence over immunities`, function () {
 		battle = common.createBattle([
-			[{species: 'Snorlax', ability: 'terashell', moves: ['sleeptalk']}],
+			[{species: 'Terapagos-Terastal', ability: 'terashell', moves: ['sleeptalk']}],
 			[{species: 'Wynaut', moves: ['shadowball']}],
 		]);
 
 		battle.makeChoices();
-		const snorlax = battle.p1.active[0];
-		assert.fullHP(snorlax);
+		const terapagos = battle.p1.active[0];
+		assert.fullHP(terapagos);
 	});
 
 	// kinda confirmed here: https://youtu.be/-nerhfXrmCM?si=hLzfrfzVDdfNFMbv&t=314
 	// confirmed here: https://www.smogon.com/forums/threads/scarlet-violet-battle-mechanics-research.3709545/post-9893781
 	it('All hits of multi-hit move should be not very effective', function () {
 		battle = common.createBattle([
-			[{species: 'Snorlax', ability: 'terashell', moves: ['sleeptalk']}],
+			[{species: 'Terapagos-Terastal', ability: 'terashell', moves: ['sleeptalk']}],
 			[{species: 'Wynaut', moves: ['surgingstrikes']}],
 		]);
 
 		battle.makeChoices();
-		const snorlax = battle.p1.active[0];
-		const damage = snorlax.maxhp - snorlax.hp;
-		assert.bounded(damage, [21, 27], `Tera Shell should yield not very effective damage for both all hits of multi-hit move, actual damage taken is ${damage}`);
+		const terapagos = battle.p1.active[0];
+		const damage = terapagos.maxhp - terapagos.hp;
+		assert.bounded(damage, [15, 18], `Tera Shell should yield not very effective damage for both all hits of multi-hit move, actual damage taken is ${damage}`);
 	});
 
 	// confirmed here: https://www.smogon.com/forums/threads/scarlet-violet-battle-mechanics-research.3709545/post-9893651
 	it(`should be suppressed by Gastro Acid`, function () {
 		battle = common.createBattle([
-			[{species: 'Snorlax', ability: 'terashell', moves: ['sleeptalk']}],
+			[{species: 'Terapagos-Terastal', ability: 'terashell', moves: ['sleeptalk']}],
 			[{species: 'Wynaut', moves: ['gastroacid', 'wickedblow']}],
 		]);
 
 		battle.makeChoices('move sleeptalk', 'move gastroacid');
 		battle.makeChoices('move sleeptalk', 'move wickedblow');
-		const snorlax = battle.p1.active[0];
-		const damage = snorlax.maxhp - snorlax.hp;
-		assert.bounded(damage, [40, 48], `Tera Shell should not reduce damage, because Tera Shell should be suppressed`);
+		const terapagos = battle.p1.active[0];
+		const damage = terapagos.maxhp - terapagos.hp;
+		assert.bounded(damage, [28, 33], `Tera Shell should not reduce damage, because Tera Shell should be suppressed`);
 	});
 
 	// TODO unconfirmed behavior, currently applies resistance to each type of a possibly multi-type pokemon
