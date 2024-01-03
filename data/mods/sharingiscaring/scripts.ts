549b9fdbeeee06b78a20cd7e8ce362d8bb9ae192
Hisuian Zoroark <96159984+HisuianZoroark@users.noreply.github.com>
2024-01-03 00:12:01 -0500

    Sharing is Caring: Fix Air Balloon (#10074)

--- data/mods/sharingiscaring/scripts.ts
+++ data/mods/sharingiscaring/scripts.ts
@@ -9,13 +9,13 @@ export const Scripts: ModdedBattleScriptsData = {
 			if ('ingrain' in this.volatiles && this.battle.gen >= 4) return true;
 			if ('smackdown' in this.volatiles) return true;
 			const item = (this.ignoringItem() ? '' : this.item);
-			if (item === 'ironball' || this.volatiles['item:ironball']) return true;
+			if (item === 'ironball' || (this.volatiles['item:ironball'] && !this.ignoringItem())) return true;
 			// If a Fire/Flying type uses Burn Up and Roost, it becomes ???/Flying-type, but it's still grounded.
 			if (!negateImmunity && this.hasType('Flying') && !(this.hasType('???') && 'roost' in this.volatiles)) return false;
 			if (this.hasAbility('levitate') && !this.battle.suppressingAbility(this)) return null;
 			if ('magnetrise' in this.volatiles) return false;
 			if ('telekinesis' in this.volatiles) return false;
-			if (item === 'airballoon' || this.volatiles['item:airballoon']) return false;
+			if (item === 'airballoon' || (this.volatiles['item:airballoon'] && !this.ignoringItem())) return false;
 			return true;
 		},
 		hasItem(item) {
