ffc20bb6a235568be0915c538f10e0372ec768a5
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-03-28 22:16:06 -0400

    Update rulesets.ts

--- data/mods/donotuse/rulesets.ts
+++ data/mods/donotuse/rulesets.ts
@@ -10,18 +10,4 @@ export const Rulesets: {[k: string]: ModdedFormatData} = {
 			this.add('-message', `https://www.smogon.com/forums/threads/3734326/`);
 		},
 	},
-	donotusepokedex: {
-		effectType: 'ValidatorRule',
-		name: 'Do Not Use Pokedex',
-		desc: "Only allows Pok&eacute;mon with less than 280 BST, with a select few exceptions",
-		onValidateSet(set, format) {
-			const dnuDex = [
-				"Applin", "Arrokuda", "Azurill", "Bidoof", "Blipbug", "Bounsweet", "Bramblin", "Budew", "Bunnelby", "Burmy", "Cascoon", "Caterpie", "Charcadet", "Cherubi", "Cleffa", "Combee", "Cosmog", "Cottonee", "Diglett", "Diglett-Alola", "Dreepy", "Feebas", "Fletchling", "Fomantis", "Gossifleur", "Happiny", "Hatenna", "Hoothoot", "Hoppip", "Igglybuff", "Impidimp", "Jigglypuff", "Kakuna", "Kirlia", "Kricketot", "Lechonk", "Ledyba", "Lillipup", "Litwick", "Lotad", "Magikarp", "Makuhita", "Mareep", "Marill", "Meditite", "Metapod", "Milcery", "Nacli", "Nickit", "Nidoran-F", "Nincada", "Noibat", "Nymble", "Patrat", "Pawmi", "Petilil", "Pichu", "Pidgey", "Pidove", "Pikipek", "Poochyena", "Ralts", "Rattata", "Rattata-Alola", "Rellor", "Rockruff", "Roggenrola", "Rolycoly", "Rookidee", "Scatterbug", "Seedot", "Sentret", "Shedinja", "Shinx", "Silcoon", "Skitty", "Skwovet", "Slakoth", "Slugma", "Smoliv", "Snom", "Spearow", "Spewpa", "Spinarak", "Starly", "Sunkern", "Surskit", "Swinub", "Tadbulb", "Taillow", "Tarountula", "Togepi", "Toxel", "Tynamo", "Tyrogue", "Venipede", "Wattrel", "Weedle", "Whismur", "Wiglett", "Wimpod", "Wooloo", "Wooper", "Wooper-Paldea", "Wurmple", "Wynaut", "Yamper", "Yungoos", "Zigzagoon-Galar", "Zubat", "Luvdisc", "Unown",
-			];
-			const species = this.dex.species.get(set.species || set.name);
-			if (!dnuDex.includes(species.baseSpecies) && !this.ruleTable.has('+' + species.id)) {
-				return [species.baseSpecies + " is not in the DNU Pokédex."];
-			}
-		},
-	},
 };
