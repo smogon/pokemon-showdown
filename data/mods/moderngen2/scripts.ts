b9b4b2e16b29c5984190b0303411716241eb0c0d
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2024-03-31 21:49:20 -0600

    Update scripts.ts

--- data/mods/moderngen2/scripts.ts
+++ data/mods/moderngen2/scripts.ts
@@ -3,14 +3,18 @@ export const Scripts: ModdedBattleScriptsData = {
 	gen: 2,
 	init() {
 		const specialTypes = ['Fire', 'Water', 'Grass', 'Ice', 'Electric', 'Dark', 'Psychic', 'Dragon'];
-		let newCategory = '';
 		for (const i in this.data.Moves) {
-			if (!this.data.Moves[i]) console.log(i);
+			this.modData('Moves', i).gen = 2;
+			if (this.data.Moves[i].isNonstandard === 'Past') this.modData('Moves', i).isNonstandard = null;
 			if (this.data.Moves[i].category === 'Status') continue;
-			newCategory = specialTypes.includes(this.data.Moves[i].type) ? 'Special' : 'Physical';
+			const newCategory = specialTypes.includes(this.data.Moves[i].type) ? 'Special' : 'Physical';
 			if (newCategory !== this.data.Moves[i].category) {
 				this.modData('Moves', i).category = newCategory;
 			}
+		}
+		for (const i in this.data.Items) {
+			this.modData('Items', i).gen = 2;
+			if (this.data.Items[i].isNonstandard === 'Past') this.modData('Items', i).isNonstandard = null;
 		}
 	},
 };
