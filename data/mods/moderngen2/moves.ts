4e9aaa9fbbec4f7074441f3ed149b1efb1a04f50
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2024-04-01 11:21:27 -0400

    Create moves.ts

---
+++ data/mods/moderngen2/moves.ts
@@ -0,0 +1,17 @@
+export const Moves: {[k: string]: ModdedMoveData} = {
+	doubleironbash: {
+		inherit: true,
+		isNonstandard: null,
+		gen: 1,
+	},
+	thousandarrows: {
+		inherit: true,
+		isNonstandard: null,
+		gen: 1,
+	},
+	thousandwaves: {
+		inherit: true,
+		isNonstandard: null,
+		gen: 1,
+	},
+};
