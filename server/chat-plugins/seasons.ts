eca59d289ce292784308e4d3edd51a9c6b7ee897
Mia <49593536+mia-pi-git@users.noreply.github.com>
2024-04-02 14:25:48 -0500

    Seasons: Fix typo
    findPeriod returns 1/2/3/4 - gotta account for indexing

--- server/chat-plugins/seasons.ts
+++ server/chat-plugins/seasons.ts
@@ -148,7 +148,7 @@ export async function updateBadgeholders() {
 	if (!data.badgeholders[period]) {
 		data.badgeholders[period] = {};
 	}
-	for (const formatName of data.formatSchedule[findPeriod()]) {
+	for (const formatName of data.formatSchedule[findPeriod() - 1]) {
 		const formatid = `gen${Dex.gen}${formatName}`;
 		const response = await getLadderTop(formatid);
 		if (!response) continue; // ??
@@ -198,7 +198,7 @@ export function rollSeason() {
 	if (findPeriod() !== data.current.period) {
 		data.current.season++;
 		data.badgeholders[data.current.season] = {};
-		for (const k of data.formatSchedule[findPeriod()]) {
+		for (const k of data.formatSchedule[findPeriod() - 1]) {
 			data.badgeholders[data.current.season][`gen${Dex.gen}${k}`] = {};
 		}
 		data.current.period = findPeriod();
