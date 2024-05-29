e2fa767925cbd3794a1b0644141a31b91934e225
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2024-05-29 08:52:49 -0600

    RU: Ban Enamorus-Therian

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -5122,7 +5122,7 @@ export const FormatsData: {[k: string]: SpeciesFormatsData} = {
 		natDexTier: "RUBL",
 	},
 	enamorustherian: {
-		tier: "RU",
+		tier: "RUBL",
 		doublesTier: "(DUU)",
 		natDexTier: "RU",
 	},
