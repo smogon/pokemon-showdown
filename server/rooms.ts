f8a2b23c4c804537f1436d9e373a621184cb5f63
Vinayak-Shukla <vinayakshukl@gmail.com>
2024-06-04 02:01:58 +0530

    fixing lint error

--- server/rooms.ts
+++ server/rooms.ts
@@ -1396,8 +1396,8 @@ export class GlobalRoomState {
 		for (const room of Rooms.rooms.values()) {
 			const player = room.game && !room.game.ended && room.game.playerTable[user.id];
 			if (!player) continue;
-			if(room.roomid == "scavengers"){
-				if(player.completed) continue;
+			if (room.roomid === "scavengers") {
+				if (player.completed) continue;
 			}
 			user.games.add(room.roomid);
 			player.name = user.name;
