a4b1ab9006d92d4215143e6f0ab4d86d42aa26d9
Vinayak-Shukla <vinayakshukl@gmail.com>
2024-06-03 20:12:58 +0530

    Scavenger Hunt Bug fix
    Once a user finished a hunt, upon refresh, they would get the scavenger hunt added back to their games list since they're present in the playerTable. Adding a check during RejoinGames to ignore players who're in scavengers and have completed the current game/hunt.

--- server/rooms.ts
+++ server/rooms.ts
@@ -1396,7 +1396,9 @@ export class GlobalRoomState {
 		for (const room of Rooms.rooms.values()) {
 			const player = room.game && !room.game.ended && room.game.playerTable[user.id];
 			if (!player) continue;
-
+			if(room.roomid == "scavengers"){
+				if(player.completed) continue;
+			}
 			user.games.add(room.roomid);
 			player.name = user.name;
 			user.joinRoom(room.roomid);
