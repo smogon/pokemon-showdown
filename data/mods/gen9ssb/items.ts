fc906d6e800c2a3e88a47b94568b582776935389
Zakuree <zach.hartin@gmail.com>
2024-06-08 21:11:11 -0400

    Update items.ts

--- data/mods/gen9ssb/items.ts
+++ data/mods/gen9ssb/items.ts
@@ -5,6 +5,7 @@ export const Items: {[k: string]: ModdedItemData} = {
 		spritenum: 158,
 		onTakeItem: false,
 		desc: "HP >= 33%: All of holder's stats are x1.33. Cannot be taken or removed.",
+		gen: 9,
 		onModifyAtkPriority: 1,
 		onModifyAtk(atk, pokemon) {
 			if (pokemon.hp >= (pokemon.hp / 3)) {
@@ -48,6 +49,29 @@ export const Items: {[k: string]: ModdedItemData} = {
 			if (source.hp >= (source.hp / 3)) {
 				return this.chainModify([3072, 4096]);
 			}
+		},
+	},
+	// Pablo
+	sketchbook: {
+		name: "Sketchbook",
+		spritenum: 200,
+		desc: "On switch-in, this Pokemon copies the stat changes of the opposing Pokemon.",
+		gen: 9,
+		onStart(target, source) {
+			let i: BoostID;
+			for (i in target.boosts) {
+				source.boosts[i] = target.boosts[i];
+			}
+			const volatilesToCopy = ['focusenergy', 'gmaxchistrike', 'laserfocus'];
+			for (const volatile of volatilesToCopy) {
+				if (target.volatiles[volatile]) {
+					source.addVolatile(volatile);
+					if (volatile === 'gmaxchistrike') source.volatiles[volatile].layers = target.volatiles[volatile].layers;
+				} else {
+					source.removeVolatile(volatile);
+				}
+			}
+			this.add('-copyboost', source, target, '[from] item: Sketchbook');
 		},
 	},
 };
