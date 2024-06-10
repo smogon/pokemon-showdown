dfa9e9fd17544ee462f50a19e3a308b00cab8896
Zakuree <zach.hartin@gmail.com>
2024-06-10 15:50:51 -0400

    Update items.ts

--- data/mods/gen9ssb/items.ts
+++ data/mods/gen9ssb/items.ts
@@ -21,13 +21,8 @@ export const Items: {[k: string]: ModdedItemData} = {
 		spritenum: 200,
 		desc: "On switch-in, this Pokemon copies the stat changes of the opposing Pokemon.",
 		gen: 9,
-		onStart(pokemon, target) {
-			const boosts: SparseBoostsTable = {};
-			let i: BoostID;
-			for (i in target.boosts) {
-				pokemon.boosts[i] = target.boosts[i];
-			}
-			this.add('-copyboost', pokemon, target, '[from] item: Sketchbook');
+		onStart(target) {
+			this.actions.useMove('Sketchbook', target);
 		},
 	},
 };
