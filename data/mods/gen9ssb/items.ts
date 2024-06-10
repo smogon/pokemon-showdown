bd07e0f7620ae97dc6812b69d20b6112e35a97b3
Zakuree <zach.hartin@gmail.com>
2024-06-10 15:07:42 -0400

    Update items.ts

--- data/mods/gen9ssb/items.ts
+++ data/mods/gen9ssb/items.ts
@@ -5,13 +5,9 @@ export const Items: {[k: string]: ModdedItemData} = {
 		spritenum: 69,
 		desc: "This Pokemon's physical attacks become special. Allows use of Fear the Finger.",
 		gen: 9,
-		onUpdate(pokemon) {
-			for (const moveSlot of pokemon.moveSlots) {
-				const move = this.dex.moves.get(moveSlot.id);
-				if (move.category === 'Physical') {
-					pokemon.disableMove(moveSlot.id);
-					move.category = 'Special';
-				}
+		onModifyMove(move, pokemon) {
+			if (move.category === 'Physical') {
+				move.category = 'Special';
 			}
 		},
 		onTakeItem: false,
@@ -25,21 +21,12 @@ export const Items: {[k: string]: ModdedItemData} = {
 		spritenum: 200,
 		desc: "On switch-in, this Pokemon copies the stat changes of the opposing Pokemon.",
 		gen: 9,
-		onStart(target, source) {
+		onStart(target, pokemon) {
 			let i: BoostID;
 			for (i in target.boosts) {
-				source.boosts[i] = target.boosts[i];
+				pokemon.boosts[i] = target.boosts[i];
 			}
-			const volatilesToCopy = ['focusenergy', 'gmaxchistrike', 'laserfocus'];
-			for (const volatile of volatilesToCopy) {
-				if (target.volatiles[volatile]) {
-					source.addVolatile(volatile);
-					if (volatile === 'gmaxchistrike') source.volatiles[volatile].layers = target.volatiles[volatile].layers;
-				} else {
-					source.removeVolatile(volatile);
-				}
-			}
-			this.add('-copyboost', source, target, '[from] item: Sketchbook');
+			this.add('-copyboost', pokemon, target, '[from] item: Sketchbook');
 		},
 	},
 };
