86743a6e10f44f193f9b5094e2737e25aacce1a6
Zakuree <zach.hartin@gmail.com>
2024-06-19 18:09:41 -0400

    Update pokedex.ts

--- data/mods/gen9ssb/pokedex.ts
+++ data/mods/gen9ssb/pokedex.ts
@@ -19,36 +19,47 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 	miniorviolet: {
 		inherit: true,
 		types: ['Rock', 'Ghost'],
+		abilities: {0: "Chimeraplex"},
 		baseStats: {hp: 100, atk: 100, def: 60, spa: 100, spd: 60, spe: 120},
 	},
 	miniorvindigo: {
 		inherit: true,
 		types: ['Rock', 'Water'],
+		abilities: {0: "Chimeraplex"},
 		baseStats: {hp: 120, atk: 80, def: 60, spa: 100, spd: 60, spe: 120},
 	},
 	miniorgreen: {
 		inherit: true,
 		types: ['Rock', 'Grass'],
+		abilities: {0: "Chimeraplex"},
 		baseStats: {hp: 100, atk: 80, def: 60, spa: 120, spd: 80, spe: 100},
 	},
 	miniorblue: {
 		inherit: true,
 		types: ['Rock', 'Ice'],
+		abilities: {0: "Chimeraplex"},
 		baseStats: {hp: 100, atk: 80, def: 80, spa: 100, spd: 60, spe: 120},
 	},
 	minioryellow: {
 		inherit: true,
 		types: ['Rock', 'Electric'],
+		abilities: {0: "Chimeraplex"},
 		baseStats: {hp: 80, atk: 100, def: 60, spa: 100, spd: 60, spe: 140},
 	},
 	miniororange: {
 		inherit: true,
 		types: ['Rock', 'Ground'],
+		abilities: {0: "Chimeraplex"},
 		baseStats: {hp: 100, atk: 100, def: 80, spa: 80, spd: 80, spe: 100},
 	},
 	minior: {
 		inherit: true,
 		types: ['Rock', 'Fire'],
+		abilities: {0: "Chimeraplex"},
 		baseStats: {hp: 100, atk: 120, def: 40, spa: 120, spd: 40, spe: 120},
+	},
+	miniormeteor: {
+		inherit: true,
+		abilities: {0: "Chimeraplex"},
 	},
 };
