cccc7db25c12eb6a38297d376a0cc3887882a05f
shrianshChari <30420527+shrianshChari@users.noreply.github.com>
2024-07-01 09:14:51 -0700

    Add [Gen 5] BW1 OU (#10380)
    * Add BW1OU
    
    * Add BW1 OU Resource thread
    
    * Add March 2012 OU/UUBL/UU tiering placements
    https://www.smogon.com/forums/threads/smogon-university-po-statistics-%E2%80%94-march-2012.3464495/
    
    * Remove Froslass and Gothitelle from UUBL
    
    * Move BW1 OU below official BW tiers
    
    * Satisfy linter 1
    
    * Satisfy linter 2
    
    * Update data/mods/gen5bw1/formats-data.ts
    
    * Update config/formats.ts
    
    ---------
    
    Co-authored-by: Kris Johnson <11083252+KrisXV@users.noreply.github.com>

--- data/mods/gen5bw1/formats-data.ts
+++ data/mods/gen5bw1/formats-data.ts
@@ -1,4 +1,79 @@
 export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+	venusaur: {
+		tier: "OU",
+	},
+	dugtrio: {
+		tier: "OU",
+	},
+	cloyster: {
+		tier: "OU",
+	},
+	chansey: {
+		tier: "OU",
+	},
+	vaporeon: {
+		tier: "OU",
+	},
+	jolteon: {
+		tier: "OU",
+	},
+	espeon: {
+		tier: "OU",
+	},
+	donphan: {
+		tier: "OU",
+	},
+	froslass: {
+		tier: "UU",
+	},
+	metagross: {
+		tier: "OU",
+	},
+	deoxysdefense: {
+		tier: "UUBL",
+	},
+	infernape: {
+		tier: "OU",
+	},
+	lucario: {
+		tier: "OU",
+	},
+	hippowdon: {
+		tier: "UUBL",
+	},
+	snover: {
+		tier: "UUBL",
+	},
+	abomasnow: {
+		tier: "UUBL",
+	},
+	garchomp: {
+		tier: "Uber",
+	},
+	excadrill: {
+		tier: "Uber",
+	},
+	scrafty: {
+		tier: "OU",
+	},
+	gothitelle: {
+		tier: "PU",
+	},
+	chandelure: {
+		tier: "UU",
+	},
+	haxorus: {
+		tier: "OU",
+	},
+	mienshao: {
+		tier: "OU",
+	},
+	virizion: {
+		tier: "OU",
+	},
+	tornadus: {
+		tier: "OU",
+	},
 	tornadustherian: {
 		isNonstandard: "Future",
 		tier: "Illegal",
@@ -6,6 +81,9 @@ export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
 	thundurustherian: {
 		isNonstandard: "Future",
 		tier: "Illegal",
+	},
+	landorus: {
+		tier: "OU",
 	},
 	landorustherian: {
 		isNonstandard: "Future",
