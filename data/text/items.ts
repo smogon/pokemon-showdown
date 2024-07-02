0af9f7029486a8182993a3a50280e1be510d7581
Marty-D <Marty-D@users.noreply.github.com>
2024-07-01 23:19:02 -0400

    Update data/text/items.ts

--- data/text/items.ts
+++ data/text/items.ts
@@ -833,7 +833,7 @@ export const ItemsText: {[id: IDEntry]: ItemText} = {
 		desc: "Evolves Alolan Sandshrew into Alolan Sandslash, Alolan Vulpix into Alolan Ninetales, Eevee into Glaceon, and Galarian Darumaka into Galarian Darmanitan when used.",
 		shortDesc: "Evolves certain species of Pokemon when used.",
 		gen7: {
-			shortDesc: "Evolves Alolan Sandshrew into Alolan Sandslash and Alolan Vulpix into Alolan Ninetales when used.",
+			desc: "Evolves Alolan Sandshrew into Alolan Sandslash and Alolan Vulpix into Alolan Ninetales when used.",
 		},
 	},
 	icicleplate: {
