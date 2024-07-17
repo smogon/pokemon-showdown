a79478f389aae24d5c273a8b5e2db229e361e058
Zakuree <zach.hartin@gmail.com>
2024-07-17 15:23:52 -0400

    Update abilities.ts

--- data/mods/gen9ssb/abilities.ts
+++ data/mods/gen9ssb/abilities.ts
@@ -15,6 +15,17 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
 	},
 	*/
 	// Please keep abilites organized alphabetically based on staff member name!
+	// Yukari Yakumo
+	spiritingaway: {
+		desc: "After using a move, this Pokemon switches to an ally of the user's choice. Sleep turns still burn while inactive.",
+		shortDesc: "User switches after move; sleep turns burn while inactive.",
+		onModifyMove(move) {
+			move.selfSwitch = true;
+		},
+		flags: {},
+		name: "Spiriting Away",
+		gen: 9,
+}	,
 	// Cylcommatic Cell
 	batterylife: {
 		name: "Battery Life",
