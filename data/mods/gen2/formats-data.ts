473797d29d5154ac15dae5908735377609153ee7
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2024-07-21 21:04:39 -0600

    GSC: Move Alakazam and Jolteon to OU

--- data/mods/gen2/formats-data.ts
+++ data/mods/gen2/formats-data.ts
@@ -210,7 +210,7 @@ export const FormatsData: import('../../../sim/dex-species').ModdedSpeciesFormat
 		tier: "UU",
 	},
 	alakazam: {
-		tier: "UUBL",
+		tier: "OU",
 	},
 	machop: {
 		tier: "LC",
@@ -450,7 +450,7 @@ export const FormatsData: import('../../../sim/dex-species').ModdedSpeciesFormat
 		tier: "OU",
 	},
 	jolteon: {
-		tier: "UUBL",
+		tier: "OU",
 	},
 	flareon: {
 		tier: "NU",
