b74ffb3d8a440ea48cf037852adf3ea0ecc95fe3
Kaen <66154904+Seerd@users.noreply.github.com>
2024-08-01 12:58:47 -0400

    Pokemoves: fix banlist

--- config/formats.ts
+++ config/formats.ts
@@ -513,13 +513,13 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		],
 		restricted: [
 			'Araquanid', 'Avalugg-Hisui', 'Baxcalibur', 'Beartic', 'Breloom', 'Brute Bonnet', 'Cacnea', 'Cacturne', 'Chandelure', 'Conkeldurr', 'Copperajah', 'Crabominable',
-			'Cubchoo', 'Darkrai', 'Dewpider', 'Diglett', 'Diglett-Alola', 'Dragonite', 'Dugtrio', 'Dugtrio-Alola', 'Enamorus-Base', 'Enamorus-Therian', 'Espeon', 'Excadrill',
-			'Flareon', 'Froslass', 'Gabite', 'Garchomp', 'Gengar', 'Gholdengo', 'Gible', 'Glaceon', 'Glastrier', 'Glimmora', 'Great Tusk', 'Grimer', 'Hatterene', 'Haxorus',
-			'Heatran', 'Hoopa-Base', 'Iron Hands', 'Iron Leaves', 'Iron Moth', 'Iron Thorns', 'Iron Valiant', 'Keldeo', 'Kingambit', 'Kleavor', 'Kyurem', 'Landorus-Therian',
-			'Latios', 'Magearna', 'Magnezone', 'Mamoswine', 'Medicham', 'Meditite', 'Meloetta-Base', 'Meloetta-Pirouette', 'Metagross', 'Muk', 'Munkidori', 'Necrozma',
-			'Ninetales-Alola', 'Okidogi', 'Palafin-Hero', 'Polteageist', 'Porygon-Z', 'Primarina', 'Raging Bolt', 'Rampardos', 'Regigigas', 'Rhydon', 'Rhyperior', 'Roaring Moon',
-			'Salamence', 'Sandshrew', 'Sandshrew-Alola', 'Sandslash', 'Sandslash-Alola', 'Scizor', 'Skuntank', 'Slaking', 'Slither Wing', 'Sneasler', 'Stunky', 'Terapagos-Stellar',
-			'Terrakion', 'Thundurus-Therian', 'Tyranitar', 'Ursaluna-Base', 'Ursaluna-Bloodmoon', 'Ursaring', 'Vikavolt', 'Volcanion', 'Volcarona', 'Vulpix-Alola', 'Yanma', 'Yanmega',
+			'Cubchoo', 'Dewpider', 'Diglett', 'Diglett-Alola', 'Dragonite', 'Dugtrio', 'Dugtrio-Alola', 'Enamorus-Base', 'Enamorus-Therian', 'Espeon', 'Excadrill', 'Flareon',
+			'Froslass', 'Gabite', 'Garchomp', 'Gengar', 'Gholdengo', 'Gible', 'Glaceon', 'Glastrier', 'Glimmora', 'Great Tusk', 'Grimer', 'Hatterene', 'Haxorus', 'Heatran',
+			'Hoopa-Base', 'Iron Hands', 'Iron Leaves', 'Iron Moth', 'Iron Thorns', 'Iron Valiant', 'Keldeo', 'Kingambit', 'Kleavor', 'Kyurem', 'Landorus-Therian', 'Latios',
+			'Magearna', 'Magnezone', 'Mamoswine', 'Medicham', 'Meditite', 'Meloetta-Base', 'Meloetta-Pirouette', 'Metagross', 'Muk', 'Munkidori', 'Necrozma', 'Ninetales-Alola',
+			'Okidogi', 'Polteageist', 'Porygon-Z', 'Primarina', 'Raging Bolt', 'Rampardos', 'Regigigas', 'Rhydon', 'Rhyperior', 'Roaring Moon', 'Salamence', 'Sandshrew',
+			'Sandshrew-Alola', 'Sandslash', 'Sandslash-Alola', 'Scizor', 'Skuntank', 'Slaking', 'Slither Wing', 'Sneasler', 'Stunky', 'Terapagos-Stellar', 'Terrakion',
+			'Thundurus-Therian', 'Tyranitar', 'Ursaluna-Base', 'Ursaluna-Bloodmoon', 'Ursaring', 'Vikavolt', 'Volcanion', 'Volcarona', 'Vulpix-Alola', 'Yanma', 'Yanmega',
 		],
 		validateSet(set, teamHas) {
 			let pokemoves = 0;
