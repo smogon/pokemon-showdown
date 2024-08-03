49bae71e16202716e29b5f4850a0b044b14180b8
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2024-08-03 00:20:20 -0600

    Move Pokemoves into a mod

--- config/formats.ts
+++ config/formats.ts
@@ -598,7 +598,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		},
 		onSwitchIn(pokemon) {
 			if (!pokemon.m.pokemove) return;
-			const pokemove = this.dex.species.get(pokemon.m.pokemove);
+			const pokemove = pokemon.m.pokemove;
 			if (!pokemove.exists) return;
 			// Place volatiles on the Pokémon to show the pokemove.
 			this.add('-start', pokemon, pokemove.name, '[silent]');
@@ -624,51 +624,6 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 					}
 				};
 			}
-		},
-		field: {
-			suppressingWeather() {
-				for (const pokemon of this.battle.getAllActive()) {
-					const pokemove = pokemon.m.pokemove;
-					if (pokemon && !pokemon.ignoringAbility() &&
-						(pokemon.getAbility().suppressWeather ||
-							(pokemove && pokemon.volatiles['ability:' + pokemove] &&
-								this.battle.dex.abilities.get(pokemove).suppressWeather))) {
-						return true;
-					}
-				}
-				return false;
-			},
-		},
-		pokemon: {
-			hasAbility(ability) {
-				if (this.ignoringAbility()) return false;
-				if (Array.isArray(ability)) return ability.some(abil => this.hasAbility(abil));
-				const abilityid = this.battle.toID(ability);
-				return this.ability === abilityid || !!this.volatiles['ability:' + abilityid];
-			},
-			ignoringAbility() {
-				// Check if any active pokemon have the ability Neutralizing Gas
-				let neutralizinggas = false;
-				for (const pokemon of this.battle.getAllActive()) {
-					// can't use hasAbility because it would lead to infinite recursion
-					if (
-						(pokemon.ability === ('neutralizinggas' as ID) || pokemon.volatiles['ability:neutralizinggas']) &&
-						!pokemon.volatiles['gastroacid'] && !pokemon.abilityState.ending
-					) {
-						neutralizinggas = true;
-						break;
-					}
-				}
-
-				return !!(
-					(this.battle.gen >= 5 && !this.isActive) ||
-					((this.volatiles['gastroacid'] ||
-						(neutralizinggas && (this.ability !== ('neutralizinggas' as ID) ||
-							this.m.volatiles['ability:neutralizinggas'])
-						)) && !this.getAbility().flags['cantsuppress']
-					)
-				);
-			},
 		},
 	},
 	{
