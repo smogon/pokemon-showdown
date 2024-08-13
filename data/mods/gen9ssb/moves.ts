0466115f3486873ebee602d78ca2f28d34dd6900
Zakuree <zach.hartin@gmail.com>
2024-08-13 14:53:18 -0400

    Update moves.ts

--- data/mods/gen9ssb/moves.ts
+++ data/mods/gen9ssb/moves.ts
@@ -1377,6 +1377,44 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 		target: "normal",
 		type: "Normal",
 	},
+	// Pablo
+	sketch: {
+		num: 166,
+		accuracy: true,
+		basePower: 0,
+		category: "Status",
+		name: "Sketch",
+		pp: 16,
+		noPPBoosts: true,
+		priority: 0,
+		flags: {
+			bypasssub: 1, allyanim: 1, failencore: 1, nosleeptalk: 1, noassist: 1,
+			failcopycat: 1, failmimic: 1, failinstruct: 1, nosketch: 1,
+		},
+		onHit(target, source) {
+			const move = target.lastMove;
+			if (source.transformed || !move || source.moves.includes(move.id)) return false;
+			if (move.flags['nosketch'] || move.isZ || move.isMax) return false;
+			const sketchIndex = source.moves.indexOf('sketch');
+			if (sketchIndex < 0) return false;
+			const sketchedMove = {
+				move: move.name,
+				id: move.id,
+				pp: move.pp,
+				maxpp: move.pp,
+				target: move.target,
+				disabled: false,
+				used: false,
+			};
+			source.moveSlots[sketchIndex] = sketchedMove;
+			this.add('-activate', source, 'move: Sketch', move.name);
+		},
+		secondary: null,
+		target: "normal",
+		type: "Normal",
+		zMove: {boost: {atk: 1, def: 1, spa: 1, spd: 1, spe: 1}},
+		contestType: "Clever",
+	},
 	// Trey
 	burstdelta: {
 		accuracy: true,
