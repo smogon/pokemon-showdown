8f91510e1ecd1f24adfaa14bb7b8c2702eba11d7
Zakuree <zach.hartin@gmail.com>
2024-08-22 08:43:15 -0400

    Update random-teams.ts

--- data/mods/gen9ssb/random-teams.ts
+++ data/mods/gen9ssb/random-teams.ts
@@ -193,6 +193,12 @@ export const ssbSets: SSBSets = {
 		signatureMove: 'Omen of Defeat',
 		evs: {hp: 252, atk: 4, spe: 252}, nature: 'Jolly',
 	},
+	Journeyman: {
+		species: 'Stonjourner', ability: 'Love of the Journey', item: 'Colossus Carrier', gender: 'M',
+		moves: ['Mighty Cleave', 'Trailblaze', 'Court Change'],
+		signatureMove: 'New Beginnings',
+		evs: {atk: 188, spd: 104, spe: 216}, ivs: {spa: 0}, nature: 'Adamant',
+	},
 	/*
 	'Castaways': {
 		species: 'Swalot', ability: 'Aftermath', item: 'Black Sludge', gender: 'M',
