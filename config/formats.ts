7856f41a3382a5fd6db4444467a04d7029497fe3
livid washed <115855253+livid-washed@users.noreply.github.com>
2024-09-01 16:45:32 +1000

    Add September 2024 Randomized Format Spotlight (#10522)
    * Add September 2024 Randomized Format Spotlight
    
    * CAP rands stuff
    
    * fix name of format
    
    * undo CAP rands stuff since it'll be in another pr
    
    * lint
    
    * Update config/formats.ts
    
    * Update config/formats.ts
    
    * Update config/formats.ts
    
    ---------
    
    Co-authored-by: Kris Johnson <11083252+KrisXV@users.noreply.github.com>

--- config/formats.ts
+++ config/formats.ts
@@ -2314,11 +2314,39 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		column: 3,
 	},
 	{
-		name: "[Gen 9] CAP Random Battle",
-		desc: "[Gen 9] Random Battle with CAP Pok&eacute;mon included. Every team will generate with at least one CAP Pok&eacute;mon.",
-		mod: 'gen9',
+		name: "[Gen 9] Shared Power RandBats (Bring 12 Pick 6)",
+		desc: `[Gen 9] Random Battle with Team Preview, Bring 12 Pick 6, and Shared Power.`,
+		mod: 'sharedpower',
 		team: 'random',
-		ruleset: ['[Gen 9] Random Battle'],
+		ruleset: ['[Gen 9] Random Battle', 'Picked Team Size = 6', 'Max Team Size = 12', 'Team Preview'],
+		onValidateRule() {
+			if (this.format.gameType !== 'singles') {
+				throw new Error(`Shared Power currently does not support ${this.format.gameType} battles.`);
+			}
+		},
+		onBeforeSwitchIn(pokemon) {
+			let format = this.format;
+			if (!format.getSharedPower) format = this.dex.formats.get('gen9sharedpower');
+			for (const ability of format.getSharedPower!(pokemon)) {
+				const effect = 'ability:' + ability;
+				pokemon.volatiles[effect] = {id: this.toID(effect), target: pokemon};
+				if (!pokemon.m.abils) pokemon.m.abils = [];
+				if (!pokemon.m.abils.includes(effect)) pokemon.m.abils.push(effect);
+			}
+		},
+		onSwitchInPriority: 2,
+		onSwitchIn(pokemon) {
+			let format = this.format;
+			if (!format.getSharedPower) format = this.dex.formats.get('gen9sharedpower');
+			for (const ability of format.getSharedPower!(pokemon)) {
+				if (ability === 'noability') {
+					this.hint(`Mirror Armor and Trace break in Shared Power formats that don't use Shared Power as a base, so they get removed from non-base users.`);
+				}
+				const effect = 'ability:' + ability;
+				delete pokemon.volatiles[effect];
+				pokemon.addVolatile(effect);
+			}
+		},
 	},
 
 	// Randomized Metas
