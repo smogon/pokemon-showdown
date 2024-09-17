247c2aa70a5a0802765ea752d3ef729605a2bcb0
adrivrie <adriaan.de.vries@hotmail.com>
2024-09-17 15:09:48 +0200

    Random typo fixes (#10556)

--- server/private-messages/index.ts
+++ server/private-messages/index.ts
@@ -75,7 +75,7 @@ export const PrivateMessages = new class {
 		case 'none':
 			// drivers+ can override
 			if (!Auth.atLeast(Users.globalAuth.get(from as ID), '%')) {
-				throw new Chat.ErrorMessage(`${to} has indicated that they do not wish to receive offine PMs.`);
+				throw new Chat.ErrorMessage(`${to} has indicated that they do not wish to receive offline PMs.`);
 			}
 			break;
 		default:
@@ -102,7 +102,7 @@ export const PrivateMessages = new class {
 		}
 		if (!(options.isLogin ? user.registered : user.autoconfirmed)) {
 			if (options.forceBool) return false;
-			throw new Chat.ErrorMessage("You must be autoconfirmed to use offine messaging.");
+			throw new Chat.ErrorMessage("You must be autoconfirmed to use offline messaging.");
 		}
 		if (!Users.globalAuth.atLeast(user, Config.usesqlitepms)) {
 			if (options.forceBool) return false;
@@ -230,4 +230,3 @@ if (Config.usesqlite) {
 		});
 	}
 }
-
