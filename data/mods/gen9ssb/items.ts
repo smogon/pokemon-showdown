d1a80648ab70762d64dd545ed201e7169baf540f
Zakuree <zach.hartin@gmail.com>
2024-10-01 12:40:38 -0400

    Update items.ts

--- data/mods/gen9ssb/items.ts
+++ data/mods/gen9ssb/items.ts
@@ -19,8 +19,8 @@ export const Items: {[k: string]: ModdedItemData} = {
 		onTryHit(pokemon, source, move) {
 			if (pokemon !== source && move.type === 'Ground' || move.type === 'Rock') {
 				this.add('-activate', pokemon, 'item: Hadean Soil', move.name);
-				this.heal(pokemon.maxhp / 16, pokemon, 'item: Hadean Soil');
-				return null;
+				this.heal(pokemon.maxhp / 16, pokemon);
+				return false;
 			}
 		},
 		onAfterMoveSecondarySelf(source, target, move) {
