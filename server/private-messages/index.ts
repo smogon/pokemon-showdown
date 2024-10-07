175d07f572617137cbd003cd8f5a3d770583bd0f
justin22a <92882364+justin22a@users.noreply.github.com>
2024-10-06 22:34:09 -0400

    add fixes index.ts
    This was to help with error handling and further documentation on the code. It also has some extra sorting logic that is further optimized. The render received function had improved efficiency.

--- server/private-messages/index.ts
+++ server/private-messages/index.ts
@@ -1,232 +1,297 @@
 /**
  * Private message handling, particularly for offline messages.
+ * Handles offline PM sending, receiving, and viewing.
  * By Mia.
  * @author mia-pi-git
  */
-import {SQL, Utils} from '../../lib';
-import {Config} from '../config-loader';
-import {Auth} from '../user-groups';
-import {statements} from './database';
-/** The time until a PM sent offline expires. Presently, 60 days. */
+import { SQL, Utils } from '../../lib';
+import { Config } from '../config-loader';
+import { Auth } from '../user-groups';
+import { statements } from './database';
+
+/** The time until a PM sent offline expires. Currently, 60 days. */
 export const EXPIRY_TIME = 60 * 24 * 60 * 60 * 1000;
-/** The time until a PM that has been seen by the user expires. Presently, one week.*/
+/** The time until a PM that has been seen by the user expires. Currently, one week. */
 export const SEEN_EXPIRY_TIME = 7 * 24 * 60 * 60 * 1000;
-/** The max PMs that one user can have pending to a specific user at one time */
+/** The max PMs that one user can have pending to a specific user at one time. */
 export const MAX_PENDING = 20;
 
-// this would be in database.ts, but for some weird reason, if the extension and the pm are the same
-// it doesn't work. all the keys in the require() result are there, but they're also set to undefined.
-// no idea why.
+// SQLite database configuration
 export const PM = SQL(module, {
-	file: 'databases/offline-pms.db',
-	extension: 'server/private-messages/database.js',
+  file: 'databases/offline-pms.db',
+  extension: 'server/private-messages/database.js',
 });
 
 export interface ReceivedPM {
-	time: number;
-	sender: string;
-	receiver: string;
-	seen: number | null;
-	message: string;
+  time: number;
+  sender: string;
+  receiver: string;
+  seen: number | null;
+  message: string;
 }
 
 export const PrivateMessages = new class {
-	database = PM;
-	clearInterval = this.nextClear();
-	offlineIsEnabled = Config.usesqlitepms && Config.usesqlite;
-	async sendOffline(to: string, from: User | string, message: string, context?: Chat.CommandContext) {
-		await this.checkCanSend(to, from);
-		const result = await PM.transaction('send', [toID(from), toID(to), message]);
-		if (result.error) throw new Chat.ErrorMessage(result.error);
-		if (typeof from === 'object') {
-			from.send(`|pm|${this.getIdentity(from)}|${this.getIdentity(to)}|${message} __[sent offline]__`);
-		}
-		const changed = !!result.changes;
-		if (changed && context) {
-			Chat.runHandlers('onMessageOffline', context, message, toID(to));
-		}
-		return changed;
-	}
-	getSettings(userid: string) {
-		return PM.get(statements.getSettings, [toID(userid)]);
-	}
-	deleteSettings(userid: string) {
-		return PM.run(statements.deleteSettings, [toID(userid)]);
-	}
-	async checkCanSend(to: string, from: User | string) {
-		from = toID(from);
-		to = toID(to);
-		const setting = await this.getSettings(to);
-		const requirement = setting?.view_only || Config.usesqlitepms || "friends";
-		switch (requirement) {
-		case 'friends':
-			if (!(await Chat.Friends.findFriendship(to, from))) {
-				if (Config.usesqlitepms === 'friends') {
-					throw new Chat.ErrorMessage(`At this time, you may only send offline PMs to friends. ${to} is not friends with you.`);
-				}
-				throw new Chat.ErrorMessage(`${to} is only accepting offline PMs from friends at this time.`);
-			}
-			break;
-		case 'trusted':
-			if (!Users.globalAuth.has(toID(from))) {
-				throw new Chat.ErrorMessage(`${to} is currently blocking offline PMs from non-trusted users.`);
-			}
-			break;
-		case 'none':
-			// drivers+ can override
-			if (!Auth.atLeast(Users.globalAuth.get(from as ID), '%')) {
-				throw new Chat.ErrorMessage(`${to} has indicated that they do not wish to receive offline PMs.`);
-			}
-			break;
-		default:
-			if (!Auth.atLeast(Users.globalAuth.get(from as ID), requirement)) {
-				if (setting?.view_only) {
-					throw new Chat.ErrorMessage(`That user is not allowing offline PMs from your rank at this time.`);
-				}
-				throw new Chat.ErrorMessage('You do not meet the rank requirement to send offline PMs at this time.');
-			}
-			break;
-		}
-	}
-	setViewOnly(user: User | string, val: string | null) {
-		const id = toID(user);
-		if (!val) { // if null, no need to save
-			return PM.run(statements.deleteSettings, [id]);
-		}
-		return PM.run(statements.setBlock, [id, val]);
-	}
-	checkCanUse(user: User, options = {forceBool: false, isLogin: false}) {
-		if (!this.offlineIsEnabled) {
-			if (options.forceBool) return false;
-			throw new Chat.ErrorMessage(`Offline PMs are currently disabled.`);
-		}
-		if (!(options.isLogin ? user.registered : user.autoconfirmed)) {
-			if (options.forceBool) return false;
-			throw new Chat.ErrorMessage("You must be autoconfirmed to use offline messaging.");
-		}
-		if (!Users.globalAuth.atLeast(user, Config.usesqlitepms)) {
-			if (options.forceBool) return false;
-			throw new Chat.ErrorMessage("You do not have the needed rank to send offline PMs.");
-		}
-		return true;
-	}
-	checkCanPM(user: User, pmTarget: ID) {
-		this.checkCanUse(user);
-		if (Config.usesqlitepms === 'friends' && !user.friends?.has(pmTarget)) {
-			throw new Chat.ErrorMessage(
-				`At this time, you may only send offline messages to friends. You do not have ${pmTarget} friended.`
-			);
-		}
-	}
-	async sendReceived(user: User) {
-		const userid = toID(user);
-		// we only want to send the unseen pms to them when they login - they can replay the rest at will otherwise
-		const messages = await this.fetchUnseen(userid);
-		for (const {message, time, sender} of messages) {
-			user.send(
-				`|pm|${this.getIdentity(sender)}|${this.getIdentity(user)}|/html ` +
-				`${Utils.escapeHTML(message)} <i>[sent offline, <time>${new Date(time).toISOString()}</time>]</i>`
-			);
-		}
-	}
-	private getIdentity(user: User | string) {
-		user = Users.getExact(user) || user;
-		if (typeof user === 'object') {
-			return user.getIdentity();
-		}
-		return `${Users.globalAuth.get(toID(user))}${user}`;
-	}
-	nextClear(): NodeJS.Timer {
-		if (!PM.isParentProcess) return null!;
-		const time = Date.now();
-		// even though we expire once a week atm, we check once a day
-		const nextMidnight = new Date(time + 24 * 60 * 60 * 1000);
-		nextMidnight.setHours(0, 0, 1);
-		if (this.clearInterval) clearTimeout(this.clearInterval);
-		this.clearInterval = setTimeout(() => {
-			void this.clearOffline();
-			void this.clearSeen();
-			this.nextClear();
-		}, nextMidnight.getTime() - time);
-		return this.clearInterval;
-	}
-	clearSeen() {
-		return PM.run(statements.clearSeen, [Date.now(), SEEN_EXPIRY_TIME]);
-	}
-	send(message: string, user: User, pmTarget: User, onlyRecipient: User | null = null) {
-		const buf = `|pm|${user.getIdentity()}|${pmTarget.getIdentity()}|${message}`;
-		if (onlyRecipient) return onlyRecipient.send(buf);
-		user.send(buf);
-		if (pmTarget !== user) pmTarget.send(buf);
-		pmTarget.lastPM = user.id;
-		user.lastPM = pmTarget.id;
-	}
-	async fetchUnseen(user: User | string): Promise<ReceivedPM[]> {
-		const userid = toID(user);
-		return (await PM.transaction('listNew', [userid])) || [];
-	}
-	async fetchAll(user: User | string): Promise<ReceivedPM[]> {
-		return (await PM.all(statements.fetch, [toID(user)])) || [];
-	}
-	async renderReceived(user: User) {
-		const all = await this.fetchAll(user);
-		let buf = `<div class="ladder pad">`;
-		buf += `<h2>PMs received offline in the last ${Chat.toDurationString(SEEN_EXPIRY_TIME)}</h2>`;
-		const sortedPMs: {[userid: string]: ReceivedPM[]} = {};
-		for (const curPM of all) {
-			if (!sortedPMs[curPM.sender]) sortedPMs[curPM.sender] = [];
-			sortedPMs[curPM.sender].push(curPM);
-		}
-		for (const k in sortedPMs) {
-			Utils.sortBy(sortedPMs[k], pm => -pm.time);
-		}
-		buf += `<div class="mainmenuwrapper" style="margin-left:40px">`;
-		for (const pair of Utils.sortBy(Object.entries(sortedPMs), ([id]) => id)) {
-			const [sender, messages] = pair;
-			const group = Users.globalAuth.get(toID(sender));
-			const name = Users.getExact(sender)?.name || sender;
-			const id = toID(name);
-			buf += Utils.html`<div class="pm-window pm-window-${id}" width="30px" data-userid="${id}" data-name="${group}${name}" style="width:300px">`;
-			buf += Utils.html`<h3><small>${group}</small>${name}</h3>`;
-			buf += `<div class="pm-log"><div class="pm-buttonbar">`;
-			for (const {message, time} of messages) {
-				buf += `<div class="chat chatmessage-${toID(sender)}">&nbsp;&nbsp;`;
-				buf += `<small>[<time>${new Date(time).toISOString()}</time>] </small>`;
-				buf += Utils.html`<small>${group}</small>`;
-				buf += Utils.html`<span class="username" data-roomgroup="${group}" data-name="${name}"><username>${name}</username></span>: `;
-				buf += `<em>${message}</em></div>`;
-			}
-			buf += `</div></div></div>`;
-			buf += `<br />`;
-		}
-		buf += `</div>`;
-		return buf;
-	}
-	clearOffline() {
-		return PM.run(statements.clearDated, [Date.now(), EXPIRY_TIME]);
-	}
-	destroy() {
-		void PM.destroy();
-	}
+  database = PM;
+  clearInterval = this.scheduleNextClear();
+  offlineIsEnabled = Config.usesqlitepms && Config.usesqlite;
+
+  /**
+   * Sends an offline PM to the specified user.
+   * @param to - The recipient's username.
+   * @param from - The sender's user object or ID.
+   * @param message - The message content.
+   * @param context - Optional command context for handling offline message events.
+   */
+  async sendOffline(to: string, from: User | string, message: string, context?: Chat.CommandContext) {
+    await this.checkCanSend(to, from); // Validate the sender's permissions.
+    const result = await PM.transaction('send', [toID(from), toID(to), message]);
+    
+    if (result.error) throw new Chat.ErrorMessage(result.error);
+
+    if (typeof from === 'object') {
+      from.send(`|pm|${this.getIdentity(from)}|${this.getIdentity(to)}|${message} __[sent offline]__`);
+    }
+
+    if (result.changes && context) {
+      Chat.runHandlers('onMessageOffline', context, message, toID(to));
+    }
+
+    return !!result.changes;
+  }
+
+  /**
+   * Retrieves user PM settings from the database.
+   * @param userid - The user's ID.
+   */
+  getSettings(userid: string) {
+    return PM.get(statements.getSettings, [toID(userid)]);
+  }
+
+  /**
+   * Deletes the PM settings for the user from the database.
+   * @param userid - The user's ID.
+   */
+  deleteSettings(userid: string) {
+    return PM.run(statements.deleteSettings, [toID(userid)]);
+  }
+
+  /**
+   * Checks whether the user is allowed to send an offline PM.
+   * Throws an error if sending is not allowed.
+   * @param to - The recipient's username.
+   * @param from - The sender's username or user object.
+   */
+  async checkCanSend(to: string, from: User | string) {
+    from = toID(from);
+    to = toID(to);
+    const setting = await this.getSettings(to);
+    const requirement = setting?.view_only || Config.usesqlitepms || "friends";
+
+    switch (requirement) {
+      case 'friends':
+        if (!(await Chat.Friends.findFriendship(to, from))) {
+          if (Config.usesqlitepms === 'friends') {
+            throw new Chat.ErrorMessage(`You may only send offline PMs to friends. ${to} is not friends with you.`);
+          }
+          throw new Chat.ErrorMessage(`${to} is only accepting offline PMs from friends at this time.`);
+        }
+        break;
+      case 'trusted':
+        if (!Users.globalAuth.has(toID(from))) {
+          throw new Chat.ErrorMessage(`${to} is currently blocking offline PMs from non-trusted users.`);
+        }
+        break;
+      case 'none':
+        if (!Auth.atLeast(Users.globalAuth.get(from as ID), '%')) {
+          throw new Chat.ErrorMessage(`${to} has indicated that they do not wish to receive offline PMs.`);
+        }
+        break;
+      default:
+        if (!Auth.atLeast(Users.globalAuth.get(from as ID), requirement)) {
+          if (setting?.view_only) {
+            throw new Chat.ErrorMessage(`That user is not allowing offline PMs from your rank.`);
+          }
+          throw new Chat.ErrorMessage('You do not meet the rank requirement to send offline PMs.');
+        }
+        break;
+    }
+  }
+
+  /**
+   * Sets view-only mode for a user. This affects who can send PMs.
+   * @param user - The user object or ID.
+   * @param val - The new value for the view-only mode.
+   */
+  setViewOnly(user: User | string, val: string | null) {
+    const id = toID(user);
+    return val ? PM.run(statements.setBlock, [id, val]) : PM.run(statements.deleteSettings, [id]);
+  }
+
+  /**
+   * Checks whether a user is allowed to use the offline PM system.
+   * Throws an error if they cannot use it.
+   * @param user - The user object.
+   * @param options - Additional options for handling login and other conditions.
+   */
+  checkCanUse(user: User, options = { forceBool: false, isLogin: false }) {
+    if (!this.offlineIsEnabled) {
+      if (options.forceBool) return false;
+      throw new Chat.ErrorMessage(`Offline PMs are currently disabled.`);
+    }
+    if (!(options.isLogin ? user.registered : user.autoconfirmed)) {
+      if (options.forceBool) return false;
+      throw new Chat.ErrorMessage("You must be autoconfirmed to use offline messaging.");
+    }
+    if (!Users.globalAuth.atLeast(user, Config.usesqlitepms)) {
+      if (options.forceBool) return false;
+      throw new Chat.ErrorMessage("You do not have the required rank to send offline PMs.");
+    }
+    return true;
+  }
+
+  /**
+   * Sends all received PMs to the user when they log in.
+   * @param user - The user object.
+   */
+  async sendReceived(user: User) {
+    const userid = toID(user);
+    const messages = await this.fetchUnseen(userid);  // Fetch unseen messages only.
+    
+    messages.forEach(({ message, time, sender }) => {
+      user.send(
+        `|pm|${this.getIdentity(sender)}|${this.getIdentity(user)}|/html ` +
+        `${Utils.escapeHTML(message)} <i>[sent offline, <time>${new Date(time).toISOString()}</time>]</i>`
+      );
+    });
+  }
+
+  /**
+   * Fetches unseen private messages for a user.
+   * @param user - The user object or ID.
+   */
+  async fetchUnseen(user: User | string): Promise<ReceivedPM[]> {
+    const userid = toID(user);
+    return (await PM.transaction('listNew', [userid])) || [];
+  }
+
+  /**
+   * Fetches all private messages for a user.
+   * @param user - The user object or ID.
+   */
+  async fetchAll(user: User | string): Promise<ReceivedPM[]> {
+    return (await PM.all(statements.fetch, [toID(user)])) || [];
+  }
+
+  /**
+   * Renders all received PMs in HTML format.
+   * @param user - The user object.
+   */
+  async renderReceived(user: User) {
+    const all = await this.fetchAll(user);
+    let buf = `<div class="ladder pad">`;
+    buf += `<h2>PMs received offline in the last ${Chat.toDurationString(SEEN_EXPIRY_TIME)}</h2>`;
+
+    // Sort PMs by sender and time.
+    const sortedPMs: { [userid: string]: ReceivedPM[] } = all.reduce((acc, curPM) => {
+      if (!acc[curPM.sender]) acc[curPM.sender] = [];
+      acc[curPM.sender].push(curPM);
+      return acc;
+    }, {});
+
+    for (const sender in sortedPMs) {
+      sortedPMs[sender].sort((a, b) => b.time - a.time);
+    }
+
+    buf += `<div class="mainmenuwrapper" style="margin-left:40px">`;
+    Object.entries(sortedPMs).forEach(([sender, messages]) => {
+      const group = Users.globalAuth.get(toID(sender));
+      const name = Users.getExact(sender)?.name || sender;
+      const id = toID(name);
+
+      buf += Utils.html`
+        <div class="pm-window pm-window-${id}" data-userid="${id}" data-name="${group}${name}" style="width:300px">
+          <h3><small>${group}</small>${name}</h3>
+          <div class="pm-log"><div class="pm-buttonbar">
+      `;
+
+      messages.forEach(({ message, time }) => {
+        buf += Utils.html`
+          <div class="chat chatmessage-${toID(sender)}">&nbsp;&nbsp;
+            <small>[<time>${new Date(time).toISOString()}</time>] </small>
+            <span class="username" data-roomgroup="${group}" data-name="${name}">
+              <username>${name}</username>
+            </span>: <em>${message}</em>
+          </div>
+        `;
+      });
+
+      buf += `</div></div></div><br />`;
+    });
+
+    buf += `</div></div>`;
+    return buf;
+  }
+
+  /**
+   * Schedules the next automatic clearing of expired PMs.
+   * @returns {NodeJS.Timer} The timer for the next clearing.
+   */
+  scheduleNextClear(): NodeJS.Timer {
+    if (!PM.isParentProcess) return null!;
+    
+    const time = Date.now();
+    const nextMidnight = new Date(time + 24 * 60 * 60 * 1000);
+    nextMidnight.setHours(0, 0, 1);
+
+    if (this.clearInterval) clearTimeout(this.clearInterval);
+
+    this.clearInterval = setTimeout(() => {
+      void this.clearOffline();
+      void this.clearSeen();
+      this.scheduleNextClear();
+    }, nextMidnight.getTime() - time);
+
+    return this.clearInterval;
+  }
+
+  /**
+   * Clears all seen private messages that have expired.
+   */
+  clearSeen() {
+    return PM.run(statements.clearSeen, [Date.now(), SEEN_EXPIRY_TIME]);
+  }
+
+  /**
+   * Clears all expired offline private messages.
+   */
+  clearOffline() {
+    return PM.run(statements.clearDated, [Date.now(), EXPIRY_TIME]);
+  }
+
+  /**
+   * Destroys the PM handler, shutting down the database connection.
+   */
+  destroy() {
+    void PM.destroy();
+  }
 };
 
 if (Config.usesqlite) {
-	if (!process.send) {
-		PM.spawn(Config.pmprocesses || 1);
-		// clear super old pms on startup
-		void PM.run(statements.clearDated, [Date.now(), EXPIRY_TIME]);
-	} else if (process.send && process.mainModule === module) {
-		global.Monitor = {
-			crashlog(error: Error, source = 'A private message child process', details: AnyObject | null = null) {
-				const repr = JSON.stringify([error.name, error.message, source, details]);
-				process.send!(`THROW\n@!!@${repr}\n${error.stack}`);
-			},
-		};
-		process.on('uncaughtException', err => {
-			Monitor.crashlog(err, 'A private message database process');
-		});
-		process.on('unhandledRejection', err => {
-			Monitor.crashlog(err as Error, 'A private message database process');
-		});
-	}
+  if (!process.send) {
+    PM.spawn(Config.pmprocesses || 1);
+    void PM.run(statements.clearDated, [Date.now(), EXPIRY_TIME]);
+  } else if (process.send && process.mainModule === module) {
+    global.Monitor = {
+      crashlog(error: Error, source = 'A private message child process', details: AnyObject | null = null) {
+        const repr = JSON.stringify([error.name, error.message, source, details]);
+        process.send!(`THROW\n@!!@${repr}\n${error.stack}`);
+      },
+    };
+    process.on('uncaughtException', err => {
+      Monitor.crashlog(err, 'A private message database process');
+    });
+    process.on('unhandledRejection', err => {
+      Monitor.crashlog(err as Error, 'A private message database process');
+    });
+  }
 }
