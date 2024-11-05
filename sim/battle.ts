097223743a23669b63f0d6c9b1c737c69dc0f8cf
MathyFurret <4866817+MathyFurret@users.noreply.github.com>
2024-11-05 15:12:26 -0600

    Fix Acupressure target resolution

--- sim/battle.ts
+++ sim/battle.ts
@@ -2284,6 +2284,9 @@ export class Battle {
 					return target;
 				}
 				if (target.isAlly(pokemon)) {
+					if (move.target === 'adjacentAllyOrSelf' && this.gen !== 5) {
+						return pokemon;
+					}
 					// Target is a fainted ally: attack shouldn't retarget
 					return target;
 				}
