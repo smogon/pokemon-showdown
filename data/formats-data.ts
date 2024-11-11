302628b31a654df908632d55709deed829605814
shrianshChari <30420527+shrianshChari@users.noreply.github.com>
2024-11-11 10:20:17 -0500

    UU: Ban Okidogi
    https://www.smogon.com/forums/threads/sv-uu-suspect-process-round-11-poison-new-suspect-process.3754053/page-2#post-10336042

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -5573,7 +5573,7 @@ export const FormatsData: import('../sim/dex-species').SpeciesFormatsDataTable =
 		natDexTier: "RU",
 	},
 	okidogi: {
-		tier: "UU",
+		tier: "UUBL",
 		doublesTier: "(DUU)",
 		natDexTier: "RU",
 	},
