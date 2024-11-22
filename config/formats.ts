fd455e8510d1ecc448ad9206b78c0a946d2daac0
quziel <quziellll@gmail.com>
2024-11-21 19:49:38 -0600

    CAP: Ban Rage Fist (#10683)

--- config/formats.ts
+++ config/formats.ts
@@ -132,7 +132,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		desc: "The Create-A-Pok&eacute;mon project is a community dedicated to exploring and understanding the competitive Pok&eacute;mon metagame by designing, creating, and playtesting new Pok&eacute;mon concepts.",
 		mod: 'gen9',
 		ruleset: ['[Gen 9] OU', '+CAP'],
-		banlist: ['Crucibellite'],
+		banlist: ['Crucibellite', 'Rage Fist'],
 	},
 	{
 		name: "[Gen 9] BSS Reg G",
