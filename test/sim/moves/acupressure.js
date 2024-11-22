f56f3d98068bbf73d753f9661b68b81fc16a19d2
Alex "Mathy <4866817+MathyFurret@users.noreply.github.com>
2024-11-21 19:47:45 -0600

    Fix Acupressure target resolution (#10656)

--- test/sim/moves/acupressure.js
+++ test/sim/moves/acupressure.js
@@ -37,4 +37,35 @@ describe('Acupressure', function () {
 		assert.cantMove(() => battle.choose('p1', 'move acupressure 2'));
 		assert.cantMove(() => battle.choose('p1', 'move acupressure -2'));
 	});
+
+	// https://www.smogon.com/forums/threads/acupressure-targeting.3733779/post-9920405
+	it(`should redirect to the user if a targetted ally faints`, () => {
+		battle = common.createBattle({gameType: 'doubles'}, [[
+			{species: 'Pincurchin', moves: ['acupressure']},
+			{species: 'Flutter Mane', moves: ['memento']},
+		], [
+			{species: 'Furret', moves: ['sleeptalk']},
+			{species: 'Chien-Pao', moves: ['haze']},
+		]]);
+
+		battle.makeChoices('move acupressure -2, move memento 1', 'auto');
+		assert(Object.values(battle.p1.active[0].boosts).some(n => n === 2));
+		battle.makeChoices('move acupressure -2, pass', 'auto');
+		assert(Object.values(battle.p1.active[0].boosts).some(n => n === 2));
+	});
+
+	it(`in Gen 5, should not redirect to the uesr if a targetted ally faints`, () => {
+		battle = common.gen(5).createBattle({gameType: 'doubles'}, [[
+			{species: 'Shuckle', moves: ['acupressure']},
+			{species: 'Dugtrio', moves: ['memento']},
+		], [
+			{species: 'Marshtomp', moves: ['sleeptalk']},
+			{species: 'Nincada', moves: ['sleeptalk']},
+		]]);
+
+		battle.makeChoices('move acupressure -2, move memento 1', 'auto');
+		assert(Object.values(battle.p1.active[0].boosts).every(n => n === 0));
+		battle.makeChoices('move acupressure -2, pass', 'auto');
+		assert(Object.values(battle.p1.active[0].boosts).every(n => n === 0));
+	});
 });
