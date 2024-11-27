37482babdf4bcc39e04904721f490f4a17d682cd
Zakuree <zach.hartin@gmail.com>
2024-11-27 12:35:48 -0500

    Update abilities.ts

--- data/mods/gen9ssb/abilities.ts
+++ data/mods/gen9ssb/abilities.ts
@@ -1395,7 +1395,8 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
 		onModifyMove(move, pokemon) {
 			const target = this.effectState.target;
 			if (target === pokemon) return;
-			this.add('-message', pokemon.side.lastMove);
+			this.add('-message', pokemon.side.lastMoveUsed);
+			this.add('-message', pokemon.side.lastMoveUsed.id);
 			if (move.type === 'Flying' && pokemon.abilityState.lastMoveUsedResidual) {
 				target.side.addSideCondition('woventogethercohereforever');
 				pokemon.abilityState.imprintedMove = move.id;
