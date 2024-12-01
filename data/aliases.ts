c85c387b4b8671e045759749034a15bde757dc10
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2024-12-01 12:41:37 -0700

    Update OMotM aliases

--- data/aliases.ts
+++ data/aliases.ts
@@ -90,8 +90,8 @@ export const Aliases: import('../sim/dex').AliasesTable = {
 	gen6ag: "[Gen 6] Anything Goes",
 	crossevo: "[Gen 9] Cross Evolution",
 	mayhem: "[Gen 9] Random Battle Mayhem",
-	omotm: "[Gen 9] Passive Aggressive",
-	lcotm: "[Gen 9] Almost Any Ability UU",
+	omotm: "[Gen 9] Twisted Dimension",
+	lcotm: "[Gen 9] Mix and Mega Doubles",
 
 	// mega evos
 	fabio: "Ampharos-Mega",
