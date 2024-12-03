5facd909ab69a96c0c2b5f13dacdaffb8cffd4fa
Zakuree <zach.hartin@gmail.com>
2024-12-03 12:26:34 -0500

    Update abilities.ts

--- data/mods/gen9ssb/abilities.ts
+++ data/mods/gen9ssb/abilities.ts
@@ -18,6 +18,7 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
 	// Aevum
 	temporaldomain: {
 		name: "Temporal Domain",
+		shortDesc: "On switch-in, this Pokemon summons Temporal Terrain.",
 		gen: 9,
 		flags: {},
 		onStart(pokemon) {
