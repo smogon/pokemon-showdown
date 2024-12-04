40f8d37caaf0de0534fe50fdcbea471451b7f569
TwiSteorra <160420881+TwiSteorra@users.noreply.github.com>
2024-12-04 15:34:30 -0400

    Descriptions: Update 2-5 hit moves with Loaded Dice interaction (#10727)

--- data/text/items.ts
+++ data/text/items.ts
@@ -987,6 +987,7 @@ export const ItemsText: {[id: IDEntry]: ItemText} = {
 	},
 	loadeddice: {
 		name: "Loaded Dice",
+		desc: "The holder's moves that normally hit 2 to 5 times instead hit 4 or 5 times. If the first hit is successful, the holder's use of Triple Kick or Triple Axel hits 3 times, and Population Bomb hits 4 to 10 times, at random.",
 		shortDesc: "Holder's moves that hit 2-5 times hit 4-5 times; Population Bomb hits 4-10 times.",
 	},
 	lopunnite: {
