cac933fdc4080a69a6037a99e1dba5321a90b008
Oldenmw <87488710+Oldenmw@users.noreply.github.com>
2024-12-20 17:54:04 -0500

    Add Gen 4 Draft (#10765)
    * added gen 4 draft ruleset
    
    * fixed typo in acupressure
    
    * Update config/formats.ts
    
    ---------
    
    Co-authored-by: Kris Johnson <11083252+KrisXV@users.noreply.github.com>

--- config/formats.ts
+++ config/formats.ts
@@ -476,6 +476,13 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		banlist: ['Soul Dew'],
 	},
 	{
+		name: "[Gen 4] Draft",
+		mod: 'gen4',
+		searchShow: false,
+		ruleset: ['Standard Draft', 'Swagger Clause', 'DryPass Clause', '!Team Preview', '!Evasion Abilities Clause'],
+		banlist: ['King\'s Rock', 'Quick Claw', 'Assist', 'Acupressure', 'Sand Stream ++ Sand Veil', 'Snow Warning ++ Snow Cloak'],
+	},
+	{
 		name: "[Gen 3] Draft",
 		mod: 'gen3',
 		searchShow: false,
