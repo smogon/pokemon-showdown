6cf673da520ca329ba6e395b136679890d32c2fd
Ngammer <cnicolas810@gmail.com>
2024-12-31 15:07:55 -0300

    moves
    rework rollout

--- data/moves.ts
+++ data/moves.ts
@@ -15810,67 +15810,30 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 	},
 	rollout: {
 		num: 205,
-		accuracy: 90,
-		basePower: 30,
+		accuracy: 100,
+		basePower: 120,
 		basePowerCallback(pokemon, target, move) {
 			let bp = move.basePower;
-			const rolloutData = pokemon.volatiles['rollout'];
-			if (rolloutData?.hitCount) {
-				bp *= Math.pow(2, rolloutData.contactHitCount);
-			}
-			if (rolloutData && pokemon.status !== 'slp') {
-				rolloutData.hitCount++;
-				rolloutData.contactHitCount++;
-				if (rolloutData.hitCount < 5) {
-					rolloutData.duration = 2;
-				}
-			}
 			if (pokemon.volatiles['defensecurl']) {
-				bp *= 2;
+				bp += 30;
 			}
-			this.debug("BP: " + bp);
 			return bp;
 		},
 		category: "Physical",
 		name: "Rollout",
 		pp: 20,
 		priority: 0,
-		flags: {contact: 1, protect: 1, mirror: 1, metronome: 1, failinstruct: 1, noparentalbond: 1},
-		onModifyMove(move, pokemon, target) {
-			if (pokemon.volatiles['rollout'] || pokemon.status === 'slp' || !target) return;
-			pokemon.addVolatile('rollout');
-			if (move.sourceEffect) pokemon.lastMoveTargetLoc = pokemon.getLocOf(target);
+		flags: {contact: 1, protect: 1, mirror: 1, metronome: 1, failinstruct: 1},
+		self: {
+			volatileStatus: 'lockedmove',
 		},
-		onAfterMove(source, target, move) {
-			const rolloutData = source.volatiles["rollout"];
-			if (
-				rolloutData &&
-				rolloutData.hitCount === 5 &&
-				rolloutData.contactHitCount < 5
-				// this conditions can only be met in gen7 and gen8dlc1
-				// see `disguise` and `iceface` abilities in the resp mod folders
-			) {
-				source.addVolatile("rolloutstorage");
-				source.volatiles["rolloutstorage"].contactHitCount =
-					rolloutData.contactHitCount;
+		onAfterMove(pokemon) {
+			if (pokemon.volatiles['lockedmove'] && pokemon.volatiles['lockedmove'].duration === 1) {
+				pokemon.removeVolatile('lockedmove');
 			}
 		},
-		condition: {
-			duration: 1,
-			onLockMove: 'rollout',
-			onStart() {
-				this.effectState.hitCount = 0;
-				this.effectState.contactHitCount = 0;
-			},
-			onResidual(target) {
-				if (target.lastMove && target.lastMove.id === 'struggle') {
-					// don't lock
-					delete target.volatiles['rollout'];
-				}
-			},
-		},
 		secondary: null,
-		target: "normal",
+		target: "randomNormal",
 		type: "Rock",
 		contestType: "Cute",
 	},
