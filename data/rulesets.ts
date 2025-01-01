6d8938af211029898e83a62fff3dfcb4e3de9e14
Ngammer <cnicolas810@gmail.com>
2025-01-01 13:57:58 -0300

    formatos
    inicio

--- data/rulesets.ts
+++ data/rulesets.ts
@@ -7,7 +7,62 @@ export const Rulesets: import('../sim/dex-formats').FormatDataTable = {
 
 	// Rulesets
 	///////////////////////////////////////////////////////////////////
-
+	standardnuevometa: {
+		effectType: 'ValidatorRule',
+		name: 'Standard NM',
+		desc: "The standard ruleset for all Nuevo Meta tiers",
+		ruleset: [
+			'Obtainable', '+Unobtainable', '+Past', 'Sketch Post-Gen 7 Moves', 'Team Preview', 'Nickname Clause', 'HP Percentage Mod', 'Cancel Mod', 'Endless Battle Clause',
+		],
+		onValidateSet(set) {
+			const species = this.dex.species.get(set.species);
+			if (species.natDexTier === 'Illegal') {
+				if (this.ruleTable.has(`+pokemon:${species.id}`)) return;
+				return [`${set.name || set.species} does not exist in the National Dex.`];
+			}
+			const requireObtainable = this.ruleTable.has('obtainable');
+			if (requireObtainable) {
+				if (species.natDexTier === "Unreleased") {
+					const basePokemon = this.toID(species.baseSpecies);
+					if (this.ruleTable.has(`+pokemon:${species.id}`) || this.ruleTable.has(`+basepokemon:${basePokemon}`)) {
+						return;
+					}
+					return [`${set.name || set.species} does not exist in the National Dex.`];
+				}
+				for (const moveid of set.moves) {
+					const move = this.dex.moves.get(moveid);
+					if (move.isNonstandard === 'Unobtainable' && move.gen === this.dex.gen || move.id === 'lightofruin') {
+						if (this.ruleTable.has(`+move:${move.id}`)) continue;
+						const problem = `${set.name}'s move ${move.name} does not exist in the National Dex.`;
+						if (this.ruleTable.has('omunobtainablemoves')) {
+							const {outOfBattleSpecies} = this.getValidationSpecies(set);
+							if (!this.omCheckCanLearn(move, outOfBattleSpecies, this.allSources(outOfBattleSpecies), set, problem)) continue;
+						}
+						return [problem];
+					}
+				}
+			}
+			// Any item that was legal in Gen 7 (Normal Gem for example) should be usable
+			if (!set.item) return;
+			let item = this.dex.items.get(set.item);
+			let gen = this.dex.gen;
+			while (item.isNonstandard && gen >= 7) {
+				item = this.dex.forGen(gen).items.get(item.id);
+				gen--;
+			}
+			if (requireObtainable && item.isNonstandard) {
+				if (this.ruleTable.has(`+item:${item.id}`)) return;
+				return [`${set.name}'s item ${item.name} does not exist in Gen ${this.dex.gen}.`];
+			}
+		},
+		onBegin() {
+			for (const pokemon of this.getAllPokemon()) {
+				if (pokemon.species.isMega || pokemon.species.isPrimal || pokemon.species.forme === "Ultra") {
+					pokemon.canTerastallize = null;
+				}
+			}
+		},
+	},
 	standard: {
 		effectType: 'ValidatorRule',
 		name: 'Standard',
@@ -246,6 +301,20 @@ export const Rulesets: import('../sim/dex-formats').FormatDataTable = {
 					if (hasMove[moveid]) return [`${species.baseSpecies} has multiple copies of ${move.name}.`];
 					hasMove[moveid] = true;
 				}
+			}
+		},
+	},
+	kantopokedex: {
+		effectType: 'ValidatorRule',
+		name: 'Kanto+ Pokedex',
+		desc: "Only allows Pok&eacute;mon native to the Hoenn region (OR/AS)",
+		onValidateSet(set, format) {
+			const hoennDex = [
+				"Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle", "Blastoise", "Caterpie", "Metapod", "Butterfree", "Weedle", "Kakuna", "Beedrill", "Pidgey", "Pidgeotto", "Pidgeot", "Rattata", "Raticate", "Raticate-Alola", "Spearow", "Fearow", "Fearill", "Ekans", "Arbok", "P"
+			];
+			const species = this.dex.species.get(set.species || set.name);
+			if (!hoennDex.includes(species.baseSpecies) && !this.ruleTable.has('+' + species.id)) {
+				return [species.baseSpecies + " is not in the Hoenn Pokédex."];
 			}
 		},
 	},
