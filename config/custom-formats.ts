a0406d198b00bbaffc99a018eb49daa3b7e4dea7
Ngammer <cnicolas810@gmail.com>
2025-01-01 18:38:35 -0300

    formats

---
+++ config/custom-formats.ts
@@ -0,0 +1,29 @@
+// Note: This is the list of formats
+// The rules that formats use are stored in data/rulesets.ts
+
+export const Formats: import('../sim/dex-formats').FormatList = [
+
+	{
+		section: "Nuevo Meta Singles",
+	},
+	{
+		name: "[Gen 9] Nuevo Meta",
+		mod: 'gen9',
+		ruleset: ['Standard NatDex', 'Species Clause', 'Sleep Clause Mod', 'Terastal Clause'],
+		banlist: ['Baton Pass'],
+	},
+	{
+		name: "[Gen 9] Nuevo Meta Random",
+		desc: `Randomized teams of Pok&eacute;mon with sets that are generated to be competitively viable.`,
+		mod: 'gen9',
+		team: 'random',
+		ruleset: ['Standard NatDex', 'PotD', 'Species Clause', 'Sleep Clause Mod', 'Illusion Level Mod'],
+	},
+
+];
+
+
+// Nuevo Meta Singles
+	///////////////////////////////////////////////////////////////////
+
+
