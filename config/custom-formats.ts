57e0c0fe828bae9672bf037ffa6559a28e627dc5
Ngammer <cnicolas810@gmail.com>
2025-01-01 18:40:20 -0300

    formats

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -17,7 +17,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		desc: `Randomized teams of Pok&eacute;mon with sets that are generated to be competitively viable.`,
 		mod: 'gen9',
 		team: 'random',
-		ruleset: ['Standard NatDex', 'PotD', 'Species Clause', 'Sleep Clause Mod', 'Illusion Level Mod'],
+		ruleset: ['Standard NatDex', 'PotD', 'Species Clause', 'Sleep Clause Mod', 'Illusion Level Mod', 'Kanto Pokedex'],
 	},
 
 ];
