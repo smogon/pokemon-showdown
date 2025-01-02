517c48858439bf9bce61838c4a26aa2943c29a83
Marty-D <Marty-D@users.noreply.github.com>
2025-01-02 17:54:05 -0500

    Add new avatars

--- server/chat-commands/avatars.tsx
+++ server/chat-commands/avatars.tsx
@@ -640,7 +640,7 @@ const OFFICIAL_AVATARS_KYLEDOVE = new Set([
 	'mrstone', 'nancy', 'nate-pokestar3', 'neroli', 'peony-league', 'phil', 'player-go', 'playerf-go', 'rhi', 'rita', 'river',
 	'rosa-pokestar3', 'sabrina-frlg', 'selene-masters', 'sierra', 'spark-casual', 'spark', 'spenser', 'toddsnap', 'toddsnap2',
 	'victor-masters', 'vince', 'wally-rse', 'willow-casual', 'willow', 'yancy', 'zinnia-masters',
-	'acerola-masters3', 'bianca-masters', 'cheren-masters', 'gardenia-masters',
+	'acerola-masters3', 'bianca-masters', 'cheren-masters', 'gardenia-masters', 'nemona-masters',
 ]);
 
 const OFFICIAL_AVATARS_HYOOPPA = new Set([
@@ -656,7 +656,7 @@ const OFFICIAL_AVATARS_FIFTY = new Set([
 ]);
 
 const OFFICIAL_AVATARS_HORO = new Set([
-	'florian-bb', 'juliana-bb', 'red-lgpe',
+	'florian-bb', 'juliana-bb', 'red-lgpe', 'liko', 'roy',
 ]);
 
 const OFFICIAL_AVATARS_SELENA = new Set([
