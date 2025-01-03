aab23cf973a785c630ef5bfef79fbe95edc774d2
Ngammer <cnicolas810@gmail.com>
2025-01-03 18:08:12 -0300

    Update moves.ts
    rework comeuppance
    rework jaw lock

--- data/moves.ts
+++ data/moves.ts
@@ -10074,6 +10074,7 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 		pp: 10,
 		priority: 0,
 		flags: {contact: 1, protect: 1, mirror: 1, metronome: 1, bite: 1},
+		
 		onHit(target, source, move) {
 			source.addVolatile('trapped', target, move, 'trapper');
 			target.addVolatile('trapped', source, move, 'trapper');
