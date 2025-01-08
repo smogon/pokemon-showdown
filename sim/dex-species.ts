50335a96561595bb3f918dfa0202d02d371e0c63
Ngammer <cnicolas810@gmail.com>
2025-01-08 00:13:49 -0300

    Update dex-species.ts

--- sim/dex-species.ts
+++ sim/dex-species.ts
@@ -333,9 +333,9 @@ export class Species extends BasicEffect implements Readonly<BasicEffect & Speci
 		if (!this.gen && this.num >= 1) {
 			if (this.num >= 906 || this.forme.includes('Paldea')) {
 				this.gen = 9;
-			} else if (this.num >= 810 || ['Gmax', 'Galar', 'Galar-Zen', 'Hisui'].includes(this.forme)) {
+			} else if (this.num >= 810 /*|| ['Gmax', 'Galar', 'Galar-Zen', 'Hisui'].includes(this.forme)*/) {
 				this.gen = 8;
-			} else if (this.num >= 722 || this.forme.startsWith('Alola') || this.forme === 'Starter') {
+			} else if (this.num >= 722 /*|| this.forme.startsWith('Alola')*/ || this.forme === 'Starter') {
 				this.gen = 7;
 			} else if (this.forme === 'Primal') {
 				this.gen = 6;
