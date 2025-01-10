2e5036732be411e60d8c0f91129229e5545d07fc
André Bastos Dias <80102738+andrebastosdias@users.noreply.github.com>
2025-01-10 05:55:30 +0000

    Fix Terastallization base power buff for priority moves called by Encore (#10808)
    * Fix: Tera priority bump and Encore
    
    * Line
    
    * Fix check for multihit

--- sim/battle-actions.ts
+++ sim/battle-actions.ts
@@ -1667,10 +1667,12 @@ export class BattleActions {
 			basePower = 0;
 		}
 
+		const dexMove = this.dex.moves.get(move.id);
 		if (
-			basePower < 60 && source.getTypes(true).includes(move.type) && source.terastallized && move.priority <= 0 &&
+			basePower < 60 && source.getTypes(true).includes(move.type) && source.terastallized &&
+			dexMove.priority <= 0 && !dexMove.multihit &&
 			// Hard move.basePower check for moves like Dragon Energy that have variable BP
-			!move.multihit && !((move.basePower === 0 || move.basePower === 150) && move.basePowerCallback)
+			!((move.basePower === 0 || move.basePower === 150) && move.basePowerCallback)
 		) {
 			basePower = 60;
 		}
