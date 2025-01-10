eba4456315e76345c0b80dd1acd4a0a5e53ebffc
Zakuree <zach.hartin@gmail.com>
2025-01-10 16:56:53 -0500

    Gift of Fortune no longer gives Charcoal

--- data/mods/gen9ssb/moves.ts
+++ data/mods/gen9ssb/moves.ts
@@ -129,10 +129,6 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 				source.abilityState.sack = [];
 				this.add('-message', `${source.name} emptied its Gift Sack!`);
 			}
-			const newItem = this.dex.items.get('charcoal')
-			target.item = newItem.id;
-			target.setItem(newItem);
-			this.add('-item', target, newItem, '[from] move: Gift of Fortune');
 		},
 		critRatio: 2,
 		type: "Water",
