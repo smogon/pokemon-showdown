1ed2515c5a912cbc2a674f85f889c036db170f5c
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2025-01-18 11:27:40 -0500

    GSC Doubles: Unban Celebi (#10831)

--- data/mods/gen2doubles/formats-data.ts
+++ data/mods/gen2doubles/formats-data.ts
@@ -892,7 +892,7 @@ export const FormatsData: import('../../../sim/dex-species').ModdedSpeciesFormat
 		doublesTier: "DUber",
 	},
 	celebi: {
-		tier: "Uber",
-		doublesTier: "DUber",
+		tier: "OU",
+		doublesTier: "DOU",
 	},
 };
