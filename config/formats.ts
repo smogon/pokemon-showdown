9311e1e6ad170a518da72e68fcc7391ee388c2b9
Oldenmw <87488710+Oldenmw@users.noreply.github.com>
2025-01-24 16:39:19 -0500

    Add 'Sleep Move Clause' to Gen 4 Draft (#10839)

--- config/formats.ts
+++ config/formats.ts
@@ -474,7 +474,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		name: "[Gen 4] Draft",
 		mod: 'gen4',
 		searchShow: false,
-		ruleset: ['Standard Draft', 'Swagger Clause', 'DryPass Clause', '!Team Preview', '!Evasion Abilities Clause'],
+		ruleset: ['Standard Draft', 'Swagger Clause', 'DryPass Clause', 'Sleep Moves Clause', '!Team Preview', '!Evasion Abilities Clause'],
 		banlist: ['King\'s Rock', 'Quick Claw', 'Assist', 'Acupressure', 'Sand Stream ++ Sand Veil', 'Snow Warning ++ Snow Cloak'],
 	},
 	{
