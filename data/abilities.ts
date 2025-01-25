f0846e48475719f0c3c21780e44c8e67975454d6
André Bastos Dias <80102738+andrebastosdias@users.noreply.github.com>
2025-01-25 18:39:18 +0000

    Tera Shell doesn't activate if the move is already resisted (#10815)
    * Don't activate Tera Shell if resisted
    
    * Fix for Hackmons
    
    Relevant if Terapagos-Terastal transforms and terastallizes, keeping Tera Shell
    
    * Update comment

--- data/abilities.ts
+++ data/abilities.ts
@@ -4852,17 +4852,7 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 		num: 309,
 	},
 	terashell: {
-		onEffectiveness(typeMod, target, type, move) {
-			if (!target || target.species.name !== 'Terapagos-Terastal') return;
-			if (this.effectState.resisted) return -1; // all hits of multi-hit move should be not very effective
-			if (move.category === 'Status' || move.id === 'struggle') return;
-			if (!target.runImmunity(move.type)) return; // immunity has priority
-			if (target.hp < target.maxhp) return;
-
-			this.add('-activate', target, 'ability: Tera Shell');
-			this.effectState.resisted = true;
-			return -1;
-		},
+		// effectiveness implemented in sim/pokemon.ts:Pokemon#runEffectiveness
 		onAnyAfterMove() {
 			this.effectState.resisted = false;
 		},
