d17d0002468a961ba479bee1f548f8a12a2b6331
André Bastos Dias <andrebastosdias@gmail.com>
2025-02-08 14:46:55 +0000

    Fix Magic Coat

--- data/moves.ts
+++ data/moves.ts
@@ -11081,7 +11081,7 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 			},
 			onTryHitPriority: 2,
 			onTryHit(target, source, move) {
-				if (target === source || move.hasBounced || !move.flags['reflectable']) {
+				if (target === source || move.hasBounced || !move.flags['reflectable'] || target.isSemiInvulnerable()) {
 					return;
 				}
 				const newMove = this.dex.getActiveMove(move.id);
@@ -11091,7 +11091,7 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 				return null;
 			},
 			onAllyTryHitSide(target, source, move) {
-				if (target.isAlly(source) || move.hasBounced || !move.flags['reflectable']) {
+				if (target.isAlly(source) || move.hasBounced || !move.flags['reflectable'] || target.isSemiInvulnerable()) {
 					return;
 				}
 				const newMove = this.dex.getActiveMove(move.id);
