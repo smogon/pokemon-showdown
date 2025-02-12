94372f46a4994bc3fd9efefa3373cf08ffab9a00
André Bastos Dias <80102738+andrebastosdias@users.noreply.github.com>
2025-02-12 17:53:18 +0000

    Remove wrong test

--- test/sim/misc/terapagos.js
+++ test/sim/misc/terapagos.js
@@ -10,20 +10,6 @@ describe(`Terapagos`, function () {
 		battle.destroy();
 	});
 
-	it.skip(`should accept the Terastallization choice, but not Terastallize while Transformed into Terapagos-Terastal`, function () {
-		battle = common.createBattle([[
-			{species: 'ditto', ability: 'imposter', moves: ['sleeptalk']},
-		], [
-			{species: 'terapagos', ability: 'terashift', moves: ['sleeptalk'], teraType: 'Stellar'},
-		]]);
-
-		// Currently throws a choice error
-		battle.makeChoices('move sleeptalk terastallize', 'auto');
-
-		const ditto = battle.p1.active[0];
-		assert.false(!!ditto.terastallized);
-	});
-
 	it(`[Hackmons] should not cause Terapagos-Terastal to become Terapagos-Stellar if the user is Transformed`, function () {
 		battle = common.createBattle([[
 			{species: 'terapagos', ability: 'terashift', moves: ['transform'], teraType: 'Stellar'},
