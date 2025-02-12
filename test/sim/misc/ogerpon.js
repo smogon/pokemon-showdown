87c5e85dd08f7d6ed920730d3421d153c4815688
André Bastos Dias <andrebastosdias@gmail.com>
2025-02-12 22:07:55 +0000

    Update tests

--- test/sim/misc/ogerpon.js
+++ test/sim/misc/ogerpon.js
@@ -5,7 +5,41 @@ const common = require('./../../common');
 
 let battle;
 
+describe(`Ogerpon`, function () {
+	afterEach(function () {
+		battle.destroy();
+	});
+
+	it(`should reject the Terastallization choice while Transformed into Ogerpon`, function () {
+		battle = common.createBattle([[
+			{species: 'ditto', ability: 'imposter', moves: ['sleeptalk']},
+		], [
+			{species: 'ogerpon', ability: 'defiant', moves: ['sleeptalk'], teraType: 'Grass'},
+		]]);
+
+		assert.throws(() => battle.makeChoices('move sleeptalk terastallize', 'auto'));
+	});
+
+	// this test passes, but there isn't a way to create a battle for a mod without a format
+	it.skip(`[DLC1] should accept the Terastallization choice, but not Terastallize while Transformed into Ogerpon`, function () {
+		battle = battle = common.createBattle({formatid: 'gen9dlc1@@@!teampreview'}, [[
+			{species: 'ditto', ability: 'imposter', moves: ['sleeptalk']},
+		], [
+			{species: 'ogerpon', ability: 'defiant', moves: ['sleeptalk'], teraType: 'Grass'},
+		]]);
+
+		battle.makeChoices('move sleeptalk terastallize', 'auto');
+
+		const ditto = battle.p1.active[0];
+		assert.false(!!ditto.terastallized);
+	});
+});
+
 describe(`[Hackmons] Ogerpon`, function () {
+	afterEach(function () {
+		battle.destroy();
+	});
+
 	// https://www.smogon.com/forums/threads/scarlet-violet-battle-mechanics-research.3709545/post-9838633
 	it(`should keep permanent abilites after Terastallizing until it switches out`, function () {
 		battle = common.gen(9).createBattle([[
