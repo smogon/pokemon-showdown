38af33695e866fec1dd26b66d03bf395a86db431
André Bastos Dias <andrebastosdias@gmail.com>
2025-02-13 03:52:21 +0000

    Fix Symbiosis's interaction with Eject Pack

--- test/sim/abilities/symbiosis.js
+++ test/sim/abilities/symbiosis.js
@@ -63,7 +63,7 @@ describe('Symbiosis', function () {
 		assert.equal(battle.p1.active[1].item, 'leftovers');
 	});
 
-	it.skip(`should not trigger on an ally using their Eject Pack`, function () {
+	it(`should not trigger on an ally using their Eject Pack`, function () {
 		battle = common.createBattle({gameType: 'doubles'}, [[
 			{species: 'oranguru', ability: 'symbiosis', item: 'leftovers', moves: ['sleeptalk']},
 			{species: 'wynaut', item: 'ejectpack', moves: ['superpower']},
