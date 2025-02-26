eba5bb23d0ec059eef17cd60ef8bbb00cf055758
André Bastos Dias <andrebastosdias@gmail.com>
2025-02-26 20:56:02 +0000

    Update to ESLint 9

--- test/sim/moves/icespinner.js
+++ test/sim/moves/icespinner.js
@@ -21,11 +21,11 @@ describe(`Ice Spinner`, () => {
 		assert.false(battle.field.isTerrain('psychicterrain'));
 	});
 
-	it(`should remove Terrains if target has a substitute`, function () {
+	it(`should remove Terrains if target has a substitute`, () => {
 		battle = common.createBattle([[
-			{species: 'wynaut', moves: ['sleeptalk', 'icespinner']},
+			{ species: 'wynaut', moves: ['sleeptalk', 'icespinner'] },
 		], [
-			{species: 'registeel', ability: 'psychicsurge', moves: ['substitute', 'sleeptalk']},
+			{ species: 'registeel', ability: 'psychicsurge', moves: ['substitute', 'sleeptalk'] },
 		]]);
 
 		battle.makeChoices('move sleeptalk', 'move substitute');
@@ -33,7 +33,7 @@ describe(`Ice Spinner`, () => {
 		assert.false(battle.field.isTerrain('psychicterrain'));
 	});
 
-	it(`should not remove Terrains if the user faints from Life Orb`, function () {
+	it(`should not remove Terrains if the user faints from Life Orb`, () => {
 		battle = common.createBattle([[
 			{ species: 'shedinja', item: 'lifeorb', moves: ['icespinner'] },
 			{ species: 'wynaut', moves: ['sleeptalk'] },
@@ -57,7 +57,7 @@ describe(`Ice Spinner`, () => {
 		assert(battle.field.isTerrain('psychicterrain'));
 	});
 
-	it(`should not remove Terrains if the user is forced out via Red Card`, function () {
+	it(`should not remove Terrains if the user is forced out via Red Card`, () => {
 		battle = common.createBattle([[
 			{ species: 'shedinja', moves: ['icespinner'] },
 			{ species: 'wynaut', moves: ['sleeptalk'] },
