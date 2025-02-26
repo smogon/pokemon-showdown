bfc79b4043e29c8637744635e089b6d5178a625b
André Bastos Dias <80102738+andrebastosdias@users.noreply.github.com>
2025-02-26 23:29:01 +0000

    Don't send upkeep if battle ended (#10927)

--- sim/battle.ts
+++ sim/battle.ts
@@ -2756,7 +2756,7 @@ export class Battle {
 			this.updateSpeed();
 			residualPokemon = this.getAllActive().map(pokemon => [pokemon, pokemon.getUndynamaxedHP()] as const);
 			this.fieldEvent('Residual');
-			this.add('upkeep');
+			if (!this.ended) this.add('upkeep');
 			break;
 		}
 
