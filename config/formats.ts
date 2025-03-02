ebdaa96175a21d609965731bef6f93bb5cf23647
Guangcong Luo <guangcongluo@gmail.com>
2025-03-02 14:47:30 -0800

    Make "All Pokemon" rules more convenient (#10932)
    * Make "All Pokemon" rules more convenient
    
    Previously, "+All Pokemon" did nothing except override "-All Pokemon",
    which switched from a default-allow to default-deny system.
    
    They still do that, but they now also override all previous pokemon
    bans/unbans. This makes it easier to replace a banlist/whitelist
    from an inherited ruleset without needing to reverse every previous
    ban/unban.
    
    This also adds an error if you use `+All Pokemon` in a ruleset where
    it doesn't do anything.
    
    Fixes #10772

--- config/formats.ts
+++ config/formats.ts
@@ -2714,7 +2714,6 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		team: 'randomHC',
 		ruleset: ['HP Percentage Mod', 'Cancel Mod'],
 		banlist: ['CAP', 'LGPE', 'MissingNo.', 'Pikachu-Cosplay', 'Pichu-Spiky-eared', 'Pokestar Smeargle', 'Pokestar UFO', 'Pokestar UFO-2', 'Pokestar Brycen-Man', 'Pokestar MT', 'Pokestar MT2', 'Pokestar Transport', 'Pokestar Giant', 'Pokestar Humanoid', 'Pokestar Monster', 'Pokestar F-00', 'Pokestar F-002', 'Pokestar Spirit', 'Pokestar Black Door', 'Pokestar White Door', 'Pokestar Black Belt', 'Pokestar UFO-PropU2', 'Xerneas-Neutral'],
-		unbanlist: ['All Pokemon'],
 	},
 	{
 		name: "[Gen 9] Doubles Hackmons Cup",
