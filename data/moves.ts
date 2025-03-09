eddef0814848a8d245d0d36746b36364e8f6760a
Ngammer <cnicolas810@gmail.com>
2025-03-09 16:10:07 -0300

    bloqueooculto

--- data/moves.ts
+++ data/moves.ts
@@ -22505,4 +22505,62 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 			zMove: {effect: 'clearnegativeboost'},
 			contestType: "Beautiful",
 		},
+		bloqueooculto: {
+			num: 0,
+			accuracy: true,
+			basePower: 0,
+			category: "Status",
+			name: "Bloqueo Oculto",
+			pp: 15,
+			priority: 4,
+			flags: {noassist: 1, failcopycat: 1},
+			stallingMove: true,
+			volatileStatus: 'bloqueooculto',
+			onPrepareHit(pokemon) {
+				return !!this.queue.willAct() && this.runEvent('StallMove', pokemon);
+			},
+			onHit(pokemon) {
+				pokemon.addVolatile('stall');
+			},
+			condition: {
+				duration: 1,
+				onStart(target) {
+					this.add('-singleturn', target, 'move: Protect');
+				},
+				onTryHitPriority: 3,
+				onTryHit(target, source, move) {
+					if (!move.flags['protect']) {
+						if (['gmaxoneblow', 'gmaxrapidflow'].includes(move.id)) return;
+						if (move.isZ || move.isMax) target.getMoveHitData(move).zBrokeProtect = true;
+						return;
+					}
+					if (move.smartTarget) {
+						move.smartTarget = false;
+					} else {
+						this.add('-activate', target, 'move: Protect');
+					}
+					const lockedmove = source.getVolatile('lockedmove');
+					if (lockedmove) {
+						// Outrage counter is reset
+						if (source.volatiles['lockedmove'].duration === 2) {
+							delete source.volatiles['lockedmove'];
+						}
+					}
+					if (this.checkMoveMakesContact(move, source, target)) {
+						this.damage(source.baseMaxhp / 8, source, target);
+					}
+					return this.NOT_FAIL;
+				},
+				onHit(target, source, move) {
+					if (move.isZOrMaxPowered && this.checkMoveMakesContact(move, source, target)) {
+						source.addVolatile('gastroacid', target);
+					}
+				},
+			},
+			secondary: null,
+			target: "self",
+			type: "Psychic",
+			zMove: {boost: {def: 1}},
+			contestType: "Tough",
+		},
 };
