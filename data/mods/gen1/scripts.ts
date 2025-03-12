9a96d8c300b20a593a2418d94a5f3edafb6e265d
Guangcong Luo <guangcongluo@gmail.com>
2025-03-12 16:41:27 -0700

    Refactor
    PP is now deducted after the move rather than before, which is more
    accurate and also simplifies the code.

--- data/mods/gen1/scripts.ts
+++ data/mods/gen1/scripts.ts
@@ -151,33 +151,32 @@ export const Scripts: ModdedBattleScriptsData = {
 				this.battle.runEvent('AfterMoveSelf', pokemon, target, move);
 				return;
 			}
-			if (move.beforeMoveCallback) {
-				if (move.beforeMoveCallback.call(this.battle, pokemon, target, move)) {
-					this.battle.clearActiveMove(true);
-					return;
-				}
+			if (move.beforeMoveCallback?.call(this.battle, pokemon, target, move)) {
+				this.battle.clearActiveMove(true);
+				return;
 			}
-			let lockedMove = this.battle.runEvent('LockMove', pokemon);
-			if (lockedMove === true) lockedMove = false;
-			if (!lockedMove) {
-				pokemon.deductPP(move, null, target);
-			} else {
+
+			let ppMove: ID = pokemon.volatiles['twoturnmove']?.ppMove || '';
+			if (pokemon.getLockedMove()) {
+				// locked moves don't deduct PP
 				sourceEffect = move;
-				if (pokemon.volatiles['twoturnmove']) {
-					// Two-turn moves like Sky Attack deduct PP on their second turn.
-					pokemon.deductPP(pokemon.volatiles['twoturnmove'].originalMove, null, target);
-				}
+			} else {
+				ppMove ||= move.id;
 			}
-			const moveSlot = pokemon.moveSlots.find(ms => ms.id === move.id);
-			if (moveSlot && moveSlot.pp < 0) {
-				moveSlot.pp += 64;
-				this.battle.hint("In Gen 1, if a player is forced to use a move with 0 PP, the move will underflow to have 63 PP.");
-			}
+
 			this.useMove(move, pokemon, { target, sourceEffect });
-			// Restore PP if the move is the first turn of a charging move. Save the move from which PP should be deducted if the move succeeds.
+
 			if (pokemon.volatiles['twoturnmove']) {
-				pokemon.deductPP(move, -1, target);
-				pokemon.volatiles['twoturnmove'].originalMove = move.id;
+				// Deduct PP on the second turn, not first
+				// If called from e.g. Metronome, remember to deduct Metronome PP
+				pokemon.volatiles['twoturnmove'].ppMove = move.id;
+			} else if (ppMove) {
+				pokemon.deductPP(ppMove, null, target);
+				const moveSlot = pokemon.getMoveData(ppMove);
+				if (moveSlot && moveSlot.pp < 0) {
+					moveSlot.pp += 64;
+					this.battle.hint("In Gen 1, if a pokemon is forced to use a move with 0 PP, the move will underflow to have 63 PP.");
+				}
 			}
 		},
 		// This function deals with AfterMoveSelf events.
