e0fc15bb10eb4d6be5feafab27be9e25bb64c4bb
Ngammer <cnicolas810@gmail.com>
2025-03-20 12:07:58 -0300

    gripclaw

--- data/conditions.ts
+++ data/conditions.ts
@@ -188,7 +188,7 @@ export const Conditions: import('../sim/dex-conditions').ConditionDataTable = {
 		name: 'partiallytrapped',
 		duration: 5,
 		durationCallback(target, source) {
-			if (source?.hasItem('gripclaw')) return 8;
+			if (source?.hasItem('gripclaw')) return 11;
 			return this.random(5, 7);
 		},
 		onStart(pokemon, source) {
