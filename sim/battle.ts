e3bf77a2384b0f8968fc0ae4b1d1fd96f43cbb81
Elrond URPG <elrond.urpg@gmail.com>
2025-03-22 11:03:37 -0400

    enable a battle to have any number of sides

--- sim/battle.ts
+++ sim/battle.ts
@@ -121,7 +121,7 @@ export class Battle {
 	 */
 	readonly activePerHalf: 1 | 2 | 3;
 	readonly field: Field;
-	readonly sides: [Side, Side] | [Side, Side, Side, Side];
+	readonly sides: Side[]; //[Side, Side] | [Side, Side, Side, Side];
 	readonly prngSeed: PRNGSeed;
 	dex: ModdedDex;
 	gen: number;
@@ -3237,33 +3237,15 @@ export class Battle {
 		this.sentLogPos = this.log.length;
 
 		if (!this.sentEnd && this.ended) {
-			const log = {
+			let log = {
 				winner: this.winner,
 				seed: this.prngSeed,
 				turns: this.turn,
-				p1: this.sides[0].name,
-				p2: this.sides[1].name,
-				p3: this.sides[2]?.name,
-				p4: this.sides[3]?.name,
-				p1team: this.sides[0].team,
-				p2team: this.sides[1].team,
-				p3team: this.sides[2]?.team,
-				p4team: this.sides[3]?.team,
-				score: [this.sides[0].pokemonLeft, this.sides[1].pokemonLeft],
+				sides: this.sides.map(side => side.name),
+				teams: this.sides.map(side => side.team),
+				score: this.sides.map(side => side.pokemonLeft),
 				inputLog: this.inputLog,
 			};
-			if (this.sides[2]) {
-				log.score.push(this.sides[2].pokemonLeft);
-			} else {
-				delete log.p3;
-				delete log.p3team;
-			}
-			if (this.sides[3]) {
-				log.score.push(this.sides[3].pokemonLeft);
-			} else {
-				delete log.p4;
-				delete log.p4team;
-			}
 			this.send('end', JSON.stringify(log));
 			this.sentEnd = true;
 		}
