34a8b7b0d9f6c482cff3d813f8157beb12c6edb1
André Bastos Dias <80102738+andrebastosdias@users.noreply.github.com>
2025-03-23 18:06:23 +0000

    Lint

--- sim/battle.ts
+++ sim/battle.ts
@@ -2600,7 +2600,7 @@ export class Battle {
 		if (!action.pokemon) {
 			action.speed = 1;
 		} else if (action.rawSpeed) {
-			action.speed = action.pokemon.getStat('spe', true, true)
+			action.speed = action.pokemon.getStat('spe', true, true);
 		} else {
 			action.speed = action.pokemon.getActionSpeed();
 		}
