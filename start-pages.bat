5fd486aef306fd35c371f9779ac10257253fa6d2
Ngammer <cnicolas810@gmail.com>
2025-03-23 23:42:19 -0300

    Create start-pages.bat

---
+++ start-pages.bat
@@ -0,0 +1,6 @@
+start cmd /k node C:\Users\cnico\OneDrive\Escritorio\IC\pokemon\pokemon-showdown\pokemon-showdown
+start cmd /k npx serve -l 3000 C:\Users\cnico\OneDrive\Escritorio\IC\pokemon\pokemon-showdown-client\play.pokemonshowdown.com
+start cmd /k npx serve -l 3001 C:\Users\cnico\OneDrive\Escritorio\IC\pokemon\pokemon-showdown-client\pokemonshowdown.com
+start cmd /k npx serve -l 3002 C:\Users\cnico\OneDrive\Escritorio\IC\pokemon\pokemon-showdown-client\replay.pokemonshowdown.com
+start cmd /k npx serve -l 3003 C:\Users\cnico\OneDrive\Escritorio\IC\pokemon\Pokemon-Showdown-Dex
+start cmd /k npx serve -l 3004 C:\Users\cnico\OneDrive\Escritorio\IC\pokemon\damage-calc\dist
