f6174c6c4a503aecc7eb5fff6f3cea9f6db04426
Ngammer <cnicolas810@gmail.com>
2025-03-25 00:38:41 -0300

    metronome

--- data/items.ts
+++ data/items.ts
@@ -3777,36 +3777,17 @@ export const Items: import('../sim/dex-items').ItemDataTable = {
 		onStart(pokemon) {
 			pokemon.addVolatile('metronome');
 		},
+		onResidual(pokemon) {
+			if (pokemon.volatiles['metronome']) {
+				pokemon.volatiles['metronome'].turns++;
+			}
+			if (pokemon.volatiles['metronome']?.turns >= 4) {
+				this.boost({spe: 1}, pokemon)
+			}
+		},
 		condition: {
-			onStart(pokemon) {
-				this.effectState.lastMove = '';
-				this.effectState.numConsecutive = 0;
-			},
-			onTryMovePriority: -2,
-			onTryMove(pokemon, target, move) {
-				if (!pokemon.hasItem('metronome')) {
-					pokemon.removeVolatile('metronome');
-					return;
-				}
-				if (move.callsMove) return;
-				if (this.effectState.lastMove === move.id && pokemon.moveLastTurnResult) {
-					this.effectState.numConsecutive++;
-				} else if (pokemon.volatiles['twoturnmove']) {
-					if (this.effectState.lastMove !== move.id) {
-						this.effectState.numConsecutive = 1;
-					} else {
-						this.effectState.numConsecutive++;
-					}
-				} else {
-					this.effectState.numConsecutive = 0;
-				}
-				this.effectState.lastMove = move.id;
-			},
-			onModifyDamage(damage, source, target, move) {
-				const dmgMod = [4096, 4915, 5734, 6553, 7372, 8192];
-				const numConsecutive = this.effectState.numConsecutive > 5 ? 5 : this.effectState.numConsecutive;
-				this.debug(`Current Metronome boost: ${dmgMod[numConsecutive]}/4096`);
-				return this.chainModify([dmgMod[numConsecutive], 4096]);
+			onStart() {
+				this.effectState.turns = 0;
 			},
 		},
 		num: 277,
