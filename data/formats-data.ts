fe21d59924dd0bc662a9d146d33f0debdf76b3f3
Pierre Mauger <pierre.mauger@slickteam.fr>
2025-03-25 22:16:14 +0100

    fix: missing tier for 2 cap

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -4666,6 +4666,14 @@ export const FormatsData: import("../sim/dex-species").SpeciesFormatsDataTable =
         isNonstandard: "CAP",
         tier: "CAP",
     },
+    chuggon: {
+        isNonstandard: "CAP",
+        tier: "CAP LC",
+    },
+    draggalong: {
+        isNonstandard: "CAP",
+        tier: "CAP NFE",
+  },
     chuggalong: {
         isNonstandard: "CAP",
         tier: "CAP",
