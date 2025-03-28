3be8e9fdd4f2ac46dc040f8eca2b65ecfeabc0dd
Sacredjuansssss04 <juanesteban04012007@gmail.com>
2025-03-28 00:08:58 -0500

    Mis bolas 2.0

--- data/abilities.ts
+++ data/abilities.ts
@@ -4808,7 +4808,9 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 			}
 		},
 		onStart(pokemon) {
+			if (pokemon.getItem().name == 'Dive Ball')
 			this.boost({spa: 1}, pokemon)
+			pokemon.useItem()
 		},
 		flags: {breakable: 1},
 		name: "Storm Drain",
