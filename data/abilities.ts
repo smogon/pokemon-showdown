0bbcf0f692309367e315ed71228cd0c704ad9e33
Ngammer <cnicolas810@gmail.com>
2025-04-02 00:45:46 -0300

    arenatrap

--- data/abilities.ts
+++ data/abilities.ts
@@ -233,9 +233,31 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 		onFoeMaybeTrapPokemon(pokemon, source) {
 			if (!source) source = this.effectState.target;
 			if (!source || !pokemon.isAdjacent(source)) return;
-			if (pokemon.isGrounded(!pokemon.knownType)) { // Negate immunity if the type is unknown
+			if (pokemon.isGrounded(!pokemon.knownType) && pokemon.isAdjacent(this.effectState.target) && !(pokemon.hasType('Ground') || pokemon.hasType('Bug') || pokemon.hasType('Flying')) && pokemon.hp <= pokemon.baseMaxhp / 2) { // Negate immunity if the type is unknown
 				pokemon.maybeTrapped = true;
 			}
+		},
+		onResidual(pokemon){
+			for (const target of pokemon.adjacentFoes()) {
+				if(!target.volatiles['arenatrap']){
+				target.addVolatile('arenatrap')
+				}
+			}
+		},
+		condition:{
+			onStart() {
+				this.effectState.turns = 0;
+			},
+			onResidual(pokemon) {
+				if (pokemon.volatiles['arenatrap']) {
+					pokemon.volatiles['arenatrap'].turns++;
+				}
+			},
+			onTrapPokemon(pokemon) {
+				if(pokemon.volatiles['arenatrap']?.turns >= 2){
+				pokemon.trapped = pokemon.maybeTrapped = false;
+				}
+			},
 		},
 		flags: {},
 		name: "Arena Trap",
