ae068860ee82f9c3067d1e4151b95c427d8907d3
HiZo <96159984+HisuianZoroark@users.noreply.github.com>
2025-04-02 00:29:56 -0400

    banlist

--- config/formats.ts
+++ config/formats.ts
@@ -558,6 +558,16 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		desc: `The effects of the move Baton Pass are triggered upon manually withdrawing a Pokémon from battle.`,
 		mod: 'gen9',
 		ruleset: ['Standard OMs', 'Sleep Moves Clause'],
+		banlist: [
+			'Annihilape', 'Arceus', 'Archaludon', 'Baxcalibur', 'Calyrex-Ice', 'Calyrex-Shadow', 'Chien-Pao', 'Chi-Yu', 'Deoxys',
+			'Deoxys-Attack', 'Dialga', 'Dialga-Origin', 'Enamorus', 'Eternatus', 'Flutter Mane', 'Giratina', 'Giratina-Origin',
+			'Gouging Fire', 'Groudon', 'Ho-Oh', 'Iron Bundle', 'Koraidon', 'Kyogre', 'Kyurem-Black', 'Kyurem-White', 'Landorus',
+			'Lugia', 'Lunala', 'Magearna', 'Mewtwo', 'Miraidon', 'Necrozma-Dawn-Wings', 'Necrozma-Dusk-Mane', 'Ogerpon-Hearthflame',
+			'Palafin', 'Palkia', 'Palkia-Origin', 'Rayquaza', 'Regieleki', 'Reshiram', 'Shaymin-Sky', 'Sneasler', 'Solgaleo', 'Spectrier',
+			'Terapagos', 'Ursaluna-Bloodmoon', 'Urshifu', 'Urshifu-Rapid-Strike', 'Zacian', 'Zacian-Crowned', 'Zamazenta-Crowned', 'Zekrom', 'Arena Trap',
+			'Moody', 'Sand Veil', 'Shadow Tag', 'Snow Cloak', 'Speed Boost', 'Bright Powder', 'King\'s Rock', 'Razor Fang', 'Clangorous Soul', 'Last Respects',
+			'Mud-Slap', 'Muddy Water', 'Night Daze', 'No Retreat', 'Sand Attack', 'Shell Smash', 'Smokescreen', 'Victory Dance', 'Quiver Dance',
+		],
 		actions: {
 			switchIn(pokemon, pos, sourceEffect, isDrag) {
 				if (!pokemon || pokemon.isActive) {
