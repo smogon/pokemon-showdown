49de29a6ff237224e76148ddb5e5c76734e40a95
Musaddik Temkar <musaddiktemkargpay@gmail.com>
2025-04-09 22:15:11 +0530

    Update misc-commands.ts

--- impulse-plugins/misc-commands.ts
+++ impulse-plugins/misc-commands.ts
@@ -170,6 +170,21 @@ export const commands: Chat.ChatCommands = {
     user.lastPoofMessage = message;
     user.disconnectAll();
   },
+	
+	seen(target: string | null, room: Room | User, user: User) {
+		if (!this.runBroadcast()) return;
+		if (!target) return this.parse('/help seen');
+		const targetUser = Users.get(target);
+		if (targetUser?.connected) {
+			return this.sendReplyBox(`${Impulse.nameColor(targetUser.name, true, true)} is <b><font color='limegreen'>Currently Online</b></font>.`);
+		}
+		const targetId = toID(target);
+		const seen = Db.seen.get(targetId);
+		if (!seen) {
+			return this.sendReplyBox(`${Impulse.nameColor(target, true, true)} has <b><font color='red'>never been online</font></b> on this server.`);
+		}
+		this.sendReplyBox(`${Impulse.nameColor(target, true, true)} was last seen <b>${Chat.toDurationString(Date.now() - seen, { precision: true })}</b> ago.`);
+	},
 
   mischelp(target, room, user) {
     if (!this.runBroadcast()) return;
@@ -178,6 +193,7 @@ export const commands: Chat.ChatCommands = {
 		 `<ul><li><code>/clearall</code> - Clears chatroom. (Requires: #)</li>` +
 		 `<li><code>/globalclearall</code> - Clears all chatrooms. (Requires: @ and higher)</li>` +
 		 `<li><code>/poof</code> - Disconnect from server with random message.</li>` +
+		 `<li><code>/seen [user]</code> - Shows when the user last connected on the server.` +
 		 `</ul></div>`);
   },
 };
