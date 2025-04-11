c8146ce50f165791f9ac1141024ea6083643da6d
Musaddik Temkar <musaddiktemkargpay@gmail.com>
2025-04-11 20:34:57 +0530

    Update chat.ts

--- server/chat.ts
+++ server/chat.ts
@@ -681,10 +681,10 @@ export class CommandContext extends MessageContext {
 		}
 
 		// Impulse Exp
-		if (Impulse.ExpSystem.canAddExp(this.user.id)) {
+		/*if (Impulse.ExpSystem.canAddExp(this.user.id)) {
 			Impulse.ExpSystem.addExp(this.user.id, 1);
 			Impulse.ExpSystem.setAddExpCooldown(this.user.id);
-		}
+		}*/
 			
 		this.update();
 
