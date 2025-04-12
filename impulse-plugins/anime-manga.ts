2bf5602e789a5690c0b5d8485727273872a5c257
Musaddik Temkar <musaddiktemkargpay@gmail.com>
2025-04-12 10:41:53 +0530

    Update anime-manga.ts

--- impulse-plugins/anime-manga.ts
+++ impulse-plugins/anime-manga.ts
@@ -1,6 +1,6 @@
 /******************************************
 * Pokemon Showdown Anime & Manga Commands *
-* Author: @Prince Sky                     *
+* author: @Prince Sky                     *
 * Html By: @TurboRx                       *
 *******************************************/
 
