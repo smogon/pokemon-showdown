a82f949425e101f15cd863ae98d2a6b2970c09af
Musaddik Temkar <musaddiktemkargpay@gmail.com>
2025-04-12 12:08:22 +0530

    Update misc-commands.ts

--- impulse-plugins/misc-commands.ts
+++ impulse-plugins/misc-commands.ts
@@ -119,7 +119,7 @@ const messages: string[] = [
 
 export const commands: Chat.ChatCommands = {
   clearall(target: string, room: ChatRoom | null, user: User): void {
-    this.checkCan('roommod');
+    this.checkCan('roomban');
     if (room?.battle) {
       return this.sendReply("You cannot clearall in battle rooms.");
     }
