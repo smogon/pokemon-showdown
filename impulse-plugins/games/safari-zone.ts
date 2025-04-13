9518500a465bf642cd10d65702903b8b1013a5db
Musaddik Temkar <musaddiktemkargpay@gmail.com>
2025-04-13 13:07:58 +0530

    Update safari-zone.ts

--- impulse-plugins/games/safari-zone.ts
+++ impulse-plugins/games/safari-zone.ts
@@ -1,6 +1,6 @@
 /*************************************
  * Pokemon Safari Zone Game          *
- * nitial Release                    *
+ * Initial Release                    *
  * Author: @musaddiktemkar           *
  **************************************/
 
