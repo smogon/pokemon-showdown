e094ca1f223ca41bbb1372e866afc4ea76b0296a
Musaddik Temkar <musaddiktemkargpay@gmail.com>
2025-04-18 13:17:42 +0530

    Delete impulse-plugins/clans/index.ts

--- impulse-plugins/clans/index.ts
+++
@@ -1,8 +0,0 @@
-import { restoreClanRooms } from './manager';
-
-export const Init = {
-    async init() {
-        // Restore clan rooms when the server starts
-        await restoreClanRooms();
-    }
-};
