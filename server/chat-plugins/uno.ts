e223a1d1824beaaf64211d715d00f7d524c823d4
Shriansh Chari <30420527+shrianshChari@users.noreply.github.com>
2025-04-28 01:37:36 -0700

    Uno: Automatically pass if drawn card cannot be played

--- server/chat-plugins/uno.ts
+++ server/chat-plugins/uno.ts
@@ -335,6 +335,17 @@ export class UNO extends Rooms.RoomGame<UNOPlayer> {
 		const card = this.onDrawCard(player, 1);
 		player.sendDisplay();
 		player.cardLock = card[0].name;
+
+		// Auto-pass if you can't play this card
+		if (
+			this.topCard &&
+			card[0].color !== 'Black' &&
+			card[0].color !== (this.topCard.changedColor || this.topCard.color) &&
+			card[0].value !== this.topCard.value
+		) {
+			this.sendToRoom(`|c:|${Math.floor(Date.now() / 1000)}|~|${player.name} has passed.`)
+			this.nextTurn()
+		}
 	}
 
 	onPlay(player: UNOPlayer, cardName: string) {
