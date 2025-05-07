f47f3294fafe31ed256398b939871694169a463e
Meijer,L. (Lucas) <l.meijer6@students.uu.nl>
2025-05-08 00:46:55 +0200

    Add Data Mod
    Pet Mods requested a Data Mod that automatically shows information for formats that have many custom changes (currently CCAPM2024 Random Battle).

---
+++ data/mods/ccapm2024/formats-data.ts
@@ -0,0 +1,191 @@
+export const FormatsData: import('../../../sim/dex-species').ModdedSpeciesFormatsDataTable = {
+	aesap: {
+		tier: "OU",
+	},
+	anxiousoil: {
+		tier: "OU",
+	},
+	araquisis: {
+		tier: "OU",
+	},
+	arthrostrike: {
+		tier: "OU",
+	},
+	bleyabat: {
+		tier: "OU",
+	},
+	boillusk: {
+		tier: "OU",
+	},
+	boogeymancer: {
+		tier: "OU",
+	},
+	buffball: {
+		tier: "OU",
+	},
+	bugsome: {
+		tier: "OU",
+	},
+	cliffilisk: {
+		tier: "OU",
+	},
+	cogwyld: {
+		tier: "OU",
+	},
+	contradox: {
+		tier: "OU",
+	},
+	cosmole: {
+		tier: "OU",
+	},
+	crashtank: {
+		tier: "OU",
+	},
+	cryobser: {
+		tier: "OU",
+	},
+	delirirak: {
+		tier: "OU",
+	},
+	depresloth: {
+		tier: "OU",
+	},
+	faellen: {
+		tier: "OU",
+	},
+	fightinfly: {
+		tier: "OU",
+	},
+	folibower: {
+		tier: "OU",
+	},
+	frenzaiai: {
+		tier: "OU",
+	},
+	fungemory: {
+		tier: "OU",
+	},
+	guarden: {
+		tier: "OU",
+	},
+	hawksectiff: {
+		tier: "OU",
+	},
+	ichthyocorn: {
+		tier: "OU",
+	},
+	lampyre: {
+		tier: "OU",
+	},
+	lazahrusk: {
+		tier: "OU",
+	},
+	leviadon: {
+		tier: "OU",
+	},
+	liwyzard: {
+		tier: "OU",
+	},
+	magmouth: {
+		tier: "OU",
+	},
+	manticrash: {
+		tier: "OU",
+	},
+	marlord: {
+		tier: "OU",
+	},
+	marsonmallow: {
+		tier: "OU",
+	},
+	mindwyrm: {
+		tier: "OU",
+	},
+	minkai: {
+		tier: "OU",
+	},
+	mosstrosity: {
+		tier: "OU",
+	},
+	nectaregal: {
+		tier: "OU",
+	},
+	nharboard: {
+		tier: "OU",
+	},
+	noyew: {
+		tier: "OU",
+	},
+	nucleophage: {
+		tier: "OU",
+	},
+	nummanutts: {
+		tier: "OU",
+	},
+	obsallas: {
+		tier: "OU",
+	},
+	oonoonsi: {
+		tier: "OU",
+	},
+	orchidauntless: {
+		tier: "OU",
+	},
+	pestifer: {
+		tier: "OU",
+	},
+	pomegrenade: {
+		tier: "OU",
+	},
+	raintoad: {
+		tier: "OU",
+	},
+	remnant: {
+		tier: "OU",
+	},
+	rizzquaza: {
+		tier: "OU",
+	},
+	roddammit: {
+		tier: "OU",
+	},
+	roolette: {
+		tier: "OU",
+	},
+	rootfraction: {
+		tier: "OU",
+	},
+	shail: {
+		tier: "OU",
+	},
+	shufflux: {
+		tier: "OU",
+	},
+	shurifluri: {
+		tier: "OU",
+	},
+	skibidragon: {
+		tier: "OU",
+	},
+	spreetah: {
+		tier: "OU",
+	},
+	suragon: {
+		tier: "OU",
+	},
+	surfsurge: {
+		tier: "OU",
+	},
+	tardeblade: {
+		tier: "OU",
+	},
+	trawlutre: {
+		tier: "OU",
+	},
+	tuxquito: {
+		tier: "OU",
+	},
+	underhazard: {
+		tier: "OU",
+	},
+};
