fb44762c9ef34b1d7d06f3257db1cfca27805c18
Guangcong Luo <guangcongluo@gmail.com>
2025-05-18 13:39:06 -0700

    Port CompoundWordNames into data/aliases (#11121)
    This is what lets you type "ww" or "wind" to find "whirlwind" in the
    teambuilder (and PSdex) search.
    
    These were previously in the client build scripts, where I think most
    people didn't realize how to find or update them.

--- test/sim/data.js
+++ test/sim/data.js
@@ -166,6 +166,19 @@ describe('Dex data', () => {
 		}
 	});
 
+	it('should have valid CompoundWordNames entries', () => {
+		const CompoundWordNames = require('../../dist/data/aliases').CompoundWordNames;
+		for (const name of CompoundWordNames) {
+			const targetid = toID(name);
+			const actualName = Dex.data.Pokedex[targetid]?.name || Dex.data.Moves[targetid]?.name ||
+				Dex.data.Abilities[targetid]?.name || Dex.data.Items[targetid]?.name;
+			assert(actualName, `CompoundWordNames entry "${name}" must be a pokemon/move/ability/item`);
+			assert.equal(actualName.replace(/-/g, ''), name.replace(/-/g, ''), `CompoundWordNames entry "${name}" should be the same as its target name (ignoring hyphens)`);
+			assert(name.includes('-'), `CompoundWordNames entry "${name}" should have at least one hyphen (to mark a word boundary)`);
+			assert(name.split('-').length <= 3, `CompoundWordNames entry "${name}" should have at most two hyphens (more aren't supported)`);
+		}
+	});
+
 	it('should have valid Rulesets entries', () => {
 		const Rulesets = Dex.data.Rulesets;
 		for (const formatid in Rulesets) {
