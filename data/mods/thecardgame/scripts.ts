cb1a8bef452ca01af53a5aa2a4ac72e711f950cd
Dieter Reinert <dieter.reinert@outlook.com>
2025-06-01 14:49:08 +0200

    Feedback

--- data/mods/thecardgame/scripts.ts
+++ data/mods/thecardgame/scripts.ts
@@ -115,7 +115,9 @@ export const Scripts: ModdedBattleScriptsData = {
 			}
 			return false;
 		},
-		runImmunity(type, message) {
+		runImmunity(source: ActiveMove | string, message?: string | boolean) {
+			if (!source) return true;
+			const type: string = typeof source !== 'string' ? source.type : source;
 			if (!type || type === '???') return true;
 			if (!this.battle.dex.types.isName(type)) {
 				throw new Error("Use runStatusImmunity for " + type);
