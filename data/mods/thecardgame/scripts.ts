399238d9fb546aebc1b4d4248d73f39258d63ec4
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2025-06-02 01:12:29 -0600

    Update `runImmunity` type for mods

--- data/mods/thecardgame/scripts.ts
+++ data/mods/thecardgame/scripts.ts
@@ -115,12 +115,18 @@ export const Scripts: ModdedBattleScriptsData = {
 			}
 			return false;
 		},
-		runImmunity(type, message) {
+		runImmunity(source, message) {
+			if (!source) return true;
+			const type: string = typeof source !== 'string' ? source.type : source;
+			if (typeof source !== 'string') {
+				if (source.ignoreImmunity && (source.ignoreImmunity === true || source.ignoreImmunity[type])) {
+					return true;
+				}
+			}
 			if (!type || type === '???') return true;
 			if (!this.battle.dex.types.isName(type)) {
 				throw new Error("Use runStatusImmunity for " + type);
 			}
-			if (this.fainted) return false;
 
 			const negateImmunity = !this.battle.runEvent('NegateImmunity', this, type);
 			const notImmune = type === 'Fighting' ?
