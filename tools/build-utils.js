16a2e562d189992b2181f080b0543602cd69d895
Dieter Reinert <dieter.reinert@outlook.com>
2025-06-05 14:32:08 +0200

    ESLint Fixes 2

--- tools/build-utils.js
+++ tools/build-utils.js
@@ -4,12 +4,12 @@ const fs = require("fs");
 const path = require("path");
 const oxc = require("oxc-transform");
 const fg = require("fast-glob");
-const {execSync} = require("child_process");
+const { execSync } = require("child_process");
 
 function copyJSON(dir = 'data') {
 	for (const src of fg.sync(`${dir}/**/*.json`)) {
 		const dst = path.join('dist', src);
-		fs.mkdirSync(path.dirname(dst), {recursive: true});
+		fs.mkdirSync(path.dirname(dst), { recursive: true });
 		fs.copyFileSync(src, dst);
 	}
 }
@@ -25,12 +25,12 @@ function sourceFiles() {
 }
 
 exports.transpile = (force, emitDecl) => {
-	fs.mkdirSync('dist', {recursive: true});
+	fs.mkdirSync('dist', { recursive: true });
 
 	for (const file of sourceFiles()) {
 		const src = fs.readFileSync(file, 'utf8');
 
-		const {code, map, errors} = oxc.transform(
+		const { code, map, errors } = oxc.transform(
 			file,
 			src,
 			{}
@@ -44,7 +44,7 @@ exports.transpile = (force, emitDecl) => {
 
 		const rel = file.replace(/\.[cm]?[jt]sx?$/, '');
 		const outJS = path.join('dist', `${rel}.js`);
-		fs.mkdirSync(path.dirname(outJS), {recursive: true});
+		fs.mkdirSync(path.dirname(outJS), { recursive: true });
 		fs.writeFileSync(outJS, code);
 		if (map) fs.writeFileSync(`${outJS}.map`, JSON.stringify(map));
 	}
@@ -60,7 +60,7 @@ exports.buildDecls = () => {
 	try {
 		execSync(
 			'npx tsc --emitDeclarationOnly --declaration --declarationMap --outDir dist',
-			{stdio: 'inherit'}
+			{ stdio: 'inherit' }
 		);
-	} catch { }
+	} catch {}
 };
