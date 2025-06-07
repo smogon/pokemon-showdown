310e6fd79120e26d2185ee3c6a792ff2a834a31b
André Bastos Dias <80102738+andrebastosdias@users.noreply.github.com>
2025-06-07 13:34:30 +0100

    Fix order

--- sim/battle-queue.ts
+++ sim/battle-queue.ts
@@ -19,7 +19,7 @@ import type { Battle } from './battle';
 export interface MoveAction {
 	/** action type */
 	choice: 'move' | 'beforeTurnMove' | 'priorityChargeMove';
-	order: 3 | 5 | 105 | 199 | 200 | 201;
+	order: 5 | 105 | 199 | 200 | 201;
 	/** priority of the action (lower first) */
 	priority: number;
 	/** fractional priority of the action (lower first) */
@@ -276,7 +276,7 @@ export class BattleQueue {
 			}
 		}
 		action.sourceEffect = sourceEffect;
-		action.order = 3;
+		action.order = action.choice === 'move' ? 199 : 3;
 		this.list.unshift(action);
 	}
 
