76a668eb81ba4b690d429f480554bdc44777f872
André Bastos Dias <andrebastosdias@gmail.com>
2025-06-11 05:04:05 +0100

    Lint

--- data/mods/gen6/abilities.ts
+++ data/mods/gen6/abilities.ts
@@ -110,7 +110,7 @@ export const Abilities: import('../../../sim/dex-abilities').ModdedAbilityDataTa
 			}
 			this.add('-activate', source, 'ability: Symbiosis', myItem, `[of] ${pokemon}`);
 		},
-		onAnyAfterMove() {}
+		onAnyAfterMove() {},
 	},
 	weakarmor: {
 		inherit: true,
