54bd4c5977a3ec56258eb82a4a43ed0e27e7fa7e
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2025-06-17 13:14:39 -0600

    Auto Responder: Fix `/ar view keys` not escaping regex keys

--- server/chat-plugins/responder.ts
+++ server/chat-plugins/responder.ts
@@ -497,8 +497,9 @@ export const pages: Chat.PageTable = {
 				for (const regex of regexes) {
 					const index = regexes.indexOf(regex) + 1;
 					const button = `<button class="button" name="send"value="/msgroom ${room.roomid},/ar remove ${item}, ${index}">Remove</button>`;
-					buffer += `<tr><td>${index}</td><td><code>${regex}</code></td>`;
-					if (canChange) buffer += `<td>${button}</td></tr>`;
+					buffer += Utils.html`<tr><td>${index}</td><td><code>${regex}</code></td>`;
+					if (canChange) buffer += `<td>${button}</td>`;
+					buffer += `</tr>`;
 				}
 				buffer += `</details>`;
 				return buffer;
