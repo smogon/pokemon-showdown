43dd5389cd66c0d00a34befdba256dc097c519ca
André Bastos Dias <andrebastosdias@gmail.com>
2025-06-19 22:42:09 +0100

    Remove outdated comment

--- sim/battle-actions.ts
+++ sim/battle-actions.ts
@@ -1044,8 +1044,6 @@ export class BattleActions {
 		targets: SpreadMoveTargets, pokemon: Pokemon, moveOrMoveName: ActiveMove,
 		hitEffect?: Dex.HitEffect, isSecondary?: boolean, isSelf?: boolean
 	): [SpreadMoveDamage, SpreadMoveTargets] {
-		// Hardcoded for single-target purposes
-		// (no spread moves have any kind of onTryHit handler)
 		let damage: (number | boolean | undefined)[] = [];
 		for (const i of targets.keys()) {
 			damage[i] = true;
