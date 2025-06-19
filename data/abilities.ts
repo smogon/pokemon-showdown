32de6e17b03dbcce7537a06a0a8bfed3912072be
André Bastos Dias <andrebastosdias@gmail.com>
2025-06-19 02:04:03 +0100

    Revert "Revert Normalize"
    This reverts commit e6f670daa8f409774c6612a62e2d8c7585816ec1.

--- data/abilities.ts
+++ data/abilities.ts
@@ -2929,9 +2929,9 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 			const noModifyType = [
 				'hiddenpower', 'judgment', 'multiattack', 'naturalgift', 'revelationdance', 'struggle', 'technoblast', 'terrainpulse', 'weatherball',
 			];
-			if (!(move.isZ && move.category !== 'Status') && !noModifyType.includes(move.id) &&
+			if (!(move.isZ && move.category !== 'Status') &&
 				// TODO: Figure out actual interaction
-				!(move.name === 'Tera Blast' && pokemon.terastallized)) {
+				(!noModifyType.includes(move.id) || this.activeMove?.isMax) && !(move.name === 'Tera Blast' && pokemon.terastallized)) {
 				move.type = 'Normal';
 				move.typeChangerBoosted = this.effect;
 			}
