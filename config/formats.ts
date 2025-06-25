0d908f6def2038090670f799489150a2c0a72bbb
André Bastos Dias <80102738+andrebastosdias@users.noreply.github.com>
2025-06-25 22:14:46 +0100

    Fix HP bar percentages and colours (#11213)

--- config/formats.ts
+++ config/formats.ts
@@ -4805,7 +4805,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		searchShow: false,
 		ruleset: [
 			'Picked Team Size = 3', 'Min Level = 50', 'Max Level = 55', 'Max Total Level = 155',
-			'Obtainable', 'Team Preview', 'Stadium Sleep Clause', 'Species Clause', 'Nickname Clause', 'HP Percentage Mod', 'Cancel Mod', 'NC 1997 Move Legality',
+			'Obtainable', 'Team Preview', 'Stadium Sleep Clause', 'Species Clause', 'Nickname Clause', 'Cancel Mod', 'NC 1997 Move Legality',
 		],
 		banlist: ['Uber'],
 	},
