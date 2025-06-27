d5982420864e78862f5b70811009a492bd77e163
Samuel Cohen <11701891+samuelcohenscali@users.noreply.github.com>
2025-06-27 11:27:00 -0400

    Update formats.ts
    Changing to b6p4.

--- config/formats.ts
+++ config/formats.ts
@@ -4516,7 +4516,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		mod: 'gen4pt',
 		gameType: 'doubles',
 		searchShow: false,
-		ruleset: ['Flat Rules', '! Adjust Level Down', 'Max Level = 50', 'Min Team Size = 4', 'Max Team Size = 4'],
+		ruleset: ['Flat Rules', '! Adjust Level Down', 'Max Level = 50', 'Picked Team Size = 4', 'Max Team Size = 6'],
 		banlist: ['Tyranitar', 'Rotom', 'Judgment', 'Soul Dew'],
 	},
 	{
