59c2e9338f4cc89a28d088dad09ee133ab887877
André Bastos Dias <80102738+andrebastosdias@users.noreply.github.com>
2025-07-12 22:17:35 +0100

    Re-number CAP moves and abilities (#11257)

--- data/abilities.ts
+++ data/abilities.ts
@@ -5601,7 +5601,7 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 		flags: { breakable: 1 },
 		name: "Mountaineer",
 		rating: 3,
-		num: -2,
+		num: -1,
 	},
 	rebound: {
 		isNonstandard: "CAP",
@@ -5634,7 +5634,7 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 		flags: { breakable: 1 },
 		name: "Rebound",
 		rating: 3,
-		num: -3,
+		num: -2,
 	},
 	persistent: {
 		isNonstandard: "CAP",
@@ -5642,6 +5642,6 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 		flags: {},
 		name: "Persistent",
 		rating: 3,
-		num: -4,
+		num: -3,
 	},
 };
