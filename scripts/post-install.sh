97ccedfcbcffb14a1132d97ec83bf363d6093773
Elrond URPG <elrond.urpg@gmail.com>
2025-07-13 18:48:50 -0400

    update post-install.sh to stop urpg-battle-bot and restart

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -1,4 +1,18 @@
+pname="node urpg-battle-bot-app.js"
+
+pid=`/bin/ps -fu $USER|grep "$pname" | grep -v "grep" | awk '{print $2}'`
+if [[ $pid -gt 0 ]]; then
+  kill $pid
+  while kill -0 $pid; do
+    sleep 1
+  done
+fi
+
 yum install -y nodejs npm
 cd /home/ec2-user/urpg-battle-bot-calc
 npm install
 npm run build
+
+cd /home/ec2-user/urpg-battle-bot
+npm install
+nohup npm run start &
