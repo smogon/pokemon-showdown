5fc7582568f7448263adf7124e38fe106e4420c6
Elrond URPG <elrond.urpg@gmail.com>
2025-07-21 15:14:33 -0400

    updating deployment files to use separate user

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -9,10 +9,10 @@ if [[ $pid -gt 0 ]]; then
 fi
 
 yum install -y nodejs npm
-cd /home/ec2-user/urpg-battle-bot-calc
+cd /etc/battlebot/urpg-battle-bot-calc
 npm install
 npm run build
 
-cd /home/ec2-user/urpg-battle-bot
-npm install
-nohup npm run start &
+#cd /home/ec2-user/urpg-battle-bot
+#npm install
+#nohup npm run start &
