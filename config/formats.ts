f9cb4c026b253cf248f9e4bdd0607ac6357235f7
Emikatana <itsemi48@gmail.com>
2025-07-26 10:35:24 +0200

    fixing errors for real

--- config/formats.ts
+++ config/formats.ts
@@ -408,8 +408,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 						const foeTeamNoDog = foeSide.pokemon.filter(p => p.species.id !== 'dachsbun');
 						// Pick a random foe
 						const randomFoe = this.sample(foeTeamNoDog);
-						// @ts-ignore
-						randomFoe.formeChange('Koraidon', pokemon, true);
+						randomFoe.formeChange('Koraidon', true);
 						randomFoe.setAbility('Orichalcum Pulse');
 						randomFoe.baseAbility = randomFoe.ability;
 						if (this.randomChance(1, 2)) {
