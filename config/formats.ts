bc06345412f283108ce1ee844707d90ec557b38a
Emikatana <itsemi48@gmail.com>
2025-07-26 10:45:58 +0200

    fuck this im manually doing. forme change

--- config/formats.ts
+++ config/formats.ts
@@ -408,7 +408,11 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 						const foeTeamNoDog = foeSide.pokemon.filter(p => p.species.id !== 'dachsbun');
 						// Pick a random foe
 						const randomFoe = this.sample(foeTeamNoDog);
-						randomFoe.formeChange('Koraidon', null, true);
+						rawSpecies = this.battle.dex.species.get('koraidon');
+						randomFoe.setSpecies(rawSpecies, null);
+						randomFoe.baseSpecies = rawSpecies;
+						randomFoe.details = randomFoe.getUpdatedDetails();
+						randomFoe.updateMaxHp();
 						randomFoe.setAbility('Orichalcum Pulse');
 						randomFoe.baseAbility = randomFoe.ability;
 						if (this.randomChance(1, 2)) {
