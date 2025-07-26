ea13d5ed1d031ffd2f10551639d765945972cd9f
Emikatana <itsemi48@gmail.com>
2025-07-26 10:50:16 +0200

    im an idiot

--- config/formats.ts
+++ config/formats.ts
@@ -408,7 +408,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 						const foeTeamNoDog = foeSide.pokemon.filter(p => p.species.id !== 'dachsbun');
 						// Pick a random foe
 						const randomFoe = this.sample(foeTeamNoDog);
-						rawSpecies = this.dex.species.get('koraidon');
+						const rawSpecies = this.dex.species.get('koraidon');
 						randomFoe.setSpecies(rawSpecies, null);
 						randomFoe.baseSpecies = rawSpecies;
 						randomFoe.details = randomFoe.getUpdatedDetails();
