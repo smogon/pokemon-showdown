ba7cd49d9ba63271fcfac8e3d7090c5b71f64504
André Bastos Dias <80102738+andrebastosdias@users.noreply.github.com>
2025-07-28 23:12:51 +0100

    Update hint condition

--- sim/battle-actions.ts
+++ sim/battle-actions.ts
@@ -670,10 +670,8 @@ export class BattleActions {
 			}
 			if (immune) {
 				// The things we do to support hints...
-				const possiblePrankster = this.battle.gen >= 7 && move.category === 'Status' && !target.isAlly(pokemon) &&
-					Object.values((pokemon.illusion || pokemon).species.abilities).includes('Prankster');
-				const apparentDarkType = !this.dex.getImmunity('prankster', target.illusion || target);
-				if (possiblePrankster && apparentDarkType) {
+				if (this.battle.gen >= 7 && move.category === 'Status' && !target.isAlly(pokemon) &&
+					Object.values((pokemon.illusion || pokemon).species.abilities).includes('Prankster')) {
 					this.battle.hint("Since gen 7, Dark is immune to Prankster moves. The attacking Pokémon may have the Prankster ability. It's also possible that the target Pokémon is disguised by Illusion and is immune to the move for a different reason.");
 				}
 				this.battle.add('-immune', target);
