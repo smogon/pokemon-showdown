e310feff568039ca54b4e03d52003992fb69fa7b
Emikatana <itsemi48@gmail.com>
2025-08-01 13:20:18 -0400

    suggested change from review
    Co-authored-by: Kris Johnson <11083252+KrisXV@users.noreply.github.com>

--- data/mods/chatbats/moves.ts
+++ data/mods/chatbats/moves.ts
@@ -492,7 +492,8 @@ export const Moves: { [moveid: string]: ModdedMoveData } = {
 			},
 			onSwitchIn(pokemon) {
 				// Order Up + king of the hill functionality
-				if (((!pokemon.isGrounded() || pokemon.hasItem('heavydutyboots')) && !pokemon.side.getSideCondition('kingofthehill')) || pokemon.side.getSideCondition('orderup')) return;
+				if (((!pokemon.isGrounded() || pokemon.hasItem('heavydutyboots')) && !pokemon.side.getSideCondition('kingofthehill')) ||
+					pokemon.side.getSideCondition('orderup')) return;
 				const damageAmounts = [0, 3, 4, 6]; // 1/8, 1/6, 1/4
 				this.damage(damageAmounts[this.effectState.layers] * pokemon.maxhp / 24);
 			},
