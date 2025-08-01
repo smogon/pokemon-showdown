525d72e6fefd964b1e9fe2b5e7f3828d68db0852
ACakeWearingAHat <45981036+ACakeWearingAHat@users.noreply.github.com>
2025-08-01 08:51:31 -0500

    Randomized format set updates (#11311)
    * Randomized format set updates
    
    * Remove punches from the other Metagross set as well
    
    ---------
    
    Co-authored-by: livid-washed <115855253+livid-washed@users.noreply.github.com>

--- data/random-battles/gen7/teams.ts
+++ data/random-battles/gen7/teams.ts
@@ -113,7 +113,7 @@ export class RandomGen7Teams extends RandomGen8Teams {
 				!counter.get('Bug') && (abilities.includes('Tinted Lens') || abilities.includes('Adaptability'))
 			),
 			Dark: (movePool, moves, abilities, types, counter) => !counter.get('Dark'),
-			Dragon: (movePool, moves, abilities, types, counter) => !counter.get('Dragon') && !abilities.includes('Aerilate'),
+			Dragon: (movePool, moves, abilities, types, counter) => !counter.get('Dragon'),
 			Electric: (movePool, moves, abilities, types, counter) => !counter.get('Electric'),
 			Fairy: (movePool, moves, abilities, types, counter) => !counter.get('Fairy'),
 			Fighting: (movePool, moves, abilities, types, counter) => !counter.get('Fighting'),
