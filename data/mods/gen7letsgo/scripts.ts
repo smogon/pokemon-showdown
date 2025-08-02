0bc6eb09f5c3c3a2163795764faeca590849c16e
André Bastos Dias <andrebastosdias@gmail.com>
2025-08-02 15:42:57 +0100

    Refactor stat calculation

--- data/mods/gen7letsgo/scripts.ts
+++ data/mods/gen7letsgo/scripts.ts
@@ -62,38 +62,22 @@ export const Scripts: ModdedBattleScriptsData = {
 	/**
 	 * Given a table of base stats and a pokemon set, return the actual stats.
 	 */
-	spreadModify(baseStats, set) {
-		const modStats: StatsTable = { hp: 10, atk: 10, def: 10, spa: 10, spd: 10, spe: 10 };
-		let statName: StatID;
-		for (statName in modStats) {
-			const stat = baseStats[statName];
-			modStats[statName] = Math.floor((Math.floor(2 * stat + set.ivs[statName]) * set.level / 100 + 5));
+	statModify(baseStats, set, statName) {
+		const tr = this.trunc;
+		let stat = baseStats[statName];
+		if (statName === 'hp') {
+			return tr(tr(2 * stat + set.ivs[statName] + 100) * set.level / 100 + 10) + set.evs[statName];
 		}
-		if ('hp' in baseStats) {
-			const stat = baseStats['hp'];
-			modStats['hp'] = Math.floor(Math.floor(2 * stat + set.ivs['hp'] + 100) * set.level / 100 + 10);
-		}
-		return this.natureModify(modStats, set);
-	},
-
-	/**
-	 * @param {StatsTable} stats
-	 * @param {PokemonSet} set
-	 * @return {StatsTable}
-	 */
-	natureModify(stats, set) {
+		stat = tr((tr(2 * stat + set.ivs[statName]) * set.level / 100 + 5));
 		const nature = this.dex.natures.get(set.nature);
-		if (nature.plus) stats[nature.plus] = Math.floor(stats[nature.plus] * 1.1);
-		if (nature.minus) stats[nature.minus] = Math.floor(stats[nature.minus] * 0.9);
-		set.happiness = 70;
-		const friendshipValue = Math.floor((set.happiness / 255 / 10 + 1) * 100);
-		let stat: StatID;
-		for (stat in stats) {
-			if (stat !== 'hp') {
-				stats[stat] = Math.floor(stats[stat] * friendshipValue / 100);
-			}
-			stats[stat] += set.evs[stat];
+		if (nature.plus && nature.plus === statName) {
+			stat = tr(tr(stat * 110, 16) / 100);
+		} else if (nature.minus && nature.minus === statName) {
+			stat = tr(tr(stat * 90, 16) / 100);
 		}
-		return stats;
+		set.happiness = 70;
+		const friendshipValue = tr((set.happiness / 255 / 10 + 1) * 100);
+		stat = tr(stat * friendshipValue / 100);
+		return stat;
 	},
 };
