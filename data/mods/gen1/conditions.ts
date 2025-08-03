377003847d7a3ff314b6c4e70a90e88f456c3eed
André Bastos Dias <andrebastosdias@gmail.com>
2025-08-03 14:00:43 +0100

    Fix Transform and Mimic PP in Gen 1

--- data/mods/gen1/conditions.ts
+++ data/mods/gen1/conditions.ts
@@ -243,6 +243,10 @@ export const Conditions: import('../../../sim/dex-conditions').ModdedConditionDa
 			}
 		},
 		onAfterMove(pokemon, target, move) {
+			if (target && target.hp <= 0) {
+				delete pokemon.volatiles['partialtrappinglock'];
+				return;
+			}
 			if (this.effectState.duration === 1) {
 				if (this.effectState.totalDuration !== 5) {
 					pokemon.addVolatile('fakepartiallytrapped');
@@ -266,6 +270,13 @@ export const Conditions: import('../../../sim/dex-conditions').ModdedConditionDa
 		duration: 0,
 		onBeforeMovePriority: 7,
 		onStart() {},
+		onAfterMove(pokemon, target, move) {
+			if (target && target.hp <= 0) {
+				delete pokemon.volatiles['mustrecharge'];
+				return;
+			}
+			this.add('-mustrecharge', pokemon);
+		},
 	},
 	lockedmove: {
 		// Thrash and Petal Dance.
