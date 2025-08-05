9e18f137ba03f61369e92d4b2012fc1f433f5b99
André Bastos Dias <80102738+andrebastosdias@users.noreply.github.com>
2025-08-05 03:48:27 +0100

    Refactor Let's Go Normal Rules (#11324)
    * Allow AVs should take happiness into account
    
    * Default happiness is already set in pokemon.ts
    
    * Default happiness in the validator
    
    natureModify only receives the set, not the actual Pokemon :/
    
    * Implement Let's Go Normal Rules
    
    * Fixes
    
    * Remove EV limit
    
    * Remove condition
    
    * Add EV Limit = 0
    
    Avoid "Pokemon has exactly 0 EVs - did you forget to EV it?"
    
    * Final fix
    
    * Happiness is fixed to 70
    
    * Revert changes
    
    * Rename Let's Go to LGPE
    
    * Re-add comment

--- sim/dex-formats.ts
+++ sim/dex-formats.ts
@@ -277,7 +277,7 @@ export class RuleTable extends Map<string, string> {
 		if (this.valueRules.get('evlimit') === 'Auto') {
 			this.evLimit = dex.gen > 2 ? 510 : null;
 			if (format.mod === 'gen7letsgo') {
-				this.evLimit = this.has('allowavs') ? null : 0;
+				this.evLimit = this.has('lgpenormalrules') ? 0 : null;
 			}
 			// Gen 6 hackmons also has a limit, which is currently implemented
 			// at the appropriate format.
