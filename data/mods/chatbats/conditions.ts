646e3ce8b6377ea896e7047dd79890b463e0ba8f
Emikatana <itsemi48@gmail.com>
2025-08-10 12:06:43 -0400

    fixing frostbite

--- data/mods/chatbats/conditions.ts
+++ data/mods/chatbats/conditions.ts
@@ -3,12 +3,14 @@ export const Conditions: import('../../../sim/dex-conditions').ModdedConditionDa
 	   name: 'frostbite',
 		effectType: 'Status',
 		onStart(target) {
-		    this.add('-message', `hi`);
-		    this.add('-status', target, 'frostbite');
+			this.add('-start', target, 'Frostbite', '[silent]');
+			this.add('-message', `${target.species.name} is inflicted with frostbite!`);
+		},
+		onSwitchIn(pokemon) {
+			this.add('-start', pokemon, 'Frostbite', '[silent]');
 		},
 		onResidualOrder: 10,
 		onResidual(pokemon) {
-			this.add('-message', `hi`);
 			this.damage(pokemon.baseMaxhp / 16);
 		},
 		onBasePower(basePower, source, target) {
