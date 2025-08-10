e2a44c2030b0d6de862e6cb7ce4ec89581fe3af8
Emikatana <itsemi48@gmail.com>
2025-08-10 13:28:00 -0400

    lint

--- data/mods/chatbats/conditions.ts
+++ data/mods/chatbats/conditions.ts
@@ -1,6 +1,6 @@
 export const Conditions: import('../../../sim/dex-conditions').ModdedConditionDataTable = {
 	frostbite: {
-	   name: 'frostbite',
+		name: 'frostbite',
 		effectType: 'Status',
 		onStart(target) {
 			this.add('-start', target, 'Frostbite', '[silent]');
@@ -14,7 +14,7 @@ export const Conditions: import('../../../sim/dex-conditions').ModdedConditionDa
 			this.damage(pokemon.baseMaxhp / 16);
 		},
 		onBasePower(basePower, source, target) {
-			return basePower/2;
+			return basePower / 2;
 		},
-	}
+	},
 };
