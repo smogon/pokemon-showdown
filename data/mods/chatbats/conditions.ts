d0bc648c17eb7242300d70b0cc4807cbb1e21d7d
Emikatana <itsemi48@gmail.com>
2025-08-10 11:29:14 -0400

    adding conditions file

---
+++ data/mods/chatbats/conditions.ts
@@ -0,0 +1,17 @@
+export const Conditions: import('../../../sim/dex-conditions').ModdedConditionDataTable = {
+frostbite: {
+    name: 'frostbite',
+	effectType: 'Status',
+	onStart(target) {
+	    this.add('-message', `hi`);
+	    this.add('-status', target, 'frostbite');
+	},
+	onResidualOrder: 10,
+	onResidual(pokemon) {
+		this.add('-message', `hi`);
+		this.damage(pokemon.baseMaxhp / 16);
+	},
+	onBasePower(basePower, source, target) {
+		return basePower/2;
+	},
+},
