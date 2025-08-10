d18e8c7b8d3e473c49714f5244fada5204582d84
Emikatana <itsemi48@gmail.com>
2025-08-10 11:38:10 -0400

    fixing

--- data/mods/chatbats/conditions.ts
+++ data/mods/chatbats/conditions.ts
@@ -1,17 +1,18 @@
 export const Conditions: import('../../../sim/dex-conditions').ModdedConditionDataTable = {
-frostbite: {
-    name: 'frostbite',
-	effectType: 'Status',
-	onStart(target) {
-	    this.add('-message', `hi`);
-	    this.add('-status', target, 'frostbite');
-	},
-	onResidualOrder: 10,
-	onResidual(pokemon) {
-		this.add('-message', `hi`);
-		this.damage(pokemon.baseMaxhp / 16);
-	},
-	onBasePower(basePower, source, target) {
-		return basePower/2;
-	},
-},
+	frostbite: {
+	    name: 'frostbite',
+		effectType: 'Status',
+		onStart(target) {
+		    this.add('-message', `hi`);
+		    this.add('-status', target, 'frostbite');
+		},
+		onResidualOrder: 10,
+		onResidual(pokemon) {
+			this.add('-message', `hi`);
+			this.damage(pokemon.baseMaxhp / 16);
+		},
+		onBasePower(basePower, source, target) {
+			return basePower/2;
+		},
+	}
+};
