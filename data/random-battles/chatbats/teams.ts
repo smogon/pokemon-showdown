6e505cf2ac8f8aa99fd1e1df1c6c1acc3bcbdfe2
Emikatana <itsemi48@gmail.com>
2025-08-10 13:30:57 -0400

    removing old comments

--- data/random-battles/chatbats/teams.ts
+++ data/random-battles/chatbats/teams.ts
@@ -907,8 +907,7 @@ export class RandomChatBatsTeams extends RandomTeams {
 			}
 			// change const to let when enforcing certain mons for testing
 			const species = this.sample(currentSpeciesPool);
-			// Code to enforce a mon on teams for testing
-			//if (pokemon.length === 1 || this.maxTeamSize === 1) species = 'hitmontop';
+
 			// let species = this.dex.species.get(this.sample(pokemonPool[baseSpecies]));
 
 			if (!species.exists) continue;
