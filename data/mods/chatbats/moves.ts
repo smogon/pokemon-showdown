e3d107a2a6bc86b977c50f30697fd11d2da86b23
Emikatana <itsemi48@gmail.com>
2025-08-11 13:57:22 -0400

    removing contact from Icicle Storm

--- data/mods/chatbats/moves.ts
+++ data/mods/chatbats/moves.ts
@@ -196,7 +196,7 @@ export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
 		name: "Icicle Storm",
 		pp: 15,
 		priority: 0,
-		flags: { contact: 1, protect: 1, mirror: 1, metronome: 1 },
+		flags: { protect: 1, mirror: 1, metronome: 1 },
 		onTryMove() {
 			this.attrLastMove('[still]');
 		},
