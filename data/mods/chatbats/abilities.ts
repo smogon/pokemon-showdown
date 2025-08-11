9c1ce6357988070c65ada777bf0dafa5aa3fa9bd
Emikatana <itsemi48@gmail.com>
2025-08-11 14:06:46 -0400

    Update data/mods/chatbats/abilities.ts
    Co-authored-by: André Bastos Dias <80102738+andrebastosdias@users.noreply.github.com>

--- data/mods/chatbats/abilities.ts
+++ data/mods/chatbats/abilities.ts
@@ -75,7 +75,7 @@ export const Abilities: import('../../../sim/dex-abilities').ModdedAbilityDataTa
 		onDamage(damage, target, source, effect) {
 			if (damage >= target.hp && effect) {
 				// Keep the Pokémon at 1 HP instead of fainting immediately
-				this.damage(target.hp - 1, target, source || target, effect);
+				this.damage(target.hp - 1, target, source, effect);
 				this.add('-message', `Illumise calls upon Volbeat for aid!`);
 				// Define new moves
 				const newMoves = ['dragondance', 'lunge', 'dragonhammer', 'earthquake'];
