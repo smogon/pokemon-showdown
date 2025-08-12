b75678b3b53894e6b8ad874a1daa7e2821d1cc68
Steve Baranko <stevencb22@gmail.com>
2025-08-12 14:11:52 -0400

    Add Enigma berry tests to bugbite.js; remove Pluck tests

--- test/sim/moves/bugbite.js
+++ test/sim/moves/bugbite.js
@@ -18,6 +18,14 @@ describe('Bug Bite', () => {
 		battle.makeChoices('move bugbite', 'move sleeptalk');
 		assert.notEqual(battle.p1.active[0].hp, battle.p1.active[0].maxhp);
 	});
+	it('should not steal and eat Enigma Berry', () => {
+		battle = common.createBattle([
+			[{ species: "combee", moves: ['bugbite'] }],
+			[{ species: "umbreon", item: 'enigmaberry', moves: ['sleeptalk'] }],
+		]);
+		battle.makeChoices('move bugbite', 'move sleeptalk');
+		assert.equal(battle.p2.active[0].hp, battle.p2.active[0].maxhp);
+	});
 });
 
 describe('Bug Bite [Gen 4]', () => {
@@ -32,5 +40,13 @@ describe('Bug Bite [Gen 4]', () => {
 		]);
 		battle.makeChoices('move bugbite', 'move sleeptalk');
 		assert.equal(battle.p1.active[0].hp, battle.p1.active[0].maxhp);
+	});
+	it('should steal and eat Enigma Berry', () => {
+		battle = common.gen(4).createBattle([
+			[{ species: "combee", level: 1, moves: ['bugbite'] }],
+			[{ species: "umbreon", item: 'enigmaberry', moves: ['sleeptalk'] }],
+		]);
+		battle.makeChoices('move bugbite', 'move sleeptalk');
+		assert.notEqual(battle.p2.active[0].hp, battle.p2.active[0].maxhp);
 	});
 });
