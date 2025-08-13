3a67660d9e4c21cacf29aaa51094cfa0fcae7677
André Bastos Dias <80102738+andrebastosdias@users.noreply.github.com>
2025-08-13 19:11:49 +0100

    Fix Trace message timing (#11282)
    Co-authored-by: Karthik99999 <bandagondak0217@gmail.com>

--- data/mods/gen4/abilities.ts
+++ data/mods/gen4/abilities.ts
@@ -534,9 +534,7 @@ export const Abilities: import('../../../sim/dex-abilities').ModdedAbilityDataTa
 			if (bannedAbilities.includes(target.ability)) {
 				return;
 			}
-			if (pokemon.setAbility(ability)) {
-				this.add('-ability', pokemon, ability, '[from] ability: Trace', `[of] ${target}`);
-			}
+			pokemon.setAbility(ability, target);
 		},
 		flags: { notrace: 1 },
 	},
