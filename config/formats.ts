4b3fa2eb24a6adee02dc4f627ec61821090a881b
Brendan Leahey <brendan_leahey@brown.edu>
2025-08-17 14:22:47 -0700

    Little Cup: Remove Torchic from LCUU Banlist

--- config/formats.ts
+++ config/formats.ts
@@ -367,7 +367,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		ruleset: ['[Gen 9] LC'],
 		banlist: [
 			'Chinchou', 'Diglett-Alola', 'Elekid', 'Foongus', 'Glimmet', 'Gothita', 'Grookey', 'Growlithe-Hisui', 'Mareanie', 'Mienfoo', 'Mudbray',
-			'Pawniard', 'Sandshrew-Alola', 'Shellder', 'Shellos', 'Shroodle', 'Snubbull', 'Stunky', 'Tinkatink', 'Toedscool', 'Torchic', 'Vullaby',
+			'Pawniard', 'Sandshrew-Alola', 'Shellder', 'Shellos', 'Shroodle', 'Snubbull', 'Stunky', 'Tinkatink', 'Toedscool', 'Vullaby',
 			'Wingull',
 			// LC UUBL
 			'Deerling', 'Minccino',
