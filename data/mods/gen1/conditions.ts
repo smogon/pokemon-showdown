76aa2749b6b72379f3c64a8f03d9cb123f11ab15
André Bastos Dias <80102738+andrebastosdias@users.noreply.github.com>
2025-08-20 23:23:01 +0100

    Maybe this wasn't related

--- data/mods/gen1/conditions.ts
+++ data/mods/gen1/conditions.ts
@@ -189,6 +189,8 @@ export const Conditions: import('../../../sim/dex-conditions').ModdedConditionDa
 		// (i.e. if the attacker switches out.)
 		// the full duration is tracked in partialtrappinglock
 		duration: 2,
+		// defender still takes PSN damage, etc
+		// TODO: research exact mechanics
 		onBeforeMovePriority: 9,
 		onBeforeMove(pokemon) {
 			this.add('cant', pokemon, 'partiallytrapped');
