19f9120b8cdfe0b62c2f5aee71731a69093c9cfe
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2025-08-29 00:19:40 -0600

    Update teams.ts

--- sim/teams.ts
+++ sim/teams.ts
@@ -634,8 +634,6 @@ export const Teams = new class Teams {
 			TeamGenerator = require(Dex.forFormat(format).dataDir + '/cg-teams').default;
 		} else if (mod === 'gen9ssb') {
 			TeamGenerator = require(`../data/mods/gen9ssb/random-teams`).default;
-		} else if (mod === 'ccapm2024') {
-			TeamGenerator = require(`../data/mods/ccapm2024/random-teams`).default;
 		} else if (mod === 'balls') {
 			TeamGenerator = require(`../data/mods/balls/random-teams`).default;
 		} else if (formatID.includes('gen9babyrandombattle')) {
