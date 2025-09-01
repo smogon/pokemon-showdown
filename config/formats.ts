de1a22250320f1d207f7d23a3bcdbc312e38ceaf
Cody Spratford <koreanpanda345@gmail.com>
2025-08-31 20:59:53 -0700

    Removing things that already exist

--- config/formats.ts
+++ config/formats.ts
@@ -25,7 +25,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		name: "[Gen 9] HELL Draft League",
 		mod: "gen9",
 		teraPreviewDefault: true,
-		ruleset: ['Team Preview', 'Standard NatDex', 'Evasion Clause', 'Sleep Clause Mod', 'Terastal Clause'],
+		ruleset: ['Team Preview', 'Standard NatDex', 'Terastal Clause'],
 	},
 	// S/V Singles
 	///////////////////////////////////////////////////////////////////
