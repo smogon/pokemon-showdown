cd253485ecec7d44c90ede040b3990367c725c93
livid washed <115855253+livid-washed@users.noreply.github.com>
2025-09-04 05:04:35 +1000

    Add September 2025 Randomized Format Spotlight (#11425)
    * Add September 2025 Randomized Format Spotlight
    
    * lol weird indent

---
+++ data/mods/gen5pokebilities/moves.ts
@@ -0,0 +1,17 @@
+export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
+	gastroacid: {
+		inherit: true,
+		condition: {
+			// Ability suppression implemented in Pokemon.ignoringAbility() within sim/pokemon.js
+			onStart(pokemon) {
+				this.add('-endability', pokemon);
+				this.singleEvent('End', pokemon.getAbility(), pokemon.abilityState, pokemon, pokemon, 'gastroacid');
+				if (pokemon.m.innates) {
+					for (const innate of pokemon.m.innates) {
+						pokemon.removeVolatile("ability" + innate);
+					}
+				}
+			},
+		},
+	},
+};
