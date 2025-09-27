78a8b41a2782df1bdda6392a81e20f802b0c4049
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2025-09-27 14:52:18 -0700

    Tyrantrum-Prime

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -491,6 +491,11 @@ export const FormatsData: import('../sim/dex-species').SpeciesFormatsDataTable =
 		tier: "HELL",
 		natDexTier: "HELL",
 	},
+	tyrantrumprime: {
+		isNonstandard: "Past",
+		tier: "HELL",
+		natDexTier: "HELL",
+	},
 	// End of Custom Pokemon Tier
 	bulbasaur: {
 		tier: "LC",
