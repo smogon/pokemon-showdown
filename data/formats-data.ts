b5c54691c3f59294415fbacc5ea8d54b2381708e
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2025-09-27 14:40:17 -0700

    Brambleghast-Prime

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -471,6 +471,11 @@ export const FormatsData: import('../sim/dex-species').SpeciesFormatsDataTable =
 		tier: "HELL",
 		natDexTier: "HELL",
 	},
+	brambleghastprime: {
+		isNonstandard: "Past",
+		tier: "HELL",
+		natDexTier: "HELL",
+	},
 	// End of Custom Pokemon Tier
 	bulbasaur: {
 		tier: "LC",
