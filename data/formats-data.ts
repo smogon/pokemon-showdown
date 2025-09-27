79001c8d0b11e2abb2ddc15bb28471dbdeff1421
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2025-09-27 15:16:49 -0700

    Delibird-Prime

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -531,6 +531,11 @@ export const FormatsData: import('../sim/dex-species').SpeciesFormatsDataTable =
 		tier: "HELL",
 		natDexTier: "HELL",
 	},
+	delibirdprime: {
+		isNonstandard: "Past",
+		tier: "HELL",
+		natDexTier: "HELL",
+	},
 	// End of Custom Pokemon Tier
 	bulbasaur: {
 		tier: "LC",
