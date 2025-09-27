bc86d8af74e16a81eb9651666c46f89aa521ce96
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2025-09-27 15:19:37 -0700

    Mandibuzz-Prime

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -536,6 +536,11 @@ export const FormatsData: import('../sim/dex-species').SpeciesFormatsDataTable =
 		tier: "HELL",
 		natDexTier: "HELL",
 	},
+	mandibuzzprime: {
+		isNonstandard: "Past",
+		tier: "HELL",
+		natDexTier: "HELL",
+	},
 	// End of Custom Pokemon Tier
 	bulbasaur: {
 		tier: "LC",
