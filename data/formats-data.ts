76d2ac6cb280e247aa81ae30d642640c88a618b6
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2025-09-27 15:22:06 -0700

    Regigigas-Prime

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -541,6 +541,11 @@ export const FormatsData: import('../sim/dex-species').SpeciesFormatsDataTable =
 		tier: "HELL",
 		natDexTier: "HELL",
 	},
+	regigigasprime: {
+		isNonstandard: "Past",
+		tier: "HELL",
+		natDexTier: "HELL",
+	},
 	// End of Custom Pokemon Tier
 	bulbasaur: {
 		tier: "LC",
