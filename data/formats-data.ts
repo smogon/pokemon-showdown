28c923a80018c563a9ecb8ed25f6e54555562388
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2025-09-27 14:24:36 -0700

    Revavroom-Prime

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -446,6 +446,11 @@ export const FormatsData: import('../sim/dex-species').SpeciesFormatsDataTable =
 		tier: "HELL",
 		natDexTier: "HELL",
 	},
+	revavroomprime: {
+		isNonstandard: "Past",
+		tier: "HELL",
+		natDexTier: "HELL",
+	},
 	// End of Custom Pokemon Tier
 	bulbasaur: {
 		tier: "LC",
