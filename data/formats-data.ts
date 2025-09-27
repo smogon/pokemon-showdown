dbb52b1eda4919198957d41ca7a4fb11422bb107
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2025-09-27 15:08:39 -0700

    Basculegion-Prime

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -516,6 +516,11 @@ export const FormatsData: import('../sim/dex-species').SpeciesFormatsDataTable =
 		tier: "HELL",
 		natDexTier: "HELL",
 	},
+	basculegionprime: {
+		isNonstandard: "Past",
+		tier: "HELL",
+		natDexTier: "HELL",
+	},
 	// End of Custom Pokemon Tier
 	bulbasaur: {
 		tier: "LC",
