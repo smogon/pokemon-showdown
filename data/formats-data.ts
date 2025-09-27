b6d70b0f02cb55e38435652731b979be000a3dcc
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2025-09-27 14:17:47 -0700

    Blaziken-Prime

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -436,6 +436,11 @@ export const FormatsData: import('../sim/dex-species').SpeciesFormatsDataTable =
 		tier: "HELL",
 		natDexTier: "HELL",
 	},
+	blazikenprime: {
+		isNonstandard: "Past",
+		tier: "HELL",
+		natDexTier: "HELL",
+	},
 	// End of Custom Pokemon Tier
 	bulbasaur: {
 		tier: "LC",
