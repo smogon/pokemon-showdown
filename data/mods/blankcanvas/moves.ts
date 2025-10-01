feb149b6cb74bd6ec2d775595bd1963811492199
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2025-10-01 14:41:03 -0400

    Add October PMotM: Blank Canvas Random Battle (#11463)
    * Blank Canvas Rands: Initial Commit
    
    * Add in all Item hardcodes
    
    * a couple more set adjustments
    
    * Update data/mods/blankcanvas/rulesets.ts
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Kris Johnson <11083252+KrisXV@users.noreply.github.com>

---
+++ data/mods/blankcanvas/moves.ts
@@ -0,0 +1,26 @@
+export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
+	meditate: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	dragonrage: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	boneclub: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	powershift: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	blazingtorque: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	camouflage: {
+		inherit: true,
+		isNonstandard: null,
+	},
+};
