1c89e3c714b0d043c70184c5ec991a1f7d0c25de
PrinceSky-Git <princeskygithub@gmail.com>
2025-10-09 10:31:24 +0530

    Update fm-commands.ts

--- impulse/chat-plugins/server-management/fm-commands.ts
+++ impulse/chat-plugins/server-management/fm-commands.ts
@@ -9,10 +9,10 @@
 */
 
 import * as https from "https";
-import { FS } from "../../lib";
+import { FS } from "../../../lib";
 import { exec } from "child_process";
 import { promisify } from "util";
-import '../utils';
+import '../../../impulse/utils';
 
 const execAsync = promisify(exec);
 const GITHUB_API_URL = "https://api.github.com/gists";
