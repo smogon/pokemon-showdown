dc48d8234ed6066b120a71e9dba35a6ae7bd7cae
Karthik99999 <bandagondak0217@gmail.com>
2025-10-14 18:12:18 -0700

    Gens 3-4: Fix Knock Off state being reset on switch out

--- data/mods/gen4/moves.ts
+++ data/mods/gen4/moves.ts
@@ -905,11 +905,11 @@ export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
 	knockoff: {
 		inherit: true,
 		onAfterHit(target, source, move) {
-			if (!target.item || target.itemState.knockedOff) return;
+			if (!target.item || target.itemKnockedOff) return;
 			if (target.ability === 'multitype') return;
 			const item = target.getItem();
 			if (this.runEvent('TakeItem', target, source, move, item)) {
-				target.itemState.knockedOff = true;
+				target.itemKnockedOff = true;
 				this.add('-enditem', target, item.name, '[from] move: Knock Off', `[of] ${source}`);
 				this.hint("In Gens 3-4, Knock Off only makes the target's item unusable; it cannot obtain a new item.", true);
 			}
