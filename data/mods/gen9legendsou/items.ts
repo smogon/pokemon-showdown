0b5443abc4d84c8ee2444ce90b060f161edde5c1
HoeenHero <HoeenCoder@users.noreply.github.com>
2025-10-22 09:46:37 -0400

    Z-A OU: Fix removal of megastones from mega greninja and slowbro

--- data/mods/gen9legendsou/items.ts
+++ data/mods/gen9legendsou/items.ts
@@ -2,14 +2,14 @@ export const Items: import('../../../sim/dex-items').ModdedItemDataTable = {
 	slowbronite: {
 		inherit: true,
 		onTakeItem(item, source) {
-			if (item.megaEvolves === source.baseSpecies.name) return false;
+			if (item.megaEvolves === source.baseSpecies.name || item.megaStone === source.baseSpecies.name) return false;
 			return true;
 		},
 	},
 	greninjite: {
 		inherit: true,
 		onTakeItem(item, source) {
-			if (item.megaEvolves === source.baseSpecies.name) return false;
+			if (item.megaEvolves === source.baseSpecies.name || item.megaStone === source.baseSpecies.name) return false;
 			return true;
 		},
 	},
