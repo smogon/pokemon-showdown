c65a7455dd56fbce5d59cfc35d2b2ded4e9f180d
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2025-10-22 12:08:18 -0600

    Z-A OU: Rework Pyroar and Greninja

--- data/mods/gen9legendsou/moves.ts
+++ data/mods/gen9legendsou/moves.ts
@@ -1,14 +1,4 @@
 export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
-	watershuriken: {
-		inherit: true,
-		basePowerCallback(pokemon, target, move) {
-			if (pokemon.species.name === 'Greninja-Mega' && pokemon.hasAbility('battlebond') &&
-				!pokemon.transformed) {
-				return move.basePower + 5;
-			}
-			return move.basePower;
-		},
-	},
 	gearup: {
 		inherit: true,
 		onHitSide(side, source, move) {
