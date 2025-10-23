8fde4bfe67a7966a42decaccb727c8d8f059858e
TurboRx <187360786+TurboRx@users.noreply.github.com>
2025-10-23 16:55:45 +0530

    server-poof: add 25+ new themed poof messages (Pokémon references, humor, QoL). Keep centered HTML and roomowner perms.

--- impulse/chat-plugins/misc/server-poof.ts
+++ impulse/chat-plugins/misc/server-poof.ts
@@ -25,6 +25,35 @@ const messages: string[] = [
 	`got rekt by Travis CI!`,
 	`exited life.exe.`,
 	`found a species called "friends" (whatever that means).`,
+	// New fun messages
+	`tripped over a wireless cable.`,
+	`ragequit due to hax.`,
+	`got lost in Verdanturf Tunnel without Flash.`,
+	`forgot to save and reset to Route 1.`,
+	`was whisked away by a Latios.`,
+	`used Teleport and couldn't find the way back.`,
+	`pressed Alt+F4 to increase FPS.`,
+	`fell asleep counting Mareep.`,
+	`slipped on a banana peel trap set by a Shiftry.`,
+	`got too comfy in the PC Box.`,
+	`went AFK to catch a shiny... still looking.`,
+	`encountered MissingNo. and evaporated.`,
+	`was KO'd by a level 1 Endeavor + Quick Attack.`,
+	`clicked the big red button.`,
+	`forgot their Exp. Share at home.`,
+	`got hugged by a Bewear a little too hard.`,
+	`used Self-Destruct IRL (bad idea).`,
+	`failed a Focus Blast and left in shame.`,
+	`got tangled in Substitute dolls.`,
+	`fell for the oldest Shedinja trick in the book.`,
+	`met a Wobbuffet in a dead-end corridor.`,
+	`ate too many RageCandyBars.`,
+	`was last seen surfing on a puddle.`,
+	`got distracted by a Poffin contest.`,
+	`forgot their HM buddy and can't cut grass.`,
+	`tried to Dynamax in a small room.`,
+	`was caught by a Master Ball and relocated.`,
+	`ran out of Repels in Victory Road.`,
 ];
 
 class PoofManager {
@@ -107,7 +136,7 @@ export const commands: Chat.ChatCommands = {
 			{cmd: "/poof on", desc: "Enable the use of /poof command. Requires: &."},
 			{cmd: "/poof off", desc: "Disable the use of the /poof command. Requires: &."},
 		];
-		const html = `<center><strong>Poof Commands:<br>Aliases: /d, /cpoof</strong></center><hr><ul style="list-style-type:none;padding-left:0;">` +
+		const html = `<center><strong>Poof Commands:<br>Aliases: /d, /cpoof</strong></center><hr><ul style=\"list-style-type:none;padding-left:0;\">` +
 			helpList.map(({cmd, desc}, i) =>
 				`<li><b>${cmd}</b> - ${desc}</li>${i < helpList.length - 1 ? '<hr>' : ''}`
 			).join('') +
