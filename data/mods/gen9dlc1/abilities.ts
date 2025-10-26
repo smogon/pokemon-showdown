b7ba7d1aefb69da058a7fd9f29f5e7e09d98da3e
André Bastos Dias <80102738+andrebastosdias@users.noreply.github.com>
2025-10-26 01:17:14 +0000

    Fix gen9dlc1 Paradox abilities

--- data/mods/gen9dlc1/abilities.ts
+++ data/mods/gen9dlc1/abilities.ts
@@ -12,129 +12,30 @@ export const Abilities: import('../../../sim/dex-abilities').ModdedAbilityDataTa
 		onWeatherChange(pokemon) {
 			// Protosynthesis is not affected by Utility Umbrella
 			if (this.field.isWeather('sunnyday')) {
-				pokemon.addVolatile('protosynthesis', pokemon, this.field.getWeather());
+				if (!this.effectState.bestStat) {
+					this.effectState.bestStat = pokemon.getBestStat(false, true);
+					this.add('-activate', pokemon, 'ability: Protosynthesis');
+					this.add('-start', pokemon, 'protosynthesis' + this.effectState.bestStat);
+				}
 				return;
 			}
-			if (!pokemon.volatiles['protosynthesis']?.fromBooster && this.field.weather !== 'sunnyday') {
+			if (!this.effectState.fromBooster && this.field.weather !== 'sunnyday') {
 				// Protosynthesis will not deactivite if Sun is suppressed, hence the direct ID check (isWeather respects suppression)
-				pokemon.removeVolatile('protosynthesis');
-			}
-			const item = pokemon.getItem();
-			if (!pokemon.volatiles['protosynthesis'] && pokemon.hasItem('boosterenergy') &&
-				pokemon.useItem(pokemon, this.effect)) {
-				pokemon.addVolatile('protosynthesis', pokemon, item);
-			}
-		},
-		// Implement as a condition so that it doesn't get suppressed by Neutralizing Gas
-		condition: {
-			noCopy: true,
-			onStart(pokemon, source, effect) {
-				if (effect?.name === 'Booster Energy') {
-					this.effectState.fromBooster = true;
-					this.add('-activate', pokemon, 'ability: Protosynthesis', '[fromitem]');
-				} else {
-					this.add('-activate', pokemon, 'ability: Protosynthesis');
-				}
-				this.effectState.bestStat = pokemon.getBestStat(false, true);
-				this.add('-start', pokemon, 'protosynthesis' + this.effectState.bestStat);
-			},
-			onModifyAtkPriority: 5,
-			onModifyAtk(atk, pokemon) {
-				if (this.effectState.bestStat !== 'atk') return;
-				this.debug('Protosynthesis atk boost');
-				return this.chainModify([5325, 4096]);
-			},
-			onModifyDefPriority: 6,
-			onModifyDef(def, pokemon) {
-				if (this.effectState.bestStat !== 'def') return;
-				this.debug('Protosynthesis def boost');
-				return this.chainModify([5325, 4096]);
-			},
-			onModifySpAPriority: 5,
-			onModifySpA(spa, pokemon) {
-				if (this.effectState.bestStat !== 'spa') return;
-				this.debug('Protosynthesis spa boost');
-				return this.chainModify([5325, 4096]);
-			},
-			onModifySpDPriority: 6,
-			onModifySpD(spd, pokemon) {
-				if (this.effectState.bestStat !== 'spd') return;
-				this.debug('Protosynthesis spd boost');
-				return this.chainModify([5325, 4096]);
-			},
-			onModifySpe(spe, pokemon) {
-				if (this.effectState.bestStat !== 'spe') return;
-				this.debug('Protosynthesis spe boost');
-				return this.chainModify(1.5);
-			},
-			onEnd(pokemon) {
+				delete this.effectState.bestStat;
 				this.add('-end', pokemon, 'Protosynthesis');
-			},
+			}
+			if (!this.effectState.bestStat && pokemon.hasItem('boosterenergy') &&
+				pokemon.useItem(pokemon, this.effect)) {
+				this.effectState.fromBooster = true;
+				this.effectState.bestStat = pokemon.getBestStat(false, true);
+				this.add('-start', pokemon, 'ability: Protosynthesis', '[fromitem]');
+				this.add('-start', pokemon, 'protosynthesis' + this.effectState.bestStat);
+			}
 		},
 		flags: { failroleplay: 1, noreceiver: 1, noentrain: 1, notrace: 1, failskillswap: 1, notransform: 1, cantsuppress: 1 },
 	},
 	quarkdrive: {
 		inherit: true,
-		onTerrainChange(pokemon) {
-			if (this.field.isTerrain('electricterrain')) {
-				pokemon.addVolatile('quarkdrive', pokemon, this.field.getTerrain());
-				return;
-			}
-			if (!pokemon.volatiles['quarkdrive']?.fromBooster) {
-				pokemon.removeVolatile('quarkdrive');
-			}
-			const item = pokemon.getItem();
-			if (!pokemon.volatiles['quarkdrive'] && pokemon.hasItem('boosterenergy') &&
-				pokemon.useItem(pokemon, this.effect)) {
-				pokemon.addVolatile('quarkdrive', pokemon, item);
-			}
-		},
-		// Implement as a condition so that it doesn't get suppressed by Neutralizing Gas
-		condition: {
-			noCopy: true,
-			onStart(pokemon, source, effect) {
-				if (effect?.name === 'Booster Energy') {
-					this.effectState.fromBooster = true;
-					this.add('-activate', pokemon, 'ability: Quark Drive', '[fromitem]');
-				} else {
-					this.add('-activate', pokemon, 'ability: Quark Drive');
-				}
-				this.effectState.bestStat = pokemon.getBestStat(false, true);
-				this.add('-start', pokemon, 'quarkdrive' + this.effectState.bestStat);
-			},
-			onModifyAtkPriority: 5,
-			onModifyAtk(atk, pokemon) {
-				if (this.effectState.bestStat !== 'atk') return;
-				this.debug('Quark Drive atk boost');
-				return this.chainModify([5325, 4096]);
-			},
-			onModifyDefPriority: 6,
-			onModifyDef(def, pokemon) {
-				if (this.effectState.bestStat !== 'def') return;
-				this.debug('Quark Drive def boost');
-				return this.chainModify([5325, 4096]);
-			},
-			onModifySpAPriority: 5,
-			onModifySpA(spa, pokemon) {
-				if (this.effectState.bestStat !== 'spa') return;
-				this.debug('Quark Drive spa boost');
-				return this.chainModify([5325, 4096]);
-			},
-			onModifySpDPriority: 6,
-			onModifySpD(spd, pokemon) {
-				if (this.effectState.bestStat !== 'spd') return;
-				this.debug('Quark Drive spd boost');
-				return this.chainModify([5325, 4096]);
-			},
-			onModifySpe(spe, pokemon) {
-				if (this.effectState.bestStat !== 'spe') return;
-				this.debug('Quark Drive spe boost');
-				return this.chainModify(1.5);
-			},
-			onEnd(pokemon) {
-				this.add('-end', pokemon, 'Quark Drive');
-			},
-		},
 		flags: { failroleplay: 1, noreceiver: 1, noentrain: 1, notrace: 1, failskillswap: 1, notransform: 1, cantsuppress: 1 },
 	},
 };
