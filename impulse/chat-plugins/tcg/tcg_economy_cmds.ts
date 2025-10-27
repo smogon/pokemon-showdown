a14afd6a364f26c9f262095f4efd06b5aa1a62cd
musaddiknpm <musaddiknpm@gmail.com>
2025-10-27 12:11:19 +0530

    Change PACKS_IN_SHOP from string to number

--- impulse/chat-plugins/tcg/tcg_economy_cmds.ts
+++ impulse/chat-plugins/tcg/tcg_economy_cmds.ts
@@ -9,7 +9,7 @@ import { generateThemedTable } from '../../utils';
 import { tcgCardsCollection, userCollectionsCollection, userProfilesCollection, userPacksCollection } from './tcg_collections';
 
 const PACK_COST = 0;
-const PACKS_IN_SHOP = '20';
+const PACKS_IN_SHOP = 20;
 
 export const economyCommands: ChatCommands = {
 	async shop(target, room, user) {
