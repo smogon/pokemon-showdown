a88264a4e2dfad1e94e24d1a0822a16ec7afc7db
musaddiknpm <musaddiknpm@gmail.com>
2025-10-28 09:14:17 +0530

    Add database types and collections for clans

---
+++ impulse/chat-plugins/clans/database.ts
@@ -0,0 +1,21 @@
+import { ImpulseDB } from '../../impulse-db';
+import type { Document } from 'mongodb';
+import type {
+	Clan,
+	UserClanInfo,
+	ClanLog,
+	ClanPointsLogEntry,
+	ClanBankLogEntry,
+} from './interface';
+
+export type ClanDoc = Omit<Clan, 'id'> & { _id: ID } & Document;
+export type UserClanDoc = UserClanInfo & { _id: ID } & Document;
+export type ClanLogDoc = ClanLog & { clanId: ID } & Document;
+export type ClanPointsLogDoc = ClanPointsLogEntry & { clanId: ID } & Document;
+export type ClanBankLogDoc = ClanBankLogEntry & { clanId: ID } & Document;
+
+export const Clans = ImpulseDB<ClanDoc>('clans');
+export const UserClans = ImpulseDB<UserClanDoc>('userclans');
+export const ClanLogs = ImpulseDB<ClanLogDoc>('clanlogs');
+export const ClanPointsLogs = ImpulseDB<ClanPointsLogDoc>('clanpointslsogs');
+export const ClanBankLogs = ImpulseDB<ClanBankLogDoc>('clanbanklogs');
