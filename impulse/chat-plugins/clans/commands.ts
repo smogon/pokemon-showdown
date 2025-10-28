5e814c98e3e7216a6722d4819f9d16fe0d7089ab
musaddiknpm <musaddiknpm@gmail.com>
2025-10-28 09:42:20 +0530

    Refactor clan permissions and notifications
    Updated clan permissions to use 'canSetMotd' instead of 'canSetMotw'. Added a new function 'hasClanPermission' to check user permissions within a clan. Improved popup notifications for clan creation and deletion.

--- impulse/chat-plugins/clans/commands.ts
+++ impulse/chat-plugins/clans/commands.ts
@@ -8,10 +8,13 @@ import {
 	UserClanDoc,
 } from './database';
 import type {
+	Clan,
 	ClanPermissions,
 	CustomClanRank,
 	ClanStats,
 } from './interface';
+import { generateThemedTable } from
+	'../../utils.ts';
 
 const ALL_PERMISSIONS: ClanPermissions = {
 	canInvite: true,
@@ -22,7 +25,7 @@ const ALL_PERMISSIONS: ClanPermissions = {
 	canEditDesc: true,
 	canEditIcon: true,
 	canEditTag: true,
-	canSetMotw: true,
+	canSetMotd: true,
 	canManageChat: true,
 	canBankWithdraw: true,
 	canManageAllies: true,
@@ -50,7 +53,7 @@ const DEFAULT_RANKS: { [rankId: string]: CustomClanRank } = {
 			canPromote: true,
 			canDemote: true,
 			canEditDesc: true,
-			canSetMotw: true,
+			canSetMotd: true,
 			canManageChat: true,
 			canManageAllies: true,
 			canManageRivals: true,
@@ -79,6 +82,18 @@ const DEFAULT_STATS: ClanStats = {
 	eventWins: 0,
 	totalPointsEarned: 0,
 };
+
+function hasClanPermission(clan: Clan, userId: ID, permission: keyof ClanPermissions): boolean {
+	if (clan.owner === userId) return true;
+
+	const memberData = clan.members[userId];
+	if (!memberData) return false;
+
+	const rank = clan.ranks[memberData.rank];
+	if (!rank) return false;
+
+	return !!rank.permissions[permission];
+}
 
 export const commands: Chat.ChatCommands = {
 	clan: {
@@ -192,10 +207,13 @@ export const commands: Chat.ChatCommands = {
 
 				if (ownerUser.connected) {
 					ownerUser.joinRoom(newRoom.roomid);
-					// Send popup notification to the owner
-					ownerUser.popup(`|html|${user.name} has created the clan <b>${clanName}</b> for you! Your clan room is <a href="/${newRoom.roomid}">#${newRoom.roomid}</a>, and you are the Room Owner (#).`);
+					const popupMessage = `|html|<div class="infobox"><div class="infobox-message">${user.name} has created the clan <b>${clanName}</b> for you! You are the Room Owner (#).</div><br /><center><button class="button" name="join" value="${newRoom.roomid}">Go to Clan Room: #${newRoom.roomid}</button></center></div>`;
+					ownerUser.popup(popupMessage);
 				}
+
+				this.privateGlobalModAction(`${user.name} created the clan ${clanName} (${clanId}) for owner ${ownerId}.`);
 				this.sendReply(`Clan "${clanName}" has been successfully created! Owner: ${ownerId}.`);
+				this.sendReply(`Persistent chatroom: #${chatRoomId} created/updated. Owner was set as room owner (#).`);
 			} catch (e) {
 				this.errorReply("An error occurred while creating the clan.");
 				await Clans.deleteOne({ _id: clanId });
@@ -216,36 +234,248 @@ export const commands: Chat.ChatCommands = {
 			if (!clan) {
 				return this.errorReply(`Clan '${clanId}' not found.`);
 			}
-			
+
 			const ownerId = clan.owner;
 			const ownerUser = Users.getExact(ownerId);
 			const chatRoomId = clan.chatRoom;
 			const chatRoom = Rooms.get(chatRoomId);
 
 			try {
-				// 1. Delete the chatroom
 				if (chatRoom) {
 					Rooms.global.deregisterChatRoom(chatRoom.roomid);
 					Rooms.global.delistChatRoom(chatRoom.roomid);
 					chatRoom.destroy();
 				}
 
-				// 2. Delete all clan data from DB
 				await Clans.deleteOne({ _id: clanId });
 				await UserClans.updateMany({ memberOf: clanId }, { $unset: { memberOf: 1 } });
 				await ClanLogs.deleteMany({ clanId: clanId });
 				await ClanPointsLogs.deleteMany({ clanId: clanId });
 				await ClanBankLogs.deleteMany({ clanId: clanId });
 
-				// 3. Send popup notification to the owner
 				if (ownerUser?.connected) {
 					ownerUser.popup(`|html|Your clan <b>${clan.name}</b> was deleted by ${user.name}. All related data and the chatroom (#${chatRoomId}) have been removed.`);
 				}
+
+				this.privateGlobalModAction(`${user.name} deleted the clan ${clan.name} (${clanId}).`);
 				this.sendReply(`Clan ${clan.name} (${clanId}) has been successfully deleted.`);
 			} catch (e) {
 				this.errorReply("An error occurred while deleting the clan.");
 				Monitor.crashlog(e as Error, "Clan deletion command");
 			}
+		},
+
+		async join(target, room, user) {
+			this.checkChat();
+			if (!user.named) return this.errorReply("You must be logged in to join a clan.");
+
+			const clanId = toID(target);
+			const userId = user.id;
+
+			if (!clanId) return this.errorReply("Specify the ID of the clan you wish to join.");
+
+			const [clan, userClanInfo] = await Promise.all([
+				Clans.findOne({ _id: clanId }),
+				UserClans.findOne({ _id: userId }),
+			]);
+
+			if (!clan) return this.errorReply(`Clan '${clanId}' not found.`);
+			if (userClanInfo?.memberOf) return this.errorReply(`You are already a member of the clan '${userClanInfo.memberOf}'.`);
+
+			const isInvited = clan.invites.some(invite => invite.userid === userId);
+
+			if (clan.inviteOnly && !isInvited) {
+				return this.errorReply(`The clan '${clan.name}' is invite-only. You must be invited to join.`);
+			}
+
+			if (!clan.inviteOnly && userClanInfo?.invites?.includes(clanId)) {
+				await UserClans.updateOne({ _id: userId }, { $pull: { invites: clanId } });
+			}
+
+			await Clans.updateOne(
+				{ _id: clanId },
+				{ $pull: { invites: { userid: userId } } }
+			);
+
+			await Clans.updateOne(
+				{ _id: clanId },
+				{
+					$set: {
+						[`members.${userId}`]: {
+							rank: 'member' as ID,
+							joinDate: Date.now(),
+							totalPointsContributed: 0,
+						},
+					},
+				}
+			);
+
+			await UserClans.upsert({ _id: userId }, {
+				$set: { memberOf: clanId },
+				$pull: { invites: clanId },
+			});
+
+			await ClanLogs.insertOne({
+				clanId: clanId,
+				timestamp: Date.now(),
+				actor: userId,
+				action: 'JOIN',
+				target: userId,
+				note: `User joined the clan.`,
+			});
+
+			const clanRoom = Rooms.get(clan.chatRoom);
+			if (clanRoom) {
+				clanRoom.auth.set(userId, '+');
+				clanRoom.saveSettings();
+
+				user.joinRoom(clanRoom.roomid, this.connection);
+				clanRoom.add(`|html|<div class="infobox">${user.name} joined the clan and was granted Room Voice.</div>`).update();
+			}
+
+			this.sendReply(`You have successfully joined the clan '${clan.name}'!`);
+			if (clanRoom) this.sendReply(`You have been automatically joined to the clan chatroom: #${clanRoom.roomid}`);
+		},
+
+		async invite(target, room, user) {
+			this.checkChat();
+			if (!user.named) return this.errorReply("You must be logged in to send invites.");
+
+			const targetId = toID(target);
+			const inviterId = user.id;
+
+			if (!targetId) return this.errorReply("Specify the user you wish to invite.");
+			if (targetId === inviterId) return this.errorReply("You cannot invite yourself.");
+
+			const [inviterClanInfo, targetClanInfo] = await Promise.all([
+				UserClans.findOne({ _id: inviterId }),
+				UserClans.findOne({ _id: targetId }),
+			]);
+
+			const clanId = inviterClanInfo?.memberOf;
+			if (!clanId) return this.errorReply("You are not currently a member of any clan.");
+
+			if (targetClanInfo?.memberOf) {
+				return this.errorReply(`'${targetId}' is already a member of the clan '${targetClanInfo.memberOf}'.`);
+			}
+
+			const clan = await Clans.findOne({ _id: clanId });
+			if (!clan) return this.errorReply(`Error: Your clan '${clanId}' was not found in the database.`);
+
+			if (!hasClanPermission(clan, inviterId, 'canInvite')) {
+				return this.errorReply(`Your rank (${clan.members[inviterId]?.rank}) does not have permission to invite users.`);
+			}
+
+			const targetUser = Users.getExact(targetId);
+
+			if (clan.invites.some(invite => invite.userid === targetId)) {
+				return this.errorReply(`'${targetId}' has already been invited to ${clan.name}.`);
+			}
+
+			const newInvite = {
+				userid: targetId,
+				actor: inviterId,
+				timestamp: Date.now(),
+			};
+
+			await Clans.updateOne(
+				{ _id: clanId },
+				{ $push: { invites: newInvite } }
+			);
+
+			await UserClans.upsert(
+				{ _id: targetId },
+				{ $addToSet: { invites: clanId } }
+			);
+
+			await ClanLogs.insertOne({
+				clanId: clanId,
+				timestamp: Date.now(),
+				actor: inviterId,
+				action: 'INVITE',
+				target: targetId,
+				note: `Invited user to the clan.`,
+			});
+
+			this.sendReply(`You invited '${targetId}' to join ${clan.name}.`);
+
+			if (targetUser?.connected) {
+				const buttonHtml = `<button class="button" name="send" value="/clan join ${clanId}">Join ${clan.name}</button>`;
+				targetUser.popup(`|html|<div class="infobox"><div class="infobox-message">You have been invited to join the clan <b>${clan.name}</b> by ${user.name}.</div><br /><center>${buttonHtml}</center></div>`);
+			} else {
+				this.sendReply(`'${targetId}' is offline and will see the invite when they log in.`);
+			}
+
+			const clanRoom = Rooms.get(clan.chatRoom);
+			if (clanRoom) {
+				clanRoom.add(`|c|${user.getIdentity(clanRoom)}|/log ${user.name} invited ${targetId} to the clan.`).update();
+			}
+		},
+
+		async invites(target, room, user) {
+			this.checkChat();
+			if (!user.named) return this.errorReply("You must be logged in to check invites.");
+
+			const userId = user.id;
+
+			const userClanInfo = await UserClans.findOne({ _id: userId });
+			const memberOfClanId = userClanInfo?.memberOf;
+
+			let output = '';
+			const dataRows: string[][] = [];
+			
+			const receivedInvites = userClanInfo?.invites || [];
+
+			if (receivedInvites.length) {
+				const invitedClans = await Clans.find({ _id: { $in: receivedInvites } });
+				
+				const headerRow = ['Clan', 'ID', 'Action'];
+				const title = 'Your Pending Clan Invites';
+				
+				invitedClans.forEach(clan => {
+					dataRows.push([
+						clan.name,
+						clan._id,
+						`<button class="button" name="send" value="/clan join ${clan._id}">Accept</button>`
+					]);
+				});
+				
+				output += generateThemedTable(title, headerRow, dataRows);
+				output += '<hr />';
+			} else {
+				output += `<div class="infobox">You have no pending clan invitations.</div><hr />`;
+			}
+
+			if (memberOfClanId) {
+				const clan = await Clans.findOne({ _id: memberOfClanId });
+				if (clan && hasClanPermission(clan, userId, 'canInvite')) {
+					const sentInvites = clan.invites;
+
+					if (sentInvites.length) {
+						const sentDataRows: string[][] = [];
+						const sentHeaderRow = ['User', 'Invited By', 'Date'];
+						const sentTitle = `Invites Sent by ${clan.name}`;
+						
+						sentInvites.forEach(invite => {
+							sentDataRows.push([
+								invite.userid,
+								invite.actor,
+								Utils.to(new Date(invite.timestamp), {date: true, time: true})
+							]);
+						});
+						
+						output += generateThemedTable(sentTitle, sentHeaderRow, sentDataRows);
+					} else {
+						output += `<div class="infobox">${clan.name} has no pending outgoing invitations.</div>`;
+					}
+				}
+			}
+
+			if (!output) {
+				return this.errorReply("You are not currently a member of a clan and have no pending invites.");
+			}
+			
+			this.sendReply(`|html|${output}`);
 		},
 	},
 };
