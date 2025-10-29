6bb4946acdb8d02154f607aeba9add0f23fb1b28
musaddiknpm <musaddiknpm@gmail.com>
2025-10-29 09:53:49 +0530

    Update chat.ts

--- server/chat.ts
+++ server/chat.ts
@@ -720,7 +720,7 @@ export class CommandContext extends MessageContext {
 			}
 			Chat.PrivateMessages.send(message, this.user, this.pmTarget);
 		} else if (this.room) {
-			this.room.add(`|c|${this.user.getIdentity(this.room)}|<br>${message}`);
+			this.room.add(`|html||c|${this.user.getIdentity(this.room)}|<br>${message}`);
 			this.room.game?.onLogMessage?.(message, this.user);
 		} else {
 			this.connection.popup(`Your message could not be sent:\n\n${message}\n\nIt needs to be sent to a user or room.`);
