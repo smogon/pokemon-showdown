215a1dcca99d60bfa11f6ab0d45a721fc60dac31
musaddiknpm <musaddiknpm@gmail.com>
2025-10-30 10:17:31 +0530

    Include clan wars

--- impulse/chat-plugins/clans/database.ts
+++ impulse/chat-plugins/clans/database.ts
@@ -5,6 +5,8 @@ import type {
 	UserClanInfo,
 	ClanLog,
 	ClanPointsLogEntry,
+	ClanBattleLogEntry, // Added
+	ClanWar, // Added
 } from './interface';
 
 export type ClanDoc = Omit<Clan, 'id'> & { _id: ID } & Document;
@@ -12,9 +14,13 @@ export type UserClanDoc = UserClanInfo & { _id: ID } & Document;
 export type ClanLogDoc = ClanLog & { clanId: ID } & Document;
 export type ClanPointsLogDoc = ClanPointsLogEntry & { clanId: ID } & Document;
 export type ClanBanDoc = { _id: ID, banned: boolean } & Document;
+export type ClanBattleLogDoc = ClanBattleLogEntry & Document; // Added
+export type ClanWarDoc = ClanWar & Document; // Added
 
 export const Clans = ImpulseDB<ClanDoc>('clans');
 export const UserClans = ImpulseDB<UserClanDoc>('userclans');
 export const ClanLogs = ImpulseDB<ClanLogDoc>('clanlogs');
 export const ClanPointsLogs = ImpulseDB<ClanPointsLogDoc>('clanpointslogs');
 export const ClanBans = ImpulseDB<ClanBanDoc>('clanbans');
+export const ClanBattleLogs = ImpulseDB<ClanBattleLogDoc>('clanbattlelogs'); // Added
+export const ClanWars = ImpulseDB<ClanWarDoc>('clanwars'); // Added
