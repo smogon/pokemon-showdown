b3cf76ddc98d762773f48fbe46ba7040bb7dbf49
André Bastos Dias <80102738+andrebastosdias@users.noreply.github.com>
2025-11-06 19:26:51 +0000

    Fix data test

--- test/sim/data.js
+++ test/sim/data.js
@@ -403,7 +403,7 @@ describe('Dex data', () => {
 	species['gen9legends'] = 231;
 	// Mega (65) + Vivillon (2) + Floette (1) + Meowstic (1) + Aegislash (1) + Pumpkaboo (3) + Gourgeist (3) +
 	// Zygarde (2) + Alola (1) + Galar (4)
-	formes['gen9legends'] = 63 + 2 + 1 + 1 + 1 + 3 + 3 + 2 + 1 + 4;
+	formes['gen9legends'] = 65 + 2 + 1 + 1 + 1 + 3 + 3 + 2 + 1 + 4;
 
 	for (const mod of ['gen7letsgo', 'gen8bdsp', 'gen8legends', 'gen9legends']) {
 		it(`${mod} should have ${species[mod]} species and ${formes[mod]} formes`, () => {
