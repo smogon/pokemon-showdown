02f89cd6f45a3de7db8892af523931bf86a89acb
André Bastos Dias <andrebastosdias@gmail.com>
2025-11-19 11:21:02 +0000

    Revert change

--- data/mods/gen4/conditions.ts
+++ data/mods/gen4/conditions.ts
@@ -123,7 +123,7 @@ export const Conditions: import('../../../sim/dex-conditions').ModdedConditionDa
 	choicelock: {
 		inherit: true,
 		onStart(pokemon) {
-			if (!pokemon.lastMove || !pokemon.hasMove(pokemon.lastMove.id)) return false;
+			if (!pokemon.lastMove) return false;
 			this.effectState.move = pokemon.lastMove.id;
 		},
 	},
