6a622bb02da9d344f68ac7e4b0d9c729c5ac12d8
PrinceSky-Git <princeskygithub@gmail.com>
2025-11-20 15:58:51 +0530

    Create trainers.ts

---
+++ impulse/chat-plugins/rpg-wip/trainers.ts
@@ -0,0 +1,15 @@
+/*
+* Pokemon Showdown
+* RPG Trainers Data
+*/
+
+import type { TrainerSpec } from './interface';
+
+export const TRAINER_DATABASE: Record<string, TrainerSpec> = {
+    // Add Trainers here
+};
+
+// Map Trainers to Locations (e.g., 'route1': ['youngster1'])
+export const TRAINER_LOCATIONS: Record<string, string[]> = {
+    
+};
