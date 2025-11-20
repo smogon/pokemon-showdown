65440f47d4ba9ed682c0dbf1232383bd27a2baad
PrinceSky-Git <princeskygithub@gmail.com>
2025-11-20 16:23:30 +0530

    Update trainers.ts

--- impulse/chat-plugins/rpg-wip/trainers.ts
+++ impulse/chat-plugins/rpg-wip/trainers.ts
@@ -1,15 +1,21 @@
-/*
-* Pokemon Showdown
-* RPG Trainers Data
-*/
-
 import type { TrainerSpec } from './interface';
 
 export const TRAINER_DATABASE: Record<string, TrainerSpec> = {
-    // Add Trainers here
+    'youngster_joey': {
+        name: 'Youngster Joey',
+        money: 200,
+        party: [
+            { species: 'rattata', level: 5, moves: ['tackle', 'tailwhip'] }
+        ],
+        dialogue: {
+            start: "My Rattata is in the top percentage of Rattata!",
+            win: "I'll never give up!",
+            lose: "You're tough!"
+        }
+    }
 };
 
-// Map Trainers to Locations (e.g., 'route1': ['youngster1'])
+// Map him to the location ID defined in locations.ts
 export const TRAINER_LOCATIONS: Record<string, string[]> = {
-    
+    'grassypath': ['youngster_joey']
 };
