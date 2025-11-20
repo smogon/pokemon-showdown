09515a52af75396681c020d42a33e452a5b206e5
PrinceSky-Git <princeskygithub@gmail.com>
2025-11-20 15:58:27 +0530

    Add initial NPCs data structure in npcs.ts

---
+++ impulse/chat-plugins/rpg-wip/npcs.ts
@@ -0,0 +1,11 @@
+/*
+* Pokemon Showdown
+* RPG NPCs Data
+*/
+
+import type { NPCData } from './interface';
+
+export const NPC_DATABASE: Record<string, NPCData> = {
+    // Add NPCs here
+    // 'guide': { id: 'guide', name: 'Guide', location: 'start_room', dialogue: 'Welcome!' }
+};
