e2088ad41e9beecc1e06ebb30c3811521da65a5a
PrinceSky-Git <princeskygithub@gmail.com>
2025-11-20 16:36:32 +0530

    Update badges.ts

--- impulse/chat-plugins/rpg-wip/badges.ts
+++ impulse/chat-plugins/rpg-wip/badges.ts
@@ -15,7 +15,7 @@ export interface BadgeInfo {
  */
 export const BADGES: BadgeInfo[] = [
     {
-        gymLeaderId: 'youngster_joey', // Defeating him awards this badge
+        gymLeaderId: 'youngsterjoey', // Defeating him awards this badge
         badgeName: 'Rat Badge',
         order: 1,
         description: 'Awarded for defeating the Top Percentage Rattata.'
