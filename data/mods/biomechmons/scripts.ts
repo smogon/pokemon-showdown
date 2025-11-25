2b8c3b87c0070d276a8bf6fd3baab1b9dfe4e557
HiZo <96159984+HisuianZoroark@users.noreply.github.com>
2025-11-24 21:43:28 -0500

    AAAA I FORGOT ABOUT THIS

--- data/mods/biomechmons/scripts.ts
+++ data/mods/biomechmons/scripts.ts
@@ -261,6 +261,93 @@ export const Scripts: ModdedBattleScriptsData = {
 			}
 			return true;
 		},
+
+		// TODO
+		eatItem(force, source, sourceEffect) {
+			console.log(sourceEffect);
+			if (!this.item) return false;
+			if ((!this.hp && this.item !== 'jabocaberry' && this.item !== 'rowapberry') || !this.isActive) return false;
+
+			if (!sourceEffect && this.battle.effect) sourceEffect = this.battle.effect;
+			if (!source && this.battle.event?.target) source = this.battle.event.target;
+			const item = this.getItem();
+			if (sourceEffect?.effectType === 'Item' && this.item !== sourceEffect.id && source === this) {
+				// if an item is telling us to eat it but we aren't holding it, we probably shouldn't eat what we are holding
+				return false;
+			}
+			if (
+				this.battle.runEvent('UseItem', this, null, null, item) &&
+				(force || this.battle.runEvent('TryEatItem', this, null, null, item))
+			) {
+				this.battle.add('-enditem', this, item, '[eat]');
+
+				this.battle.singleEvent('Eat', item, this.itemState, this, source, sourceEffect);
+				this.battle.runEvent('EatItem', this, source, sourceEffect, item);
+
+				if (RESTORATIVE_BERRIES.has(item.id)) {
+					switch (this.pendingStaleness) {
+						case 'internal':
+							if (this.staleness !== 'external') this.staleness = 'internal';
+							break;
+						case 'external':
+							this.staleness = 'external';
+							break;
+					}
+					this.pendingStaleness = undefined;
+				}
+
+				this.lastItem = this.item;
+				this.item = '';
+				this.battle.clearEffectState(this.itemState);
+				this.usedItemThisTurn = true;
+				this.ateBerry = true;
+				this.battle.runEvent('AfterUseItem', this, null, null, item);
+				return true;
+			}
+			return false;
+		},
+
+		// TODO
+		useItem(source, sourceEffect) {
+			console.log(sourceEffect)
+			if ((!this.hp && !this.getItem().isGem) || !this.isActive) return false;
+			if (!this.item) return false;
+
+			if (!sourceEffect && this.battle.effect) sourceEffect = this.battle.effect;
+			if (!source && this.battle.event?.target) source = this.battle.event.target;
+			const item = this.getItem();
+			if (sourceEffect?.effectType === 'Item' && this.item !== sourceEffect.id && source === this) {
+				// if an item is telling us to eat it but we aren't holding it, we probably shouldn't eat what we are holding
+				return false;
+			}
+			if (this.battle.runEvent('UseItem', this, null, null, item)) {
+				switch (item.id) {
+					case 'redcard':
+						this.battle.add('-enditem', this, item, `[of] ${source}`);
+						break;
+					default:
+						if (item.isGem) {
+							this.battle.add('-enditem', this, item, '[from] gem');
+						} else {
+							this.battle.add('-enditem', this, item);
+						}
+						break;
+				}
+				if (item.boosts) {
+					this.battle.boost(item.boosts, this, source, item);
+				}
+
+				this.battle.singleEvent('Use', item, this.itemState, this, source, sourceEffect);
+
+				this.lastItem = this.item;
+				this.item = '';
+				this.battle.clearEffectState(this.itemState);
+				this.usedItemThisTurn = true;
+				this.battle.runEvent('AfterUseItem', this, null, null, item);
+				return true;
+			}
+			return false;
+		},
 	},
 	field: {
 		suppressingWeather() {
