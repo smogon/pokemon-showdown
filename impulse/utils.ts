51e2542f159109c4b3d295f7d0c011f4c31ce0a8
Clark Jones <clarkj338@gmail.com>
2025-11-28 23:10:48 +0530

    Update utils.ts

--- impulse/utils.ts
+++ impulse/utils.ts
@@ -3,7 +3,7 @@
 * Utility Module
 */
 
-export const generateThemedTable = (title: string, headerRow: string[], dataRows: string[][]): string => {
+export const Table = (title: string, headerRow: string[], dataRows: string[][]): string => {
 	let output = `<div class="themed-table-container" style="max-width: 100%; max-height: 380px; overflow-y: auto;">`;
 	output += `<h3 class="themed-table-title">${title}</h3>`;
 	output += `<table class="themed-table" style="width: 100%; border-collapse: collapse;">`;
