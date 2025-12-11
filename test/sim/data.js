727fc3f1357246ef6fd4051fa6f97a158ee8e0c6
André Bastos Dias <andrebastosdias@gmail.com>
2025-12-11 15:44:20 +0000

    Add final number of species

--- test/sim/data.js
+++ test/sim/data.js
@@ -400,7 +400,7 @@ describe('Dex data', () => {
 	// Vulpix (1) + Ninetales (1) + Wormadam (2) + Cherrim (1) + Rotom (5) + Origin (3) + Arceus (17) +
 	// Shaymin (1) + Therian (4) + Hisui (16) + Basculin (1) + Basculegion (1)
 	formes['gen8legends'] = 1 + 1 + 2 + 1 + 5 + 3 + 17 + 1 + 4 + 16 + 1 + 1;
-	species['gen9legends'] = 232;
+	species['gen9legends'] = 364;
 	// Mega (68) + Vivillon (2) + Floette (1) + Meowstic (1) + Aegislash (1) + Pumpkaboo (3) + Gourgeist (3) +
 	// Zygarde (2) + Alola (1) + Galar (4)
 	formes['gen9legends'] = 68 + 2 + 1 + 1 + 1 + 3 + 3 + 2 + 1 + 4;
