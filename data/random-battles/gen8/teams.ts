7a5ada2ddbff8d47af0c5acd3e9f4af416617eed
adrivrie <adriaan.de.vries@hotmail.com>
2025-12-12 16:28:18 +0100

    Random Battles: Prevent Magearna-Mega from showing up (#11615)

--- data/random-battles/gen8/teams.ts
+++ data/random-battles/gen8/teams.ts
@@ -2156,10 +2156,11 @@ export class RandomGen8Teams {
 		if (species.name.endsWith('-Gmax')) return species.name.slice(0, -5);
 
 		// Consolidate mostly-cosmetic formes, at least for the purposes of Random Battles
-		if (['Magearna', 'Polteageist', 'Zarude'].includes(species.baseSpecies)) {
+		if (['Polteageist', 'Zarude'].includes(species.baseSpecies)) {
 			return this.sample([species.name].concat(species.otherFormes!));
 		}
 		if (species.baseSpecies === 'Basculin') return 'Basculin' + this.sample(['', '-Blue-Striped']);
+		if (species.baseSpecies === 'Magearna') return 'Magearna' + this.sample(['', '-Original']);
 		if (species.baseSpecies === 'Keldeo' && this.gen <= 7) return 'Keldeo' + this.sample(['', '-Resolute']);
 		if (species.baseSpecies === 'Pikachu' && this.dex.currentMod === 'gen8') {
 			return 'Pikachu' + this.sample(
