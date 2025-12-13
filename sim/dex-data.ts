a73076243d617285c39f2097064e5d54a70546d1
André Bastos Dias <andrebastosdias@gmail.com>
2025-12-13 18:09:01 +0000

    Fix

--- sim/dex-data.ts
+++ sim/dex-data.ts
@@ -227,6 +227,8 @@ export class TypeInfo implements Readonly<TypeData> {
 	 * non-alphanumeric characters removed. e.g. 'flying'
 	 */
 	readonly id: ID;
+	/** Number. e.g. 2 for Flying */
+	readonly num: number;
 	/** Name. e.g. 'Flying' */
 	readonly name: string;
 	/** Effect type. */
@@ -261,6 +263,7 @@ export class TypeInfo implements Readonly<TypeData> {
 	constructor(data: AnyObject) {
 		this.name = data.name;
 		this.id = data.id;
+		this.num = data.num;
 		this.effectType = Utils.getString(data.effectType) as TypeInfoEffectType || 'Type';
 		this.exists = data.exists ?? !!this.id;
 		this.gen = data.gen || 0;
