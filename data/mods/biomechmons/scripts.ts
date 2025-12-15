0adbee0504189f9004ead943ea87e06cba606279
HiZo <96159984+HisuianZoroark@users.noreply.github.com>
2025-12-14 22:46:06 -0500

    add transformInto

--- data/mods/biomechmons/scripts.ts
+++ data/mods/biomechmons/scripts.ts
@@ -29,8 +29,9 @@ export const Scripts: ModdedBattleScriptsData = {
 				// can't use hasAbility because it would lead to infinite recursion
 				if (
 					(pokemon.ability === ('neutralizinggas' as ID) ||
-						(pokemon.m.scrambled.abilities as { thing: string }[]).some(abils => this.battle.toID(abils.thing) === 'neutralizinggas')) &&
-						!pokemon.volatiles['gastroacid'] && !pokemon.abilityState.ending
+						(pokemon.m.scrambled.abilities as { thing: string }[]).some(
+							abils => this.battle.toID(abils.thing) === 'neutralizinggas')) &&
+							!pokemon.volatiles['gastroacid'] && !pokemon.abilityState.ending
 				) {
 					neutralizinggas = true;
 					break;
@@ -97,7 +98,7 @@ export const Scripts: ModdedBattleScriptsData = {
 					this.m.scrambled.items.splice(isItem, 1);
 				} else if ((this.m.scrambled.moves as { inSlot: string }[]).findIndex(e => e.inSlot === 'Ability') >= 0) {
 					const isMove = (this.m.scrambled.moves as { inSlot: string }[]).findIndex(e => e.inSlot === 'Ability');
-					this.baseMoveSlots.splice(
+					if (!this.transformInto) this.baseMoveSlots.splice(
 						this.baseMoveSlots.findIndex(m => this.battle.toID(this.m.scrambled.moves[isMove].thing) === m.id), 1);
 					// this.moveSlots.splice(this.moveSlots.findIndex(m => this.battle.toID(this.m.scrambled.items[isMove].thing) === m.id), 1);
 					this.m.scrambled.moves.splice(isMove, 1);
@@ -105,7 +106,7 @@ export const Scripts: ModdedBattleScriptsData = {
 			}
 			this.ability = ability.id;
 			// ability changes are permanent in BioMechMons
-			this.baseAbility = ability.id;
+			if (!isTransform) this.baseAbility = ability.id;
 			this.abilityState = this.battle.initEffectState({ id: ability.id, target: this });
 			if (sourceEffect && !isFromFormeChange && !isTransform) {
 				if (source) {
@@ -136,7 +137,7 @@ export const Scripts: ModdedBattleScriptsData = {
 						disabled: false,
 						used: false,
 					};
-					this.baseMoveSlots.push(newMove);
+					if (!this.transformInto) this.baseMoveSlots.push(newMove);
 				}
 			}
 			return oldAbility.id;
@@ -176,7 +177,7 @@ export const Scripts: ModdedBattleScriptsData = {
 					this.m.scrambled.abilties.splice(wrongSlot, 1);
 				} else if ((this.m.scrambled.moves as { inSlot: string }[]).findIndex(e => e.inSlot === 'Item') >= 0) {
 					wrongSlot = (this.m.scrambled.moves as { inSlot: string }[]).findIndex(e => e.inSlot === 'Item');
-					this.baseMoveSlots.splice(
+					if (!this.transformInto) this.baseMoveSlots.splice(
 						this.baseMoveSlots.findIndex(m => this.battle.toID(this.m.scrambled.moves[wrongSlot].thing) === m.id), 1);
 					this.m.scrambled.moves.splice(wrongSlot, 1);
 				}
@@ -229,7 +230,7 @@ export const Scripts: ModdedBattleScriptsData = {
 					this.m.scrambled.abilities.splice(isAbil, 1);
 				} else if ((this.m.scrambled.moves as { inSlot: string }[]).findIndex(e => e.inSlot === 'Item') >= 0) {
 					const isMove = (this.m.scrambled.moves as { inSlot: string }[]).findIndex(e => e.inSlot === 'Item');
-					this.baseMoveSlots.splice(
+					if (!this.transformInto) this.baseMoveSlots.splice(
 						this.baseMoveSlots.findIndex(m => this.battle.toID(this.m.scrambled.moves[isMove].thing) === m.id), 1);
 					// this.moveSlots.splice(this.moveSlots.findIndex(m => this.battle.toID(this.m.scrambled.items[isMove].thing) === m.id), 1);
 					this.m.scrambled.moves.splice(isMove, 1);
@@ -255,7 +256,7 @@ export const Scripts: ModdedBattleScriptsData = {
 							disabled: false,
 							used: false,
 						};
-						this.baseMoveSlots.push(newMove);
+						if (!this.transformInto) this.baseMoveSlots.push(newMove);
 					}
 				}
 			}
@@ -365,6 +366,128 @@ export const Scripts: ModdedBattleScriptsData = {
 				return true;
 			}
 			return false;
+		},
+		transformInto(pokemon, effect) {
+			const species = pokemon.species;
+			if (
+				pokemon.fainted || this.illusion || pokemon.illusion || (pokemon.volatiles['substitute'] && this.battle.gen >= 5) ||
+				(pokemon.transformed && this.battle.gen >= 2) || (this.transformed && this.battle.gen >= 5) ||
+				species.name === 'Eternatus-Eternamax' ||
+				(['Ogerpon', 'Terapagos'].includes(species.baseSpecies) && (this.terastallized || pokemon.terastallized)) ||
+				this.terastallized === 'Stellar'
+			) {
+				return false;
+			}
+
+			if (this.battle.dex.currentMod === 'gen1stadium' && (
+				species.name === 'Ditto' ||
+				(this.species.name === 'Ditto' && pokemon.moves.includes('transform'))
+			)) {
+				return false;
+			}
+
+			if (!this.setSpecies(species, effect, true)) return false;
+
+			this.transformed = true;
+			this.weighthg = pokemon.weighthg;
+
+			const types = pokemon.getTypes(true, true);
+			this.setType(pokemon.volatiles['roost'] ? pokemon.volatiles['roost'].typeWas : types, true);
+			this.addedType = pokemon.addedType;
+			this.knownType = this.isAlly(pokemon) && pokemon.knownType;
+			this.apparentType = pokemon.apparentType;
+
+			let statName: StatIDExceptHP;
+			for (statName in this.storedStats) {
+				this.storedStats[statName] = pokemon.storedStats[statName];
+				if (this.modifiedStats) this.modifiedStats[statName] = pokemon.modifiedStats![statName]; // Gen 1: Copy modified stats.
+			}
+			this.moveSlots = [];
+			this.hpType = (this.battle.gen >= 5 ? this.hpType : pokemon.hpType);
+			this.hpPower = (this.battle.gen >= 5 ? this.hpPower : pokemon.hpPower);
+			this.timesAttacked = pokemon.timesAttacked;
+			for (const moveSlot of pokemon.moveSlots) {
+				let moveName = moveSlot.move;
+				if (moveSlot.id === 'hiddenpower') {
+					moveName = 'Hidden Power ' + this.hpType;
+				}
+				this.moveSlots.push({
+					move: moveName,
+					id: moveSlot.id,
+					pp: moveSlot.maxpp === 1 ? 1 : 5,
+					maxpp: this.battle.gen >= 5 ? (moveSlot.maxpp === 1 ? 1 : 5) : moveSlot.maxpp,
+					target: moveSlot.target,
+					disabled: false,
+					used: false,
+					virtual: true,
+				});
+			}
+			let boostName: BoostID;
+			for (boostName in pokemon.boosts) {
+				this.boosts[boostName] = pokemon.boosts[boostName];
+			}
+			if (this.battle.gen >= 6) {
+				// we need to remove all of the overlapping crit volatiles before adding any of them
+				const volatilesToCopy = ['dragoncheer', 'focusenergy', 'gmaxchistrike', 'laserfocus'];
+				for (const volatile of volatilesToCopy) this.removeVolatile(volatile);
+				for (const volatile of volatilesToCopy) {
+					if (pokemon.volatiles[volatile]) {
+						this.addVolatile(volatile);
+						if (volatile === 'gmaxchistrike') this.volatiles[volatile].layers = pokemon.volatiles[volatile].layers;
+						if (volatile === 'dragoncheer') this.volatiles[volatile].hasDragonType = pokemon.volatiles[volatile].hasDragonType;
+					}
+				}
+			}
+			if (effect) {
+				this.battle.add('-transform', this, pokemon, '[from] ' + effect.fullname);
+			} else {
+				this.battle.add('-transform', this, pokemon);
+			}
+			if (this.terastallized) {
+				this.knownType = true;
+				this.apparentType = this.terastallized;
+			}
+			if (this.battle.gen > 2) this.setAbility(pokemon.ability, this, null, true, true);
+
+			// Change formes based on held items (for Transform)
+			// Only ever relevant in Generation 4 since Generation 3 didn't have item-based forme changes
+			if (this.battle.gen === 4) {
+				if (this.species.num === 487) {
+					// Giratina formes
+					if (this.species.name === 'Giratina' && this.item === 'griseousorb') {
+						this.formeChange('Giratina-Origin');
+					} else if (this.species.name === 'Giratina-Origin' && this.item !== 'griseousorb') {
+						this.formeChange('Giratina');
+					}
+				}
+				if (this.species.num === 493) {
+					// Arceus formes
+					const item = this.getItem();
+					const targetForme = (item?.onPlate ? 'Arceus-' + item.onPlate : 'Arceus');
+					if (this.species.name !== targetForme) {
+						this.formeChange(targetForme);
+					}
+				}
+			}
+
+			// Pokemon transformed into Ogerpon cannot Terastallize
+			// restoring their ability to tera after they untransform is handled ELSEWHERE
+			if (['Ogerpon', 'Terapagos'].includes(this.species.baseSpecies) && this.canTerastallize) this.canTerastallize = false;
+
+			for (const volatile in this.volatiles) {
+				if (this.volatiles[volatile].inSlot && this.volatiles[volatile].inSlot === 'Move') {
+					this.removeVolatile(volatile);
+				}
+			}
+
+			for (const volatile in pokemon.volatiles) {
+				if (pokemon.volatiles[volatile].inSlot && pokemon.volatiles[volatile].inSlot === 'Move') {
+					this.addVolatile(volatile);
+					this.volatiles[volatile].inSlot = 'Move';
+				}
+			}
+
+			return true;
 		},
 	},
 	field: {
