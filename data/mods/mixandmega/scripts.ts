2ed92845c95af2c3e28f4ac2e69790bb140878aa
HiZo <96159984+HisuianZoroark@users.noreply.github.com>
2025-12-16 01:41:04 -0500

    Fix bugs with Zygardite (#11628)
    * Fix bugs with Zygardite
    
    * Update data/mods/mixandmega/scripts.ts
    
    * Update data/items.ts
    
    * fix error
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Kris Johnson <11083252+KrisXV@users.noreply.github.com>

--- data/mods/mixandmega/scripts.ts
+++ data/mods/mixandmega/scripts.ts
@@ -466,7 +466,11 @@ export const Scripts: ModdedBattleScriptsData = {
 			return species;
 		},
 		getFormeChangeDeltas(formeChangeSpecies, pokemon) {
-			const baseSpecies = this.dex.species.get(formeChangeSpecies.baseSpecies);
+			// Should be fine as long as Necrozma-U doesn't get added or Game Freak makes me sad with some convoluted forme change
+			let baseSpecies = this.dex.species.get(formeChangeSpecies.battleOnly as string);
+			if (formeChangeSpecies.name === 'Zygarde-Mega') {
+				baseSpecies = this.dex.species.get('Zygarde-Complete');
+			}
 			const deltas: {
 				ability: string,
 				baseStats: SparseStatsTable,
