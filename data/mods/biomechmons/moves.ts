9e41ab4b6dbab5ad3774b1b9d9eac8f66cf3380f
HiZo <96159984+HisuianZoroark@users.noreply.github.com>
2025-12-17 01:40:28 -0500

    start work on edgecases
    mostly just the stuff from SP/SiC

--- data/mods/biomechmons/moves.ts
+++ data/mods/biomechmons/moves.ts
@@ -1,3 +1,24 @@
 export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
-
+  gastroacid: {
+		inherit: true,
+		condition: {
+			// Ability suppression implemented in Pokemon.ignoringAbility() within sim/pokemon.js
+			onStart(pokemon) {
+				this.add('-endability', pokemon);
+				this.singleEvent('End', pokemon.getAbility(), pokemon.abilityState, pokemon, pokemon, 'gastroacid');
+				const keys = Object.keys(pokemon.volatiles).filter(x => x.startsWith("ability:"));
+				if (keys.length) {
+					for (const abil of keys) {
+						pokemon.removeVolatile(abil);
+					}
+				}
+			},
+		},
+	},
+  poltergeist: {
+		inherit: true,
+		onTry(source, target) {
+			return !!target.item || Object.keys(target.volatiles).some(volatile => volatile.startsWith('item:'));
+		},
+	},
 };
