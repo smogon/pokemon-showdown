51eae1b871aa970b81fedf58e07e1d926cbd62ed
Atlas <109841009+EyeOfAtlas@users.noreply.github.com>
2026-01-02 12:06:40 +1000

    Bio Mech Mons: Fix desc (#11667)

--- config/formats.ts
+++ config/formats.ts
@@ -547,7 +547,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 	},
 	{
 		name: "[Gen 9] Bio Mech Mons",
-		desc: `Items, abilites, and moves a Pok&eacute;mon has access to can be put in any item/move/ability slot.`,
+		desc: `Items, abilities, and moves a Pok&eacute;mon has access to can be put in any item/move/ability slot.`,
 		mod: 'biomechmons',
 		// searchShow: false,
 		ruleset: ['Standard OMs', 'Sleep Moves Clause'],
