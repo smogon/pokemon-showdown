df66a3f8202db1e24b90c4ca49b80698abca304e
HiZo <96159984+HisuianZoroark@users.noreply.github.com>
2026-01-01 23:05:01 -0500

    Fix bug with air balloon

--- data/mods/biomechmons/scripts.ts
+++ data/mods/biomechmons/scripts.ts
@@ -2,6 +2,20 @@ import { RESTORATIVE_BERRIES } from "../../../sim/pokemon";
 
 export const Scripts: ModdedBattleScriptsData = {
 	pokemon: {
+		isGrounded(negateImmunity) {
+			if ('gravity' in this.battle.field.pseudoWeather) return true;
+			if ('ingrain' in this.volatiles && this.battle.gen >= 4) return true;
+			if ('smackdown' in this.volatiles) return true;
+			const item = (this.ignoringItem() ? '' : this.item);
+			if (item === 'ironball' || (this.volatiles['item:ironball'] && !this.ignoringItem())) return true;
+			// If a Fire/Flying type uses Burn Up and Roost, it becomes ???/Flying-type, but it's still grounded.
+			if (!negateImmunity && this.hasType('Flying') && !(this.hasType('???') && 'roost' in this.volatiles)) return false;
+			if (this.hasAbility('levitate') && !this.battle.suppressingAbility(this)) return null;
+			if ('magnetrise' in this.volatiles) return false;
+			if ('telekinesis' in this.volatiles) return false;
+			if (item === 'airballoon' || (this.volatiles['item:airballoon'] && !this.ignoringItem())) return false;
+			return true;
+		},
 		getAbility() {
 			const ability = this.battle.dex.abilities.getByID(this.ability);
 			if (ability.exists) return ability;
