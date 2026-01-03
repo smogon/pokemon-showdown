07dbfd2a209ee658a293b8be8ac327fe234b8961
Smell of curry <75345244+smell-of-curry@users.noreply.github.com>
2026-01-02 20:05:29 -0600

    remove mega rayquaza gen check

--- sim/battle-actions.ts
+++ sim/battle-actions.ts
@@ -1873,11 +1873,13 @@ export class BattleActions {
 		const altForme = species.otherFormes && this.dex.species.get(species.otherFormes[0]);
 		const item = pokemon.getItem();
 		// Mega Rayquaza
-		if ((this.battle.gen <= 7 || this.battle.ruleTable.has('+pokemontag:past')) &&
-			altForme?.isMega && altForme?.requiredMove &&
-			pokemon.baseMoves.includes(toID(altForme.requiredMove)) && !item.zMove) {
+			altForme?.isMega &&
+			altForme?.requiredMove &&
+			pokemon.baseMoves.includes(toID(altForme.requiredMove)) &&
+			!item.zMove
+		)
 			return altForme.name;
-		}
+
 		// a hacked-in Megazard X can mega evolve into Megazard Y, but not into Megazard X
 		if (item.megaEvolves === species.baseSpecies && item.megaStone !== species.name) {
 			return item.megaStone;
