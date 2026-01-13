c6c64754ca5f988237c4b11948dcce805da84ff8
Emikatana <itsemi48@gmail.com>
2026-01-13 11:05:55 -0500

    lint fixes

--- data/mods/chatbats/moves.ts
+++ data/mods/chatbats/moves.ts
@@ -1715,9 +1715,12 @@ export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
 		priority: 0,
 		flags: { protect: 1, mirror: 1, metronome: 1, contact: 1, bite: 1 },
 		onHit(target, source, move) {
-			const numberBerries = 0 + 1 * source.side.totalFainted;
+			const numberBerries = 0 + 1 * Number(source.side.totalFainted);
 			for (let i = 0; i < numberBerries; i++) {
-				const possibleBerries = ['aguavberry', 'apicotberry', 'enigmaberry', 'figyberry', 'ganlonberry', 'iapapaberry', 'keeberry', 'lansatberry', 'leppaberry', 'liechiberry', 'lumberry', 'magoberry', 'marangaberry', 'micleberry', 'oranberry', 'petayaberry', 'salacberry', 'sitrusberry', 'starfberry', 'wikiberry', 'aspearberry', 'cheriberry', 'chestoberry', 'lumberry', 'pechaberry', 'rawstberry', 'persimberry'];
+				const possibleBerries = ['aguavberry', 'apicotberry', 'enigmaberry', 'figyberry', 'ganlonberry', 'iapapaberry', 'keeberry',
+												 'lansatberry', 'leppaberry', 'liechiberry', 'lumberry', 'magoberry', 'marangaberry', 'micleberry',
+												 'oranberry', 'petayaberry', 'salacberry', 'sitrusberry', 'starfberry', 'wikiberry',
+												 'aspearberry', 'cheriberry', 'chestoberry', 'lumberry', 'pechaberry', 'rawstberry', 'persimberry'];
 				const chosenBerry = this.sample(possibleBerries);
 				const berry = this.dex.items.get(chosenBerry);
 				if (source.hp && berry.isBerry) {
