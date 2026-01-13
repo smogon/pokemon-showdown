91230fcae77fe9f299bd4b3ef9d8ec5eb18dd510
André Bastos Dias <80102738+andrebastosdias@users.noreply.github.com>
2026-01-13 12:12:36 +0000

    Lint

--- test/sim/moves/mindblown.js
+++ test/sim/moves/mindblown.js
@@ -27,7 +27,7 @@ describe('Mind Blown', () => {
 		]]);
 		assert.hurtsBy(battle.p1.active[0], Math.ceil(battle.p1.active[0].maxhp / 2), () => battle.makeChoices());
 	});
-	
+
 	it('should not deal damage to the user if there is no target', () => {
 		battle = common.createBattle([[
 			{ species: 'Dugtrio', ability: 'sandveil', moves: ['memento'] },
