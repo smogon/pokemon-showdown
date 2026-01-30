b1a86d17b94aec6bce24b4e3d5a207fa7f9edeb2
Meijer,L. (Lucas) <l.meijer6@students.uu.nl>
2026-01-30 17:02:06 +0100

    Remove later-gen pre-evolutions from earlier gens
    Affects /dt[gen], where they showed up, as well as consistency for re-evolutionmod.

---
+++ data/mods/gen3/pokedex.ts
@@ -0,0 +1,30 @@
+export const Pokedex: import('../../../sim/dex-species').ModdedSpeciesDataTable = {
+	chansey: {
+		inherit: true,
+		prevo: undefined,
+	},
+	mrmime: {
+		inherit: true,
+		prevo: undefined,
+	},
+	snorlax: {
+		inherit: true,
+		prevo: undefined,
+	},
+	marill: {
+		inherit: true,
+		prevo: undefined,
+	},
+	mantine: {
+		inherit: true,
+		prevo: undefined,
+	},
+	roselia: {
+		inherit: true,
+		prevo: undefined,
+	},
+	chimecho: {
+		inherit: true,
+		prevo: undefined,
+	},
+};
