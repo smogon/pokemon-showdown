4806556fe6082bc3275d12f3d5af1097148c659d
Yoshiblaze <53023564+Yoshiblaze@users.noreply.github.com>
2026-02-01 14:49:08 -0500

    Add New Pet Mod of the Season, Six by Six Random Battle, and Remove Previous PMOTM (#11737)
    * Delete data/random-battles/scootopiav2 directory
    
    * 6x6 Part 1
    
    * Delete data/mods/scootopiav2 directory
    
    * 6x6 Pt 2
    
    * 6x6 Pt 3

---
+++ data/mods/sixbysix/formats-data.ts
@@ -0,0 +1,113 @@
+export const FormatsData: import('../../../sim/dex-species').ModdedSpeciesFormatsDataTable = {
+	kingdraclassic: {
+		tier: "OU",
+	},
+	kingdrabrinepool: {
+		tier: "OU",
+	},
+	kingdradruid: {
+		tier: "OU",
+	},
+	kingdrabard: {
+		tier: "OU",
+	},
+	kingdragourmet: {
+		tier: "OU",
+	},
+	kingdraancient: {
+		tier: "OU",
+	},
+	nidoqueenclassic: {
+		tier: "OU",
+	},
+	nidoqueenerudite: {
+		tier: "OU",
+	},
+	nidoqueenvolcanic: {
+		tier: "OU",
+	},
+	nidoqueenrosegold: {
+		tier: "OU",
+	},
+	nidoqueenshaman: {
+		tier: "OU",
+	},
+	nidoqueenjasper: {
+		tier: "OU",
+	},
+	bisharpcenturion: {
+		tier: "OU",
+	},
+	bisharpsavage: {
+		tier: "OU",
+	},
+	bisharpronin: {
+		tier: "OU",
+	},
+	bisharpmantis: {
+		tier: "OU",
+	},
+	bisharprevenant: {
+		tier: "OU",
+	},
+	bisharpassassin: {
+		tier: "OU",
+	},
+	corviknightthunderbird: {
+		tier: "OU",
+	},
+	corviknightgenie: {
+		tier: "OU",
+	},
+	corviknightyeti: {
+		tier: "OU",
+	},
+	corviknightanalytical: {
+		tier: "OU",
+	},
+	corviknightmartian: {
+		tier: "OU",
+	},
+	corviknightfalcon: {
+		tier: "OU",
+	},
+	krookodileinfernal: {
+		tier: "OU",
+	},
+	krookodileabyssal: {
+		tier: "OU",
+	},
+	krookodilewetlander: {
+		tier: "OU",
+	},
+	krookodilecavedweller: {
+		tier: "OU",
+	},
+	krookodilecliffside: {
+		tier: "OU",
+	},
+	krookodilequantum: {
+		tier: "OU",
+	},
+	ogerponmuerta: {
+		tier: "OU",
+	},
+	ogerponeaster: {
+		tier: "OU",
+	},
+	ogerponankh: {
+		tier: "OU",
+	},
+	ogerponeid: {
+		tier: "OU",
+	},
+	ogerponvessel: {
+		tier: "OU",
+	},
+	ogerponkitsune: {
+		tier: "OU",
+	},
+	ogerpontealtera: {
+		tier: "Illegal",
+	},
+};
