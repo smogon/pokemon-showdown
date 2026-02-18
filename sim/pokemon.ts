c2b08493f288412ce3e9404da5d74bf9f904125f
André Bastos Dias <80102738+andrebastosdias@users.noreply.github.com>
2026-02-18 01:36:37 +0000

    Gen 1: Fix underflow glitch applied to mimicked duplicate moves (#11746)
    * Fix underflow glitch applied to mimicked duplicate moves
    
    * Remove flags
    
    * Remove console.log

--- sim/pokemon.ts
+++ sim/pokemon.ts
@@ -876,16 +876,15 @@ export class Pokemon {
 	}
 
 	deductPP(move: string | Move, amount?: number | null, target?: Pokemon | null | false) {
-		const gen = this.battle.gen;
 		move = this.battle.dex.moves.get(move);
 		const ppData = this.getMoveData(move);
 		if (!ppData) return 0;
 		ppData.used = true;
-		if (!ppData.pp && gen > 1) return 0;
+		if (!ppData.pp) return 0;
 
 		if (!amount) amount = 1;
 		ppData.pp -= amount;
-		if (ppData.pp < 0 && gen > 1) {
+		if (ppData.pp < 0) {
 			amount += ppData.pp;
 			ppData.pp = 0;
 		}
