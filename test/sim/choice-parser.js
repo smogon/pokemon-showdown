9c45af188796c26e94be43012f28476b44e747e2
Cameron Angliss <cameronangliss99@gmail.com>
2026-02-20 01:52:21 -0500

    add test to prevent future regression

--- test/sim/choice-parser.js
+++ test/sim/choice-parser.js
@@ -92,6 +92,23 @@ describe('Choice parser', () => {
 				assert(battle.p1.choose(validChoice));
 				battle.p1.clearChoice();
 			});
+
+			it('should prefer a non-transformed switch target when matching by species', () => {
+				battle = common.createBattle();
+				battle.setPlayer('p1', { team: [
+					{ species: "Ditto", ability: 'imposter', moves: ['splash'] },
+					{ species: "Rhydon", ability: 'rockhead', moves: ['splash'] },
+				] });
+				battle.setPlayer('p2', { team: [{ species: "Rhydon", ability: 'lightningrod', moves: ['earthquake'] }] });
+
+				assert.species(battle.p1.active[0], 'Rhydon');
+				assert(battle.p1.active[0].transformed);
+				battle.makeChoices('move 1', 'move 1');
+				assert.equal(battle.p1.requestState, 'switch');
+				assert(battle.choose('p1', 'switch Rhydon'));
+				assert.equal(battle.p1.active[0], battle.p1.pokemon[1]);
+				assert.false(battle.p1.active[0].transformed);
+			});
 		});
 
 		describe('Doubles/Triples', () => {
