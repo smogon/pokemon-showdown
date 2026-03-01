21f2de627a57cd1b24c5e1d2dd114a5be6d04844
TurboRx <TurboRx@users.noreply.github.com>
2026-03-01 08:46:50 +0000

    feat: add NC 1997 Sleep Clause with auto-forfeit on violation

--- config/formats.ts
+++ config/formats.ts
@@ -731,7 +731,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		// searchShow: false,
 		ruleset: [
 			'Picked Team Size = 3', 'Min Level = 50', 'Max Level = 55', 'Max Total Level = 155',
-			'Obtainable', 'Team Preview', 'Stadium Sleep Clause', 'Species Clause', 'Nickname Clause', 'Cancel Mod', 'NC 1997 Move Legality',
+			'Obtainable', 'Team Preview', 'NC 1997 Sleep Clause', 'Species Clause', 'Nickname Clause', 'Cancel Mod', 'NC 1997 Move Legality',
 		],
 		banlist: ['Uber'],
 	},
