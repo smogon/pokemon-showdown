e1cec65d8871cd4df7794487be9d630351af2b31
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2026-03-01 03:12:41 -0700

    Update rotational ladders

--- data/mods/linked/scripts.ts
+++ data/mods/linked/scripts.ts
@@ -1,6 +1,5 @@
 export const Scripts: ModdedBattleScriptsData = {
-	inherit: 'gen8',
-	gen: 8,
+	gen: 9,
 	getActionSpeed(action) {
 		if (action.choice === 'move') {
 			let move = action.move;
@@ -106,8 +105,9 @@ export const Scripts: ModdedBattleScriptsData = {
 				// @ts-expect-error modded
 				const linkedMoves: ActiveMove[] = action.linked;
 				for (let i = linkedMoves.length - 1; i >= 0; i--) {
-					const validTarget = this.validTargetLoc(action.targetLoc, action.pokemon, linkedMoves[i].target);
-					const targetLoc = validTarget ? action.targetLoc : 0;
+					const isValidTarget = this.validTargetLoc(action.targetLoc, action.pokemon, linkedMoves[i].target);
+					const targetLoc = isValidTarget ? action.targetLoc :
+						action.pokemon.getLocOf(this.getRandomTarget(action.pokemon, linkedMoves[i])!);
 					const pseudoAction: Action = {
 						choice: 'move', priority: action.priority, speed: action.speed, pokemon: action.pokemon,
 						targetLoc, moveid: linkedMoves[i].id, move: linkedMoves[i], mega: action.mega,
@@ -334,15 +334,12 @@ export const Scripts: ModdedBattleScriptsData = {
 				// THIS IS PURELY A SANITY CHECK
 				// DO NOT TAKE ADVANTAGE OF THIS TO PREVENT A POKEMON FROM MOVING;
 				// USE this.queue.cancelMove INSTEAD
-				this.debug(`${pokemon.id} INCONSISTENT STATE, ALREADY MOVED: ${pokemon.moveThisTurn}`);
-				this.clearActiveMove(true);
+				this.battle.debug(`${pokemon.id} INCONSISTENT STATE, ALREADY MOVED: ${pokemon.moveThisTurn}`);
+				this.battle.clearActiveMove(true);
 				return;
-				} */
+			} */
 			const willTryMove = this.battle.runEvent('BeforeMove', pokemon, target, move);
 			if (!willTryMove) {
-				if (pokemon.volatiles['twoturnmove']?.move === move.id) {
-					pokemon.removeVolatile('twoturnmove');
-				}
 				this.battle.runEvent('MoveAborted', pokemon, target, move);
 				this.battle.clearActiveMove(true);
 				// The event 'BeforeMove' could have returned false or null
@@ -351,6 +348,12 @@ export const Scripts: ModdedBattleScriptsData = {
 				pokemon.moveThisTurnResult = willTryMove;
 				return;
 			}
+
+			// Used exclusively for a hint later
+			if (move.flags['cantusetwice'] && pokemon.m.lastMoveAbsolute?.id === move.id) {
+				pokemon.addVolatile(move.id);
+			}
+
 			if (move.beforeMoveCallback) {
 				if (move.beforeMoveCallback.call(this.battle, pokemon, target, move)) {
 					this.battle.clearActiveMove(true);
@@ -366,10 +369,6 @@ export const Scripts: ModdedBattleScriptsData = {
 				if (!lockedMove) {
 					if (!pokemon.deductPP(baseMove, null, target) && (move.id !== 'struggle')) {
 						this.battle.add('cant', pokemon, 'nopp', move);
-						const gameConsole = [
-							null, 'Game Boy', 'Game Boy Color', 'Game Boy Advance', 'DS', 'DS', '3DS', '3DS',
-						][this.battle.gen] || 'Switch';
-						this.battle.hint(`This is not a bug, this is really how it works on the ${gameConsole}; try it yourself if you don't believe us.`);
 						this.battle.clearActiveMove(true);
 						pokemon.moveThisTurnResult = false;
 						return;
@@ -382,7 +381,7 @@ export const Scripts: ModdedBattleScriptsData = {
 
 			// Dancer Petal Dance hack
 			// TODO: implement properly
-			const noLock = externalMove && !pokemon.volatiles.lockedmove;
+			const noLock = externalMove && !pokemon.volatiles['lockedmove'];
 
 			if (zMove) {
 				if (pokemon.illusion) {
@@ -391,12 +390,19 @@ export const Scripts: ModdedBattleScriptsData = {
 				this.battle.add('-zpower', pokemon);
 				pokemon.side.zMoveUsed = true;
 			}
+
+			const oldActiveMove = move;
+
 			const moveDidSomething = this.useMove(baseMove, pokemon, { target, sourceEffect, zMove, maxMove });
 			this.battle.lastSuccessfulMoveThisTurn = moveDidSomething ? this.battle.activeMove && this.battle.activeMove.id : null;
 			if (this.battle.activeMove) move = this.battle.activeMove;
 			this.battle.singleEvent('AfterMove', move, null, pokemon, target, move);
 			this.battle.runEvent('AfterMove', pokemon, target, move);
+			if (move.flags['cantusetwice'] && pokemon.removeVolatile(move.id)) {
+				this.battle.add('-hint', `Some effects can force a Pokemon to use ${move.name} again in a row.`);
+			}
 
+			// TODO: Refactor to use BattleQueue#prioritizeAction in onAnyAfterMove handlers
 			// Dancer's activation order is completely different from any other event, so it's handled separately
 			if (move.flags['dance'] && moveDidSomething && !move.isExternal) {
 				const dancers = [];
@@ -410,20 +416,29 @@ export const Scripts: ModdedBattleScriptsData = {
 				// Note that the speed stat used is after any volatile replacements like Speed Swap,
 				// but before any multipliers like Agility or Choice Scarf
 				// Ties go to whichever Pokemon has had the ability for the least amount of time
-				dancers.sort((a, b) => -(b.storedStats['spe'] - a.storedStats['spe']) ||
-					b.abilityState.effectOrder - a.abilityState.effectOrder);
+				dancers.sort(
+					(a, b) => -(b.storedStats['spe'] - a.storedStats['spe']) || b.abilityState.effectOrder - a.abilityState.effectOrder
+				);
+				const targetOf1stDance = this.battle.activeTarget!;
 				for (const dancer of dancers) {
 					if (this.battle.faintMessages()) break;
 					if (dancer.fainted) continue;
 					this.battle.add('-activate', dancer, 'ability: Dancer');
-					const dancersTarget = !target!.isAlly(dancer) && pokemon.isAlly(dancer) ? target! : pokemon;
-					this.runMove(move.id, dancer, dancer.getLocOf(dancersTarget),
-						{ sourceEffect: this.dex.abilities.get('dancer'), externalMove: true });
+					const dancersTarget = !targetOf1stDance.isAlly(dancer) && pokemon.isAlly(dancer) ?
+						targetOf1stDance :
+						pokemon;
+					const dancersTargetLoc = dancer.getLocOf(dancersTarget);
+					this.runMove(move.id, dancer, dancersTargetLoc, { sourceEffect: this.dex.abilities.get('dancer'), externalMove: true });
 				}
 			}
 			if (noLock && pokemon.volatiles['lockedmove']) delete pokemon.volatiles['lockedmove'];
 			this.battle.faintMessages();
 			this.battle.checkWin();
+
+			if (this.battle.gen <= 4) {
+				// In gen 4, the outermost move is considered the last move for Copycat
+				this.battle.activeMove = oldActiveMove;
+			}
 		},
 	},
 	queue: {
@@ -441,13 +456,16 @@ export const Scripts: ModdedBattleScriptsData = {
 					instaswitch: 3,
 					beforeTurn: 4,
 					beforeTurnMove: 5,
+					revivalblessing: 6,
 
-					runUnnerve: 100,
 					runSwitch: 101,
-					// runPrimal: 102, (deprecated)
 					switch: 103,
 					megaEvo: 104,
+					megaEvoX: 104,
+					megaEvoY: 104,
 					runDynamax: 105,
+					terastallize: 106,
+					priorityChargeMove: 107,
 
 					shift: 200,
 					// default is 200 (for moves)
@@ -476,10 +494,35 @@ export const Scripts: ModdedBattleScriptsData = {
 							pokemon: action.pokemon,
 						}));
 					}
+					if (action.megax && !action.pokemon.isSkyDropped()) {
+						actions.unshift(...this.resolveAction({
+							choice: 'megaEvoX',
+							pokemon: action.pokemon,
+						}));
+					}
+					if (action.megay && !action.pokemon.isSkyDropped()) {
+						actions.unshift(...this.resolveAction({
+							choice: 'megaEvoY',
+							pokemon: action.pokemon,
+						}));
+					}
+					if (action.terastallize && !action.pokemon.terastallized) {
+						actions.unshift(...this.resolveAction({
+							choice: 'terastallize',
+							pokemon: action.pokemon,
+						}));
+					}
 					if (action.maxMove && !action.pokemon.volatiles['dynamax']) {
 						actions.unshift(...this.resolveAction({
 							choice: 'runDynamax',
 							pokemon: action.pokemon,
+						}));
+					}
+					if (!action.maxMove && !action.zmove && action.move.priorityChargeCallback) {
+						actions.unshift(...this.resolveAction({
+							choice: 'priorityChargeMove',
+							pokemon: action.pokemon,
+							move: action.move,
 						}));
 					}
 					action.fractionalPriority = this.battle.runEvent('FractionalPriority', action.pokemon, null, action.move, 0);
