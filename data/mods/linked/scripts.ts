d4d4a09c46ef04a528108b3977e9e8500e86fc6b
Ivo Julca <ivojulca@hotmail.com>
2026-03-03 03:29:15 -0500

    Fix Linked last move checks
    At some point between af6affb and 045fe456, Linked's concept of
    last absolute move changed from using the 2nd move in a link to
    using the 1st move, likely in an attempt to preserve the 1st move
    information that used to be stored for debugging purposes.
    
    However, the redefinition has impacted the mechanics of Sketch, and
    other moves for a long time.
    
    This commit restores the original mechanics, and gets rid of the
    last absolute move concept, in favor of the orthodox pokemon.lastMove.
    In consequence, all modded data entries that now match gen9 are
    also deleted.

--- data/mods/linked/scripts.ts
+++ data/mods/linked/scripts.ts
@@ -439,7 +439,7 @@ export const Scripts: ModdedBattleScriptsData = {
 			}
 
 			// Used exclusively for a hint later
-			if (move.flags['cantusetwice'] && pokemon.m.lastMoveAbsolute?.id === move.id) {
+			if (move.flags['cantusetwice'] && pokemon.lastMove?.id === move.id) {
 				pokemon.addVolatile(move.id);
 			}
 
@@ -668,69 +668,7 @@ export const Scripts: ModdedBattleScriptsData = {
 		},
 	},
 	pokemon: {
-		clearVolatile(includeSwitchFlags = true) {
-			this.boosts = {
-				atk: 0,
-				def: 0,
-				spa: 0,
-				spd: 0,
-				spe: 0,
-				accuracy: 0,
-				evasion: 0,
-			};
-
-			if (this.battle.gen === 1 && this.baseMoves.includes('mimic' as ID) && !this.transformed) {
-				const moveslot = this.baseMoves.indexOf('mimic' as ID);
-				const mimicPP = this.moveSlots[moveslot] ? this.moveSlots[moveslot].pp : 16;
-				this.moveSlots = this.baseMoveSlots.slice();
-				this.moveSlots[moveslot].pp = mimicPP;
-			} else {
-				this.moveSlots = this.baseMoveSlots.slice();
-			}
-
-			this.transformed = false;
-			this.ability = this.baseAbility;
-			this.hpType = this.baseHpType;
-			this.hpPower = this.baseHpPower;
-			if (this.canTerastallize === false) this.canTerastallize = this.teraType;
-			for (const i in this.volatiles) {
-				if (this.volatiles[i].linkedStatus) {
-					this.removeLinkedVolatiles(this.volatiles[i].linkedStatus, this.volatiles[i].linkedPokemon);
-				}
-			}
-			if (this.species.name === 'Eternatus-Eternamax' && this.volatiles['dynamax']) {
-				this.volatiles = { dynamax: this.volatiles['dynamax'] };
-			} else {
-				this.volatiles = {};
-			}
-			if (includeSwitchFlags) {
-				this.switchFlag = false;
-				this.forceSwitchFlag = false;
-			}
-
-			this.m.lastMoveAbsolute = null;
-			this.lastMove = null;
-			if (this.battle.gen === 2) this.lastMoveEncore = null;
-			this.lastMoveUsed = null;
-			this.moveThisTurn = '';
-			this.moveLastTurnResult = undefined;
-			this.moveThisTurnResult = undefined;
-
-			this.lastDamage = 0;
-			this.attackedBy = [];
-			this.hurtThisTurn = null;
-			this.newlySwitched = true;
-			this.beingCalledBack = false;
-
-			this.volatileStaleness = undefined;
-
-			delete this.abilityState.started;
-			delete this.itemState.started;
-
-			this.setSpecies(this.baseSpecies);
-		},
 		moveUsed(move, targetLoc) {
-			if (!this.moveThisTurn) this.m.lastMoveAbsolute = move;
 			this.lastMove = move;
 			this.moveThisTurn = move.id;
 			this.lastMoveTargetLoc = targetLoc;
