669ce3e2d5231e3c8e4e81b9fc7216f0a6d7cf7d
André Bastos Dias <80102738+andrebastosdias@users.noreply.github.com>
2026-03-20 05:08:45 +0000

    Remove unnecessary type assertion in Beat Up (#11832)

--- data/mods/gen4/moves.ts
+++ data/mods/gen4/moves.ts
@@ -88,8 +88,7 @@ export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
 			onModifyAtkPriority: -101,
 			onModifyAtk(atk, pokemon, defender, move) {
 				if (!this.ruleTable.has('beatupnicknamesmod')) {
-					// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
-					this.add('-activate', pokemon, 'move: Beat Up', '[of] ' + move.allies![0]!.name);
+					this.add('-activate', pokemon, 'move: Beat Up', '[of] ' + move.allies![0].name);
 				}
 				this.event.modifier = 1;
 				return this.dex.species.get(move.allies!.shift()!.set.species).baseStats.atk;
