cce022848c2b714f2375ced2d56ac5790a4b10ed
ry <ryr4242@gmail.com>
2026-03-21 20:01:05 -0500

    Clean up gen7sm

--- data/mods/gen7sm/pokedex.ts
+++ data/mods/gen7sm/pokedex.ts
@@ -39,32 +39,6 @@ export const Pokedex: import('../../../sim/dex-species').ModdedSpeciesDataTable 
 		inherit: true,
 		unreleasedHidden: true,
 	},
-	rockruff: {
-		num: 744,
-		name: "Rockruff",
-		baseForme: "Midday",
-		types: ["Rock"],
-		baseStats: { hp: 45, atk: 65, def: 40, spa: 30, spd: 40, spe: 60 },
-		abilities: { 0: "Keen Eye", 1: "Vital Spirit", H: "Steadfast" },
-		heightm: 0.5,
-		weightkg: 9.2,
-		color: "Brown",
-		evos: ["Lycanroc", "Lycanroc-Midnight"],
-		eggGroups: ["Field"],
-	},
-	rockruffdusk: {
-		inherit: true,
-		isNonstandard: "Future",
-	},
-	lycanroc: {
-		inherit: true,
-		otherFormes: ["Lycanroc-Midnight"],
-		formeOrder: ["Lycanroc", "Lycanroc-Midnight"],
-	},
-	lycanrocdusk: {
-		inherit: true,
-		isNonstandard: "Future",
-	},
 	oranguru: {
 		inherit: true,
 		unreleasedHidden: true,
