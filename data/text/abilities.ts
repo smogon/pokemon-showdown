4e3e0255e12020365a0b1df82cd9c4d2b035f0e3
iforgetwhyimhere <thereal.quinzep@gmail.com>
2026-03-25 14:34:09 -0400

    add descs to Dragonize and Mega Sol

--- data/text/abilities.ts
+++ data/text/abilities.ts
@@ -379,6 +379,10 @@ export const AbilitiesText: { [id: IDEntry]: AbilityText } = {
 		desc: "On switch-in, this Pokemon's Attack or Special Attack is raised by 1 stage based on the weaker combined defensive stat of all opposing Pokemon. Attack is raised if their Defense is lower, and Special Attack is raised if their Special Defense is the same or lower.",
 		shortDesc: "On switch-in, Attack or Sp. Atk is raised 1 stage based on the foes' weaker Defense.",
 	},
+	dragonize: {
+		desc: "This Pokemon's Normal-type moves become Ice-type moves and have their power multiplied by 1.2. This effect comes after other effects that change a move's type, but before Ion Deluge and Electrify's effects.",
+		shortDesc: "This Pokemon's Normal-type moves are Dragon-type and have 1.2x power.",
+	},
 	dragonsmaw: {
 		name: "Dragon's Maw",
 		shortDesc: "This Pokemon's offensive stat is multiplied by 1.5 while using a Dragon-type attack.",
@@ -979,6 +983,10 @@ export const AbilitiesText: { [id: IDEntry]: AbilityText } = {
 		name: "Mega Launcher",
 		desc: "This Pokemon's pulse moves have their power multiplied by 1.5. Heal Pulse restores 3/4 of a target's maximum HP, rounded half down.",
 		shortDesc: "This Pokemon's pulse moves have 1.5x power. Heal Pulse heals 3/4 target's max HP.",
+	},
+	megasol: {
+		desc: "Moves used by this Pokemon have the effects they would when they are used under harsh sunlight.",
+		shortDesc: "This Pokemon's moves act as if they were used under harsh sunlight.",
 	},
 	merciless: {
 		name: "Merciless",
