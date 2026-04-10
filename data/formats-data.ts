051240ea62ae31e6890e69b2dbb5085fcedd5b7a
missshowdown <gladstonesecond@gmail.com>
2026-04-09 23:06:42 -0700

    Fix ZU by usage NFEs
    I forgot in the last fix that Primeape fell out of ZU by usage in the last three month period.
    Jan: 4.25%
    Feb: 4.71%
    Mar: 2.17%
    Avg: 3.71%

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -464,7 +464,7 @@ export const FormatsData: import('../sim/dex-species').SpeciesFormatsDataTable =
 		tier: "LC",
 	},
 	primeape: {
-		tier: "ZU",
+		tier: "NFE",
 		doublesTier: "NFE",
 		natDexTier: "NFE",
 	},
