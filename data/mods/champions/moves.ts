e31682fd0c8cd671c25ef0104249efd3fae7be83
André Bastos Dias <80102738+andrebastosdias@users.noreply.github.com>
2026-04-10 10:23:00 +0100

    Add 'Past' moves (#23)
    * Add past moves
    
    * Fix move availability

--- data/mods/champions/moves.ts
+++ data/mods/champions/moves.ts
@@ -1,4 +1,16 @@
 export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
+	absorb: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	acid: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	aeroblast: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
 	anchorshot: {
 		inherit: true,
 		basePower: 90,
@@ -7,38 +19,168 @@ export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
 		inherit: true,
 		basePower: 90,
 	},
+	armthrust: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	astonish: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
 	astralbarrage: {
 		inherit: true,
 		basePower: 110,
+		isNonstandard: "Past",
+	},
+	attackorder: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	aurorabeam: {
+		inherit: true,
+		isNonstandard: "Past",
 	},
 	banefulbunker: {
 		inherit: true,
 		pp: 5,
+	},
+	barbbarrage: {
+		inherit: true,
+		isNonstandard: "Past",
 	},
 	beakblast: {
 		inherit: true,
 		basePower: 120,
 		pp: 5,
 	},
+	behemothbash: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	behemothblade: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	blazingtorque: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	bleakwindstorm: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
 	bloodmoon: {
 		inherit: true,
 		basePower: 130,
+		isNonstandard: "Past",
+	},
+	blueflare: {
+		inherit: true,
+		isNonstandard: "Past",
 	},
 	boltbeak: {
 		inherit: true,
 		basePower: 80,
 	},
+	boltstrike: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
 	bonerush: {
 		inherit: true,
 		basePower: 30,
+	},
+	branchpoke: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	brine: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	bubblebeam: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	burningbulwark: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	burnup: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	celebrate: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	chloroblast: {
+		inherit: true,
+		isNonstandard: "Past",
 	},
 	clangoroussoul: {
 		inherit: true,
 		accuracy: true,
 	},
+	collisioncourse: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	combattorque: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	confide: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	confusion: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	conversion: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	conversion2: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	corrosivegas: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	courtchange: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
 	crabhammer: {
 		inherit: true,
 		accuracy: 95,
+	},
+	crushgrip: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	cut: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	darkvoid: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	defendorder: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	defensecurl: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	diamondstorm: {
+		inherit: true,
+		isNonstandard: "Past",
 	},
 	direclaw: {
 		inherit: true,
@@ -51,7 +193,31 @@ export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
 			},
 		},
 	},
+	disarmingvoice: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	doodle: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	doomdesire: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	doublekick: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
 	doubleshock: {
+		inherit: true,
+		isNonstandard: "Custom",
+	},
+	dragonascent: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	dragonbreath: {
 		inherit: true,
 		isNonstandard: "Past",
 	},
@@ -59,13 +225,74 @@ export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
 		inherit: true,
 		flags: { contact: 1, protect: 1, mirror: 1, metronome: 1, slicing: 1 },
 	},
+	dragonenergy: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
 	dragonhammer: {
 		inherit: true,
 		basePower: 100,
+		isNonstandard: "Past",
+	},
+	dreameater: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	drumbeating: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	dynamaxcannon: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	echoedvoice: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	electrify: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	electrodrift: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	ember: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	esperwing: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	fairywind: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	falsesurrender: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	falseswipe: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	fierywrath: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	filletaway: {
+		inherit: true,
+		isNonstandard: "Past",
 	},
 	firelash: {
 		inherit: true,
 		basePower: 90,
+	},
+	firepledge: {
+		inherit: true,
+		isNonstandard: "Past",
 	},
 	firstimpression: {
 		inherit: true,
@@ -75,22 +302,143 @@ export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
 		inherit: true,
 		basePower: 80,
 	},
+	flamewheel: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	fleurcannon: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	floralhealing: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	forcepalm: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	freezeshock: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	freezingglare: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	furyattack: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	furycutter: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	furyswipes: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	fusionbolt: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	fusionflare: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
 	geargrind: {
 		inherit: true,
 		accuracy: 90,
 		basePower: 60,
 	},
+	glaciallance: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	glaciate: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	glaiverush: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	grasspledge: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
 	gravapple: {
 		inherit: true,
 		basePower: 90,
+	},
+	growl: {
+		inherit: true,
+		isNonstandard: "Past",
 	},
 	growth: {
 		inherit: true,
 		type: "Grass",
 	},
+	gust: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	happyhour: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	harden: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	headbutt: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	heartswap: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	holdback: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	holdhands: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	honeclaws: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	hornattack: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	hydrosteam: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
 	hyperdrill: {
 		inherit: true,
 		basePower: 120,
+		isNonstandard: "Past",
+	},
+	hyperspacefury: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	hyperspacehole: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	iceburn: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	incinerate: {
+		inherit: true,
+		isNonstandard: "Past",
 	},
 	infernalparade: {
 		inherit: true,
@@ -103,13 +451,107 @@ export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
 			volatileStatus: 'flinch',
 		},
 	},
+	ivycudgel: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	jawlock: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	judgment: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	junglehealing: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
 	kingsshield: {
 		inherit: true,
 		pp: 5,
+		isNonstandard: null,
+	},
+	leafage: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	leer: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	lick: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	lightofruin: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	lunarblessing: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	lunardance: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	lusterpurge: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	magicalleaf: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	magicaltorque: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	magmastorm: {
+		inherit: true,
+		isNonstandard: "Past",
 	},
 	makeitrain: {
 		inherit: true,
 		accuracy: 95,
+		isNonstandard: "Past",
+	},
+	malignantchain: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	megadrain: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	megapunch: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	metalclaw: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	metronome: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	mightycleave: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	mimic: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	mist: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	mistball: {
+		inherit: true,
+		isNonstandard: "Past",
 	},
 	moonblast: {
 		inherit: true,
@@ -120,9 +562,17 @@ export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
 			},
 		},
 	},
+	moongeistbeam: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
 	mountaingale: {
 		inherit: true,
 		basePower: 120,
+	},
+	mysticalpower: {
+		inherit: true,
+		isNonstandard: "Past",
 	},
 	nightdaze: {
 		inherit: true,
@@ -136,27 +586,156 @@ export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
 		inherit: true,
 		pp: 5,
 	},
+	noretreat: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	noxioustorque: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
 	obstruct: {
 		inherit: true,
 		pp: 5,
+	},
+	orderup: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	originpulse: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	overdrive: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	payday: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	peck: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	photongeyser: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	playnice: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	poisongas: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	poisonsting: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	poisontail: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	powdersnow: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	powershift: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	precipiceblades: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	present: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	prismaticlaser: {
+		inherit: true,
+		isNonstandard: "Past",
 	},
 	protect: {
 		inherit: true,
 		pp: 5,
 	},
+	psybeam: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	psyblade: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	psychoboost: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
 	psyshieldbash: {
 		inherit: true,
 		basePower: 90,
+	},
+	psystrike: {
+		inherit: true,
+		isNonstandard: "Past",
 	},
 	purify: {
 		inherit: true,
 		pp: 5,
 	},
+	pyroball: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	ragefist: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	razorleaf: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	relicsong: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	retaliate: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
 	revelationdance: {
 		inherit: true,
 		basePower: 100,
+		isNonstandard: "Past",
 	},
 	revivalblessing: {
+		inherit: true,
+		isNonstandard: "Custom",
+	},
+	roaroftime: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	rocksmash: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	rockthrow: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	rollout: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	ruination: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	sacredfire: {
 		inherit: true,
 		isNonstandard: "Past",
 	},
@@ -169,29 +748,103 @@ export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
 			},
 		},
 	},
+	sandattack: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	sandsearstorm: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
 	sandstorm: {
 		inherit: true,
 		pp: 5,
+	},
+	scratch: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	secretsword: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	seedflare: {
+		inherit: true,
+		isNonstandard: "Past",
 	},
 	shadowclaw: {
 		inherit: true,
 		flags: { contact: 1, protect: 1, mirror: 1, metronome: 1, slicing: 1 },
 	},
+	shadowforce: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
 	shelltrap: {
 		inherit: true,
 		pp: 10,
 	},
+	shiftgear: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	shockwave: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	shoreup: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	silktrap: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	sketch: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	slam: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	slash: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	sludge: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	smog: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	smokescreen: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
 	snaptrap: {
 		inherit: true,
 		type: "Steel",
+		isNonstandard: null,
 	},
 	snipeshot: {
 		inherit: true,
 		basePower: 85,
+		isNonstandard: "Past",
 	},
 	snowscape: {
 		inherit: true,
 		pp: 5,
+	},
+	spacialrend: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	spark: {
+		inherit: true,
+		isNonstandard: "Past",
 	},
 	spikyshield: {
 		inherit: true,
@@ -200,14 +853,123 @@ export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
 	spinout: {
 		inherit: true,
 		pp: 10,
+		isNonstandard: "Past",
+	},
+	spiritbreak: {
+		inherit: true,
+		isNonstandard: "Past",
 	},
 	spiritshackle: {
 		inherit: true,
 		basePower: 90,
 	},
+	splash: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	springtidestorm: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	steameruption: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	stomp: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	stormthrow: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	strangesteam: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	strength: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	sunsteelstrike: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	supersonic: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	surgingstrikes: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	swift: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
 	syrupbomb: {
 		inherit: true,
 		accuracy: 90,
+	},
+	tachyoncutter: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	tackle: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	tailglow: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	tailwhip: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	takedown: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	takeheart: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	tarshot: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	teleport: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	terablast: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	terastarstorm: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	thundercage: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	thunderclap: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	thunderouskick: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	thundershock: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	topsyturvy: {
+		inherit: true,
+		isNonstandard: "Past",
 	},
 	toxicthread: {
 		inherit: true,
@@ -215,12 +977,77 @@ export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
 			spe: -2,
 		},
 	},
+	trickortreat: {
+		inherit: true,
+		isNonstandard: null,
+	},
 	tripledive: {
 		inherit: true,
 		basePower: 35,
+		isNonstandard: "Past",
+	},
+	triplekick: {
+		inherit: true,
+		isNonstandard: "Past",
 	},
 	tropkick: {
 		inherit: true,
 		basePower: 85,
+	},
+	twister: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	vcreate: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	victorydance: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	vinewhip: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	visegrip: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	watergun: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	waterpledge: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	wickedblow: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	wickedtorque: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	wildboltstorm: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	wingattack: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	withdraw: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	workup: {
+		inherit: true,
+		isNonstandard: "Past",
+	},
+	zingzap: {
+		inherit: true,
+		isNonstandard: "Past",
 	},
 };
