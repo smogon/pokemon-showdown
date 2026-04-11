b391601d79be985e8f6685fd86164cbc0e763348
Karthik99999 <bandagondak0217@gmail.com>
2026-04-10 20:02:04 -0700

    Don't show Tera Type in OTS if format doesn't support Tera

--- sim/battle.ts
+++ sim/battle.ts
@@ -3213,8 +3213,8 @@ export class Battle {
 					ivs: null!,
 					level: set.level,
 				};
-				if (this.gen === 8) newSet.gigantamax = set.gigantamax;
-				if (this.gen === 9) newSet.teraType = set.teraType;
+				if (this.gen === 8 && !this.ruleTable.has('dynamaxclause')) newSet.gigantamax = set.gigantamax;
+				if (this.gen === 9 && !this.ruleTable.has('terastalclause')) newSet.teraType = set.teraType;
 				// Only display Hidden Power type if the Pokemon has Hidden Power
 				// This is based on how team sheets were written in past VGC formats
 				if (set.moves.some(m => this.dex.moves.get(m).id === 'hiddenpower')) newSet.hpType = set.hpType;
