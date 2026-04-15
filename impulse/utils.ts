a9fcf7e62160a6b4352e2977a5dfc33553e34526
PrinceSky-Git <princeskygithub@gmail.com>
2026-04-15 11:59:50 +0530

    Update utils.ts

--- impulse/utils.ts
+++ impulse/utils.ts
@@ -4,13 +4,11 @@
 */
 
 export const Table = (title: string, headerRow: string[], dataRows: string[][]): string => {
-	let output = `<div class="themed-table-container" 
-		style="max-width: 100%; max-height: 380px; overflow-y: auto; overflow-x: hidden;">`;
+	let output = `<div class="themed-table-container" style="max-width: 100%; max-height: 380px; overflow-y: auto; overflow-x: hidden;">`;
 	
 	output += `<h3 class="themed-table-title">${title}</h3>`;
 	
-	output += `<table class="themed-table" 
-		style="width: 100%; border-collapse: collapse; table-layout: fixed;">`;
+	output += `<table class="themed-table" style="width: 100%; border-collapse: collapse; table-layout: fixed;">`;
 	
 	output += `<tr class="themed-table-header">`;
 	headerRow.forEach(header => { output += `<th>${header}</th>`; });
