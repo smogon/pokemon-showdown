8a5f10ffb3c834c3f0c73c61e237b6b6237517fb
PrinceSky-Git <princeskygithub@gmail.com>
2026-04-15 11:56:20 +0530

    Only allow vertical scrolling

--- impulse/utils.ts
+++ impulse/utils.ts
@@ -4,17 +4,24 @@
 */
 
 export const Table = (title: string, headerRow: string[], dataRows: string[][]): string => {
-	let output = `<div class="themed-table-container" style="max-width: 100%; max-height: 380px; overflow-y: auto;">`;
+	let output = `<div class="themed-table-container" 
+		style="max-width: 100%; max-height: 380px; overflow-y: auto; overflow-x: hidden;">`;
+	
 	output += `<h3 class="themed-table-title">${title}</h3>`;
-	output += `<table class="themed-table" style="width: 100%; border-collapse: collapse;">`;
+	
+	output += `<table class="themed-table" 
+		style="width: 100%; border-collapse: collapse; table-layout: fixed;">`;
+	
 	output += `<tr class="themed-table-header">`;
 	headerRow.forEach(header => { output += `<th>${header}</th>`; });
 	output += `</tr>`;
+	
 	dataRows.forEach(row => {
 		output += `<tr class="themed-table-row">`;
 		row.forEach(cell => { output += `<td>${cell}</td>`; });
 		output += `</tr>`;
 	});
+	
 	output += `</table></div>`;
 	return output;
 };
