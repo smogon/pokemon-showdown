e95d8fbff81928fc7671611f7ea4085a3c6ac299
ry <ryr4242@gmail.com>
2026-04-14 23:01:04 -0500

    update snooze

--- data/moves.ts
+++ data/moves.ts
@@ -23568,8 +23568,12 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 		heal: [1, 2],
 		onHit(target, source, move) {
 			for (const pokemon of this.getAllActive()) {
-				if (!(pokemon.volatiles['protect'] || pokemon.volatiles['banefulbunker'] || pokemon.volatiles['kingsshield'] ||
-				pokemon.volatiles['spikyshield'] || pokemon.side.getSideCondition('matblock') || pokemon.side.getSideCondition('wideguard'))) {
+				if (
+					!(pokemon.volatiles['protect'] || pokemon.volatiles['banefulbunker'] || pokemon.volatiles['kingsshield'] ||
+						pokemon.volatiles['spikyshield'] || pokemon.side.getSideCondition('matblock') || pokemon.volatiles['comradesarmor'] ||
+						pokemon.volatiles['shockshelter'] || pokemon.volatiles['frostification'] ||
+						pokemon.side.getSideCondition('wideguard'))
+				) {
 					if (!pokemon.status && pokemon.runStatusImmunity('slp') && !pokemon.hasAbility('soundproof')) {
 						pokemon.addVolatile('yawn');
 					}
