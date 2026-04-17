9f10b17b732a0758f2fc1e332d58e50cced0ea36
shrianshChari <30420527+shrianshChari@users.noreply.github.com>
2026-04-16 19:28:57 -0700

    Fix Swanna's tier too

--- data/mods/gen5/formats-data.ts
+++ data/mods/gen5/formats-data.ts
@@ -2162,7 +2162,7 @@ export const FormatsData: import('../../../sim/dex-species').ModdedSpeciesFormat
 		tier: "LC",
 	},
 	swanna: {
-		tier: "PU",
+		tier: "PUBL",
 		doublesTier: "DUU",
 	},
 	vanillite: {
