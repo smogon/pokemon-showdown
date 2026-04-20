a3d93956ec445de4c04c3a802e2dac730e17e7f3
André Bastos Dias <80102738+andrebastosdias@users.noreply.github.com>
2026-04-20 19:36:50 +0100

    Champions: Implement multi-hit vs Disguise effectiveness (#11688)
    * Simplify Tera Shell implementation
    
    * Champions: Implement multi-hit vs Disguise effectiveness
    
    * Add line breaks

--- data/mods/champions/abilities.ts
+++ data/mods/champions/abilities.ts
@@ -11,6 +11,26 @@ export const Abilities: import('../../../sim/dex-abilities').ModdedAbilityDataTa
 			this.effectState.checkedBerserk = !(effect.effectType === "Move" && !effect.multihit);
 		},
 	},
+	disguise: {
+		inherit: true,
+		onEffectiveness(typeMod, target, type, move) {
+			if (!target || move.category === 'Status') return;
+
+			if (move.hit === 1) delete this.effectState.neutral;
+			if (this.effectState.neutral) return 0;
+
+			if (!['mimikyu', 'mimikyutotem'].includes(target.species.id)) {
+				return;
+			}
+
+			const hitSub = target.volatiles['substitute'] && !move.flags['bypasssub'] && !(move.infiltrates && this.gen >= 6);
+			if (hitSub) return;
+
+			if (!target.runImmunity(move)) return;
+			this.effectState.neutral = true;
+			return 0;
+		},
+	},
 	dragonize: {
 		inherit: true,
 		isNonstandard: null,
