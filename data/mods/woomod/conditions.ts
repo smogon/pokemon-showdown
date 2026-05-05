4e03127f3be648c206b6f783eb23cad3732f877d
ry <ryr4242@gmail.com>
2026-05-04 21:39:08 -0500

    forced to do this

---
+++ data/mods/woomod/conditions.ts
@@ -0,0 +1,20 @@
+export const Conditions: import('../../../sim/dex-conditions').ModdedConditionDataTable = {
+	frz: {
+		onStart(target, source, sourceEffect) {
+			this.add('-message', `${target.name} was frozen!`);
+			this.hint(`(Freeze halves SpA + 1/16 chip, like in Pokemon Legends: Arceus)`);
+			if (sourceEffect && sourceEffect.effectType === 'Ability') {
+				this.add('-status', target, 'frz', '[from] ability: ' + sourceEffect.name, `[of] ${source}`);
+			} else {
+				this.add('-status', target, 'frz');
+			}
+		},
+		onResidualOrder: 10,
+		onResidual(pokemon) {
+			this.damage(pokemon.baseMaxhp / 16);
+		},
+		onModifySpA(spa, pokemon) {
+			return this.chainModify(0.5);
+		},
+	},
+};
