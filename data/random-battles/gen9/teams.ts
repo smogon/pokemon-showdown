abd73738db19f7816e55e54c11320cadcaf11d09
A Cake Wearing A Hat <carrotlobotomy@gmail.com>
2026-05-31 02:45:33 -0500

    do a mega invasion fix

--- data/random-battles/gen9/teams.ts
+++ data/random-battles/gen9/teams.ts
@@ -135,7 +135,7 @@ const MOVE_PAIRS = [
 
 /** Pokemon who always want priority STAB, and are fine with it as its only STAB move of that type */
 const PRIORITY_POKEMON = [
-	'breloom', 'brutebonnet', 'cacturne', 'honchkrow', 'mimikyu', 'ragingbolt', 'scizor',
+	'breloom', 'brutebonnet', 'cacturne', 'honchkrow', 'mimikyu', 'ragingbolt', 'scizor', 'scizormega',
 ];
 
 /** Pokemon who should never be in the lead slot */
