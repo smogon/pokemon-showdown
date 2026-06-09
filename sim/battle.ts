d9f736e462e8a73aad9861660c294febfe80cccd
André Bastos Dias <80102738+andrebastosdias@users.noreply.github.com>
2026-06-09 17:15:59 +0100

    Gens 3-4: Last move flag should be cleared if the moves fails because of a status/volatile
    https://www.smogon.com/forums/threads/past-gens-research-thread.3506992/post-11024393

--- sim/battle.ts
+++ sim/battle.ts
@@ -377,6 +377,8 @@ export class Battle {
 		if (this.activeMove) {
 			if (!failed) {
 				this.lastMove = this.activeMove;
+			} else if (this.gen === 3 || this.gen === 4) {
+				this.lastMove = null;
 			}
 			this.activeMove = null;
 			this.activePokemon = null;
