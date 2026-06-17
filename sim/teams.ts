96543b5ce0e09a99bcef3f4e00e51379c9c70dcc
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2026-06-17 16:24:10 -0600

    Update teams.ts

--- sim/teams.ts
+++ sim/teams.ts
@@ -642,7 +642,7 @@ export const Teams = new class Teams {
 		} else if (formatID.includes('gen9freeforallrandombattle')) {
 			TeamGenerator = require(`../data/random-battles/gen9ffa/teams`).default;
 		} else {
-			TeamGenerator = require(`../data/random-battles/${mod}/teams`).default;
+			TeamGenerator = require(`../data/random-battles/${mod.startsWith('champions') ? 'champions' : mod}/teams`).default;
 		}
 
 		return new TeamGenerator(format, seed);
