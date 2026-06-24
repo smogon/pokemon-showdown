951f3d84f5515251e815da1ff9f5421a5da1da00
Mia <49593536+mia-pi-git@users.noreply.github.com>
2026-06-23 23:29:13 -0500

    Fix crash in champs scripts

--- data/mods/champions/scripts.ts
+++ data/mods/champions/scripts.ts
@@ -190,7 +190,7 @@ export const Scripts: ModdedBattleScriptsData = {
 			if ((this.battle.gen <= 7 || this.battle.ruleTable.has('+tag:past') ||
 				this.battle.ruleTable.has('+tag:future')) &&
 				altForme?.isMega && altForme?.requiredMove &&
-				pokemon.baseMoves.includes(toID(altForme.requiredMove)) && !item.zMove) {
+				pokemon.baseMoves.includes(this.battle.toID(altForme.requiredMove)) && !item.zMove) {
 				return altForme.name;
 			}
 			return item.megaStone?.[species.name] || null;
