21c6dbc2c42131bbd224597756957ad15a7eca37
iforgetwhyimhere <thereal.quinzep@gmail.com>
2026-06-24 18:43:42 -0400

    fix typo in Dewgong's gen1stadiumrental set (#12129)

--- data/mods/gen1stadium/rulesets.ts
+++ data/mods/gen1stadium/rulesets.ts
@@ -451,7 +451,7 @@ export const Rulesets: import('../../../sim/dex-formats').ModdedFormatDataTable 
 			case 'Dewgong':
 				set.evs = { hp: 113, atk: 112, def: 112, spa: 112, spd: 112, spe: 112 };
 				set.ivs = { hp: 4, atk: 4, def: 4, spa: 4, spd: 4, spe: 6 };
-				set.moves = ['Aurora Beam', 'Heabutt', 'Rest', 'Surf'];
+				set.moves = ['Aurora Beam', 'Headbutt', 'Rest', 'Surf'];
 				break;
 			case 'Grimer':
 				set.evs = { hp: 145, atk: 144, def: 144, spa: 144, spd: 144, spe: 144 };
