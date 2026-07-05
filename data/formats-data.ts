a32d80f068068a88f642e163a6474b85fca5d6d9
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2026-07-05 17:05:05 -0600

    PU: Unban Inteleon for suspect

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -4836,7 +4836,7 @@ export const FormatsData: import('../sim/dex-species').SpeciesFormatsDataTable =
 		tier: "NFE",
 	},
 	inteleon: {
-		tier: "PUBL",
+		tier: "PU",
 		doublesTier: "(DUU)",
 		natDexTier: "RU",
 	},
