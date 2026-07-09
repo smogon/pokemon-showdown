13b17934109b5b88ccc69d0a069604f9955f435e
iforgetwhyimhere <thereal.quinzep@gmail.com>
2026-07-09 13:18:25 -0400

    Champions: Qwilfish can learn Barb Barrage

--- data/mods/champions/learnsets.ts
+++ data/mods/champions/learnsets.ts
@@ -3425,6 +3425,7 @@ export const Learnsets: import('../../../sim/dex-species').ModdedLearnsetDataTab
 			aquajet: ["9M"],
 			aquatail: ["9M"],
 			assurance: ["9M"],
+			barbbarrage: ["9M"],
 			blizzard: ["9M"],
 			chillingwater: ["9M"],
 			crunch: ["9M"],
