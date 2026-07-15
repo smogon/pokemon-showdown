367580dab68648a1fb828490a0ccabce10543a09
André Bastos Dias <dias.andre@siemens.com>
2026-07-15 18:19:33 +0100

    Fix interaction between Rock Head and Mind Blown

--- data/mods/gen4/scripts.ts
+++ data/mods/gen4/scripts.ts
@@ -205,7 +205,7 @@ export const Scripts: ModdedBattleScriptsData = {
 		applyRecoilDamage(damageDealt: number, move: Move, pokemon: Pokemon): number | null {
 			let recoilDamage = 0;
 			if (move.struggleRecoil) recoilDamage = this.battle.clampIntRange(Math.floor(pokemon.baseMaxhp / 4), 1);
-			else if (move.mindBlownRecoil) recoilDamage = Math.round(pokemon.maxhp / 2);
+			else if (move.mindBlownRecoil || move.chloroblastRecoil) recoilDamage = Math.round(pokemon.maxhp / 2);
 			else if (move.recoil) {
 				recoilDamage = this.battle.clampIntRange(Math.floor(damageDealt * move.recoil[0] / move.recoil[1]), 1);
 			} else return null;
@@ -213,7 +213,8 @@ export const Scripts: ModdedBattleScriptsData = {
 			if (move.struggleRecoil) {
 				this.battle.directDamage(recoilDamage, pokemon, pokemon, { id: 'strugglerecoil' } as Condition);
 			} else {
-				this.battle.damage(recoilDamage, pokemon, pokemon, 'recoil');
+				const effect = move.mindBlownRecoil ? this.dex.conditions.get(move.name) : 'recoil';
+				this.battle.damage(recoilDamage, pokemon, pokemon, effect);
 			}
 			return recoilDamage;
 		},
