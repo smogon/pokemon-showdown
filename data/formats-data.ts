267b80e8350077c1b4fb45940be067697a87c0cd
Chessking345 <chessking345@gmail.com>
2026-07-25 20:24:39 -0700

    PU: Reban Inteleon
    https://www.smogon.com/forums/threads/np-stage-18-pumped-up-kicks-inteleon-stays-banned.3784882/post-11067560

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -4836,7 +4836,7 @@ export const FormatsData: import('../sim/dex-species').SpeciesFormatsDataTable =
 		tier: "NFE",
 	},
 	inteleon: {
-		tier: "PU",
+		tier: "PUBL",
 		doublesTier: "(DUU)",
 		natDexTier: "RU",
 	},
