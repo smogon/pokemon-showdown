a374f2c29fb1a41293fadf1af4abeb186ffebd5d
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2026-08-01 16:09:23 -0600

    NDUU: Ban Blacephalon

--- data/formats-data.ts
+++ data/formats-data.ts
@@ -4774,7 +4774,7 @@ export const FormatsData: import('../sim/dex-species').SpeciesFormatsDataTable =
 	blacephalon: {
 		isNonstandard: "Past",
 		tier: "Illegal",
-		natDexTier: "UU",
+		natDexTier: "UUBL",
 	},
 	zeraora: {
 		isNonstandard: "Past",
