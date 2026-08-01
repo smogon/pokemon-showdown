6f17eb005d9d474b84f70e917435ed6f370ba1a7
iforgetwhyimhere <thereal.quinzep@gmail.com>
2026-08-01 15:54:40 -0400

    balance changes

---
+++ data/mods/gen9mnmlimitedsupply/pokedex.ts
@@ -0,0 +1,6 @@
+export const Pokedex: import('../../../sim/dex-species').ModdedSpeciesDataTable = {
+	sableyemega: {
+		inherit: true,
+		baseStats: { hp: 50, atk: 90, def: 120, spa: 80, spd: 110, spe: 30 },
+	}
+};
