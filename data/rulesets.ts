a320db0c92fea80c7fa69af9410164d1dc7d501e
Chessking345 <chessking345@gmail.com>
2026-08-04 12:06:54 -0700

    Add G-Max Move Clause

--- data/rulesets.ts
+++ data/rulesets.ts
@@ -1338,6 +1338,22 @@ export const Rulesets: import('../sim/dex-formats').FormatDataTable = {
 			this.add('rule', 'Z-Move Clause: Z-Moves are banned');
 		},
 	},
+	gmaxmoveclause: {
+		effectType: 'ValidatorRule',
+		name: "G-Max Move Clause",
+		desc: "Bans the use of G-Max moves",
+		banlist: [
+			'G-Max Befuddle', 'G-Max Cannonade', 'G-Max Centiferno', 'G-Max Chi Strike', 'G-Max Cuddle', 'G-Max Depletion',
+			'G-Max Drum Solo', 'G-Max Finale', 'G-Max Fireball', 'G-Max Foam Burst', 'G-Max Gold Rush', 'G-Max Gravitas',
+			'G-Max Hydrosnipe', 'G-Max Malodor', 'G-Max Meltdown', 'G-Max One Blow', 'G-Max Rapid Flow', 'G-Max Replenish',
+			'G-Max Resonance', 'G-Max Sandblast', 'G-Max Smite', 'G-Max Snooze', 'G-Max Steelsurge', 'G-Max Stonesurge',
+			'G-Max Stun Shock', 'G-Max Sweetness', 'G-Max Tartness', 'G-Max Terror', 'G-Max Vine Lash', 'G-Max Volcalith',
+			'G-Max Volt Crash', 'G-Max Wildfire', 'G-Max Wind Rage',
+		],
+		onBegin() {
+			this.add('rule', 'G-Max Move Clause: G-Max moves are banned');
+		},
+	},
 	notfullyevolved: {
 		effectType: 'ValidatorRule',
 		name: 'Not Fully Evolved',
