f0039f571384310ef99f64a0fb2a80d82eb60ac6
Karthik99999 <bandagondak0217@gmail.com>
2026-08-08 19:00:08 -0400

    Remove extra immune messages from Spicy Spray and Dire Claw

--- data/abilities.ts
+++ data/abilities.ts
@@ -4456,9 +4456,7 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 	spicyspray: {
 		isNonstandard: "Future",
 		onDamagingHit(damage, target, source, move) {
-			if (!source.trySetStatus('brn', target) && !source.status && source.hasType('Fire')) {
-				this.add('-immune', source);
-			}
+			source.trySetStatus('brn', target);
 		},
 		flags: {},
 		name: "Spicy Spray",
