ecac20e0d174d00467e7604cf09b0316eb74c4d7
André Bastos Dias <80102738+andrebastosdias@users.noreply.github.com>
2026-08-30 19:13:22 +0100

    Champions OU: Ban King's Rock (#12250)
    https://www.smogon.com/forums/threads/champions-ou-tiering-updates.3780556/post-11102568

--- config/formats.ts
+++ config/formats.ts
@@ -264,13 +264,13 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		name: "[Gen 9 Champions] OU",
 		mod: 'champions',
 		ruleset: ['Standard'],
-		banlist: ['AG', 'Uber', 'Moody', 'Damp Rock', 'Light Clay', 'Quick Claw', 'Baton Pass', 'Last Respects', 'Shed Tail'],
+		banlist: ['AG', 'Uber', 'Moody', 'Damp Rock', 'King\'s Rock', 'Light Clay', 'Quick Claw', 'Baton Pass', 'Last Respects', 'Shed Tail'],
 	},
 	{
 		name: "[Gen 9 Champions] UU",
 		mod: 'champions',
 		ruleset: ['[Gen 9 Champions] OU'],
-		banlist: ['OU', 'UUBL', 'Drought', 'King\'s Rock'],
+		banlist: ['OU', 'UUBL', 'Drought'],
 	},
 	{
 		name: "[Gen 9 Champions] BSS Reg M-A",
