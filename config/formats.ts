52c371433e52c6f6452d639d8043ea711859257f
Niadevv <niadeleloc@gmail.com>
2026-09-02 21:03:03 +0100

    Remove Slowbro + Slowbronite clause from Gen 9 National Dex UU

--- config/formats.ts
+++ config/formats.ts
@@ -3638,7 +3638,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		name: "[Gen 9] National Dex UU",
 		mod: 'gen9',
 		ruleset: ['[Gen 9] National Dex'],
-		banlist: ['ND OU', 'ND UUBL', 'Drizzle', 'Drought', 'Light Clay', 'Slowbro-Base + Slowbronite'],
+		banlist: ['ND OU', 'ND UUBL', 'Drizzle', 'Drought', 'Light Clay'],
 	},
 	{
 		name: "[Gen 9] National Dex RU",
