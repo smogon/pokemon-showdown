e91754b4160be637b8a9f506b833d11e6777f252
iforgetwhyimhere <thereal.quinzep@gmail.com>
2026-09-04 18:18:41 -0400

    Update data/mods/gen9deltamon/conditions.ts

--- data/mods/gen9deltamon/conditions.ts
+++ data/mods/gen9deltamon/conditions.ts
@@ -3,7 +3,7 @@ export const Conditions: import('../../../sim/dex-conditions').ModdedConditionDa
 		inherit: true,
 		onStart(target, source) {
 			this.effectState.targetSlot = target.getSlot();
-			this.effectState.endingTurn = (this.turn - 1) + (source.baseSpecies.id !== 'blue' ? 5 : 2);
+			this.effectState.endingTurn = (this.turn - 1) + (source.baseSpecies.id === 'aqua' ? 5 : 2);
 			if (this.effectState.endingTurn >= 254) {
 				this.hint(`In Gen 8+, Future attacks will never resolve when used on the 255th turn or later.`);
 			}
